b52ed81c68866e9792a1db46413c74a3f8d2be03
Martin Jones <martin.jones@nokia.com>
2011-05-27 12:06:20 +1000

    Revert "Make QSGTextNode back-end for QML's TextInput"
    This reverts commit e430f1336881850a73a54a291e0276160d568212.

--- src/declarative/items/qsgtextinput_p.h
+++ src/declarative/items/qsgtextinput_p.h
@@ -54,7 +54,7 @@ QT_MODULE(Declarative)
 
 class QSGTextInputPrivate;
 class QValidator;
-class Q_AUTOTEST_EXPORT QSGTextInput : public QSGImplicitSizeItem
+class Q_AUTOTEST_EXPORT QSGTextInput : public QSGImplicitSizePaintedItem
 {
     Q_OBJECT
     Q_ENUMS(HAlignment)
@@ -204,6 +204,7 @@ public:
 
     bool hasAcceptableInput() const;
 
+    void paint(QPainter *p);
     QVariant inputMethodQuery(Qt::InputMethodQuery property) const;
 
     QRectF boundingRect() const;
@@ -259,7 +260,6 @@ protected:
     bool event(QEvent *e);
     void focusInEvent(QFocusEvent *event);
     virtual void itemChange(ItemChange, const ItemChangeData &);
-    QSGNode *updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *data);
 
 public Q_SLOTS:
     void selectAll();
