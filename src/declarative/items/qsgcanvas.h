6b0af9fe9fd9114bd579510010c09519928f91ce
Gunnar Sletta <gunnar.sletta@nokia.com>
2011-06-06 08:05:56 +0200

    A better threaded renderer

--- src/declarative/items/qsgcanvas.h
+++ src/declarative/items/qsgcanvas.h
@@ -75,6 +75,8 @@ public:
 
     QSGEngine *sceneGraphEngine() const;
 
+    QImage grabFrameBuffer();
+
 Q_SIGNALS:
     void sceneGraphInitialized();
 
@@ -109,8 +111,6 @@ private Q_SLOTS:
 
 private:
     Q_DISABLE_COPY(QSGCanvas)
-    Q_PRIVATE_SLOT(d_func(), void _q_animationStarted())
-    Q_PRIVATE_SLOT(d_func(), void _q_animationStopped())
 };
 
 QT_END_NAMESPACE
