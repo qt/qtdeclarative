qt/qtdeclarative

commit 9b5f9c8056186c9cf3c40dbac66e5f90369339dc

Refactor ListView and GridView implementations
Places common code into QSGItemView.

Change-Id: Ic310dbe7e16774163ba393860da64a0da7d4ea0a
Reviewed-on: http://codereview.qt.nokia.com/1200
Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Bea Lam <bea.lam@nokia.com>

src/declarative/items/qsgitemview_p_p.h
@@ -1,0 +1,208 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtDeclarative module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QSGITEMVIEW_P_P_H
+#define QSGITEMVIEW_P_P_H
+
+#include "qsgitemview_p.h"
+#include "qsgflickable_p_p.h"
+#include "qsgvisualitemmodel_p.h"
+
+
+QT_BEGIN_HEADER
+
+QT_BEGIN_NAMESPACE
+
+QT_MODULE(Declarative)
+
+class FxViewItem
+{
+public:
+    FxViewItem(QSGItem *, bool own);
+    ~FxViewItem();
+
+    // these are positions and sizes along the current direction of scrolling/flicking
+    virtual qreal position() const = 0;
+    virtual qreal endPosition() const = 0;
+    virtual qreal size() const = 0;
+    virtual qreal sectionSize() const = 0;
+
+    virtual bool contains(qreal x, qreal y) const = 0;
+
+    QSGItem *item;
+    bool ownItem;
+    int index;
+    QSGItemViewAttached *attached;
+};
+
+class QSGItemViewPrivate : public QSGFlickablePrivate
+{
+    Q_DECLARE_PUBLIC(QSGItemView)
+public:
+    QSGItemViewPrivate();
+
+    enum BufferMode { NoBuffer = 0x00, BufferBefore = 0x01, BufferAfter = 0x02 };
+    enum MovementReason { Other, SetIndex, Mouse };
+
+    bool isValid() const;
+    qreal position() const;
+    qreal size() const;
+    int findLastVisibleIndex(int defaultValue = -1) const;
+    FxViewItem *visibleItem(int modelIndex) const;
+    FxViewItem *firstVisibleItem() const;
+    int mapFromModel(int modelIndex) const;
+
+    virtual void init();
+    virtual void updateCurrent(int modelIndex);
+    virtual void clear();
+    virtual void regenerate();
+    virtual void updateViewport();
+    void layout();
+    void refill();
+    void refill(qreal from, qreal to, bool doBuffer = false);
+    void scheduleLayout();
+    void mirrorChange();
+
+    FxViewItem *createItem(int modelIndex);
+    virtual void releaseItem(FxViewItem *item);
+
+    QSGItem *createHighlightItem();
+    QSGItem *createComponentItem(QDeclarativeComponent *component, bool receiveItemGeometryChanges, bool createDefault = false);
+
+    void updateTrackedItem();
+    void updateUnrequestedIndexes();
+    void updateUnrequestedPositions();
+    void positionViewAtIndex(int index, int mode);
+
+    void checkVisible() const;
+
+    QDeclarativeGuard<QSGVisualModel> model;
+    QVariant modelVariant;
+    int itemCount;
+    int buffer;
+    int bufferMode;
+    Qt::LayoutDirection layoutDirection;
+
+    MovementReason moveReason;
+
+    QList<FxViewItem *> visibleItems;
+    int visibleIndex;
+    int currentIndex;
+    FxViewItem *currentItem;
+    FxViewItem *trackedItem;
+    QHash<QSGItem*,int> unrequestedItems;
+    int requestedIndex;
+
+    // XXX split into struct
+    QDeclarativeComponent *highlightComponent;
+    FxViewItem *highlight;
+    int highlightRange;     // enum value
+    qreal highlightRangeStart;
+    qreal highlightRangeEnd;
+    int highlightMoveDuration;
+
+    QDeclarativeComponent *headerComponent;
+    FxViewItem *header;
+    QDeclarativeComponent *footerComponent;
+    FxViewItem *footer;
+
+    mutable qreal minExtent;
+    mutable qreal maxExtent;
+
+    bool ownModel : 1;
+    bool wrap : 1;
+    bool lazyRelease : 1;
+    bool deferredRelease : 1;
+    bool layoutScheduled : 1;
+    bool inViewportMoved : 1;
+    bool currentIndexCleared : 1;
+    bool haveHighlightRange : 1;
+    bool autoHighlight : 1;
+    bool highlightRangeStartValid : 1;
+    bool highlightRangeEndValid : 1;
+    mutable bool minExtentDirty : 1;
+    mutable bool maxExtentDirty : 1;
+
+protected:
+    virtual Qt::Orientation layoutOrientation() const = 0;
+    virtual bool isContentFlowReversed() const = 0;
+
+    virtual qreal startPosition() const = 0;
+    virtual qreal positionAt(int index) const = 0;
+    virtual qreal endPosition() const = 0;
+    virtual qreal endPositionAt(int index) const = 0;
+    virtual qreal lastPosition() const  = 0;
+
+    virtual qreal headerSize() const = 0;
+    virtual qreal footerSize() const = 0;
+    virtual void updateHeader() = 0;
+    virtual void updateFooter() = 0;
+
+    virtual void createHighlight() = 0;
+    virtual void updateHighlight() = 0;
+    virtual void resetHighlightPosition() = 0;
+
+    virtual void setPosition(qreal pos) = 0;
+    virtual void fixupPosition() = 0;
+
+    virtual bool addVisibleItems(int fillFrom, int fillTo, bool doBuffer) = 0;
+    virtual bool removeNonVisibleItems(int bufferFrom, int bufferTo) = 0;
+    virtual void visibleItemsChanged() = 0;
+
+    virtual FxViewItem *newViewItem(int index, QSGItem *item) = 0;
+    virtual void initializeViewItem(FxViewItem *) {}
+    virtual void repositionPackageItemAt(QSGItem *item, int index) = 0;
+
+    virtual void layoutVisibleItems() = 0;
+
+    virtual void updateSections() {}
+    virtual void changedVisibleIndex(int newIndex) = 0;
+    virtual void initializeCurrentItem() {}
+
+    virtual void itemGeometryChanged(QSGItem *item, const QRectF &newGeometry, const QRectF &oldGeometry);
+};
+
+
+QT_END_NAMESPACE
+
+QT_END_HEADER
+
+#endif // QSGITEMVIEW_P_P_H

src/declarative/items/items.pri
@@ -66,6 +66,8 @@ HEADERS += \
     $$PWD/qsgspriteimage_p.h \
     $$PWD/qsgevent.h \
     $$PWD/qsgdragtarget_p.h \
+    $$PWD/qsgitemview_p.h \
+    $$PWD/qsgitemview_p_p.h
 
 SOURCES += \
     $$PWD/qsgevents.cpp \
@@ -109,6 +111,7 @@ SOURCES += \
     $$PWD/qsgsprite.cpp \
     $$PWD/qsgspriteimage.cpp \
     $$PWD/qsgdragtarget.cpp \
+    $$PWD/qsgitemview.cpp
 
 SOURCES += \
     $$PWD/qsgshadereffectitem.cpp \

src/declarative/items/qsggridview_p.h
@@ -43,7 +43,7 @@
 #ifndef QSGGRIDVIEW_P_H
 #define QSGGRIDVIEW_P_H
 
-#include "qsgflickable_p.h"
+#include "qsgitemview_p.h"
 
 #include <private/qdeclarativeguard_p.h>
 
@@ -55,97 +55,31 @@ QT_MODULE(Declarative)
 class QSGVisualModel;
 class QSGGridViewAttached;
 class QSGGridViewPrivate;
-class Q_AUTOTEST_EXPORT QSGGridView : public QSGFlickable
+class Q_AUTOTEST_EXPORT QSGGridView : public QSGItemView
 {
     Q_OBJECT
     Q_DECLARE_PRIVATE(QSGGridView)
 
-    Q_PROPERTY(QVariant model READ model WRITE setModel NOTIFY modelChanged)
-    Q_PROPERTY(QDeclarativeComponent *delegate READ delegate WRITE setDelegate NOTIFY delegateChanged)
-    Q_PROPERTY(int currentIndex READ currentIndex WRITE setCurrentIndex NOTIFY currentIndexChanged)
-    Q_PROPERTY(QSGItem *currentItem READ currentItem NOTIFY currentIndexChanged)
-    Q_PROPERTY(int count READ count NOTIFY countChanged)
-
-    Q_PROPERTY(QDeclarativeComponent *highlight READ highlight WRITE setHighlight NOTIFY highlightChanged)
-    Q_PROPERTY(QSGItem *highlightItem READ highlightItem NOTIFY highlightItemChanged)
-    Q_PROPERTY(bool highlightFollowsCurrentItem READ highlightFollowsCurrentItem WRITE setHighlightFollowsCurrentItem)
-    Q_PROPERTY(int highlightMoveDuration READ highlightMoveDuration WRITE setHighlightMoveDuration NOTIFY highlightMoveDurationChanged)
-
-    Q_PROPERTY(qreal preferredHighlightBegin READ preferredHighlightBegin WRITE setPreferredHighlightBegin NOTIFY preferredHighlightBeginChanged RESET resetPreferredHighlightBegin)
-    Q_PROPERTY(qreal preferredHighlightEnd READ preferredHighlightEnd WRITE setPreferredHighlightEnd NOTIFY preferredHighlightEndChanged RESET resetPreferredHighlightEnd)
-    Q_PROPERTY(HighlightRangeMode highlightRangeMode READ highlightRangeMode WRITE setHighlightRangeMode NOTIFY highlightRangeModeChanged)
-
     Q_PROPERTY(Flow flow READ flow WRITE setFlow NOTIFY flowChanged)
-    Q_PROPERTY(Qt::LayoutDirection layoutDirection READ layoutDirection WRITE setLayoutDirection NOTIFY layoutDirectionChanged)
-    Q_PROPERTY(Qt::LayoutDirection effectiveLayoutDirection READ effectiveLayoutDirection NOTIFY effectiveLayoutDirectionChanged)
-    Q_PROPERTY(bool keyNavigationWraps READ isWrapEnabled WRITE setWrapEnabled NOTIFY keyNavigationWrapsChanged)
-    Q_PROPERTY(int cacheBuffer READ cacheBuffer WRITE setCacheBuffer NOTIFY cacheBufferChanged)
     Q_PROPERTY(int cellWidth READ cellWidth WRITE setCellWidth NOTIFY cellWidthChanged)
     Q_PROPERTY(int cellHeight READ cellHeight WRITE setCellHeight NOTIFY cellHeightChanged)
 
     Q_PROPERTY(SnapMode snapMode READ snapMode WRITE setSnapMode NOTIFY snapModeChanged)
 
-    Q_PROPERTY(QDeclarativeComponent *header READ header WRITE setHeader NOTIFY headerChanged)
-    Q_PROPERTY(QDeclarativeComponent *footer READ footer WRITE setFooter NOTIFY footerChanged)
-
-    Q_ENUMS(HighlightRangeMode)
     Q_ENUMS(SnapMode)
     Q_ENUMS(Flow)
-    Q_ENUMS(PositionMode)
     Q_CLASSINFO("DefaultProperty", "data")
 
 public:
     QSGGridView(QSGItem *parent=0);
     ~QSGGridView();
 
-    QVariant model() const;
-    int modelCount() const;
-    void setModel(const QVariant &);
-
-    QDeclarativeComponent *delegate() const;
-    void setDelegate(QDeclarativeComponent *);
-
-    int currentIndex() const;
-    void setCurrentIndex(int idx);
-
-    QSGItem *currentItem();
-    QSGItem *highlightItem();
-    int count() const;
-
-    QDeclarativeComponent *highlight() const;
-    void setHighlight(QDeclarativeComponent *highlight);
-
-    bool highlightFollowsCurrentItem() const;
-    void setHighlightFollowsCurrentItem(bool);
-
-    int highlightMoveDuration() const;
-    void setHighlightMoveDuration(int);
-
-    enum HighlightRangeMode { NoHighlightRange, ApplyRange, StrictlyEnforceRange };
-    HighlightRangeMode highlightRangeMode() const;
-    void setHighlightRangeMode(HighlightRangeMode mode);
-
-    qreal preferredHighlightBegin() const;
-    void setPreferredHighlightBegin(qreal);
-    void resetPreferredHighlightBegin();
-
-    qreal preferredHighlightEnd() const;
-    void setPreferredHighlightEnd(qreal);
-    void resetPreferredHighlightEnd();
-
-    Qt::LayoutDirection layoutDirection() const;
-    void setLayoutDirection(Qt::LayoutDirection);
-    Qt::LayoutDirection effectiveLayoutDirection() const;
+    virtual void setHighlightFollowsCurrentItem(bool);
+    virtual void setHighlightMoveDuration(int);
 
     enum Flow { LeftToRight, TopToBottom };
     Flow flow() const;
     void setFlow(Flow);
-
-    bool isWrapEnabled() const;
-    void setWrapEnabled(bool);
-
-    int cacheBuffer() const;
-    void setCacheBuffer(int);
 
     int cellWidth() const;
     void setCellWidth(int);
@@ -157,22 +91,6 @@ public:
     SnapMode snapMode() const;
     void setSnapMode(SnapMode mode);
 
-    QDeclarativeComponent *footer() const;
-    void setFooter(QDeclarativeComponent *);
-
-    QDeclarativeComponent *header() const;
-    void setHeader(QDeclarativeComponent *);
-
-    virtual void setContentX(qreal pos);
-    virtual void setContentY(qreal pos);
-
-    enum PositionMode { Beginning, Center, End, Visible, Contain };
-
-    Q_INVOKABLE void positionViewAtIndex(int index, int mode);
-    Q_INVOKABLE int indexAt(qreal x, qreal y) const;
-    Q_INVOKABLE void positionViewAtBeginning();
-    Q_INVOKABLE void positionViewAtEnd();
-
     static QSGGridViewAttached *qmlAttachedProperties(QObject *);
 
 public Q_SLOTS:
@@ -182,58 +100,28 @@ public Q_SLOTS:
     void moveCurrentIndexRight();
 
 Q_SIGNALS:
-    void countChanged();
-    void currentIndexChanged();
     void cellWidthChanged();
     void cellHeightChanged();
-    void highlightChanged();
-    void highlightItemChanged();
-    void preferredHighlightBeginChanged();
-    void preferredHighlightEndChanged();
-    void highlightRangeModeChanged();
     void highlightMoveDurationChanged();
-    void modelChanged();
-    void delegateChanged();
     void flowChanged();
-    void layoutDirectionChanged();
-    void effectiveLayoutDirectionChanged();
-    void keyNavigationWrapsChanged();
-    void cacheBufferChanged();
     void snapModeChanged();
-    void headerChanged();
-    void footerChanged();
 
 protected:
-    virtual void updatePolish();
     virtual void viewportMoved();
-    virtual qreal minYExtent() const;
-    virtual qreal maxYExtent() const;
-    virtual qreal minXExtent() const;
-    virtual qreal maxXExtent() const;
     virtual void keyPressEvent(QKeyEvent *);
-    virtual void componentComplete();
 
 private Q_SLOTS:
-    void trackedPositionChanged();
     void itemsInserted(int index, int count);
     void itemsRemoved(int index, int count);
     void itemsMoved(int from, int to, int count);
-    void modelReset();
-    void destroyRemoved();
-    void createdItem(int index, QSGItem *item);
-    void destroyingItem(QSGItem *item);
-    void animStopped();
-
-private:
-    void refill();
 };
 
-class QSGGridViewAttached : public QObject
+class QSGGridViewAttached : public QSGItemViewAttached
 {
     Q_OBJECT
 public:
     QSGGridViewAttached(QObject *parent)
-        : QObject(parent), m_view(0), m_isCurrent(false), m_delayRemove(false) {}
+        : QSGItemViewAttached(parent), m_view(0) {}
     ~QSGGridViewAttached() {}
 
     Q_PROPERTY(QSGGridView *view READ view NOTIFY viewChanged)
@@ -245,38 +133,11 @@ public:
         }
     }
 
-    Q_PROPERTY(bool isCurrentItem READ isCurrentItem NOTIFY currentItemChanged)
-    bool isCurrentItem() const { return m_isCurrent; }
-    void setIsCurrentItem(bool c) {
-        if (m_isCurrent != c) {
-            m_isCurrent = c;
-            emit currentItemChanged();
-        }
-    }
-
-    Q_PROPERTY(bool delayRemove READ delayRemove WRITE setDelayRemove NOTIFY delayRemoveChanged)
-    bool delayRemove() const { return m_delayRemove; }
-    void setDelayRemove(bool delay) {
-        if (m_delayRemove != delay) {
-            m_delayRemove = delay;
-            emit delayRemoveChanged();
-        }
-    }
-
-    void emitAdd() { emit add(); }
-    void emitRemove() { emit remove(); }
-
 Q_SIGNALS:
-    void currentItemChanged();
-    void delayRemoveChanged();
-    void add();
-    void remove();
     void viewChanged();
 
 public:
     QDeclarativeGuard<QSGGridView> m_view;
-    bool m_isCurrent : 1;
-    bool m_delayRemove : 1;
 };
 
 

src/declarative/items/qsgitemview_p.h
@@ -1,0 +1,285 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the QtDeclarative module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** No Commercial Usage
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**
+**
+**
+**
+**
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QSGITEMVIEW_P_H
+#define QSGITEMVIEW_P_H
+
+#include "qsgflickable_p.h"
+
+QT_BEGIN_HEADER
+
+QT_BEGIN_NAMESPACE
+
+QT_MODULE(Declarative)
+
+
+class QSGItemViewPrivate;
+
+class Q_AUTOTEST_EXPORT QSGItemView : public QSGFlickable
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QVariant model READ model WRITE setModel NOTIFY modelChanged)
+    Q_PROPERTY(QDeclarativeComponent *delegate READ delegate WRITE setDelegate NOTIFY delegateChanged)
+    Q_PROPERTY(int count READ count NOTIFY countChanged)
+
+    Q_PROPERTY(int currentIndex READ currentIndex WRITE setCurrentIndex NOTIFY currentIndexChanged)
+    Q_PROPERTY(QSGItem *currentItem READ currentItem NOTIFY currentIndexChanged)
+
+    Q_PROPERTY(bool keyNavigationWraps READ isWrapEnabled WRITE setWrapEnabled NOTIFY keyNavigationWrapsChanged)
+    Q_PROPERTY(int cacheBuffer READ cacheBuffer WRITE setCacheBuffer NOTIFY cacheBufferChanged)
+
+    Q_PROPERTY(Qt::LayoutDirection layoutDirection READ layoutDirection WRITE setLayoutDirection NOTIFY layoutDirectionChanged)
+    Q_PROPERTY(Qt::LayoutDirection effectiveLayoutDirection READ effectiveLayoutDirection NOTIFY effectiveLayoutDirectionChanged)
+
+    Q_PROPERTY(QDeclarativeComponent *header READ header WRITE setHeader NOTIFY headerChanged)
+    Q_PROPERTY(QDeclarativeComponent *footer READ footer WRITE setFooter NOTIFY footerChanged)
+
+    Q_PROPERTY(QDeclarativeComponent *highlight READ highlight WRITE setHighlight NOTIFY highlightChanged)
+    Q_PROPERTY(QSGItem *highlightItem READ highlightItem NOTIFY highlightItemChanged)
+    Q_PROPERTY(bool highlightFollowsCurrentItem READ highlightFollowsCurrentItem WRITE setHighlightFollowsCurrentItem NOTIFY highlightFollowsCurrentItemChanged)
+    Q_PROPERTY(HighlightRangeMode highlightRangeMode READ highlightRangeMode WRITE setHighlightRangeMode NOTIFY highlightRangeModeChanged)
+    Q_PROPERTY(qreal preferredHighlightBegin READ preferredHighlightBegin WRITE setPreferredHighlightBegin NOTIFY preferredHighlightBeginChanged RESET resetPreferredHighlightBegin)
+    Q_PROPERTY(qreal preferredHighlightEnd READ preferredHighlightEnd WRITE setPreferredHighlightEnd NOTIFY preferredHighlightEndChanged RESET resetPreferredHighlightEnd)
+    Q_PROPERTY(int highlightMoveDuration READ highlightMoveDuration WRITE setHighlightMoveDuration NOTIFY highlightMoveDurationChanged)
+
+    Q_ENUMS(HighlightRangeMode)
+    Q_ENUMS(PositionMode)
+
+public:
+    QSGItemView(QSGFlickablePrivate &dd, QSGItem *parent = 0);
+    ~QSGItemView();
+
+    QVariant model() const;
+    void setModel(const QVariant &);
+
+    QDeclarativeComponent *delegate() const;
+    void setDelegate(QDeclarativeComponent *);
+
+    int count() const;
+
+    int currentIndex() const;
+    void setCurrentIndex(int idx);
+
+    QSGItem *currentItem() const;
+
+    bool isWrapEnabled() const;
+    void setWrapEnabled(bool);
+
+    int cacheBuffer() const;
+    void setCacheBuffer(int);
+
+    Qt::LayoutDirection layoutDirection() const;
+    void setLayoutDirection(Qt::LayoutDirection);
+    Qt::LayoutDirection effectiveLayoutDirection() const;
+
+    QDeclarativeComponent *footer() const;
+    void setFooter(QDeclarativeComponent *);
+
+    QDeclarativeComponent *header() const;
+    void setHeader(QDeclarativeComponent *);
+
+    QDeclarativeComponent *highlight() const;
+    void setHighlight(QDeclarativeComponent *);
+
+    QSGItem *highlightItem() const;
+
+    bool highlightFollowsCurrentItem() const;
+    virtual void setHighlightFollowsCurrentItem(bool);
+
+    enum HighlightRangeMode { NoHighlightRange, ApplyRange, StrictlyEnforceRange };
+    HighlightRangeMode highlightRangeMode() const;
+    void setHighlightRangeMode(HighlightRangeMode mode);
+
+    qreal preferredHighlightBegin() const;
+    void setPreferredHighlightBegin(qreal);
+    void resetPreferredHighlightBegin();
+
+    qreal preferredHighlightEnd() const;
+    void setPreferredHighlightEnd(qreal);
+    void resetPreferredHighlightEnd();
+
+    int highlightMoveDuration() const;
+    virtual void setHighlightMoveDuration(int);
+
+    enum PositionMode { Beginning, Center, End, Visible, Contain };
+
+    Q_INVOKABLE void positionViewAtIndex(int index, int mode);
+    Q_INVOKABLE int indexAt(qreal x, qreal y) const;
+    Q_INVOKABLE void positionViewAtBeginning();
+    Q_INVOKABLE void positionViewAtEnd();
+
+    virtual void setContentX(qreal pos);
+    virtual void setContentY(qreal pos);
+
+signals:
+    void modelChanged();
+    void delegateChanged();
+    void countChanged();
+    void currentIndexChanged();
+
+    void keyNavigationWrapsChanged();
+    void cacheBufferChanged();
+
+    void layoutDirectionChanged();
+    void effectiveLayoutDirectionChanged();
+
+    void headerChanged();
+    void footerChanged();
+
+    void highlightChanged();
+    void highlightItemChanged();
+    void highlightFollowsCurrentItemChanged();
+    void highlightRangeModeChanged();
+    void preferredHighlightBeginChanged();
+    void preferredHighlightEndChanged();
+    void highlightMoveDurationChanged();
+
+protected:
+    virtual void updatePolish();
+    virtual void componentComplete();
+    virtual void geometryChanged(const QRectF &newGeometry, const QRectF &oldGeometry);
+    virtual qreal minYExtent() const;
+    virtual qreal maxYExtent() const;
+    virtual qreal minXExtent() const;
+    virtual qreal maxXExtent() const;
+
+protected slots:
+    virtual void updateSections() {}
+    void destroyRemoved();
+    void itemsChanged(int index, int count);
+    void createdItem(int index, QSGItem *item);
+    void modelReset();
+    void destroyingItem(QSGItem *item);
+    void animStopped();
+    void trackedPositionChanged();
+
+private:
+    Q_DECLARE_PRIVATE(QSGItemView)
+};
+
+
+class Q_AUTOTEST_EXPORT QSGItemViewAttached : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(bool isCurrentItem READ isCurrentItem NOTIFY currentItemChanged)
+    Q_PROPERTY(bool delayRemove READ delayRemove WRITE setDelayRemove NOTIFY delayRemoveChanged)
+
+    Q_PROPERTY(QString section READ section NOTIFY sectionChanged)
+    Q_PROPERTY(QString previousSection READ prevSection NOTIFY prevSectionChanged)
+    Q_PROPERTY(QString nextSection READ nextSection NOTIFY nextSectionChanged)
+
+public:
+    QSGItemViewAttached(QObject *parent)
+        : QObject(parent), m_isCurrent(false), m_delayRemove(false) {}
+    ~QSGItemViewAttached() {}
+
+    bool isCurrentItem() const { return m_isCurrent; }
+    void setIsCurrentItem(bool c) {
+        if (m_isCurrent != c) {
+            m_isCurrent = c;
+            emit currentItemChanged();
+        }
+    }
+
+    bool delayRemove() const { return m_delayRemove; }
+    void setDelayRemove(bool delay) {
+        if (m_delayRemove != delay) {
+            m_delayRemove = delay;
+            emit delayRemoveChanged();
+        }
+    }
+
+    QString section() const { return m_section; }
+    void setSection(const QString &sect) {
+        if (m_section != sect) {
+            m_section = sect;
+            emit sectionChanged();
+        }
+    }
+
+    QString prevSection() const { return m_prevSection; }
+    void setPrevSection(const QString &sect) {
+        if (m_prevSection != sect) {
+            m_prevSection = sect;
+            emit prevSectionChanged();
+        }
+    }
+
+    QString nextSection() const { return m_nextSection; }
+    void setNextSection(const QString &sect) {
+        if (m_nextSection != sect) {
+            m_nextSection = sect;
+            emit nextSectionChanged();
+        }
+    }
+
+    void emitAdd() { emit add(); }
+    void emitRemove() { emit remove(); }
+
+signals:
+    void currentItemChanged();
+    void delayRemoveChanged();
+
+    void add();
+    void remove();
+
+    void sectionChanged();
+    void prevSectionChanged();
+    void nextSectionChanged();
+
+public:
+    bool m_isCurrent : 1;
+    bool m_delayRemove : 1;
+
+    // current only used by list view
+    mutable QString m_section;
+    QString m_prevSection;
+    QString m_nextSection;
+};
+
+
+QT_END_NAMESPACE
+QT_END_HEADER
+
+#endif // QSGITEMVIEW_P_H
+

commit 9dfd621aec9a031297c50dd7df5ca90c771f46a3

Call base class init() from QSGListViewPrivate
Regression from 9b5f9c8056186c9cf3c40dbac66e5f90369339dc when
QSGItemView was created.

Also fixed other reimplemented methods in QSGListViewPrivate to call
their base class functions (which are currently empty) and changed
some virtual methods in QSGItemViewPrivate to non-virtual where they
didn't need to be virtual.

Task-number: QTBUG-20679
Change-Id: I489593b3eab4f5bbb4126906f170342d1e8811cd
Reviewed-on: http://codereview.qt.nokia.com/2462
Reviewed-by: Bea Lam <bea.lam@nokia.com>
Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>

src/declarative/items/qsgitemview_p_p.h
@@ -95,10 +95,10 @@ public:
     void adjustMoveParameters(int *from, int *to, int *count) const;
 
     virtual void init();
-    virtual void updateCurrent(int modelIndex);
     virtual void clear();
-    virtual void regenerate();
     virtual void updateViewport();
+
+    void regenerate();
     void layout();
     void refill();
     void refill(qreal from, qreal to, bool doBuffer = false);
@@ -111,6 +111,7 @@ public:
     QSGItem *createHighlightItem();
     QSGItem *createComponentItem(QDeclarativeComponent *component, bool receiveItemGeometryChanges, bool createDefault = false);
 
+    void updateCurrent(int modelIndex);
     void updateTrackedItem();
     void updateUnrequestedIndexes();
     void updateUnrequestedPositions();
@@ -191,14 +192,14 @@ protected:
     virtual void visibleItemsChanged() = 0;
 
     virtual FxViewItem *newViewItem(int index, QSGItem *item) = 0;
-    virtual void initializeViewItem(FxViewItem *) {}
     virtual void repositionPackageItemAt(QSGItem *item, int index) = 0;
 
     virtual void layoutVisibleItems() = 0;
-
-    virtual void updateSections() {}
     virtual void changedVisibleIndex(int newIndex) = 0;
+
+    virtual void initializeViewItem(FxViewItem *) {}
     virtual void initializeCurrentItem() {}
+    virtual void updateSections() {}
 
     virtual void itemGeometryChanged(QSGItem *item, const QRectF &newGeometry, const QRectF &oldGeometry);
 };

src/declarative/items/qsglistview.cpp
@@ -455,6 +455,7 @@ void QSGListViewPrivate::changedVisibleIndex(int newIndex)
 
 void QSGListViewPrivate::init()
 {
+    QSGItemViewPrivate::init();
     ::memset(sectionCache, 0, sizeof(QSGItem*) * sectionCacheSize);
 }
 
@@ -498,6 +499,8 @@ FxViewItem *QSGListViewPrivate::newViewItem(int modelIndex, QSGItem *item)
 
 void QSGListViewPrivate::initializeViewItem(FxViewItem *item)
 {
+    QSGItemViewPrivate::initializeViewItem(item);
+
     QSGItemPrivate *itemPrivate = QSGItemPrivate::get(item->item);
     itemPrivate->addItemChangeListener(this, QSGItemPrivate::Geometry);
 
@@ -815,6 +818,8 @@ void QSGListViewPrivate::createSection(FxListItemSG *listItem)
 
 void QSGListViewPrivate::updateSections()
 {
+    QSGItemViewPrivate::updateSections();
+
     if (sectionCriteria && !visibleItems.isEmpty()) {
         QString prevSection;
         if (visibleIndex > 0)
@@ -871,6 +876,8 @@ void QSGListViewPrivate::updateCurrentSection()
 
 void QSGListViewPrivate::initializeCurrentItem()
 {
+    QSGItemViewPrivate::initializeCurrentItem();
+
     if (currentItem) {
         FxListItemSG *listItem = static_cast<FxListItemSG *>(currentItem);
 