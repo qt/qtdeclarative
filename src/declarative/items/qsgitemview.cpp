qt/qtdeclarative

commit 1dd8b509074ba60da671f7671f8cf09c3fc001ae

Remove scheduleLayout() and layoutScheduled
No longer necessary since views now call QSGItem::polish() which
does its own layout scheduling and only does layouts when
necessary

Change-Id: I7d265fcf4177344c4ba10600b551a5545284316f
Reviewed-on: http://codereview.qt.nokia.com/3843
Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Martin Jones <martin.jones@nokia.com>

src/declarative/items/qsgitemview.cpp
@@ -786,7 +786,7 @@ void QSGItemView::itemsInserted(int index, int count)
 
     d->currentChanges.prepare(d->currentIndex, d->itemCount);
     d->currentChanges.doInsert(index, count);
-    d->scheduleLayout();
+    polish();
 }
 
 void QSGItemView::itemsRemoved(int index, int count)
@@ -797,7 +797,7 @@ void QSGItemView::itemsRemoved(int index, int count)
 
     d->currentChanges.prepare(d->currentIndex, d->itemCount);
     d->currentChanges.doRemove(index, count);
-    d->scheduleLayout();
+    polish();
 }
 
 void QSGItemView::itemsMoved(int from, int to, int count)
@@ -811,7 +811,7 @@ void QSGItemView::itemsMoved(int from, int to, int count)
 
     d->currentChanges.prepare(d->currentIndex, d->itemCount);
     d->currentChanges.doMove(from, to, count);
-    d->scheduleLayout();
+    polish();
 }
 
 void QSGItemView::itemsChanged(int index, int count)
@@ -822,7 +822,7 @@ void QSGItemView::itemsChanged(int index, int count)
 
     d->currentChanges.prepare(d->currentIndex, d->itemCount);
     d->currentChanges.doChange(index, count);
-    d->scheduleLayout();
+    polish();
 }
 
 void QSGItemView::modelReset()
@@ -1166,7 +1166,7 @@ QSGItemViewPrivate::QSGItemViewPrivate()
     , headerComponent(0), header(0), footerComponent(0), footer(0)
     , minExtent(0), maxExtent(0)
     , ownModel(false), wrap(false), lazyRelease(false), deferredRelease(false)
-    , layoutScheduled(false), inApplyModelChanges(false), inViewportMoved(false), forceLayout(false), currentIndexCleared(false)
+    , inApplyModelChanges(false), inViewportMoved(false), forceLayout(false), currentIndexCleared(false)
     , haveHighlightRange(false), autoHighlight(true), highlightRangeStartValid(false), highlightRangeEndValid(false)
     , minExtentDirty(true), maxExtentDirty(true)
 {
@@ -1406,15 +1406,6 @@ void QSGItemViewPrivate::regenerate()
     }
 }
 
-void QSGItemViewPrivate::scheduleLayout()
-{
-    Q_Q(QSGItemView);
-    if (!layoutScheduled) {
-        layoutScheduled = true;
-        q->polish();
-    }
-}
-
 void QSGItemViewPrivate::updateViewport()
 {
     Q_Q(QSGItemView);
@@ -1429,7 +1420,6 @@ void QSGItemViewPrivate::updateViewport()
 void QSGItemViewPrivate::layout()
 {
     Q_Q(QSGItemView);
-    layoutScheduled = false;
     if (!isValid() && !visibleItems.count()) {
         clear();
         setPosition(contentStartPosition());

src/declarative/items/qsggridview.cpp
@@ -774,7 +774,7 @@ void QSGGridViewPrivate::itemGeometryChanged(QSGItem *item, const QRectF &newGeo
     if (item == q) {
         if (newGeometry.height() != oldGeometry.height() || newGeometry.width() != oldGeometry.width()) {
             updateViewport();
-            scheduleLayout();
+            q->polish();
         }
     }
 }

src/declarative/items/qsgitemview_p_p.h
@@ -126,7 +126,6 @@ public:
     void layout();
     void refill();
     void refill(qreal from, qreal to, bool doBuffer = false);
-    void scheduleLayout();
     void mirrorChange();
 
     FxViewItem *createItem(int modelIndex);
@@ -184,7 +183,6 @@ public:
     bool wrap : 1;
     bool lazyRelease : 1;
     bool deferredRelease : 1;
-    bool layoutScheduled : 1;
     bool inApplyModelChanges : 1;
     bool inViewportMoved : 1;
     bool forceLayout : 1;

src/declarative/items/qsglistview.cpp
@@ -1037,7 +1037,7 @@ void QSGListViewPrivate::itemGeometryChanged(QSGItem *item, const QRectF &newGeo
         if ((orient == QSGListView::Vertical && newGeometry.height() != oldGeometry.height())
             || (orient == QSGListView::Horizontal && newGeometry.width() != oldGeometry.width())) {
             forceLayout = true;
-            scheduleLayout();
+            q->polish();
         }
     }
 }

commit e824620e9306d665b81cb12e167f50fb1090fddc

Ensure layout is done after delayed deletion of items
Regression from 1dd8b509074ba60da671f7671f8cf09c3fc001ae

Change-Id: I5957444e2f1493aa8fbf3b532fd1ef704deec50c
Reviewed-on: http://codereview.qt.nokia.com/4118
Reviewed-by: Bea Lam <bea.lam@nokia.com>

src/declarative/items/qsgitemview.cpp
@@ -775,6 +775,7 @@ void QSGItemView::destroyRemoved()
 
     // Correct the positioning of the items
     d->updateSections();
+    d->forceLayout = true;
     d->layout();
 }
 