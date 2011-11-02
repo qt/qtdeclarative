31ce3cb4ebb78ca67ef050a6282e8b4d5b1cedac
Christiaan Janssen <christiaan.janssen@nokia.com>
2011-11-02 15:59:17 +0100

    QmlProfiler: Send trace start event when starting profiling
    Change-Id: Ic81c99922f73b0bf9ee830adc2fe35f1c154d868
    Reviewed-by: Kai Koehne <kai.koehne@nokia.com>

--- src/declarative/debugger/qdeclarativedebugtrace.cpp
+++ src/declarative/debugger/qdeclarativedebugtrace.cpp
@@ -267,16 +267,20 @@ void QDeclarativeDebugTrace::messageReceived(const QByteArray &message)
     QByteArray rwData = message;
     QDataStream stream(&rwData, QIODevice::ReadOnly);
 
-    bool wasEnabled = m_enabled;
-    stream >> m_enabled;
+    bool enabled;
+    stream >> enabled;
 
     m_messageReceived = true;
 
-    if (!m_enabled && wasEnabled) {
-        m_enabled = true;
-        addEvent(EndTrace);
-        m_enabled = false;
-        sendMessages();
+    if (m_enabled != enabled) {
+        if (enabled) {
+            m_enabled = true;
+            addEvent(StartTrace);
+        } else {
+            addEvent(EndTrace);
+            m_enabled = false;
+            sendMessages();
+        }
     }
 }
 
--- src/declarative/debugger/qdeclarativedebugtrace_p.h
+++ src/declarative/debugger/qdeclarativedebugtrace_p.h
@@ -102,6 +102,7 @@ public:
         Key,
         AnimationFrame,
         EndTrace,
+        StartTrace,
 
         MaximumEventType
     };
