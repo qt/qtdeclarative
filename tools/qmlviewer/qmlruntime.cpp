ef1860e8c47a5816b151e5d63dbc0db196f9d0a8
Xizhi Zhu (Steven) <xizhi.zhu@gmail.com>
2012-01-14 08:57:04 +0200

    Fix build break in qmlviewer.
    It's introduced by commit 14b929e9c448f2cc74fa047aaf8af283b81fbaf9 in QtBase.
    
    Change-Id: Iacb5350b5f42a09b7419bf491ac9620a406422e6
    Reviewed-by: Xizhi Zhu <xizhi.zhu@nokia.com>

--- tools/qmlviewer/qmlruntime.cpp
+++ tools/qmlviewer/qmlruntime.cpp
@@ -59,6 +59,7 @@
 #include <QXmlStreamReader>
 #include <QBuffer>
 #include <QNetworkReply>
+#include <QNetworkCookie>
 #include <QNetworkCookieJar>
 #include <QNetworkDiskCache>
 #include <QNetworkAccessManager>
