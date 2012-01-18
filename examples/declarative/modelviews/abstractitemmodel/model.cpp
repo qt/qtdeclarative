9a08d3e79fee46b33a75d17f76f0ff63687a648e
Matthew Vogt <matthew.vogt@nokia.com>
2012-01-19 09:51:18 +1000

    Fix range checking in AbstractItemModel examples
    Fix range checking in AbstractItemModel examples
    
    Task-number: QTBUG-23574
    Change-Id: I6f2d4c18e1fb33ee369ebc75f501e524e8e63615
    Reviewed-by: Martin Jones <martin.jones@nokia.com>

--- examples/declarative/modelviews/abstractitemmodel/model.cpp
+++ examples/declarative/modelviews/abstractitemmodel/model.cpp
@@ -77,7 +77,7 @@ int AnimalModel::rowCount(const QModelIndex & parent) const {
 }
 
 QVariant AnimalModel::data(const QModelIndex & index, int role) const {
-    if (index.row() < 0 || index.row() > m_animals.count())
+    if (index.row() < 0 || index.row() >= m_animals.count())
         return QVariant();
 
     const Animal &animal = m_animals[index.row()];
