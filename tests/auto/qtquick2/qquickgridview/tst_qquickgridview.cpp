qt/qtdeclarative

commit 80d85e0017cb5cc4b0a0df6c19d4126bf5062731

Handle views with negative width/height
A view with a negative d->size() would get stuck in an infinite
loop.  Also make sure item layout/visibility is updated when
the view size changes.

Change-Id: I1f16a714ecebe1c4b71902c460e27fb0f1c4406f
Reviewed-by: Bea Lam <bea.lam@nokia.com>

src/quick/items/qquickgridview.cpp
@@ -195,7 +195,6 @@ public:
     virtual void initializeCurrentItem();
 
     virtual void updateViewport();
-    virtual void itemGeometryChanged(QQuickItem *item, const QRectF &newGeometry, const QRectF &oldGeometry);
     virtual void fixupPosition();
     virtual void fixup(AxisData &data, qreal minExtent, qreal maxExtent);
     virtual void flick(QQuickItemViewPrivate::AxisData &data, qreal minExtent, qreal maxExtent, qreal vSize,
@@ -787,21 +786,6 @@ void QQuickGridViewPrivate::initializeCurrentItem()
     if (currentItem && currentIndex >= 0) {
         FxGridItemSG *gridItem = static_cast<FxGridItemSG*>(currentItem);
         gridItem->setPosition(colPosAt(currentIndex), rowPosAt(currentIndex));
-    }
-}
-
-void QQuickGridViewPrivate::itemGeometryChanged(QQuickItem *item, const QRectF &newGeometry, const QRectF &oldGeometry)
-{
-    Q_Q(QQuickGridView);
-    QQuickItemViewPrivate::itemGeometryChanged(item, newGeometry, oldGeometry);
-    if (!q->isComponentComplete())
-        return;
-    if (item == q) {
-        if (newGeometry.height() != oldGeometry.height() || newGeometry.width() != oldGeometry.width()) {
-            updateViewport();
-            forceLayout = true;
-            q->polish();
-        }
     }
 }
 

src/quick/items/qquickitemview.cpp
@@ -878,6 +878,10 @@ void QQuickItemView::geometryChanged(const QRectF &newGeometry, const QRectF &ol
 {
     Q_D(QQuickItemView);
     d->markExtentsDirty();
+    if (isComponentComplete() && d->isValid()) {
+        d->forceLayout = true;
+        polish();
+    }
     QQuickFlickable::geometryChanged(newGeometry, oldGeometry);
 }
 
@@ -1212,7 +1216,6 @@ void QQuickItemViewPrivate::init()
     Q_Q(QQuickItemView);
     QQuickItemPrivate::get(contentItem)->childrenDoNotOverlap = true;
     q->setFlag(QQuickItem::ItemIsFocusScope);
-    addItemChangeListener(this, Geometry);
     QObject::connect(q, SIGNAL(movementEnded()), q, SLOT(animStopped()));
     q->setFlickableDirection(QQuickFlickable::VerticalFlick);
 }
@@ -1291,10 +1294,11 @@ void QQuickItemViewPrivate::mirrorChange()
 
 void QQuickItemViewPrivate::refill()
 {
+    qreal s = qMax(size(), qreal(0.));
     if (isContentFlowReversed())
-        refill(-position()-size(), -position());
+        refill(-position()-s, -position());
     else
-        refill(position(), position()+size());
+        refill(position(), position()+s);
 }
 
 void QQuickItemViewPrivate::refill(qreal from, qreal to, bool doBuffer)

tests/auto/qtquick2/qquickgridview/tst_qquickgridview.cpp
@@ -3175,6 +3175,41 @@ void tst_QQuickGridView::resizeViewAndRepaint()
     gridview->setHeight(100);
     QTRY_VERIFY(!findItem<QQuickItem>(contentItem, "wrapper", 10));
 
+    // Ensure we handle -ve sizes
+    gridview->setHeight(-100);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper").count(), 3);
+
+    gridview->setCacheBuffer(120);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper").count(), 9);
+
+    // ensure items in cache become visible
+    gridview->setHeight(120);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper").count(), 15);
+
+    int itemCount = findItems<QQuickItem>(contentItem, "wrapper").count();
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        if (!item) qWarning() << "Item" << i << "not found";
+        QTRY_VERIFY(item);
+        QTRY_COMPARE(item->x(), qreal((i%3)*80));
+        QTRY_COMPARE(item->y(), qreal((i/3)*60));
+        QCOMPARE(item->isVisible(), i < 9); // inside view visible, outside not visible
+    }
+
+    // ensure items outside view become invisible
+    gridview->setHeight(60);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper").count(), 12);
+
+    itemCount = findItems<QQuickItem>(contentItem, "wrapper").count();
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        if (!item) qWarning() << "Item" << i << "not found";
+        QTRY_VERIFY(item);
+        QTRY_COMPARE(item->x(), qreal((i%3)*80));
+        QTRY_COMPARE(item->y(), qreal((i/3)*60));
+        QCOMPARE(item->isVisible(), i < 6); // inside view visible, outside not visible
+    }
+
     delete canvas;
 }
 

tests/auto/qtquick2/qquicklistview/tst_qquicklistview.cpp
@@ -3576,6 +3576,7 @@ void tst_QQuickListView::resizeView()
     ctxt->setContextProperty("testObject", testObject);
 
     canvas->setSource(testFileUrl("listviewtest.qml"));
+    canvas->show();
     qApp->processEvents();
 
     QQuickListView *listview = findItem<QQuickListView>(canvas->rootObject(), "list");
@@ -3601,6 +3602,40 @@ void tst_QQuickListView::resizeView()
 
     QMetaObject::invokeMethod(canvas->rootObject(), "heightRatio", Q_RETURN_ARG(QVariant, heightRatio));
     QCOMPARE(heightRatio.toReal(), 0.25);
+
+    // Ensure we handle -ve sizes
+    listview->setHeight(-100);
+    QTRY_COMPARE(QQuickItemPrivate::get(listview)->polishScheduled, false);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper", false).count(), 1);
+
+    listview->setCacheBuffer(200);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper", false).count(), 11);
+
+    // ensure items in cache become visible
+    listview->setHeight(200);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper", false).count(), 21);
+
+    itemCount = findItems<QQuickItem>(contentItem, "wrapper", false).count();
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        if (!item) qWarning() << "Item" << i << "not found";
+        QTRY_VERIFY(item);
+        QTRY_COMPARE(item->y(), i*20.);
+        QCOMPARE(item->isVisible(), i < 11); // inside view visible, outside not visible
+    }
+
+    // ensure items outside view become invisible
+    listview->setHeight(100);
+    QTRY_COMPARE(findItems<QQuickItem>(contentItem, "wrapper", false).count(), 16);
+
+    itemCount = findItems<QQuickItem>(contentItem, "wrapper", false).count();
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        if (!item) qWarning() << "Item" << i << "not found";
+        QTRY_VERIFY(item);
+        QTRY_COMPARE(item->y(), i*20.);
+        QCOMPARE(item->isVisible(), i < 6); // inside view visible, outside not visible
+    }
 
     delete canvas;
     delete testObject;

commit 67ebd7014134df8c7981b815a99cd9d749e167db

Must update columns when GridView geometry changes
Regression from changes in 80d85e0017cb5cc4b0a0df6c19d4126bf5062731

Also remove shared duplicate code in visibleItemsChanged() in
ListView and GridView.

Change-Id: Ic3ab3ba070ddf9901a232ed5b3250c725797cf69
Reviewed-by: Martin Jones <martin.jones@nokia.com>

src/quick/items/qquickgridview.cpp
@@ -167,9 +167,10 @@ public:
     FxViewItem *snapItemAt(qreal pos) const;
     int snapIndex() const;
 
+    void resetColumns();
+
     virtual bool addVisibleItems(qreal fillFrom, qreal fillTo, bool doBuffer);
     virtual bool removeNonVisibleItems(qreal bufferFrom, qreal bufferTo);
-    virtual void visibleItemsChanged();
 
     virtual FxViewItem *newViewItem(int index, QQuickItem *item);
     virtual void repositionPackageItemAt(QQuickItem *item, int index);
@@ -395,6 +396,13 @@ int QQuickGridViewPrivate::snapIndex() const
     return index;
 }
 
+void QQuickGridViewPrivate::resetColumns()
+{
+    Q_Q(QQuickGridView);
+    qreal length = flow == QQuickGridView::LeftToRight ? q->width() : q->height();
+    columns = (int)qMax((length + colSize()/2) / colSize(), qreal(1.));
+}
+
 FxViewItem *QQuickGridViewPrivate::newViewItem(int modelIndex, QQuickItem *item)
 {
     Q_Q(QQuickGridView);
@@ -535,18 +543,9 @@ bool QQuickGridViewPrivate::removeNonVisibleItems(qreal bufferFrom, qreal buffer
     return changed;
 }
 
-void QQuickGridViewPrivate::visibleItemsChanged()
-{
-    updateHeader();
-    updateFooter();
-    updateViewport();
-}
-
 void QQuickGridViewPrivate::updateViewport()
 {
-    Q_Q(QQuickGridView);
-    qreal length = flow == QQuickGridView::LeftToRight ? q->width() : q->height();
-    columns = (int)qMax((length + colSize()/2) / colSize(), qreal(1.));
+    resetColumns();
     QQuickItemViewPrivate::updateViewport();
 }
 
@@ -1620,6 +1619,14 @@ void QQuickGridView::keyPressEvent(QKeyEvent *event)
     event->ignore();
     QQuickItemView::keyPressEvent(event);
 }
+
+void QQuickGridView::geometryChanged(const QRectF &newGeometry, const QRectF &oldGeometry)
+{
+    Q_D(QQuickGridView);
+    d->resetColumns();
+    QQuickItemView::geometryChanged(newGeometry, oldGeometry);
+}
+
 /*!
     \qmlmethod QtQuick2::GridView::moveCurrentIndexUp()
 

src/quick/items/qquickgridview_p.h
@@ -108,6 +108,7 @@ Q_SIGNALS:
 protected:
     virtual void viewportMoved();
     virtual void keyPressEvent(QKeyEvent *);
+    virtual void geometryChanged(const QRectF &newGeometry, const QRectF &oldGeometry);
 };
 
 class QQuickGridViewAttached : public QQuickItemViewAttached

src/quick/items/qquickitemview.cpp
@@ -1331,6 +1331,9 @@ void QQuickItemViewPrivate::refill(qreal from, qreal to)
         markExtentsDirty();
         updateBeginningEnd();
         visibleItemsChanged();
+        updateHeader();
+        updateFooter();
+        updateViewport();
     }
 
     if (prevCount != itemCount)

src/quick/items/qquickitemview_p_p.h
@@ -243,7 +243,7 @@ protected:
 
     virtual bool addVisibleItems(qreal fillFrom, qreal fillTo, bool doBuffer) = 0;
     virtual bool removeNonVisibleItems(qreal bufferFrom, qreal bufferTo) = 0;
-    virtual void visibleItemsChanged() = 0;
+    virtual void visibleItemsChanged() {}
 
     virtual FxViewItem *newViewItem(int index, QQuickItem *item) = 0;
     virtual void repositionPackageItemAt(QQuickItem *item, int index) = 0;

src/quick/items/qquicklistview.cpp
@@ -695,9 +695,6 @@ void QQuickListViewPrivate::visibleItemsChanged()
     }
     if (sectionCriteria)
         updateCurrentSection();
-    updateHeader();
-    updateFooter();
-    updateViewport();
     updateUnrequestedPositions();
 }
 

tests/auto/qtquick2/qquickgridview/data/resizeview.qml
@@ -3,12 +3,13 @@ import QtQuick 2.0
 Rectangle {
     id: root
 
-    property real initialHeight
+    width: 240
+    height: 320
 
     GridView {
         id: grid
         objectName: "grid"
-        width: 240
+        width: initialWidth
         height: initialHeight
         cellWidth: 80
         cellHeight: 60

tests/auto/qtquick2/qquickgridview/tst_qquickgridview.cpp
@@ -105,6 +105,7 @@ private slots:
     void header();
     void header_data();
     void resizeViewAndRepaint();
+    void changeColumnCount();
     void indexAt_itemAt_data();
     void indexAt_itemAt();
     void onAdd();
@@ -3185,6 +3186,7 @@ void tst_QQuickGridView::resizeViewAndRepaint()
 
     QDeclarativeContext *ctxt = canvas->rootContext();
     ctxt->setContextProperty("testModel", &model);
+    ctxt->setContextProperty("initialWidth", 240);
     ctxt->setContextProperty("initialHeight", 100);
 
     canvas->setSource(testFileUrl("resizeview.qml"));
@@ -3242,6 +3244,64 @@ void tst_QQuickGridView::resizeViewAndRepaint()
         QTRY_COMPARE(item->x(), qreal((i%3)*80));
         QTRY_COMPARE(item->y(), qreal((i/3)*60));
         QCOMPARE(item->isVisible(), i < 6); // inside view visible, outside not visible
+    }
+
+    delete canvas;
+}
+
+void tst_QQuickGridView::changeColumnCount()
+{
+    TestModel model;
+    for (int i = 0; i < 40; i++)
+        model.addItem("Item" + QString::number(i), "");
+
+    QQuickView *canvas = createView();
+    QDeclarativeContext *ctxt = canvas->rootContext();
+    ctxt->setContextProperty("testModel", &model);
+    ctxt->setContextProperty("initialWidth", 100);
+    ctxt->setContextProperty("initialHeight", 320);
+    canvas->setSource(testFileUrl("resizeview.qml"));
+    canvas->show();
+    qApp->processEvents();
+
+    QQuickGridView *gridview = findItem<QQuickGridView>(canvas->rootObject(), "grid");
+    QTRY_VERIFY(gridview != 0);
+    QQuickItem *contentItem = gridview->contentItem();
+    QTRY_VERIFY(contentItem != 0);
+    QTRY_COMPARE(QQuickItemPrivate::get(gridview)->polishScheduled, false);
+
+    // a single column of 6 items are visible
+    int itemCount = findItems<QQuickItem>(contentItem, "wrapper").count();
+    QCOMPARE(itemCount, 6);
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        QVERIFY2(item, QTest::toString(QString("Item %1 not found").arg(i)));
+        QCOMPARE(item->x(), 0.0);
+        QCOMPARE(item->y(), qreal(i*60));
+    }
+
+    // now 6x3 grid is visible, plus 1 extra below for refill
+    gridview->setWidth(240);
+    QTRY_COMPARE(QQuickItemPrivate::get(gridview)->polishScheduled, false);
+    itemCount = findItems<QQuickItem>(contentItem, "wrapper").count();
+    QCOMPARE(itemCount, 6*3 + 1);
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        QVERIFY2(item, QTest::toString(QString("Item %1 not found").arg(i)));
+        QCOMPARE(item->x(), qreal((i%3)*80));
+        QCOMPARE(item->y(), qreal((i/3)*60));
+    }
+
+    // back to single column
+    gridview->setWidth(100);
+    QTRY_COMPARE(QQuickItemPrivate::get(gridview)->polishScheduled, false);
+    itemCount = findItems<QQuickItem>(contentItem, "wrapper").count();
+    QCOMPARE(itemCount, 6);
+    for (int i = 0; i < model.count() && i < itemCount; ++i) {
+        QQuickItem *item = findItem<QQuickItem>(contentItem, "wrapper", i);
+        QVERIFY2(item, QTest::toString(QString("Item %1 not found").arg(i)));
+        QCOMPARE(item->x(), 0.0);
+        QCOMPARE(item->y(), qreal(i*60));
     }
 
     delete canvas;