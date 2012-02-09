e299f0d86dc7f3d158fbb59d055584e768e39c6b
Alan Alpert <alan.alpert@nokia.com>
2012-02-09 19:40:34 +1000

    Doc fix
    Change-Id: I94712d5e7ca81618c4c506f5984ff94c46b730dd
    Reviewed-by: Michael Brasser <michael.brasser@nokia.com>

--- src/quick/particles/qquickimageparticle.cpp
+++ src/quick/particles/qquickimageparticle.cpp
@@ -770,9 +770,9 @@ void fillUniformArrayFromImage(float* array, const QImage& img, int size)
 
     Acceptable values are
     \list
-    \o None: Particles just appear and disappear.
-    \o Fade: Particles fade in from 0 opacity at the start of their life, and fade out to 0 at the end.
-    \o Scale: Particles scale in from 0 size at the start of their life, and scale back to 0 at the end.
+    \o ImageParticle.None: Particles just appear and disappear.
+    \o ImageParticle.Fade: Particles fade in from 0 opacity at the start of their life, and fade out to 0 at the end.
+    \o ImageParticle.Scale: Particles scale in from 0 size at the start of their life, and scale back to 0 at the end.
     \endlist
 
     Default value is Fade.
