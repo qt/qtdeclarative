be47ec24cf51b1d4b7692e35345704bac9897a2e
Glenn Watson <glenn.watson@nokia.com>
2012-02-14 09:05:12 +1000

    Fix typo in error string.
    Provide correct line/column information in case of error
    while creating dynamic qml object.
    
    Task-number: QTBUG-24065
    Change-Id: I283408c898706dc6df57e21a57d97bafd56d9aaa
    Reviewed-by: Martin Jones <martin.jones@nokia.com>

--- src/declarative/qml/v8/qdeclarativebuiltinfunctions.cpp
+++ src/declarative/qml/v8/qdeclarativebuiltinfunctions.cpp
@@ -992,7 +992,7 @@ v8::Handle<v8::Value> createQmlObject(const v8::Arguments &args)
                 errorstr += QLatin1String("\n    ") + error.toString();
                 v8::Local<v8::Object> qmlerror = v8::Object::New();
                 qmlerror->Set(v8::String::New("lineNumber"), v8::Integer::New(error.line()));
-                qmlerror->Set(v8::String::New("columnNumber"), v8::Integer::New(error.line()));
+                qmlerror->Set(v8::String::New("columnNumber"), v8::Integer::New(error.column()));
                 qmlerror->Set(v8::String::New("fileName"), engine->toString(error.url().toString()));
                 qmlerror->Set(v8::String::New("message"), engine->toString(error.description()));
                 qmlerrors->Set(ii, qmlerror);
