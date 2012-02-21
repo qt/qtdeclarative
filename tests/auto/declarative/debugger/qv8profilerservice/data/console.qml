qt/qtdeclarative

commit 82dfecab38031f8009ea64ef93063c02b79eaf94

Debugger: Allow trace service to send data on exit
Add a statusAboutToBeChanged virtual that allows
services to send data e.g. on application exit.

Change-Id: I28fa513ab2a12d6973c444aac3062d64a0957207
Reviewed-by: Christiaan Janssen <christiaan.janssen@nokia.com>

src/declarative/debugger/qv8profilerservice.cpp
@@ -97,15 +97,13 @@ public:
     QList<QV8ProfilerData> m_data;
 
     bool initialized;
-    bool m_enabled;
+    QList<QString> m_ongoing;
 };
 
 QV8ProfilerService::QV8ProfilerService(QObject *parent)
     : QDeclarativeDebugService(*(new QV8ProfilerServicePrivate()), QLatin1String("V8Profiler"), 1, parent)
 {
     Q_D(QV8ProfilerService);
-
-    d->m_enabled = false;
 
     if (registerService() == Enabled) {
         // ,block mode, client attached
@@ -129,6 +127,20 @@ void QV8ProfilerService::initialize()
     v8ProfilerInstance();
 }
 
+void QV8ProfilerService::statusAboutToBeChanged(QDeclarativeDebugService::Status newStatus)
+{
+    Q_D(QV8ProfilerService);
+
+    if (status() == newStatus)
+        return;
+
+    if (status() == Enabled) {
+        foreach (const QString &title, d->m_ongoing)
+            QMetaObject::invokeMethod(this, "stopProfiling", Qt::QueuedConnection, Q_ARG(QString, title));
+        sendProfilingData();
+    }
+}
+
 void QV8ProfilerService::messageReceived(const QByteArray &message)
 {
     Q_D(QV8ProfilerService);
@@ -141,13 +153,12 @@ void QV8ProfilerService::messageReceived(const QByteArray &message)
 
     if (command == "V8PROFILER") {
         ds >>  title;
-        if (option == "start" && !d->m_enabled) {
-            QMetaObject::invokeMethod(this, "startProfiling", Qt::QueuedConnection, Q_ARG(QString, QString::fromUtf8(title)));
-            d->m_enabled = true;
-        } else if (option == "stop" && d->m_enabled) {
-            QMetaObject::invokeMethod(this, "stopProfiling", Qt::QueuedConnection, Q_ARG(QString, QString::fromUtf8(title)));
+        QString titleStr = QString::fromUtf8(title);
+        if (option == "start") {
+            QMetaObject::invokeMethod(this, "startProfiling", Qt::QueuedConnection, Q_ARG(QString, titleStr));
+        } else if (option == "stop" && d->initialized) {
+            QMetaObject::invokeMethod(this, "stopProfiling", Qt::QueuedConnection, Q_ARG(QString, titleStr));
             QMetaObject::invokeMethod(this, "sendProfilingData", Qt::QueuedConnection);
-            d->m_enabled = false;
         }
         d->initialized = true;
     }
@@ -165,7 +176,12 @@ void QV8ProfilerService::messageReceived(const QByteArray &message)
 
 void QV8ProfilerService::startProfiling(const QString &title)
 {
+    Q_D(QV8ProfilerService);
     // Start Profiling
+
+    if (d->m_ongoing.contains(title))
+        return;
+
     v8::HandleScope handle_scope;
     v8::Handle<v8::String> v8title = v8::String::New(reinterpret_cast<const uint16_t*>(title.data()), title.size());
     v8::CpuProfiler::StartProfiling(v8title);
@@ -175,6 +191,11 @@ void QV8ProfilerService::stopProfiling(const QString &title)
 {
     Q_D(QV8ProfilerService);
     // Stop profiling
+
+    if (!d->m_ongoing.contains(title))
+        return;
+    d->m_ongoing.removeOne(title);
+
     v8::HandleScope handle_scope;
     v8::Handle<v8::String> v8title = v8::String::New(reinterpret_cast<const uint16_t*>(title.data()), title.size());
     const v8::CpuProfile *cpuProfile = v8::CpuProfiler::StopProfiling(v8title);

tests/auto/declarative/debugger/qv8profilerservice/tst_qv8profilerservice.cpp
@@ -115,7 +115,7 @@ private:
     QDeclarativeDebugConnection *m_connection;
     QV8ProfilerClient *m_client;
 
-    void connect(bool block);
+    void connect(bool block, const QString &testFile);
 
 private slots:
     void cleanup();
@@ -124,6 +124,7 @@ private slots:
     void blockingConnectWithTraceDisabled();
     void nonBlockingConnect();
     void snapshot();
+    void profileOnExit();
 };
 
 void QV8ProfilerClient::messageReceived(const QByteArray &message)
@@ -164,7 +165,7 @@ void QV8ProfilerClient::messageReceived(const QByteArray &message)
     QVERIFY(stream.atEnd());
 }
 
-void tst_QV8ProfilerService::connect(bool block)
+void tst_QV8ProfilerService::connect(bool block, const QString &testFile)
 {
     const QString executable = QLibraryInfo::location(QLibraryInfo::BinariesPath) + "/qmlscene";
     QStringList arguments;
@@ -174,7 +175,7 @@ void tst_QV8ProfilerService::connect(bool block)
     else
         arguments << QString("-qmljsdebugger=port:"STR_PORT);
 
-    arguments << QDeclarativeDataTest::instance()->testFile("test.qml");
+    arguments << QDeclarativeDataTest::instance()->testFile(testFile);
 
     m_process = new QDeclarativeDebugProcess(executable);
     m_process->start(QStringList() << arguments);
@@ -199,7 +200,7 @@ void tst_QV8ProfilerService::cleanup()
 
 void tst_QV8ProfilerService::blockingConnectWithTraceEnabled()
 {
-    connect(true);
+    connect(true, "test.qml");
     QTRY_COMPARE(m_client->status(), QDeclarativeDebugClient::Enabled);
 
     m_client->startProfiling("");
@@ -213,7 +214,7 @@ void tst_QV8ProfilerService::blockingConnectWithTraceEnabled()
 
 void tst_QV8ProfilerService::blockingConnectWithTraceDisabled()
 {
-    connect(true);
+    connect(true, "test.qml");
     QTRY_COMPARE(m_client->status(), QDeclarativeDebugClient::Enabled);
 
     m_client->stopProfiling("");
@@ -233,7 +234,7 @@ void tst_QV8ProfilerService::blockingConnectWithTraceDisabled()
 
 void tst_QV8ProfilerService::nonBlockingConnect()
 {
-    connect(false);
+    connect(false, "test.qml");
     QTRY_COMPARE(m_client->status(), QDeclarativeDebugClient::Enabled);
 
     m_client->startProfiling("");
@@ -247,13 +248,27 @@ void tst_QV8ProfilerService::nonBlockingConnect()
 
 void tst_QV8ProfilerService::snapshot()
 {
-    connect(false);
+    connect(false, "test.qml");
     QTRY_COMPARE(m_client->status(), QDeclarativeDebugClient::Enabled);
 
     m_client->takeSnapshot();
     if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(snapshot()))) {
         QString failMsg
                 = QString("No snapshot received in time. App output: %1\n\n").arg(m_process->output());
+        QFAIL(qPrintable(failMsg));
+    }
+}
+
+void tst_QV8ProfilerService::profileOnExit()
+{
+    connect(true, "exit.qml");
+    QTRY_COMPARE(m_client->status(), QDeclarativeDebugClient::Enabled);
+
+    m_client->startProfiling("");
+
+    if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete()))) {
+        QString failMsg
+                = QString("No trace received in time. App output: \n%1\n").arg(m_process->output());
         QFAIL(qPrintable(failMsg));
     }
 }

src/declarative/debugger/qdeclarativedebugserver.cpp
@@ -221,6 +221,10 @@ void QDeclarativeDebugServerThread::run()
     }
 
     exec();
+
+    // make sure events still waiting are processed
+    QEventLoop eventLoop;
+    eventLoop.processEvents(QEventLoop::AllEvents);
 }
 
 bool QDeclarativeDebugServer::hasDebuggingClient() const
@@ -325,6 +329,7 @@ QDeclarativeDebugServer::~QDeclarativeDebugServer()
     QReadLocker(&d->pluginsLock);
     {
         foreach (QDeclarativeDebugService *service, d->plugins.values()) {
+            service->statusAboutToBeChanged(QDeclarativeDebugService::NotConnected);
             service->d_func()->server = 0;
             service->d_func()->status = QDeclarativeDebugService::NotConnected;
             service->statusChanged(QDeclarativeDebugService::NotConnected);
@@ -333,8 +338,7 @@ QDeclarativeDebugServer::~QDeclarativeDebugServer()
 
     if (d->thread) {
         d->thread->exit();
-        if (!d->thread->wait(1000))
-            d->thread->terminate();
+        d->thread->wait();
         delete d->thread;
     }
     delete d->connection;
@@ -487,8 +491,9 @@ bool QDeclarativeDebugServer::removeService(QDeclarativeDebugService *service)
     }
     {
         QReadLocker(&d->pluginsLock);
-        d->advertisePlugins();
         QDeclarativeDebugService::Status newStatus = QDeclarativeDebugService::NotConnected;
+        service->statusAboutToBeChanged(newStatus);
+        d->advertisePlugins();
         service->d_func()->server = 0;
         service->d_func()->status = newStatus;
         service->statusChanged(newStatus);

src/declarative/debugger/qdeclarativedebugservice.cpp
@@ -253,6 +253,10 @@ bool QDeclarativeDebugService::waitForMessage()
     return d->server->waitForMessage(this);
 }
 
+void QDeclarativeDebugService::statusAboutToBeChanged(Status)
+{
+}
+
 void QDeclarativeDebugService::statusChanged(Status)
 {
 }

src/declarative/debugger/qdeclarativedebugservice_p.h
@@ -96,6 +96,7 @@ protected:
 
     Status registerService();
 
+    virtual void statusAboutToBeChanged(Status);
     virtual void statusChanged(Status);
     virtual void messageReceived(const QByteArray &);
 

src/declarative/debugger/qdeclarativedebugtrace.cpp
@@ -299,14 +299,27 @@ void QDeclarativeDebugTrace::sendMessages()
     QList<QByteArray> messages;
     for (int i = 0; i < m_data.count(); ++i)
         messages << m_data.at(i).toByteArray();
-    QDeclarativeDebugService::sendMessages(messages);
     m_data.clear();
 
     //indicate completion
     QByteArray data;
     QDataStream ds(&data, QIODevice::WriteOnly);
     ds << (qint64)-1 << (int)Complete;
-    sendMessage(data);
+    messages << data;
+
+    QDeclarativeDebugService::sendMessages(messages);
+}
+
+void QDeclarativeDebugTrace::statusAboutToBeChanged(QDeclarativeDebugService::Status newStatus)
+{
+    if (status() == newStatus)
+        return;
+
+    if (status() == Enabled
+            && m_enabled) {
+        stopProfilingImpl();
+        sendMessages();
+    }
 }
 
 void QDeclarativeDebugTrace::messageReceived(const QByteArray &message)

src/declarative/debugger/qdeclarativedebugtrace_p.h
@@ -100,7 +100,7 @@ public:
         RangeData,
         RangeLocation,
         RangeEnd,
-        Complete,
+        Complete, // end of transmission
 
         MaximumMessage
     };
@@ -145,6 +145,7 @@ public:
     ~QDeclarativeDebugTrace();
 
 protected:
+    virtual void statusAboutToBeChanged(Status status);
     virtual void messageReceived(const QByteArray &);
 
 private:

src/declarative/debugger/qv8profilerservice_p.h
@@ -104,6 +104,7 @@ public slots:
     void sendProfilingData();
 
 protected:
+    void statusAboutToBeChanged(Status status);
     void messageReceived(const QByteArray &);
 
 private:

commit 784d867d2b79fcfdbf039456fcfe0692652fec52

v8profiler: Actually submit data
Fix regression introduced in 82dfecab3.

Change-Id: Icd90c483b9b2fde8d4cf522f3a35368cd395b120
Reviewed-by: Christiaan Janssen <christiaan.janssen@nokia.com>

src/declarative/debugger/qv8profilerservice.cpp
@@ -185,6 +185,8 @@ void QV8ProfilerService::startProfiling(const QString &title)
     v8::HandleScope handle_scope;
     v8::Handle<v8::String> v8title = v8::String::New(reinterpret_cast<const uint16_t*>(title.data()), title.size());
     v8::CpuProfiler::StartProfiling(v8title);
+
+    d->m_ongoing.append(title);
 }
 
 void QV8ProfilerService::stopProfiling(const QString &title)

tests/auto/declarative/debugger/qv8profilerservice/data/console.qml
@@ -1,0 +1,14 @@
+import QtQuick 2.0
+
+
+Item {
+    function f()
+    {
+    }
+
+    Component.onCompleted:  {
+        console.profile();
+        f();
+        console.profileEnd();
+    }
+}

tests/auto/declarative/debugger/qv8profilerservice/tst_qv8profilerservice.cpp
@@ -125,6 +125,7 @@ private slots:
     void nonBlockingConnect();
     void snapshot();
     void profileOnExit();
+    void console();
 };
 
 void QV8ProfilerClient::messageReceived(const QByteArray &message)
@@ -193,6 +194,9 @@ void tst_QV8ProfilerService::connect(bool block, const QString &testFile)
 
 void tst_QV8ProfilerService::cleanup()
 {
+    if (QTest::currentTestFailed())
+        qDebug() << "Application Output:" << m_process->output();
+
     delete m_process;
     delete m_connection;
     delete m_client;
@@ -205,11 +209,8 @@ void tst_QV8ProfilerService::blockingConnectWithTraceEnabled()
 
     m_client->startProfiling("");
     m_client->stopProfiling("");
-    if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete()))) {
-        QString failMsg
-                = QString("No trace received in time. App output: %1\n\n").arg(m_process->output());
-        QFAIL(qPrintable(failMsg));
-    }
+    QVERIFY2(QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete())),
+             "No trace received in time.");
 }
 
 void tst_QV8ProfilerService::blockingConnectWithTraceDisabled()
@@ -225,11 +226,8 @@ void tst_QV8ProfilerService::blockingConnectWithTraceDisabled()
     }
     m_client->startProfiling("");
     m_client->stopProfiling("");
-    if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete()))) {
-        QString failMsg
-                = QString("No trace received in time. App output: %1\n\n").arg(m_process->output());
-        QFAIL(qPrintable(failMsg));
-    }
+    QVERIFY2(QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete())),
+             "No trace received in time.");
 }
 
 void tst_QV8ProfilerService::nonBlockingConnect()
@@ -239,11 +237,8 @@ void tst_QV8ProfilerService::nonBlockingConnect()
 
     m_client->startProfiling("");
     m_client->stopProfiling("");
-    if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete()))) {
-        QString failMsg
-                = QString("No trace received in time. App output: %1\n\n").arg(m_process->output());
-        QFAIL(qPrintable(failMsg));
-    }
+    QVERIFY2(QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete())),
+             "No trace received in time.");
 }
 
 void tst_QV8ProfilerService::snapshot()
@@ -252,11 +247,8 @@ void tst_QV8ProfilerService::snapshot()
     QTRY_COMPARE(m_client->state(), QDeclarativeDebugClient::Enabled);
 
     m_client->takeSnapshot();
-    if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(snapshot()))) {
-        QString failMsg
-                = QString("No snapshot received in time. App output: %1\n\n").arg(m_process->output());
-        QFAIL(qPrintable(failMsg));
-    }
+    QVERIFY2(QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(snapshot())),
+             "No trace received in time.");
 }
 
 void tst_QV8ProfilerService::profileOnExit()
@@ -266,11 +258,21 @@ void tst_QV8ProfilerService::profileOnExit()
 
     m_client->startProfiling("");
 
-    if (!QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete()))) {
-        QString failMsg
-                = QString("No trace received in time. App output: \n%1\n").arg(m_process->output());
-        QFAIL(qPrintable(failMsg));
-    }
+    QVERIFY2(QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete())),
+             "No trace received in time.");
+    //QVERIFY(!m_client->traceMessages.isEmpty());
+}
+
+void tst_QV8ProfilerService::console()
+{
+    connect(true, "console.qml");
+    QTRY_COMPARE(m_client->state(), QDeclarativeDebugClient::Enabled);
+
+    m_client->stopProfiling("");
+
+    QVERIFY2(QDeclarativeDebugTest::waitForSignal(m_client, SIGNAL(complete())),
+             "No trace received in time.");
+    QVERIFY(!m_client->traceMessages.isEmpty());
 }
 
 QTEST_MAIN(tst_QV8ProfilerService)