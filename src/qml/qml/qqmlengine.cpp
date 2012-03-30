8ab5f4ade2157559b6a999a36537ed638f501e74
Matthew Vogt <matthew.vogt@nokia.com>
2012-03-30 16:27:44 +1000

    Improve the documentation of clearComponentCache()
    Change-Id: I842632a3173632b0fca521138caffe960b752da1
    Reviewed-by: Chris Adams <christopher.adams@nokia.com>

--- src/qml/qml/qqmlengine.cpp
+++ src/qml/qml/qqmlengine.cpp
@@ -563,9 +563,17 @@ QQmlEngine::~QQmlEngine()
 /*!
   Clears the engine's internal component cache.
 
-  Normally the QQmlEngine caches components loaded from qml
-  files.  This method clears this cache and forces the component to be
-  reloaded.
+  This function causes the property metadata of all components previously
+  loaded by the engine to be destroyed.  All previously loaded components and
+  the property bindings for all extant objects created from those components will
+  cease to function.
+
+  This function returns the engine to a state where it does not contain any loaded
+  component data.  This may be useful in order to reload a smaller subset of the
+  previous component set, or to load a new version of a previously loaded component.
+
+  Once the component cache has been cleared, components must be loaded before
+  any new objects can be created.
  */
 void QQmlEngine::clearComponentCache()
 {
