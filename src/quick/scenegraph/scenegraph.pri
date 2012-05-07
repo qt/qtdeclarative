qt/qtdeclarative

commit 8867be2d1d92e981ed736330f036a681ce4b520b

Remove QSGEngine
Change-Id: Iaab0d9f607b1f4ca6dfb13495a456d1b31bb980a
Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

src/quick/scenegraph/scenegraph.pri
@@ -1,4 +1,4 @@
-!contains(QT_CONFIG, egl):DEFINES += QT_NO_EGL
+qsg!contains(QT_CONFIG, egl):DEFINES += QT_NO_EGL
 
 # Core API
 HEADERS += \
@@ -24,7 +24,6 @@ SOURCES += \
 HEADERS += \
     $$PWD/util/qsgareaallocator_p.h \
     $$PWD/util/qsgdepthstencilbuffer_p.h \
-    $$PWD/util/qsgengine.h \
     $$PWD/util/qsgflatcolormaterial.h \
     $$PWD/util/qsgsimplematerial.h \
     $$PWD/util/qsgsimplerectnode.h \
@@ -41,7 +40,6 @@ HEADERS += \
 SOURCES += \
     $$PWD/util/qsgareaallocator.cpp \
     $$PWD/util/qsgdepthstencilbuffer.cpp \
-    $$PWD/util/qsgengine.cpp \
     $$PWD/util/qsgflatcolormaterial.cpp \
     $$PWD/util/qsgsimplerectnode.cpp \
     $$PWD/util/qsgsimpletexturenode.cpp \

examples/quick/openglunderqml/squircle.cpp
@@ -41,7 +41,6 @@
 
 #include "squircle.h"
 
-#include <QtQuick/qsgengine.h>
 #include <QtQuick/qquickcanvas.h>
 #include <QOpenGLShaderProgram>
 

src/particles/qquickimageparticle.cpp
@@ -51,7 +51,6 @@
 #include <private/qquicksprite_p.h>
 #include <private/qquickspriteengine_p.h>
 #include <QOpenGLFunctions>
-#include <QtQuick/qsgengine.h>
 #include <QtQuick/private/qsgtexture_p.h>
 #include <private/qqmlglobal_p.h>
 #include <QtQml/qqmlinfo.h>

src/quick/items/qquickanimatedsprite.cpp
@@ -45,7 +45,6 @@
 #include <QtQuick/private/qsgcontext_p.h>
 #include <private/qsgadaptationlayer_p.h>
 #include <QtQuick/qsgnode.h>
-#include <QtQuick/qsgengine.h>
 #include <QtQuick/qsgtexturematerial.h>
 #include <QtQuick/qsgtexture.h>
 #include <QtQuick/qquickcanvas.h>

src/quick/items/qquickcanvas.cpp
@@ -49,7 +49,6 @@
 #include <QtQuick/private/qsgrenderer_p.h>
 #include <QtQuick/private/qsgtexture_p.h>
 #include <QtQuick/private/qsgflashnode_p.h>
-#include <QtQuick/qsgengine.h>
 
 #include <private/qquickwindowmanager_p.h>
 
@@ -350,10 +349,6 @@ void QQuickCanvasPrivate::init(QQuickCanvas *c)
     QObject::connect(context, SIGNAL(initialized()), q, SIGNAL(sceneGraphInitialized()), Qt::DirectConnection);
     QObject::connect(context, SIGNAL(invalidated()), q, SIGNAL(sceneGraphInvalidated()), Qt::DirectConnection);
     QObject::connect(context, SIGNAL(invalidated()), q, SLOT(cleanupSceneGraph()), Qt::DirectConnection);
-
-    // ### TODO: remove QSGEngine
-    engine = new QSGEngine();
-    engine->setCanvas(q);
 }
 
 QQmlListProperty<QObject> QQuickCanvasPrivate::data()
@@ -2125,26 +2120,6 @@ QOpenGLContext *QQuickCanvas::openglContext() const
 
     This signal will be emitted from the scene graph rendering thread.
  */
-
-/*!
-    Returns the QSGEngine used for this scene.
-
-    The engine will only be available once the scene graph has been
-    initialized. Register for the sceneGraphEngine() signal to get
-    notification about this.
-
-    \deprecated
- */
-
-QSGEngine *QQuickCanvas::sceneGraphEngine() const
-{
-    Q_D(const QQuickCanvas);
-    qWarning("QQuickCanvas::sceneGraphEngine() is deprecated, use members of QQuickCanvas instead");
-    if (d->context && d->context->isReady())
-        return d->engine;
-    return 0;
-}
-
 
 
 /*!

src/quick/items/qquickcanvas.h
@@ -52,7 +52,6 @@ QT_BEGIN_HEADER
 
 QT_BEGIN_NAMESPACE
 
-class QSGEngine;
 class QQuickItem;
 class QSGTexture;
 class QInputMethodEvent;
@@ -88,8 +87,6 @@ public:
     QQuickItem *mouseGrabberItem() const;
 
     bool sendEvent(QQuickItem *, QEvent *);
-
-    QSGEngine *sceneGraphEngine() const;
 
     QImage grabFrameBuffer();
 

src/quick/items/qquickcanvas_p.h
@@ -174,7 +174,6 @@ public:
 
     void fireFrameSwapped() { emit q_func()->frameSwapped(); }
 
-    QSGEngine *engine;
     QSGContext *context;
     QSGRenderer *renderer;
 

src/quick/items/qquickitem.cpp
@@ -2144,11 +2144,6 @@ QQuickItem *QQuickItem::parentItem() const
     return d->parentItem;
 }
 
-QSGEngine *QQuickItem::sceneGraphEngine() const
-{
-    return canvas()->sceneGraphEngine();
-}
-
 QQuickCanvas *QQuickItem::canvas() const
 {
     Q_D(const QQuickItem);

commit bda67d861e1093d0aadbaab6daded4f4f3b19db1

Fix qmake logic
Regression introduced with 8867be2d1d92e981ed736330f036a681ce4b520b

Change-Id: I8c109399c28c729403535332f8f15df8dfe32b8d
Reviewed-by: Gunnar Sletta <gunnar.sletta@nokia.com>

src/quick/scenegraph/scenegraph.pri
@@ -1,4 +1,4 @@
-qsg!contains(QT_CONFIG, egl):DEFINES += QT_NO_EGL
+!contains(QT_CONFIG, egl):DEFINES += QT_NO_EGL
 
 # Core API
 HEADERS += \