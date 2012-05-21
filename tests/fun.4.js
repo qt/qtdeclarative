730302a642abc0224184d8f04672238152800299
Roberto Raggi <roberto.raggi@nokia.com>
2012-05-21 17:48:39 +0200

    Add non generic version of Array.prototype.every

--- tests/fun.4.js
+++ tests/fun.4.js
@@ -5,7 +5,9 @@ function foo(a,b,c) {
 
 foo.call(null, 1,2,3);
 
-[10,20,30].forEach(function (v,k,o) { print(v,k,o); })
+[10,20,30].forEach(function (v,k,o) { print(v,k,o); });
+
+print([10, 20, 30].every(function (v,k,o) { return v > 9 }));
 
 
 
