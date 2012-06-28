qt/qtdeclarative

commit 27416448a1c3259ecad80172ba35ab9aa504a78d

use auto-defined QT_BUILD_*_LIB variables
Change-Id: Iefe0b0982ee482bc47b4f66e514a7d0900674e42
Reviewed-by: Alan Alpert <alan.alpert@nokia.com>

src/particles/qtquickparticlesglobal_p.h
@@ -48,7 +48,7 @@
 
 #if defined(Q_OS_WIN)
 #  if defined(QT_MAKEDLL) /* create a Qt DLL library */
-#    if defined(QT_BUILD_QUICKPARTICLES_LIB)
+#    if defined(QT_BUILD_PARTICLES_LIB)
 #      define Q_QUICKPARTICLES_PRIVATE_EXPORT Q_DECL_EXPORT
 #    else
 #      define Q_QUICKPARTICLES_PRIVATE_EXPORT Q_DECL_IMPORT

src/qmltest/quicktestglobal.h
@@ -54,7 +54,7 @@ QT_BEGIN_HEADER
 #    if defined(QT_DLL)
 #      undef QT_DLL
 #    endif
-#    if defined(QT_BUILD_QUICK_TEST_LIB)
+#    if defined(QT_BUILD_QMLTEST_LIB)
 #        define Q_QUICK_TEST_EXPORT Q_DECL_EXPORT
 #    else
 #        define Q_QUICK_TEST_EXPORT Q_DECL_IMPORT

commit 4c1addd2c3b019d66b5c19fcd8ba9e0918e92978

Fix exports in QtQuickParticles.
Undo breakage introduced by
27416448a1c3259ecad80172ba35ab9aa504a78d.

Change-Id: Ia01a97688ecb91137a2649948af7479bd8323936
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

src/particles/qtquickparticlesglobal_p.h
@@ -48,7 +48,7 @@
 
 #if defined(Q_OS_WIN)
 #  if defined(QT_MAKEDLL) /* create a Qt DLL library */
-#    if defined(QT_BUILD_PARTICLES_LIB)
+#    if defined(QT_BUILD_QUICKPARTICLES_LIB)
 #      define Q_QUICKPARTICLES_PRIVATE_EXPORT Q_DECL_EXPORT
 #    else
 #      define Q_QUICKPARTICLES_PRIVATE_EXPORT Q_DECL_IMPORT