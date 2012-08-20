4fdf98a11f6919c04c5ef3fb78ecd39dd5f4b1fa
Sean Harmer <sean.harmer@kdab.com>
2012-08-20 21:09:48 +0100

    Fix compilation on OSX 10.6
    OS X 10.6 does not support the GL_ARB_framebuffer_sRGB extension.
    
    Change-Id: Ifea8eaf3ffca6242d908538afeb983131a113c73
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>

--- src/quick/scenegraph/qsgdefaultglyphnode_p.cpp
+++ src/quick/scenegraph/qsgdefaultglyphnode_p.cpp
@@ -128,7 +128,7 @@ void QSGTextMaskMaterialData::activate()
     QSGMaterialShader::activate();
     glBlendFunc(GL_CONSTANT_COLOR, GL_ONE_MINUS_SRC_COLOR);
 
-#if !defined(QT_OPENGL_ES_2)
+#if !defined(QT_OPENGL_ES_2) && defined(GL_ARB_framebuffer_sRGB)
     // 0.25 was found to be acceptable error margin by experimentation. On Mac, the gamma is 2.0,
     // but using sRGB looks okay.
     if (qAbs(fontSmoothingGamma() - 2.2) < 0.25)
@@ -141,7 +141,7 @@ void QSGTextMaskMaterialData::deactivate()
     QSGMaterialShader::deactivate();
     glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
-#if !defined(QT_OPENGL_ES_2)
+#if !defined(QT_OPENGL_ES_2) && defined(GL_ARB_framebuffer_sRGB)
     if (qAbs(fontSmoothingGamma() - 2.2) < 0.25)
         glDisable(GL_FRAMEBUFFER_SRGB);
 #endif
