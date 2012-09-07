qt/qtdeclarative

commit 70a2c0491d66aa05f9e9e67f8a845f4df84da857

Refactor singleton type registration code
Previously each singleton type was registered as an implicit separate
import.  This commit changes the code so that these types are treated
just like any other type in the registration sense.

It also ensures that singleton types are instantiated per-engine.

Change-Id: I5c81c4ca5bf65210f7125d74a62a282a21838068
Reviewed-by: Matthew Vogt <matthew.vogt@nokia.com>

tools/qmlplugindump/main.cpp
@@ -177,17 +177,6 @@ QByteArray convertToId(const QMetaObject *mo)
     return className;
 }
 
-/* All exported singleton Types are collected into this list */
-class SingletonType {
-public:
-    QString uri;
-    int majorVersion;
-    int minorVersion;
-    QByteArray objectId;
-    QString typeName;
-};
-QList<SingletonType> singletonTypes;
-
 QSet<const QMetaObject *> collectReachableMetaObjects(QQmlEngine *engine, const QList<QQmlType *> &skip = QList<QQmlType *>())
 {
     QSet<const QMetaObject *> metas;
@@ -252,41 +241,28 @@ QSet<const QMetaObject *> collectReachableMetaObjects(QQmlEngine *engine, const 
             continue;
 
         inObjectInstantiation = tyName;
-        QObject *object = ty->create();
+        QObject *object = 0;
+
+        if (ty->isSingleton()) {
+            QQmlType::SingletonInstanceInfo *siinfo = ty->singletonInstanceInfo();
+            if (siinfo->qobjectCallback) {
+                siinfo->init(engine);
+                collectReachableMetaObjects(object, &metas);
+                object = siinfo->qobjectApi(engine);
+            } else {
+                inObjectInstantiation.clear();
+                continue; // we don't handle QJSValue singleton types.
+            }
+        } else {
+            ty->create();
+        }
+
         inObjectInstantiation.clear();
 
         if (object)
             collectReachableMetaObjects(object, &metas);
         else
             qWarning() << "Could not create" << tyName;
-    }
-
-    // extract exported singleton type
-    QHashIterator<QString, QList<QQmlMetaType::SingletonType> > singletonTypeIt(QQmlMetaType::singletonTypes());
-    while (singletonTypeIt.hasNext()) {
-        singletonTypeIt.next();
-        foreach (const QQmlMetaType::SingletonType &api, singletonTypeIt.value()) {
-            SingletonType singletonType;
-            singletonType.uri = singletonTypeIt.key();
-            singletonType.majorVersion = api.major;
-            singletonType.minorVersion = api.minor;
-            singletonType.typeName = api.typeName;
-
-            if (api.qobject) {
-                if (QObject *object = (*api.qobject)(engine, engine)) {
-                    collectReachableMetaObjects(object, &metas);
-                    singletonType.objectId = convertToId(object->metaObject()->className());
-                    delete object;
-                }
-            } else if (api.script) {
-                qWarning() << "Can't dump the singleton type in " << singletonType.uri << ". QJSValue based singleton Type is not supported.";
-//                QJSValue value = (*api.script)(engine, engine);
-//                IdToObjectHash jsObjects;
-//                collectReachableJSObjects(value, &jsObjects, &metas);
-            }
-
-            singletonTypes += singletonType;
-        }
     }
 
     return metas;
@@ -415,19 +391,6 @@ public:
                 dump(meta->method(index), implicitSignals);
         }
 
-        qml->writeEndObject();
-    }
-
-    void dump(const SingletonType &api)
-    {
-        qml->writeStartObject(QLatin1String("SingletonType"));
-        if (api.uri != relocatableModuleUri)
-            qml->writeScriptBinding(QLatin1String("uri"), enquote(api.uri));
-        qml->writeScriptBinding(QLatin1String("version"), QString("%1.%2").arg(
-                                    QString::number(api.majorVersion),
-                                    QString::number(api.minorVersion)));
-        qml->writeScriptBinding(QLatin1String("name"), enquote(api.objectId));
-        qml->writeScriptBinding(QLatin1String("typeName"), enquote(api.typeName));
         qml->writeEndObject();
     }
 
@@ -791,11 +754,6 @@ int main(int argc, char *argv[])
     // properties using the QEasingCurve type get useful type information.
     if (pluginImportUri.isEmpty())
         dumper.writeEasingCurve();
-
-    // write out singleton type elements
-    foreach (const SingletonType &api, singletonTypes) {
-        dumper.dump(api);
-    }
 
     qml.writeEndObject();
     qml.writeEndDocument();

src/qml/qml/qqml.h
@@ -441,22 +441,25 @@ inline int qmlRegisterSingletonType(const char *uri, int versionMajor, int versi
 
         uri, versionMajor, versionMinor, typeName,
 
-        callback, 0, 0
+        callback, 0, 0, 0, 0
     };
 
     return QQmlPrivate::qmlregister(QQmlPrivate::SingletonRegistration, &api);
 }
 
+static const int CurrentSingletonTypeRegistrationVersion = 2;
 template <typename T>
 inline int qmlRegisterSingletonType(const char *uri, int versionMajor, int versionMinor, const char *typeName,
                                 QObject *(*callback)(QQmlEngine *, QJSEngine *))
 {
+    QML_GETTYPENAMES
+
     QQmlPrivate::RegisterSingletonType api = {
-        1,
+        CurrentSingletonTypeRegistrationVersion,
 
         uri, versionMajor, versionMinor, typeName,
 
-        0, callback, &T::staticMetaObject
+        0, callback, &T::staticMetaObject, qRegisterNormalizedMetaType<T *>(pointerName.constData()), 0
     };
 
     return QQmlPrivate::qmlregister(QQmlPrivate::SingletonRegistration, &api);

src/qml/qml/qqmlcompiler.cpp
@@ -896,7 +896,7 @@ void QQmlCompiler::compileTree(QQmlScript::Object *tree)
         output->importCache->add(qualifier, scriptIndex++, enclosingNamespace);
     }
 
-    unit->imports().populateCache(output->importCache, engine);
+    unit->imports().populateCache(output->importCache);
 
     if (!buildObject(tree, BindingContext()) || !completeComponentBuild())
         return;

src/qml/qml/qqmlengine.cpp
@@ -462,10 +462,6 @@ QQmlEnginePrivate::~QQmlEnginePrivate()
         (*iter)->release();
     for(QHash<QPair<QQmlType *, int>, QQmlPropertyCache *>::Iterator iter = typePropertyCache.begin(); iter != typePropertyCache.end(); ++iter)
         (*iter)->release();
-    for (QHash<QQmlMetaType::SingletonType, QQmlMetaType::SingletonInstance *>::Iterator iter = singletonTypeInstances.begin(); iter != singletonTypeInstances.end(); ++iter) {
-        delete (*iter)->qobjectApi;
-        delete *iter;
-    }
     for (QHash<int, QQmlCompiledData *>::Iterator iter = m_compositeTypes.begin(); iter != m_compositeTypes.end(); ++iter)
         iter.value()->isRegisteredWithEngine = false;
 }
@@ -743,21 +739,13 @@ QQmlEngine::~QQmlEngine()
     // may be required to handle the destruction signal.
     QQmlContextData::get(rootContext())->emitDestruction();
 
-    // if we are the parent of any of the qobject singleton type instances,
-    // we need to remove them from our internal list, in order to prevent
-    // a segfault in engine private dtor.
-    QList<QQmlMetaType::SingletonType> keys = d->singletonTypeInstances.keys();
-    QObject *currQObjectApi = 0;
-    QQmlMetaType::SingletonInstance *currInstance = 0;
-    foreach (const QQmlMetaType::SingletonType &key, keys) {
-        currInstance = d->singletonTypeInstances.value(key);
-        currQObjectApi = currInstance->qobjectApi;
-        if (this->children().contains(currQObjectApi)) {
-            delete currQObjectApi;
-            delete currInstance;
-            d->singletonTypeInstances.remove(key);
-        }
-    }
+    // clean up all singleton type instances which we own.
+    // we do this here and not in the private dtor since otherwise a crash can
+    // occur (if we are the QObject parent of the QObject singleton instance)
+    // XXX TODO: performance -- store list of singleton types separately?
+    QList<QQmlType*> singletonTypes = QQmlMetaType::qmlSingletonTypes();
+    foreach (QQmlType *currType, singletonTypes)
+        currType->singletonInstanceInfo()->destroy(this);
 
     if (d->incubationController)
         d->incubationController->d = 0;
@@ -1888,23 +1876,6 @@ QQmlPropertyCache *QQmlEnginePrivate::createCache(QQmlType *type, int minorVersi
     }
 
     return raw;
-}
-
-QQmlMetaType::SingletonInstance *
-QQmlEnginePrivate::singletonTypeInstance(const QQmlMetaType::SingletonType &module)
-{
-    Locker locker(this);
-
-    QQmlMetaType::SingletonInstance *a = singletonTypeInstances.value(module);
-    if (!a) {
-        a = new QQmlMetaType::SingletonInstance;
-        a->scriptCallback = module.script;
-        a->qobjectCallback = module.qobject;
-        a->instanceMetaObject = module.instanceMetaObject;
-        singletonTypeInstances.insert(module, a);
-    }
-
-    return a;
 }
 
 bool QQmlEnginePrivate::isQObject(int t)

src/qml/qml/qqmlengine_p.h
@@ -221,9 +221,6 @@ public:
     inline static void deleteInEngineThread(QQmlEngine *, T *);
 
     // These methods may be called from the loader thread
-    QQmlMetaType::SingletonInstance *singletonTypeInstance(const QQmlMetaType::SingletonType &module);
-
-    // These methods may be called from the loader thread
     inline QQmlPropertyCache *cache(QObject *obj);
     inline QQmlPropertyCache *cache(const QMetaObject *);
     inline QQmlPropertyCache *cache(QQmlType *, int, QQmlError &error);
@@ -303,7 +300,6 @@ private:
 
     // These members must be protected by a QQmlEnginePrivate::Locker as they are required by
     // the threaded loader.  Only access them through their respective accessor methods.
-    QHash<QQmlMetaType::SingletonType, QQmlMetaType::SingletonInstance *> singletonTypeInstances;
     QHash<const QMetaObject *, QQmlPropertyCache *> propertyCache;
     QHash<QPair<QQmlType *, int>, QQmlPropertyCache *> typePropertyCache;
     QHash<int, int> m_qmlLists;

src/qml/qml/qqmlimport.cpp
@@ -290,23 +290,15 @@ QUrl QQmlImports::baseUrl() const
     return d->baseUrl;
 }
 
-void QQmlImports::populateCache(QQmlTypeNameCache *cache, QQmlEngine *engine) const
+void QQmlImports::populateCache(QQmlTypeNameCache *cache) const
 {
     const QQmlImportNamespace &set = d->unqualifiedset;
 
     for (int ii = set.imports.count() - 1; ii >= 0; --ii) {
         const QQmlImportNamespace::Import *import = set.imports.at(ii);
         QQmlTypeModule *module = QQmlMetaType::typeModule(import->uri, import->majversion);
-        if (module)
+        if (module) {
             cache->m_anonymousImports.append(QQmlTypeModuleVersion(module, import->minversion));
-
-        QQmlMetaType::SingletonType singletonType = QQmlMetaType::singletonType(import->uri, import->majversion,
-                                                                    import->minversion);
-        if (singletonType.script || singletonType.qobject) {
-            QQmlEnginePrivate *ep = QQmlEnginePrivate::get(engine);
-            QQmlMetaType::SingletonInstance *apiInstance = ep->singletonTypeInstance(singletonType);
-
-            cache->addSingletonType(singletonType.typeName, apiInstance);
         }
     }
 
@@ -320,16 +312,6 @@ void QQmlImports::populateCache(QQmlTypeNameCache *cache, QQmlEngine *engine) co
             if (module) {
                 QQmlTypeNameCache::Import &typeimport = cache->m_namedImports[set.prefix];
                 typeimport.modules.append(QQmlTypeModuleVersion(module, import->minversion));
-            }
-
-            QQmlMetaType::SingletonType singletonType = QQmlMetaType::singletonType(import->uri, import->majversion,
-                                                                        import->minversion);
-            if (singletonType.script || singletonType.qobject) {
-                QQmlEnginePrivate *ep = QQmlEnginePrivate::get(engine);
-                QQmlMetaType::SingletonInstance *apiInstance = ep->singletonTypeInstance(singletonType);
-
-                cache->add(set.prefix);
-                cache->addSingletonType(singletonType.typeName, apiInstance, set.prefix);
             }
         }
     }

src/qml/qml/qqmlimport_p.h
@@ -112,7 +112,7 @@ public:
                       const QString &uri, int vmaj, int vmin,
                       QString *qmldirFilePath, QString *url);
 
-    void populateCache(QQmlTypeNameCache *cache, QQmlEngine *) const;
+    void populateCache(QQmlTypeNameCache *cache) const;
 
     struct ScriptReference
     {

commit 5b47966f5c7910e29f456d9a809f120a99c10ee4

qmlplugindump: Fix dumping of types
Dumping broke with commit 70a2c049.

Change-Id: Ifbae3dbc3e2d255c1e7f8848c579c04d0bf4b48e
Reviewed-by: Christian Kamm <christian.d.kamm@nokia.com>

tools/qmlplugindump/main.cpp
@@ -254,7 +254,7 @@ QSet<const QMetaObject *> collectReachableMetaObjects(QQmlEngine *engine, const 
                 continue; // we don't handle QJSValue singleton types.
             }
         } else {
-            ty->create();
+            object = ty->create();
         }
 
         inObjectInstantiation.clear();