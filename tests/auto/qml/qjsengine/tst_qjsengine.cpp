5958b88d0dca3058d4fd8d10260e647f7242ca2b
J-P Nurmi <j-p.nurmi@nokia.com>
2012-09-10 17:42:01 +0200

    tst_QJSEngine::jsFutureReservedWords(): QSKIP -> QEXPECT_FAIL
    Task-number: QTBUG-27193
    Change-Id: Iaf0bed44374396e59ad3d7f977e3f29d7d5112ee
    Reviewed-by: Sergio Ahumada <sergio.ahumada@nokia.com>

--- tests/auto/qml/qjsengine/tst_qjsengine.cpp
+++ tests/auto/qml/qjsengine/tst_qjsengine.cpp
@@ -2357,7 +2357,6 @@ void tst_QJSEngine::jsFutureReservedWords_data()
 
 void tst_QJSEngine::jsFutureReservedWords()
 {
-    QSKIP("Fails");
     // See ECMA-262 Section 7.6.1.2, "Future Reserved Words".
     // In real-world implementations, most of these words are
     // actually allowed as normal identifiers.
@@ -2378,6 +2377,16 @@ void tst_QJSEngine::jsFutureReservedWords()
         // this should probably be allowed (see task 162567)
         QJSEngine eng;
         QJSValue ret = eng.evaluate("o = {}; o." + word + " = 123");
+
+        QEXPECT_FAIL("class", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("const", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("debugger", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("enum", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("export", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("extends", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("import", "QTBUG-27193", Abort);
+        QEXPECT_FAIL("super", "QTBUG-27193", Abort);
+
         QCOMPARE(ret.isNumber(), allowed);
         QCOMPARE(!ret.isError(), allowed);
     }
