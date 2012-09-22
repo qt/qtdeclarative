b76104cf983b930c0eea68ee6a4b8b403a513657
Thiago Macieira <thiago.macieira@intel.com>
2012-09-22 13:47:55 +0200

    Add QT_{BEGIN,END}_{NAMESPACE,HEADER} to qtquickglobal.h
    Unit test is testing for it
    
    Change-Id: Ie37a0f7103dbe8c19a4cfbca2d4fe026c805ba0b
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>
    Reviewed-by: hjk <qthjk@ovi.com>

--- src/quick/qtquickglobal.h
+++ src/quick/qtquickglobal.h
@@ -44,6 +44,9 @@
 
 #include <QtCore/qglobal.h>
 
+QT_BEGIN_HEADER
+QT_BEGIN_NAMESPACE
+
 #ifndef QT_STATIC
 #  if defined(QT_BUILD_QUICK_LIB)
 #    define Q_QUICK_EXPORT Q_DECL_EXPORT
@@ -53,5 +56,8 @@
 #else
 #  define Q_QUICK_EXPORT
 #endif
+
+QT_END_NAMESPACE
+QT_END_HEADER
 
 #endif // QTQUICKGLOBAL_H
