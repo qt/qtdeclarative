d89f1dbe16b80b5e375f213c45b6130443f61a48
Fabian Bumberger <fbumberger@rim.com>
2013-01-14 16:01:38 +0100

    Fixing a mistake in the GridView documentation
    The item size of the GridView is solely determined by the cellHeight and cellWidth property.
    I.e. the GridView will not resize the cells based on the root item size of the delegate.
    
    Change-Id: I007e4b1719d78ebf126d166c89bfe6c64e2206f7
    Reviewed-by: Alan Alpert <aalpert@rim.com>

--- src/quick/items/qquickgridview.cpp
+++ src/quick/items/qquickgridview.cpp
@@ -1350,8 +1350,8 @@ void QQuickGridView::setHighlightFollowsCurrentItem(bool autoHighlight)
     that is not needed for the normal display of the delegate in a \l Loader which
     can load additional components when needed.
 
-    The GridView will layout the items based on the size of the root item
-    in the delegate.
+    The item size of the GridView is determined by cellHeight and cellWidth. It will not resize the items
+    based on the size of the root item in the delegate.
 
     \note Delegates are instantiated as needed and may be destroyed at any time.
     State should \e never be stored in a delegate.
