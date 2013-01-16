qt/qtdeclarative

commit e5f5251a020be728015068f3cd033f55f1d6b693

Replaced unnecessary vector with a boolean.
Change-Id: Iea163e59b0a8efbe79065844b77e782330fc1564
Reviewed-by: Lars Knoll <lars.knoll@digia.com>

qv4isel_p.cpp
@@ -49,8 +49,7 @@ VM::Function *EvalInstructionSelection::createFunctionMapping(VM::ExecutionEngin
         if (local)
             vmFunction->locals.append(*local);
 
-    foreach (IR::Function *function, irFunction->nestedFunctions)
-        vmFunction->nestedFunctions.append(createFunctionMapping(engine, function));
+    vmFunction->hasNestedFunctions = !irFunction->nestedFunctions.isEmpty();
 
     if (engine->debugger)
         engine->debugger->mapFunction(vmFunction, irFunction);

qmljs_objects.h
@@ -231,25 +231,24 @@ struct Function {
 
     QList<QString> formals;
     QList<QString> locals;
-    QVector<Function *> nestedFunctions;
 
-    bool hasDirectEval: 1;
+    bool hasNestedFunctions  : 1;
+    bool hasDirectEval       : 1;
     bool usesArgumentsObject : 1;
-    bool isStrict: 1;
+    bool isStrict            : 1;
 
     Function(const QString &name)
         : name(name)
         , code(0)
         , codeData(0)
+        , hasNestedFunctions(0)
         , hasDirectEval(false)
         , usesArgumentsObject(false)
         , isStrict(false)
     {}
     ~Function();
 
-    inline bool hasNestedFunctions() const { return !nestedFunctions.isEmpty(); }
-
-    inline bool needsActivation() const { return hasNestedFunctions() || hasDirectEval || usesArgumentsObject; }
+    inline bool needsActivation() const { return hasNestedFunctions || hasDirectEval || usesArgumentsObject; }
 };
 
 struct FunctionObject: Object {

commit 825dbba8c89be424765015d22754fe318647472f

Fix createFunctionMapping
This fixes a bad regression introduced with
e5f5251a020be728015068f3cd033f55f1d6b693.

Change-Id: If4961f46171ea6c919daae7eaab9ef70c7b0608e
Reviewed-by: Erik Verbruggen <erik.verbruggen@digia.com>

qv4isel_p.cpp
@@ -49,6 +49,9 @@ VM::Function *EvalInstructionSelection::createFunctionMapping(VM::ExecutionEngin
         if (local)
             vmFunction->locals.append(*local);
 
+    foreach (IR::Function *function, irFunction->nestedFunctions)
+        createFunctionMapping(engine, function);
+
     vmFunction->hasNestedFunctions = !irFunction->nestedFunctions.isEmpty();
 
     if (engine->debugger)