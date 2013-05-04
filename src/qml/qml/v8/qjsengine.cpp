2a71d0e938a8ea42223d33dfab71cf9bb982d804
Lars Knoll <lars.knoll@digia.com>
2013-05-04 23:33:23 +0200

    Fix QJSEngine::evaluate
    if EvalFunction::parseSource returns 0, it means there wasn't
    anything to parse, not that the input was invalid, so simply
    return undefined in this case.
    
    Change-Id: I13aafaba40723d4aa7c0f2e7f3eddfac86239cd0
    Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

--- src/qml/qml/v8/qjsengine.cpp
+++ src/qml/qml/v8/qjsengine.cpp
@@ -258,7 +258,7 @@ QJSValue QJSEngine::evaluate(const QString& program, const QString& fileName, in
         QV4::Function *f = QV4::EvalFunction::parseSource(d->m_v4Engine->current, fileName, program, QQmlJS::Codegen::EvalCode,
                                                                         d->m_v4Engine->current->strictMode, true);
         if (!f)
-            return new QJSValuePrivate(d->m_v4Engine, d->m_v4Engine->newSyntaxErrorObject(QString()));
+            return QJSValue();
 
         QV4::Value result = d->m_v4Engine->run(f);
         return new QJSValuePrivate(d->m_v4Engine, result);
