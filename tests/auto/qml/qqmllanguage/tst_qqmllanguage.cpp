bb6916dc5f52020229fd76c851fa5669f9b9ccf9
Dominik Holland <dominik.holland@pelagicore.com>
2013-07-01 22:28:06 +0200

    Fix crash tst_qqmllanguage
    qmlClearTypeRegistrations() shouldn't be called when instances of
    QQmlEngine are still open. This can lead to unexpected behavior.
    
    For the qmlClearTypeRegistrations() function there is separate
    autotest
    
    Task-number: QTBUG-32078
    Change-Id: I32612bc82602593d902bbffc58911e427b50524f
    Reviewed-by: Alan Alpert <aalpert@blackberry.com>

--- tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
+++ tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
@@ -245,7 +245,6 @@ private:
 void tst_qqmllanguage::cleanupTestCase()
 {
     QVERIFY(QFile::remove(testFile(QString::fromUtf8("I18nType\303\201\303\242\303\243\303\244\303\245.qml"))));
-    qmlClearTypeRegistrations(); // Should not crash
 }
 
 void tst_qqmllanguage::insertedSemicolon_data()
