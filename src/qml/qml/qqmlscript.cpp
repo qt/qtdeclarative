dfa234a60a112903995944573a7d2cf400af7f24
Mitch Curtis <mitch.curtis@digia.com>
2013-10-09 12:44:13 +0200

    Fix MSVC2010 warnings.
    Change-Id: Id7a2465db4f9b1cd4d3b7ecd61538ee7110893df
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/qml/qml/qqmlscript.cpp
+++ src/qml/qml/qqmlscript.cpp
@@ -991,7 +991,7 @@ bool ProcessAST::visit(AST::UiPublicMember *node)
             for(int typeIndex = 0; typeIndex < propTypeNameToTypesCount; ++typeIndex) {
                 const TypeNameToType *t = propTypeNameToTypes + typeIndex;
                 if (t->nameLength == size_t(memberType.length()) &&
-                    QHashedString::compare(memberType.constData(), t->name, t->nameLength)) {
+                    QHashedString::compare(memberType.constData(), t->name, int(t->nameLength))) {
                     type = t;
                     break;
                 }
@@ -1033,7 +1033,7 @@ bool ProcessAST::visit(AST::UiPublicMember *node)
         Object::DynamicProperty::Type type;
 
         if ((unsigned)memberType.length() == strlen("alias") && 
-            QHashedString::compare(memberType.constData(), "alias", strlen("alias"))) {
+            QHashedString::compare(memberType.constData(), "alias", int(strlen("alias")))) {
             type = Object::DynamicProperty::Alias;
             typeFound = true;
         } 
@@ -1041,7 +1041,7 @@ bool ProcessAST::visit(AST::UiPublicMember *node)
         for(int ii = 0; !typeFound && ii < propTypeNameToTypesCount; ++ii) {
             const TypeNameToType *t = propTypeNameToTypes + ii;
             if (t->nameLength == size_t(memberType.length()) &&
-                QHashedString::compare(memberType.constData(), t->name, t->nameLength)) {
+                QHashedString::compare(memberType.constData(), t->name, int(t->nameLength))) {
                 type = t->type;
                 typeFound = true;
             }
@@ -1053,7 +1053,7 @@ bool ProcessAST::visit(AST::UiPublicMember *node)
             if (typeModifier.isEmpty()) {
                 type = Object::DynamicProperty::Custom;
             } else if((unsigned)typeModifier.length() == strlen("list") && 
-                      QHashedString::compare(typeModifier.constData(), "list", strlen("list"))) {
+                      QHashedString::compare(typeModifier.constData(), "list", int(strlen("list")))) {
                 type = Object::DynamicProperty::CustomList;
             } else {
                 QQmlError error;
