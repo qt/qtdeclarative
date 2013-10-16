qt/qtdeclarative

commit 84627464eb11ca1149d46946b12e3c82eb54a8bf

Fallback to QMetaObject for properties not in QQmlPropertyCache
QQmlOpenMetaObject does not update the QQmlPropertyCache when new
properties are added, meaning that the QQmlPropertyCache might not
contain all of the dynamic properties of an object. Therefore, make
QQmlPropertyCache fallback to reading the QMetaObject when a property is
not found.

Task-number: QTBUG-31226
Change-Id: I760aaa155b1952f6f52ab9ce173fb9547f8e34a6
Reviewed-by: Alan Alpert <aalpert@blackberry.com>

src/qml/qml/qqmlpropertycache.cpp
@@ -1328,8 +1328,14 @@ QQmlPropertyData qQmlPropertyCacheCreate(const QMetaObject *metaObject, const QS
                     rv.load(p);
                     return rv;
                 } else {
-                    while (cmo && cmo->propertyOffset() >= idx)
+                    bool changed = false;
+                    while (cmo && cmo->propertyOffset() >= idx) {
                         cmo = cmo->superClass();
+                        changed = true;
+                    }
+                    /* If the "cmo" variable didn't change, set it to 0 to
+                     * avoid running into an infinite loop */
+                    if (!changed) cmo = 0;
                 }
             } else {
                 cmo = 0;
@@ -1395,7 +1401,8 @@ qQmlPropertyCacheProperty(QQmlEngine *engine, QObject *obj, const T &name,
 
     if (cache) {
         rv = cache->property(name, obj, context);
-    } else {
+    }
+    if (!rv) {
         local = qQmlPropertyCacheCreate(obj->metaObject(), qQmlPropertyCacheToString(name));
         if (local.isValid())
             rv = &local;

src/qml/qml/v8/qv8qobjectwrapper.cpp
@@ -524,7 +524,7 @@ v8::Handle<v8::Value> QV8QObjectWrapper::GetProperty(QV8Engine *engine, QObject 
         QQmlData *ddata = QQmlData::get(object, false);
         if (ddata && ddata->propertyCache)
             result = ddata->propertyCache->property(property, object, context);
-        else
+        if (!result)
             result = QQmlPropertyCache::property(engine->engine(), object, property, context, local);
     }
 

tests/auto/qml/qqmlpropertymap/tst_qqmlpropertymap.cpp
@@ -66,6 +66,7 @@ private slots:
     void crashBug();
     void QTBUG_17868();
     void metaObjectAccessibility();
+    void QTBUG_31226();
 };
 
 void tst_QQmlPropertyMap::insert()
@@ -310,6 +311,34 @@ void tst_QQmlPropertyMap::metaObjectAccessibility()
     QCOMPARE(map.metaObject()->className(), "MyEnhancedPropertyMap");
 
     QVERIFY2(messageHandler.messages().isEmpty(), qPrintable(messageHandler.messageString()));
+}
+
+void tst_QQmlPropertyMap::QTBUG_31226()
+{
+    /* Instantiate a property map from QML, and verify that property changes
+     * made from C++ are visible from QML */
+    QQmlEngine engine;
+    QQmlContext context(&engine);
+    qmlRegisterType<QQmlPropertyMap>("QTBUG_31226", 1, 0, "PropertyMap");
+    QQmlComponent c(&engine);
+    c.setData("import QtQuick 2.0\nimport QTBUG_31226 1.0\n"
+              "Item {\n"
+              "  property string myProp\n"
+              "  PropertyMap { id: qmlPropertyMap; objectName: \"qmlPropertyMap\" }\n"
+              "  Timer { interval: 5; running: true; onTriggered: { myProp = qmlPropertyMap.greeting; } }\n"
+              "}",
+              QUrl());
+    QObject *obj = c.create(&context);
+    QVERIFY(obj);
+
+    QQmlPropertyMap *qmlPropertyMap = obj->findChild<QQmlPropertyMap*>(QString("qmlPropertyMap"));
+    QVERIFY(qmlPropertyMap);
+
+    qmlPropertyMap->insert("greeting", QString("Hello world!"));
+    QTRY_COMPARE(obj->property("myProp").toString(), QString("Hello world!"));
+
+    delete obj;
+
 }
 
 QTEST_MAIN(tst_QQmlPropertyMap)

commit 7b1a8fa821ef5bac4ecad91be7f0e62809e80a12

Fix performance regression when doing property lookups for non-existant properties
Commit 84627464eb11ca1149d46946b12e3c82eb54a8bf introduced a performance
regression of falling back to reading the QMetaObject, when the lookup for a
property in QML failed, after we've tried in the property cache. This is very
very expensive to do and was only due to QQmlPropertyMap not correctly
invalidating the property cache.

Instead remove the property cache from the property map's QObject and on the
lookup side rely on the property cache being correct in the result (positive or
negative).

Change-Id: I8a013483203f2007d48b71feafa10b3ea02c53fd
Reviewed-by: Lars Knoll <lars.knoll@digia.com>

src/qml/jsruntime/qv4qobjectwrapper.cpp
@@ -263,7 +263,7 @@ QQmlPropertyData *QObjectWrapper::findProperty(ExecutionEngine *engine, QQmlCont
     QQmlPropertyData *result = 0;
     if (ddata && ddata->propertyCache)
         result = ddata->propertyCache->property(name, m_object, qmlContext);
-    if (!result)
+    else
         result = QQmlPropertyCache::property(engine->v8Engine->engine(), m_object, name, qmlContext, *local);
     return result;
 }

src/qml/qml/qqmlopenmetaobject.cpp
@@ -357,9 +357,18 @@ void QQmlOpenMetaObject::setCached(bool c)
 
 int QQmlOpenMetaObject::createProperty(const char *name, const char *)
 {
-    if (d->autoCreate)
-        return d->type->createProperty(name);
-    else
+    if (d->autoCreate) {
+        int result = d->type->createProperty(name);
+
+        if (QQmlData *ddata = QQmlData::get(d->object, /*create*/false)) {
+            if (ddata->propertyCache) {
+                ddata->propertyCache->release();
+                ddata->propertyCache = 0;
+            }
+        }
+
+        return result;
+    } else
         return -1;
 }
 

src/qml/qml/qqmlpropertycache.cpp
@@ -1419,8 +1419,7 @@ qQmlPropertyCacheProperty(QQmlEngine *engine, QObject *obj, T name,
 
     if (cache) {
         rv = cache->property(name, obj, context);
-    }
-    if (!rv) {
+    } else {
         local = qQmlPropertyCacheCreate(obj->metaObject(), qQmlPropertyCacheToString(name));
         if (local.isValid())
             rv = &local;