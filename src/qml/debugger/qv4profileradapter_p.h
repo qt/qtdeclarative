qt/qtdeclarative

commit eb8a09145759f53786706c748afb813e1475feef

V4 profiler adapter
This adapter attaches the V4 profiler to the QML profiling
infrastructure.

Change-Id: Ifaf77a3ba0790bc56d4b155260b0e05593ac799e
Reviewed-by: Kai Koehne <kai.koehne@digia.com>

src/qml/debugger/qv4profileradapter_p.h
@@ -1,0 +1,83 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the QtQml module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QV4PROFILERADAPTER_P_H
+#define QV4PROFILERADAPTER_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "jsruntime/qv4profiling_p.h"
+#include "qqmlabstractprofileradapter_p.h"
+
+#include <QStack>
+#include <QList>
+
+QT_BEGIN_NAMESPACE
+
+class QQmlProfilerService;
+class QV4ProfilerAdapter : public QQmlAbstractProfilerAdapter {
+    Q_OBJECT
+
+public:
+    QV4ProfilerAdapter(QQmlProfilerService *service, QV4::ExecutionEngine *engine);
+
+    virtual qint64 sendMessages(qint64 until, QList<QByteArray> &messages);
+
+public slots:
+    void receiveData(const QList<QV4::Profiling::FunctionCallProperties> &);
+
+private:
+    QList<QV4::Profiling::FunctionCallProperties> data;
+    QStack<qint64> stack;
+};
+
+QT_END_NAMESPACE
+
+#endif // QV4PROFILERADAPTER_P_H

src/qml/debugger/debugger.pri
@@ -9,7 +9,8 @@ SOURCES += \
     $$PWD/qv4debugservice.cpp \
     $$PWD/qqmlconfigurabledebugservice.cpp \
     $$PWD/qqmlenginecontrolservice.cpp \
-    $$PWD/qqmlabstractprofileradapter.cpp
+    $$PWD/qqmlabstractprofileradapter.cpp \
+    $$PWD/qv4profileradapter.cpp
 
 HEADERS += \
     $$PWD/qqmldebugservice_p.h \
@@ -29,4 +30,5 @@ HEADERS += \
     $$PWD/qqmlconfigurabledebugservice_p_p.h \
     $$PWD/qqmlenginecontrolservice_p.h \
     $$PWD/qqmlprofilerdefinitions_p.h \
-    $$PWD/qqmlabstractprofileradapter_p.h
+    $$PWD/qqmlabstractprofileradapter_p.h \
+    $$PWD/qv4profileradapter_p.h

src/qml/debugger/qqmlprofilerservice.cpp
@@ -41,6 +41,7 @@
 
 #include "qqmlprofilerservice_p.h"
 #include "qqmldebugserver_p.h"
+#include "qv4profileradapter_p.h"
 #include <private/qqmlengine_p.h>
 
 #include <QtCore/qdatastream.h>
@@ -209,7 +210,9 @@ void QQmlProfilerService::engineAboutToBeAdded(QQmlEngine *engine)
 
     QMutexLocker lock(configMutex());
     QQmlProfiler *qmlAdapter = new QQmlProfiler(this);
+    QV4ProfilerAdapter *v4Adapter = new QV4ProfilerAdapter(this, QV8Engine::getV4(engine->handle()));
     addEngineProfiler(qmlAdapter, engine);
+    addEngineProfiler(v4Adapter, engine);
     QQmlConfigurableDebugService::engineAboutToBeAdded(engine);
 }
 

src/qml/debugger/qv4profileradapter.cpp
@@ -1,0 +1,109 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the QtQml module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qv4profileradapter_p.h"
+#include "qqmlprofilerservice_p.h"
+#include "qqmldebugservice_p.h"
+
+QT_BEGIN_NAMESPACE
+
+QV4ProfilerAdapter::QV4ProfilerAdapter(QQmlProfilerService *service, QV4::ExecutionEngine *engine) :
+    QQmlAbstractProfilerAdapter(service)
+{
+    engine->enableProfiler();
+    connect(this, SIGNAL(profilingEnabled()), engine->profiler, SLOT(startProfiling()));
+    connect(this, SIGNAL(profilingEnabledWhileWaiting()), engine->profiler, SLOT(startProfiling()),
+            Qt::DirectConnection);
+    connect(this, SIGNAL(profilingDisabled()), engine->profiler, SLOT(stopProfiling()));
+    connect(this, SIGNAL(profilingDisabledWhileWaiting()), engine->profiler, SLOT(stopProfiling()),
+            Qt::DirectConnection);
+    connect(this, SIGNAL(dataRequested()), engine->profiler, SLOT(reportData()));
+    connect(this, SIGNAL(referenceTimeKnown(QElapsedTimer)),
+            engine->profiler, SLOT(setTimer(QElapsedTimer)));
+    connect(engine->profiler, SIGNAL(dataReady(QList<QV4::Profiling::FunctionCallProperties>)),
+            this, SLOT(receiveData(QList<QV4::Profiling::FunctionCallProperties>)));
+}
+
+
+qint64 QV4ProfilerAdapter::sendMessages(qint64 until, QList<QByteArray> &messages)
+{
+    QByteArray message;
+    while (true) {
+        while (!stack.empty() && (data.empty() || stack.top() <= data.front().start)) {
+            if (stack.top() > until)
+                return stack.top();
+            QQmlDebugStream d(&message, QIODevice::WriteOnly);
+            d << stack.pop() << RangeEnd << Javascript;
+            messages.append(message);
+        }
+        while (!data.empty() && (stack.empty() || data.front().start < stack.top())) {
+            if (data.front().start > until)
+                return data.front().start;
+            const QV4::Profiling::FunctionCallProperties &props = data.front();
+            QQmlDebugStream d_start(&message, QIODevice::WriteOnly);
+            d_start << props.start << RangeStart << Javascript;
+            messages.push_back(message);
+            message.clear();
+            QQmlDebugStream d_location(&message, QIODevice::WriteOnly);
+            d_location << props.start << RangeLocation << Javascript << props.file << props.line
+                       << props.column;
+            messages.push_back(message);
+            message.clear();
+            QQmlDebugStream d_data(&message, QIODevice::WriteOnly);
+            d_data << props.start << RangeData << Javascript << props.name;
+            messages.push_back(message);
+            message.clear();
+            stack.push(props.end);
+            data.pop_front();
+        }
+        if (stack.empty() && data.empty())
+            return -1;
+    }
+}
+
+void QV4ProfilerAdapter::receiveData(const QList<QV4::Profiling::FunctionCallProperties> &new_data)
+{
+    data = new_data;
+    stack.clear();
+    service->dataReady(this);
+}
+
+QT_END_NAMESPACE

tests/auto/qml/debugger/qqmlprofilerservice/tst_qqmlprofilerservice.cpp
@@ -536,18 +536,22 @@ void tst_QQmlProfilerService::signalSourceLocation()
     m_client->setTraceState(false);
     QVERIFY2(QQmlDebugTest::waitForSignal(m_client, SIGNAL(complete())), "No trace received in time.");
 
-    QVERIFY(m_client->traceMessages.count() >= 17);
+    QVERIFY(m_client->traceMessages.count() >= 20);
     // must start with "StartTrace"
     QCOMPARE(m_client->traceMessages.first().messageType, (int)QQmlProfilerClient::Event);
     QCOMPARE(m_client->traceMessages.first().detailType, (int)QQmlProfilerClient::StartTrace);
 
+    QVERIFY(m_client->traceMessages[14].messageType == QQmlProfilerClient::RangeLocation);
+    QVERIFY(m_client->traceMessages[14].detailType == QQmlProfilerClient::HandlingSignal);
     QVERIFY(m_client->traceMessages[14].detailData.endsWith("signalSourceLocation.qml"));
     QVERIFY(m_client->traceMessages[14].line == 8);
     QVERIFY(m_client->traceMessages[14].column == 28);
 
-    QVERIFY(m_client->traceMessages[16].detailData.endsWith("signalSourceLocation.qml"));
-    QVERIFY(m_client->traceMessages[16].line == 7);
-    QVERIFY(m_client->traceMessages[16].column == 21);
+    QVERIFY(m_client->traceMessages[19].messageType == QQmlProfilerClient::RangeLocation);
+    QVERIFY(m_client->traceMessages[19].detailType == QQmlProfilerClient::HandlingSignal);
+    QVERIFY(m_client->traceMessages[19].detailData.endsWith("signalSourceLocation.qml"));
+    QVERIFY(m_client->traceMessages[19].line == 7);
+    QVERIFY(m_client->traceMessages[19].column == 21);
 
     // must end with "EndTrace"
     QCOMPARE(m_client->traceMessages.last().messageType, (int)QQmlProfilerClient::Event);

commit 27e2593fa2948d3afc2ace4b36194f0c0e54b908

Compile fix for MSVC2013
This fixes an inclusion error caused by eb8a091 under WinRT x64 MSVC2013.

Change-Id: I3ebae0788264f4e3ccc0ae9f2bb4dbc4cceaa155
Reviewed-by: Kai Koehne <kai.koehne@digia.com>
Reviewed-by: Ulf Hermann <ulf.hermann@digia.com>

src/qml/debugger/qv4profileradapter_p.h
@@ -53,7 +53,7 @@
 // We mean it.
 //
 
-#include "jsruntime/qv4profiling_p.h"
+#include "qv4profiling_p.h"
 #include "qqmlabstractprofileradapter_p.h"
 
 #include <QStack>