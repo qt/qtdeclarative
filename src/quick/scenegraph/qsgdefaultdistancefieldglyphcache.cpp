qt/qtdeclarative

commit fd5c629f4336b1f79afc653fceaf0f930e71eccd

Don't use Vertexarrays
If custom Glcode is used and a buffer is not unbound
the drawcall will take the bound buffer over the
vertex array, so its a bad idea to use them, besides
the fact that in core profile buffers should be used.

Change-Id: I4107d85b664a0c67207652ae34c2569572fa6a8b
Reviewed-by: Gunnar Sletta <gunnar.sletta@jollamobile.com>

src/quick/scenegraph/qsgdefaultdistancefieldglyphcache.cpp
@@ -61,28 +61,18 @@ QSGDefaultDistanceFieldGlyphCache::QSGDefaultDistanceFieldGlyphCache(QSGDistance
     , m_maxTextureSize(0)
     , m_maxTextureCount(3)
     , m_blitProgram(0)
+    , m_blitBuffer(QOpenGLBuffer::VertexBuffer)
     , m_fboGuard(0)
 #if !defined(QT_OPENGL_ES_2)
     , m_funcs(0)
 #endif
 {
-    m_blitVertexCoordinateArray[0] = -1.0f;
-    m_blitVertexCoordinateArray[1] = -1.0f;
-    m_blitVertexCoordinateArray[2] =  1.0f;
-    m_blitVertexCoordinateArray[3] = -1.0f;
-    m_blitVertexCoordinateArray[4] =  1.0f;
-    m_blitVertexCoordinateArray[5] =  1.0f;
-    m_blitVertexCoordinateArray[6] = -1.0f;
-    m_blitVertexCoordinateArray[7] =  1.0f;
-
-    m_blitTextureCoordinateArray[0] = 0.0f;
-    m_blitTextureCoordinateArray[1] = 0.0f;
-    m_blitTextureCoordinateArray[2] = 1.0f;
-    m_blitTextureCoordinateArray[3] = 0.0f;
-    m_blitTextureCoordinateArray[4] = 1.0f;
-    m_blitTextureCoordinateArray[5] = 1.0f;
-    m_blitTextureCoordinateArray[6] = 0.0f;
-    m_blitTextureCoordinateArray[7] = 1.0f;
+    m_blitBuffer.create();
+    m_blitBuffer.bind();
+    static GLfloat buffer[16] = {1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f,
+                                 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f};
+    m_blitBuffer.allocate(buffer, sizeof(buffer));
+    m_blitBuffer.release();
 
     m_areaAllocator = new QSGAreaAllocator(QSize(maxTextureSize(), m_maxTextureCount * maxTextureSize()));
 }
@@ -415,12 +405,13 @@ void QSGDefaultDistanceFieldGlyphCache::resizeTexture(TextureInfo *texInfo, int 
 
     glViewport(0, 0, oldWidth, oldHeight);
 
-    ctx->functions()->glVertexAttribPointer(QT_VERTEX_COORDS_ATTR, 2, GL_FLOAT, GL_FALSE, 0, m_blitVertexCoordinateArray);
-    ctx->functions()->glVertexAttribPointer(QT_TEXTURE_COORDS_ATTR, 2, GL_FLOAT, GL_FALSE, 0, m_blitTextureCoordinateArray);
+    m_blitBuffer.bind();
 
     m_blitProgram->bind();
     m_blitProgram->enableAttributeArray(int(QT_VERTEX_COORDS_ATTR));
     m_blitProgram->enableAttributeArray(int(QT_TEXTURE_COORDS_ATTR));
+    m_blitProgram->setAttributeBuffer(int(QT_VERTEX_COORDS_ATTR), GL_FLOAT, 0, 2);
+    m_blitProgram->setAttributeBuffer(int(QT_TEXTURE_COORDS_ATTR), GL_FLOAT, 8, 2);
     m_blitProgram->disableAttributeArray(int(QT_OPACITY_ATTR));
     m_blitProgram->setUniformValue("imageTexture", GLuint(0));
 
@@ -456,6 +447,7 @@ void QSGDefaultDistanceFieldGlyphCache::resizeTexture(TextureInfo *texInfo, int 
 
     m_blitProgram->disableAttributeArray(int(QT_VERTEX_COORDS_ATTR));
     m_blitProgram->disableAttributeArray(int(QT_TEXTURE_COORDS_ATTR));
+    m_blitBuffer.release();
 }
 
 bool QSGDefaultDistanceFieldGlyphCache::useTextureResizeWorkaround() const

src/quick/scenegraph/qsgdefaultdistancefieldglyphcache_p.h
@@ -45,6 +45,7 @@
 #include "qsgadaptationlayer_p.h"
 #include <QtGui/qopenglfunctions.h>
 #include <qopenglshaderprogram.h>
+#include <qopenglbuffer.h>
 #include <QtGui/private/qopenglengineshadersource_p.h>
 #include <private/qsgareaallocator_p.h>
 
@@ -133,8 +134,7 @@ private:
     QSGAreaAllocator *m_areaAllocator;
 
     QOpenGLShaderProgram *m_blitProgram;
-    GLfloat m_blitVertexCoordinateArray[8];
-    GLfloat m_blitTextureCoordinateArray[8];
+    QOpenGLBuffer m_blitBuffer;
 
     QOpenGLSharedResourceGuard *m_fboGuard;
 #if !defined(QT_OPENGL_ES_2)

commit 10f0485464acebd281f37b78f9ab5d02ee4ada9b

Fixed regression from fd5c629f4336b1f79
Buffer offsets were wrong, and a type
in the buffer content.

Task-number: QTBUG-36989

Change-Id: I863976420d495b006692c4b614f74c285649cb1a
Reviewed-by: Mitch Curtis <mitch.curtis@digia.com>
Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
Reviewed-by: Gunnar Sletta <gunnar.sletta@jollamobile.com>

src/quick/scenegraph/qsgdefaultdistancefieldglyphcache.cpp
@@ -69,7 +69,7 @@ QSGDefaultDistanceFieldGlyphCache::QSGDefaultDistanceFieldGlyphCache(QSGDistance
 {
     m_blitBuffer.create();
     m_blitBuffer.bind();
-    static GLfloat buffer[16] = {1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f,
+    static GLfloat buffer[16] = {-1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f,
                                  0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f};
     m_blitBuffer.allocate(buffer, sizeof(buffer));
     m_blitBuffer.release();
@@ -405,13 +405,21 @@ void QSGDefaultDistanceFieldGlyphCache::resizeTexture(TextureInfo *texInfo, int 
 
     glViewport(0, 0, oldWidth, oldHeight);
 
-    m_blitBuffer.bind();
-
+    const bool vaoInit = m_vao.isCreated();
+    if (isCoreProfile()) {
+        if ( !vaoInit )
+            m_vao.create();
+        m_vao.bind();
+    }
     m_blitProgram->bind();
-    m_blitProgram->enableAttributeArray(int(QT_VERTEX_COORDS_ATTR));
-    m_blitProgram->enableAttributeArray(int(QT_TEXTURE_COORDS_ATTR));
-    m_blitProgram->setAttributeBuffer(int(QT_VERTEX_COORDS_ATTR), GL_FLOAT, 0, 2);
-    m_blitProgram->setAttributeBuffer(int(QT_TEXTURE_COORDS_ATTR), GL_FLOAT, 8, 2);
+    if (!vaoInit || !isCoreProfile()) {
+        m_blitBuffer.bind();
+
+        m_blitProgram->enableAttributeArray(int(QT_VERTEX_COORDS_ATTR));
+        m_blitProgram->enableAttributeArray(int(QT_TEXTURE_COORDS_ATTR));
+        m_blitProgram->setAttributeBuffer(int(QT_VERTEX_COORDS_ATTR), GL_FLOAT, 0, 2);
+        m_blitProgram->setAttributeBuffer(int(QT_TEXTURE_COORDS_ATTR), GL_FLOAT, 32, 2);
+    }
     m_blitProgram->disableAttributeArray(int(QT_OPACITY_ATTR));
     m_blitProgram->setUniformValue("imageTexture", GLuint(0));
 
@@ -447,7 +455,8 @@ void QSGDefaultDistanceFieldGlyphCache::resizeTexture(TextureInfo *texInfo, int 
 
     m_blitProgram->disableAttributeArray(int(QT_VERTEX_COORDS_ATTR));
     m_blitProgram->disableAttributeArray(int(QT_TEXTURE_COORDS_ATTR));
-    m_blitBuffer.release();
+    if (isCoreProfile())
+        m_vao.release();
 }
 
 bool QSGDefaultDistanceFieldGlyphCache::useTextureResizeWorkaround() const

src/quick/scenegraph/qsgdefaultdistancefieldglyphcache_p.h
@@ -46,6 +46,7 @@
 #include <QtGui/qopenglfunctions.h>
 #include <qopenglshaderprogram.h>
 #include <qopenglbuffer.h>
+#include <qopenglvertexarrayobject.h>
 #include <QtGui/private/qopenglengineshadersource_p.h>
 #include <private/qsgareaallocator_p.h>
 
@@ -135,6 +136,7 @@ private:
 
     QOpenGLShaderProgram *m_blitProgram;
     QOpenGLBuffer m_blitBuffer;
+    QOpenGLVertexArrayObject m_vao;
 
     QOpenGLSharedResourceGuard *m_fboGuard;
 #if !defined(QT_OPENGL_ES_2)