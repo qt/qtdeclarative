qt/qtdeclarative

commit 1ca5e82cccae04b3f0e91f6445811b967018b477

QML testlib: Do not modify the arguments passed into QGuiApplication.
argc/argv passed  into QGuiApplication must match those
of main on Windows due to the special command line parsing.

Task-number: QTBUG-36466
Change-Id: Id722692d2bdc18a6627fdaeffb349b78137ae314
Reviewed-by: Alan Alpert <aalpert@blackberry.com>

src/qmltest/quicktest.cpp
@@ -208,8 +208,10 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
 #ifdef QT_QMLTEST_WITH_WIDGETS
     bool withWidgets = false;
 #endif
-    int outargc = 1;
     int index = 1;
+    QScopedArrayPointer<char *> testArgV(new char *[argc + 1]);
+    testArgV[0] = argv[0];
+    int testArgC = 1;
     while (index < argc) {
         if (strcmp(argv[index], "-import") == 0 && (index + 1) < argc) {
             imports += stripQuotes(QString::fromLocal8Bit(argv[index + 1]));
@@ -227,15 +229,11 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
         } else if (strcmp(argv[index], "-translation") == 0 && (index + 1) < argc) {
             translationFile = stripQuotes(QString::fromLocal8Bit(argv[index + 1]));
             index += 2;
-        } else if (outargc != index) {
-            argv[outargc++] = argv[index++];
         } else {
-            ++outargc;
-            ++index;
+            testArgV[testArgC++] = argv[index++];
         }
     }
-    argv[outargc] = 0;
-    argc = outargc;
+    testArgV[testArgC] = 0;
 
     QCoreApplication* app = 0;
     if (!QCoreApplication::instance()) {
@@ -256,7 +254,7 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
     QuickTestResult::setCurrentAppname(argv[0]);
     QuickTestResult::setProgramName(name);
 
-    QuickTestResult::parseArgs(argc, argv);
+    QuickTestResult::parseArgs(testArgC, testArgV.data());
 
 #ifndef QT_NO_TRANSLATION
     QTranslator translator;

commit cf06b028f71fb80616be7fbb3ef3aaf7d5474eca

QtQuick test: Do not complain about Qt specific arguments
Commit 1ca5e82cccae0 caused a regression for Qt arguments like
-qmlsjsdebugger=xxx . These are automatically removed from argv by
QCoreApplication, but since we copied argv before instantiating
QCoreApplication this didn't have any effect.

Fix this by moving Q[Core]Application instantiation again before
the parsing and copying of testlib-specific arguments.

Task-number: QTBUG-37793
Change-Id: Ief41640b6cf3251f700a5d24d2e1141233a3888f
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
Reviewed-by: Liang Qi <liang.qi@digia.com>

src/qmltest/quicktest.cpp
@@ -198,6 +198,29 @@ bool qWaitForSignal(QObject *obj, const char* signal, int timeout = 5000)
 
 int quick_test_main(int argc, char **argv, const char *name, const char *sourceDir)
 {
+    // Peek at arguments to check for '-widgets' argument
+#ifdef QT_QMLTEST_WITH_WIDGETS
+    bool withWidgets = false;
+    for (int index = 1; index < argc; ++index) {
+        if (strcmp(argv[index], "-widgets") == 0) {
+            withWidgets = true;
+            break;
+        }
+    }
+#endif
+
+    QCoreApplication *app = 0;
+    if (!QCoreApplication::instance()) {
+#ifdef QT_QMLTEST_WITH_WIDGETS
+        if (withWidgets)
+            app = new QApplication(argc, argv);
+        else
+#endif
+        {
+            app = new QGuiApplication(argc, argv);
+        }
+    }
+
     // Look for QML-specific command-line options.
     //      -import dir         Specify an import directory.
     //      -input dir          Specify the input directory for test cases.
@@ -205,9 +228,6 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
     QStringList imports;
     QString testPath;
     QString translationFile;
-#ifdef QT_QMLTEST_WITH_WIDGETS
-    bool withWidgets = false;
-#endif
     int index = 1;
     QScopedArrayPointer<char *> testArgV(new char *[argc + 1]);
     testArgV[0] = argv[0];
@@ -234,20 +254,6 @@ int quick_test_main(int argc, char **argv, const char *name, const char *sourceD
         }
     }
     testArgV[testArgC] = 0;
-
-    QCoreApplication* app = 0;
-    if (!QCoreApplication::instance()) {
-#ifdef QT_QMLTEST_WITH_WIDGETS
-        if (withWidgets)
-            app = new QApplication(argc, argv);
-        else
-#endif
-        {
-            app = new QGuiApplication(argc, argv);
-        }
-    }
-
-    // Parse the command-line arguments.
 
     // Setting currentAppname and currentTestObjectName (via setProgramName) are needed
     // for the code coverage analysis. Must be done before parseArgs is called.