937fdde5d3b26291d417f856ee05ba479a6ba730
Topi Reinio <topi.reinio@digia.com>
2014-04-22 13:32:56 +0200

    Make the Clocks example use the resource system
    Make the Clocks example use the Qt resource system and the
    shared main(), in order to have easy deployment to all
    platforms and support for shadow builds.
    
    Task-number: QTBUG-37203
    Change-Id: Ia235b6b2397cfaeb20f0c7ea0d6196c603b20099
    Reviewed-by: Alan Alpert <aalpert@blackberry.com>

--- examples/quick/demos/clocks/main.cpp
+++ examples/quick/demos/clocks/main.cpp
@@ -37,52 +37,5 @@
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
-
-import QtQuick 2.0
-import "content" as Content
-
-Rectangle {
-    id: root
-    width: 640; height: 320
-    color: "#646464"
-
-    ListView {
-        id: clockview
-        anchors.fill: parent
-        orientation: ListView.Horizontal
-        cacheBuffer: 2000
-        snapMode: ListView.SnapOneItem
-        highlightRangeMode: ListView.ApplyRange
-
-        delegate: Content.Clock { city: cityName; shift: timeShift }
-        model: ListModel {
-            ListElement { cityName: "New York"; timeShift: -4 }
-            ListElement { cityName: "London"; timeShift: 0 }
-            ListElement { cityName: "Oslo"; timeShift: 1 }
-            ListElement { cityName: "Mumbai"; timeShift: 5.5 }
-            ListElement { cityName: "Tokyo"; timeShift: 9 }
-            ListElement { cityName: "Brisbane"; timeShift: 10 }
-            ListElement { cityName: "Los Angeles"; timeShift: -8 }
-        }
-    }
-
-    Image {
-        anchors.left: parent.left
-        anchors.bottom: parent.bottom
-        anchors.margins: 10
-        source: "content/arrow.png"
-        rotation: -90
-        opacity: clockview.atXBeginning ? 0 : 0.5
-        Behavior on opacity { NumberAnimation { duration: 500 } }
-    }
-
-    Image {
-        anchors.right: parent.right
-        anchors.bottom: parent.bottom
-        anchors.margins: 10
-        source: "content/arrow.png"
-        rotation: 90
-        opacity: clockview.atXEnd ? 0 : 0.5
-        Behavior on opacity { NumberAnimation { duration: 500 } }
-    }
-}
+#include "../../shared/shared.h"
+DECLARATIVE_EXAMPLE_MAIN(demos/clocks/clocks)
