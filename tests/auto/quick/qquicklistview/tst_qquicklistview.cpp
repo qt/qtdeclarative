qt/qtdeclarative

commit a0aefe111758b2267f1bf5fbb26991cbc6dd804d

Implement proper support for layoutChange in QQmlDelegateModel
Current implementation is treating model layoutChange the same way as modelReset,
which causes problems when using ListView on top of a QSortFilterProxyModel. The
model emits layoutChanged whenever a data within sorting column change. Treating
it as modelReset leads to poor performance on large models and caused UI issues,
because the scrolling position is reset every time.

This patch implements proper handling for layoutChanged signals by first handling
all items moves and then simulating dataChange for all items.

This fixes regression from Qt 5.1 introduced by Change I16b859d9

Task-number: QTBUG-37983
Task-number: QTBUG-34391
Change-Id: I6d3873b7b87e7f0e8fc0c1ed5dc80c6f8fdf6c22
Reviewed-by: Albert Astals Cid <albert.astals@canonical.com>
Reviewed-by: Michael Zanetti <michael.zanetti@canonical.com>
Reviewed-by: Alan Alpert <aalpert@blackberry.com>
Reviewed-by: Gunnar Sletta <gunnar.sletta@jollamobile.com>

src/qml/types/qqmldelegatemodel.cpp
@@ -1521,9 +1521,64 @@ void QQmlDelegateModel::_q_dataChanged(const QModelIndex &begin, const QModelInd
         _q_itemsChanged(begin.row(), end.row() - begin.row() + 1, roles);
 }
 
-void QQmlDelegateModel::_q_layoutChanged()
+void QQmlDelegateModel::_q_layoutAboutToBeChanged(const QList<QPersistentModelIndex> &parents, QAbstractItemModel::LayoutChangeHint hint)
 {
-    _q_modelReset();
+    Q_D(QQmlDelegateModel);
+    if (!d->m_complete)
+        return;
+
+    if (hint == QAbstractItemModel::VerticalSortHint) {
+        d->m_storedPersistentIndexes.clear();
+        if (!parents.contains(d->m_adaptorModel.rootIndex))
+            return;
+
+        for (int i = 0; i < d->m_count; ++i) {
+            const QModelIndex index = d->m_adaptorModel.aim()->index(i, 0, d->m_adaptorModel.rootIndex);
+            d->m_storedPersistentIndexes.append(index);
+        }
+    } else if (hint == QAbstractItemModel::HorizontalSortHint) {
+        // Ignored
+    } else {
+        // Triggers model reset, no preparations for that are needed
+    }
+}
+
+void QQmlDelegateModel::_q_layoutChanged(const QList<QPersistentModelIndex> &parents, QAbstractItemModel::LayoutChangeHint hint)
+{
+    Q_D(QQmlDelegateModel);
+    if (!d->m_complete)
+        return;
+
+    if (hint == QAbstractItemModel::VerticalSortHint) {
+        if (!parents.contains(d->m_adaptorModel.rootIndex))
+            return;
+
+        for (int i = 0, c = d->m_storedPersistentIndexes.count(); i < c; ++i) {
+            const QPersistentModelIndex &index = d->m_storedPersistentIndexes.at(i);
+            if (i == index.row())
+                continue;
+
+            QVector<Compositor::Insert> inserts;
+            QVector<Compositor::Remove> removes;
+            d->m_compositor.listItemsMoved(&d->m_adaptorModel, i, index.row(), 1, &removes, &inserts);
+            if (!removes.isEmpty() || !inserts.isEmpty()) {
+                d->itemsMoved(removes, inserts);
+            }
+        }
+
+        d->m_storedPersistentIndexes.clear();
+
+        // layoutUpdate does not necessarily have any move changes, but it can
+        // also mean data changes. We can't detect what exactly has changed, so
+        // just emit it for all items
+        _q_itemsChanged(0, d->m_count, QVector<int>());
+
+    } else if (hint == QAbstractItemModel::HorizontalSortHint) {
+        // Ignored
+    } else {
+        // We don't know what's going on, so reset the model
+        _q_modelReset();
+    }
 }
 
 QQmlDelegateModelAttached *QQmlDelegateModel::qmlAttachedProperties(QObject *obj)

src/qml/types/qqmldelegatemodel_p.h
@@ -139,7 +139,8 @@ private Q_SLOTS:
     void _q_rowsRemoved(const QModelIndex &,int,int);
     void _q_rowsMoved(const QModelIndex &, int, int, const QModelIndex &, int);
     void _q_dataChanged(const QModelIndex&,const QModelIndex&,const QVector<int> &);
-    void _q_layoutChanged();
+    void _q_layoutAboutToBeChanged(const QList<QPersistentModelIndex>&, QAbstractItemModel::LayoutChangeHint);
+    void _q_layoutChanged(const QList<QPersistentModelIndex>&, QAbstractItemModel::LayoutChangeHint);
 
 private:
     Q_DISABLE_COPY(QQmlDelegateModel)

src/qml/types/qqmldelegatemodel_p_p.h
@@ -331,6 +331,8 @@ public:
         };
         QQmlDelegateModelGroup *m_groups[Compositor::MaximumGroupCount];
     };
+
+    QList<QPersistentModelIndex> m_storedPersistentIndexes;
 };
 
 class QQmlPartsModel : public QQmlInstanceModel, public QQmlDelegateModelGroupEmitter

src/qml/util/qqmladaptormodel.cpp
@@ -467,8 +467,10 @@ public:
                                 vdm, SLOT(_q_rowsMoved(QModelIndex,int,int,QModelIndex,int)));
             QObject::disconnect(aim, SIGNAL(modelReset()),
                                 vdm, SLOT(_q_modelReset()));
-            QObject::disconnect(aim, SIGNAL(layoutChanged()),
-                                vdm, SLOT(_q_layoutChanged()));
+            QObject::disconnect(aim, SIGNAL(layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                                vdm, SLOT(_q_layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
+            QObject::disconnect(aim, SIGNAL(layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                                vdm, SLOT(_q_layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
         }
 
         const_cast<VDMAbstractItemModelDataType *>(this)->release();
@@ -920,8 +922,10 @@ void QQmlAdaptorModel::setModel(const QVariant &variant, QQmlDelegateModel *vdm,
                               vdm, QQmlDelegateModel, SLOT(_q_rowsMoved(QModelIndex,int,int,QModelIndex,int)));
             qmlobject_connect(model, QAbstractItemModel, SIGNAL(modelReset()),
                               vdm, QQmlDelegateModel, SLOT(_q_modelReset()));
-            qmlobject_connect(model, QAbstractItemModel, SIGNAL(layoutChanged()),
-                              vdm, QQmlDelegateModel, SLOT(_q_layoutChanged()));
+            qmlobject_connect(model, QAbstractItemModel, SIGNAL(layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                              vdm, QQmlDelegateModel, SLOT(_q_layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
+            qmlobject_connect(model, QAbstractItemModel, SIGNAL(layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                              vdm, QQmlDelegateModel, SLOT(_q_layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
         } else {
             accessors = new VDMObjectDelegateDataType;
         }

commit 692c0d3526b4bd5e5b17e17b4a6547046c2bfcbc

Fix QQmlDelegateModel ignoring layoutChange in certain situations
Fix a regression introduced by commit a0aefe1 which caused the model to
ignore layout changes if d->m_adaptorModel.rootIndex was just a descendant
of any of the parent indexes, or when no parent indexes at all were provided
in the notification.

Task-number: QTBUG-39492
Change-Id: I4c97929d25ef75947ccfcbbe5bc234096689c58d
Reviewed-by: Alan Alpert <aalpert@blackberry.com>

src/qml/types/qqmldelegatemodel.cpp
@@ -1521,6 +1521,18 @@ void QQmlDelegateModel::_q_dataChanged(const QModelIndex &begin, const QModelInd
         _q_itemsChanged(begin.row(), end.row() - begin.row() + 1, roles);
 }
 
+bool QQmlDelegateModel::isDescendantOf(const QPersistentModelIndex& desc, const QList< QPersistentModelIndex >& parents) const
+{
+    for (int i = 0, c = parents.count(); i < c; ++i) {
+        for (QPersistentModelIndex parent = desc; parent.isValid(); parent = parent.parent()) {
+            if (parent == parents[i])
+                return true;
+        }
+    }
+
+    return false;
+}
+
 void QQmlDelegateModel::_q_layoutAboutToBeChanged(const QList<QPersistentModelIndex> &parents, QAbstractItemModel::LayoutChangeHint hint)
 {
     Q_D(QQmlDelegateModel);
@@ -1529,8 +1541,9 @@ void QQmlDelegateModel::_q_layoutAboutToBeChanged(const QList<QPersistentModelIn
 
     if (hint == QAbstractItemModel::VerticalSortHint) {
         d->m_storedPersistentIndexes.clear();
-        if (!parents.contains(d->m_adaptorModel.rootIndex))
+        if (!parents.isEmpty() && d->m_adaptorModel.rootIndex.isValid() &&  !isDescendantOf(d->m_adaptorModel.rootIndex, parents)) {
             return;
+        }
 
         for (int i = 0; i < d->m_count; ++i) {
             const QModelIndex index = d->m_adaptorModel.aim()->index(i, 0, d->m_adaptorModel.rootIndex);
@@ -1550,8 +1563,9 @@ void QQmlDelegateModel::_q_layoutChanged(const QList<QPersistentModelIndex> &par
         return;
 
     if (hint == QAbstractItemModel::VerticalSortHint) {
-        if (!parents.contains(d->m_adaptorModel.rootIndex))
+        if (!parents.isEmpty() && d->m_adaptorModel.rootIndex.isValid() && !isDescendantOf(d->m_adaptorModel.rootIndex, parents)) {
             return;
+        }
 
         for (int i = 0, c = d->m_storedPersistentIndexes.count(); i < c; ++i) {
             const QPersistentModelIndex &index = d->m_storedPersistentIndexes.at(i);

src/qml/types/qqmldelegatemodel_p.h
@@ -143,6 +143,8 @@ private Q_SLOTS:
     void _q_layoutChanged(const QList<QPersistentModelIndex>&, QAbstractItemModel::LayoutChangeHint);
 
 private:
+    bool isDescendantOf(const QPersistentModelIndex &desc, const QList<QPersistentModelIndex> &parents) const;
+
     Q_DISABLE_COPY(QQmlDelegateModel)
 };
 

tests/auto/quick/qquicklistview/data/qtbug39492.qml
@@ -1,0 +1,40 @@
+import QtQuick 2.0
+import QtQml.Models 2.1
+
+Rectangle {
+    id: root
+    width: 240
+    height: 320
+    color: "#ffffff"
+
+
+    Component {
+        id: myDelegate
+        Rectangle {
+            id: wrapper
+            objectName: "wrapper"
+            height: 20
+            width: 240
+            Text {
+                objectName: "delegateText"
+                text: display
+            }
+            color: ListView.isCurrentItem ? "lightsteelblue" : "white"
+        }
+    }
+
+    DelegateModel {
+        id: delegateModel
+        objectName: "delegateModel"
+        model: testModel
+        delegate: myDelegate
+    }
+
+    ListView {
+        id: list
+        objectName: "listView"
+        model: delegateModel;
+        focus: true
+        anchors.fill: parent
+    }
+}

tests/auto/quick/qquicklistview/tst_qquicklistview.cpp
@@ -41,6 +41,8 @@
 
 #include <QtTest/QtTest>
 #include <QtCore/QStringListModel>
+#include <QtCore/QSortFilterProxyModel>
+#include <QtGui/QStandardItemModel>
 #include <QtQuick/qquickview.h>
 #include <QtQml/qqmlengine.h>
 #include <QtQml/qqmlcontext.h>
@@ -50,6 +52,7 @@
 #include <QtQuick/private/qquicktext_p.h>
 #include <QtQml/private/qqmlobjectmodel_p.h>
 #include <QtQml/private/qqmllistmodel_p.h>
+#include <QtQml/private/qqmldelegatemodel_p.h>
 #include "../../shared/util.h"
 #include "../shared/viewtestutil.h"
 #include "../shared/visualtestutil.h"
@@ -62,6 +65,7 @@ Q_DECLARE_METATYPE(QQuickItemView::VerticalLayoutDirection)
 Q_DECLARE_METATYPE(QQuickItemView::PositionMode)
 Q_DECLARE_METATYPE(QQuickListView::Orientation)
 Q_DECLARE_METATYPE(Qt::Key)
+Q_DECLARE_METATYPE(QPersistentModelIndex)
 
 using namespace QQuickViewTestUtil;
 using namespace QQuickVisualTestUtil;
@@ -227,6 +231,9 @@ private slots:
 
     void roundingErrors();
     void roundingErrors_data();
+
+    void QTBUG_39492_data();
+    void QTBUG_39492();
 
 private:
     template <class T> void items(const QUrl &source);
@@ -7309,8 +7316,85 @@ void tst_QQuickListView::roundingErrors_data()
     QTest::newRow("pixelAligned=false") << false;
 }
 
+void tst_QQuickListView::QTBUG_39492_data()
+{
+    QStandardItemModel *sourceModel = new QStandardItemModel(this);
+    for (int i = 0; i < 5; ++i) {
+        QStandardItem *item = new QStandardItem(QString::number(i));
+        for (int j = 0; j < 5; ++j) {
+            QStandardItem *subItem = new QStandardItem(QString("%1-%2").arg(i).arg(j));
+            item->appendRow(subItem);
+        }
+        sourceModel->appendRow(item);
+    }
+
+    QSortFilterProxyModel *sortModel = new QSortFilterProxyModel(this);
+    sortModel->setSourceModel(sourceModel);
+
+    QTest::addColumn<QSortFilterProxyModel*>("model");
+    QTest::addColumn<QPersistentModelIndex>("rootIndex");
+
+    QTest::newRow("invalid rootIndex")
+        << sortModel
+        << QPersistentModelIndex();
+
+    QTest::newRow("rootIndex 1")
+        << sortModel
+        << QPersistentModelIndex(sortModel->index(1, 0));
+
+    QTest::newRow("rootIndex 3")
+        << sortModel
+        << QPersistentModelIndex(sortModel->index(3, 0));
+
+    const QModelIndex rootIndex2 = sortModel->index(2, 0);
+    QTest::newRow("rootIndex 2-1")
+        << sortModel
+        << QPersistentModelIndex(sortModel->index(1, 0, rootIndex2));
+}
+
+void tst_QQuickListView::QTBUG_39492()
+{
+    QFETCH(QSortFilterProxyModel*, model);
+    QFETCH(QPersistentModelIndex, rootIndex);
+
+    QQuickView *window = getView();
+    window->rootContext()->setContextProperty("testModel", QVariant::fromValue(model));
+    window->setSource(testFileUrl("qtbug39492.qml"));
+
+    QQuickListView *listview = window->rootObject()->findChild<QQuickListView *>("listView");
+    QVERIFY(listview);
+
+    QQmlDelegateModel *delegateModel = window->rootObject()->findChild<QQmlDelegateModel *>("delegateModel");
+    QVERIFY(delegateModel);
+
+    delegateModel->setRootIndex(QVariant::fromValue(QModelIndex(rootIndex)));
+    model->sort(0, Qt::AscendingOrder);
+    listview->forceLayout();
+
+    for (int i = 0; i < model->rowCount(rootIndex); ++i) {
+        QQuickItem *delegateItem = listview->itemAt(10, 10 + i * 20);
+        QVERIFY(delegateItem);
+        QQuickItem *delegateText = delegateItem->findChild<QQuickItem *>("delegateText");
+        QVERIFY(delegateText);
+        QCOMPARE(delegateText->property("text").toString(),
+                 model->index(i, 0, rootIndex).data().toString());
+    }
+
+    model->sort(0, Qt::DescendingOrder);
+    listview->forceLayout();
+
+    for (int i = 0; i < model->rowCount(rootIndex); ++i) {
+        QQuickItem *delegateItem = listview->itemAt(10, 10 + i * 20);
+        QVERIFY(delegateItem);
+        QQuickItem *delegateText = delegateItem->findChild<QQuickItem *>("delegateText");
+        QVERIFY(delegateText);
+        QCOMPARE(delegateText->property("text").toString(),
+                 model->index(i, 0, rootIndex).data().toString());
+    }
+
+    releaseView(window);
+}
+
 QTEST_MAIN(tst_QQuickListView)
 
 #include "tst_qquicklistview.moc"
-
-