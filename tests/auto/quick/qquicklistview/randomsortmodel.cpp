qt/qtdeclarative

commit a0aefe111758b2267f1bf5fbb26991cbc6dd804d

Implement proper support for layoutChange in QQmlDelegateModel
Current implementation is treating model layoutChange the same way as modelReset,
which causes problems when using ListView on top of a QSortFilterProxyModel. The
model emits layoutChanged whenever a data within sorting column change. Treating
it as modelReset leads to poor performance on large models and caused UI issues,
because the scrolling position is reset every time.

This patch implements proper handling for layoutChanged signals by first handling
all items moves and then simulating dataChange for all items.

This fixes regression from Qt 5.1 introduced by Change I16b859d9

Task-number: QTBUG-37983
Task-number: QTBUG-34391
Change-Id: I6d3873b7b87e7f0e8fc0c1ed5dc80c6f8fdf6c22
Reviewed-by: Albert Astals Cid <albert.astals@canonical.com>
Reviewed-by: Michael Zanetti <michael.zanetti@canonical.com>
Reviewed-by: Alan Alpert <aalpert@blackberry.com>
Reviewed-by: Gunnar Sletta <gunnar.sletta@jollamobile.com>

src/qml/types/qqmldelegatemodel.cpp
@@ -1521,9 +1521,64 @@ void QQmlDelegateModel::_q_dataChanged(const QModelIndex &begin, const QModelInd
         _q_itemsChanged(begin.row(), end.row() - begin.row() + 1, roles);
 }
 
-void QQmlDelegateModel::_q_layoutChanged()
+void QQmlDelegateModel::_q_layoutAboutToBeChanged(const QList<QPersistentModelIndex> &parents, QAbstractItemModel::LayoutChangeHint hint)
 {
-    _q_modelReset();
+    Q_D(QQmlDelegateModel);
+    if (!d->m_complete)
+        return;
+
+    if (hint == QAbstractItemModel::VerticalSortHint) {
+        d->m_storedPersistentIndexes.clear();
+        if (!parents.contains(d->m_adaptorModel.rootIndex))
+            return;
+
+        for (int i = 0; i < d->m_count; ++i) {
+            const QModelIndex index = d->m_adaptorModel.aim()->index(i, 0, d->m_adaptorModel.rootIndex);
+            d->m_storedPersistentIndexes.append(index);
+        }
+    } else if (hint == QAbstractItemModel::HorizontalSortHint) {
+        // Ignored
+    } else {
+        // Triggers model reset, no preparations for that are needed
+    }
+}
+
+void QQmlDelegateModel::_q_layoutChanged(const QList<QPersistentModelIndex> &parents, QAbstractItemModel::LayoutChangeHint hint)
+{
+    Q_D(QQmlDelegateModel);
+    if (!d->m_complete)
+        return;
+
+    if (hint == QAbstractItemModel::VerticalSortHint) {
+        if (!parents.contains(d->m_adaptorModel.rootIndex))
+            return;
+
+        for (int i = 0, c = d->m_storedPersistentIndexes.count(); i < c; ++i) {
+            const QPersistentModelIndex &index = d->m_storedPersistentIndexes.at(i);
+            if (i == index.row())
+                continue;
+
+            QVector<Compositor::Insert> inserts;
+            QVector<Compositor::Remove> removes;
+            d->m_compositor.listItemsMoved(&d->m_adaptorModel, i, index.row(), 1, &removes, &inserts);
+            if (!removes.isEmpty() || !inserts.isEmpty()) {
+                d->itemsMoved(removes, inserts);
+            }
+        }
+
+        d->m_storedPersistentIndexes.clear();
+
+        // layoutUpdate does not necessarily have any move changes, but it can
+        // also mean data changes. We can't detect what exactly has changed, so
+        // just emit it for all items
+        _q_itemsChanged(0, d->m_count, QVector<int>());
+
+    } else if (hint == QAbstractItemModel::HorizontalSortHint) {
+        // Ignored
+    } else {
+        // We don't know what's going on, so reset the model
+        _q_modelReset();
+    }
 }
 
 QQmlDelegateModelAttached *QQmlDelegateModel::qmlAttachedProperties(QObject *obj)

src/qml/types/qqmldelegatemodel_p.h
@@ -139,7 +139,8 @@ private Q_SLOTS:
     void _q_rowsRemoved(const QModelIndex &,int,int);
     void _q_rowsMoved(const QModelIndex &, int, int, const QModelIndex &, int);
     void _q_dataChanged(const QModelIndex&,const QModelIndex&,const QVector<int> &);
-    void _q_layoutChanged();
+    void _q_layoutAboutToBeChanged(const QList<QPersistentModelIndex>&, QAbstractItemModel::LayoutChangeHint);
+    void _q_layoutChanged(const QList<QPersistentModelIndex>&, QAbstractItemModel::LayoutChangeHint);
 
 private:
     Q_DISABLE_COPY(QQmlDelegateModel)

src/qml/types/qqmldelegatemodel_p_p.h
@@ -331,6 +331,8 @@ public:
         };
         QQmlDelegateModelGroup *m_groups[Compositor::MaximumGroupCount];
     };
+
+    QList<QPersistentModelIndex> m_storedPersistentIndexes;
 };
 
 class QQmlPartsModel : public QQmlInstanceModel, public QQmlDelegateModelGroupEmitter

src/qml/util/qqmladaptormodel.cpp
@@ -467,8 +467,10 @@ public:
                                 vdm, SLOT(_q_rowsMoved(QModelIndex,int,int,QModelIndex,int)));
             QObject::disconnect(aim, SIGNAL(modelReset()),
                                 vdm, SLOT(_q_modelReset()));
-            QObject::disconnect(aim, SIGNAL(layoutChanged()),
-                                vdm, SLOT(_q_layoutChanged()));
+            QObject::disconnect(aim, SIGNAL(layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                                vdm, SLOT(_q_layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
+            QObject::disconnect(aim, SIGNAL(layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                                vdm, SLOT(_q_layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
         }
 
         const_cast<VDMAbstractItemModelDataType *>(this)->release();
@@ -920,8 +922,10 @@ void QQmlAdaptorModel::setModel(const QVariant &variant, QQmlDelegateModel *vdm,
                               vdm, QQmlDelegateModel, SLOT(_q_rowsMoved(QModelIndex,int,int,QModelIndex,int)));
             qmlobject_connect(model, QAbstractItemModel, SIGNAL(modelReset()),
                               vdm, QQmlDelegateModel, SLOT(_q_modelReset()));
-            qmlobject_connect(model, QAbstractItemModel, SIGNAL(layoutChanged()),
-                              vdm, QQmlDelegateModel, SLOT(_q_layoutChanged()));
+            qmlobject_connect(model, QAbstractItemModel, SIGNAL(layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                              vdm, QQmlDelegateModel, SLOT(_q_layoutAboutToBeChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
+            qmlobject_connect(model, QAbstractItemModel, SIGNAL(layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
+                              vdm, QQmlDelegateModel, SLOT(_q_layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
         } else {
             accessors = new VDMObjectDelegateDataType;
         }

commit 47ffb30a69285d6ced1287f14ff5842a298e60e0

Fix QQmlDelegateModel getting out of sync
Fixes a regression introduced by a0aefe1, which caused that the
source data model and adaptorModel could sometimes get out of
sync.

Change-Id: Ia6b5fc380cc6cf6549ae857e6da54e088a5dadb5
Reviewed-by: Alan Alpert (Personal) <416365416c@gmail.com>

src/qml/types/qqmldelegatemodel.cpp
@@ -1558,12 +1558,7 @@ void QQmlDelegateModel::_q_layoutChanged(const QList<QPersistentModelIndex> &par
             if (i == index.row())
                 continue;
 
-            QVector<Compositor::Insert> inserts;
-            QVector<Compositor::Remove> removes;
-            d->m_compositor.listItemsMoved(&d->m_adaptorModel, i, index.row(), 1, &removes, &inserts);
-            if (!removes.isEmpty() || !inserts.isEmpty()) {
-                d->itemsMoved(removes, inserts);
-            }
+            _q_itemsMoved(i, index.row(), 1);
         }
 
         d->m_storedPersistentIndexes.clear();

tests/auto/quick/qquicklistview/qquicklistview.pro
@@ -4,10 +4,13 @@ TARGET = tst_qquicklistview
 macx:CONFIG -= app_bundle
 
 HEADERS += incrementalmodel.h \
-           proxytestinnermodel.h
+           proxytestinnermodel.h \
+           randomsortmodel.h
 SOURCES += tst_qquicklistview.cpp \
            incrementalmodel.cpp \
-           proxytestinnermodel.cpp
+           proxytestinnermodel.cpp \
+           randomsortmodel.cpp
+
 
 include (../../shared/util.pri)
 include (../shared/util.pri)

tests/auto/quick/qquicklistview/randomsortmodel.cpp
@@ -1,0 +1,107 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "randomsortmodel.h"
+
+RandomSortModel::RandomSortModel(QObject* parent):
+    QAbstractListModel(parent)
+{
+    for (int i = 0; i < 10; ++i) {
+        mData.append(qMakePair(QString::fromLatin1("Item %1").arg(i), i * 10));
+    }
+}
+
+QHash<int, QByteArray> RandomSortModel::roleNames() const
+{
+    QHash<int,QByteArray> roles = QAbstractItemModel::roleNames();
+    roles[Qt::UserRole] = "SortRole";
+    return roles;
+}
+
+
+int RandomSortModel::rowCount(const QModelIndex& parent) const
+{
+    if (!parent.isValid())
+        return mData.count();
+
+    return 0;
+}
+
+QVariant RandomSortModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid()) {
+        return QVariant();
+    }
+
+    if (index.row() >= mData.count()) {
+        return QVariant();
+    }
+
+    if (role == Qt::DisplayRole) {
+        return QString::fromLatin1("%1 (weight %2)").arg(mData[index.row()].first).arg(mData[index.row()].second);
+    } else if (role == Qt::UserRole) {
+        return mData[index.row()].second;
+    }
+
+    return QVariant();
+}
+
+void RandomSortModel::randomize()
+{
+    const int row = qrand() % mData.count();
+    int random;
+    bool exists = false;
+    // Make sure we won't end up with two items with the same weight, as that
+    // would make unit-testing much harder
+    do {
+        exists = false;
+        random = qrand() % (mData.count() * 10);
+        QList<QPair<QString, int> >::ConstIterator iter, end;
+        for (iter = mData.constBegin(), end = mData.constEnd(); iter != end; ++iter) {
+            if ((*iter).second == random) {
+                exists = true;
+                break;
+            }
+        }
+    } while (exists);
+    mData[row].second = random;
+    Q_EMIT dataChanged(index(row, 0), index(row, 0));
+}

tests/auto/quick/qquicklistview/randomsortmodel.h
@@ -1,0 +1,64 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef RANDOMSORTMODEL_H
+#define RANDOMSORTMODEL_H
+
+#include <QAbstractListModel>
+
+class RandomSortModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    explicit RandomSortModel(QObject* parent = 0);
+    QHash<int, QByteArray> roleNames() const;
+
+    QVariant data(const QModelIndex& index, int role) const;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const;
+
+    void randomize();
+
+  private:
+    QList<QPair<QString, int> > mData;
+};
+
+#endif // RANDOMSORTMODEL_H

tests/auto/quick/qquicklistview/tst_qquicklistview.cpp
@@ -55,6 +55,7 @@
 #include "../shared/visualtestutil.h"
 #include "incrementalmodel.h"
 #include "proxytestinnermodel.h"
+#include "randomsortmodel.h"
 #include <math.h>
 
 Q_DECLARE_METATYPE(Qt::LayoutDirection)
@@ -230,6 +231,8 @@ private slots:
 
     void QTBUG_38209();
     void programmaticFlickAtBounds();
+
+    void layoutChange();
 
 private:
     template <class T> void items(const QUrl &source);
@@ -7363,6 +7366,41 @@ void tst_QQuickListView::programmaticFlickAtBounds()
 
     // verify that there is no movement beyond bounds
     QVERIFY(!spy.wait(100));
+}
+
+void tst_QQuickListView::layoutChange()
+{
+    RandomSortModel *model = new RandomSortModel;
+    QSortFilterProxyModel *sortModel = new QSortFilterProxyModel;
+    sortModel->setSourceModel(model);
+    sortModel->setSortRole(Qt::UserRole);
+    sortModel->setDynamicSortFilter(true);
+    sortModel->sort(0);
+
+    QScopedPointer<QQuickView> window(createView());
+    window->rootContext()->setContextProperty("testModel", QVariant::fromValue(sortModel));
+    window->setSource(testFileUrl("layoutChangeSort.qml"));
+    window->show();
+    QVERIFY(QTest::qWaitForWindowExposed(window.data()));
+
+    QQuickListView *listview = window->rootObject()->findChild<QQuickListView *>("listView");
+    QVERIFY(listview);
+
+    for (int iter = 0; iter < 100; iter++) {
+        for (int i = 0; i < sortModel->rowCount(); ++i) {
+            QQuickItem *delegateItem = listview->itemAt(10, 10 + 2 * i * 20 + 20); // item + group
+            QVERIFY(delegateItem);
+            QQuickItem *delegateText = delegateItem->findChild<QQuickItem *>("delegateText");
+            QVERIFY(delegateText);
+
+            QCOMPARE(delegateText->property("text").toString(),
+                     sortModel->index(i, 0, QModelIndex()).data().toString());
+        }
+
+        model->randomize();
+        listview->forceLayout();
+        QTest::qWait(5); // give view a chance to update
+    }
 }
 
 QTEST_MAIN(tst_QQuickListView)