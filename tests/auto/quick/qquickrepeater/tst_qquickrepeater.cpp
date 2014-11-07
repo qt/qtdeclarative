qt/qtdeclarative

commit 5f5aba5b6e690ca54e66f41b93474f7e67e83c8b

Non-blocking view delegate instantiation.
Task-number: QTBUG-21792

Change-Id: I29a4028cd24eb55d4768aacaa3abbd1786061398
Reviewed-by: Andrew den Exter <andrew.den-exter@nokia.com>

src/declarative/items/qquickrepeater.cpp
@@ -51,7 +51,7 @@
 QT_BEGIN_NAMESPACE
 
 QQuickRepeaterPrivate::QQuickRepeaterPrivate()
-: model(0), ownModel(false)
+    : model(0), ownModel(false), inRequest(false), itemCount(0), createFrom(-1)
 {
 }
 
@@ -188,10 +188,8 @@ void QQuickRepeater::setModel(const QVariant &model)
     if (d->model) {
         disconnect(d->model, SIGNAL(modelUpdated(QDeclarativeChangeSet,bool)),
                 this, SLOT(modelUpdated(QDeclarativeChangeSet,bool)));
-        /*
         disconnect(d->model, SIGNAL(createdItem(int,QQuickItem*)), this, SLOT(createdItem(int,QQuickItem*)));
-        disconnect(d->model, SIGNAL(destroyingItem(QQuickItem*)), this, SLOT(destroyingItem(QQuickItem*)));
-    */
+//        disconnect(d->model, SIGNAL(destroyingItem(QQuickItem*)), this, SLOT(destroyingItem(QQuickItem*)));
     }
     d->dataSource = model;
     QObject *object = qvariant_cast<QObject*>(model);
@@ -215,10 +213,8 @@ void QQuickRepeater::setModel(const QVariant &model)
     if (d->model) {
         connect(d->model, SIGNAL(modelUpdated(QDeclarativeChangeSet,bool)),
                 this, SLOT(modelUpdated(QDeclarativeChangeSet,bool)));
-        /*
         connect(d->model, SIGNAL(createdItem(int,QQuickItem*)), this, SLOT(createdItem(int,QQuickItem*)));
-        connect(d->model, SIGNAL(destroyingItem(QQuickItem*)), this, SLOT(destroyingItem(QQuickItem*)));
-        */
+//        connect(d->model, SIGNAL(destroyingItem(QQuickItem*)), this, SLOT(destroyingItem(QQuickItem*)));
         regenerate();
     }
     emit modelChanged();
@@ -339,14 +335,15 @@ void QQuickRepeater::clear()
     bool complete = isComponentComplete();
 
     if (d->model) {
-        while (d->deletables.count() > 0) {
-            QQuickItem *item = d->deletables.takeLast();
+        for (int i = 0; i < d->deletables.count(); ++i) {
+            QQuickItem *item = d->deletables.at(i);
             if (complete)
-                emit itemRemoved(d->deletables.count()-1, item);
+                emit itemRemoved(i, item);
             d->model->release(item);
         }
     }
     d->deletables.clear();
+    d->itemCount = 0;
 }
 
 void QQuickRepeater::regenerate()
@@ -360,16 +357,51 @@ void QQuickRepeater::regenerate()
     if (!d->model || !d->model->count() || !d->model->isValid() || !parentItem() || !isComponentComplete())
         return;
 
-    for (int ii = 0; ii < count(); ++ii) {
-        QQuickItem *item = d->model->item(ii);
-        if (item) {
-            QDeclarative_setParent_noEvent(item, parentItem());
-            item->setParentItem(parentItem());
-            item->stackBefore(this);
-            d->deletables << item;
-            emit itemAdded(ii, item);
+    d->itemCount = count();
+    d->deletables.resize(d->itemCount);
+    d->createFrom = 0;
+    d->createItems();
+}
+
+void QQuickRepeaterPrivate::createItems()
+{
+    Q_Q(QQuickRepeater);
+    if (createFrom == -1)
+        return;
+    inRequest = true;
+    for (int ii = createFrom; ii < itemCount; ++ii) {
+        if (!deletables.at(ii)) {
+            QQuickItem *item = model->item(ii, false);
+            if (!item) {
+                createFrom = ii;
+                break;
+            }
+            deletables[ii] = item;
+            QDeclarative_setParent_noEvent(item, q->parentItem());
+            item->setParentItem(q->parentItem());
+            if (ii > 0 && deletables.at(ii-1)) {
+                item->stackAfter(deletables.at(ii-1));
+            } else {
+                QQuickItem *after = q;
+                for (int si = ii+1; si < itemCount; ++si) {
+                    if (deletables.at(si)) {
+                        after = deletables.at(si);
+                        break;
+                    }
+                }
+                item->stackBefore(after);
+            }
+            emit q->itemAdded(ii, item);
         }
     }
+    inRequest = false;
+}
+
+void QQuickRepeater::createdItem(int index, QQuickItem *item)
+{
+    Q_D(QQuickRepeater);
+    if (!d->inRequest)
+        d->createItems();
 }
 
 void QQuickRepeater::modelUpdated(const QDeclarativeChangeSet &changeSet, bool reset)
@@ -385,7 +417,7 @@ void QQuickRepeater::modelUpdated(const QDeclarativeChangeSet &changeSet, bool r
     }
 
     int difference = 0;
-    QHash<int, QList<QPointer<QQuickItem> > > moved;
+    QHash<int, QVector<QPointer<QQuickItem> > > moved;
     foreach (const QDeclarativeChangeSet::Remove &remove, changeSet.removes()) {
         int index = qMin(remove.index, d->deletables.count());
         int count = qMin(remove.index + remove.count, d->deletables.count()) - index;
@@ -395,19 +427,22 @@ void QQuickRepeater::modelUpdated(const QDeclarativeChangeSet &changeSet, bool r
                     d->deletables.begin() + index,
                     d->deletables.begin() + index + count);
         } else while (count--) {
-            QQuickItem *item = d->deletables.takeAt(index);
+            QQuickItem *item = d->deletables.at(index);
+            d->deletables.remove(index);
             emit itemRemoved(index, item);
             if (item)
                 d->model->release(item);
+            --d->itemCount;
         }
 
         difference -= remove.count;
     }
 
+    d->createFrom = -1;
     foreach (const QDeclarativeChangeSet::Insert &insert, changeSet.inserts()) {
         int index = qMin(insert.index, d->deletables.count());
         if (insert.isMove()) {
-            QList<QPointer<QQuickItem> > items = moved.value(insert.moveId);
+            QVector<QPointer<QQuickItem> > items = moved.value(insert.moveId);
             d->deletables = d->deletables.mid(0, index) + items + d->deletables.mid(index);
             QQuickItem *stackBefore = index + items.count() < d->deletables.count()
                     ? d->deletables.at(index + items.count())
@@ -416,20 +451,15 @@ void QQuickRepeater::modelUpdated(const QDeclarativeChangeSet &changeSet, bool r
                 d->deletables.at(i)->stackBefore(stackBefore);
         } else for (int i = 0; i < insert.count; ++i) {
             int modelIndex = index + i;
-            QQuickItem *item = d->model->item(modelIndex);
-            if (item) {
-                QDeclarative_setParent_noEvent(item, parentItem());
-                item->setParentItem(parentItem());
-                if (modelIndex < d->deletables.count())
-                    item->stackBefore(d->deletables.at(modelIndex));
-                else
-                    item->stackBefore(this);
-                d->deletables.insert(modelIndex, item);
-                emit itemAdded(modelIndex, item);
-            }
+            ++d->itemCount;
+            d->deletables.insert(modelIndex, 0);
+            if (d->createFrom == -1)
+                d->createFrom = modelIndex;
         }
         difference += insert.count;
     }
+
+    d->createItems();
 
     if (difference != 0)
         emit countChanged();

tests/auto/declarative/qquickrepeater/tst_qquickrepeater.cpp
@@ -46,10 +46,12 @@
 #include <QtDeclarative/qquickview.h>
 #include <QtDeclarative/qdeclarativecontext.h>
 #include <QtDeclarative/qdeclarativeexpression.h>
+#include <QtDeclarative/qdeclarativeincubator.h>
 #include <private/qquickrepeater_p.h>
 #include <private/qquicktext_p.h>
 
 #include "../shared/util.h"
+#include "../../../shared/util.h"
 
 inline QUrl TEST_FILE(const QString &filename)
 {
@@ -73,6 +75,7 @@ private slots:
     void resetModel();
     void modelChanged();
     void properties();
+    void asynchronous();
 
 private:
     QQuickView *createView();
@@ -634,6 +637,62 @@ void tst_QQuickRepeater::properties()
     QCOMPARE(delegateSpy.count(),1);
 
     delete rootObject;
+}
+
+void tst_QQuickRepeater::asynchronous()
+{
+    QQuickView *canvas = createView();
+    canvas->show();
+    QDeclarativeIncubationController controller;
+    canvas->engine()->setIncubationController(&controller);
+
+    canvas->setSource(TEST_FILE("asyncloader.qml"));
+
+    QQuickItem *rootObject = qobject_cast<QQuickItem*>(canvas->rootObject());
+    QVERIFY(rootObject);
+
+    QQuickItem *container = findItem<QQuickItem>(rootObject, "container");
+    QVERIFY(!container);
+    while (!container) {
+        bool b = false;
+        controller.incubateWhile(&b);
+        container = findItem<QQuickItem>(rootObject, "container");
+    }
+
+    QQuickRepeater *repeater = 0;
+    while (!repeater) {
+        bool b = false;
+        controller.incubateWhile(&b);
+        repeater = findItem<QQuickRepeater>(rootObject, "repeater");
+    }
+
+    // items will be created one at a time
+    for (int i = 0; i < 10; ++i) {
+        QString name("delegate");
+        name += QString::number(i);
+        QVERIFY(findItem<QQuickItem>(container, name) == 0);
+        QQuickItem *item = 0;
+        while (!item) {
+            bool b = false;
+            controller.incubateWhile(&b);
+            item = findItem<QQuickItem>(container, name);
+        }
+    }
+
+    {
+        bool b = true;
+        controller.incubateWhile(&b);
+    }
+
+    // verify positioning
+    for (int i = 0; i < 10; ++i) {
+        QString name("delegate");
+        name += QString::number(i);
+        QQuickItem *item = findItem<QQuickItem>(container, name);
+        QTRY_COMPARE(item->y(), i * 50.0);
+    }
+
+    delete canvas;
 }
 
 QQuickView *tst_QQuickRepeater::createView()

src/declarative/items/qquickgridview.cpp
@@ -294,7 +294,10 @@ qreal QQuickGridViewPrivate::colPosAt(int modelIndex) const
     if (FxViewItem *item = visibleItem(modelIndex))
         return static_cast<FxGridItemSG*>(item)->colPos();
     if (!visibleItems.isEmpty()) {
-        if (modelIndex < visibleIndex) {
+        if (modelIndex == visibleIndex) {
+            FxGridItemSG *firstItem = static_cast<FxGridItemSG*>(visibleItems.first());
+            return firstItem->colPos();
+        } else if (modelIndex < visibleIndex) {
             int count = (visibleIndex - modelIndex) % columns;
             int col = static_cast<FxGridItemSG*>(visibleItems.first())->colPos() / colSize();
             col = (columns - count + col) % columns;
@@ -312,7 +315,10 @@ qreal QQuickGridViewPrivate::rowPosAt(int modelIndex) const
     if (FxViewItem *item = visibleItem(modelIndex))
         return static_cast<FxGridItemSG*>(item)->rowPos();
     if (!visibleItems.isEmpty()) {
-        if (modelIndex < visibleIndex) {
+        if (modelIndex == visibleIndex) {
+            FxGridItemSG *firstItem = static_cast<FxGridItemSG*>(visibleItems.first());
+            return firstItem->rowPos();
+        } else if (modelIndex < visibleIndex) {
             FxGridItemSG *firstItem = static_cast<FxGridItemSG*>(visibleItems.first());
             int firstCol = firstItem->colPos() / colSize();
             int col = visibleIndex - modelIndex + (columns - firstCol - 1);
@@ -438,10 +444,11 @@ bool QQuickGridViewPrivate::addVisibleItems(qreal fillFrom, qreal fillTo, bool d
     bool changed = false;
 
     while (modelIndex < model->count() && rowPos <= fillTo + rowSize()*(columns - colNum)/(columns+1)) {
-//        qDebug() << "refill: append item" << modelIndex;
-        if (!(item = static_cast<FxGridItemSG*>(createItem(modelIndex))))
+//        qDebug() << "refill: append item" << modelIndex << colPos << rowPos;
+        if (!(item = static_cast<FxGridItemSG*>(createItem(modelIndex, doBuffer))))
             break;
         item->setPosition(colPos, rowPos);
+        item->item->setVisible(!doBuffer);
         visibleItems.append(item);
         if (++colNum >= columns) {
             colNum = 0;
@@ -450,8 +457,6 @@ bool QQuickGridViewPrivate::addVisibleItems(qreal fillFrom, qreal fillTo, bool d
         colPos = colNum * colSize();
         ++modelIndex;
         changed = true;
-        if (doBuffer) // never buffer more than one item per frame
-            break;
     }
 
     // Find first column
@@ -471,10 +476,11 @@ bool QQuickGridViewPrivate::addVisibleItems(qreal fillFrom, qreal fillTo, bool d
     colPos = colNum * colSize();
     while (visibleIndex > 0 && rowPos + rowSize() - 1 >= fillFrom - rowSize()*(colNum+1)/(columns+1)){
 //        qDebug() << "refill: prepend item" << visibleIndex-1 << "top pos" << rowPos << colPos;
-        if (!(item = static_cast<FxGridItemSG*>(createItem(visibleIndex-1))))
+        if (!(item = static_cast<FxGridItemSG*>(createItem(visibleIndex-1, doBuffer))))
             break;
         --visibleIndex;
         item->setPosition(colPos, rowPos);
+        item->item->setVisible(!doBuffer);
         visibleItems.prepend(item);
         if (--colNum < 0) {
             colNum = columns-1;
@@ -482,8 +488,6 @@ bool QQuickGridViewPrivate::addVisibleItems(qreal fillFrom, qreal fillTo, bool d
         }
         colPos = colNum * colSize();
         changed = true;
-        if (doBuffer) // never buffer more than one item per frame
-            break;
     }
 
     return changed;
@@ -491,6 +495,7 @@ bool QQuickGridViewPrivate::addVisibleItems(qreal fillFrom, qreal fillTo, bool d
 
 bool QQuickGridViewPrivate::removeNonVisibleItems(qreal bufferFrom, qreal bufferTo)
 {
+    Q_Q(QQuickGridView);
     FxGridItemSG *item = 0;
     bool changed = false;
 
@@ -499,7 +504,7 @@ bool QQuickGridViewPrivate::removeNonVisibleItems(qreal bufferFrom, qreal buffer
                 && item->rowPos()+rowSize()-1 < bufferFrom - rowSize()*(item->colPos()/colSize()+1)/(columns+1)) {
         if (item->attached->delayRemove())
             break;
-//            qDebug() << "refill: remove first" << visibleIndex << "top end pos" << item->endRowPos();
+//        qDebug() << "refill: remove first" << visibleIndex << "top end pos" << item->endRowPos();
         if (item->index != -1)
             visibleIndex++;
         visibleItems.removeFirst();
@@ -511,7 +516,7 @@ bool QQuickGridViewPrivate::removeNonVisibleItems(qreal bufferFrom, qreal buffer
                 && item->rowPos() > bufferTo + rowSize()*(columns - item->colPos()/colSize())/(columns+1)) {
         if (item->attached->delayRemove())
             break;
-//            qDebug() << "refill: remove last" << visibleIndex+visibleItems.count()-1;
+//        qDebug() << "refill: remove last" << visibleIndex+visibleItems.count()-1;
         visibleItems.removeLast();
         releaseItem(item);
         changed = true;
@@ -1341,11 +1346,16 @@ void QQuickGridView::setHighlightFollowsCurrentItem(bool autoHighlight)
     This property determines whether delegates are retained outside the
     visible area of the view.
 
-    If non-zero the view will keep as many delegates
+    If non-zero the view may keep as many delegates
     instantiated as will fit within the buffer specified.  For example,
-    if in a vertical view the delegate is 20 pixels high and \c cacheBuffer is
-    set to 40, then up to 2 delegates above and 2 delegates below the visible
-    area may be retained.
+    if in a vertical view the delegate is 20 pixels high, there are 3
+    columns and \c cacheBuffer is
+    set to 40, then up to 6 delegates above and 6 delegates below the visible
+    area may be created/retained.  The buffered delegates are created asynchronously,
+    allowing creation to occur across multiple frames and reducing the
+    likelihood of skipping frames.  In order to improve painting performance
+    delegates outside the visible area have their \l visible property set to
+    false until they are moved into the visible area.
 
     Note that cacheBuffer is not a pixel buffer - it only maintains additional
     instantiated delegates.
@@ -1507,22 +1517,21 @@ void QQuickGridView::viewportMoved()
         return;
     d->inViewportMoved = true;
 
-    d->lazyRelease = true;
-    if (d->hData.flicking || d->vData.flicking) {
-        if (yflick()) {
-            if (d->vData.velocity > 0)
-                d->bufferMode = QQuickGridViewPrivate::BufferBefore;
-            else if (d->vData.velocity < 0)
-                d->bufferMode = QQuickGridViewPrivate::BufferAfter;
-        }
-
-        if (xflick()) {
-            if (d->hData.velocity > 0)
-                d->bufferMode = QQuickGridViewPrivate::BufferBefore;
-            else if (d->hData.velocity < 0)
-                d->bufferMode = QQuickGridViewPrivate::BufferAfter;
-        }
+    // Set visibility of items to eliminate cost of items outside the visible area.
+    qreal from = d->isContentFlowReversed() ? -d->position()-d->size() : d->position();
+    qreal to = d->isContentFlowReversed() ? -d->position() : d->position()+d->size();
+    for (int i = 0; i < d->visibleItems.count(); ++i) {
+        FxGridItemSG *item = static_cast<FxGridItemSG*>(d->visibleItems.at(i));
+        item->item->setVisible(item->rowPos() + d->rowSize() >= from && item->rowPos() <= to);
     }
+
+    if (yflick())
+        d->bufferMode = d->vData.smoothVelocity < 0 ? QQuickItemViewPrivate::BufferBefore : QQuickItemViewPrivate::BufferAfter;
+    else if (d->isRightToLeftTopToBottom())
+        d->bufferMode = d->hData.smoothVelocity < 0 ? QQuickItemViewPrivate::BufferAfter : QQuickItemViewPrivate::BufferBefore;
+    else
+        d->bufferMode = d->hData.smoothVelocity < 0 ? QQuickItemViewPrivate::BufferBefore : QQuickItemViewPrivate::BufferAfter;
+
     d->refill();
     if (d->hData.flicking || d->vData.flicking || d->hData.moving || d->vData.moving)
         d->moveReason = QQuickGridViewPrivate::Mouse;
@@ -1815,7 +1824,10 @@ bool QQuickGridViewPrivate::applyInsertionChange(const QDeclarativeChangeSet::In
                 }
                 if (!item)
                     item = createItem(modelIndex + i);
+                if (!item)
+                    return false;
 
+                item->item->setVisible(true);
                 visibleItems.insert(insertionIdx, item);
                 if (!change.isMove()) {
                     insertResult->addedItems.append(item);
@@ -1843,7 +1855,10 @@ bool QQuickGridViewPrivate::applyInsertionChange(const QDeclarativeChangeSet::In
             }
             if (!item)
                 item = createItem(modelIndex + i);
+            if (!item)
+                return false;
 
+            item->item->setVisible(true);
             visibleItems.insert(index, item);
             if (!change.isMove())
                 insertResult->addedItems.append(item);

commit 9d1cd3098a066c7b6689d4776bfd3a25621a26fc

QQuickRepeater::clear: Remove items from the end backwards.
This way, the indices for itemRemoved will make sense consistently.

This was broken with 5f5aba5b6e690ca54e66f41b93474f7e67e83c8b, dated November
2011.

Task-number: QTBUG-42243
Change-Id: I5fecfd4174049f51e0cec90e40e6332de5d5bf01
Reviewed-by: Gunnar Sletta <gunnar@sletta.org>

src/quick/items/qquickrepeater.cpp
@@ -350,7 +350,9 @@ void QQuickRepeater::clear()
     bool complete = isComponentComplete();
 
     if (d->model) {
-        for (int i = 0; i < d->deletables.count(); ++i) {
+        // We remove in reverse order deliberately; so that signals are emitted
+        // with sensible indices.
+        for (int i = d->deletables.count() - 1; i >= 0; --i) {
             if (QQuickItem *item = d->deletables.at(i)) {
                 if (complete)
                     emit itemRemoved(i, item);

tests/auto/quick/qquickrepeater/data/clearremovalorder.qml
@@ -1,0 +1,31 @@
+import QtQuick 2.0
+
+Item {
+    ListModel {
+        id: firstModel
+        objectName: "firstModel"
+
+        ListElement {
+            name: "Captain"
+        }
+        ListElement {
+            name: "Jack"
+        }
+        ListElement {
+            name: "Sparrow"
+        }
+    }
+    ListModel {
+        id: secondModel
+        objectName: "secondModel"
+    }
+    Repeater {
+        objectName: "repeater"
+        model: firstModel
+        delegate: Rectangle {
+            width: 5
+            height: 5
+            color: "green"
+        }
+    }
+}

tests/auto/quick/qquickrepeater/tst_qquickrepeater.cpp
@@ -40,6 +40,7 @@
 #include <QtQml/qqmlincubator.h>
 #include <private/qquickrepeater_p.h>
 #include <QtQuick/private/qquicktext_p.h>
+#include <QtQml/private/qqmllistmodel_p.h>
 
 #include "../../shared/util.h"
 #include "../shared/viewtestutil.h"
@@ -73,6 +74,7 @@ private slots:
     void visualItemModelCrash();
     void invalidContextCrash();
     void jsArrayChange();
+    void clearRemovalOrder();
 };
 
 class TestObject : public QObject
@@ -805,6 +807,42 @@ void tst_QQuickRepeater::jsArrayChange()
     // no change
     repeater->setModel(QVariant::fromValue(array2));
     QCOMPARE(spy.count(), 1);
+}
+
+void tst_QQuickRepeater::clearRemovalOrder()
+{
+    // Here, we're going to test that when the model is cleared, item removal
+    // signals are sent in a sensible order that gives us correct indices.
+    // (QTBUG-42243)
+    QQmlEngine engine;
+    QQmlComponent component(&engine, testFileUrl("clearremovalorder.qml"));
+
+    QQuickItem *rootObject = qobject_cast<QQuickItem*>(component.create());
+    QVERIFY(rootObject);
+
+    QQuickRepeater *repeater = findItem<QQuickRepeater>(rootObject, "repeater");
+    QVERIFY(repeater);
+    QCOMPARE(repeater->count(), 3);
+
+    QQmlListModel *model = rootObject->findChild<QQmlListModel*>("secondModel");
+    QVERIFY(model);
+    QCOMPARE(model->count(), 0);
+
+    // Now change the model
+    QSignalSpy removedSpy(repeater, &QQuickRepeater::itemRemoved);
+    repeater->setModel(QVariant::fromValue(model));
+
+    // we should have 0 items, and 3 removal signals.
+    QCOMPARE(repeater->count(), 0);
+    QCOMPARE(removedSpy.count(), 3);
+
+    // column 1 is for the items, we won't bother verifying these. just look at
+    // the indices and make sure they're sane.
+    QCOMPARE(removedSpy.at(0).at(0).toInt(), 2);
+    QCOMPARE(removedSpy.at(1).at(0).toInt(), 1);
+    QCOMPARE(removedSpy.at(2).at(0).toInt(), 0);
+
+    delete rootObject;
 }
 
 QTEST_MAIN(tst_QQuickRepeater)