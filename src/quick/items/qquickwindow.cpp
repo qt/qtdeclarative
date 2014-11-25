ca5e643ea9d2878f4a00546ed63a004e10da6ae0
Morten Johan Sørvig <morten.sorvig@digia.com>
2014-11-25 14:22:15 +0100

    Make effectiveDevicePixelRatio() return a qreal.
    Fractional scale factors are not as broken as previously
    believed, especially for Qt Quick. Keep the door open
    for finding a way to support it at some point in the
    future.
    
    Change-Id: Ifeadcc53175ac6c25ea0288d5fe1966e3de408f9
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>

--- src/quick/items/qquickwindow.cpp
+++ src/quick/items/qquickwindow.cpp
@@ -3945,7 +3945,7 @@ void QQuickWindow::runJobsAfterSwap()
  *
  * \sa QWindow::devicePixelRatio()
  */
-int QQuickWindow::effectiveDevicePixelRatio() const
+qreal QQuickWindow::effectiveDevicePixelRatio() const
 {
     QWindow *w = QQuickRenderControl::renderWindowFor(const_cast<QQuickWindow *>(this));
     return w ? w->devicePixelRatio() : devicePixelRatio();
