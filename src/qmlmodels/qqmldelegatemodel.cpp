37fcffa035d55ac00f85f57ce1390fff3be213c6
Elvis Lee <kwangwoong.lee@lge.com>
2014-12-10 15:28:17 +0900

    Prevent items from being deleted while removing
    Delegate items can be deleted when remove them from cache list. That may
    cause a crash. So call referenceObject to keep the item from being
    deleted.
    
    Task-number: QTBUG-83352
    Pick-to: 5.15
    Change-Id: Id5d7ab6dbf21682940f5393ea4e843c2448f7d81
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qmlmodels/qqmldelegatemodel.cpp
+++ src/qmlmodels/qqmldelegatemodel.cpp
@@ -1711,6 +1711,10 @@ void QQmlDelegateModel::_q_itemsRemoved(int index, int count)
 
     d->m_count -= count;
     const QList<QQmlDelegateModelItem *> cache = d->m_cache;
+    //Prevents items being deleted in remove loop
+    for (QQmlDelegateModelItem *item : cache)
+        item->referenceObject();
+
     for (int i = 0, c = cache.count();  i < c; ++i) {
         QQmlDelegateModelItem *item = cache.at(i);
         // layout change triggered by removal of a previous item might have
@@ -1727,6 +1731,9 @@ void QQmlDelegateModel::_q_itemsRemoved(int index, int count)
             item->setModelIndex(-1, -1, -1);
         }
     }
+    //Release items which are referenced before the loop
+    for (QQmlDelegateModelItem *item : cache)
+        item->releaseObject();
 
     QVector<Compositor::Remove> removes;
     d->m_compositor.listItemsRemoved(&d->m_adaptorModel, index, count, &removes);
