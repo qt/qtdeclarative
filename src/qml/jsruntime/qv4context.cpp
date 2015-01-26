qt/qtdeclarative

commit d24b7fb904771ba078ac52e05984b23327ddff3b

Remove the realArgumentsCount member in CallContext
The data is easily available through the CallData, only used by
the Arguments object and we save a pointer in Heap::CallData this way.

For this to work, let CallData::argc always return the real number
of arguments passed into the function.

Change-Id: I59c7c41e8c1af160db09fa794977ab7084c9e12d
Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

src/qml/jsruntime/qv4context.cpp
@@ -57,7 +57,6 @@ Heap::CallContext *ExecutionContext::newCallContext(FunctionObject *function, Ca
     new (c) Heap::CallContext(d()->engine, Heap::ExecutionContext::Type_CallContext);
 
     c->function = function->d();
-    c->realArgumentCount = callData->argc;
 
     c->strictMode = function->strictMode();
     c->outer = function->scope();
@@ -77,7 +76,6 @@ Heap::CallContext *ExecutionContext::newCallContext(FunctionObject *function, Ca
     ::memcpy(c->callData, callData, sizeof(CallData) + (callData->argc - 1) * sizeof(Value));
     if (callData->argc < static_cast<int>(compiledFunction->nFormals))
         std::fill(c->callData->args + c->callData->argc, c->callData->args + compiledFunction->nFormals, Primitive::undefinedValue());
-    c->callData->argc = qMax((uint)callData->argc, compiledFunction->nFormals);
 
     return c;
 }

src/qml/jsruntime/qv4argumentsobject.cpp
@@ -68,7 +68,7 @@ Heap::ArgumentsObject::ArgumentsObject(QV4::CallContext *context)
         args->memberData()->data[CalleePropertyIndex] = context->d()->function->asReturnedValue();
     }
     Q_ASSERT(LengthPropertyIndex == args->internalClass()->find(context->d()->engine->id_length));
-    args->memberData()->data[LengthPropertyIndex] = Primitive::fromInt32(context->d()->realArgumentCount);
+    args->memberData()->data[LengthPropertyIndex] = Primitive::fromInt32(context->d()->callData->argc);
 }
 
 void ArgumentsObject::fullyCreate()
@@ -76,8 +76,8 @@ void ArgumentsObject::fullyCreate()
     if (fullyCreated())
         return;
 
-    uint numAccessors = qMin((int)context()->function->formalParameterCount(), context()->realArgumentCount);
-    uint argCount = qMin(context()->realArgumentCount, context()->callData->argc);
+    uint numAccessors = qMin((int)context()->function->formalParameterCount(), context()->callData->argc);
+    uint argCount = context()->callData->argc;
     ArrayData::realloc(this, Heap::ArrayData::Sparse, argCount, true);
     context()->engine->requireArgumentsAccessors(numAccessors);
 
@@ -105,7 +105,7 @@ bool ArgumentsObject::defineOwnProperty(ExecutionEngine *engine, uint index, con
     ScopedProperty map(scope);
     PropertyAttributes mapAttrs;
     bool isMapped = false;
-    uint numAccessors = qMin((int)context()->function->formalParameterCount(), context()->realArgumentCount);
+    uint numAccessors = qMin((int)context()->function->formalParameterCount(), context()->callData->argc);
     if (pd && index < (uint)numAccessors)
         isMapped = arrayData()->attributes(index).isAccessor() &&
                 pd->getter() == context()->engine->argumentsAccessors[index].getter();
@@ -188,8 +188,8 @@ PropertyAttributes ArgumentsObject::queryIndexed(const Managed *m, uint index)
     if (args->fullyCreated())
         return Object::queryIndexed(m, index);
 
-    uint numAccessors = qMin((int)args->context()->function->formalParameterCount(), args->context()->realArgumentCount);
-    uint argCount = qMin(args->context()->realArgumentCount, args->context()->callData->argc);
+    uint numAccessors = qMin((int)args->context()->function->formalParameterCount(), args->context()->callData->argc);
+    uint argCount = args->context()->callData->argc;
     if (index >= argCount)
         return PropertyAttributes();
     if (index >= numAccessors)

src/qml/jsruntime/qv4context_p.h
@@ -107,7 +107,6 @@ struct CallContext : ExecutionContext {
     CallContext(ExecutionEngine *engine, QV4::Object *qml, QV4::FunctionObject *function);
 
     FunctionObject *function;
-    int realArgumentCount;
     Value *locals;
     Object *activation;
 };

src/qml/jsruntime/qv4functionobject.cpp
@@ -498,10 +498,8 @@ ReturnedValue SimpleScriptFunction::construct(Managed *that, CallData *callData)
     ctx.lookups = ctx.compilationUnit->runtimeLookups;
     ctx.outer = f->scope();
     ctx.locals = scope.alloc(f->varCount());
-    while (callData->argc < (int)f->formalParameterCount()) {
-        callData->args[callData->argc] = Encode::undefined();
-        ++callData->argc;
-    }
+    for (int i = callData->argc; i < (int)f->formalParameterCount(); ++i)
+        callData->args[i] = Encode::undefined();
     Q_ASSERT(v4->currentContext() == &ctx);
 
     ScopedObject result(scope, Q_V4_PROFILE(v4, f->function()));
@@ -535,10 +533,8 @@ ReturnedValue SimpleScriptFunction::call(Managed *that, CallData *callData)
     ctx.lookups = ctx.compilationUnit->runtimeLookups;
     ctx.outer = f->scope();
     ctx.locals = scope.alloc(f->varCount());
-    while (callData->argc < (int)f->formalParameterCount()) {
-        callData->args[callData->argc] = Encode::undefined();
-        ++callData->argc;
-    }
+    for (int i = callData->argc; i < (int)f->formalParameterCount(); ++i)
+        callData->args[i] = Encode::undefined();
     Q_ASSERT(v4->currentContext() == &ctx);
 
     ScopedValue result(scope, Q_V4_PROFILE(v4, f->function()));

commit 72f8b50571cecbc5ad0eb9fbee79af56eb2ea65e

Properly mark all arguments in the call context
This broke in change d24b7fb904771ba078ac52e05984b23327ddff3b,
where argc changed to contain the number of arguments passed into
the function, not the number of arguments it really has.

Change-Id: I2d25f159a9f60922b863c238147564f542d62ba0
Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

src/qml/jsruntime/qv4context.cpp
@@ -254,13 +254,11 @@ void ExecutionContext::markObjects(Heap::Base *m, ExecutionEngine *engine)
     if (ctx->outer)
         ctx->outer->mark(engine);
 
-    // ### shouldn't need these 3 lines
-    ctx->callData->thisObject.mark(engine);
-    for (int arg = 0; arg < ctx->callData->argc; ++arg)
-        ctx->callData->args[arg].mark(engine);
-
     if (ctx->type >= Heap::ExecutionContext::Type_CallContext) {
         QV4::Heap::CallContext *c = static_cast<Heap::CallContext *>(ctx);
+        ctx->callData->thisObject.mark(engine);
+        for (int arg = 0; arg < qMax(ctx->callData->argc, (int)c->function->formalParameterCount()); ++arg)
+            ctx->callData->args[arg].mark(engine);
         for (unsigned local = 0, lastLocal = c->function->varCount(); local < lastLocal; ++local)
             c->locals[local].mark(engine);
         if (c->activation)