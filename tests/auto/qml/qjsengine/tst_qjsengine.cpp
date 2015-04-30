qt/qtdeclarative

commit 75c22465cf8fe262edfe6178bb9ca19661fb710e

V4: Split arguments/locals from temps.
There are a couple of reasons to split the temporaries off from the
arguments and locals:

Temporaries are invisible, and changes to them cannot be observed.
On the other hand, arguments and locals are visible, and writes to them
can be seen from other places (nested functions), or by using the
arguments array. So, in practice these correspond to memory locations.
(One could argue that if neither nested functions, nor eval(), nor
arguments[] is used, the loads/stores are invisible too. But that's an
optimization, and changing locals/arguments to temporaries can be done
in a separate pass.)

Because of the "volatile" nature of arguments and locals, their usage
cannot be optimized. All optimizations (SSA construction, register
allocation, copy elimination, etc.) work on temporaries. Being able to
easily ignore all non-temporaries has the benefit that optimizations can
be faster.

Previously, Temps were not uniquely numbered: argument 1, local 1, and
temporary 1 all had the same number and were distinguishable by their
type. So, for any mapping from Temp to something else, a QHash was used.
Now that Temps only hold proper temporaries, the indexes do uniquely
identify them. Add to that the fact that after transforming to SSA form
all temporaries are renumbered starting from 0 and without any holes in
the numbering, many of those datastructures can be changed to simple
vectors. That change gives a noticeable performance improvement.

One implication of this change is that a number of functions that took
a Temp as their argument, now need to take Temp-or-ArgLocal, so Expr.
However, it turns out that there are very few places where that applies,
as many of those places also need to take constants or names. However,
explicitly separating memory loads/stores for arguments/locals from
temporaries adds the benefit that it's now easier to do a peep-hole
optimizer for those load/store operations in the future: when a load is

src/qml/compiler/qv4codegen.cpp
@@ -509,8 +509,8 @@ IR::Expr *Codegen::member(IR::Expr *base, const QString *name)
     if (hasError)
         return 0;
 
-    if (base->asTemp() /*|| base->asName()*/)
-        return _block->MEMBER(base->asTemp(), name);
+    if (base->asTemp() || base->asArgLocal())
+        return _block->MEMBER(base, name);
     else {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), base);
@@ -523,25 +523,26 @@ IR::Expr *Codegen::subscript(IR::Expr *base, IR::Expr *index)
     if (hasError)
         return 0;
 
-    if (! base->asTemp()) {
+    if (! base->asTemp() || base->asArgLocal()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), base);
         base = _block->TEMP(t);
     }
 
-    if (! index->asTemp()) {
+    if (! index->asTemp() || index->asArgLocal()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), index);
         index = _block->TEMP(t);
     }
 
-    Q_ASSERT(base->asTemp() && index->asTemp());
+    Q_ASSERT(base->asTemp() || base->asArgLocal());
+    Q_ASSERT(index->asTemp() || index->asArgLocal());
     return _block->SUBSCRIPT(base->asTemp(), index->asTemp());
 }
 
 IR::Expr *Codegen::argument(IR::Expr *expr)
 {
-    if (expr && ! expr->asTemp()) {
+    if (expr && !expr->asTemp() && !expr->asArgLocal()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), expr);
         expr = _block->TEMP(t);
@@ -555,7 +556,7 @@ IR::Expr *Codegen::reference(IR::Expr *expr)
     if (hasError)
         return 0;
 
-    if (expr && !expr->asTemp() && !expr->asName() && !expr->asMember() && !expr->asSubscript()) {
+    if (expr && !expr->asTemp() && !expr->asArgLocal() && !expr->asName() && !expr->asMember() && !expr->asSubscript()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), expr);
         expr = _block->TEMP(t);
@@ -591,13 +592,13 @@ IR::Expr *Codegen::unop(IR::AluOp op, IR::Expr *expr)
             }
         }
     }
-    if (! expr->asTemp()) {
+    if (!expr->asTemp() && !expr->asArgLocal()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), expr);
         expr = _block->TEMP(t);
     }
-    Q_ASSERT(expr->asTemp());
-    return _block->UNOP(op, expr->asTemp());
+    Q_ASSERT(expr->asTemp() || expr->asArgLocal());
+    return _block->UNOP(op, expr);
 }
 
 IR::Expr *Codegen::binop(IR::AluOp op, IR::Expr *left, IR::Expr *right)
@@ -655,20 +656,20 @@ IR::Expr *Codegen::binop(IR::AluOp op, IR::Expr *left, IR::Expr *right)
         }
     }
 
-    if (!left->asTemp()) {
+    if (!left->asTemp() && !left->asArgLocal()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), left);
         left = _block->TEMP(t);
     }
 
-    if (!right->asTemp()) {
+    if (!right->asTemp() && !right->asArgLocal()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), right);
         right = _block->TEMP(t);
     }
 
-    Q_ASSERT(left->asTemp());
-    Q_ASSERT(right->asTemp());
+    Q_ASSERT(left->asTemp() || left->asArgLocal());
+    Q_ASSERT(right->asTemp() || right->asArgLocal());
 
     return _block->BINOP(op, left, right);
 }
@@ -693,12 +694,12 @@ void Codegen::move(IR::Expr *target, IR::Expr *source, IR::AluOp op)
         return;
     }
 
-    if (!source->asTemp() && !source->asConst() && !target->asTemp()) {
+    if (!source->asTemp() && !source->asConst() && !target->asTemp() && !source->asArgLocal() && !target->asArgLocal()) {
         unsigned t = _block->newTemp();
         _block->MOVE(_block->TEMP(t), source);
         source = _block->TEMP(t);
     }
-    if (source->asConst() && !target->asTemp()) {
+    if (source->asConst() && !target->asTemp() && !target->asArgLocal()) {
         unsigned t = _block->newTemp();
         _block->MOVE(_block->TEMP(t), source);
         source = _block->TEMP(t);
@@ -749,7 +750,7 @@ void Codegen::statement(ExpressionNode *ast)
         if (r.format == ex) {
             if (r->asCall()) {
                 _block->EXP(*r); // the nest nx representation for calls is EXP(CALL(c..))
-            } else if (r->asTemp()) {
+            } else if (r->asTemp() || r->asArgLocal()) {
                 // there is nothing to do
             } else {
                 unsigned t = _block->newTemp();
@@ -1065,7 +1066,7 @@ bool Codegen::visit(ArrayLiteral *ast)
         current = arg;
 
         IR::Expr *exp = *expr;
-        if (exp->asTemp() || exp->asConst()) {
+        if (exp->asTemp() || expr->asArgLocal() || exp->asConst()) {
             current->expr = exp;
         } else {
             unsigned value = _block->newTemp();
@@ -1236,7 +1237,7 @@ bool Codegen::visit(BinaryExpression *ast)
     case QSOperator::Lt:
     case QSOperator::StrictEqual:
     case QSOperator::StrictNotEqual: {
-        if (!left->asTemp() && !left->asConst()) {
+        if (!left->asTemp() && !left->asArgLocal() && !left->asConst()) {
             const unsigned t = _block->newTemp();
             move(_block->TEMP(t), left);
             left = _block->TEMP(t);
@@ -1270,7 +1271,7 @@ bool Codegen::visit(BinaryExpression *ast)
     case QSOperator::RShift:
     case QSOperator::Sub:
     case QSOperator::URShift: {
-        if (!left->asTemp() && !left->asConst()) {
+        if (!left->asTemp() && !left->asArgLocal() && !left->asConst()) {
             const unsigned t = _block->newTemp();
             move(_block->TEMP(t), left);
             left = _block->TEMP(t);
@@ -1347,8 +1348,8 @@ bool Codegen::visit(DeleteExpression *ast)
 
     IR::Expr* expr = *expression(ast->expression);
     // Temporaries cannot be deleted
-    IR::Temp *t = expr->asTemp();
-    if (t && t->index < static_cast<unsigned>(_env->members.size())) {
+    IR::ArgLocal *al = expr->asArgLocal();
+    if (al && al->index < static_cast<unsigned>(_env->members.size())) {
         // Trying to delete a function argument might throw.
         if (_function->isStrict) {
             throwSyntaxError(ast->deleteToken, QStringLiteral("Delete of an unqualified identifier in strict mode."));
@@ -1375,7 +1376,9 @@ bool Codegen::visit(DeleteExpression *ast)
         _expr.code = _block->CONST(IR::BoolType, 1);
         return false;
     }
-    if (expr->asTemp() && expr->asTemp()->index >=  static_cast<unsigned>(_env->members.size())) {
+    if (expr->asTemp() ||
+            (expr->asArgLocal() &&
+             expr->asArgLocal()->index >= static_cast<unsigned>(_env->members.size()))) {
         _expr.code = _block->CONST(IR::BoolType, 1);
         return false;
     }
@@ -1435,10 +1438,10 @@ IR::Expr *Codegen::identifier(const QString &name, int line, int col)
         int index = e->findMember(name);
         Q_ASSERT (index < e->members.size());
         if (index != -1) {
-            IR::Temp *t = _block->LOCAL(index, scope);
+            IR::ArgLocal *al = _block->LOCAL(index, scope);
             if (name == QStringLiteral("arguments") || name == QStringLiteral("eval"))
-                t->isArgumentsOrEval = true;
-            return t;
+                al->isArgumentsOrEval = true;
+            return al;
         }
         const int argIdx = f->indexOfArgument(&name);
         if (argIdx != -1)
@@ -1497,7 +1500,7 @@ bool Codegen::visit(NewExpression *ast)
 
     Result base = expression(ast->expression);
     IR::Expr *expr = *base;
-    if (expr && !expr->asTemp() && !expr->asName() && !expr->asMember()) {
+    if (expr && !expr->asTemp() && !expr->asArgLocal() && !expr->asName() && !expr->asMember()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), expr);
         expr = _block->TEMP(t);
@@ -1513,7 +1516,7 @@ bool Codegen::visit(NewMemberExpression *ast)
 
     Result base = expression(ast->base);
     IR::Expr *expr = *base;
-    if (expr && !expr->asTemp() && !expr->asName() && !expr->asMember()) {
+    if (expr && !expr->asTemp() && !expr->asArgLocal() && !expr->asName() && !expr->asMember()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), expr);
         expr = _block->TEMP(t);
@@ -2798,8 +2801,8 @@ bool Codegen::throwSyntaxErrorOnEvalOrArgumentsInStrictMode(IR::Expr *expr, cons
     if (IR::Name *n = expr->asName()) {
         if (*n->id != QLatin1String("eval") && *n->id != QLatin1String("arguments"))
             return false;
-    } else if (IR::Temp *t = expr->asTemp()) {
-        if (!t->isArgumentsOrEval)
+    } else if (IR::ArgLocal *al = expr->asArgLocal()) {
+        if (!al->isArgumentsOrEval)
             return false;
     } else {
         return false;

src/qml/compiler/qqmltypecompiler_p.h
@@ -394,6 +394,7 @@ private:
     virtual void visitClosure(QV4::IR::Closure *closure);
 
     virtual void visitTemp(QV4::IR::Temp *) {}
+    virtual void visitArgLocal(QV4::IR::ArgLocal *) {}
 
     virtual void visitMove(QV4::IR::Move *s) {
         s->source->accept(this);

src/qml/compiler/qv4isel_moth_p.h
@@ -80,60 +80,60 @@ protected:
     virtual void visitCJump(IR::CJump *);
     virtual void visitRet(IR::Ret *);
 
-    virtual void callBuiltinInvalid(IR::Name *func, IR::ExprList *args, IR::Temp *result);
-    virtual void callBuiltinTypeofMember(IR::Expr *base, const QString &name, IR::Temp *result);
-    virtual void callBuiltinTypeofSubscript(IR::Expr *base, IR::Expr *index, IR::Temp *result);
-    virtual void callBuiltinTypeofName(const QString &name, IR::Temp *result);
-    virtual void callBuiltinTypeofValue(IR::Expr *value, IR::Temp *result);
-    virtual void callBuiltinDeleteMember(IR::Temp *base, const QString &name, IR::Temp *result);
-    virtual void callBuiltinDeleteSubscript(IR::Temp *base, IR::Expr *index, IR::Temp *result);
-    virtual void callBuiltinDeleteName(const QString &name, IR::Temp *result);
-    virtual void callBuiltinDeleteValue(IR::Temp *result);
+    virtual void callBuiltinInvalid(IR::Name *func, IR::ExprList *args, IR::Expr *result);
+    virtual void callBuiltinTypeofMember(IR::Expr *base, const QString &name, IR::Expr *result);
+    virtual void callBuiltinTypeofSubscript(IR::Expr *base, IR::Expr *index, IR::Expr *result);
+    virtual void callBuiltinTypeofName(const QString &name, IR::Expr *result);
+    virtual void callBuiltinTypeofValue(IR::Expr *value, IR::Expr *result);
+    virtual void callBuiltinDeleteMember(IR::Expr *base, const QString &name, IR::Expr *result);
+    virtual void callBuiltinDeleteSubscript(IR::Expr *base, IR::Expr *index, IR::Expr *result);
+    virtual void callBuiltinDeleteName(const QString &name, IR::Expr *result);
+    virtual void callBuiltinDeleteValue(IR::Expr *result);
     virtual void callBuiltinThrow(IR::Expr *arg);
     virtual void callBuiltinReThrow();
-    virtual void callBuiltinUnwindException(IR::Temp *);
+    virtual void callBuiltinUnwindException(IR::Expr *);
     virtual void callBuiltinPushCatchScope(const QString &exceptionName);
-    virtual void callBuiltinForeachIteratorObject(IR::Expr *arg, IR::Temp *result);
-    virtual void callBuiltinForeachNextPropertyname(IR::Temp *arg, IR::Temp *result);
-    virtual void callBuiltinPushWithScope(IR::Temp *arg);
+    virtual void callBuiltinForeachIteratorObject(IR::Expr *arg, IR::Expr *result);
+    virtual void callBuiltinForeachNextPropertyname(IR::Expr *arg, IR::Expr *result);
+    virtual void callBuiltinPushWithScope(IR::Expr *arg);
     virtual void callBuiltinPopScope();
     virtual void callBuiltinDeclareVar(bool deletable, const QString &name);
-    virtual void callBuiltinDefineArray(IR::Temp *result, IR::ExprList *args);
-    virtual void callBuiltinDefineObjectLiteral(IR::Temp *result, int keyValuePairCount, IR::ExprList *keyValuePairs, IR::ExprList *arrayEntries, bool needSparseArray);
-    virtual void callBuiltinSetupArgumentObject(IR::Temp *result);
+    virtual void callBuiltinDefineArray(IR::Expr *result, IR::ExprList *args);
+    virtual void callBuiltinDefineObjectLiteral(IR::Expr *result, int keyValuePairCount, IR::ExprList *keyValuePairs, IR::ExprList *arrayEntries, bool needSparseArray);
+    virtual void callBuiltinSetupArgumentObject(IR::Expr *result);
     virtual void callBuiltinConvertThisToObject();
-    virtual void callValue(IR::Temp *value, IR::ExprList *args, IR::Temp *result);
-    virtual void callProperty(IR::Expr *base, const QString &name, IR::ExprList *args, IR::Temp *result);
-    virtual void callSubscript(IR::Expr *base, IR::Expr *index, IR::ExprList *args, IR::Temp *result);
-    virtual void convertType(IR::Temp *source, IR::Temp *target);
-    virtual void constructActivationProperty(IR::Name *func, IR::ExprList *args, IR::Temp *result);
-    virtual void constructProperty(IR::Temp *base, const QString &name, IR::ExprList *args, IR::Temp *result);
-    virtual void constructValue(IR::Temp *value, IR::ExprList *args, IR::Temp *result);
-    virtual void loadThisObject(IR::Temp *temp);
-    virtual void loadQmlIdArray(IR::Temp *temp);
-    virtual void loadQmlImportedScripts(IR::Temp *temp);
-    virtual void loadQmlContextObject(IR::Temp *temp);
-    virtual void loadQmlScopeObject(IR::Temp *temp);
-    virtual void loadQmlSingleton(const QString &name, IR::Temp *temp);
-    virtual void loadConst(IR::Const *sourceConst, IR::Temp *targetTemp);
-    virtual void loadString(const QString &str, IR::Temp *targetTemp);
-    virtual void loadRegexp(IR::RegExp *sourceRegexp, IR::Temp *targetTemp);
-    virtual void getActivationProperty(const IR::Name *name, IR::Temp *temp);
+    virtual void callValue(IR::Expr *value, IR::ExprList *args, IR::Expr *result);
+    virtual void callProperty(IR::Expr *base, const QString &name, IR::ExprList *args, IR::Expr *result);
+    virtual void callSubscript(IR::Expr *base, IR::Expr *index, IR::ExprList *args, IR::Expr *result);
+    virtual void convertType(IR::Expr *source, IR::Expr *target);
+    virtual void constructActivationProperty(IR::Name *func, IR::ExprList *args, IR::Expr *result);
+    virtual void constructProperty(IR::Expr *base, const QString &name, IR::ExprList *args, IR::Expr *result);
+    virtual void constructValue(IR::Expr *value, IR::ExprList *args, IR::Expr *result);
+    virtual void loadThisObject(IR::Expr *e);
+    virtual void loadQmlIdArray(IR::Expr *e);
+    virtual void loadQmlImportedScripts(IR::Expr *e);
+    virtual void loadQmlContextObject(IR::Expr *e);
+    virtual void loadQmlScopeObject(IR::Expr *e);
+    virtual void loadQmlSingleton(const QString &name, IR::Expr *e);
+    virtual void loadConst(IR::Const *sourceConst, IR::Expr *e);
+    virtual void loadString(const QString &str, IR::Expr *target);
+    virtual void loadRegexp(IR::RegExp *sourceRegexp, IR::Expr *target);
+    virtual void getActivationProperty(const IR::Name *name, IR::Expr *target);
     virtual void setActivationProperty(IR::Expr *source, const QString &targetName);
-    virtual void initClosure(IR::Closure *closure, IR::Temp *target);
-    virtual void getProperty(IR::Expr *base, const QString &name, IR::Temp *target);
+    virtual void initClosure(IR::Closure *closure, IR::Expr *target);
+    virtual void getProperty(IR::Expr *base, const QString &name, IR::Expr *target);
     virtual void setProperty(IR::Expr *source, IR::Expr *targetBase, const QString &targetName);
     virtual void setQObjectProperty(IR::Expr *source, IR::Expr *targetBase, int propertyIndex);
-    virtual void getQObjectProperty(IR::Expr *base, int propertyIndex, bool captureRequired, int attachedPropertiesId, IR::Temp *target);
-    virtual void getElement(IR::Expr *base, IR::Expr *index, IR::Temp *target);
+    virtual void getQObjectProperty(IR::Expr *base, int propertyIndex, bool captureRequired, int attachedPropertiesId, IR::Expr *target);
+    virtual void getElement(IR::Expr *base, IR::Expr *index, IR::Expr *target);
     virtual void setElement(IR::Expr *source, IR::Expr *targetBase, IR::Expr *targetIndex);
-    virtual void copyValue(IR::Temp *sourceTemp, IR::Temp *targetTemp);
-    virtual void swapValues(IR::Temp *sourceTemp, IR::Temp *targetTemp);
-    virtual void unop(IR::AluOp oper, IR::Temp *sourceTemp, IR::Temp *targetTemp);
-    virtual void binop(IR::AluOp oper, IR::Expr *leftSource, IR::Expr *rightSource, IR::Temp *target);
+    virtual void copyValue(IR::Expr *source, IR::Expr *target);
+    virtual void swapValues(IR::Expr *source, IR::Expr *target);
+    virtual void unop(IR::AluOp oper, IR::Expr *source, IR::Expr *target);
+    virtual void binop(IR::AluOp oper, IR::Expr *leftSource, IR::Expr *rightSource, IR::Expr *target);
 
 private:
-    Param binopHelper(IR::AluOp oper, IR::Expr *leftSource, IR::Expr *rightSource, IR::Temp *target);
+    Param binopHelper(IR::AluOp oper, IR::Expr *leftSource, IR::Expr *rightSource, IR::Expr *target);
 
     struct Instruction {
 #define MOTH_INSTR_DATA_TYPEDEF(I, FMT) typedef InstrData<Instr::I> I;
@@ -145,7 +145,7 @@ private:
 
     Param getParam(IR::Expr *e);
 
-    Param getResultParam(IR::Temp *result)
+    Param getResultParam(IR::Expr *result)
     {
         if (result)
             return getParam(result);

commit 2fdb6eba0a58b629db32f9eefec2f26df08d3d2e

Fix passing of locals as function arguments with side-effects
Commit 75c22465cf8fe262edfe6178bb9ca19661fb710e regressed in allowing
locals and arguments to be passed directly as further arguments to function
calls, but that's incorrect when considering

    var i = 2;
    testFunction(i, i += 2)

where it is instrumental to place the first argument into a temp (making a
copy) instead of passing it directly.

Change-Id: Iffcf6c6eda92a8fb665982cda1db0b96359cd092
Task-number: QTBUG-45879
Reviewed-by: Lars Knoll <lars.knoll@digia.com>

src/qml/compiler/qv4codegen.cpp
@@ -538,7 +538,7 @@ IR::Expr *Codegen::subscript(IR::Expr *base, IR::Expr *index)
 
 IR::Expr *Codegen::argument(IR::Expr *expr)
 {
-    if (expr && !expr->asTemp() && !expr->asArgLocal()) {
+    if (expr && !expr->asTemp()) {
         const unsigned t = _block->newTemp();
         move(_block->TEMP(t), expr);
         expr = _block->TEMP(t);

tests/auto/qml/qjsengine/tst_qjsengine.cpp
@@ -176,6 +176,8 @@ private slots:
     void intConversion_QTBUG43309();
     void toFixed();
 
+    void argumentEvaluationOrder();
+
 signals:
     void testSignal();
 };
@@ -3626,6 +3628,25 @@ void tst_QJSEngine::toFixed()
     result = engine.evaluate(QStringLiteral("(12.05).toFixed(1)"));
     QVERIFY(result.isString());
     QCOMPARE(result.toString(), QStringLiteral("12.1"));
+}
+
+void tst_QJSEngine::argumentEvaluationOrder()
+{
+    QJSEngine engine;
+    QJSValue ok = engine.evaluate(
+            "function record(arg1, arg2) {\n"
+            "    parameters = [arg1, arg2]\n"
+            "}\n"
+            "function test() {\n"
+            "    var i = 2;\n"
+            "    record(i, i += 2);\n"
+            "}\n"
+            "test()\n"
+            "parameters[0] == 2 && parameters[1] == 4");
+    qDebug() << ok.toString();
+    QVERIFY(ok.isBool());
+    QVERIFY(ok.toBool());
+
 }
 
 QTEST_MAIN(tst_QJSEngine)