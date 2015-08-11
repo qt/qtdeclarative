d7de74d6e608fbbd36fc9abad27fa82d095cb692
Andy Nichols <andy.nichols@theqtcompany.com>
2015-08-11 12:03:16 +0200

    Update Copyright and License headers
    Change-Id: I308ac6bc053ed2648802f433e01be92f36e223b8
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>

--- src/plugins/scenegraph/softwarecontext/threadedrenderloop.cpp
+++ src/plugins/scenegraph/softwarecontext/threadedrenderloop.cpp
@@ -1,23 +1,23 @@
 /****************************************************************************
 **
-** Copyright (C) 2014 Digia Plc
 ** Copyright (C) 2014 Jolla Ltd, author: <gunnar.sletta@jollamobile.com>
-** All rights reserved.
-** For any questions to Digia, please use contact form at http://qt.digia.com
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
 **
-** This file is part of the Qt SceneGraph Raster Add-on.
+** This file is part of H.
 **
 ** $QT_BEGIN_LICENSE$
-** Licensees holding valid Qt Commercial licenses may use this file in
-** accordance with the Qt Commercial License Agreement provided with the
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia.
-**
-** If you have questions regarding the use of this file, please use
-** contact form at http://qt.digia.com
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
 ** $QT_END_LICENSE$
 **
 ****************************************************************************/
+
 #include "threadedrenderloop.h"
 
 #include <QtCore/QMutex>
