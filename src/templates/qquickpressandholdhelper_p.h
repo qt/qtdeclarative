7de4e4c98aa0297057f92afafef697696c3b17f3
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-10-05 11:08:54 +0200

    Add missing "We mean it" comments to private header.
    Change-Id: I89476d298bde3880dd2c60def9ddb5f3ad1833b6
    Reviewed-by: Liang Qi <liang.qi@theqtcompany.com>

--- src/templates/qquickpressandholdhelper_p.h
+++ src/templates/qquickpressandholdhelper_p.h
@@ -37,6 +37,17 @@
 #ifndef QQUICKPRESSANDHOLDHELPER_H
 #define QQUICKPRESSANDHOLDHELPER_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtCore/qpoint.h>
 #include <QtCore/qbasictimer.h>
 
