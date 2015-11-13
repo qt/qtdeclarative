qt/qtdeclarative

commit 134d980a7fcf61c5440019bcfb3fdfc39c3f5f3c

Don't change the currentItem after a viewport resize
When highlightRangeMode is set to StrictlyEnforceRange and the view gets
resized, we don't want the currentIndex to change.

The code touched by this patch was introduced by commit
I08b7e61496a79f71c3b40fafaca985ae90f88503 back in Qt 4.7 times, and
never changed since then.

Task-number: QTBUG-43555
Change-Id: Ie67faa6898f13a1f7b0f1c4ae6c29721cb7dfa41
Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>
Reviewed-by: Michael Brasser <michael.brasser@live.com>
Reviewed-by: Alan Alpert <aalpert@blackberry.com>

src/quick/items/qquickgridview.cpp
@@ -922,13 +922,13 @@ void QQuickGridViewPrivate::fixup(AxisData &data, qreal minExtent, qreal maxExte
             tempPosition -= bias;
         }
         FxViewItem *topItem = snapItemAt(tempPosition+highlightRangeStart);
-        if (!topItem && strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             topItem = currentItem;
         }
         FxViewItem *bottomItem = snapItemAt(tempPosition+highlightRangeEnd);
-        if (!bottomItem && strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             bottomItem = currentItem;

src/quick/items/qquicklistview.cpp
@@ -1470,13 +1470,13 @@ void QQuickListViewPrivate::fixup(AxisData &data, qreal minExtent, qreal maxExte
             tempPosition -= bias;
         }
         FxViewItem *topItem = snapItemAt(tempPosition+highlightRangeStart);
-        if (!topItem && strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             topItem = currentItem;
         }
         FxViewItem *bottomItem = snapItemAt(tempPosition+highlightRangeEnd);
-        if (!bottomItem && strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             bottomItem = currentItem;

tests/auto/quick/qquicklistview/tst_qquicklistview.cpp
@@ -179,6 +179,7 @@ private slots:
     void creationContext();
     void snapToItem_data();
     void snapToItem();
+    void snapOneItemResize_QTBUG_43555();
     void snapOneItem_data();
     void snapOneItem();
 
@@ -5201,6 +5202,37 @@ void tst_QQuickListView::snapToItem()
         QCOMPARE(listview->contentX(), startExtent);
 
     releaseView(window);
+}
+
+void tst_QQuickListView::snapOneItemResize_QTBUG_43555()
+{
+    QQuickView *window = createView();
+    window->resize(QSize(100, 320));
+    window->setResizeMode(QQuickView::SizeRootObjectToView);
+    QQuickViewTestUtil::moveMouseAway(window);
+
+    window->setSource(testFileUrl("snapOneItemResize.qml"));
+    window->show();
+    QVERIFY(QTest::qWaitForWindowExposed(window));
+
+    QQuickListView *listview = qobject_cast<QQuickListView*>(window->rootObject());
+    QTRY_VERIFY(listview != 0);
+
+    QSignalSpy currentIndexSpy(listview, SIGNAL(currentIndexChanged()));
+
+    QTRY_COMPARE(QQuickItemPrivate::get(listview)->polishScheduled, false);
+    QTRY_COMPARE(listview->currentIndex(), 5);
+    currentIndexSpy.clear();
+
+    window->resize(QSize(400, 320));
+
+    QTRY_COMPARE(int(listview->width()), 400);
+    QTRY_COMPARE(QQuickItemPrivate::get(listview)->polishScheduled, false);
+
+    QTRY_COMPARE(listview->currentIndex(), 5);
+    QCOMPARE(currentIndexSpy.count(), 0);
+
+    delete window;
 }
 
 void tst_QQuickListView::qAbstractItemModel_package_items()

tests/auto/quick/qquicklistview/data/snapOneItemResize.qml
@@ -1,0 +1,16 @@
+import QtQuick 2.0
+
+ListView {
+    id: list
+    currentIndex: 5
+    snapMode: ListView.SnapOneItem
+    orientation: ListView.Horizontal
+    highlightRangeMode: ListView.StrictlyEnforceRange
+    highlightFollowsCurrentItem: true
+    model: 10
+    spacing: 10
+    delegate: Item {
+        width: ListView.view.width
+        height: ListView.view.height
+    }
+}

commit 2b779fbd25aaf09897ee2cdc4edffd12a980420b

Don't make currentIndex skip an extra item on removal
Improve the logic for determining the desired viewport position, which
got partially broken with 134d980a7fcf61c5440019bcfb3fdfc39c3f5f3c.
Let's not alter topItem and bottomItem if their index appears to be
correct.

Task-number: QTBUG-49330
Change-Id: Ib1c88de51be28cbb0afb1741440adb03ae8ebd87
Reviewed-by: Albert Astals Cid <albert.astals@canonical.com>
Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

src/quick/items/qquickgridview.cpp
@@ -922,13 +922,13 @@ void QQuickGridViewPrivate::fixup(AxisData &data, qreal minExtent, qreal maxExte
             tempPosition -= bias;
         }
         FxViewItem *topItem = snapItemAt(tempPosition+highlightRangeStart);
-        if (strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem && (!topItem || topItem->index != currentIndex)) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             topItem = currentItem;
         }
         FxViewItem *bottomItem = snapItemAt(tempPosition+highlightRangeEnd);
-        if (strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem && (!bottomItem || bottomItem->index != currentIndex)) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             bottomItem = currentItem;

src/quick/items/qquicklistview.cpp
@@ -1479,13 +1479,13 @@ void QQuickListViewPrivate::fixup(AxisData &data, qreal minExtent, qreal maxExte
             tempPosition -= bias;
         }
         FxViewItem *topItem = snapItemAt(tempPosition+highlightRangeStart);
-        if (strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem && (!topItem || topItem->index != currentIndex)) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             topItem = currentItem;
         }
         FxViewItem *bottomItem = snapItemAt(tempPosition+highlightRangeEnd);
-        if (strictHighlightRange && currentItem) {
+        if (strictHighlightRange && currentItem && (!bottomItem || bottomItem->index != currentIndex)) {
             // StrictlyEnforceRange always keeps an item in range
             updateHighlight();
             bottomItem = currentItem;

tests/auto/quick/qquicklistview/data/snapOneItemCurrentIndexRemoveAnimation.qml
@@ -1,0 +1,39 @@
+import QtQuick 2.4
+
+ListView {
+    id: root
+    height: 400
+    width: height
+    model: ListModel {
+        id: lmodel
+        ListElement { dummy: 0 }
+        ListElement { dummy: 0 }
+        ListElement { dummy: 0 }
+        ListElement { dummy: 0 }
+        ListElement { dummy: 0 }
+        ListElement { dummy: 0 }
+    }
+
+    function removeItemZero()
+    {
+        lmodel.remove(0);
+    }
+
+    orientation: ListView.Horizontal
+    snapMode: ListView.SnapOneItem
+    highlightRangeMode: ListView.StrictlyEnforceRange
+
+    property int transitionsRun: 0
+
+    removeDisplaced: Transition {
+        id: transition
+        PropertyAnimation { property: "x"; duration: 500 }
+        onRunningChanged: if (!running) transitionsRun++;
+    }
+
+    delegate: Text {
+        text: index + " of " + lmodel.count
+        width: root.width
+        height: root.height
+    }
+}

tests/auto/quick/qquicklistview/tst_qquicklistview.cpp
@@ -182,6 +182,7 @@ private slots:
     void snapOneItemResize_QTBUG_43555();
     void snapOneItem_data();
     void snapOneItem();
+    void snapOneItemCurrentIndexRemoveAnimation();
 
     void QTBUG_9791();
     void QTBUG_11105();
@@ -5585,6 +5586,32 @@ void tst_QQuickListView::snapOneItem()
     }
 
     releaseView(window);
+}
+
+void tst_QQuickListView::snapOneItemCurrentIndexRemoveAnimation()
+{
+    QQuickView *window = createView();
+
+    window->setSource(testFileUrl("snapOneItemCurrentIndexRemoveAnimation.qml"));
+    window->show();
+    QVERIFY(QTest::qWaitForWindowExposed(window));
+
+    QQuickListView *listview = qobject_cast<QQuickListView*>(window->rootObject());
+    QTRY_VERIFY(listview != 0);
+
+    QTRY_COMPARE(QQuickItemPrivate::get(listview)->polishScheduled, false);
+    QTRY_COMPARE(listview->currentIndex(), 0);
+    QSignalSpy currentIndexSpy(listview, SIGNAL(currentIndexChanged()));
+
+    QMetaObject::invokeMethod(window->rootObject(), "removeItemZero");
+    QTRY_COMPARE(listview->property("transitionsRun").toInt(), 1);
+
+    QTRY_COMPARE(QQuickItemPrivate::get(listview)->polishScheduled, false);
+
+    QCOMPARE(listview->currentIndex(), 0);
+    QCOMPARE(currentIndexSpy.count(), 0);
+
+    delete window;
 }
 
 void tst_QQuickListView::attachedProperties_QTBUG_32836()