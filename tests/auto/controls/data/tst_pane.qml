qt/qtdeclarative

commit 38c47c0a7cf9e40aedea7fadefc82e448a740c45

Introduce Pane
Provides a background color that matches with the application style
and theme.

Change-Id: I5e7ea4fededaaf46687a2ea0391f43dbf55e3b42
Reviewed-by: Mitch Curtis <mitch.curtis@theqtcompany.com>

src/templates/qquickpane.cpp
@@ -1,0 +1,182 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the Qt Labs Templates module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qquickpane_p.h"
+#include "qquickpane_p_p.h"
+
+QT_BEGIN_NAMESPACE
+
+/*!
+    \qmltype Pane
+    \inherits Control
+    \instantiates QQuickPane
+    \inqmlmodule Qt.labs.controls
+    \ingroup qtlabscontrols-containers
+    \brief A pane control.
+
+    Pane provides a background color that matches with the application style
+    and theme. Pane does not provide a layout of its own, but requires you to
+    position its contents, for instance by creating a \l RowLayout or a
+    \l ColumnLayout.
+
+    If only a single item is used within a Pane, it will resize to fit the
+    implicit size of its contained item. This makes it particularly suitable
+    for use together with layouts.
+
+    \image qtlabscontrols-pane.png
+
+    \snippet qtlabscontrols-pane.qml 1
+
+    \sa {Customizing Pane}, {Container Controls}
+*/
+
+QQuickPanePrivate::QQuickPanePrivate() : contentWidth(0), contentHeight(0)
+{
+}
+
+QQuickPane::QQuickPane(QQuickItem *parent) :
+    QQuickControl(*(new QQuickPanePrivate), parent)
+{
+    setFlag(QQuickItem::ItemIsFocusScope);
+}
+
+QQuickPane::QQuickPane(QQuickPanePrivate &dd, QQuickItem *parent) :
+    QQuickControl(dd, parent)
+{
+    setFlag(QQuickItem::ItemIsFocusScope);
+}
+
+/*!
+    \qmlproperty real Qt.labs.controls::Pane::contentWidth
+
+    This property holds the content width. It is used for calculating the
+    total implicit width of the pane.
+
+    \note If only a single item is used within the pane, the implicit width
+          of its contained item is used as the content width.
+*/
+qreal QQuickPane::contentWidth() const
+{
+    Q_D(const QQuickPane);
+    return d->contentWidth;
+}
+
+void QQuickPane::setContentWidth(qreal width)
+{
+    Q_D(QQuickPane);
+    if (d->contentWidth != width) {
+        d->contentWidth = width;
+        emit contentWidthChanged();
+    }
+}
+
+/*!
+    \qmlproperty real Qt.labs.controls::Pane::contentHeight
+
+    This property holds the content height. It is used for calculating the
+    total implicit height of the pane.
+
+    \note If only a single item is used within the pane, the implicit height
+          of its contained item is used as the content height.
+*/
+qreal QQuickPane::contentHeight() const
+{
+    Q_D(const QQuickPane);
+    return d->contentHeight;
+}
+
+void QQuickPane::setContentHeight(qreal height)
+{
+    Q_D(QQuickPane);
+    if (d->contentHeight != height) {
+        d->contentHeight = height;
+        emit contentHeightChanged();
+    }
+}
+
+/*!
+    \qmlproperty list<Object> Qt.labs.controls::Pane::contentData
+    \default
+
+    This property holds the list of content data.
+
+    \sa Item::data
+*/
+QQmlListProperty<QObject> QQuickPane::contentData()
+{
+    Q_D(QQuickPane);
+    return QQmlListProperty<QObject>(d->contentItem, Q_NULLPTR,
+                                     QQuickItemPrivate::data_append,
+                                     QQuickItemPrivate::data_count,
+                                     QQuickItemPrivate::data_at,
+                                     QQuickItemPrivate::data_clear);
+}
+
+/*!
+    \qmlproperty list<Item> Qt.labs.controls::Pane::contentChildren
+
+    This property holds the list of content children.
+
+    \sa Item::children
+*/
+QQmlListProperty<QQuickItem> QQuickPane::contentChildren()
+{
+    Q_D(QQuickPane);
+    return QQmlListProperty<QQuickItem>(d->contentItem, Q_NULLPTR,
+                                        QQuickItemPrivate::children_append,
+                                        QQuickItemPrivate::children_count,
+                                        QQuickItemPrivate::children_at,
+                                        QQuickItemPrivate::children_clear);
+}
+
+void QQuickPane::contentItemChange(QQuickItem *newItem, QQuickItem *oldItem)
+{
+    QQuickControl::contentItemChange(newItem, oldItem);
+    if (oldItem)
+        disconnect(oldItem, &QQuickItem::childrenChanged, this, &QQuickPane::contentChildrenChanged);
+    if (newItem)
+        connect(newItem, &QQuickItem::childrenChanged, this, &QQuickPane::contentChildrenChanged);
+    emit contentChildrenChanged();
+}
+
+#ifndef QT_NO_ACCESSIBILITY
+QAccessible::Role QQuickPane::accessibleRole() const
+{
+    return QAccessible::Pane;
+}
+#endif
+
+QT_END_NAMESPACE

tests/auto/controls/data/tst_pane.qml
@@ -1,0 +1,116 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+import QtQuick 2.2
+import QtTest 1.0
+import Qt.labs.controls 1.0
+
+TestCase {
+    id: testCase
+    width: 400
+    height: 400
+    visible: true
+    when: windowShown
+    name: "Pane"
+
+    Component {
+        id: pane
+        Pane { }
+    }
+
+    Component {
+        id: oneChildPane
+        GroupBox {
+            Item {
+                implicitWidth: 100
+                implicitHeight: 30
+            }
+        }
+    }
+
+    Component {
+        id: twoChildrenPane
+        GroupBox {
+            Item {
+                implicitWidth: 100
+                implicitHeight: 30
+            }
+            Item {
+                implicitWidth: 200
+                implicitHeight: 60
+            }
+        }
+    }
+
+    function test_empty() {
+        var control = pane.createObject(testCase)
+        verify(control)
+
+        verify(control.contentItem)
+        compare(control.contentWidth, 0)
+        compare(control.contentHeight, 0)
+
+        control.destroy()
+    }
+
+    function test_oneChild() {
+        var control = oneChildPane.createObject(testCase)
+        verify(control)
+
+        compare(control.contentWidth, 100)
+        compare(control.contentHeight, 30)
+        verify(control.implicitWidth > 100)
+        verify(control.implicitHeight > 30)
+
+        control.destroy()
+    }
+
+    function test_twoChildren() {
+        var control = twoChildrenPane.createObject(testCase)
+        verify(control)
+
+        compare(control.contentWidth, 0)
+        compare(control.contentHeight, 0)
+        verify(control.implicitWidth > 0)
+        verify(control.implicitHeight > 0)
+
+        control.destroy()
+    }
+}

src/imports/controls/Pane.qml
@@ -3,7 +3,7 @@
 ** Copyright (C) 2015 The Qt Company Ltd.
 ** Contact: http://www.qt.io/licensing/
 **
-** This file is part of the Qt Labs Templates module of the Qt Toolkit.
+** This file is part of the Qt Labs Controls module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL3$
 ** Commercial License Usage
@@ -34,72 +34,27 @@
 **
 ****************************************************************************/
 
-#include "qquickframe_p.h"
-#include "qquickframe_p_p.h"
+import QtQuick 2.6
+import Qt.labs.templates 1.0 as T
 
-QT_BEGIN_NAMESPACE
+T.Frame {
+    id: control
 
-/*!
-    \qmltype Frame
-    \inherits Pane
-    \instantiates QQuickFrame
-    \inqmlmodule Qt.labs.controls
-    \ingroup qtlabscontrols-containers
-    \brief A frame control.
+    implicitWidth: Math.max(background ? background.implicitWidth : 0, contentWidth + leftPadding + rightPadding)
+    implicitHeight: Math.max(background ? background.implicitHeight : 0, contentHeight + topPadding + bottomPadding)
 
-    Frame is used to layout a logical group of controls together, within a
-    visual frame. Frame does not provide a layout of its own, but requires
-    you to position its contents, for instance by creating a \l RowLayout
-    or a \l ColumnLayout.
+    contentWidth: contentChildren.length === 1 ? contentChildren[0].implicitWidth : 0
+    contentHeight: contentChildren.length === 1 ? contentChildren[0].implicitHeight : 0
 
-    If only a single item is used within a Frame, it will resize to fit the
-    implicit size of its contained item. This makes it particularly suitable
-    for use together with layouts.
+    padding: 6
 
-    \image qtlabscontrols-frame.png
+    //! [contentItem]
+    contentItem: Item { }
+    //! [contentItem]
 
-    \snippet qtlabscontrols-frame.qml 1
-
-    \sa {Customizing Frame}, {Container Controls}
-*/
-
-QQuickFramePrivate::QQuickFramePrivate() : frame(Q_NULLPTR)
-{
-}
-
-QQuickFrame::QQuickFrame(QQuickItem *parent) :
-    QQuickPane(*(new QQuickFramePrivate), parent)
-{
-}
-
-QQuickFrame::QQuickFrame(QQuickFramePrivate &dd, QQuickItem *parent) :
-    QQuickPane(dd, parent)
-{
-}
-
-/*!
-    \qmlproperty Item Qt.labs.controls::Frame::frame
-
-    This property holds the visual frame item.
-
-    \sa {Customizing Frame}
-*/
-QQuickItem *QQuickFrame::frame() const
-{
-    Q_D(const QQuickFrame);
-    return d->frame;
-}
-
-void QQuickFrame::setFrame(QQuickItem *frame)
-{
-    Q_D(QQuickFrame);
-    if (d->frame != frame) {
-        delete d->frame;
-        d->frame = frame;
-        if (frame && !frame->parentItem())
-            frame->setParentItem(this);
-        emit frameChanged();
+    //! [background]
+    background: Rectangle {
+        color: "#ffffff"
     }
+    //! [background]
 }
-
-QT_END_NAMESPACE

src/imports/controls/controls.pri
@@ -12,6 +12,7 @@ QML_FILES = \
     Label.qml \
     Menu.qml \
     MenuItem.qml \
+    Pane.qml \
     PageIndicator.qml \
     ProgressBar.qml \
     RadioButton.qml \

src/imports/controls/doc/snippets/qtlabscontrols-pane-background.qml
@@ -1,0 +1,38 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the documentation of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:FDL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Free Documentation License Usage
+** Alternatively, this file may be used under the terms of the GNU Free
+** Documentation License version 1.3 as published by the Free Software
+** Foundation and appearing in the file included in the packaging of
+** this file. Please review the following information to ensure
+** the GNU Free Documentation License version 1.3 requirements
+** will be met: http://www.gnu.org/copyleft/fdl.html.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+import QtQuick 2.0
+import Qt.labs.controls 1.0
+
+Pane {
+    width: 100
+    height: 100
+    background: Rectangle {
+        color: 'transparent'
+        border.color: 'red'
+    }
+}

src/imports/controls/doc/snippets/qtlabscontrols-pane.qml
@@ -1,0 +1,41 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the documentation of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:FDL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Free Documentation License Usage
+** Alternatively, this file may be used under the terms of the GNU Free
+** Documentation License version 1.3 as published by the Free Software
+** Foundation and appearing in the file included in the packaging of
+** this file. Please review the following information to ensure
+** the GNU Free Documentation License version 1.3 requirements
+** will be met: http://www.gnu.org/copyleft/fdl.html.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+import QtQuick 2.0
+import QtQuick.Layouts 1.0
+import Qt.labs.controls 1.0
+
+//! [1]
+Pane {
+    ColumnLayout {
+        anchors.fill: parent
+        CheckBox { text: qsTr("E-mail") }
+        CheckBox { text: qsTr("Calendar") }
+        CheckBox { text: qsTr("Contacts") }
+    }
+}
+//! [1]

commit 9d39a470d1393ab429d03010bedb8cd631457842

Pane: don't let presses leak through
A regression introduced by 38c47c0. Pane is the base type for Frame,
GroupBox, and ToolBar. These type of containers shouldn't let presses
through. For example, an application that has a listview scrolled
below a toolbar, list items under the toolbar should not react when
interacting with the toolbar.

Change-Id: I7668452fe2d6d89372f032220fa5623b50d334bb
Reviewed-by: Mitch Curtis <mitch.curtis@theqtcompany.com>

src/templates/qquickpane.cpp
@@ -71,12 +71,14 @@ QQuickPane::QQuickPane(QQuickItem *parent) :
     QQuickControl(*(new QQuickPanePrivate), parent)
 {
     setFlag(QQuickItem::ItemIsFocusScope);
+    setAcceptedMouseButtons(Qt::AllButtons);
 }
 
 QQuickPane::QQuickPane(QQuickPanePrivate &dd, QQuickItem *parent) :
     QQuickControl(dd, parent)
 {
     setFlag(QQuickItem::ItemIsFocusScope);
+    setAcceptedMouseButtons(Qt::AllButtons);
 }
 
 /*!

tests/auto/controls/data/tst_pane.qml
@@ -79,6 +79,19 @@ TestCase {
         }
     }
 
+    Component {
+        id: pressPane
+        MouseArea {
+            width: 200
+            height: 200
+            property int pressCount
+            onPressed: ++pressCount
+            Pane {
+                anchors.fill: parent
+            }
+        }
+    }
+
     function test_empty() {
         var control = pane.createObject(testCase)
         verify(control)
@@ -110,6 +123,21 @@ TestCase {
         compare(control.contentHeight, 0)
         verify(control.implicitWidth > 0)
         verify(control.implicitHeight > 0)
+
+        control.destroy()
+    }
+
+    function test_press() {
+        var control = pressPane.createObject(testCase)
+        verify(control)
+
+        compare(control.pressCount, 0)
+        mouseClick(control)
+        compare(control.pressCount, 0)
+
+        control.children[0].enabled = false
+        mouseClick(control)
+        compare(control.pressCount, 1)
 
         control.destroy()
     }