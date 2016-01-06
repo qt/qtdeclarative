893067a47458ffd686a419b9dd6288b6ee2667fa
Edward Welbourne <edward.welbourne@theqtcompany.com>
2016-01-06 10:43:14 +0100

    Correct declaration in doc to match const in code.
    QSGTextureProvider::texture() was getting a warning from qdoc because
    it didn't find an exact match for its signature: the doc lacked const.
    Ditch some spurious semicolons while we're about it.
    
    Change-Id: I785ae8b8d46c839a1e29339284834a8b8e44bffe
    Reviewed-by: Topi Reiniö <topi.reinio@theqtcompany.com>

--- src/quick/scenegraph/util/qsgtextureprovider.cpp
+++ src/quick/scenegraph/util/qsgtextureprovider.cpp
@@ -45,12 +45,12 @@ QT_BEGIN_NAMESPACE
     \sa {Scene Graph - Two Texture Providers}
  */
 /*!
-    \fn QSGTexture *QSGTextureProvider::texture();
+    \fn QSGTexture *QSGTextureProvider::texture() const
 
     Returns a pointer to the texture object.
  */
 /*!
-    \fn void QSGTextureProvider::textureChanged();
+    \fn void QSGTextureProvider::textureChanged()
 
     This signal is emitted when the texture changes.
  */
