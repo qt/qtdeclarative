54ad7f2456cd851b6d55f9289414585de952a882
J-P Nurmi <jpnurmi@gmail.com>
2016-02-01 21:47:13 +0100

    Fix Universal & Material style font checks
    Use QFont::exactMatch() to find out whether the font exists.
    
    Change-Id: Ie4f0a3a55c9337c504684aa285c93391e41279aa
    Task-number: QTBUG-50815
    Reviewed-by: Liang Qi <liang.qi@theqtcompany.com>

--- src/imports/controls/universal/qtlabsuniversalstyleplugin.cpp
+++ src/imports/controls/universal/qtlabsuniversalstyleplugin.cpp
@@ -84,7 +84,7 @@ void QtLabsUniversalStylePlugin::initializeEngine(QQmlEngine *engine, const char
 {
     QQuickStyleSelector selector;
     if (selector.style() == QLatin1String("universal")) {
-        if (QFont(QStringLiteral("Segoe UI")).family() == QLatin1String("Segoe UI")) {
+        if (QFont(QStringLiteral("Segoe UI")).exactMatch()) {
             QPlatformTheme *old = QGuiApplicationPrivate::platform_theme;
             if (old) {
                 QQuickProxyTheme *theme = new QQuickUniversalTheme(old);
