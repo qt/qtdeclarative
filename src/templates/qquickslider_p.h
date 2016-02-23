d0ab538c1ffadecfe0aa7130918726de7126dd16
J-P Nurmi <jpnurmi@theqtcompany.com>
2016-02-23 15:29:05 +0100

    Slider: add wheel support
    Change-Id: I413d8c853bfe3cc748584dfebd28172ead89d638
    Task-number: QTBUG-50221
    Reviewed-by: Mitch Curtis <mitch.curtis@theqtcompany.com>

--- src/templates/qquickslider_p.h
+++ src/templates/qquickslider_p.h
@@ -133,6 +133,8 @@ protected:
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
     void mouseUngrabEvent() override;
+    void wheelEvent(QWheelEvent *event) override;
+
     void mirrorChange() override;
     void componentComplete() override;
 
