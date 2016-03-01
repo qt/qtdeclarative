bce843844b8edabc4365d0ef7175b54551217631
J-P Nurmi <jpnurmi@theqtcompany.com>
2016-03-01 16:25:00 +0100

    Fix QQuickPopup::setParentItem()
    Don't call naturalControlFont() or calcLocale() with a null pointer,
    because it will crash.
    
    Change-Id: Ib4b6dba85e6c55832c30b5178b74ebf39429a5f8
    Reviewed-by: Liang Qi <liang.qi@theqtcompany.com>

--- src/templates/qquickpopup.cpp
+++ src/templates/qquickpopup.cpp
@@ -1176,7 +1176,7 @@ void QQuickPopup::setParentItem(QQuickItem *parent)
         d->parentItem = parent;
         if (d->positioner.parentItem())
             d->positioner.setParentItem(parent);
-        if (d->popupItem) {
+        if (parent) {
             QQuickControlPrivate::updateFontRecur(d->popupItem, QQuickControlPrivate::naturalControlFont(parent));
             QQuickControlPrivate::updateLocaleRecur(d->popupItem, QQuickControlPrivate::calcLocale(parent));
         }
