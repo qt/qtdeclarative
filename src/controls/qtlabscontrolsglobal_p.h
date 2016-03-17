d86ab5674ac231138791aa2d699a1a8b934735ad
J-P Nurmi <jpnurmi@theqtcompany.com>
2016-03-17 14:37:25 +0100

    qtlabscontrolsglobal_p.h: add missing "we mean it"
        QtLabsControls: WARNING: qtlabscontrolsglobal_p.h does not have the "We mean it." warning
    
    Change-Id: I0f9e71b0623dcb02494d622a7d67488ea980a944
    Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

--- src/controls/qtlabscontrolsglobal_p.h
+++ src/controls/qtlabscontrolsglobal_p.h
@@ -37,6 +37,17 @@
 #ifndef QTLABSCONTROLSSGLOBAL_P_H
 #define QTLABSCONTROLSSGLOBAL_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtLabsControls/qtlabscontrolsglobal.h>
 
 #define Q_LABSCONTROLS_PRIVATE_EXPORT Q_LABSCONTROLS_EXPORT
