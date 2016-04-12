qt/qtdeclarative

commit 834f9c76151857b8e8bc07341f592187fafa11dc

QQuickImageBase: Minimize the cost for devicePixelRatio setting.
* Don't convert QUrl to QString twice (that massively blows up transient
  allocations and is very slow)
* Don't do anything at all if a sourceSize isn't set (checking it is super
  cheap)

This fixes the performance regression (introduced in 428b92583ee2102c7ef16776a9b7785df7ff4e2c)
in the delegates_image creation benchmark (from ~520 up to ~540 items per frame).

Change-Id: I1cd350e42e763b7a771ff207d23e3e411810b268
Reviewed-by: Gunnar Sletta <gunnar@sletta.org>

src/quick/items/qquickimagebase.cpp
@@ -213,15 +213,27 @@ void QQuickImageBase::load()
         d->devicePixelRatio = 1.0;
 
         QUrl loadUrl = d->url;
-        if (d->url.scheme() == QStringLiteral("image")
-            || d->url.toString().endsWith(QLatin1String(".svg"))
-            || d->url.toString().endsWith(QLatin1String(".svgz"))) {
-            // QQuickImageProvider and SVG can generate a high resolution image when
-            // sourceSize is set. If sourceSize is not set then the provider default size
-            // will be used, as usual.
-            if (!d->sourcesize.isEmpty())
+
+        // QQuickImageProvider and SVG can generate a high resolution image when
+        // sourceSize is set. If sourceSize is not set then the provider default size
+        // will be used, as usual.
+        bool setDevicePixelRatio = false;
+        if (!d->sourcesize.isValid()) {
+            if (loadUrl.scheme() == QStringLiteral("image")) {
+                setDevicePixelRatio = true;
+            } else {
+                QString stringUrl = loadUrl.toString();
+                if (stringUrl.endsWith(QLatin1String("svg")) ||
+                    stringUrl.endsWith(QLatin1String("svgz"))) {
+                    setDevicePixelRatio = true;
+                }
+            }
+
+            if (setDevicePixelRatio)
                 d->devicePixelRatio = targetDevicePixelRatio;
-        } else {
+        }
+
+        if (!setDevicePixelRatio) {
             // (possible) local file: loadUrl and d->devicePixelRatio will be modified if
             // an "@2x" file is found.
             resolve2xLocalFile(d->url, targetDevicePixelRatio, &loadUrl, &d->devicePixelRatio);

commit f8c53e88ee45b7b545dfa2d98419a3f6066c7e5b

Fix high-DPI image and svg providers
Commit 834f9c76 accidentally inverted a validity
test by going from !isEmpty() to !isValid().

Change to isValid() which preserves the previous logic:
a set sourceSize is automatically scaled by the target
devicePixelRatio.

Verified with:
	tests/manual/highdpi/imageprovider.qml
	tests/manual/highdpi/svg.qml

Change-Id: Ie979d566418c365d4d7fc5e5c3e6a795a891db51
Reviewed-by: Robin Burchell <robin.burchell@viroteck.net>

src/quick/items/qquickimagebase.cpp
@@ -219,7 +219,7 @@ void QQuickImageBase::load()
         // sourceSize is set. If sourceSize is not set then the provider default size
         // will be used, as usual.
         bool setDevicePixelRatio = false;
-        if (!d->sourcesize.isValid()) {
+        if (d->sourcesize.isValid()) {
             if (loadUrl.scheme() == QStringLiteral("image")) {
                 setDevicePixelRatio = true;
             } else {