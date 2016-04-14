32a957f31aa00b1e13ed78e5ad19de8599c7c09a
J-P Nurmi <jpnurmi@theqtcompany.com>
2016-04-14 07:57:25 +0200

    Templates: update license headers
    This file is part of the Qt Quick Templates 2 module of the Qt Toolkit.
    
    Change-Id: I39ef9cbb00f55a32b7a43f11ffbdfbb40b84e124
    Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

--- src/quicktemplates2/qquickradiodelegate.cpp
+++ src/quicktemplates2/qquickradiodelegate.cpp
@@ -3,7 +3,7 @@
 ** Copyright (C) 2016 The Qt Company Ltd.
 ** Contact: http://www.qt.io/licensing/
 **
-** This file is part of the Qt Labs Templates module of the Qt Toolkit.
+** This file is part of the Qt Quick Templates 2 module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL3$
 ** Commercial License Usage
