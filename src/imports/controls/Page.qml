e6790e5eb03618f445c9d86069fa9215ffd0f0a7
J-P Nurmi <jpnurmi@theqtcompany.com>
2016-04-14 08:07:48 +0200

    Controls: update license headers
    This file is part of the Qt Quick Controls 2 module of the Qt Toolkit.
    
    Change-Id: Ib653135662bfd353a73290539995e8e5be211587
    Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

--- src/imports/controls/Page.qml
+++ src/imports/controls/Page.qml
@@ -1,9 +1,9 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
+** Copyright (C) 2016 The Qt Company Ltd.
 ** Contact: http://www.qt.io/licensing/
 **
-** This file is part of the Qt Labs Controls module of the Qt Toolkit.
+** This file is part of the Qt Quick Controls 2 module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL3$
 ** Commercial License Usage
--- src/quickcontrols2/qquickstyleselector.cpp
+++ src/quickcontrols2/qquickstyleselector.cpp
@@ -1,10 +1,10 @@
 /***************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd.
+** Copyright (C) 2016 The Qt Company Ltd.
 ** Copyright (C) 2013 BlackBerry Limited. All rights reserved.
 ** Contact: http://www.qt.io/licensing/
 **
-** This file is part of the Qt Labs Controls module of the Qt Toolkit.
+** This file is part of the Qt Quick Controls 2 module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL21$
 ** Commercial License Usage
