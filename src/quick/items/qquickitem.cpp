3b0cb49ef0cff2e7ae4ba295d035b87f09fdea4f
J-P Nurmi <jpnurmi@qt.io>
2016-04-29 10:56:03 +0200

    Fix EnterKey::type docs
    It's an attached property, not an ordinary property.
    
    Change-Id: I08123670953288b21daca8bd56129c1e14e36365
    Reviewed-by: Mitch Curtis <mitch.curtis@theqtcompany.com>

--- src/quick/items/qquickitem.cpp
+++ src/quick/items/qquickitem.cpp
@@ -1624,7 +1624,7 @@ void QQuickItemPrivate::setLayoutMirror(bool mirror)
 */
 
 /*!
-    \qmlproperty enumeration QtQuick::EnterKey::type
+    \qmlattachedproperty enumeration QtQuick::EnterKey::type
 
     Holds the type of the Enter key.
 
