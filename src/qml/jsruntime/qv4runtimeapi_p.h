7c287efccc1fc2432de206c339af60a3fee9fe04
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-05-04 16:23:06 +0200

    qv4runtimeapi_p.h: Add "We mean it" comment.
    Change-Id: Icd0060aef1ebc595a5988ef827096567c14cfc3c
    Reviewed-by: Simon Hausmann <simon.hausmann@theqtcompany.com>

--- src/qml/jsruntime/qv4runtimeapi_p.h
+++ src/qml/jsruntime/qv4runtimeapi_p.h
@@ -39,6 +39,17 @@
 #ifndef QV4RUNTIMEAPI_P_H
 #define QV4RUNTIMEAPI_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <private/qv4global_p.h>
 
 QT_BEGIN_NAMESPACE
