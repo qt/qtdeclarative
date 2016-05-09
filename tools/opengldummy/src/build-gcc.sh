01549cff2ed180a6854e776453fcba6a4285b6fd
Laszlo Agocs <laszlo.agocs@theqtcompany.com>
2016-05-09 15:51:59 +0200

    Remove opengldummy
    Change-Id: I3538a6a4dfb05b3d6cd35e864619ec7a00a3a3c2
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- tools/opengldummy/src/build-gcc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-${CC} -DQGS_BUILD_CLIENT_DLL -fPIC -shared --sysroot ${SYSROOT} -I../3rdparty/include -o libEGL.so egl.cpp
-${CC} -DQGS_BUILD_CLIENT_DLL -fPIC -shared --sysroot ${SYSROOT} -I../3rdparty/include -o libGLESv2.so gles2.cpp
