qt/qtdeclarative

commit 4b4cf31c7a4bcb89cabca09102c4e0a22ab0c6b1

QQuickWidget: update() when resetting updatePending in showEvent()
If updatePending is set, that means we want to call update()
eventually. If we just reset updatePending without calling
update(), we produce UI glitches.

Change-Id: Ie7353b2f5da567e196dbee8c113920e0e4702304
Task-number: QTCREATORBUG-16022
Reviewed-by: Robert Loehning <robert.loehning@qt.io>
Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>

src/quickwidgets/qquickwidget.cpp
@@ -1099,12 +1099,16 @@ void QQuickWidget::mouseDoubleClickEvent(QMouseEvent *e)
 void QQuickWidget::showEvent(QShowEvent *)
 {
     Q_D(QQuickWidget);
-    d->updatePending = false;
     d->createContext();
-    if (d->offscreenWindow->openglContext())
+    if (d->offscreenWindow->openglContext()) {
         d->render(true);
-    else
+        if (d->updatePending) {
+            d->updatePending = false;
+            update();
+        }
+    } else {
         triggerUpdate();
+    }
     QWindowPrivate *offscreenPrivate = QWindowPrivate::get(d->offscreenWindow);
     if (!offscreenPrivate->visible) {
         offscreenPrivate->visible = true;

commit 1b897195a14b63a553b139983736d8dfdd419ffd

Fix missing QQuickWidget updates
Having an animated QQuickFramebufferObject inside a QQuickWidget is currently
broken due to losing update requests.

The problems are introduced in 4b4cf31c7a4bcb89cabca09102c4e0a22ab0c6b1 where
we may erroneously reset the updatePending false even though there may be an
active triggerUpdate ticking via the 5 ms update timer.

Task-number: QTBUG-54239
Change-Id: If84af8ec9c992761cfef9049de642a943f91cfe6
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/quickwidgets/qquickwidget.cpp
@@ -1103,7 +1103,14 @@ void QQuickWidget::showEvent(QShowEvent *)
     d->createContext();
     if (d->offscreenWindow->openglContext()) {
         d->render(true);
-        if (d->updatePending) {
+        // render() may have led to a QQuickWindow::update() call (for
+        // example, having a scene with a QQuickFramebufferObject::Renderer
+        // calling update() in its render()) which in turn results in
+        // renderRequested in the rendercontrol, ending up in
+        // triggerUpdate. In this case just calling update() is not
+        // acceptable, we need the full renderSceneGraph issued from
+        // timerEvent().
+        if (!d->eventPending && d->updatePending) {
             d->updatePending = false;
             update();
         }