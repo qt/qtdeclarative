qt/qtdeclarative

commit 541eb6b704b53a6e889ea206d8e058ec3017100c

qmlplugindump: Allow dumping of WebEngine plugins
WebEngineView requires a shared opengl context. The plugin checks for
this when initializing.

Change-Id: I686b9c22be95f7ef16ea68ea49b8f9aee47789ae
Reviewed-by: Marco Benelli <marco.benelli@qt.io>

tools/qmlplugindump/main.cpp
@@ -972,6 +972,7 @@ int main(int argc, char *argv[])
     if (!requireWindowManager)
         qputenv("QT_QPA_PLATFORM", QByteArrayLiteral("minimal"));
 
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
     QGuiApplication app(argc, argv);
     const QStringList args = app.arguments();
     const QString appName = QFileInfo(app.applicationFilePath()).baseName();

commit ac44a5b574289e8a24c7ad76760fdbb0a0728478

qmlplugindump: Avoid warnings about missing createPlatformOpenGLContext
Fixes a regression introduced in 541eb6b704b53a6e88. There's no point in
setting AA_ShareOpenGLContexts for the minimal plugin.

Change-Id: Ibcc65ede41acf5a8cf22eacef4b94c20048a354a
Reviewed-by: Marco Benelli <marco.benelli@qt.io>

tools/qmlplugindump/main.cpp
@@ -979,8 +979,9 @@ int main(int argc, char *argv[])
 
     if (!requireWindowManager)
         qputenv("QT_QPA_PLATFORM", QByteArrayLiteral("minimal"));
+    else
+        QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
 
-    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
     QGuiApplication app(argc, argv);
     const QStringList args = app.arguments();
     const QString appName = QFileInfo(app.applicationFilePath()).baseName();