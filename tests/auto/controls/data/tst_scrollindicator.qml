qt/qtdeclarative

commit aa823b2961f90a3fa459a220b3eab51c227b110a

Fix build after QQuickItemChangeListener changes
qtdeclarative commit e2c296c changed the itemGeometryChanged()
signature.

Change-Id: Ibe163e1d6fe8535a4ec01de047d72a0d4863979d
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quicktemplates2/qquickscrollbar.cpp
@@ -405,7 +405,7 @@ public:
     void layoutHorizontal(bool move = true);
     void layoutVertical(bool move = true);
 
-    void itemGeometryChanged(QQuickItem *item, const QRectF &newGeometry, const QRectF &oldGeometry) override;
+    void itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change, const QRectF &diff) override;
     void itemDestroyed(QQuickItem *item) override;
 
     QQuickFlickable *flickable;
@@ -476,16 +476,16 @@ void QQuickScrollBarAttachedPrivate::layoutVertical(bool move)
         vertical->setX(vertical->isMirrored() ? 0 : flickable->width() - vertical->width());
 }
 
-void QQuickScrollBarAttachedPrivate::itemGeometryChanged(QQuickItem *item, const QRectF &newGeometry, const QRectF &oldGeometry)
+void QQuickScrollBarAttachedPrivate::itemGeometryChanged(QQuickItem *item, const QQuickGeometryChange change, const QRectF &diff)
 {
     Q_UNUSED(item);
-    Q_UNUSED(newGeometry);
+    Q_UNUSED(change);
     if (horizontal && horizontal->height() > 0) {
-        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), oldGeometry.height() - horizontal->height());
+        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), item->height() - diff.height());
         layoutHorizontal(move);
     }
     if (vertical && vertical->width() > 0) {
-        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), oldGeometry.width() - vertical->width());
+        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), item->width() - diff.width());
         layoutVertical(move);
     }
 }
@@ -504,7 +504,7 @@ QQuickScrollBarAttached::QQuickScrollBarAttached(QQuickFlickable *flickable) :
     Q_D(QQuickScrollBarAttached);
     if (flickable) {
         QQuickItemPrivate *p = QQuickItemPrivate::get(flickable);
-        p->updateOrAddGeometryChangeListener(d, QQuickItemPrivate::SizeChange);
+        p->updateOrAddGeometryChangeListener(d, QQuickGeometryChange::Size);
     }
 }
 

src/quicktemplates2/qquickscrollindicator.cpp
@@ -242,7 +242,7 @@ public:
     void layoutHorizontal(bool move = true);
     void layoutVertical(bool move = true);
 
-    void itemGeometryChanged(QQuickItem *item, const QRectF &newGeometry, const QRectF &oldGeometry) override;
+    void itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change, const QRectF &diff) override;
     void itemDestroyed(QQuickItem *item) override;
 
     QQuickFlickable *flickable;
@@ -276,16 +276,16 @@ void QQuickScrollIndicatorAttachedPrivate::layoutVertical(bool move)
         vertical->setX(flickable->width() - vertical->width());
 }
 
-void QQuickScrollIndicatorAttachedPrivate::itemGeometryChanged(QQuickItem *item, const QRectF &newGeometry, const QRectF &oldGeometry)
+void QQuickScrollIndicatorAttachedPrivate::itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change, const QRectF &diff)
 {
     Q_UNUSED(item);
-    Q_UNUSED(newGeometry);
+    Q_UNUSED(change);
     if (horizontal && horizontal->height() > 0) {
-        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), oldGeometry.height() - horizontal->height());
+        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), item->height() - diff.height());
         layoutHorizontal(move);
     }
     if (vertical && vertical->width() > 0) {
-        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), oldGeometry.width() - vertical->width());
+        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), item->width() - diff.width());
         layoutVertical(move);
     }
 }
@@ -304,7 +304,7 @@ QQuickScrollIndicatorAttached::QQuickScrollIndicatorAttached(QQuickFlickable *fl
     Q_D(QQuickScrollIndicatorAttached);
     if (flickable) {
         QQuickItemPrivate *p = QQuickItemPrivate::get(flickable);
-        p->updateOrAddGeometryChangeListener(d, QQuickItemPrivate::SizeChange);
+        p->updateOrAddGeometryChangeListener(d, QQuickGeometryChange::Size);
     }
 }
 

src/quicktemplates2/qquickapplicationwindow.cpp
@@ -119,7 +119,7 @@ public:
 
     void relayout();
 
-    void itemGeometryChanged(QQuickItem *item, const QRectF &newRect, const QRectF &oldRect) override;
+    void itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change, const QRectF &diff) override;
     void itemVisibilityChanged(QQuickItem *item) override;
     void itemImplicitWidthChanged(QQuickItem *item) override;
     void itemImplicitHeightChanged(QQuickItem *item) override;
@@ -195,11 +195,11 @@ void QQuickApplicationWindowPrivate::relayout()
     }
 }
 
-void QQuickApplicationWindowPrivate::itemGeometryChanged(QQuickItem *item, const QRectF &newRect, const QRectF &oldRect)
+void QQuickApplicationWindowPrivate::itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change, const QRectF &diff)
 {
     Q_UNUSED(item)
-    Q_UNUSED(newRect)
-    Q_UNUSED(oldRect)
+    Q_UNUSED(change)
+    Q_UNUSED(diff)
     relayout();
 }
 

src/quicktemplates2/qquickmenu.cpp
@@ -201,7 +201,7 @@ void QQuickMenuPrivate::itemDestroyed(QQuickItem *item)
         removeItem(index, item);
 }
 
-void QQuickMenuPrivate::itemGeometryChanged(QQuickItem *, const QRectF &, const QRectF &)
+void QQuickMenuPrivate::itemGeometryChanged(QQuickItem *, QQuickGeometryChange, const QRectF &)
 {
     if (complete)
         resizeItems();

src/quicktemplates2/qquickmenu_p_p.h
@@ -77,7 +77,7 @@ public:
     void itemSiblingOrderChanged(QQuickItem *item) override;
     void itemParentChanged(QQuickItem *item, QQuickItem *parent) override;
     void itemDestroyed(QQuickItem *item) override;
-    void itemGeometryChanged(QQuickItem *, const QRectF &newGeometry, const QRectF &oldGeometry) override;
+    void itemGeometryChanged(QQuickItem *, QQuickGeometryChange change, const QRectF &diff) override;
 
     void onItemPressed();
     void onItemActiveFocusChanged();

src/quicktemplates2/qquickpagelayout.cpp
@@ -172,7 +172,7 @@ void QQuickPageLayout::itemImplicitHeightChanged(QQuickItem *)
     update();
 }
 
-void QQuickPageLayout::itemGeometryChanged(QQuickItem *, const QRectF &, const QRectF &)
+void QQuickPageLayout::itemGeometryChanged(QQuickItem *, QQuickGeometryChange, const QRectF &)
 {
     update();
 }

src/quicktemplates2/qquickpagelayout_p_p.h
@@ -72,7 +72,7 @@ protected:
     void itemVisibilityChanged(QQuickItem *item) override;
     void itemImplicitWidthChanged(QQuickItem *item) override;
     void itemImplicitHeightChanged(QQuickItem *item) override;
-    void itemGeometryChanged(QQuickItem *item, const QRectF &newRect, const QRectF &oldRect) override;
+    void itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change, const QRectF &diff) override;
     void itemDestroyed(QQuickItem *item) override;
 
 private:

src/quicktemplates2/qquickpopup.cpp
@@ -529,7 +529,7 @@ void QQuickPopupPositioner::setParentItem(QQuickItem *parent)
         m_popup->reposition();
 }
 
-void QQuickPopupPositioner::itemGeometryChanged(QQuickItem *, const QRectF &, const QRectF &)
+void QQuickPopupPositioner::itemGeometryChanged(QQuickItem *, QQuickGeometryChange, const QRectF &)
 {
     if (m_parentItem && m_popup->popupItem->isVisible())
         m_popup->reposition();

commit 2e19d8eafa9aeb75a09f2f54f8da272acd9700be

Fix missing scroll bars and indicators
The layouting code broke in commit aa823b2 that fixed the build
after item change listener changes in qtdeclarative.

Change-Id: Ife3c598c6aca03f0aebe1fdf32f5ca412de62f0d
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quicktemplates2/qquickscrollbar.cpp
@@ -481,11 +481,11 @@ void QQuickScrollBarAttachedPrivate::itemGeometryChanged(QQuickItem *item, const
     Q_UNUSED(item);
     Q_UNUSED(change);
     if (horizontal && horizontal->height() > 0) {
-        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), item->height() - diff.height());
+        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), item->height() - diff.height() - horizontal->height());
         layoutHorizontal(move);
     }
     if (vertical && vertical->width() > 0) {
-        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), item->width() - diff.width());
+        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), item->width() - diff.width() - vertical->width());
         layoutVertical(move);
     }
 }

src/quicktemplates2/qquickscrollindicator.cpp
@@ -281,11 +281,11 @@ void QQuickScrollIndicatorAttachedPrivate::itemGeometryChanged(QQuickItem *item,
     Q_UNUSED(item);
     Q_UNUSED(change);
     if (horizontal && horizontal->height() > 0) {
-        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), item->height() - diff.height());
+        bool move = qFuzzyIsNull(horizontal->y()) || qFuzzyCompare(horizontal->y(), item->height() - diff.height() - horizontal->height());
         layoutHorizontal(move);
     }
     if (vertical && vertical->width() > 0) {
-        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), item->width() - diff.width());
+        bool move = qFuzzyIsNull(vertical->x()) || qFuzzyCompare(vertical->x(), item->width() - diff.width() - vertical->width());
         layoutVertical(move);
     }
 }

tests/auto/controls/data/tst_scrollbar.qml
@@ -107,6 +107,13 @@ TestCase {
         compare(vertical.y, 0)
         verify(vertical.width > 0)
         compare(vertical.height, container.height)
+        // vertical scroll bar follows flickable's width
+        container.width += 10
+        compare(vertical.x, container.width - vertical.width)
+        // ...unless explicitly positioned
+        vertical.x = 123
+        container.width += 10
+        compare(vertical.x, 123)
 
         var horizontal = scrollBar.createObject()
         verify(!horizontal.parent)
@@ -128,6 +135,13 @@ TestCase {
         compare(horizontal.y, container.height - horizontal.height)
         compare(horizontal.width, container.width)
         verify(horizontal.height > 0)
+        // horizontal scroll bar follows flickable's height
+        container.height += 10
+        compare(horizontal.y, container.height - horizontal.height)
+        // ...unless explicitly positioned
+        horizontal.y = 123
+        container.height += 10
+        compare(horizontal.y, 123)
 
         var velocity = container.maximumFlickVelocity
 

tests/auto/controls/data/tst_scrollindicator.qml
@@ -92,6 +92,13 @@ TestCase {
         compare(vertical.y, 0)
         verify(vertical.width > 0)
         compare(vertical.height, container.height)
+        // vertical scroll indicator follows flickable's width
+        container.width += 10
+        compare(vertical.x, container.width - vertical.width)
+        // ...unless explicitly positioned
+        vertical.x = 123
+        container.width += 10
+        compare(vertical.x, 123)
 
         var horizontal = scrollIndicator.createObject()
         verify(!horizontal.parent)
@@ -113,6 +120,13 @@ TestCase {
         compare(horizontal.y, container.height - horizontal.height)
         compare(horizontal.width, container.width)
         verify(horizontal.height > 0)
+        // horizontal scroll indicator follows flickable's height
+        container.height += 10
+        compare(horizontal.y, container.height - horizontal.height)
+        // ...unless explicitly positioned
+        horizontal.y = 123
+        container.height += 10
+        compare(horizontal.y, 123)
 
         var velocity = container.maximumFlickVelocity
 