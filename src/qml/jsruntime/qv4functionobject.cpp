5795e059dd40c0feeb3e408d47fa2ff64d56c791
Lars Knoll <lars.knoll@qt.io>
2016-08-09 10:50:45 +0200

    Fix signatures of FunctionObject::construct and call
    Fixup for 702c4247d74ffb7e4fb1aaca96d70f4591203ba2.
    
    Fixes the test failures in section 15.3.4 of the JS
    test suite.
    
    Change-Id: Id44886e0301b560f2975104757467bb0c22aed60
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- src/qml/jsruntime/qv4functionobject.cpp
+++ src/qml/jsruntime/qv4functionobject.cpp
@@ -166,14 +166,14 @@ ReturnedValue FunctionObject::name() const
     return get(scope()->engine->id_name());
 }
 
-ReturnedValue FunctionObject::construct(const Managed *that, CallData *)
+void FunctionObject::construct(const Managed *that, Scope &scope, CallData *)
 {
-    return static_cast<const FunctionObject *>(that)->engine()->throwTypeError();
+    scope.result = static_cast<const FunctionObject *>(that)->engine()->throwTypeError();
 }
 
-ReturnedValue FunctionObject::call(const Managed *, CallData *)
+void FunctionObject::call(const Managed *, Scope &scope, CallData *)
 {
-    return Encode::undefined();
+    scope.result = Encode::undefined();
 }
 
 void FunctionObject::markObjects(Heap::Base *that, ExecutionEngine *e)
