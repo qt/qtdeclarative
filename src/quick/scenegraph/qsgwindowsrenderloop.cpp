c5cfce8553d591418f4752b62ac26f8b37e70515
Gunnar Sletta <gunnar@sletta.org>
2016-08-25 13:40:33 +0200

    Ensure windows renderloop doesn't stop existing, running, animations
    If there is a normal C++ Qt animation running when the animation
    driver is installed, and we don't have a visible QQuickWindow
    at that time, the animation will await the QQuickWindow's vsync
    to be ticked (which might never happen). So don't install
    the driver until after we've registered started/stopped
    (which will be called by install) and when m_vsyncDelta is
    correctly initialized.
    
    Change-Id: If36633ba78d8b16afa658ae5279372158a4f7924
    Task-number: QTBUG-55521
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/quick/scenegraph/qsgwindowsrenderloop.cpp
+++ src/quick/scenegraph/qsgwindowsrenderloop.cpp
@@ -84,17 +84,16 @@ QSGWindowsRenderLoop::QSGWindowsRenderLoop()
 {
     m_rc = static_cast<QSGDefaultRenderContext *>(m_sg->createRenderContext());
 
-    m_animationDriver = m_sg->createAnimationDriver(m_sg);
-    m_animationDriver->install();
-
-    connect(m_animationDriver, SIGNAL(started()), this, SLOT(started()));
-    connect(m_animationDriver, SIGNAL(stopped()), this, SLOT(stopped()));
-
     m_vsyncDelta = 1000 / QGuiApplication::primaryScreen()->refreshRate();
     if (m_vsyncDelta <= 0)
         m_vsyncDelta = 16;
 
     RLDEBUG("Windows Render Loop created");
+
+    m_animationDriver = m_sg->createAnimationDriver(m_sg);
+    connect(m_animationDriver, SIGNAL(started()), this, SLOT(started()));
+    connect(m_animationDriver, SIGNAL(stopped()), this, SLOT(stopped()));
+    m_animationDriver->install();
 
     qsg_render_timer.start();
 }
