d1d1b689ee483004bbf8a65684e785f70319118e
Friedemann Kleint <Friedemann.Kleint@qt.io>
2016-10-21 10:32:29 +0200

    Add "We mean it" comment to qquickdesignercustomparserobject_p.h
    Amends 54765de17916948ef56a3fd1111e8c765c07155a
    
    Change-Id: I30b9e31326d20f176a01ff625fbe8597b43cbc62
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/quick/designer/qquickdesignercustomparserobject_p.h
+++ src/quick/designer/qquickdesignercustomparserobject_p.h
@@ -40,6 +40,17 @@
 #ifndef QUICKDESIGNERCUSTOMPARSEROBJECT_H
 #define QUICKDESIGNERCUSTOMPARSEROBJECT_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API. It exists purely as an
+// implementation detail. This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QObject>
 #include <private/qqmlcustomparser_p.h>
 
