qt/qtdeclarative

commit 9c50216c7bbbdb2bb51d4485286bf09e12fb5b62

Scale images correctly with sourceSize and PreserveAspectCrop/Fit
It also introduces a private QQuickImageProviderWithOptions to allow
passing options to image providers so that they can return more
fine-tuned images.  This private class will disappear in Qt6 and the
functionality will be merged into QQuickImageProvider.

Change-Id: I619065d889d21d3a9e1f8e45fdb6076b9657c7ed
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/quick/util/qquickpixmapcache_p.h
@@ -65,12 +65,7 @@ QT_BEGIN_NAMESPACE
 class QQmlEngine;
 class QQuickPixmapData;
 class QQuickTextureFactory;
-
-enum AutoTransform {
-    UsePluginDefault = -1,
-    ApplyTransform = 0,
-    DoNotApplyTransform = 1
-};
+class QQuickImageProviderOptionsPrivate;
 
 class QQuickDefaultTextureFactory : public QQuickTextureFactory
 {
@@ -85,6 +80,45 @@ public:
 private:
     QImage im;
     QSize size;
+};
+
+class QQuickImageProviderPrivate
+{
+public:
+    QQuickImageProvider::ImageType type;
+    QQuickImageProvider::Flags flags;
+    bool isProviderWithOptions;
+};
+
+// ### Qt 6: Make public moving to qquickimageprovider.h
+class Q_QUICK_EXPORT QQuickImageProviderOptions
+{
+public:
+    enum AutoTransform {
+        UsePluginDefaultTransform = -1,
+        ApplyTransform = 0,
+        DoNotApplyTransform = 1
+    };
+
+    QQuickImageProviderOptions();
+    ~QQuickImageProviderOptions();
+
+    QQuickImageProviderOptions(const QQuickImageProviderOptions&);
+    QQuickImageProviderOptions& operator=(const QQuickImageProviderOptions&);
+
+    bool operator==(const QQuickImageProviderOptions&) const;
+
+    AutoTransform autoTransform() const;
+    void setAutoTransform(AutoTransform autoTransform);
+
+    bool preserveAspectRatioCrop() const;
+    void setPreserveAspectRatioCrop(bool preserveAspectRatioCrop);
+
+    bool preserveAspectRatioFit() const;
+    void setPreserveAspectRatioFit(bool preserveAspectRatioFit);
+
+private:
+    QSharedDataPointer<QQuickImageProviderOptionsPrivate> d;
 };
 
 class Q_QUICK_PRIVATE_EXPORT QQuickPixmap
@@ -115,7 +149,7 @@ public:
     const QUrl &url() const;
     const QSize &implicitSize() const;
     const QSize &requestSize() const;
-    AutoTransform autoTransform() const;
+    QQuickImageProviderOptions::AutoTransform autoTransform() const;
     QImage image() const;
     void setImage(const QImage &);
     void setPixmap(const QQuickPixmap &other);
@@ -130,7 +164,7 @@ public:
     void load(QQmlEngine *, const QUrl &, QQuickPixmap::Options options);
     void load(QQmlEngine *, const QUrl &, const QSize &);
     void load(QQmlEngine *, const QUrl &, const QSize &, QQuickPixmap::Options options);
-    void load(QQmlEngine *, const QUrl &, const QSize &, QQuickPixmap::Options options, AutoTransform autoTransform);
+    void load(QQmlEngine *, const QUrl &, const QSize &, QQuickPixmap::Options options, const QQuickImageProviderOptions &providerOptions);
 
     void clear();
     void clear(QObject *);
@@ -151,6 +185,24 @@ private:
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(QQuickPixmap::Options)
+
+// This class will disappear with Qt6 and will just be the regular QQuickImageProvider
+// ### Qt 6: Remove this class and fold it with QQuickImageProvider
+class Q_QUICK_EXPORT QQuickImageProviderWithOptions : public QQuickAsyncImageProvider
+{
+public:
+    QQuickImageProviderWithOptions(ImageType type, Flags flags = Flags());
+
+    QImage requestImage(const QString &id, QSize *size, const QSize& requestedSize) override;
+    QPixmap requestPixmap(const QString &id, QSize *size, const QSize& requestedSize) override;
+    QQuickTextureFactory *requestTexture(const QString &id, QSize *size, const QSize &requestedSize) override;
+    QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize) override;
+
+    virtual QImage requestImage(const QString &id, QSize *size, const QSize& requestedSize, const QQuickImageProviderOptions &options);
+    virtual QPixmap requestPixmap(const QString &id, QSize *size, const QSize& requestedSize, const QQuickImageProviderOptions &options);
+    virtual QQuickTextureFactory *requestTexture(const QString &id, QSize *size, const QSize &requestedSize, const QQuickImageProviderOptions &options);
+    virtual QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize, const QQuickImageProviderOptions &options);
+};
 
 QT_END_NAMESPACE
 

src/quick/items/qquickimage.cpp
@@ -304,6 +304,15 @@ void QQuickImage::setFillMode(FillMode mode)
     if (d->fillMode == mode)
         return;
     d->fillMode = mode;
+    if ((mode == PreserveAspectCrop) != d->providerOptions.preserveAspectRatioCrop()) {
+        d->providerOptions.setPreserveAspectRatioCrop(mode == PreserveAspectCrop);
+        if (isComponentComplete())
+            load();
+    } else if ((mode == PreserveAspectFit) != d->providerOptions.preserveAspectRatioFit()) {
+        d->providerOptions.setPreserveAspectRatioFit(mode == PreserveAspectFit);
+        if (isComponentComplete())
+            load();
+    }
     update();
     updatePaintedGeometry();
     emit fillModeChanged();
@@ -423,7 +432,9 @@ qreal QQuickImage::paintedHeight() const
     (The \l fillMode is independent of this.)
 
     If both the sourceSize.width and sourceSize.height are set the image will be scaled
-    down to fit within the specified size, maintaining the image's aspect ratio.  The actual
+    down to fit within the specified size (unless PreserveAspectCrop or PreserveAspectFit
+    are used, then it will be scaled to match the optimal size for cropping/fitting),
+    maintaining the image's aspect ratio.  The actual
     size of the image after scaling is available via \l Item::implicitWidth and \l Item::implicitHeight.
 
     If the source is an intrinsically scalable image (eg. SVG), this property

src/quick/items/qquickimagebase.cpp
@@ -246,7 +246,7 @@ void QQuickImageBase::load()
             resolve2xLocalFile(d->url, targetDevicePixelRatio, &loadUrl, &d->devicePixelRatio);
         }
 
-        d->pix.load(qmlEngine(this), loadUrl, d->sourcesize * d->devicePixelRatio, options, d->autoTransform);
+        d->pix.load(qmlEngine(this), loadUrl, d->sourcesize * d->devicePixelRatio, options, d->providerOptions);
 
         if (d->pix.isLoading()) {
             if (d->progress != 0.0) {
@@ -381,17 +381,18 @@ void QQuickImageBase::resolve2xLocalFile(const QUrl &url, qreal targetDevicePixe
 bool QQuickImageBase::autoTransform() const
 {
     Q_D(const QQuickImageBase);
-    if (d->autoTransform == UsePluginDefault)
-        return d->pix.autoTransform() == ApplyTransform;
-    return d->autoTransform == ApplyTransform;
+    if (d->providerOptions.autoTransform() == QQuickImageProviderOptions::UsePluginDefaultTransform)
+        return d->pix.autoTransform() == QQuickImageProviderOptions::ApplyTransform;
+    return d->providerOptions.autoTransform() == QQuickImageProviderOptions::ApplyTransform;
 }
 
 void QQuickImageBase::setAutoTransform(bool transform)
 {
     Q_D(QQuickImageBase);
-    if (d->autoTransform != UsePluginDefault && transform == (d->autoTransform == ApplyTransform))
+    if (d->providerOptions.autoTransform() != QQuickImageProviderOptions::UsePluginDefaultTransform &&
+        transform == (d->providerOptions.autoTransform() == QQuickImageProviderOptions::ApplyTransform))
         return;
-    d->autoTransform = transform ? ApplyTransform : DoNotApplyTransform;
+    d->providerOptions.setAutoTransform(transform ? QQuickImageProviderOptions::ApplyTransform : QQuickImageProviderOptions::DoNotApplyTransform);
     emitAutoTransformBaseChanged();
 }
 

src/quick/items/qquickimagebase_p_p.h
@@ -68,7 +68,6 @@ public:
       : status(QQuickImageBase::Null),
         progress(0.0),
         devicePixelRatio(1.0),
-        autoTransform(UsePluginDefault),
         async(false),
         cache(true),
         mirror(false),
@@ -83,7 +82,7 @@ public:
     QSize sourcesize;
     QSize oldSourceSize;
     qreal devicePixelRatio;
-    AutoTransform autoTransform;
+    QQuickImageProviderOptions providerOptions;
     bool async : 1;
     bool cache : 1;
     bool mirror: 1;

src/quick/util/qquickimageprovider.cpp
@@ -44,13 +44,6 @@
 
 QT_BEGIN_NAMESPACE
 
-class QQuickImageProviderPrivate
-{
-public:
-    QQuickImageProvider::ImageType type;
-    QQuickImageProvider::Flags flags;
-};
-
 /*!
     \class QQuickTextureFactory
     \since 5.0
@@ -349,6 +342,7 @@ QQuickImageProvider::QQuickImageProvider(ImageType type, Flags flags)
 {
     d->type = type;
     d->flags = flags;
+    d->isProviderWithOptions = false;
 }
 
 /*!
@@ -502,26 +496,165 @@ QQuickAsyncImageProvider::~QQuickAsyncImageProvider()
     implementation of this method is reentrant.
 */
 
-/*!
-   \fn QImage QQuickImageProvider::requestImage(const QString &id, QSize *size, const QSize& requestedSize, bool requestedAutoTransform);
 
-   \internal
-   For future reference.
+class QQuickImageProviderOptionsPrivate : public QSharedData
+{
+public:
+    QQuickImageProviderOptionsPrivate()
+     : autoTransform(QQuickImageProviderOptions::UsePluginDefaultTransform)
+     , preserveAspectRatioCrop(false)
+     , preserveAspectRatioFit(false)
+    {
+    }
+
+    QQuickImageProviderOptions::AutoTransform autoTransform;
+    bool preserveAspectRatioCrop;
+    bool preserveAspectRatioFit;
+};
+
+/*!
+    \class QQuickImageProviderOptions
+    \since 5.9
+    \brief The QQuickImageProviderOptions class provides options for QQuickImageProviderWithOptions image requests.
+    \inmodule QtQuick
+
+    \sa QQuickImageProviderWithOptions
 */
 
 /*!
-   \fn QPixmap QQuickImageProvider::requestPixmap(const QString &id, QSize *size, const QSize& requestedSize, bool requestedAutoTransform);
+    \enum QQuickImageProviderOptions::AutoTransform
 
-   \internal
-   For future reference.
+    Whether the image provider should apply transformation metadata on read().
+
+    \value UsePluginDefaultTransform Image provider should do its default behavior on whether applying transformation metadata on read or not
+    \value ApplyTransform Image provider should apply transformation metadata on read
+    \value DoNotApplyTransform Image provider should not apply transformation metadata on read
 */
+
+QQuickImageProviderOptions::QQuickImageProviderOptions()
+ : d(new QQuickImageProviderOptionsPrivate())
+{
+}
+
+QQuickImageProviderOptions::~QQuickImageProviderOptions()
+{
+}
+
+QQuickImageProviderOptions::QQuickImageProviderOptions(const QQuickImageProviderOptions &other)
+ : d(other.d)
+{
+}
+
+QQuickImageProviderOptions& QQuickImageProviderOptions::operator=(const QQuickImageProviderOptions &other)
+{
+    d = other.d;
+    return *this;
+}
+
+bool QQuickImageProviderOptions::operator==(const QQuickImageProviderOptions &other) const
+{
+    return d->autoTransform == other.d->autoTransform &&
+           d->preserveAspectRatioCrop == other.d->preserveAspectRatioCrop &&
+           d->preserveAspectRatioFit == other.d->preserveAspectRatioFit;
+}
 
 /*!
-   \fn QQuickTextureFactory *QQuickImageProvider::requestTexture(const QString &id, QSize *size, const QSize &requestedSize, bool requestedAutoTransform);
-
-   \internal
-   For future reference.
+    Returns whether the image provider should apply transformation metadata on read().
 */
+QQuickImageProviderOptions::AutoTransform QQuickImageProviderOptions::autoTransform() const
+{
+    return d->autoTransform;
+}
+
+void QQuickImageProviderOptions::setAutoTransform(QQuickImageProviderOptions::AutoTransform autoTransform)
+{
+    d->autoTransform = autoTransform;
+}
+
+/*!
+    Returns whether the image request is for a PreserveAspectCrop Image.
+    This allows the provider to better optimize the size of the returned image.
+*/
+bool QQuickImageProviderOptions::preserveAspectRatioCrop() const
+{
+    return d->preserveAspectRatioCrop;
+}
+
+void QQuickImageProviderOptions::setPreserveAspectRatioCrop(bool preserveAspectRatioCrop)
+{
+    d->preserveAspectRatioCrop = preserveAspectRatioCrop;
+}
+
+/*!
+    Returns whether the image request is for a PreserveAspectFit Image.
+    This allows the provider to better optimize the size of the returned image.
+*/
+bool QQuickImageProviderOptions::preserveAspectRatioFit() const
+{
+    return d->preserveAspectRatioFit;
+}
+
+void QQuickImageProviderOptions::setPreserveAspectRatioFit(bool preserveAspectRatioFit)
+{
+    d->preserveAspectRatioFit = preserveAspectRatioFit;
+}
+
+
+QQuickImageProviderWithOptions::QQuickImageProviderWithOptions(ImageType type, Flags flags)
+ : QQuickAsyncImageProvider()
+{
+    QQuickImageProvider::d->type = type;
+    QQuickImageProvider::d->flags = flags;
+    QQuickImageProvider::d->isProviderWithOptions = true;
+}
+
+QImage QQuickImageProviderWithOptions::requestImage(const QString &id, QSize *size, const QSize& requestedSize)
+{
+    return requestImage(id, size, requestedSize, QQuickImageProviderOptions());
+}
+
+QPixmap QQuickImageProviderWithOptions::requestPixmap(const QString &id, QSize *size, const QSize& requestedSize)
+{
+    return requestPixmap(id, size, requestedSize, QQuickImageProviderOptions());
+}
+
+QQuickTextureFactory *QQuickImageProviderWithOptions::requestTexture(const QString &id, QSize *size, const QSize &requestedSize)
+{
+    return requestTexture(id, size, requestedSize, QQuickImageProviderOptions());
+}
+
+QImage QQuickImageProviderWithOptions::requestImage(const QString &id, QSize *size, const QSize& requestedSize, const QQuickImageProviderOptions &options)
+{
+    Q_UNUSED(options);
+    return QQuickAsyncImageProvider::requestImage(id, size, requestedSize);
+}
+
+QPixmap QQuickImageProviderWithOptions::requestPixmap(const QString &id, QSize *size, const QSize& requestedSize, const QQuickImageProviderOptions &options)
+{
+    Q_UNUSED(options);
+    return QQuickAsyncImageProvider::requestPixmap(id, size, requestedSize);
+}
+
+QQuickTextureFactory *QQuickImageProviderWithOptions::requestTexture(const QString &id, QSize *size, const QSize &requestedSize, const QQuickImageProviderOptions &options)
+{
+    Q_UNUSED(options);
+    return QQuickAsyncImageProvider::requestTexture(id, size, requestedSize);
+}
+
+QQuickImageResponse *QQuickImageProviderWithOptions::requestImageResponse(const QString &id, const QSize &requestedSize)
+{
+    Q_UNUSED(id);
+    Q_UNUSED(requestedSize);
+    if (imageType() == ImageResponse)
+        qWarning("ImageProvider is of ImageResponse type but has not implemented requestImageResponse()");
+    return nullptr;
+}
+
+QQuickImageResponse *QQuickImageProviderWithOptions::requestImageResponse(const QString &id, const QSize &requestedSize, const QQuickImageProviderOptions &options)
+{
+    Q_UNUSED(options);
+    return requestImageResponse(id, requestedSize);
+}
 
 QT_END_NAMESPACE
 

src/quick/util/qquickimageprovider.h
@@ -50,6 +50,7 @@ QT_BEGIN_NAMESPACE
 
 class QQuickImageProviderPrivate;
 class QQuickAsyncImageProviderPrivate;
+class QQuickImageProviderOptionsPrivate;
 class QSGTexture;
 class QQuickWindow;
 
@@ -86,6 +87,8 @@ Q_SIGNALS:
 
 class Q_QUICK_EXPORT QQuickImageProvider : public QQmlImageProviderBase
 {
+    friend class QQuickImageProviderWithOptions; // ### Qt 6 Remove
+    friend class QQuickPixmapReader; // ### Qt 6 Remove
 public:
     QQuickImageProvider(ImageType type, Flags flags = Flags());
     virtual ~QQuickImageProvider();
@@ -94,9 +97,9 @@ public:
     Flags flags() const override;
 
 #if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
-    virtual QImage requestImage(const QString &id, QSize *size, const QSize& requestedSize, bool requestedAutoTransform);
-    virtual QPixmap requestPixmap(const QString &id, QSize *size, const QSize& requestedSize, bool requestedAutoTransform);
-    virtual QQuickTextureFactory *requestTexture(const QString &id, QSize *size, const QSize &requestedSize, bool requestedAutoTransform);
+    virtual QImage requestImage(const QString &id, QSize *size, const QSize& requestedSize, const QQuickImageProviderOptions &options);
+    virtual QPixmap requestPixmap(const QString &id, QSize *size, const QSize& requestedSize, const QQuickImageProviderOptions &options);
+    virtual QQuickTextureFactory *requestTexture(const QString &id, QSize *size, const QSize &requestedSize, const QQuickImageProviderOptions &options);
 #else
     virtual QImage requestImage(const QString &id, QSize *size, const QSize& requestedSize);
     virtual QPixmap requestPixmap(const QString &id, QSize *size, const QSize& requestedSize);
@@ -113,7 +116,11 @@ public:
     QQuickAsyncImageProvider();
     virtual ~QQuickAsyncImageProvider();
 
+#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
+    virtual QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize, const QQuickImageProviderOptions &options) = 0;
+#else
     virtual QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize) = 0;
+#endif
 
 private:
     QQuickAsyncImageProviderPrivate *d;

commit d0649a1b457f321cfafee1aaf443543fd11a7112

Make Item::grabToImage work in more cases, and improve test coverage
This fixes a regression caused by 9c50216c7bbbdb2bb51d4485286bf09e12fb5b62
when the Image specified fillMode: aspectRatioCrop/Fit.

[ChangeLog][QtQuick][Image] Item::grabToImage on an Image element will now work
regardless of the Image's sourceSize or cache properties.

Change-Id: I225854c48f0c35a3cb2ef0dd56bf51bd88c31779
Reviewed-by: Robin Burchell <robin.burchell@crimson.no>

src/quick/items/qquickitemgrabresult.cpp
@@ -75,7 +75,7 @@ public:
 
     void ensureImageInCache() const {
         if (url.isEmpty() && !image.isNull()) {
-            url.setScheme(QStringLiteral("ItemGrabber"));
+            url.setScheme(QQuickPixmap::itemGrabberScheme);
             url.setPath(QVariant::fromValue(item.data()).toString());
             static uint counter = 0;
             url.setFragment(QString::number(++counter));

src/quick/util/qquickpixmapcache.cpp
@@ -84,6 +84,7 @@
 
 QT_BEGIN_NAMESPACE
 
+const QLatin1String QQuickPixmap::itemGrabberScheme = QLatin1String("itemgrabber");
 
 #ifndef QT_NO_DEBUG
 static const bool qsg_leak_check = !qEnvironmentVariableIsEmpty("QML_LEAK_CHECK");
@@ -1462,8 +1463,15 @@ void QQuickPixmap::load(QQmlEngine *engine, const QUrl &url, const QSize &reques
     QHash<QQuickPixmapKey, QQuickPixmapData *>::Iterator iter = store->m_cache.end();
 
     // If Cache is disabled, the pixmap will always be loaded, even if there is an existing
-    // cached version.
-    if (options & QQuickPixmap::Cache)
+    // cached version. Unless it's an itemgrabber url, since the cache is used to pass
+    // the result between QQuickItemGrabResult and QQuickImage.
+    if (url.scheme() == itemGrabberScheme) {
+        QSize dummy;
+        if (requestSize != dummy)
+            qWarning() << "Ignoring sourceSize request for image url that came from grabToImage. Use the targetSize parameter of the grabToImage() function instead.";
+        const QQuickPixmapKey grabberKey = { &url, &dummy, QQuickImageProviderOptions() };
+        iter = store->m_cache.find(grabberKey);
+    } else if (options & QQuickPixmap::Cache)
         iter = store->m_cache.find(key);
 
     if (iter == store->m_cache.end()) {

src/quick/util/qquickpixmapcache_p.h
@@ -177,6 +177,8 @@ public:
     static void purgeCache();
     static bool isCached(const QUrl &url, const QSize &requestSize);
 
+    static const QLatin1String itemGrabberScheme;
+
 private:
     Q_DISABLE_COPY(QQuickPixmap)
     QQuickPixmapData *d;

tests/auto/qmltest/itemgrabber/tst_itemgrabber.qml
@@ -37,8 +37,9 @@ Item {
     TestCase {
         id: testCase
         name: "item-grabber"
-        when: imageOnDisk.ready && imageOnDiskSmall.ready && imageInCache.ready && imageInCacheSmall.ready
-        function test_endresult() {
+        when: imageOnDisk.ready && imageOnDiskSmall.ready
+
+        function test_endresult_disk() {
             var image = grabImage(root);
 
             // imageOnDisk at (0, 0) - (100x100)
@@ -52,6 +53,40 @@ Item {
             compare(imageOnDiskSmall.height, 50);
             verify(image.pixel(100, 0) === Qt.rgba(1, 0, 0, 1));
             verify(image.pixel(149, 49) === Qt.rgba(0, 0, 1, 1));
+        }
+
+        function test_endresult_cache_data() {
+            return [
+                { cache: true, sourceSize: Qt.size(-1, -1), fillMode: Image.Stretch },
+                { cache: true, sourceSize: Qt.size(-1, -1), fillMode: Image.PreserveAspectFit },
+                { cache: true, sourceSize: Qt.size(-1, -1), fillMode: Image.PreserveAspectCrop },
+                { cache: true, sourceSize: Qt.size(10, 10), fillMode: Image.Stretch },
+                { cache: true, sourceSize: Qt.size(10, 10), fillMode: Image.PreserveAspectFit },
+                { cache: true, sourceSize: Qt.size(10, 10), fillMode: Image.PreserveAspectCrop },
+                { cache: false, sourceSize: Qt.size(-1, -1), fillMode: Image.Stretch },
+                { cache: false, sourceSize: Qt.size(-1, -1), fillMode: Image.PreserveAspectFit },
+                { cache: false, sourceSize: Qt.size(-1, -1), fillMode: Image.PreserveAspectCrop },
+                { cache: false, sourceSize: Qt.size(10, 10), fillMode: Image.Stretch },
+                { cache: false, sourceSize: Qt.size(10, 10), fillMode: Image.PreserveAspectFit },
+                { cache: false, sourceSize: Qt.size(10, 10), fillMode: Image.PreserveAspectCrop },
+            ];
+        }
+
+        function test_endresult_cache(data) {
+            imageInCache.cache = data.cache;
+            imageInCache.sourceSize = data.sourceSize;
+            imageInCache.fillMode = data.fillMode;
+            imageInCacheSmall.cache = data.cache;
+            imageInCacheSmall.sourceSize = data.sourceSize;
+            imageInCacheSmall.fillMode = data.fillMode;
+
+            box.grabToImage(imageInCache.handleGrab);
+            box.grabToImage(imageInCacheSmall.handleGrab, Qt.size(50, 50));
+
+            tryCompare(imageInCache, "ready", true);
+            tryCompare(imageInCacheSmall, "ready", true);
+
+            var image = grabImage(root);
 
             // imageInCache at (0, 100) - 100x100
             compare(imageInCache.width, 100);
@@ -72,8 +107,6 @@ Item {
         onWindowShownChanged: {
             box.grabToImage(imageOnDisk.handleGrab);
             box.grabToImage(imageOnDiskSmall.handleGrab, Qt.size(50, 50));
-            box.grabToImage(imageInCache.handleGrab);
-            box.grabToImage(imageInCacheSmall.handleGrab, Qt.size(50, 50));
         }
 
     }