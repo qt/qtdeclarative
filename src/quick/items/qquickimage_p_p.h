7af9656e789f4fa5073798581b858ae867585d79
Mitch Curtis <mitch.curtis@qt.io>
2017-02-03 15:30:55 +0100

    Privately expose QQuickImagePrivate
    The new private type QQuickIconImage in Controls 2 needs to derive from
    QQuickImage.
    
    Task-number: QTBUG-49820
    Change-Id: Idb6f755eca8db52ab9211b0cd332ed3cf425d68c
    Reviewed-by: J-P Nurmi <jpnurmi@qt.io>
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

--- src/quick/items/qquickimage_p_p.h
+++ src/quick/items/qquickimage_p_p.h
@@ -58,7 +58,7 @@ QT_BEGIN_NAMESPACE
 
 class QQuickImageTextureProvider;
 
-class QQuickImagePrivate : public QQuickImageBasePrivate
+class Q_QUICK_PRIVATE_EXPORT QQuickImagePrivate : public QQuickImageBasePrivate
 {
     Q_DECLARE_PUBLIC(QQuickImage)
 
