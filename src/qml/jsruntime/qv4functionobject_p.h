qt/qtdeclarative

commit 180decaf11ea6fb1147825a78a95c455400f7c7e

Optimize code in instanceOf() of FunctionObject::protoForConstructor()
Saves around 1.5% instructions for the Earley Boyer benchmark

Change-Id: I552d324d5e1713f655ab9909f30c9527bb4ff777
Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

src/qml/jsruntime/qv4functionobject_p.h
@@ -79,6 +79,8 @@ struct Q_QML_PRIVATE_EXPORT FunctionObject : Object {
     unsigned int varCount() { return function ? function->compiledFunction->nLocals : 0; }
     bool needsActivation() const { return function ? function->needsActivation() : false; }
 
+    const QV4::Object *protoProperty() const { return propertyData(Index_Prototype)->cast<QV4::Object>(); }
+
     Pointer<ExecutionContext> scope;
     Function *function;
 };
@@ -144,8 +146,6 @@ struct Q_QML_EXPORT FunctionObject: Object {
     static void call(const Managed *that, Scope &scope, CallData *d);
 
     static Heap::FunctionObject *createScriptFunction(ExecutionContext *scope, Function *function);
-
-    ReturnedValue protoProperty() const { return propertyData(Heap::FunctionObject::Index_Prototype)->asReturnedValue(); }
 
     bool needsActivation() const { return d()->needsActivation(); }
     bool strictMode() const { return d()->function ? d()->function->isStrict() : false; }

src/qml/jsruntime/qv4functionobject.cpp
@@ -451,11 +451,10 @@ void Heap::ScriptFunction::init(QV4::ExecutionContext *scope, Function *function
 
 Heap::Object *ScriptFunction::protoForConstructor() const
 {
-    Scope scope(engine());
-    ScopedObject p(scope, protoProperty());
-    if (p)
-        return p->d();
-    return scope.engine->objectPrototype()->d();
+    const Object *o = d()->protoProperty();
+    if (o)
+        return o->d();
+    return engine()->objectPrototype()->d();
 }
 
 

src/qml/jsruntime/qv4runtime.cpp
@@ -345,28 +345,29 @@ ReturnedValue Runtime::method_deleteName(ExecutionEngine *engine, int nameIndex)
 
 QV4::ReturnedValue Runtime::method_instanceof(ExecutionEngine *engine, const Value &left, const Value &right)
 {
-    Scope scope(engine);
-    ScopedFunctionObject f(scope, right.as<FunctionObject>());
-    if (!f)
+    const FunctionObject *function = right.as<FunctionObject>();
+    if (!function)
         return engine->throwTypeError();
 
-    if (f->isBoundFunction())
-        f = static_cast<BoundFunction *>(f.getPointer())->target();
+    Heap::FunctionObject *f = function->d();
+    if (function->isBoundFunction())
+        f = function->cast<BoundFunction>()->target();
 
-    ScopedObject v(scope, left.as<Object>());
-    if (!v)
+    const Object *o = left.as<Object>();
+    if (!o)
         return Encode(false);
+    Heap::Object *v = o->d();
 
-    ScopedObject o(scope, f->protoProperty());
+    o = f->protoProperty();
     if (!o)
         return engine->throwTypeError();
 
     while (v) {
-        v = v->prototype();
+        v = v->prototype;
 
         if (!v)
             break;
-        else if (o->d() == v->d())
+        else if (o->d() == v)
             return Encode(true);
     }
 

commit c32468189941c61be6266da3913de5c1c469bd95

Fix prototype property when set to non-objects
Commit 180decaf11ea6fb1147825a78a95c455400f7c7e regressed
ch15/15.3/15.3.5/S15.3.5.3_A2_T6 in the sense that when a non-object was
set for the prototype property, the protoProperty() helper function
would perform an unsafe cast to Object * and return a non-null pointer,
which however instanceof() does not expect.

Change-Id: I134fd41f6b2d3349ebe7d9e91c6b6e5788f599b6
Reviewed-by: Robin Burchell <robin.burchell@crimson.no>

src/qml/jsruntime/qv4functionobject_p.h
@@ -81,7 +81,7 @@ struct Q_QML_PRIVATE_EXPORT FunctionObject : Object {
     unsigned int varCount() { return function ? function->compiledFunction->nLocals : 0; }
     bool needsActivation() const { return function ? function->needsActivation() : false; }
 
-    const QV4::Object *protoProperty() const { return propertyData(Index_Prototype)->cast<QV4::Object>(); }
+    const QV4::Object *protoProperty() const { return propertyData(Index_Prototype)->as<QV4::Object>(); }
 
     Pointer<ExecutionContext> scope;
     Function *function;