e4a959aacc26336cc8f57267d76d3f8484f3f3a0
Erik Verbruggen <erik.verbruggen@qt.io>
2017-06-30 12:15:24 +0200

    Add "we mean it" disclaimer to private header
    Change-Id: I281954d1f421c9aaaa9100a1505db0ba225cf488
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/qml/compiler/qv4bytecodegenerator_p.h
+++ src/qml/compiler/qv4bytecodegenerator_p.h
@@ -40,6 +40,16 @@
 #ifndef QV4BYTECODEGENERATOR_P_H
 #define QV4BYTECODEGENERATOR_P_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
 #include <private/qv4instr_moth_p.h>
 
 QT_BEGIN_NAMESPACE
