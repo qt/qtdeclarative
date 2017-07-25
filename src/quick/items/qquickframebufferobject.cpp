264989606420f940cdab14f3f39362b9c82c3885
Wieland Hagen <wieland.hagen@kdab.com>
2017-07-25 12:46:38 +0200

    Fix QQuickFramebufferObject::Renderer::invalidateFramebufferObject()
    invalidateFramebufferObject() sets the invalidatePending flag, which
    should then trigger the deletion of the old FBO and allocation via
    Renderer::createFramebufferObject(). This does only happen though,
    if the size has changed.
    
    Instead, always create a new FBO if invalidateFramebufferObject() has
    been called, regardless of whether the size changes or not.
    
    Change-Id: I849cb858afac89038343457c6362233c34956d58
    Task-number: QTBUG-54434
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/quick/items/qquickframebufferobject.cpp
+++ src/quick/items/qquickframebufferobject.cpp
@@ -312,14 +312,12 @@ QSGNode *QQuickFramebufferObject::updatePaintNode(QSGNode *node, UpdatePaintNode
     n->devicePixelRatio = window()->effectiveDevicePixelRatio();
     desiredFboSize *= n->devicePixelRatio;
 
-    if (n->fbo && (d->followsItemSize || n->invalidatePending)) {
-        if (n->fbo->size() != desiredFboSize) {
-            delete n->fbo;
-            n->fbo = 0;
-            delete n->msDisplayFbo;
-            n->msDisplayFbo = 0;
-            n->invalidatePending = false;
-        }
+    if (n->fbo && ((d->followsItemSize && n->fbo->size() != desiredFboSize) || n->invalidatePending)) {
+        delete n->fbo;
+        n->fbo = 0;
+        delete n->msDisplayFbo;
+        n->msDisplayFbo = 0;
+        n->invalidatePending = false;
     }
 
     if (!n->fbo) {
