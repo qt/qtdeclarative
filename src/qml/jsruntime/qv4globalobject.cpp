661f9203c411b080fce6abf606ec8fb5c17a6bfe
Lars Knoll <lars.knoll@qt.io>
2017-08-12 17:10:38 +0200

    Get the thisObject from the current stackframe
    Retrieving it from the context might not always give the
    correct answer anymore (and will get removed from there).
    
    Change-Id: I40900626d9382a2b9f9dad8243b84d283f077509
    Reviewed-by: Erik Verbruggen <erik.verbruggen@qt.io>

--- src/qml/jsruntime/qv4globalobject.cpp
+++ src/qml/jsruntime/qv4globalobject.cpp
@@ -374,7 +374,7 @@ ReturnedValue EvalFunction::evalCall(CallData *callData, bool directCall) const
     if (function->isStrict() || (ctx->d()->v4Function->isStrict())) {
         ScopedFunctionObject e(scope, FunctionObject::createScriptFunction(ctx, function));
         ScopedCallData callData(scope, 0);
-        callData->thisObject = ctx->thisObject();
+        callData->thisObject = scope.engine->currentStackFrame->thisObject();
         return e->call(callData);
     }
 
