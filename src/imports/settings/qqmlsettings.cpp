1532a15002082486757a475d0c01b7779da3e276
Mitch Curtis <mitch.curtis@qt.io>
2017-09-07 11:49:16 +0200

    Settings: document how to have several categories
    Change-Id: I0f27a162936015e2aeb9d52079a65b218498b50a
    Reviewed-by: J-P Nurmi <jpnurmi@qt.io>

--- src/imports/settings/qqmlsettings.cpp
+++ src/imports/settings/qqmlsettings.cpp
@@ -188,6 +188,9 @@ QT_BEGIN_NAMESPACE
     only provides a cleaner settings structure, but also prevents possible
     conflicts between setting keys.
 
+    If several categories are required, use several Settings objects, each with
+    their own category:
+
     \qml
     Item {
         id: panel
@@ -197,6 +200,12 @@ QT_BEGIN_NAMESPACE
         Settings {
             category: "OutputPanel"
             property alias visible: panel.visible
+            // ...
+        }
+
+        Settings {
+            category: "General"
+            property alias fontSize: fontSizeSpinBox.value
             // ...
         }
     }
