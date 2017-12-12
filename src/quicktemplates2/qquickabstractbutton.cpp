qt/qtdeclarative

commit 458eb65f730178bc93ba7b18f0e4dd2a13efad2e

Buttons: defer the execution of the delegates
In practice, deferring the execution of the delegates (until
accessed) means that the default delegate item does not get created
at all, when a custom control replaces it at construction time.

    Button {
        background: Rectangle { ... }
    }

Before, such custom Button would never be faster than the original
one, because the default delegate was first created and then thrown
away at construction time. Originally, this was not considered a huge
problem, because the plan was to keep the default delegates so light-
weight that it wouldn't matter. However, then came the fancy styles
with shadows and effects and thus, heavier default delegates. There's
also a growing demand for more features, and the default delegates
are slowly getting heavier...

Now, after this patch, if you replace a heavy default delegate with
a light and simple custom delegate, the result is a much faster
control. For example, replacing Material style Button's background,
which has a shadow effect, with a plain Rectangle gives a ~10x boost,
because the default background with its heavy shadow effect is not
executed at all.

At the same time, deferring the execution of the default delegates
avoids troubles with asynchronous incubation, because we don't need
to destroy an object in the middle of the incubation process.

Task-number: QTBUG-50992
Change-Id: I2274bff99b9ff126d3748278d58d859222910c98
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quicktemplates2/qquickabstractbutton.cpp
@@ -37,6 +37,7 @@
 #include "qquickabstractbutton_p.h"
 #include "qquickabstractbutton_p_p.h"
 #include "qquickbuttongroup_p.h"
+#include "qquickdeferredexecute_p_p.h"
 
 #include <QtGui/qstylehints.h>
 #include <QtGui/qguiapplication.h>
@@ -128,6 +129,12 @@ QQuickAbstractButtonPrivate::QQuickAbstractButtonPrivate()
       indicator(nullptr),
       group(nullptr)
 {
+}
+
+QQuickItem *QQuickAbstractButtonPrivate::getContentItem()
+{
+    executeContentItem();
+    return QQuickControlPrivate::getContentItem();
 }
 
 void QQuickAbstractButtonPrivate::handlePress(const QPointF &point)
@@ -256,6 +263,12 @@ void QQuickAbstractButtonPrivate::toggle(bool value)
     q->setChecked(value);
     if (wasChecked != checked)
         emit q->toggled();
+}
+
+void QQuickAbstractButtonPrivate::executeIndicator()
+{
+    Q_Q(QQuickAbstractButton);
+    quickExecuteDeferred(q, QStringLiteral("indicator"), indicator);
 }
 
 QQuickAbstractButton *QQuickAbstractButtonPrivate::findCheckedButton() const
@@ -545,7 +558,8 @@ void QQuickAbstractButton::setAutoRepeat(bool repeat)
 */
 QQuickItem *QQuickAbstractButton::indicator() const
 {
-    Q_D(const QQuickAbstractButton);
+    QQuickAbstractButtonPrivate *d = const_cast<QQuickAbstractButtonPrivate *>(d_func());
+    d->executeIndicator();
     return d->indicator;
 }
 
@@ -555,14 +569,15 @@ void QQuickAbstractButton::setIndicator(QQuickItem *indicator)
     if (d->indicator == indicator)
         return;
 
-    QQuickControlPrivate::destroyDelegate(d->indicator, this);
+    delete d->indicator;
     d->indicator = indicator;
     if (indicator) {
         if (!indicator->parentItem())
             indicator->setParentItem(this);
         indicator->setAcceptedMouseButtons(Qt::LeftButton);
     }
-    emit indicatorChanged();
+    if (!d->indicator.isExecuting())
+        emit indicatorChanged();
 }
 
 /*!
@@ -574,6 +589,15 @@ void QQuickAbstractButton::toggle()
 {
     Q_D(QQuickAbstractButton);
     setChecked(!d->checked);
+}
+
+void QQuickAbstractButton::componentComplete()
+{
+    Q_D(QQuickAbstractButton);
+    d->executeIndicator();
+    d->executeBackground();
+    d->executeContentItem();
+    QQuickControl::componentComplete();
 }
 
 void QQuickAbstractButton::focusOutEvent(QFocusEvent *event)

src/quicktemplates2/qquickcontrol.cpp
@@ -48,6 +48,7 @@
 #include "qquickpopup_p.h"
 #include "qquickpopupitem_p_p.h"
 #include "qquickapplicationwindow_p.h"
+#include "qquickdeferredexecute_p_p.h"
 
 #include <QtGui/private/qguiapplication_p.h>
 #include <QtGui/qpa/qplatformtheme.h>
@@ -290,7 +291,7 @@ void QQuickControlPrivate::setContentItem_helper(QQuickItem *item, bool notify)
         return;
 
     q->contentItemChange(item, contentItem);
-    destroyDelegate(contentItem, q);
+    delete contentItem;
     contentItem = item;
 
     if (item) {
@@ -300,7 +301,7 @@ void QQuickControlPrivate::setContentItem_helper(QQuickItem *item, bool notify)
             resizeContent();
     }
 
-    if (notify)
+    if (notify && !contentItem.isExecuting())
         emit q->contentItemChanged();
 }
 
@@ -933,6 +934,18 @@ QLocale QQuickControlPrivate::calcLocale(const QQuickItem *item)
     return QLocale();
 }
 
+void QQuickControlPrivate::executeContentItem()
+{
+    Q_Q(QQuickControl);
+    quickExecuteDeferred(q, QStringLiteral("contentItem"), contentItem);
+}
+
+void QQuickControlPrivate::executeBackground()
+{
+    Q_Q(QQuickControl);
+    quickExecuteDeferred(q, QStringLiteral("background"), background);
+}
+
 /*
     Cancels incubation recursively to avoid "Object destroyed during incubation" (QTBUG-50992)
 */
@@ -1214,7 +1227,8 @@ void QQuickControl::setWheelEnabled(bool enabled)
 */
 QQuickItem *QQuickControl::background() const
 {
-    Q_D(const QQuickControl);
+    QQuickControlPrivate *d = const_cast<QQuickControlPrivate *>(d_func());
+    d->executeBackground();
     return d->background;
 }
 
@@ -1224,7 +1238,7 @@ void QQuickControl::setBackground(QQuickItem *background)
     if (d->background == background)
         return;
 
-    QQuickControlPrivate::destroyDelegate(d->background, this);
+    delete d->background;
     d->background = background;
     if (background) {
         background->setParentItem(this);
@@ -1233,7 +1247,8 @@ void QQuickControl::setBackground(QQuickItem *background)
         if (isComponentComplete())
             d->resizeBackground();
     }
-    emit backgroundChanged();
+    if (!d->background.isExecuting())
+        emit backgroundChanged();
 }
 
 /*!

src/quicktemplates2/qquickcontrol_p_p.h
@@ -49,6 +49,7 @@
 //
 
 #include "qquickcontrol_p.h"
+#include "qquickdeferredpointer_p_p.h"
 
 #include <QtQuick/private/qquickitem_p.h>
 #include <QtQml/private/qlazilyallocated_p.h>
@@ -126,6 +127,9 @@ public:
     static bool calcHoverEnabled(const QQuickItem *item);
 #endif
 
+    void executeContentItem();
+    void executeBackground();
+
     static void destroyDelegate(QObject *object, QObject *parent);
 
     struct ExtraData {
@@ -155,8 +159,8 @@ public:
     QLocale locale;
     Qt::FocusPolicy focusPolicy;
     Qt::FocusReason focusReason;
-    QQuickItem *background;
-    QQuickItem *contentItem;
+    QQuickDeferredPointer<QQuickItem> background;
+    QQuickDeferredPointer<QQuickItem> contentItem;
     QQuickAccessibleAttached *accessibleAttached;
 };
 

src/quicktemplates2/qquickdeferredexecute.cpp
@@ -1,0 +1,110 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the Qt Quick Templates 2 module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "qquickdeferredexecute_p_p.h"
+
+#include <QtQml/qqmlengine.h>
+#include <QtQml/private/qqmldata_p.h>
+#include <QtQml/private/qqmlcomponent_p.h>
+#include <QtQml/private/qqmlobjectcreator_p.h>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtQuickPrivate {
+
+static void beginDeferred(QQmlEnginePrivate *enginePriv, const QQmlProperty &property, QQmlComponentPrivate::DeferredState *deferredState)
+{
+    QObject *object = property.object();
+    QQmlData *ddata = QQmlData::get(object);
+    Q_ASSERT(!ddata->deferredData.isEmpty());
+
+    int propertyIndex = property.index();
+
+    for (auto dit = ddata->deferredData.rbegin(); dit != ddata->deferredData.rend(); ++dit) {
+        QQmlData::DeferredData *deferData = *dit;
+
+        auto range = deferData->bindings.equal_range(propertyIndex);
+        if (range.first == deferData->bindings.end())
+            continue;
+
+        QQmlComponentPrivate::ConstructionState *state = new QQmlComponentPrivate::ConstructionState;
+        state->completePending = true;
+
+        QQmlContextData *creationContext = nullptr;
+        state->creator.reset(new QQmlObjectCreator(deferData->context->parent, deferData->compilationUnit, creationContext));
+
+        enginePriv->inProgressCreations++;
+
+        typedef QMultiHash<int, const QV4::CompiledData::Binding *> QV4PropertyBindingHash;
+        auto it = std::reverse_iterator<QV4PropertyBindingHash::iterator>(range.second);
+        auto last = std::reverse_iterator<QV4PropertyBindingHash::iterator>(range.first);
+        while (it != last) {
+            if (!state->creator->populateDeferredBinding(property, deferData, *it))
+                state->errors << state->creator->errors;
+            ++it;
+        }
+
+        deferredState->constructionStates += state;
+
+        // Cleanup any remaining deferred bindings for this property, also in inner contexts,
+        // to avoid executing them later and overriding the property that was just populated.
+        while (dit != ddata->deferredData.rend()) {
+            (*dit)->bindings.remove(propertyIndex);
+            ++dit;
+        }
+        break;
+    }
+}
+
+void executeDeferred(QObject *object, const QString &property)
+{
+    QQmlData *data = QQmlData::get(object);
+    if (data && !data->deferredData.isEmpty() && !data->wasDeleted(object)) {
+        QQmlEnginePrivate *ep = QQmlEnginePrivate::get(data->context->engine);
+
+        QQmlComponentPrivate::DeferredState state;
+        beginDeferred(ep, QQmlProperty(object, property), &state);
+
+        // Release deferred data for those compilation units that no longer have deferred bindings
+        data->releaseDeferredData();
+
+        QQmlComponentPrivate::completeDeferred(ep, &state);
+    }
+}
+
+} // QtQuickPrivate
+
+QT_END_NAMESPACE

src/quicktemplates2/qquickdeferredexecute_p_p.h
@@ -34,8 +34,8 @@
 **
 ****************************************************************************/
 
-#ifndef QQUICKABSTRACTBUTTON_P_P_H
-#define QQUICKABSTRACTBUTTON_P_P_H
+#ifndef QQUICKDEFERREDEXECUTE_P_P_H
+#define QQUICKDEFERREDEXECUTE_P_P_H
 
 //
 //  W A R N I N G
@@ -48,66 +48,32 @@
 // We mean it.
 //
 
-#include <QtQuickTemplates2/private/qquickabstractbutton_p.h>
-#include <QtQuickTemplates2/private/qquickcontrol_p_p.h>
+#include <QtCore/qglobal.h>
+#include <QtQuickTemplates2/private/qquickdeferredpointer_p_p.h>
 
 QT_BEGIN_NAMESPACE
 
-class QQuickButtonGroup;
+class QString;
+class QObject;
 
-class Q_QUICKTEMPLATES2_PRIVATE_EXPORT QQuickAbstractButtonPrivate : public QQuickControlPrivate
+namespace QtQuickPrivate {
+    void executeDeferred(QObject *object, const QString &property);
+}
+
+template<typename T>
+void quickExecuteDeferred(QObject *object, const QString &property, QQuickDeferredPointer<T> &delegate)
 {
-    Q_DECLARE_PUBLIC(QQuickAbstractButton)
+    if (!delegate.isNull() || delegate.wasExecuted())
+        return;
 
-public:
-    QQuickAbstractButtonPrivate();
+    delegate.setExecuting(true);
+    QtQuickPrivate::executeDeferred(object, property);
+    delegate.setExecuting(false);
 
-    static QQuickAbstractButtonPrivate *get(QQuickAbstractButton *button)
-    {
-        return button->d_func();
-    }
-
-    QQuickItem *getContentItem() override;
-
-    void handlePress(const QPointF &point) override;
-    void handleMove(const QPointF &point) override;
-    void handleRelease(const QPointF &point) override;
-    void handleUngrab() override;
-
-    bool isPressAndHoldConnected();
-    void startPressAndHold();
-    void stopPressAndHold();
-
-    void startRepeatDelay();
-    void startPressRepeat();
-    void stopPressRepeat();
-
-    QQuickAbstractButton *findCheckedButton() const;
-    QList<QQuickAbstractButton *> findExclusiveButtons() const;
-
-    void toggle(bool value);
-
-    void executeIndicator();
-
-    QString text;
-    bool down;
-    bool explicitDown;
-    bool pressed;
-    bool keepPressed;
-    bool checked;
-    bool checkable;
-    bool autoExclusive;
-    bool autoRepeat;
-    bool wasHeld;
-    int holdTimer;
-    int delayTimer;
-    int repeatTimer;
-    QPointF pressPoint;
-    Qt::MouseButtons pressButtons;
-    QQuickDeferredPointer<QQuickItem> indicator;
-    QQuickButtonGroup *group;
-};
+    if (!delegate.isNull())
+        delegate.setExecuted();
+}
 
 QT_END_NAMESPACE
 
-#endif // QQUICKABSTRACTBUTTON_P_P_H
+#endif // QQUICKDEFERREDEXECUTE_P_P_H

.gitignore
@@ -32,6 +32,7 @@
 /tests/auto/controls/material/tst_material
 /tests/auto/controls/universal/tst_universal
 /tests/auto/cursor/tst_cursor
+/tests/auto/customization/tst_customization
 /tests/auto/focus/tst_focus
 /tests/auto/font/tst_font
 /tests/auto/platform/tst_platform

src/quicktemplates2/qquickabstractbutton_p.h
@@ -64,6 +64,7 @@ class Q_QUICKTEMPLATES2_PRIVATE_EXPORT QQuickAbstractButton : public QQuickContr
     Q_PROPERTY(bool checkable READ isCheckable WRITE setCheckable NOTIFY checkableChanged FINAL)
     Q_PROPERTY(bool autoExclusive READ autoExclusive WRITE setAutoExclusive NOTIFY autoExclusiveChanged FINAL)
     Q_PROPERTY(QQuickItem *indicator READ indicator WRITE setIndicator NOTIFY indicatorChanged FINAL)
+    Q_CLASSINFO("DeferredPropertyNames", "background,contentItem,indicator")
 
 public:
     explicit QQuickAbstractButton(QQuickItem *parent = nullptr);
@@ -116,6 +117,8 @@ Q_SIGNALS:
 
 protected:
     QQuickAbstractButton(QQuickAbstractButtonPrivate &dd, QQuickItem *parent);
+
+    void componentComplete() override;
 
     void focusOutEvent(QFocusEvent *event) override;
     void keyPressEvent(QKeyEvent *event) override;

src/quicktemplates2/qquickdeferredpointer_p_p.h
@@ -1,0 +1,189 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the Qt Quick Templates 2 module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QQUICKDEFERREDPOINTER_P_P_H
+#define QQUICKDEFERREDPOINTER_P_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/qglobal.h>
+
+QT_BEGIN_NAMESPACE
+
+template<typename T>
+class QQuickDeferredPointer
+{
+public:
+    inline QQuickDeferredPointer();
+    inline QQuickDeferredPointer(T *);
+    inline QQuickDeferredPointer(const QQuickDeferredPointer<T> &o);
+
+    inline bool isNull() const;
+
+    inline bool wasExecuted() const;
+    inline void setExecuted();
+
+    inline bool isExecuting() const;
+    inline void setExecuting(bool);
+
+    inline operator T*() const;
+    inline operator bool() const;
+
+    inline T *data() const;
+    inline T *operator*() const;
+    inline T *operator->() const;
+
+    inline QQuickDeferredPointer<T> &operator=(T *);
+    inline QQuickDeferredPointer<T> &operator=(const QQuickDeferredPointer &o);
+
+private:
+    quintptr ptr_value;
+
+    static const quintptr WasExecutedBit = 0x1;
+    static const quintptr IsExecutingBit = 0x2;
+    static const quintptr FlagsMask = WasExecutedBit | IsExecutingBit;
+};
+
+template<typename T>
+QQuickDeferredPointer<T>::QQuickDeferredPointer()
+: ptr_value(0)
+{
+}
+
+template<typename T>
+QQuickDeferredPointer<T>::QQuickDeferredPointer(T *v)
+: ptr_value(quintptr(v))
+{
+    Q_ASSERT((ptr_value & FlagsMask) == 0);
+}
+
+template<typename T>
+QQuickDeferredPointer<T>::QQuickDeferredPointer(const QQuickDeferredPointer<T> &o)
+: ptr_value(o.ptr_value)
+{
+}
+
+template<typename T>
+bool QQuickDeferredPointer<T>::isNull() const
+{
+    return 0 == (ptr_value & (~FlagsMask));
+}
+
+template<typename T>
+bool QQuickDeferredPointer<T>::wasExecuted() const
+{
+    return ptr_value & WasExecutedBit;
+}
+
+template<typename T>
+void QQuickDeferredPointer<T>::setExecuted()
+{
+    ptr_value |= WasExecutedBit;
+}
+
+template<typename T>
+bool QQuickDeferredPointer<T>::isExecuting() const
+{
+    return ptr_value & IsExecutingBit;
+}
+
+template<typename T>
+void QQuickDeferredPointer<T>::setExecuting(bool b)
+{
+    if (b)
+        ptr_value |= IsExecutingBit;
+    else
+        ptr_value &= ~IsExecutingBit;
+}
+
+template<typename T>
+QQuickDeferredPointer<T>::operator T*() const
+{
+    return data();
+}
+
+template<typename T>
+QQuickDeferredPointer<T>::operator bool() const
+{
+    return !isNull();
+}
+
+template<typename T>
+T *QQuickDeferredPointer<T>::data() const
+{
+    return (T *)(ptr_value & ~FlagsMask);
+}
+
+template<typename T>
+T *QQuickDeferredPointer<T>::operator*() const
+{
+    return (T *)(ptr_value & ~FlagsMask);
+}
+
+template<typename T>
+T *QQuickDeferredPointer<T>::operator->() const
+{
+    return (T *)(ptr_value & ~FlagsMask);
+}
+
+template<typename T>
+QQuickDeferredPointer<T> &QQuickDeferredPointer<T>::operator=(T *o)
+{
+    Q_ASSERT((quintptr(o) & FlagsMask) == 0);
+
+    ptr_value = quintptr(o) | (ptr_value & FlagsMask);
+    return *this;
+}
+
+template<typename T>
+QQuickDeferredPointer<T> &QQuickDeferredPointer<T>::operator=(const QQuickDeferredPointer &o)
+{
+    ptr_value = o.ptr_value;
+    return *this;
+}
+
+QT_END_NAMESPACE
+
+#endif // QQUICKDEFERREDPOINTER_P_P_H

commit d9e740d2ccf8c1d3caacfcd95537718048e3a7ba

Migitate the performance regression caused by deferred execution
458eb65f7 introduced a performance regression. Before 458eb65f7, qmlbench
delegates_button.qml results were around 40 frames on a TX1. After 458eb65f,
it dropped to 32. This patch is unfortunately not able to bring it on the
original level, but at least improves the results to 37.

For QQuickText, it is extremely important that implicitWidth() gets called
before the component is completed, to avoid doing implicit size calculation
multiple times. Deferred execution caused a performance regression by creating
contentItem in "one go". We need to split the deferred execution to two parts
so that bindings get first setup, and later enabled upon completion. This way,
we utilize QQuickText's performance optimization for implicit size calculation.

Task-number: QTBUG-50992
Change-Id: I4bf00af71b6d7dbf1d4b58b00fe547c6c321f8ed
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quicktemplates2/qquickabstractbutton.cpp
@@ -133,8 +133,9 @@ QQuickAbstractButtonPrivate::QQuickAbstractButtonPrivate()
 
 QQuickItem *QQuickAbstractButtonPrivate::getContentItem()
 {
-    executeContentItem();
-    return QQuickControlPrivate::getContentItem();
+    if (!contentItem)
+        executeContentItem();
+    return contentItem;
 }
 
 void QQuickAbstractButtonPrivate::handlePress(const QPointF &point)
@@ -265,10 +266,18 @@ void QQuickAbstractButtonPrivate::toggle(bool value)
         emit q->toggled();
 }
 
-void QQuickAbstractButtonPrivate::executeIndicator()
+static inline QString indicatorName() { return QStringLiteral("indicator"); }
+
+void QQuickAbstractButtonPrivate::executeIndicator(bool complete)
 {
-    Q_Q(QQuickAbstractButton);
-    quickExecuteDeferred(q, QStringLiteral("indicator"), indicator);
+    Q_Q(QQuickControl);
+    if (indicator.wasExecuted())
+        return;
+
+    if (!indicator)
+        quickBeginDeferred(q, indicatorName(), indicator);
+    if (complete)
+        quickCompleteDeferred(q, indicatorName(), indicator);
 }
 
 QQuickAbstractButton *QQuickAbstractButtonPrivate::findCheckedButton() const
@@ -559,7 +568,8 @@ void QQuickAbstractButton::setAutoRepeat(bool repeat)
 QQuickItem *QQuickAbstractButton::indicator() const
 {
     QQuickAbstractButtonPrivate *d = const_cast<QQuickAbstractButtonPrivate *>(d_func());
-    d->executeIndicator();
+    if (!d->indicator)
+        d->executeIndicator();
     return d->indicator;
 }
 
@@ -594,9 +604,9 @@ void QQuickAbstractButton::toggle()
 void QQuickAbstractButton::componentComplete()
 {
     Q_D(QQuickAbstractButton);
-    d->executeIndicator();
-    d->executeBackground();
-    d->executeContentItem();
+    d->executeIndicator(true);
+    d->executeBackground(true);
+    d->executeContentItem(true);
     QQuickControl::componentComplete();
 }
 

src/quicktemplates2/qquickabstractbutton_p_p.h
@@ -87,7 +87,7 @@ public:
 
     void toggle(bool value);
 
-    void executeIndicator();
+    void executeIndicator(bool complete = false);
 
     QString text;
     bool down;

src/quicktemplates2/qquickcontrol.cpp
@@ -934,16 +934,32 @@ QLocale QQuickControlPrivate::calcLocale(const QQuickItem *item)
     return QLocale();
 }
 
-void QQuickControlPrivate::executeContentItem()
+static inline QString contentItemName() { return QStringLiteral("contentItem"); }
+
+void QQuickControlPrivate::executeContentItem(bool complete)
 {
     Q_Q(QQuickControl);
-    quickExecuteDeferred(q, QStringLiteral("contentItem"), contentItem);
+    if (contentItem.wasExecuted())
+        return;
+
+    if (!contentItem)
+        quickBeginDeferred(q, contentItemName(), contentItem);
+    if (complete)
+        quickCompleteDeferred(q, contentItemName(), contentItem);
 }
 
-void QQuickControlPrivate::executeBackground()
+static inline QString backgroundName() { return QStringLiteral("background"); }
+
+void QQuickControlPrivate::executeBackground(bool complete)
 {
     Q_Q(QQuickControl);
-    quickExecuteDeferred(q, QStringLiteral("background"), background);
+    if (background.wasExecuted())
+        return;
+
+    if (!background)
+        quickBeginDeferred(q, backgroundName(), background);
+    if (complete)
+        quickCompleteDeferred(q, backgroundName(), background);
 }
 
 /*
@@ -1228,7 +1244,8 @@ void QQuickControl::setWheelEnabled(bool enabled)
 QQuickItem *QQuickControl::background() const
 {
     QQuickControlPrivate *d = const_cast<QQuickControlPrivate *>(d_func());
-    d->executeBackground();
+    if (!d->background)
+        d->executeBackground();
     return d->background;
 }
 

src/quicktemplates2/qquickcontrol_p_p.h
@@ -127,8 +127,8 @@ public:
     static bool calcHoverEnabled(const QQuickItem *item);
 #endif
 
-    void executeContentItem();
-    void executeBackground();
+    void executeContentItem(bool complete = false);
+    void executeBackground(bool complete = false);
 
     static void destroyDelegate(QObject *object, QObject *parent);
 

src/quicktemplates2/qquickdeferredexecute.cpp
@@ -36,6 +36,7 @@
 
 #include "qquickdeferredexecute_p_p.h"
 
+#include <QtCore/qhash.h>
 #include <QtQml/qqmlengine.h>
 #include <QtQml/private/qqmldata_p.h>
 #include <QtQml/private/qqmlcomponent_p.h>
@@ -44,6 +45,15 @@
 QT_BEGIN_NAMESPACE
 
 namespace QtQuickPrivate {
+
+typedef QHash<uint, QQmlComponentPrivate::DeferredState *> DeferredStates;
+
+static inline uint qHash(QObject *object, const QString &propertyName)
+{
+    return ::qHash(object) + ::qHash(propertyName);
+}
+
+Q_GLOBAL_STATIC(DeferredStates, deferredStates)
 
 static void beginDeferred(QQmlEnginePrivate *enginePriv, const QQmlProperty &property, QQmlComponentPrivate::DeferredState *deferredState)
 {
@@ -89,20 +99,31 @@ static void beginDeferred(QQmlEnginePrivate *enginePriv, const QQmlProperty &pro
     }
 }
 
-void executeDeferred(QObject *object, const QString &property)
+void beginDeferred(QObject *object, const QString &property)
 {
     QQmlData *data = QQmlData::get(object);
     if (data && !data->deferredData.isEmpty() && !data->wasDeleted(object)) {
         QQmlEnginePrivate *ep = QQmlEnginePrivate::get(data->context->engine);
 
-        QQmlComponentPrivate::DeferredState state;
-        beginDeferred(ep, QQmlProperty(object, property), &state);
+        QQmlComponentPrivate::DeferredState *state = new QQmlComponentPrivate::DeferredState;
+        beginDeferred(ep, QQmlProperty(object, property), state);
 
         // Release deferred data for those compilation units that no longer have deferred bindings
         data->releaseDeferredData();
 
-        QQmlComponentPrivate::completeDeferred(ep, &state);
+        deferredStates()->insert(qHash(object, property), state);
     }
+}
+
+void completeDeferred(QObject *object, const QString &property)
+{
+    QQmlData *data = QQmlData::get(object);
+    QQmlComponentPrivate::DeferredState *state = deferredStates()->take(qHash(object, property));
+    if (data && state && !data->wasDeleted(object)) {
+        QQmlEnginePrivate *ep = QQmlEnginePrivate::get(data->context->engine);
+        QQmlComponentPrivate::completeDeferred(ep, state);
+    }
+    delete state;
 }
 
 } // QtQuickPrivate

src/quicktemplates2/qquickdeferredexecute_p_p.h
@@ -57,21 +57,27 @@ class QString;
 class QObject;
 
 namespace QtQuickPrivate {
-    void executeDeferred(QObject *object, const QString &property);
+    void beginDeferred(QObject *object, const QString &property);
+    void completeDeferred(QObject *object, const QString &property);
 }
 
 template<typename T>
-void quickExecuteDeferred(QObject *object, const QString &property, QQuickDeferredPointer<T> &delegate)
+void quickBeginDeferred(QObject *object, const QString &property, QQuickDeferredPointer<T> &delegate)
 {
-    if (!delegate.isNull() || delegate.wasExecuted())
-        return;
-
+    Q_ASSERT(delegate.isNull());
     delegate.setExecuting(true);
-    QtQuickPrivate::executeDeferred(object, property);
+    QtQuickPrivate::beginDeferred(object, property);
     delegate.setExecuting(false);
+}
 
-    if (!delegate.isNull())
-        delegate.setExecuted();
+template<typename T>
+void quickCompleteDeferred(QObject *object, const QString &property, QQuickDeferredPointer<T> &delegate)
+{
+    Q_ASSERT(!delegate.wasExecuted());
+    delegate.setExecuting(true);
+    QtQuickPrivate::completeDeferred(object, property);
+    delegate.setExecuting(false);
+    delegate.setExecuted();
 }
 
 QT_END_NAMESPACE