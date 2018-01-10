7cf3421858c96ff3a2fe3b860be01a3e0e1133cf
Samuli Piippo <samuli.piippo@qt.io>
2018-01-10 16:11:30 +0200

    Enable JIT on QNX7 for ARM64
    Remove abs(long) definition, since the #ifdef check no longer works
    with QNX7 and QNX < 6.6 is no longer supported.
    
    [ChangeLog][Qml] Enabled ARM64 JIT for QNX7
    
    Change-Id: Ife02f3edb508eddaf15da65954496265366e232d
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- src/qml/jsruntime/qv4global_p.h
+++ src/qml/jsruntime/qv4global_p.h
@@ -104,7 +104,7 @@ inline double trunc(double d) { return d > 0 ? floor(d) : ceil(d); }
 #    define V4_ENABLE_JIT
 #  endif
 #elif defined(Q_PROCESSOR_ARM_64) && (QT_POINTER_SIZE == 8)
-#  if defined(Q_OS_LINUX)
+#  if defined(Q_OS_LINUX) || defined(Q_OS_QNX)
 #    define V4_ENABLE_JIT
 #  endif
 #elif defined(Q_PROCESSOR_MIPS_32) && defined(Q_OS_LINUX)
