qt/qtdeclarative

commit e6d4df156e9aec62054740dc99ab8ba2855eaafc

Make sure we send the UngrabMouse event when clearing grabbers
This caused a regression in QtLocation autotest
declarative_ui\tst_map_mouse.qml
where the sequence of tests test_basic_press_release() and test_enabled()
caused test_enabled() to fail.

Change-Id: I53621a9a18d0574163260674c11bdcb02c3e1218
Reviewed-by: Paolo Angelelli <paolo.angelelli@qt.io>
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/quick/items/qquickwindow.cpp
@@ -2284,6 +2284,7 @@ QQuickPointerEvent *QQuickWindowPrivate::pointerEventInstance(QEvent *event) con
 
 void QQuickWindowPrivate::deliverPointerEvent(QQuickPointerEvent *event)
 {
+    Q_Q(QQuickWindow);
     // If users spin the eventloop as a result of event delivery, we disable
     // event compression and send events directly. This is because we consider
     // the usecase a bit evil, but we at least don't want to lose events.
@@ -2293,8 +2294,11 @@ void QQuickWindowPrivate::deliverPointerEvent(QQuickPointerEvent *event)
     if (event->asPointerMouseEvent()) {
         deliverMouseEvent(event->asPointerMouseEvent());
         // failsafe: never allow any kind of grab to persist after release
-        if (event->isReleaseEvent() && event->buttons() == Qt::NoButton)
+        QQuickItem *grabber = q->mouseGrabberItem();
+        if (event->isReleaseEvent() && event->buttons() == Qt::NoButton && grabber) {
             event->clearGrabbers();
+            sendUngrabEvent(grabber, false);
+        }
     } else if (event->asPointerTouchEvent()) {
         deliverTouchEvent(event->asPointerTouchEvent());
     } else {

commit 5bcbba1312ac91f7b9f23e7a1574225e190bded4

Make sure passive grabbers are cleared on release
This got regressed by change e6d4df156e9aec62054740dc99ab8ba2855eaafc. Before
that change, we always cleared both the exclusive and passive grabbers.

Task-number: QTBUG-66152
Change-Id: I93d2568bd2a23ddd55a5294d544f978a50a5543e
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/quick/items/qquickwindow.cpp
@@ -2294,10 +2294,9 @@ void QQuickWindowPrivate::deliverPointerEvent(QQuickPointerEvent *event)
     if (event->asPointerMouseEvent()) {
         deliverMouseEvent(event->asPointerMouseEvent());
         // failsafe: never allow any kind of grab to persist after release
-        QQuickItem *grabber = q->mouseGrabberItem();
-        if (event->isReleaseEvent() && event->buttons() == Qt::NoButton && grabber) {
+        if (event->isReleaseEvent() && event->buttons() == Qt::NoButton) {
             event->clearGrabbers();
-            sendUngrabEvent(grabber, false);
+            sendUngrabEvent(q->mouseGrabberItem(), false);
         }
     } else if (event->asPointerTouchEvent()) {
         deliverTouchEvent(event->asPointerTouchEvent());

tests/auto/quick/pointerhandlers/flickableinterop/tst_flickableinterop.cpp
@@ -544,6 +544,20 @@ void tst_FlickableInterop::mouseDragFlickableBehindItemWithHandlers()
         QCOMPARE(originP1 + QPoint(3*dragThreshold, 0), p1);
     }
     QTest::mouseRelease(window, Qt::LeftButton, Qt::NoModifier, p1);
+    // wait until flickable stops
+    QTRY_COMPARE(flickable->isMoving(), false);
+
+    // After the mouse button has been released, move the mouse and ensure that nothing is moving
+    // because of that (this tests if all grabs are released when the mouse button is released).
+    p1 = rect->mapToScene(rect->clipRect().center()).toPoint();
+    originP1 = p1;
+    for (int i = 0; i < 3; ++i) {
+        p1 += QPoint(dragThreshold, 0);
+        QTest::mouseMove(window, p1);
+        QQuickTouchUtils::flush(window);
+    }
+    QCOMPARE(flickable->isMoving(), false);
+    QCOMPARE(originP1, rect->mapToScene(rect->clipRect().center()).toPoint());
 }
 
 QTEST_MAIN(tst_FlickableInterop)