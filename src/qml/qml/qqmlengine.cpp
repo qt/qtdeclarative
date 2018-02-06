qt/qtdeclarative

commit 3b6eeee177b64eebe240d51be0c7bb5f031471d8

Fix execution of deferred properties
When deferred properties were assigned in multiple contexts, only the
outermost context was executed. Any deferred property assignments in
other inner contexts were never executed. Collect the deferred data to
a container to be able to execute them all.

Task-number: QTBUG-63200
Change-Id: I88fab27c1f81b5188430ada086dcc19842507e99
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/qml/qml/qqmlengine.cpp
@@ -739,7 +739,7 @@ QQmlData::QQmlData()
       hasInterceptorMetaObject(false), hasVMEMetaObject(false), parentFrozen(false),
       bindingBitsSize(MaxInlineBits), bindingBitsValue(0), notifyList(0),
       bindings(0), signalHandlers(0), nextContextObject(0), prevContextObject(0),
-      lineNumber(0), columnNumber(0), jsEngineId(0), compilationUnit(0), deferredData(0),
+      lineNumber(0), columnNumber(0), jsEngineId(0), compilationUnit(0),
       propertyCache(0), guards(0), extendedData(0)
 {
     init();
@@ -1469,18 +1469,16 @@ void qmlExecuteDeferred(QObject *object)
 {
     QQmlData *data = QQmlData::get(object);
 
-    if (data && data->deferredData && !data->wasDeleted(object)) {
+    if (data && !data->deferredData.isEmpty() && !data->wasDeleted(object)) {
         QQmlEnginePrivate *ep = QQmlEnginePrivate::get(data->context->engine);
 
-        QQmlComponentPrivate::ConstructionState state;
+        QQmlComponentPrivate::DeferredState state;
         QQmlComponentPrivate::beginDeferred(ep, object, &state);
 
         // Release the reference for the deferral action (we still have one from construction)
-        data->deferredData->compilationUnit->release();
-        delete data->deferredData;
-        data->deferredData = 0;
+        data->releaseDeferredData();
 
-        QQmlComponentPrivate::complete(ep, &state);
+        QQmlComponentPrivate::completeDeferred(ep, &state);
     }
 }
 
@@ -1638,6 +1636,15 @@ void QQmlData::NotifyList::layout()
     todo = 0;
 }
 
+void QQmlData::releaseDeferredData()
+{
+    for (DeferredData *deferData : qAsConst(deferredData)) {
+        deferData->compilationUnit->release();
+        delete deferData;
+    }
+    deferredData.clear();
+}
+
 void QQmlData::addNotify(int index, QQmlNotifierEndpoint *endpoint)
 {
     if (!notifyList) {
@@ -1712,11 +1719,7 @@ void QQmlData::destroyed(QObject *object)
         compilationUnit = 0;
     }
 
-    if (deferredData) {
-        deferredData->compilationUnit->release();
-        delete deferredData;
-        deferredData = 0;
-    }
+    releaseDeferredData();
 
     QQmlBoundSignal *signalHandler = signalHandlers;
     while (signalHandler) {

src/qml/qml/qqmlcomponent.cpp
@@ -878,19 +878,33 @@ QQmlComponentPrivate::beginCreate(QQmlContextData *context)
 }
 
 void QQmlComponentPrivate::beginDeferred(QQmlEnginePrivate *enginePriv,
-                                                 QObject *object, ConstructionState *state)
+                                                 QObject *object, DeferredState *deferredState)
 {
-    enginePriv->inProgressCreations++;
-    state->errors.clear();
-    state->completePending = true;
-
     QQmlData *ddata = QQmlData::get(object);
-    Q_ASSERT(ddata->deferredData);
-    QQmlData::DeferredData *deferredData = ddata->deferredData;
-    QQmlContextData *creationContext = 0;
-    state->creator.reset(new QQmlObjectCreator(deferredData->context->parent, deferredData->compilationUnit, creationContext));
-    if (!state->creator->populateDeferredProperties(object))
-        state->errors << state->creator->errors;
+    Q_ASSERT(!ddata->deferredData.isEmpty());
+
+    deferredState->constructionStates.reserve(ddata->deferredData.size());
+
+    for (QQmlData::DeferredData *deferredData : qAsConst(ddata->deferredData)) {
+        enginePriv->inProgressCreations++;
+
+        ConstructionState *state = new ConstructionState;
+        state->completePending = true;
+
+        QQmlContextData *creationContext = nullptr;
+        state->creator.reset(new QQmlObjectCreator(deferredData->context->parent, deferredData->compilationUnit, creationContext));
+
+        if (!state->creator->populateDeferredProperties(object, deferredData))
+            state->errors << state->creator->errors;
+
+        deferredState->constructionStates += state;
+    }
+}
+
+void QQmlComponentPrivate::completeDeferred(QQmlEnginePrivate *enginePriv, QQmlComponentPrivate::DeferredState *deferredState)
+{
+    for (ConstructionState *state : qAsConst(deferredState->constructionStates))
+        complete(enginePriv, state);
 }
 
 void QQmlComponentPrivate::complete(QQmlEnginePrivate *enginePriv, ConstructionState *state)

src/qml/qml/qqmlcomponent_p.h
@@ -114,8 +114,17 @@ public:
     };
     ConstructionState state;
 
-    static void beginDeferred(QQmlEnginePrivate *enginePriv, QObject *object,
-                              ConstructionState *state);
+    struct DeferredState {
+        ~DeferredState() {
+            qDeleteAll(constructionStates);
+            constructionStates.clear();
+        }
+        QVector<ConstructionState *> constructionStates;
+    };
+
+    static void beginDeferred(QQmlEnginePrivate *enginePriv, QObject *object, DeferredState* deferredState);
+    static void completeDeferred(QQmlEnginePrivate *enginePriv, DeferredState *deferredState);
+
     static void complete(QQmlEnginePrivate *enginePriv, ConstructionState *state);
 
     QQmlEngine *engine;

src/qml/qml/qqmldata_p.h
@@ -57,6 +57,7 @@
 #include <private/qv4value_p.h>
 #include <private/qv4persistent_p.h>
 #include <qjsengine.h>
+#include <qvector.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -219,7 +220,9 @@ public:
         QQmlContextData *context;//Could be either context or outerContext
     };
     QV4::CompiledData::CompilationUnit *compilationUnit;
-    DeferredData *deferredData;
+    QVector<DeferredData *> deferredData;
+
+    void releaseDeferredData();
 
     QV4::WeakValue jsWrapper;
 

src/qml/qml/qqmlobjectcreator.cpp
@@ -233,10 +233,10 @@ QObject *QQmlObjectCreator::create(int subComponentIndex, QObject *parent, QQmlI
     return instance;
 }
 
-bool QQmlObjectCreator::populateDeferredProperties(QObject *instance)
+bool QQmlObjectCreator::populateDeferredProperties(QObject *instance, QQmlData::DeferredData *deferredData)
 {
     QQmlData *declarativeData = QQmlData::get(instance);
-    context = declarativeData->deferredData->context;
+    context = deferredData->context;
     sharedState->rootContext = context;
 
     QObject *bindingTarget = instance;
@@ -260,7 +260,7 @@ bool QQmlObjectCreator::populateDeferredProperties(QObject *instance)
     qSwap(_propertyCache, cache);
     qSwap(_qobject, instance);
 
-    int objectIndex = declarativeData->deferredData->deferredIdx;
+    int objectIndex = deferredData->deferredIdx;
     qSwap(_compiledObjectIndex, objectIndex);
 
     const QV4::CompiledData::Object *obj = qmlUnit->objectAt(_compiledObjectIndex);
@@ -1347,7 +1347,7 @@ bool QQmlObjectCreator::populateInstance(int index, QObject *instance, QObject *
         deferData->compilationUnit = compilationUnit;
         deferData->compilationUnit->addref();
         deferData->context = context;
-        _ddata->deferredData = deferData;
+        _ddata->deferredData.append(deferData);
     }
 
     if (_compiledObject->nFunctions > 0)

src/qml/qml/qqmlobjectcreator_p.h
@@ -89,7 +89,7 @@ public:
     ~QQmlObjectCreator();
 
     QObject *create(int subComponentIndex = -1, QObject *parent = 0, QQmlInstantiationInterrupt *interrupt = 0);
-    bool populateDeferredProperties(QObject *instance);
+    bool populateDeferredProperties(QObject *instance, QQmlData::DeferredData *deferredData);
     QQmlContextData *finalize(QQmlInstantiationInterrupt &interrupt);
     void cancel(QObject *object);
     void clear();

tests/auto/qml/qqmllanguage/data/MyDeferredProperties.qml
@@ -1,0 +1,19 @@
+import QtQml 2.0
+import Test 1.0
+DeferredProperties {
+    groupProperty: QtObject {
+        objectName: "innerobj"
+        property bool wasCompleted: false
+        Component.onCompleted: wasCompleted = true
+    }
+    QtObject {
+        objectName: "innerlist1"
+        property bool wasCompleted: false
+        Component.onCompleted: wasCompleted = true
+    }
+    QtObject {
+        objectName: "innerlist2"
+        property bool wasCompleted: false
+        Component.onCompleted: wasCompleted = true
+    }
+}

tests/auto/qml/qqmllanguage/data/deferredProperties.qml
@@ -1,0 +1,19 @@
+import QtQml 2.0
+import Test 1.0
+MyDeferredProperties {
+    groupProperty: QtObject {
+        objectName: "outerobj"
+        property bool wasCompleted: false
+        Component.onCompleted: wasCompleted = true
+    }
+    QtObject {
+        objectName: "outerlist1"
+        property bool wasCompleted: false
+        Component.onCompleted: wasCompleted = true
+    }
+    QtObject {
+        objectName: "outerlist2"
+        property bool wasCompleted: false
+        Component.onCompleted: wasCompleted = true
+    }
+}

commit c6b3c69e014f846a142c2429cd2d675c75b74245

Fix memory leak with deferred properties
This is a regression introduced with commit
3b6eeee177b64eebe240d51be0c7bb5f031471d8 in the 5.9 branch. When
constructing an object with deferred properties and not running
qmlExecuteDeferred, then the deferred data would never get deleted
because the bindings list remains non-empty and we would leak the
deferred data as well as the entire compilation unit behind it.

This happens for example when declaring when instantiating a QML file
with states:

   states: [ State { ... }, State { ... }, ... }

Unless every state is entered, its deferred changes property is never
applied (via qmlExecuteDeferred) and thus the defer data is leaked.

Task-number: QTBUG-66189
Change-Id: I1b2119c601d1e0ab4e37f53d4cf2f569586ee883
Reviewed-by: J-P Nurmi <jpnurmi@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/qml/qml/qqmlengine.cpp
@@ -1763,7 +1763,8 @@ void QQmlData::destroyed(QObject *object)
 
     compilationUnit = nullptr;
 
-    releaseDeferredData();
+    qDeleteAll(deferredData);
+    deferredData.clear();
 
     QQmlBoundSignal *signalHandler = signalHandlers;
     while (signalHandler) {