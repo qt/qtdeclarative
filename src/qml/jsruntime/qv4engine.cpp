qt/qtdeclarative

commit b56f7d6f79b0de73c405b1503bfeb71ef5caf58f

Re-enable the YarrJIT
This makes a huge difference for regexp evaluation.

Change-Id: I6b466f60280583c85d34be8734e8fef209b7b989
Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

src/qml/jsruntime/qv4regexp.cpp
@@ -106,7 +106,7 @@ void Heap::RegExp::init(ExecutionEngine* engine, const QString &pattern, bool ig
     byteCode = p.take();
 #if ENABLE(YARR_JIT)
     jitCode = new JSC::Yarr::YarrCodeBlock;
-    if (!yarrPattern.m_containsBackreferences && engine->iselFactory->jitCompileRegexps()) {
+    if (!yarrPattern.m_containsBackreferences) {
         JSC::JSGlobalData dummy(engine->regExpAllocator);
         JSC::Yarr::jitCompile(yarrPattern, JSC::Yarr::Char16, &dummy, *jitCode);
     }

src/qml/jsruntime/qv4global_p.h
@@ -118,9 +118,6 @@ inline double trunc(double d) { return d > 0 ? floor(d) : ceil(d); }
 #endif
 #endif
 
-// ### for now we only work with the interpreter
-#undef V4_ENABLE_JIT
-
 // For debug purposes: add CONFIG+=force-compile-jit to qmake's command-line to always compile the JIT.
 #if defined(V4_FORCE_COMPILE_JIT) && !defined(V4_ENABLE_JIT)
 #  define V4_ENABLE_JIT

commit 6a2ebc47cfa57dacf5e29083dade8701e1f82bbe

Fix crashes on WinRT when allocation of executable memory is disabled
This regressed in commit b56f7d6f79b0de73c405b1503bfeb71ef5caf58f. We
need to choose the YARR JIT (as well as the regular JIT) only if we can
allocate executable memory.

Change-Id: I150238fda7b3699cb1d7ffedeeed3c6f3f54132b
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/qml/jsruntime/qv4engine.cpp
@@ -120,6 +120,9 @@ QQmlEngine *ExecutionEngine::qmlEngine() const
 #endif // V4_BOOTSTRAP
 
 qint32 ExecutionEngine::maxCallDepth = -1;
+#if defined(V4_ENABLE_JIT) && !defined(V4_BOOTSTRAP)
+const bool ExecutionEngine::canAllocateExecutableMemory = OSAllocator::canAllocateExecutableMemory();
+#endif
 
 ExecutionEngine::ExecutionEngine()
     : executableAllocator(new QV4::ExecutableAllocator)

src/qml/jsruntime/qv4engine_p.h
@@ -484,10 +484,14 @@ public:
 
     bool checkStackLimits();
 
-    bool canJIT(Function *f)
+    bool canJIT(Function *f = nullptr)
     {
 #if defined(V4_ENABLE_JIT) && !defined(V4_BOOTSTRAP)
-        return f->interpreterCallCount >= jitCallCountThreshold;
+        if (!canAllocateExecutableMemory)
+            return false;
+        if (f)
+            return f->interpreterCallCount >= jitCallCountThreshold;
+        return true;
 #else
         Q_UNUSED(f);
         return false;
@@ -502,6 +506,9 @@ private:
     QScopedPointer<QV4::Profiling::Profiler> m_profiler;
 #endif
     int jitCallCountThreshold;
+#if defined(V4_ENABLE_JIT) && !defined(V4_BOOTSTRAP)
+    static const bool canAllocateExecutableMemory;
+#endif
 };
 
 // This is a trick to tell the code generators that functions taking a NoThrowContext won't

src/qml/jsruntime/qv4regexp.cpp
@@ -82,7 +82,7 @@ Heap::RegExp *RegExp::create(ExecutionEngine* engine, const QString& pattern, bo
         return result->d();
 
     Scope scope(engine);
-    Scoped<RegExp> result(scope, engine->memoryManager->alloc<RegExp>(pattern, ignoreCase, multiline, global));
+    Scoped<RegExp> result(scope, engine->memoryManager->alloc<RegExp>(engine, pattern, ignoreCase, multiline, global));
 
     result->d()->cache = cache;
     cachedValue.set(engine, result);
@@ -90,7 +90,7 @@ Heap::RegExp *RegExp::create(ExecutionEngine* engine, const QString& pattern, bo
     return result->d();
 }
 
-void Heap::RegExp::init(const QString &pattern, bool ignoreCase, bool multiline, bool global)
+void Heap::RegExp::init(ExecutionEngine *engine, const QString &pattern, bool ignoreCase, bool multiline, bool global)
 {
     Base::init();
     this->pattern = new QString(pattern);
@@ -106,7 +106,7 @@ void Heap::RegExp::init(const QString &pattern, bool ignoreCase, bool multiline,
         return;
     subPatternCount = yarrPattern.m_numSubpatterns;
 #if ENABLE(YARR_JIT)
-    if (!yarrPattern.m_containsBackreferences) {
+    if (!yarrPattern.m_containsBackreferences && engine->canJIT()) {
         jitCode = new JSC::Yarr::YarrCodeBlock;
         JSC::JSGlobalData dummy(internalClass->engine->regExpAllocator);
         JSC::Yarr::jitCompile(yarrPattern, JSC::Yarr::Char16, &dummy, *jitCode);

src/qml/jsruntime/qv4regexp_p.h
@@ -76,7 +76,7 @@ struct RegExpCacheKey;
 namespace Heap {
 
 struct RegExp : Base {
-    void init(const QString& pattern, bool ignoreCase, bool multiline, bool global);
+    void init(ExecutionEngine *engine, const QString& pattern, bool ignoreCase, bool multiline, bool global);
     void destroy();
 
     QString *pattern;