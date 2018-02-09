qt/qtdeclarative

commit e22b624d9ab1f36021adb9cdbfa9b37054282bb8

Fix crashes with closures created in QML components
When closures created inside QML components are called after the
surrounding component (and consequently QML context) has been destroyed,
we are in a somewhat limited environment. Initially we would just crash
as the calling QML context is not valid anymore. We can alleviate that
by introducing reference counting on the context and letting the QML
context wrapper keep a strong reference. This avoids the crashes and
also ensures that at least imports continue to be accessible within
these contexts (as the singleton test case demonstrates).

Task-number: QTBUG-61781
Change-Id: I893f171842d01b0863d95a02ea738adc2620e236
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/qml/jsruntime/qv4qmlcontext.cpp
@@ -62,20 +62,17 @@ using namespace QV4;
 DEFINE_OBJECT_VTABLE(QmlContextWrapper);
 DEFINE_MANAGED_VTABLE(QmlContext);
 
-void Heap::QmlContextWrapper::init(QQmlContextData *context, QObject *scopeObject, bool ownsContext)
+void Heap::QmlContextWrapper::init(QQmlContextData *context, QObject *scopeObject)
 {
     Object::init();
     readOnly = true;
-    this->ownsContext = ownsContext;
     isNullWrapper = false;
-    this->context = new QQmlGuardedContextData(context);
+    this->context = new QQmlContextDataRef(context);
     this->scopeObject.init(scopeObject);
 }
 
 void Heap::QmlContextWrapper::destroy()
 {
-    if (*context && ownsContext)
-        (*context)->destroy();
     delete context;
     scopeObject.destroy();
     Object::destroy();
@@ -321,7 +318,7 @@ Heap::QmlContext *QmlContext::createWorkerContext(ExecutionContext *parent, cons
     context->isInternal = true;
     context->isJSContext = true;
 
-    Scoped<QmlContextWrapper> qml(scope, scope.engine->memoryManager->allocObject<QmlContextWrapper>(context, (QObject*)0, true));
+    Scoped<QmlContextWrapper> qml(scope, scope.engine->memoryManager->allocObject<QmlContextWrapper>(context, (QObject*)0));
     qml->d()->isNullWrapper = true;
 
     qml->setReadOnly(false);

src/qml/qml/qqmlcontext.cpp
@@ -161,6 +161,7 @@ QQmlContext::QQmlContext(QQmlEngine *e, bool)
 {
     Q_D(QQmlContext);
     d->data = new QQmlContextData(this);
+    ++d->data->refCount;
 
     d->data->engine = e;
 }
@@ -174,6 +175,7 @@ QQmlContext::QQmlContext(QQmlEngine *engine, QObject *parent)
 {
     Q_D(QQmlContext);
     d->data = new QQmlContextData(this);
+    ++d->data->refCount;
 
     d->data->setParent(engine?QQmlContextData::get(engine->rootContext()):0);
 }
@@ -187,6 +189,7 @@ QQmlContext::QQmlContext(QQmlContext *parentContext, QObject *parent)
 {
     Q_D(QQmlContext);
     d->data = new QQmlContextData(this);
+    ++d->data->refCount;
 
     d->data->setParent(parentContext?QQmlContextData::get(parentContext):0);
 }
@@ -199,6 +202,7 @@ QQmlContext::QQmlContext(QQmlContextData *data)
 {
     Q_D(QQmlContext);
     d->data = data;
+    // don't add a refcount here, as the data owns this context
 }
 
 /*!
@@ -212,7 +216,8 @@ QQmlContext::~QQmlContext()
 {
     Q_D(QQmlContext);
 
-    if (!d->data->isInternal)
+    d->data->publicContext = 0;
+    if (!--d->data->refCount)
         d->data->destroy();
 }
 
@@ -521,12 +526,12 @@ QQmlContextData::QQmlContextData()
 }
 
 QQmlContextData::QQmlContextData(QQmlContext *ctxt)
-: parent(0), engine(0), isInternal(false), ownedByParent(false), isJSContext(false),
-  isPragmaLibraryContext(false), unresolvedNames(false), hasEmittedDestruction(false), isRootObjectInCreation(false),
-  publicContext(ctxt), incubator(0), componentObjectIndex(-1),
-  contextObject(0), childContexts(0), nextChild(0), prevChild(0),
-  expressions(0), contextObjects(0), contextGuards(0), idValues(0), idValueCount(0), linkedContext(0),
-  componentAttached(0)
+    : engine(0), isInternal(false), isJSContext(false),
+      isPragmaLibraryContext(false), unresolvedNames(false), hasEmittedDestruction(false), isRootObjectInCreation(false),
+      publicContext(ctxt), incubator(0), componentObjectIndex(-1),
+      contextObject(0), nextChild(0), prevChild(0),
+      expressions(0), contextObjects(0), idValues(0), idValueCount(0),
+      componentAttached(0)
 {
 }
 
@@ -563,11 +568,8 @@ void QQmlContextData::invalidate()
     emitDestruction();
 
     while (childContexts) {
-        if (childContexts->ownedByParent) {
-            childContexts->destroy();
-        } else {
-            childContexts->invalidate();
-        }
+        Q_ASSERT(childContexts != this);
+        childContexts->invalidate();
     }
 
     if (prevChild) {
@@ -601,12 +603,17 @@ void QQmlContextData::clearContext()
 
 void QQmlContextData::destroy()
 {
-    if (linkedContext)
-        linkedContext->destroy();
+    Q_ASSERT(refCount == 0);
+    linkedContext = 0;
 
-    if (engine) invalidate();
+    // avoid recursion
+    ++refCount;
+    if (engine)
+        invalidate();
 
+    Q_ASSERT(refCount == 1);
     clearContext();
+    Q_ASSERT(refCount == 1);
 
     while (contextObjects) {
         QQmlData *co = contextObjects;
@@ -617,6 +624,7 @@ void QQmlContextData::destroy()
         co->nextContextObject = 0;
         co->prevContextObject = 0;
     }
+    Q_ASSERT(refCount == 1);
 
     QQmlGuardedContextData *contextGuard = contextGuards;
     while (contextGuard) {
@@ -627,17 +635,29 @@ void QQmlContextData::destroy()
         contextGuard = next;
     }
     contextGuards = 0;
+    Q_ASSERT(refCount == 1);
 
     delete [] idValues;
+    idValues = 0;
 
-    if (isInternal)
+    Q_ASSERT(refCount == 1);
+    if (publicContext) {
+        // the QQmlContext destructor will remove one ref again
+        ++refCount;
         delete publicContext;
+    }
+
+    Q_ASSERT(refCount == 1);
+    --refCount;
+    Q_ASSERT(refCount == 0);
 
     delete this;
 }
 
-void QQmlContextData::setParent(QQmlContextData *p, bool parentTakesOwnership)
+void QQmlContextData::setParent(QQmlContextData *p)
 {
+    if (p == parent)
+        return;
     if (p) {
         parent = p;
         engine = p->engine;
@@ -645,7 +665,6 @@ void QQmlContextData::setParent(QQmlContextData *p, bool parentTakesOwnership)
         if (nextChild) nextChild->prevChild = &nextChild;
         prevChild = &p->childContexts;
         p->childContexts = this;
-        ownedByParent = parentTakesOwnership;
     }
 }
 
@@ -658,6 +677,10 @@ void QQmlContextData::refreshExpressionsRecursive(QQmlJavaScriptExpression *expr
 
     if (!w.wasDeleted())
         expression->refresh();
+}
+
+QQmlContextData::~QQmlContextData()
+{
 }
 
 static inline bool expressions_to_run(QQmlContextData *ctxt, bool isGlobalRefresh)

src/qml/qml/qqmltypeloader.cpp
@@ -2817,7 +2817,7 @@ QV4::ReturnedValue QQmlScriptData::scriptValueForContext(QQmlContextData *parent
         effectiveCtxt = 0;
 
     // Create the script context if required
-    QQmlContextData *ctxt = new QQmlContextData;
+    QQmlContextDataRef ctxt(new QQmlContextData);
     ctxt->isInternal = true;
     ctxt->isJSContext = true;
     if (shared)
@@ -2837,7 +2837,7 @@ QV4::ReturnedValue QQmlScriptData::scriptValueForContext(QQmlContextData *parent
     }
 
     if (effectiveCtxt) {
-        ctxt->setParent(effectiveCtxt, true);
+        ctxt->setParent(effectiveCtxt);
     } else {
         ctxt->engine = parentCtxt->engine; // Fix for QTBUG-21620
     }
@@ -2859,12 +2859,10 @@ QV4::ReturnedValue QQmlScriptData::scriptValueForContext(QQmlContextData *parent
     if (!m_program) {
         if (shared)
             m_loaded = true;
-        ctxt->destroy();
         return QV4::Encode::undefined();
     }
 
     QV4::Scoped<QV4::QmlContext> qmlContext(scope, QV4::QmlContext::create(v4->rootContext(), ctxt, 0));
-    qmlContext->takeContextOwnership();
 
     m_program->qmlContext.set(scope.engine, qmlContext);
     m_program->run();

tests/auto/qml/qqmlcontext/tst_qqmlcontext.cpp
@@ -62,6 +62,7 @@ private slots:
     void evalAfterInvalidate();
     void qobjectDerived();
     void qtbug_49232();
+    void contextViaClosureAfterDestruction();
 
 private:
     QQmlEngine engine;
@@ -721,6 +722,33 @@ void tst_qqmlcontext::qtbug_49232()
 
     QCOMPARE(obj->property("valueOne"), QVariant('a'));
     QCOMPARE(obj->property("valueTwo"), QVariant(97));
+}
+
+void tst_qqmlcontext::contextViaClosureAfterDestruction()
+{
+    qmlRegisterSingletonType(testFileUrl("Singleton.qml"), "constants", 1, 0, "Sing");
+    QQmlEngine engine;
+    QQmlComponent component(&engine, testFileUrl("contextViaClosureAfterDestruction.qml"));
+    QJSValue valueClosure;
+    QJSValue componentFactoryClosure;
+    {
+        QScopedPointer<QObject> obj(component.create());
+        QVERIFY(!obj.isNull());
+        // meta-calls don't support QJSValue return types, so do the call "by hand"
+        valueClosure = engine.newQObject(obj.data()).property(QStringLiteral("createClosure")).call();
+        QVERIFY(valueClosure.isCallable());
+        componentFactoryClosure = engine.newQObject(obj.data()).property(QStringLiteral("createComponentFactory")).call();
+        QVERIFY(componentFactoryClosure.isCallable());
+    }
+    QCOMPARE(valueClosure.call().toString(), QLatin1String("Highway to Hell"));
+
+    QScopedPointer<QObject> parent(new QObject);
+    QJSValue parentWrapper = engine.newQObject(parent.data());
+    QQmlEngine::setObjectOwnership(parent.data(), QQmlEngine::CppOwnership);
+
+    QJSValue subObject = componentFactoryClosure.callWithInstance(componentFactoryClosure, QJSValueList() << parentWrapper);
+    QVERIFY(subObject.isError());
+    QCOMPARE(subObject.toString(), QLatin1String("Error: Qt.createQmlObject(): Cannot create a component in an invalid context"));
 }
 
 QTEST_MAIN(tst_qqmlcontext)

src/qml/jsruntime/qv4qmlcontext_p.h
@@ -67,13 +67,12 @@ struct QmlContextWrapper;
 namespace Heap {
 
 struct QmlContextWrapper : Object {
-    void init(QQmlContextData *context, QObject *scopeObject, bool ownsContext = false);
+    void init(QQmlContextData *context, QObject *scopeObject);
     void destroy();
     bool readOnly;
-    bool ownsContext;
     bool isNullWrapper;
 
-    QQmlGuardedContextData *context;
+    QQmlContextDataRef *context;
     QQmlQPointer<QObject> scopeObject;
 };
 
@@ -89,10 +88,6 @@ struct Q_QML_EXPORT QmlContextWrapper : Object
 {
     V4_OBJECT2(QmlContextWrapper, Object)
     V4_NEEDS_DESTROY
-
-    void takeContextOwnership() {
-        d()->ownsContext = true;
-    }
 
     inline QObject *getScopeObject() const { return d()->scopeObject; }
     inline QQmlContextData *getContext() const { return *d()->context; }
@@ -115,10 +110,6 @@ struct Q_QML_EXPORT QmlContext : public ExecutionContext
     }
     QQmlContextData *qmlContext() const {
         return *d()->qml->context;
-    }
-
-    void takeContextOwnership() {
-        d()->qml->ownsContext = true;
     }
 };
 

src/qml/jsruntime/qv4qobjectwrapper.cpp
@@ -977,8 +977,12 @@ void QObjectWrapper::destroyObject(bool lastCall)
         QQmlData *ddata = QQmlData::get(h->object(), false);
         if (ddata) {
             if (!h->object()->parent() && !ddata->indestructible) {
-                if (ddata && ddata->ownContext && ddata->context)
-                    ddata->context->emitDestruction();
+                if (ddata && ddata->ownContext) {
+                    Q_ASSERT(ddata->ownContext == ddata->context);
+                    ddata->ownContext->emitDestruction();
+                    ddata->ownContext = 0;
+                    ddata->context = 0;
+                }
                 // This object is notionally destroyed now
                 ddata->isQueuedForDeletion = true;
                 if (lastCall)

src/qml/qml/qqmlcontext_p.h
@@ -78,6 +78,7 @@ class QQmlExpression;
 class QQmlExpressionPrivate;
 class QQmlJavaScriptExpression;
 class QQmlContextData;
+class QQmlGuardedContextData;
 class QQmlIncubatorPrivate;
 
 class QQmlContextPrivate : public QObjectPrivate
@@ -106,7 +107,7 @@ public:
 };
 
 class QQmlComponentAttached;
-class QQmlGuardedContextData;
+
 class Q_QML_PRIVATE_EXPORT QQmlContextData
 {
 public:
@@ -114,7 +115,6 @@ public:
     QQmlContextData(QQmlContext *);
     void emitDestruction();
     void clearContext();
-    void destroy();
     void invalidate();
 
     inline bool isValid() const {
@@ -122,10 +122,10 @@ public:
     }
 
     // My parent context and engine
-    QQmlContextData *parent;
+    QQmlContextData *parent = nullptr;
     QQmlEngine *engine;
 
-    void setParent(QQmlContextData *, bool parentTakesOwnership = false);
+    void setParent(QQmlContextData *);
     void refreshExpressions();
 
     void addObject(QObject *);
@@ -136,14 +136,14 @@ public:
     // If internal is false publicContext owns this.
     QQmlContext *asQQmlContext();
     QQmlContextPrivate *asQQmlContextPrivate();
+    quint32 refCount = 0;
     quint32 isInternal:1;
-    quint32 ownedByParent:1; // unrelated to isInternal; parent context deletes children if true.
     quint32 isJSContext:1;
     quint32 isPragmaLibraryContext:1;
     quint32 unresolvedNames:1; // True if expressions in this context failed to resolve a toplevel name
     quint32 hasEmittedDestruction:1;
     quint32 isRootObjectInCreation:1;
-    quint32 dummy:25;
+    quint32 dummy:26;
     QQmlContext *publicContext;
 
     // The incubator that is constructing this context if any
@@ -178,7 +178,7 @@ public:
     QQmlRefPointer<QQmlTypeNameCache> imports;
 
     // My children
-    QQmlContextData *childContexts;
+    QQmlContextData *childContexts = 0;
 
     // My peers in parent's childContexts list
     QQmlContextData  *nextChild;
@@ -191,7 +191,7 @@ public:
     QQmlData *contextObjects;
 
     // Doubly-linked list of context guards (XXX merge with contextObjects)
-    QQmlGuardedContextData *contextGuards;
+    QQmlGuardedContextData *contextGuards = 0;
 
     // id guards
     struct ContextGuard : public QQmlGuard<QObject>
@@ -210,7 +210,7 @@ public:
     void setIdProperty(int, QObject *);
 
     // Linked contexts. this owns linkedContext.
-    QQmlContextData *linkedContext;
+    QQmlContextDataRef linkedContext;
 
     // Linked list of uses of the Component attached property in this
     // context
@@ -224,57 +224,53 @@ public:
     }
 
 private:
+    friend class QQmlContextDataRef;
+    friend class QQmlContext; // needs to do manual refcounting :/
     void refreshExpressionsRecursive(bool isGlobal);
     void refreshExpressionsRecursive(QQmlJavaScriptExpression *);
-    ~QQmlContextData() {}
+    ~QQmlContextData();
+    void destroy();
 };
+
 
 class QQmlGuardedContextData
 {
 public:
-    inline QQmlGuardedContextData();
-    inline QQmlGuardedContextData(QQmlContextData *);
-    inline ~QQmlGuardedContextData();
+    inline QQmlGuardedContextData() = default;
+    inline QQmlGuardedContextData(QQmlContextData *data)
+    { setContextData(data); }
+    inline ~QQmlGuardedContextData()
+    { clear(); }
 
-    inline QQmlContextData *contextData() const;
+    inline QQmlContextData *contextData() const
+    { return m_contextData; }
     inline void setContextData(QQmlContextData *);
 
     inline bool isNull() const { return !m_contextData; }
 
     inline operator QQmlContextData*() const { return m_contextData; }
     inline QQmlContextData* operator->() const { return m_contextData; }
-    inline QQmlGuardedContextData &operator=(QQmlContextData *d);
+    inline QQmlGuardedContextData &operator=(QQmlContextData *d) {
+        setContextData(d); return *this;
+    }
 
 private:
-    QQmlGuardedContextData &operator=(const QQmlGuardedContextData &);
-    QQmlGuardedContextData(const QQmlGuardedContextData &);
+    QQmlGuardedContextData &operator=(const QQmlGuardedContextData &) = delete;
+    QQmlGuardedContextData(const QQmlGuardedContextData &) = delete;
     friend class QQmlContextData;
 
     inline void clear();
 
-    QQmlContextData *m_contextData;
-    QQmlGuardedContextData  *m_next;
-    QQmlGuardedContextData **m_prev;
+    QQmlContextData *m_contextData = 0;
+    QQmlGuardedContextData  *m_next = 0;
+    QQmlGuardedContextData **m_prev = 0;
 };
 
-QQmlGuardedContextData::QQmlGuardedContextData()
-: m_contextData(0), m_next(0), m_prev(0)
-{
-}
-
-QQmlGuardedContextData::QQmlGuardedContextData(QQmlContextData *data)
-: m_contextData(0), m_next(0), m_prev(0)
-{
-    setContextData(data);
-}
-
-QQmlGuardedContextData::~QQmlGuardedContextData()
-{
-    clear();
-}
 
 void QQmlGuardedContextData::setContextData(QQmlContextData *contextData)
-{
+ {
+    if (m_contextData == contextData)
+        return;
     clear();
 
     if (contextData) {
@@ -284,11 +280,6 @@ void QQmlGuardedContextData::setContextData(QQmlContextData *contextData)
         m_prev = &contextData->contextGuards;
         contextData->contextGuards = this;
     }
-}
-
-QQmlContextData *QQmlGuardedContextData::contextData() const
-{
-    return m_contextData;
 }
 
 void QQmlGuardedContextData::clear()
@@ -302,10 +293,65 @@ void QQmlGuardedContextData::clear()
     }
 }
 
-QQmlGuardedContextData &
-QQmlGuardedContextData::operator=(QQmlContextData *d)
+QQmlContextDataRef::QQmlContextDataRef()
+    : m_contextData(0)
+{
+}
+
+QQmlContextDataRef::QQmlContextDataRef(const QQmlContextDataRef &other)
+    : m_contextData(other.m_contextData)
+{
+    if (m_contextData)
+        ++m_contextData->refCount;
+}
+
+QQmlContextDataRef::QQmlContextDataRef(QQmlContextData *data)
+    : m_contextData(data)
+{
+    if (m_contextData)
+        ++m_contextData->refCount;
+}
+
+QQmlContextDataRef::~QQmlContextDataRef()
+{
+    clear();
+}
+
+void QQmlContextDataRef::setContextData(QQmlContextData *contextData)
+{
+    if (m_contextData == contextData)
+        return;
+    clear();
+
+    if (contextData) {
+        m_contextData = contextData;
+        ++m_contextData->refCount;
+    }
+}
+
+QQmlContextData *QQmlContextDataRef::contextData() const
+{
+    return m_contextData;
+}
+
+void QQmlContextDataRef::clear()
+{
+    if (m_contextData && !--m_contextData->refCount)
+        m_contextData->destroy();
+    m_contextData = 0;
+}
+
+QQmlContextDataRef &
+QQmlContextDataRef::operator=(QQmlContextData *d)
 {
     setContextData(d);
+    return *this;
+}
+
+QQmlContextDataRef &
+QQmlContextDataRef::operator=(const QQmlContextDataRef &other)
+{
+    setContextData(other.m_contextData);
     return *this;
 }
 

src/qml/qml/qqmldata_p.h
@@ -78,6 +78,34 @@ struct CompilationUnit;
 }
 }
 
+// This is declared here because QQmlData below needs it and this file
+// in turn is included from qqmlcontext_p.h.
+class QQmlContextData;
+class Q_QML_PRIVATE_EXPORT QQmlContextDataRef
+{
+public:
+    inline QQmlContextDataRef();
+    inline QQmlContextDataRef(QQmlContextData *);
+    inline QQmlContextDataRef(const QQmlContextDataRef &);
+    inline ~QQmlContextDataRef();
+
+    inline QQmlContextData *contextData() const;
+    inline void setContextData(QQmlContextData *);
+
+    inline bool isNull() const { return !m_contextData; }
+
+    inline operator QQmlContextData*() const { return m_contextData; }
+    inline QQmlContextData* operator->() const { return m_contextData; }
+    inline QQmlContextDataRef &operator=(QQmlContextData *d);
+    inline QQmlContextDataRef &operator=(const QQmlContextDataRef &other);
+
+private:
+
+    inline void clear();
+
+    QQmlContextData *m_contextData;
+};
+
 // This class is structured in such a way, that simply zero'ing it is the
 // default state for elemental object allocations.  This is crucial in the
 // workings of the QQmlInstruction::CreateSimpleObject instruction.
@@ -114,7 +142,6 @@ public:
 
     quint32 ownedByQml1:1; // This bit is shared with QML1's QDeclarativeData.
     quint32 ownMemory:1;
-    quint32 ownContext:1;
     quint32 indestructible:1;
     quint32 explicitIndestructibleSet:1;
     quint32 hasTaintedV4Object:1;
@@ -127,7 +154,7 @@ public:
     quint32 hasInterceptorMetaObject:1;
     quint32 hasVMEMetaObject:1;
     quint32 parentFrozen:1;
-    quint32 dummy:21;
+    quint32 dummy:22;
 
     // When bindingBitsSize < sizeof(ptr), we store the binding bit flags inside
     // bindingBitsValue. When we need more than sizeof(ptr) bits, we allocated
@@ -161,9 +188,10 @@ public:
     void disconnectNotifiers();
 
     // The context that created the C++ object
-    QQmlContextData *context;
+    QQmlContextData *context = 0;
     // The outermost context in which this object lives
-    QQmlContextData *outerContext;
+    QQmlContextData *outerContext = 0;
+    QQmlContextDataRef ownContext;
 
     QQmlAbstractBinding *bindings;
     QQmlBoundSignal *signalHandlers;

commit d4ff2907162e20e2288357c428b0df88f7396f92

Fix memory leak with JS imports
Strictly speaking this is a regression introduced with commit
e22b624d9ab1f36021adb9cdbfa9b37054282bb8, making the QQmlContextData
objects reference counted, especially from the V4 QML context wrapper
objects.

That change (correct as it is) introduced an accidental circular
dependency in the simple scenario of importing a .js file in a .qml
file:

Each time the type in the .qml file is instantiated, we create a
dedicated QQmlContextData for the .js file. If the .js file has no
imports itself, that new context will get the same ctx->importedScripts
JS array as the QML context of the .qml file. That is a strong reference
via QV4::PersistentValue. That array in turn contains the
QV4::QmlContextWrapper that belongs to the imported script, which in
turn holds a strong reference (via refcount) to the script's context.

This patch breaks the circular reference when we perform context
invalidation, as the least intrusive measure.

For the auto-test to work, we must also clear the qmlContext persistent
of the QV4::Script that's used to evaluate the .js file. In subsequent
imports that persistent will be initialized to new values, so it will
only hold a strong reference to the last import, but strictly speaking
that is still a leak - hence also part of this fix.

Change-Id: I3e543c946e5e683425072dc3df7e49ca0e0c0215
Task-number: QTBUG-66189
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/qml/jsruntime/qv4qmlcontext.cpp
@@ -138,7 +138,9 @@ ReturnedValue QmlContextWrapper::get(const Managed *m, String *name, bool *hasPr
                 *hasProperty = true;
             if (r.scriptIndex != -1) {
                 QV4::ScopedObject scripts(scope, context->importedScripts.valueRef());
-                return scripts->getIndexed(r.scriptIndex);
+                if (scripts)
+                    return scripts->getIndexed(r.scriptIndex);
+                return QV4::Encode::null();
             } else if (r.type.isValid()) {
                 return QmlTypeWrapper::create(v4, scopeObject, r.type);
             } else if (r.importNamespace) {

src/qml/qml/qqmlcontext.cpp
@@ -579,6 +579,8 @@ void QQmlContextData::invalidate()
         prevChild = 0;
     }
 
+    importedScripts.clear();
+
     engine = 0;
     parent = 0;
 }

src/qml/qml/qqmltypeloader.cpp
@@ -2917,6 +2917,7 @@ QV4::ReturnedValue QQmlScriptData::scriptValueForContext(QQmlContextData *parent
 
     m_program->qmlContext.set(scope.engine, qmlContext);
     m_program->run();
+    m_program->qmlContext.clear();
     if (scope.engine->hasException) {
         QQmlError error = scope.engine->catchExceptionAsQmlError();
         if (error.isValid())

tests/auto/qml/qqmlcontext/data/ContextLeak.js
@@ -1,0 +1,1 @@
+var value = 42

tests/auto/qml/qqmlcontext/data/contextLeak.qml
@@ -1,0 +1,5 @@
+import QtQml 2.0
+import "ContextLeak.js" as ContextLeak
+QtObject {
+    property int value: ContextLeak.value
+}

tests/auto/qml/qqmlcontext/tst_qqmlcontext.cpp
@@ -33,6 +33,8 @@
 #include <QQmlComponent>
 #include <QQmlExpression>
 #include <private/qqmlcontext_p.h>
+#include <private/qv4qmlcontext_p.h>
+#include <private/qv4object_p.h>
 #include "../../shared/util.h"
 
 class tst_qqmlcontext : public QQmlDataTest
@@ -63,6 +65,7 @@ private slots:
     void qobjectDerived();
     void qtbug_49232();
     void contextViaClosureAfterDestruction();
+    void contextLeak();
 
 private:
     QQmlEngine engine;
@@ -749,6 +752,40 @@ void tst_qqmlcontext::contextViaClosureAfterDestruction()
     QJSValue subObject = componentFactoryClosure.callWithInstance(componentFactoryClosure, QJSValueList() << parentWrapper);
     QVERIFY(subObject.isError());
     QCOMPARE(subObject.toString(), QLatin1String("Error: Qt.createQmlObject(): Cannot create a component in an invalid context"));
+}
+
+void tst_qqmlcontext::contextLeak()
+{
+    QQmlEngine engine;
+    QQmlComponent component(&engine, testFileUrl("contextLeak.qml"));
+
+    QQmlGuardedContextData scriptContext;
+
+    {
+        QScopedPointer<QObject> obj(component.create());
+        QVERIFY(!obj.isNull());
+        QCOMPARE(obj->property("value").toInt(), 42);
+
+        QQmlData *ddata = QQmlData::get(obj.data());
+        QVERIFY(ddata);
+        QQmlContextData *context = ddata->context;
+        QVERIFY(context);
+        QVERIFY(!context->importedScripts.isNullOrUndefined());
+        QCOMPARE(int(context->importedScripts.valueRef()->as<QV4::Object>()->getLength()), 1);
+
+        {
+            QV4::Scope scope(ddata->jsWrapper.engine());
+            QV4::ScopedValue scriptContextWrapper(scope);
+            scriptContextWrapper = context->importedScripts.valueRef()->as<QV4::Object>()->getIndexed(0);
+            scriptContext = scriptContextWrapper->as<QV4::QmlContextWrapper>()->getContext();
+        }
+    }
+
+    engine.collectGarbage();
+
+    // Each time a JS file (non-pragma-shared) is imported, we create a QQmlContext(Data) for it.
+    // Make sure that context does not leak.
+    QVERIFY(scriptContext.isNull());
 }
 
 QTEST_MAIN(tst_qqmlcontext)