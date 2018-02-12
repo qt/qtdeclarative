qt/qtdeclarative

commit 3cc589c98390992e3ee8a7970dc2913ea857d623

Make sure we destroy all QV4::QObjectWrapper objects
Delay freeing QObjectWrapper Value to MemoryManager::sweep() to make sure
we can destroy all QObjectWrapper objects.
We also keep track of QObjectWrapper in QV4::Heap::ModelObject to make
sure we destory them in QV4::MemoryManager::sweep()

Change-Id: I3a8a3b07faab1f88c2eb746f68aa8d9584b40026
Reviewed-by: Simon Hausmann <simon.hausmann@theqtcompany.com>

src/qml/types/qqmllistmodel.cpp
@@ -2277,6 +2277,9 @@ QQmlV4Handle QQmlListModel::get(int index) const
         } else {
             QObject *object = m_listModel->getOrCreateModelObject(const_cast<QQmlListModel *>(this), index);
             result = scope.engine->memoryManager->allocObject<QV4::ModelObject>(object, const_cast<QQmlListModel *>(this), index);
+            // Keep track of the QObjectWrapper in persistent value storage
+            QV4::Value *val = scope.engine->memoryManager->m_weakValues->allocate();
+            *val = result;
         }
     }
 

src/qml/jsruntime/qv4persistent.cpp
@@ -34,6 +34,7 @@
 #include "qv4persistent_p.h"
 #include <private/qv4mm_p.h>
 #include "qv4object_p.h"
+#include "qv4qobjectwrapper_p.h"
 #include "PageAllocation.h"
 
 using namespace QV4;
@@ -381,7 +382,7 @@ WeakValue &WeakValue::operator=(const WeakValue &other)
 
 WeakValue::~WeakValue()
 {
-    PersistentValueStorage::free(val);
+    free();
 }
 
 void WeakValue::set(ExecutionEngine *engine, const Value &value)
@@ -410,5 +411,23 @@ void WeakValue::markOnce(ExecutionEngine *e)
     if (!val)
         return;
     val->mark(e);
+}
+
+void WeakValue::free()
+{
+    if (!val)
+        return;
+
+    ExecutionEngine *e = engine();
+    if (e && val->as<QObjectWrapper>()) {
+        // Some QV4::QObjectWrapper Value will be freed in WeakValue::~WeakValue() before MemoryManager::sweep() is being called,
+        // in this case we will never have a chance to call detroyObject() on those QV4::QObjectWrapper objects.
+        // Here we don't free these Value immediately, instead we keep track of them to free them later in MemoryManager::sweep()
+        e->memoryManager->m_pendingFreedObjectWrapperValue.push_back(val);
+    } else {
+        PersistentValueStorage::free(val);
+    }
+
+    val = 0;
 }
 

src/qml/jsruntime/qv4persistent_p.h
@@ -178,15 +178,15 @@ public:
 
     bool isUndefined() const { return !val || val->isUndefined(); }
     bool isNullOrUndefined() const { return !val || val->isNullOrUndefined(); }
-    void clear() {
-        PersistentValueStorage::free(val);
-        val = 0;
-    }
+    void clear() { free(); }
 
     void markOnce(ExecutionEngine *e);
 
 private:
     Value *val;
+
+private:
+    void free();
 };
 
 } // namespace QV4

src/qml/memory/qv4mm.cpp
@@ -41,8 +41,6 @@
 #include "StdLibExtras.h"
 
 #include <QTime>
-#include <QVector>
-#include <QVector>
 #include <QMap>
 
 #include <iostream>
@@ -435,6 +433,21 @@ void MemoryManager::sweep(bool lastSweep)
             qobjectWrapper->destroyObject(lastSweep);
 
         (*it) = Primitive::undefinedValue();
+    }
+
+    // Now it is time to free QV4::QObjectWrapper Value, we must check the Value's tag to make sure its object has been destroyed
+    const int pendingCount = m_pendingFreedObjectWrapperValue.count();
+    if (pendingCount) {
+        QVector<Value *> remainingWeakQObjectWrappers;
+        remainingWeakQObjectWrappers.reserve(pendingCount);
+        for (int i = 0; i < pendingCount; ++i) {
+            Value *v = m_pendingFreedObjectWrapperValue.at(i);
+            if (v->tag() == Value::Undefined_Type)
+                PersistentValueStorage::free(v);
+            else
+                remainingWeakQObjectWrappers.append(v);
+        }
+        m_pendingFreedObjectWrapperValue = remainingWeakQObjectWrappers;
     }
 
     if (MultiplyWrappedQObjectMap *multiplyWrappedQObjects = engine->m_multiplyWrappedQObjects) {

src/qml/memory/qv4mm_p.h
@@ -49,6 +49,7 @@
 #include <private/qv4value_p.h>
 #include <private/qv4scopedvalue_p.h>
 #include <private/qv4object_p.h>
+#include <QVector>
 
 //#define DETAILED_MM_STATS
 
@@ -327,6 +328,7 @@ public:
     QScopedPointer<Data> m_d;
     PersistentValueStorage *m_persistentValues;
     PersistentValueStorage *m_weakValues;
+    QVector<Value *> m_pendingFreedObjectWrapperValue;
 };
 
 }

commit 22d43f74e264626d0c28654c42c91839f9de45b5

Fix memory leak with ListModel.get
This is a regression introduced with commit
3cc589c98390992e3ee8a7970dc2913ea857d623, which in turn fixed a leak
with QV4::QObjectWrapper objects. Unfortunately the allocate() call into
the persistent (weak) value storage in the list model introduced a leak
of the weak value itself. This is fixed by replacing the free standing
weak value allocation with the use of the existing jsWrapper weak value
in the declarative data (QQmlData). That weak value is freed property in
the destroy() method of the QV4::QObjectWRapper. The extra QQmlData
allocation is hidden behind a unified allocation, similar to what we do
in void QQmlType::create(QObject **, void **, size_t) const.

Task-number: QTBUG-66189
Change-Id: I5351e3e484542709a6b210e84aa19b14d28e11ad
Reviewed-by: Lars Knoll <lars.knoll@qt.io>

src/qml/types/qqmllistmodel.cpp
@@ -265,7 +265,12 @@ QObject *ListModel::getOrCreateModelObject(QQmlListModel *model, int elementInde
 {
     ListElement *e = elements[elementIndex];
     if (e->m_objectCache == 0) {
-        e->m_objectCache = new QObject;
+        void *memory = operator new(sizeof(QObject) + sizeof(QQmlData));
+        void *ddataMemory = ((char *)memory) + sizeof(QObject);
+        e->m_objectCache = new (memory) QObject;
+        QQmlData *ddata = new (ddataMemory) QQmlData;
+        ddata->ownMemory = false;
+        QObjectPrivate::get(e->m_objectCache)->declarativeData = ddata;
         (void)new ModelNodeMetaObject(e->m_objectCache, model, elementIndex);
     }
     return e->m_objectCache;
@@ -2428,8 +2433,7 @@ QQmlV4Handle QQmlListModel::get(int index) const
             QObject *object = m_listModel->getOrCreateModelObject(const_cast<QQmlListModel *>(this), index);
             result = scope.engine->memoryManager->allocObject<QV4::ModelObject>(object, const_cast<QQmlListModel *>(this), index);
             // Keep track of the QObjectWrapper in persistent value storage
-            QV4::Value *val = scope.engine->memoryManager->m_weakValues->allocate();
-            *val = result;
+            QQmlData::get(object)->jsWrapper.set(scope.engine, result);
         }
     }
 

tests/auto/qml/qqmllistmodel/tst_qqmllistmodel.cpp
@@ -124,6 +124,7 @@ private slots:
     void modify_through_delegate();
     void bindingsOnGetResult();
     void stringifyModelEntry();
+    void qobjectTrackerForDynamicModelObjects();
 };
 
 bool tst_qqmllistmodel::compareVariantList(const QVariantList &testList, QVariant object)
@@ -1503,6 +1504,33 @@ void tst_qqmllistmodel::stringifyModelEntry()
     QVERIFY2(!expr.hasError(), QTest::toString(expr.error().toString()));
     const QString expectedString = QStringLiteral("{\"age\":22,\"name\":\"Joe\"}");
     QCOMPARE(v.toString(), expectedString);
+}
+
+void tst_qqmllistmodel::qobjectTrackerForDynamicModelObjects()
+{
+    QQmlEngine engine;
+    QQmlComponent component(&engine);
+    component.setData(
+                      "import QtQuick 2.0\n"
+                      "Item {\n"
+                      "   ListModel {\n"
+                      "       id: testModel\n"
+                      "       objectName: \"testModel\"\n"
+                      "       ListElement { name: \"Joe\"; age: 22 }\n"
+                      "   }\n"
+                      "}\n", QUrl());
+    QScopedPointer<QObject> scene(component.create());
+    QQmlListModel *model = scene->findChild<QQmlListModel*>("testModel");
+    QQmlExpression expr(engine.rootContext(), model, "get(0);");
+    QVariant v = expr.evaluate();
+    QVERIFY2(!expr.hasError(), QTest::toString(expr.error().toString()));
+
+    QObject *obj = v.value<QObject*>();
+    QVERIFY(obj);
+
+    QQmlData *ddata = QQmlData::get(obj, /*create*/false);
+    QVERIFY(ddata);
+    QVERIFY(!ddata->jsWrapper.isNullOrUndefined());
 }
 
 QTEST_MAIN(tst_qqmllistmodel)