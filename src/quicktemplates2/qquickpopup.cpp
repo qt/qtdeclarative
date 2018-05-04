qt/qtdeclarative

commit bcd1bed0b49e4ea8174bd323b6ad1f613abc0b13

QQuickPopupPositioner: use QQuickItem::isAncestorOf()
It was introduced in Qt 5.7, but wasn't taken into use yet...

Change-Id: Ib18f7e09a2de7bcd8fa51fd272cd555dc83103c9
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quicktemplates2/qquickpopuppositioner.cpp
@@ -102,7 +102,7 @@ void QQuickPopupPositioner::itemParentChanged(QQuickItem *, QQuickItem *parent)
 
 void QQuickPopupPositioner::itemChildRemoved(QQuickItem *item, QQuickItem *child)
 {
-    if (isAncestor(child))
+    if (child->isAncestorOf(m_parentItem))
         removeAncestorListeners(item);
 }
 
@@ -128,21 +128,6 @@ void QQuickPopupPositioner::addAncestorListeners(QQuickItem *item)
         QQuickItemPrivate::get(p)->addItemChangeListener(this, AncestorChangeTypes);
         p = p->parentItem();
     }
-}
-
-// TODO: use QQuickItem::isAncestorOf() in dev/5.7
-bool QQuickPopupPositioner::isAncestor(QQuickItem *item) const
-{
-    if (!m_parentItem)
-        return false;
-
-    QQuickItem *parent = m_parentItem;
-    while (parent) {
-        if (parent == item)
-            return true;
-        parent = parent->parentItem();
-    }
-    return false;
 }
 
 QT_END_NAMESPACE

src/quicktemplates2/qquickpopuppositioner_p_p.h
@@ -74,8 +74,6 @@ private:
     void removeAncestorListeners(QQuickItem *item);
     void addAncestorListeners(QQuickItem *item);
 
-    bool isAncestor(QQuickItem *item) const;
-
     QQuickItem *m_parentItem;
     QQuickPopupPrivate *m_popup;
 };

commit a5d2731b1ec56580a79784759307d3b3c21ab87f

Fix QQuickPopupPositioner
This is a double-regression caused by commits bcd1bed and 03a8c88. Both
of these changes caused dangling pointers being left on the list of item
change listeners.

First, bcd1bed made QQuickPopupPositioner use QQuickItem::isAncestorOf()
without noticing the difference that the old self-made isAncestorOf()
conveniently returned true for the item itself, whereas the new built-in
QQuickItem::isAncestorOf() doesn't. This had a nasty side effect that
when the popup's parent item was removed from its parent,
QQuickPopupPositioner::itemChildRemoved() no longer removed the ancestor
listeners.

Then, 03a8c88 made things worse by changing the stack allocation of
QQuickPopupPositioner to heap allocation without ever deleting the
positioner object.

Valgrind output for tst_tooltip:

    ==14391== Invalid read of size 8
    ==14391==    at 0x79EFB9A: QQuickItemPrivate::itemChange(QQuickItem::ItemChange, QQuickItem::ItemChangeData const&) (qquickitem.cpp:6206)
    ==14391==    by 0x79F3D98: QQuickItem::setParentItem(QQuickItem*) (qquickitem.cpp:2791)
    ==14391==    by 0x79F3E87: QQuickItem::~QQuickItem() (qquickitem.cpp:2385)
    ==14391==    by 0x7A16073: ~QQmlElement (qqmlprivate.h:103)
    ==14391==    by 0x7A16073: QQmlPrivate::QQmlElement<QQuickItem>::~QQmlElement() (qqmlprivate.h:103)
    ==14391==    by 0x7A9CFEE: QQuickView::~QQuickView() (qquickview.cpp:218)
    ==14391==    by 0x4038215: quick_test_main_with_setup(int, char**, char const*, char const*, QObject*) (quicktest.cpp:512)
    ==14391==    by 0x4038CFF: quick_test_main(int, char**, char const*, char const*) (quicktest.cpp:330)
    ==14391==    by 0x400BD7: main (tst_default.cpp:44)
    ==14391==  Address 0x1f2d3f10 is 0 bytes inside a block of size 32 free'd
    ==14391==    at 0x4C2F29C: operator delete(void*) (vg_replace_malloc.c:576)
    ==14391==    by 0x29CA2641: QQuickPopupPositioner::~QQuickPopupPositioner() (qquickpopuppositioner.cpp:68)

src/quicktemplates2/qquickpopup.cpp
@@ -775,6 +775,8 @@ QQuickPopup::~QQuickPopup()
     d->popupItem->ungrabShortcut();
     delete d->popupItem;
     d->popupItem = nullptr;
+    delete d->positioner;
+    d->positioner = nullptr;
 }
 
 /*!

src/quicktemplates2/qquickpopuppositioner.cpp
@@ -248,7 +248,7 @@ void QQuickPopupPositioner::itemParentChanged(QQuickItem *, QQuickItem *parent)
 
 void QQuickPopupPositioner::itemChildRemoved(QQuickItem *item, QQuickItem *child)
 {
-    if (child->isAncestorOf(m_parentItem))
+    if (child == m_parentItem || child->isAncestorOf(m_parentItem))
         removeAncestorListeners(item);
 }
 