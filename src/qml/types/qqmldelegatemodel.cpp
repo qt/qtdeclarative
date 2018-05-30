edc191f85bf12bf6d036b2979aa76fc3edb18dab
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2018-05-30 15:16:21 +0200

    QQmlDelegateModelPrivate: refactor release()
    Change the structure a bit, so it becomes a little
    less nested, and to make some "clean" space for the
    patch that handles recycling if items, which will
    add an extra section into this function in a
    subsequent patch.
    
    Change-Id: Ib94142827373d6894b508718f58e9f5e61e8c8e1
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>

--- src/qml/types/qqmldelegatemodel.cpp
+++ src/qml/types/qqmldelegatemodel.cpp
@@ -565,25 +565,24 @@ int QQmlDelegateModel::count() const
 
 QQmlDelegateModel::ReleaseFlags QQmlDelegateModelPrivate::release(QObject *object)
 {
-    QQmlDelegateModel::ReleaseFlags stat = nullptr;
     if (!object)
-        return stat;
+        return QQmlDelegateModel::ReleaseFlags(0);
 
-    if (QQmlDelegateModelItem *cacheItem = QQmlDelegateModelItem::dataForObject(object)) {
-        if (cacheItem->releaseObject()) {
-            cacheItem->destroyObject();
-            emitDestroyingItem(object);
-            if (cacheItem->incubationTask) {
-                releaseIncubator(cacheItem->incubationTask);
-                cacheItem->incubationTask = nullptr;
-            }
-            cacheItem->Dispose();
-            stat |= QQmlInstanceModel::Destroyed;
-        } else {
-            stat |= QQmlDelegateModel::Referenced;
-        }
+    QQmlDelegateModelItem *cacheItem = QQmlDelegateModelItem::dataForObject(object);
+    if (!cacheItem)
+        return QQmlDelegateModel::ReleaseFlags(0);
+
+    if (!cacheItem->releaseObject())
+        return QQmlDelegateModel::Referenced;
+
+    cacheItem->destroyObject();
+    emitDestroyingItem(object);
+    if (cacheItem->incubationTask) {
+        releaseIncubator(cacheItem->incubationTask);
+        cacheItem->incubationTask = nullptr;
     }
-    return stat;
+    cacheItem->Dispose();
+    return QQmlInstanceModel::Destroyed;
 }
 
 /*
