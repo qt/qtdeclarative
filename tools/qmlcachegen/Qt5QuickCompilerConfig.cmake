qt/qtdeclarative

commit 8735ff6499f4ef93675a9e8158af77461f6e93bc

CMake: replace deprecated exec_program() with execute_process()
Change-Id: Ic3fc1a59703c11aab17529d4b0e92789e4650ca7
Reviewed-by: Kevin Funk <kevin.funk@kdab.com>
Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

tools/qmlcachegen/Qt5QuickCompilerConfig.cmake
@@ -44,7 +44,7 @@ function(QTQUICK_COMPILER_ADD_RESOURCES outfiles)
 
         set(rcc_file_with_compilation_units)
 
-        exec_program(${rcc_path} ARGS -list \"${input_resource}\" OUTPUT_VARIABLE rcc_contents)
+        execute_process(COMMAND ${rcc_path} -list \"${input_resource}\" OUTPUT_VARIABLE rcc_contents)
         string(REGEX REPLACE "[\r\n]+" ";" rcc_contents ${rcc_contents})
         foreach(it ${rcc_contents})
             get_filename_component(extension ${it} EXT)

commit 053e76ab80f72eb026894fc05ee83593ea2079fd

CMake: fix bad argument quoting when calling rcc
Fixes: 8735ff6499f4ef93675a9e8158af77461f6e93bc

Change-Id: If8a3a394e52027fe99acc98ea8f4b7847db81497
Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
Reviewed-by: Kevin Funk <kevin.funk@kdab.com>

tools/qmlcachegen/Qt5QuickCompilerConfig.cmake
@@ -44,7 +44,7 @@ function(QTQUICK_COMPILER_ADD_RESOURCES outfiles)
 
         set(rcc_file_with_compilation_units)
 
-        execute_process(COMMAND ${rcc_path} -list \"${input_resource}\" OUTPUT_VARIABLE rcc_contents)
+        execute_process(COMMAND ${rcc_path} -list "${input_resource}" OUTPUT_VARIABLE rcc_contents)
         string(REGEX REPLACE "[\r\n]+" ";" rcc_contents ${rcc_contents})
         foreach(it ${rcc_contents})
             get_filename_component(extension ${it} EXT)