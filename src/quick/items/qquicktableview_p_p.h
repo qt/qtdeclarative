5b2791545ebe648d21fbc7297193779f5d24b631
Kai Koehne <kai.koehne@qt.io>
2018-06-20 14:03:17 +0200

    Add missing 'we mean it' warning to private header
    Fixes syncqt warning:
      QtQuick: WARNING: src/quick/items/qquicktableview_p_p.h does not have the "We mean it." warning
    
    Change-Id: Iebda5832bb6a931aec9b34db149c062cf5e03343
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

--- src/quick/items/qquicktableview_p_p.h
+++ src/quick/items/qquicktableview_p_p.h
@@ -37,6 +37,17 @@
 **
 ****************************************************************************/
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qquicktableview_p.h"
 
 #include <QtCore/qtimer.h>
