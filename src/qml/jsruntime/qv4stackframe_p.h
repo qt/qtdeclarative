0a0e6e2ad2ed8224b5c1b6cd179677198e87288c
Frederik Gladhorn <frederik.gladhorn@qt.io>
2018-07-31 18:22:09 +0200

    Add missing "We mean it." warning
    Change-Id: I3de691385a7884c4b38454e7a9f6f27b3b839286
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- src/qml/jsruntime/qv4stackframe_p.h
+++ src/qml/jsruntime/qv4stackframe_p.h
@@ -39,6 +39,17 @@
 #ifndef QV4STACKFRAME_H
 #define QV4STACKFRAME_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <private/qv4context_p.h>
 #include <private/qv4enginebase_p.h>
 #ifndef V4_BOOTSTRAP
