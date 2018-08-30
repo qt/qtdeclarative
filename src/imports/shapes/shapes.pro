8725f9873deda85eb426b989aa911f07fd80bf09
Kai Koehne <kai.koehne@qt.io>
2018-08-30 10:51:26 +0200

    qmlplugindump: Automatically use most up to date QtQuick version
    QtQuick / QML minor version is nowadays the same as the Qt minor
    version.
    
    Change-Id: I4d7ef0b101320ee2179746c95093b62e063adba2
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- src/imports/shapes/shapes.pro
+++ src/imports/shapes/shapes.pro
@@ -1,7 +1,7 @@
 CXX_MODULE = qml
 TARGET  = qmlshapesplugin
 TARGETPATH = QtQuick/Shapes
-IMPORT_VERSION = 1.11
+IMPORT_VERSION = 1.$$QT_MINOR_VERSION
 
 QT = core gui-private qml quick-private quickshapes-private
 
--- tools/qmlplugindump/main.cpp
+++ tools/qmlplugindump/main.cpp
@@ -79,9 +79,9 @@
 namespace {
 
 const uint qtQmlMajorVersion = 2;
-const uint qtQmlMinorVersion = 2;
+const uint qtQmlMinorVersion = QT_VERSION_MINOR;
 const uint qtQuickMajorVersion = 2;
-const uint qtQuickMinorVersion = 8;
+const uint qtQuickMinorVersion = QT_VERSION_MINOR;
 
 const QString qtQuickQualifiedName = QString::fromLatin1("QtQuick %1.%2")
         .arg(qtQuickMajorVersion)
