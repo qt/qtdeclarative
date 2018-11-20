8380e4c4a4f3f49a74a8bc0ff330e1c9e14dbafc
Shawn Rutledge <shawn.rutledge@qt.io>
2018-11-20 16:01:20 +0100

    Add handlers declared as Flickable children to its contentItem
    QQuickItemPrivate::data_append() was not invoked when any kind of
    Pointer Handler was directly declared in a Flickable (or subclass)
    because QQuickFlickable redefines the default property to be its own
    flickableData property. So we need to repeat the special handling
    in QQuickFlickablePrivate::data_append() too.  The handler must
    be added to the private->extra->pointerHandlers vector, so that
    QQuickItemPrivate::handlePointerEvent() will attempt to deliver
    events to those handlers.
    
    TapHandler seems OK (especially with its default gesturePolicy
    so that it does not do an exclusive grab).
    
    PointHandler seems OK.
    
    DragHandler competes with Flickable for the exclusive grab.
    pressDelay can help; or set acceptedDevices: PointerDevice.Mouse
    to allow the mouse to drag but not flick, and the touchscreen
    to flick but not drag.
    
    Fixes: QTBUG-71918
    Fixes: QTBUG-73035
    Change-Id: Icb97ed5230abe0cb6ec0230b5b5759a0528df7e8
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>

--- src/quick/handlers/qquicktaphandler.cpp
+++ src/quick/handlers/qquicktaphandler.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2017 The Qt Company Ltd.
+** Copyright (C) 2019 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQuick module of the Qt Toolkit.
@@ -315,6 +315,8 @@ void QQuickTapHandler::setPressed(bool press, bool cancel, QQuickEventPoint *poi
         if (cancel) {
             emit canceled(point);
             setExclusiveGrab(point, false);
+            // In case there is a filtering parent (Flickable), we should not give up the passive grab,
+            // so that it can continue to filter future events.
             reset();
             emit pointChanged();
         }
--- src/quick/items/qquickflickable_p_p.h
+++ src/quick/items/qquickflickable_p_p.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2019 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQuick module of the Qt Toolkit.
@@ -205,6 +205,8 @@ public:
     bool isViewMoving() const;
 
     void cancelInteraction();
+
+    void addPointerHandler(QQuickPointerHandler *h) override;
 
 public:
     QQuickItem *contentItem;
