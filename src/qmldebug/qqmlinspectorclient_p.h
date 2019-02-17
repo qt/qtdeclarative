1b01a8b809989c873d7b5bd9c213d8866584ec19
Kai Koehne <kai.koehne@qt.io>
2019-02-17 19:06:20 +0100

    Add 'We mean it' header to qqmlinspectorclient_p.h
    Fixes
      WARNING: src/qmldebug/qqmlinspectorclient_p.h does not have the "We mean it." warning
    
    Change-Id: I440c19a797c2780b6c775196682240a8cc7ca6fe
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qmldebug/qqmlinspectorclient_p.h
+++ src/qmldebug/qqmlinspectorclient_p.h
@@ -42,6 +42,17 @@
 
 #include <private/qqmldebugclient_p.h>
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 QT_BEGIN_NAMESPACE
 
 class QQmlInspectorClientPrivate;
