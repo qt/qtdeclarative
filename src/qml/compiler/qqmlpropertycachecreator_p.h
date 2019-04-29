qt/qtdeclarative

commit bc00353cffbfe0f74b602a16452f2e7bcd588152

Detect and reject cyclic aliases
Previously those would result in infinite recursion.

Fixes: QTBUG-74867
Change-Id: I6c0043b43e72fe7bc3a2a139ca600af2d5bca5ad
Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

src/qml/compiler/qqmlpropertycachecreator_p.h
@@ -692,11 +692,6 @@ inline QQmlCompileError QQmlPropertyCacheAliasCreator<ObjectContainer>::property
         const CompiledObject &component, const QV4::CompiledData::Alias &alias, int *type, int *minorVersion,
         QQmlPropertyData::Flags *propertyFlags)
 {
-    const int targetObjectIndex = objectForId(component, alias.targetObjectId);
-    Q_ASSERT(targetObjectIndex >= 0);
-
-    const CompiledObject &targetObject = *objectContainer->objectAt(targetObjectIndex);
-
     *type = 0;
     bool writable = false;
     bool resettable = false;
@@ -704,11 +699,36 @@ inline QQmlCompileError QQmlPropertyCacheAliasCreator<ObjectContainer>::property
     propertyFlags->isAlias = true;
 
     if (alias.aliasToLocalAlias) {
-        auto targetAlias = targetObject.aliasesBegin();
-        for (uint i = 0; i < alias.localAliasIndex; ++i)
-            ++targetAlias;
-        return propertyDataForAlias(component, *targetAlias, type, minorVersion, propertyFlags);
-    } else if (alias.encodedMetaPropertyIndex == -1) {
+        const QV4::CompiledData::Alias *lastAlias = &alias;
+        QVarLengthArray<const QV4::CompiledData::Alias *, 4> seenAliases({lastAlias});
+
+        do {
+            const CompiledObject *targetObject = objectContainer->objectAt(
+                    objectForId(component, lastAlias->targetObjectId));
+            Q_ASSERT(targetObject);
+
+            auto nextAlias = targetObject->aliasesBegin();
+            for (uint i = 0; i < lastAlias->localAliasIndex; ++i)
+                ++nextAlias;
+
+            const QV4::CompiledData::Alias *targetAlias = &(*nextAlias);
+            if (seenAliases.contains(targetAlias)) {
+                return QQmlCompileError(targetAlias->location,
+                                        QQmlPropertyCacheCreatorBase::tr("Cyclic alias"));
+            }
+
+            seenAliases.append(targetAlias);
+            lastAlias = targetAlias;
+        } while (lastAlias->aliasToLocalAlias);
+
+        return propertyDataForAlias(component, *lastAlias, type, minorVersion, propertyFlags);
+    }
+
+    const int targetObjectIndex = objectForId(component, alias.targetObjectId);
+    Q_ASSERT(targetObjectIndex >= 0);
+    const CompiledObject &targetObject = *objectContainer->objectAt(targetObjectIndex);
+
+    if (alias.encodedMetaPropertyIndex == -1) {
         Q_ASSERT(alias.flags & QV4::CompiledData::Alias::AliasPointsToPointerObject);
         auto *typeRef = objectContainer->resolvedType(targetObject.inheritedTypeNameIndex);
         if (!typeRef) {

tests/auto/qml/qqmllanguage/data/cyclicAlias.qml
@@ -1,0 +1,5 @@
+import QtQml 2.2
+QtObject {
+    id: o
+    property alias t: o.t
+}

tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
@@ -612,6 +612,7 @@ void tst_qqmllanguage::errors_data()
 
     QTest::newRow("badCompositeRegistration.1") << "badCompositeRegistration.1.qml" << "badCompositeRegistration.1.errors.txt" << false;
     QTest::newRow("badCompositeRegistration.2") << "badCompositeRegistration.2.qml" << "badCompositeRegistration.2.errors.txt" << false;
+    QTest::newRow("cyclicAlias") << "cyclicAlias.qml" << "cyclicAlias.errors.txt" << false;
 }
 
 

commit a452f14fd9b20b429048fc28aeac0c0542484a50

Fix coverity warning
Commit bc00353cffbfe0f74b602a16452f2e7bcd588152 accidentally removed the
assert that expressed how objectForId will always succeed with the alias
target. That caused coverity to complain that objectAt() may be called
with a negative (then array) index.

Change-Id: I8651e0826c92e41ab00bf8a44f1abfd1cbfb0e06
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/qml/compiler/qqmlpropertycachecreator_p.h
@@ -703,8 +703,9 @@ inline QQmlCompileError QQmlPropertyCacheAliasCreator<ObjectContainer>::property
         QVarLengthArray<const QV4::CompiledData::Alias *, 4> seenAliases({lastAlias});
 
         do {
-            const CompiledObject *targetObject = objectContainer->objectAt(
-                    objectForId(component, lastAlias->targetObjectId));
+            const int targetObjectIndex = objectForId(component, lastAlias->targetObjectId);
+            Q_ASSERT(targetObjectIndex >= 0);
+            const CompiledObject *targetObject = objectContainer->objectAt(targetObjectIndex);
             Q_ASSERT(targetObject);
 
             auto nextAlias = targetObject->aliasesBegin();