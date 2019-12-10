ae44beb0cee29d915fd6807e6c57176e3416b4bc
Ulf Hermann <ulf.hermann@qt.io>
2019-12-10 09:49:46 +0100

    Make QML_FORCE_DISK_CACHE override isDebugging()
    The way it was before, QML_FORCE_DISK_CACHE was rather useless.
    
    Change-Id: Id6be04ff1feaaced97455a797656bd041878fa9f
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/qml/qml/qqmltypeloader.cpp
+++ src/qml/qml/qqmltypeloader.cpp
@@ -710,7 +710,7 @@ bool QQmlTypeLoader::Blob::isDebugging() const
 
 bool QQmlTypeLoader::Blob::diskCacheEnabled() const
 {
-    return (!disableDiskCache() || forceDiskCache()) && !isDebugging();
+    return (!disableDiskCache() && !isDebugging()) || forceDiskCache();
 }
 
 bool QQmlTypeLoader::Blob::qmldirDataAvailable(const QQmlRefPointer<QQmlQmldirData> &data, QList<QQmlError> *errors)
