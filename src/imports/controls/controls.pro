qt/qtdeclarative

commit 01d076d0e81e5d8825fcdc16544f826530bd5ace

Fix static builds
The error was:

Error in '.rcc\debug\qmake_Qt_labs_controls.qrc':
Cannot find file 'C:/dev/qt5.7/qtquickcontrols2/src/imports/controls/designer/images/*.png'

Change-Id: Icfd1225e8c130d8d48f821af981b64202c687956
Task-number: QTBUG-51708
Reviewed-by: J-P Nurmi <jpnurmi@theqtcompany.com>

src/imports/controls/controls.pro
@@ -25,7 +25,7 @@ RESOURCES += \
     $$PWD/qtlabscontrolsplugin.qrc
 
 include(controls.pri)
-!ios: include(designer/designer.pri)
+!static: include(designer/designer.pri)
 
 CONFIG += no_cxx_module
 load(qml_plugin)

commit 91eb563b278ca9c831c444380030c458adbf02f0

Copy Qt Quick Designer-related files also for static builds
Make sure that the 'designer' directory is copied to the build
directory, or installed, also for static builds.

This reverts the !static condition introduced to fix a build
breakage in 01d076d0e81e5 . Anyhow, I couldn't reproduce
the original problem (QTBUG-51708), nor is it clear why it
was caused by the static build.

Fixes: QTBUG-75682
Change-Id: I47af3907e464d4fd9e9965ac28545b7350f450ee
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/imports/controls/controls.pro
@@ -19,7 +19,7 @@ SOURCES += \
 RESOURCES += \
     $$PWD/qtquickcontrols2plugin.qrc
 
-!static: qtConfig(quick-designer): include(designer/designer.pri)
+qtConfig(quick-designer): include(designer/designer.pri)
 include(doc/doc.pri)
 
 CONFIG += no_cxx_module install_qml_files builtin_resources qtquickcompiler