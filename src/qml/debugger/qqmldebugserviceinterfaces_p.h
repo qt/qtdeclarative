qt/qtdeclarative

commit 4975a33ba9aa357ba2bca93e292b1fbcfb34c24e

QmlDebug: add new debugtranslationservice
Users were asking for having the possibility to see where the translated
text will not fit in the reserved/available space.

This is more a preparation patch to get the right connectors to change the
visualization of findings or maybe log this to a file.

Task-number: QDS-1463
Change-Id: Ic0a7a930141d6eeb79964e51c0a165c69888cf5d
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/qml/debugger/qqmldebugserviceinterfaces_p.h
@@ -65,6 +65,7 @@ QT_BEGIN_NAMESPACE
 
 class QWindow;
 class QQuickWindow;
+class QQmlTranslationBinding;
 
 #if !QT_CONFIG(qml_debug)
 
@@ -103,6 +104,10 @@ public:
 class QDebugMessageService {};
 class QQmlEngineControlService {};
 class QQmlNativeDebugService {};
+class QQmlDebugTranslationService {
+    virtual QString foundElidedText(QObject *, const QString &, const QString &) {return {};}
+    virtual void foundTranslationBinding(QQmlTranslationBinding *, QObject *, QQmlContextData *) {}
+};
 
 #else
 
@@ -160,6 +165,22 @@ protected:
         QQmlDebugService(s_key, version, parent) {}
 
     QQmlBoundSignal *nextSignal(QQmlBoundSignal *prev) { return prev->m_nextSignal; }
+};
+
+class Q_QML_PRIVATE_EXPORT QQmlDebugTranslationService : public QQmlDebugService
+{
+    Q_OBJECT
+public:
+    static const QString s_key;
+
+    virtual QString foundElidedText(QObject *qQuickTextObject, const QString &layoutText, const QString &elideText) = 0;
+    virtual void foundTranslationBinding(QQmlTranslationBinding *binding, QObject *scopeObject, QQmlContextData *contextData) = 0;
+protected:
+    friend class QQmlDebugConnector;
+
+    QQmlDebugTranslationService(float version, QObject *parent = nullptr) :
+        QQmlDebugService(s_key, version, parent) {}
+
 };
 
 class Q_QML_PRIVATE_EXPORT QQmlInspectorService : public QQmlDebugService

src/plugins/qmltooling/qmldbg_preview/qmldbg_preview.pro
@@ -3,6 +3,7 @@ QT += core-private qml-private packetprotocol-private network quick-private gui-
 TARGET = qmldbg_preview
 
 SOURCES += \
+    $$PWD/qqmldebugtranslationservice.cpp \
     $$PWD/qqmlpreviewblacklist.cpp \
     $$PWD/qqmlpreviewfileengine.cpp \
     $$PWD/qqmlpreviewfileloader.cpp \
@@ -12,6 +13,7 @@ SOURCES += \
     $$PWD/qqmlpreviewservicefactory.cpp
 
 HEADERS += \
+    $$PWD/qqmldebugtranslationservice.h \
     $$PWD/qqmlpreviewblacklist.h \
     $$PWD/qqmlpreviewfileengine.h \
     $$PWD/qqmlpreviewfileloader.h \

src/plugins/qmltooling/qmldbg_preview/qqmldebugtranslationservice.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2020 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQml module of the Qt Toolkit.
@@ -37,19 +37,32 @@
 **
 ****************************************************************************/
 
-#include "qqmldebugserviceinterfaces_p.h"
+#include "qqmldebugtranslationservice.h"
 
 QT_BEGIN_NAMESPACE
 
-const QString QV4DebugService::s_key = QStringLiteral("V8Debugger");
-const QString QQmlEngineDebugService::s_key = QStringLiteral("QmlDebugger");
-const QString QQmlInspectorService::s_key = QStringLiteral("QmlInspector");
-const QString QQmlProfilerService::s_key = QStringLiteral("CanvasFrameRate");
-const QString QDebugMessageService::s_key = QStringLiteral("DebugMessages");
-const QString QQmlEngineControlService::s_key = QStringLiteral("EngineControl");
-const QString QQmlNativeDebugService::s_key = QStringLiteral("NativeQmlDebugger");
-const QString QQmlDebugTranslationService::s_key = QStringLiteral("DebugTranslation");
+QQmlDebugTranslationServiceImpl::QQmlDebugTranslationServiceImpl(QObject *parent) :
+    QQmlDebugTranslationService(1, parent)
+{
+}
+
+void QQmlDebugTranslationServiceImpl::messageReceived(const QByteArray &message)
+{
+    Q_UNUSED(message)
+}
+
+QString QQmlDebugTranslationServiceImpl::foundElidedText(QObject *textObject, const QString &layoutText, const QString &elideText)
+{
+    Q_UNUSED(textObject)
+    Q_UNUSED(layoutText)
+    return elideText;
+}
+
+void QQmlDebugTranslationServiceImpl::foundTranslationBinding(QQmlTranslationBinding *binding, QObject *scopeObject, QQmlContextData *contextData)
+{
+    Q_UNUSED(binding)
+    Q_UNUSED(scopeObject)
+    Q_UNUSED(contextData)
+}
 
 QT_END_NAMESPACE
-
-#include "moc_qqmldebugserviceinterfaces_p.cpp"

src/plugins/qmltooling/qmldbg_preview/qqmldebugtranslationservice.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2020 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQml module of the Qt Toolkit.
@@ -37,19 +37,51 @@
 **
 ****************************************************************************/
 
-#include "qqmldebugserviceinterfaces_p.h"
+#ifndef QDEBUGMESSAGESERVICE_H
+#define QDEBUGMESSAGESERVICE_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <private/qqmldebugserviceinterfaces_p.h>
+
+#include <QtCore/qlogging.h>
+#include <QtCore/qmutex.h>
+#include <QtCore/qelapsedtimer.h>
 
 QT_BEGIN_NAMESPACE
 
-const QString QV4DebugService::s_key = QStringLiteral("V8Debugger");
-const QString QQmlEngineDebugService::s_key = QStringLiteral("QmlDebugger");
-const QString QQmlInspectorService::s_key = QStringLiteral("QmlInspector");
-const QString QQmlProfilerService::s_key = QStringLiteral("CanvasFrameRate");
-const QString QDebugMessageService::s_key = QStringLiteral("DebugMessages");
-const QString QQmlEngineControlService::s_key = QStringLiteral("EngineControl");
-const QString QQmlNativeDebugService::s_key = QStringLiteral("NativeQmlDebugger");
-const QString QQmlDebugTranslationService::s_key = QStringLiteral("DebugTranslation");
+class QQmlDebugTranslationServicePrivate;
+
+class QQmlDebugTranslationServiceImpl : public QQmlDebugTranslationService
+{
+    Q_OBJECT
+public:
+    //needs to be in sync with QQmlDebugTranslationClient in qqmldebugtranslationclient_p.h
+    enum Command {
+        ChangeLanguage,
+        ChangeWarningColor,
+        ChangeElidedTextWarningString,
+        SetDebugTranslationServiceLogFile,
+        EnableElidedTextWarning,
+        DisableElidedTextWarning,
+        TestAllLanguages
+    };
+    QQmlDebugTranslationServiceImpl(QObject *parent = 0);
+
+    QString foundElidedText(QObject *textObject, const QString &layoutText, const QString &elideText) override;
+    void foundTranslationBinding(QQmlTranslationBinding *binding, QObject *scopeObject, QQmlContextData *contextData) override;
+    void messageReceived(const QByteArray &message) override;
+};
 
 QT_END_NAMESPACE
 
-#include "moc_qqmldebugserviceinterfaces_p.cpp"
+#endif // QDEBUGMESSAGESERVICE_H

src/plugins/qmltooling/qmldbg_preview/qqmlpreviewservicefactory.cpp
@@ -39,12 +39,18 @@
 
 #include "qqmlpreviewservicefactory.h"
 #include "qqmlpreviewservice.h"
+#include "qqmldebugtranslationservice.h"
 
 QT_BEGIN_NAMESPACE
 
 QQmlDebugService *QQmlPreviewServiceFactory::create(const QString &key)
 {
-    return key == QQmlPreviewServiceImpl::s_key ? new QQmlPreviewServiceImpl(this) : nullptr;
+    if (key == QQmlPreviewServiceImpl::s_key)
+        return new QQmlPreviewServiceImpl(this);
+    if (key == QQmlDebugTranslationServiceImpl::s_key)
+        return new QQmlDebugTranslationServiceImpl(this);
+
+    return nullptr;
 }
 
 QT_END_NAMESPACE

src/plugins/qmltooling/qmldbg_server/qqmldebugserver.cpp
@@ -422,6 +422,11 @@ void QQmlDebugServerImpl::parseArguments()
             << tr("Sends qDebug() and similar messages over the QML debug\n"
                "\t\t  connection. QtCreator uses this for showing debug\n"
                "\t\t  messages in the debugger console.") << '\n'
+            << '\n' << QQmlDebugTranslationService::s_key << "\t- "
+            //: Please preserve the line breaks and formatting
+            << tr("helps to see if a translated text\n"
+               "\t\t  will result in an elided text\n"
+               "\t\t  in QML elements.") << '\n'
            << tr("Other services offered by qmltooling plugins that implement "
                  "QQmlDebugServiceFactory and which can be found in the standard plugin "
                  "paths will also be available and can be specified. If no \"services\" "

src/qml/qml/qqmlbinding.cpp
@@ -43,6 +43,10 @@
 #include "qqmlcontext.h"
 #include "qqmlinfo.h"
 #include "qqmldata_p.h"
+
+#include <private/qqmldebugserviceinterfaces_p.h>
+#include <private/qqmldebugconnector_p.h>
+
 #include <private/qqmlprofiler_p.h>
 #include <private/qqmlexpression_p.h>
 #include <private/qqmlscriptstring_p.h>
@@ -391,6 +395,11 @@ QQmlBinding *QQmlBinding::createTranslationBinding(const QQmlRefPointer<QV4::Exe
     b->setNotifyOnValueChanged(true);
     b->QQmlJavaScriptExpression::setContext(ctxt);
     b->setScopeObject(obj);
+
+    if (QQmlDebugTranslationService *service
+                 = QQmlDebugConnector::service<QQmlDebugTranslationService>()) {
+        service->foundTranslationBinding(b, obj, ctxt);
+    }
 
     return b;
 }

src/qmldebug/qmldebug.pro
@@ -1,5 +1,5 @@
 TARGET    =  QtQmlDebug
-QT        =  core-private network packetprotocol-private
+QT        =  core-private qml-private network packetprotocol-private
 CONFIG    += static internal_module
 
 load(qt_module)
@@ -8,6 +8,7 @@ SOURCES += \
     qqmldebugclient.cpp \
     qqmldebugconnection.cpp \
     qqmldebugmessageclient.cpp \
+    qqmldebugtranslationclient.cpp \
     qqmlenginecontrolclient.cpp \
     qqmlenginedebugclient.cpp \
     qqmlinspectorclient.cpp \
@@ -24,6 +25,7 @@ HEADERS += \
     qqmldebugclient_p_p.h \
     qqmldebugconnection_p.h \
     qqmldebugmessageclient_p.h \
+    qqmldebugtranslationclient_p.h \
     qqmlenginedebugclient_p.h \
     qqmlenginedebugclient_p_p.h \
     qqmlenginecontrolclient_p.h \

commit 2eb5b3f5c5f32404b8cb4ebb1d65ed5d5781b9f3

Fix broken -no-qml-debug
4975a33ba9aa357ba2bca93e292b1fbcfb34c24e introduced QQmlDebugTranslationService.
If -no-qml-debug is provided the dummy class misses "public:".

qml/qqmlbinding.cpp:401:54: error: 'virtual void QQmlDebugTranslationService::foundTranslationBinding(QQmlTranslationBinding*, QObject*, QQmlContextData*)' is private within this context

Change-Id: I0f4ebd4d935a17aa7a679d2d87d98d03b0e1fcbb
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/qml/debugger/qqmldebugserviceinterfaces_p.h
@@ -105,6 +105,7 @@ class QDebugMessageService {};
 class QQmlEngineControlService {};
 class QQmlNativeDebugService {};
 class QQmlDebugTranslationService {
+public:
     virtual QString foundElidedText(QObject *, const QString &, const QString &) {return {};}
     virtual void foundTranslationBinding(QQmlTranslationBinding *, QObject *, QQmlContextData *) {}
 };