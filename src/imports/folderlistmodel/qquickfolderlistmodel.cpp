692e7cfdad0df2989dea37c9bfaeb0a0bcc41935
Michael Brasser <mbrasser@ford.com>
2020-02-22 16:24:35 -0600

    Add standard casing for FolderListModel::fileUrl
    Task-number: QTBUG-82298
    Change-Id: Iad95b7a90c2c247c44b8656b0fd104843bd4fa8c
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

--- src/imports/folderlistmodel/qquickfolderlistmodel.cpp
+++ src/imports/folderlistmodel/qquickfolderlistmodel.cpp
@@ -255,7 +255,8 @@ QString QQuickFolderListModelPrivate::resolvePath(const QUrl &path)
     \list
     \li \c fileName
     \li \c filePath
-    \li \c fileURL (since Qt 5.2)
+    \li \c fileURL (since Qt 5.2; deprecated since Qt 5.15)
+    \li \c fileUrl (since Qt 5.15)
     \li \c fileBaseName
     \li \c fileSuffix
     \li \c fileSize
@@ -334,7 +335,8 @@ QQuickFolderListModel::QQuickFolderListModel(QObject *parent)
     d->roleNames[FileLastModifiedRole] = "fileModified";
     d->roleNames[FileLastReadRole] = "fileAccessed";
     d->roleNames[FileIsDirRole] = "fileIsDir";
-    d->roleNames[FileUrlRole] = "fileURL";
+    d->roleNames[FileUrlRole] = "fileUrl";
+    d->roleNames[FileURLRole] = "fileURL";
     d->init();
 }
 
@@ -377,6 +379,7 @@ QVariant QQuickFolderListModel::data(const QModelIndex &index, int role) const
             rv = d->data.at(index.row()).isDir();
             break;
         case FileUrlRole:
+        case FileURLRole:
             rv = QUrl::fromLocalFile(d->data.at(index.row()).filePath());
             break;
         default:
@@ -886,7 +889,8 @@ void QQuickFolderListModel::setSortCaseSensitive(bool on)
     \list
         \li \c fileName
         \li \c filePath
-        \li \c fileURL (since Qt 5.2)
+        \li \c fileURL (since Qt 5.2; deprecated since Qt 5.15)
+        \li \c fileUrl (since Qt 5.15)
         \li \c fileBaseName
         \li \c fileSuffix
         \li \c fileSize
