f579c4c9ad6b2cfeb4900bf3ad8a1b19fe8e476d
Fabian Kosmale <fabian.kosmale@qt.io>
2020-04-20 10:28:31 +0200

    Check in even more places for exceptions
    Amends commit 4c5ed04e64ea9ac0038ae30e1189cfe745b29bd9
    
    Task-number: QTBUG-83384
    Change-Id: I0918c27dfa73dff83cbf0f58b41ce8620dff8a0a
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>
    (cherry picked from commit f31a2dc6fc713ee06ea7a9ba45c3ceaace7d735d)
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/qml/jsruntime/qv4stringobject.cpp
+++ src/qml/jsruntime/qv4stringobject.cpp
@@ -570,7 +570,7 @@ ReturnedValue StringPrototype::method_match(const FunctionObject *b, const Value
             ScopedFunctionObject fo(scope, f);
             if (!fo)
                 return scope.engine->throwTypeError();
-            return fo->call(r, thisObject, 1);
+            return checkedResult(scope.engine, fo->call(r, thisObject, 1));
         }
     }
 
@@ -861,6 +861,7 @@ ReturnedValue StringPrototype::method_replace(const FunctionObject *b, const Val
 
             Value that = Value::undefinedValue();
             replacement = searchCallback->call(&that, arguments, numCaptures + 2);
+            CHECK_EXCEPTION();
             result += string.midRef(lastEnd, matchStart - lastEnd);
             result += replacement->toQString();
             lastEnd = matchEnd;
