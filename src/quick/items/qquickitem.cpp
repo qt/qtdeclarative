a0769549cdb81c7104b60c2a67dfa62e8c42c224
Mitch Curtis <mitch.curtis@qt.io>
2020-11-09 18:49:09 +0100

    Doc: mention that Item's childrenRect property is local to the item
    As opposed to being local to the item's parent.
    
    Pick-to: 6.0 5.15
    Change-Id: Idcf2cdbedcac24a6890ce761c3f2a23f2c7a8dc4
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/quick/items/qquickitem.cpp
+++ src/quick/items/qquickitem.cpp
@@ -3673,6 +3673,10 @@ QQmlListProperty<QObject> QQuickItemPrivate::data()
 
     This property is useful if you need to access the collective geometry
     of an item's children in order to correctly size the item.
+
+    The geometry that is returned is local to the item. For example:
+
+    \snippet qml/item/childrenRect.qml local
 */
 /*!
     \property QQuickItem::childrenRect
@@ -3682,6 +3686,10 @@ QQmlListProperty<QObject> QQuickItemPrivate::data()
 
     This property is useful if you need to access the collective geometry
     of an item's children in order to correctly size the item.
+
+    The geometry that is returned is local to the item. For example:
+
+    \snippet qml/item/childrenRect.qml local
 */
 QRectF QQuickItem::childrenRect()
 {
