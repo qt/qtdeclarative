f4afe4934819a7f0fb9754b9bb08fb1acb818058
Shinichi Okada <shinichi.okada@qt.io>
2020-11-17 14:15:50 +0900

    QML Text doesn't reset lineCount when text is empty
    lineCount is not reset when replacing a multi-line QML Text 'text'
    property with an "" empty string. Also, the lineCountChanged signal is
    not emitted
    
    Task-number: QTBUG-84458
    Change-Id: Ic3c02e6a90e6675eadbaafc6af6ab0356ee98123
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit ae1b9c6d94001411efeef600e22638906e0fa990)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/items/qquicktext.cpp
+++ src/quick/items/qquicktext.cpp
@@ -398,6 +398,8 @@ void QQuickTextPrivate::updateSize()
         layedOutTextRect = QRectF(0, 0, 0, fontHeight);
         advance = QSizeF();
         signalSizeChange(previousSize);
+        lineCount = 1;
+        emit q->lineCountChanged();
         updateType = UpdatePaintNode;
         q->update();
         return;
