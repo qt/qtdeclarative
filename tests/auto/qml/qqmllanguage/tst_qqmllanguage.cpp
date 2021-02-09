b2fccf2f2d15c91cf52ca9a2a779eae3004f8b55
Maximilian Goldstein <max.goldstein@qt.io>
2021-02-09 11:37:45 +0100

    Fix QJSValue string parameters used in signals
    Fixes: QTBUG-86482
    Change-Id: If938fad22f51b08fe3cb20b94634efe46a1eed47
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit e887f25dd6c4c9630a7367c3a2ed95a284191843)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
+++ tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
@@ -346,6 +346,7 @@ private slots:
     void factorySingleton();
     void extendedSingleton();
     void qtbug_85932();
+    void qtbug_86482();
 
     void multiExtension();
     void invalidInlineComponent();
@@ -6164,6 +6165,26 @@ void tst_qqmllanguage::invalidInlineComponent()
               "}", QUrl());
     QVERIFY(c.isError());
     QVERIFY(c.errorString().contains("\"Window.visibility\" is not available in QtQuick 2.0."));
+}
+
+void tst_qqmllanguage::qtbug_86482()
+{
+    QQmlEngine engine;
+    QQmlComponent component(&engine);
+    component.setData(QByteArray(R"(import QtQml 2.0
+                                 import StaticTest
+                                 QtObject {
+                                     id: root
+                                     property string result
+                                     property StringSignaler str: StringSignaler {
+                                        onSignal: function(value) { root.result = value; }
+                                     }
+                                     Component.onCompleted: str.call();
+                                 })"), QUrl());
+    VERIFY_ERRORS(0);
+    QScopedPointer<QObject> o(component.create());
+    QVERIFY2(component.isReady(), qPrintable(component.errorString()));
+    QCOMPARE(o->property("result").toString(), QStringLiteral("Hello world!"));
 }
 
 QTEST_MAIN(tst_qqmllanguage)
