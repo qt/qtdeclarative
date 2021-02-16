dae0fe461b26b6db85586db1979bc1d9fb05670b
Fabian Kosmale <fabian.kosmale@qt.io>
2021-02-16 15:49:37 +0100

    QQmlPropertyPrivate::signalExpression: handle object being null
    QQmlData::get expects a non-null pointer, therefore we need to check
    whether the object still exists.
    
    Note that while this fixes the crash in the referenced bug,
    PropertyChanges still does not support a dynamic target.
    
    Task-number: QTBUG-46350
    Change-Id: Ifeecf5df83e87468a1d314ce2b120006124d6f4b
    Reviewed-by: Maximilian Goldstein <max.goldstein@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 1ff376e64bf5af6df7e0079700d2b9164037dc89)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/qml/qqmlproperty.cpp
+++ src/qml/qml/qqmlproperty.cpp
@@ -920,6 +920,8 @@ QQmlPropertyPrivate::signalExpression(const QQmlProperty &that)
     if (!(that.type() & QQmlProperty::SignalProperty))
         return nullptr;
 
+    if (!that.d->object)
+        return nullptr;
     QQmlData *data = QQmlData::get(that.d->object);
     if (!data)
         return nullptr;
@@ -959,6 +961,8 @@ void QQmlPropertyPrivate::takeSignalExpression(const QQmlProperty &that,
         return;
     }
 
+    if (!that.d->object)
+        return;
     QQmlData *data = QQmlData::get(that.d->object, nullptr != expr);
     if (!data)
         return;
