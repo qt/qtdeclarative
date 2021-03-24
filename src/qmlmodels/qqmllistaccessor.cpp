afe3fcb80af2535534dc4c92c683b593dfe1f3c3
Fabian Kosmale <fabian.kosmale@qt.io>
2021-03-24 14:55:52 +0100

    Cleanup QQmlListAccessor
    - Do not use qvariant_cast; we already know the exact type, so we can
      just reinterpret_cast the variant's constData pointer. Q_ASSERT that
      the metatype matches to verify that reality matches our expectations.
    - Do not convert pointers to QObject derived classes to QObject *.
      Instead, simply classify the list as Instance.
    - Reorder the cases in setList a bit, to avoid checking for possible
      conversions to int if possible. Also, simply call toInt and check if
      that worked.
    - Remove default case in switch statement, and use Q_UNREACHABLE after
      the switch statement instead.
    
    Task-number: QTBUG-88766
    Change-Id: I95aac7516988406eff1c34a0bba6cf0ebf1351a3
    Reviewed-by: Andrei Golubev <andrei.golubev@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qmlmodels/qqmllistaccessor.cpp
+++ src/qmlmodels/qqmllistaccessor.cpp
@@ -69,17 +69,24 @@ void QQmlListAccessor::setList(const QVariant &v)
 
     // An incoming JS array as model is treated as a variant list, so we need to
     // convert it first with toVariant().
-    if (d.userType() == qMetaTypeId<QJSValue>())
+    QMetaType variantsType = d.metaType();
+    if (variantsType == QMetaType::fromType<QJSValue>()) {
         d = d.value<QJSValue>().toVariant();
+        variantsType = d.metaType();
+    }
     if (!d.isValid()) {
         m_type = Invalid;
-    } else if (d.userType() == QMetaType::QStringList) {
+    } else if (variantsType == QMetaType::fromType<QStringList>()) {
         m_type = StringList;
-    } else if (d.userType() == QMetaType::QVariantList) {
+    } else if (variantsType == QMetaType::fromType<QVariantList>()) {
         m_type = VariantList;
-    } else if (d.userType() == qMetaTypeId<QList<QObject *>>()) {
+    } else if (variantsType == QMetaType::fromType<QList<QObject *>>()) {
         m_type = ObjectList;
-    } else if (d.canConvert(QMetaType(QMetaType::Int))) {
+    } else if (variantsType == QMetaType::fromType<QQmlListReference>()) {
+        m_type = ListProperty;
+    } else if (variantsType.flags() & QMetaType::PointerToQObject) {
+        m_type = Instance;
+    } else if (int i = 0; [&](){bool ok = false; i = v.toInt(&ok); return ok;}()) {
         // Here we have to check for an upper limit, because down the line code might (well, will)
         // allocate memory depending on the number of elements. The upper limit cannot be INT_MAX:
         //      QVector<QPointer<QQuickItem>> something;
@@ -90,7 +97,6 @@ void QQmlListAccessor::setList(const QVariant &v)
         // So, doing an approximate round-down-to-nice-number, we get:
         const int upperLimit = 100 * 1000 * 1000;
 
-        int i = v.toInt();
         if (i < 0) {
             qWarning("Model size of %d is less than 0", i);
             m_type = Invalid;
@@ -100,12 +106,6 @@ void QQmlListAccessor::setList(const QVariant &v)
         } else {
             m_type = Integer;
         }
-    } else if (d.metaType().flags().testFlag(QMetaType::PointerToQObject)) {
-        QObject *data = QQmlMetaType::toQObject(d);
-        d = QVariant::fromValue(data);
-        m_type = Instance;
-    } else if (d.userType() == qMetaTypeId<QQmlListReference>()) {
-        m_type = ListProperty;
     } else {
         m_type = Instance;
     }
@@ -115,21 +115,26 @@ qsizetype QQmlListAccessor::count() const
 {
     switch(m_type) {
     case StringList:
-        return qvariant_cast<QStringList>(d).count();
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QStringList>());
+        return reinterpret_cast<const QStringList *>(d.constData())->count();
     case VariantList:
-        return qvariant_cast<QVariantList>(d).count();
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QVariantList>());
+        return reinterpret_cast<const QVariantList *>(d.constData())->count();
     case ObjectList:
-        return qvariant_cast<QList<QObject *>>(d).count();
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QList<QObject *>>());
+        return reinterpret_cast<const QList<QObject *> *>(d.constData())->count();
     case ListProperty:
-        return ((const QQmlListReference *)d.constData())->count();
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QQmlListReference>());
+        return reinterpret_cast<const QQmlListReference *>(d.constData())->count();
     case Instance:
         return 1;
     case Integer:
-        return d.toInt();
-    default:
+        return *reinterpret_cast<const int *>(d.constData());
     case Invalid:
         return 0;
     }
+    Q_UNREACHABLE();
+    return 0;
 }
 
 QVariant QQmlListAccessor::at(qsizetype idx) const
@@ -137,21 +142,26 @@ QVariant QQmlListAccessor::at(qsizetype idx) const
     Q_ASSERT(idx >= 0 && idx < count());
     switch(m_type) {
     case StringList:
-        return QVariant::fromValue(qvariant_cast<QStringList>(d).at(idx));
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QStringList>());
+        return QVariant::fromValue(reinterpret_cast<const QStringList *>(d.constData())->at(idx));
     case VariantList:
-        return qvariant_cast<QVariantList>(d).at(idx);
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QVariantList>());
+        return reinterpret_cast<const QVariantList *>(d.constData())->at(idx);
     case ObjectList:
-        return QVariant::fromValue(qvariant_cast<QList<QObject *>>(d).at(idx));
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QList<QObject *>>());
+        return QVariant::fromValue(reinterpret_cast<const QList<QObject *> *>(d.constData())->at(idx));
     case ListProperty:
-        return QVariant::fromValue(((const QQmlListReference *)d.constData())->at(idx));
+        Q_ASSERT(d.metaType() == QMetaType::fromType<QQmlListReference>());
+        return QVariant::fromValue(reinterpret_cast<const QQmlListReference *>(d.constData())->at(idx));
     case Instance:
         return d;
     case Integer:
         return QVariant(idx);
-    default:
     case Invalid:
         return QVariant();
     }
+    Q_UNREACHABLE();
+    return QVariant();
 }
 
 bool QQmlListAccessor::isValid() const
