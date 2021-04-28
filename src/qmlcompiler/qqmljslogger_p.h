51f856828366c0e8598f9f750a1e86f88aea5081
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-04-28 11:50:08 +0200

    Add "we mean it" notice to private header
    Silence configure/syncqt warning about missing notice.
    
    Change-Id: I3cfc275f7d67854c1cfc2237282444162a7ccc93
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/qmlcompiler/qqmljslogger_p.h
+++ src/qmlcompiler/qqmljslogger_p.h
@@ -29,6 +29,17 @@
 #ifndef QLOGGER_H
 #define QLOGGER_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include "qcoloroutput_p.h"
 
 #include <private/qqmljsdiagnosticmessage_p.h>
