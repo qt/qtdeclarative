96b528efcba1226d2980828d1255160bdceae4cf
Albert Astals Cid <albert.astals.cid@kdab.com>
2021-05-21 13:42:35 +0200

    Support &apos; in styled text
    Pick-to: 6.1 5.15
    Change-Id: I4a8db963e52a7899ab1796f9a560e8029cc1c929
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>

--- src/quick/items/qquicktext.cpp
+++ src/quick/items/qquicktext.cpp
@@ -2175,7 +2175,7 @@ void QQuickText::resetMaximumLineCount()
     <img src="" align="top,middle,bottom" width="" height=""> - inline images
     <ol type="">, <ul type=""> and <li> - ordered and unordered lists
     <pre></pre> - preformatted
-    &gt; &lt; &amp; &quot; &nbsp;
+    &gt; &lt; &amp; &quot; &nbsp; &apos;
     \endcode
 
     \c Text.StyledText parser is strict, requiring tags to be correctly nested.
