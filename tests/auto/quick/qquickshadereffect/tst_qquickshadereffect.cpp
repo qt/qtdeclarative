538d81bc69c4fb6eed17bc23c992bcee0c5fa393
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2021-06-15 11:30:15 +0200

    Make tst_qquickshadereffect::testConnection() more robust
    The test is there to verify that the sourceChanged property
    notifier gets a connection, but it also counted all other
    connections. Since b65159a5ea8db05165b2eaab8e180a12f30063e4 in
    qtbase the parentChanged, windowChanged and enabledChanged
    signals are also connected to something so this broke the
    test.
    
    Fix is to make the test explicitly look for the sourceChanged
    signal and ignore all others.
    
    Pick-to: 6.2
    Change-Id: Ia188384b37c9c078e78d09670bd955a69d10c7de
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

--- tests/auto/quick/qquickshadereffect/tst_qquickshadereffect.cpp
+++ tests/auto/quick/qquickshadereffect/tst_qquickshadereffect.cpp
@@ -56,8 +56,15 @@ public:
     int signalsConnected = 0;
 
 protected:
-    void connectNotify(const QMetaMethod &) override { ++signalsConnected; }
-    void disconnectNotify(const QMetaMethod &) override { --signalsConnected; }
+    void connectNotify(const QMetaMethod &s) override {
+        if (s.name() == "sourceChanged")
+            ++signalsConnected;
+    }
+    void disconnectNotify(const QMetaMethod &s) override
+    {
+        if (s.name() == "sourceChanged")
+            --signalsConnected;
+    }
 
 signals:
     void dummyChanged();
