ef265596e9d6b610a6f6e180c75075b392593161
Ulf Hermann <ulf.hermann@qt.io>
2021-06-21 14:30:47 +0200

    Add basic QML application to test CMake API for QML modules
    An application with two modules which allows us to check that all the
    files end up in the right places.
    
    Change-Id: I58557d453e644e13f2eb646ec3195bba2654f569
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 5e78dc847d64cf99548d2b26cd957fb550c90cf2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/qml/qmlbasicapp/TimeExample/Clock.qml
@@ -0,0 +1,6 @@
+import QtQml
+
+QtObject {
+    property int hours: -1
+    property int minutes: -1
+}
---
+++ tests/auto/qml/qmlbasicapp/tst_qmlbasicapp.cpp
@@ -0,0 +1,86 @@
+/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QQmlEngine>
+#include <QQmlComponent>
+#include <QObject>
+#include <qtest.h>
+
+class tst_basicapp : public QObject
+{
+    Q_OBJECT
+private slots:
+    void loadComponent();
+    void resourceFiles();
+    void fileSystemFiles();
+};
+
+void tst_basicapp::loadComponent()
+{
+    QQmlEngine engine;
+    QQmlComponent c(&engine, QStringLiteral("qrc:/BasicApp/main.qml"));
+    QEXPECT_FAIL(nullptr, "Tries to load plugin from qrc", Abort);
+    QVERIFY2(c.isReady(), qPrintable(c.errorString()));
+    QScopedPointer o(c.create());
+    QVERIFY(!o.isNull());
+
+    const QTime time = QTime::currentTime();
+    const int hour = o->property("hours").toInt();
+    QVERIFY(hour >= time.hour() - 1);
+    QVERIFY(hour <= time.hour() + 1);
+
+    const int minute = o->property("minutes").toInt();
+    QVERIFY(minute >= time.minute() - 1);
+    QVERIFY(minute <= time.minute() + 1);
+}
+
+void tst_basicapp::resourceFiles()
+{
+    QVERIFY(QFile::exists(QStringLiteral(":/BasicApp/main.qml")));
+    QVERIFY(QFile::exists(QStringLiteral(":/TimeExample/Clock.qml")));
+    QVERIFY(QFile::exists(QStringLiteral(":/TimeExample/center.png")));
+    QVERIFY(QFile::exists(QStringLiteral(":/TimeExample/clock.png")));
+    QVERIFY(QFile::exists(QStringLiteral(":/TimeExample/hour.png")));
+    QVERIFY(QFile::exists(QStringLiteral(":/TimeExample/minute.png")));
+}
+
+void tst_basicapp::fileSystemFiles()
+{
+    const QString basedir = QCoreApplication::applicationDirPath();
+    QVERIFY(QFile::exists(basedir + QStringLiteral("/main.qml")));
+    QVERIFY(QFile::exists(basedir + QStringLiteral("/TimeExample/Clock.qml")));
+
+    QEXPECT_FAIL(nullptr, "Does not install RESOURCES in qrc", Abort);
+    QVERIFY(QFile::exists(basedir + QStringLiteral("/TimeExample/center.png")));
+    QVERIFY(QFile::exists(basedir + QStringLiteral("/TimeExample/clock.png")));
+    QVERIFY(QFile::exists(basedir + QStringLiteral("/TimeExample/hour.png")));
+    QVERIFY(QFile::exists(basedir + QStringLiteral("/TimeExample/minute.png")));
+}
+
+QTEST_MAIN(tst_basicapp)
+#include "tst_qmlbasicapp.moc"
