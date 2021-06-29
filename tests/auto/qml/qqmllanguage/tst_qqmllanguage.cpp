ea3ef53488e3fd2bc305642366baa8ecf16cea31
Ulf Hermann <ulf.hermann@qt.io>
2021-06-29 11:33:35 +0200

    tst_qqmllanguage: Make hangOnWarning independent of disk cache
    We force parsing of the file by attaching a dummy debugger to the
    engine.
    
    Change-Id: Ibacebf78493148f0d75b3c5a858889723230d92c
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
+++ tests/auto/qml/qqmllanguage/tst_qqmllanguage.cpp
@@ -50,6 +50,7 @@
 #include <private/qqmlvmemetaobject_p.h>
 #include <private/qqmlcomponent_p.h>
 #include <private/qqmltype_p_p.h>
+#include <private/qv4debugging_p.h>
 
 #include "testtypes.h"
 #include "testhttpserver.h"
@@ -6299,8 +6300,23 @@ void tst_qqmllanguage::bareInlineComponent()
     QVERIFY(tab1Found);
 }
 
+struct DummyDebugger : public QV4::Debugging::Debugger
+{
+    bool pauseAtNextOpportunity() const final { return false; }
+    void maybeBreakAtInstruction() final { }
+    void enteringFunction() final { }
+    void leavingFunction(const QV4::ReturnedValue &) final { }
+    void aboutToThrow() final { }
+};
+
 void tst_qqmllanguage::hangOnWarning()
 {
+    QQmlEngine engine;
+
+    // A debugger prevents the disk cache.
+    // If we load the file from disk cache we don't parse it and we don't see the warning.
+    engine.handle()->setDebugger(new DummyDebugger);
+
     QTest::ignoreMessage(QtWarningMsg,
                          qPrintable(QStringLiteral("%1:3 : Ignored annotation")
                                             .arg(testFileUrl("hangOnWarning.qml").toString())));
