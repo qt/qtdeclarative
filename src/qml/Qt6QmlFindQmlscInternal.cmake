48f7b85241539de2ef2fadb82bc10805885c38f5
Ulf Hermann <ulf.hermann@qt.io>
2021-07-14 15:34:52 +0200

    Rename qmlcachegenplus to qmlsc
    The binary and CMake target names should reflect the marketing name.
    
    Pick-to: 6.2
    Change-Id: I979db95999ccdd3113febfc76215ca091be0119b
    Reviewed-by: Andrei Golubev <andrei.golubev@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Vladimir Minenko <vladimir.minenko@qt.io>

--- src/qml/Qt6QmlFindQmlscInternal.cmake
+++ src/qml/Qt6QmlFindQmlscInternal.cmake
@@ -1,8 +1,8 @@
-if (QT_NO_FIND_QML_CACHEGEN_PLUS)
+if (QT_NO_FIND_QMLSC)
     return()
 endif()
 
-set(QT_NO_FIND_QML_CACHEGEN_PLUS TRUE)
+set(QT_NO_FIND_QMLSC TRUE)
 
 # FIXME: Make this work with cross-builds
 find_package(Qt6QmlCompilerPlus QUIET)
