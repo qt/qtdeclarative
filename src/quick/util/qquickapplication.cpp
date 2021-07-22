9ab16d4117e2fd309bd5ce7c08090e27c7073d21
Fabian Kosmale <fabian.kosmale@qt.io>
2021-07-22 13:26:48 +0200

    Document Application singleton
    Move the documentation from Qt.application to the newly created
    Application type entry. Add more examples to show how one can interact
    with the singleton.
    Now that we do not need to deal with documenting nested properties, we
    can get rid of the table based format, and instead use proper
    \qmlproperty and \qmlsignal commands.
    The documentation of Qt.application now simply links to the Application
    singleton documentation, with the additional note about properties being
    undefined if QGuiApplication is not used.
    
    Pick-to: 6.2
    Task-number: QTBUG-95265
    Change-Id: I55f8a1746d551430133a07680082282781880f45
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

--- src/quick/util/qquickapplication.cpp
+++ src/quick/util/qquickapplication.cpp
@@ -50,11 +50,175 @@
 
 QT_BEGIN_NAMESPACE
 
-/*
-    This object and its properties are documented as part of the Qt object,
-    in qqmlengine.cpp
+/*!
+    \qmltype Application
+    \instantiates QQuickApplication
+    \inqmlmodule QtQuick
+    //! once exposed: \inherits CoreApplication?
+    //! TODO: \ingroup ?
+
+    \brief Provides access to global application
+    state properties shared by many QML components.
+
+    The Application singleton exposes a subset of QApplication's properties to
+    QML applications.
+
+    It also provides an aboutToQuit() signal, which is the same as
+    QCoreApplication::aboutToQuit().
+
+    \qml
+    import QtQuick
+
+    Window {
+        id: root
+        visible: true
+        width: 800
+        height: 680
+
+        title: `${Application.name} (${Application.version})`
+
+        Connections {
+            target: Application
+            function onAboutToQuit() {
+                console.log("Bye!")
+            }
+        }
+    }
+    \endqml
 */
 
+/*!
+    \qmlproperty bool Application::active
+    \deprecated [5.2]
+
+    Returns  whether the application is active.
+    Use Qt.application.state == Qt.ApplicationActive instead
+*/
+
+/*!
+    \qmlproperty Qt::ApplicationState Application::state
+
+    This property represents the current state of the application.
+
+    \qml
+    Timer {
+        interval: 1000; repeat: true
+        active: Application.state === Qt.Qt.ApplicationActive
+        onTriggered: imageFetcher.fetchLatestImages()
+    }
+    \endqml
+*/
+
+/*!
+    \qmlproperty Qt::LayoutDiretcion Application::layoutDirection
+
+    This read-only property can be used to query the default layout
+    direction of the application. On system start-up, the default layout
+    direction depends on the application's language. The property has a
+    value of \c Qt.RightToLeft in locales where text and graphic elements
+    are read from right to left, and \c Qt.LeftToRight where the reading
+    direction flows from left to right. You can bind to this property to
+    customize your application layouts to support both layout directions.
+
+    \qml
+    RowLayout {
+        layoutDirection: Application.layoutDirection
+    }
+    \endqml
+*/
+
+/*!
+    \qmlproperty bool Application::supportsMultipleWindows
+
+    Returns \c true if the platform supports multiple windows. Some embedded
+    platforms do not support multiple windows, for example.
+ */
+
+/*!
+    \qmlproperty QFont Application::font
+    Returns the default application font as returned by
+    \l QGuiApplication::font().
+*/
+
+
+/*!
+    \qmlproperty QString Application::displayName
+
+    This property represents the application display name set on the
+    QGuiApplication instance. This property can be written to in order to set
+    the application display name.
+
+    \qml
+    Binding {
+        target: Application
+        property: "displayName"
+        value: "My Awesome Application"
+    }
+    \endqml
+*/
+
+/*!
+    \qmlproperty QQmlListProperty<QQuickScreenInfo> Application::screens
+
+    An array containing the descriptions of all connected screens. The
+    elements of the array are objects with the same properties as the
+    \l{Screen} attached object. In practice the array corresponds to the screen
+    list returned by QGuiApplication::screens(). In addition to examining
+    properties like name, width, height, etc., the array elements can also be
+    assigned to the screen property of Window items, thus serving as an
+    alternative to the C++ side's QWindow::setScreen().
+
+    \sa Screen, Window, {Window::screen}{Window.screen}
+*/
+
+/* The following properties are from QQmlApplication.
+   ### Document those in QQmlApplication instead once it is exposed
+*/
+
+/*!
+    \qmlproperty QStringList Application::arguments
+
+    This is a string list of the arguments the executable was invoked with.
+ */
+
+/*!
+    \qmlproperty QString Application::name
+
+    This is the application name set on the QCoreApplication instance. This
+    property can be written to in order to set the application name.
+ */
+
+/*!
+    \qmlproperty QString Application::version
+
+    This is the application version set on the QCoreApplication instance. This
+    property can be written to in order to set the application version.
+ */
+
+/*!
+    \qmlproperty QString Application::organization
+
+    This is the organization name set on the QCoreApplication instance.
+    This property can be written to in order to set the organization name.
+ */
+
+/*!
+    \qmlproperty QString Application::domain
+
+    This is the organization domain set on the QCoreApplication instance.
+    This property can be written to in order to set the organization domain.
+ */
+
+/*!
+    \qmlsignal Application::aboutToQuit()
+
+    This signal is emitted when the application is about to quit the main
+    event loop. The signal is particularly useful if your application has to
+    do some last-secondcleanup.
+    Note that no user interaction is possible in this state.
+
+    \sa QCoreApplication::aboutToQuit
+*/
 QQuickApplication::QQuickApplication(QObject *parent)
     : QQmlApplication(parent)
 {
