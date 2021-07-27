af141f0ea484549d89f99ca7261d73e6aa2244ac
Mitch Curtis <mitch.curtis@qt.io>
2021-07-27 10:02:33 +0200

    Exclude non-standalone qtquickcontrols2 QML from tst_quick_examples
    The examples they are a part of require resources and so the QML files
    can't be run on their own.
    
    Task-number: QTBUG-95173
    Change-Id: Ic70c77ece1ccae98ff3d1cd7798285d98a0330c4
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/quick/examples/tst_examples.cpp
+++ tests/auto/quick/examples/tst_examples.cpp
@@ -85,6 +85,8 @@ tst_examples::tst_examples()
     excludedDirs << "snippets/qml/visualdatamodel_rootindex";
     excludedDirs << "snippets/qml/qtbinding";
     excludedDirs << "snippets/qml/imports";
+    excludedDirs << "examples/quickcontrols2/imagine";
+    excludedDirs << "examples/quickcontrols2/texteditor";
     excludedFiles << "snippets/qml/image-ext.qml";
     excludedFiles << "examples/quick/shapes/content/main.qml"; // relies on resources
     excludedFiles << "examples/quick/shapes/content/interactive.qml"; // relies on resources
