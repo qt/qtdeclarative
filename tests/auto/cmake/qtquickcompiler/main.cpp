723b411a23ca98555310e30236806a2780f0cdc2
Craig Scott <craig.scott@qt.io>
2021-08-06 20:05:46 +1000

    Update and re-add qtquickcompiler cmake test
    Change-Id: I9bd3147f102168fb6d901a27dc8dc9ec85e0a62b
    Reviewed-by: Andrei Golubev <andrei.golubev@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 8b2b39a000e4ff4605fd3f0dd7d9c63230c9f4a1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/cmake/qtquickcompiler/main.cpp
+++ tests/auto/cmake/qtquickcompiler/main.cpp
@@ -1,3 +1,30 @@
+/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
 
 #include <QtCore>
 #include <QtQml>
@@ -12,14 +39,13 @@ private slots:
 
 void tst_QQC::packaging()
 {
-    QVERIFY(QFile::exists(":/main.qml"));
-    QCOMPARE(QFileInfo(":/main.qml").size(), 0);
-    QVERIFY(QFile::exists(":/main.cpp"));
-    QVERIFY(QFileInfo(":/main.cpp").size() > 0);
-
+    QVERIFY(QFile::exists(":/Test/main.qml"));
+    QVERIFY(QFileInfo(":/Test/main.qml").size() > 0);
+    QVERIFY(QFile::exists(":/Test/main.cpp"));
+    QVERIFY(QFileInfo(":/Test/main.cpp").size() > 0);
 
     QQmlEngine engine;
-    QQmlComponent component(&engine, QUrl("qrc:/main.qml"));
+    QQmlComponent component(&engine, QUrl("qrc:/Test/main.qml"));
     QScopedPointer<QObject> obj(component.create());
     QVERIFY(!obj.isNull());
     QCOMPARE(obj->property("success").toInt(), 42);
