qt/qtdeclarative

commit 2acb31641fc9c34d24ac29232cdfec2673834629

QQuickListView: prevent mouse delivery in floating header or footer
Earlier we reimplemented the contains() method of ListView to prevent
dragging in an Overlay or Pullback header or footer. But in QQuickWindow
(QQuickWindowPrivate::pointerTargets()), an early check prevents
delivery of pointer events to an item that is clipped and for which
contains() returns false, and also to its children.  In that case, the
header or footer no longer responds to a mouse event even if you put a
MouseArea in it.

Reverts 6ad3445f1e159d9beea936b66d267dcaacdc5d6c; reimplemented using
similar logic in a new QQuickListViewPrivate::wantsPointerEvent()
method, overriding QQuickFlickablePrivate::wantsPointerEvent(), which
is now checked in event-handling code in addition to checking the
interactive flag.

Done-with: Wang Chuan <ouchuanm@outlook.com>
Task-number: QTBUG-74046
Fixes: QTBUG-85302
Change-Id: I9474f035d26b74ee36c0ac19e45a77de2e694bf1
Reviewed-by: Wang Chuan <ouchuanm@outlook.com>
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
(cherry picked from commit 6857ad3e686a5e2b45d28a7f47dca3210608da50)
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/quick/items/qquickflickable.cpp
@@ -1424,7 +1424,7 @@ void QQuickFlickablePrivate::handleMouseReleaseEvent(QMouseEvent *event)
 void QQuickFlickable::mousePressEvent(QMouseEvent *event)
 {
     Q_D(QQuickFlickable);
-    if (d->interactive) {
+    if (d->interactive && d->wantsPointerEvent(event)) {
         if (!d->pressed)
             d->handleMousePressEvent(event);
         event->accept();
@@ -1436,7 +1436,7 @@ void QQuickFlickable::mousePressEvent(QMouseEvent *event)
 void QQuickFlickable::mouseMoveEvent(QMouseEvent *event)
 {
     Q_D(QQuickFlickable);
-    if (d->interactive) {
+    if (d->interactive && d->wantsPointerEvent(event)) {
         d->handleMouseMoveEvent(event);
         event->accept();
     } else {
@@ -1447,7 +1447,7 @@ void QQuickFlickable::mouseMoveEvent(QMouseEvent *event)
 void QQuickFlickable::mouseReleaseEvent(QMouseEvent *event)
 {
     Q_D(QQuickFlickable);
-    if (d->interactive) {
+    if (d->interactive && d->wantsPointerEvent(event)) {
         if (d->delayedPressEvent) {
             d->replayDelayedPress();
 
@@ -1475,7 +1475,7 @@ void QQuickFlickable::mouseReleaseEvent(QMouseEvent *event)
 void QQuickFlickable::wheelEvent(QWheelEvent *event)
 {
     Q_D(QQuickFlickable);
-    if (!d->interactive) {
+    if (!d->interactive || !d->wantsPointerEvent(event)) {
         QQuickItem::wheelEvent(event);
         return;
     }
@@ -2445,7 +2445,7 @@ bool QQuickFlickable::filterMouseEvent(QQuickItem *receiver, QMouseEvent *event)
 bool QQuickFlickable::childMouseEventFilter(QQuickItem *i, QEvent *e)
 {
     Q_D(QQuickFlickable);
-    if (!isVisible() || !isEnabled() || !isInteractive()) {
+    if (!isVisible() || !isEnabled() || !isInteractive() || !d->wantsPointerEvent(e)) {
         d->cancelInteraction();
         return QQuickItem::childMouseEventFilter(i, e);
     }

src/quick/items/qquickflickable_p_p.h
@@ -208,6 +208,9 @@ public:
 
     void addPointerHandler(QQuickPointerHandler *h) override;
 
+    // TODO Qt 6: QPointerEvent
+    virtual bool wantsPointerEvent(const QEvent *) { return true; }
+
 public:
     QQuickItem *contentItem;
 

src/quick/items/qquicklistview.cpp
@@ -58,6 +58,8 @@ QT_BEGIN_NAMESPACE
 #define QML_FLICK_SNAPONETHRESHOLD 30
 #endif
 
+Q_LOGGING_CATEGORY(lcEvents, "qt.quick.listview.events")
+
 class FxListItemSG;
 
 class QQuickListViewPrivate : public QQuickItemViewPrivate
@@ -143,6 +145,9 @@ public:
 
     void fixupHeader();
     void fixupHeaderCompleted();
+
+    bool wantsPointerEvent(const QEvent *event) override;
+
     QQuickListView::Orientation orient;
     qreal visiblePos;
     qreal averageSize;
@@ -179,6 +184,7 @@ public:
 
     bool correctFlick : 1;
     bool inFlickCorrection : 1;
+    bool wantedMousePress : 1;
 
     QQuickListViewPrivate()
         : orient(QQuickListView::Vertical)
@@ -192,7 +198,7 @@ public:
         , sectionCriteria(nullptr), currentSectionItem(nullptr), nextSectionItem(nullptr)
         , overshootDist(0.0), desiredViewportPosition(0.0), fixupHeaderPosition(0.0)
         , headerNeedsSeparateFixup(false), desiredHeaderVisible(false)
-        , correctFlick(false), inFlickCorrection(false)
+        , correctFlick(false), inFlickCorrection(false), wantedMousePress(false)
     {
         highlightMoveDuration = -1; //override default value set in base class
     }
@@ -3819,20 +3825,52 @@ QQuickListViewAttached *QQuickListView::qmlAttachedProperties(QObject *obj)
     return new QQuickListViewAttached(obj);
 }
 
-bool QQuickListView::contains(const QPointF &point) const
+/*! \internal
+    Prevents clicking or dragging through floating headers (QTBUG-74046).
+*/
+bool QQuickListViewPrivate::wantsPointerEvent(const QEvent *event)
 {
-    bool ret = QQuickItemView::contains(point);
-    // QTBUG-74046: if a mouse press "falls through" a floating header or footer, don't allow dragging the list from there
-    if (ret) {
-        if (auto header = headerItem()) {
-            if (headerPositioning() != QQuickListView::InlineHeader && header->contains(mapToItem(header, point)))
+    Q_Q(const QQuickListView);
+    bool ret = true;
+
+    QPointF pos;
+    // TODO switch not needed in Qt 6: use points().first().position()
+    switch (event->type()) {
+    case QEvent::Wheel:
+        pos = static_cast<const QWheelEvent *>(event)->position();
+        break;
+    case QEvent::MouseButtonPress:
+        pos = static_cast<const QMouseEvent *>(event)->localPos();
+        break;
+    default:
+        break;
+    }
+
+    if (!pos.isNull()) {
+        if (auto header = q->headerItem()) {
+            if (q->headerPositioning() != QQuickListView::InlineHeader &&
+                    header->contains(q->mapToItem(header, pos)))
                 ret = false;
         }
-        if (auto footer = footerItem()) {
-            if (footerPositioning() != QQuickListView::InlineFooter && footer->contains(mapToItem(footer, point)))
+        if (auto footer = q->footerItem()) {
+            if (q->footerPositioning() != QQuickListView::InlineFooter &&
+                    footer->contains(q->mapToItem(footer, pos)))
                 ret = false;
         }
     }
+
+    switch (event->type()) {
+    case QEvent::MouseButtonPress:
+        wantedMousePress = ret;
+        break;
+    case QEvent::MouseMove:
+        ret = wantedMousePress;
+        break;
+    default:
+        break;
+    }
+
+    qCDebug(lcEvents) << q << (ret ? "WANTS" : "DOESN'T want") << event;
     return ret;
 }
 

src/quick/items/qquicklistview_p.h
@@ -179,8 +179,6 @@ public:
 
     static QQuickListViewAttached *qmlAttachedProperties(QObject *);
 
-    bool contains(const QPointF &point) const override;
-
 public Q_SLOTS:
     void incrementCurrentIndex();
     void decrementCurrentIndex();

tests/auto/quick/qquicklistview/data/clickHeaderAndFooterWhenClip.qml
@@ -1,0 +1,60 @@
+import QtQuick 2.9
+import QtQuick.Window 2.15
+
+ListView {
+    id: list
+    anchors.fill: parent
+    property bool headerPressed: false
+    property bool footerPressed: false
+    model: ListModel {
+        ListElement {
+            name: "Element 1"
+        }
+        ListElement {
+            name: "Element 2"
+        }
+        ListElement {
+            name: "Element 3"
+        }
+        ListElement {
+            name: "Element 4"
+        }
+        ListElement {
+            name: "Element 5"
+        }
+        ListElement {
+            name: "Element 6"
+        }
+    }
+    clip: true
+    headerPositioning: ListView.OverlayHeader
+    footerPositioning: ListView.OverlayHeader
+
+    delegate: Text {
+        height: 100
+        text: name
+    }
+
+    header: Rectangle {
+        width: parent.width
+        height: 50
+        z: 2
+        color: "blue"
+        MouseArea {
+            objectName: "header"
+            anchors.fill: parent
+            onClicked: list.headerPressed = true
+        }
+    }
+    footer: Rectangle {
+        width: parent.width
+        height: 50
+        color: "red"
+        z: 2
+        MouseArea {
+            objectName: "footer"
+            anchors.fill: parent
+            onClicked: list.footerPressed = true
+        }
+    }
+}

tests/auto/quick/qquicklistview/tst_qquicklistview.cpp
@@ -297,6 +297,7 @@ private slots:
     void objectModelCulling();
 
     void requiredObjectListModel();
+    void clickHeaderAndFooterWhenClip();
 
 private:
     template <class T> void items(const QUrl &source);
@@ -10065,6 +10066,32 @@ void tst_QQuickListView::requiredObjectListModel()
         QCOMPARE(rect->color(), QColor(data->color()));
         QCOMPARE(rect->property("name").toString(), data->name());
     }
+}
+
+void tst_QQuickListView::clickHeaderAndFooterWhenClip() // QTBUG-85302
+{
+    QScopedPointer<QQuickView> window(createView());
+    window->setSource(testFileUrl("clickHeaderAndFooterWhenClip.qml"));
+    window->resize(640, 480);
+    window->show();
+
+    QVERIFY(QTest::qWaitForWindowExposed(window.data()));
+    auto *root = window->rootObject();
+    QVERIFY(root);
+
+    auto *header = root->findChild<QQuickItem *>("header");
+    QVERIFY(header);
+
+    auto *footer = root->findChild<QQuickItem *>("footer");
+    QVERIFY(footer);
+
+    QVERIFY(root->property("headerPressed").isValid() && root->property("headerPressed").canConvert<bool>() && !root->property("headerPressed").toBool());
+    QTest::mouseClick(window.data(), Qt::LeftButton, Qt::NoModifier, header->mapToItem(root, QPoint(header->width() / 2, header->height() / 2)).toPoint());
+    QVERIFY(root->property("headerPressed").toBool());
+
+    QVERIFY(root->property("footerPressed").isValid() && root->property("footerPressed").canConvert<bool>() && !root->property("footerPressed").toBool());
+    QTest::mouseClick(window.data(), Qt::LeftButton, Qt::NoModifier, footer->mapToItem(root, QPoint(footer->width() / 2, footer->height() / 2)).toPoint());
+    QVERIFY(root->property("footerPressed").toBool());
 }
 
 QTEST_MAIN(tst_QQuickListView)

commit b2e4f09bfa2c42af562f6741c0a0246a53028f0a

Fix regression in ListView/Flickable event delivery
The regression was introduced with 2acb31641fc9c34d24ac29232cdfec

The problem was that when we entered
QQuickListViewPrivate::wantsPointerEvent(), the event was not localized
to the QQuickFlickable when mouse filtering was done.

Therefore, since the code assumed that the localPos was local to the
Flickable, it always ended up assuming that the point was within the
bounds of the header/footer.

Fixes: QTBUG-89409
Pick-to: 5.15 6.1 6.2
Change-Id: Ib595e61b7995241b58e3051da09139e1e1f13cba
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/quick/items/qquickflickable.cpp
@@ -2566,8 +2566,18 @@ bool QQuickFlickable::filterPointerEvent(QQuickItem *receiver, QPointerEvent *ev
 bool QQuickFlickable::childMouseEventFilter(QQuickItem *i, QEvent *e)
 {
     Q_D(QQuickFlickable);
+
+    auto wantsPointerEvent_helper = [=]() {
+        QPointerEvent *pe = static_cast<QPointerEvent *>(e);
+        QQuickDeliveryAgentPrivate::localizePointerEvent(pe, this);
+        const bool wants = d->wantsPointerEvent(pe);
+        // re-localize event back to \a i before returning
+        QQuickDeliveryAgentPrivate::localizePointerEvent(pe, i);
+        return wants;
+    };
+
     if (!isVisible() || !isEnabled() || !isInteractive() ||
-            (e->isPointerEvent() && !d->wantsPointerEvent(static_cast<QPointerEvent *>(e)))) {
+            (e->isPointerEvent() && !wantsPointerEvent_helper())) {
         d->cancelInteraction();
         return QQuickItem::childMouseEventFilter(i, e);
     }

tests/auto/quick/qquicklistview2/data/delegateWithMouseArea.qml
@@ -1,0 +1,120 @@
+/****************************************************************************
+**
+** Copyright (C) 2021 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the test suite of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+import QtQuick
+
+Rectangle {
+
+    width: 240
+    height: 320
+    color: "#ffffff"
+
+    Component {
+        id: myDelegate
+        Rectangle {
+            id: wrapper
+            width: list.orientation == ListView.Vertical ? 240 : 20
+            height: list.orientation == ListView.Vertical ? 20 : 240
+            border.width: 1
+            border.color: "black"
+            MouseArea {
+                anchors.fill: parent
+            }
+            Text {
+                text: index + ":" + (list.orientation == ListView.Vertical ? parent.y : parent.x).toFixed(0)
+            }
+            color: ListView.isCurrentItem ? "lightsteelblue" : "white"
+        }
+    }
+
+    ListView {
+        id: list
+        objectName: "list"
+        focus: true
+        width: 240
+        height: 200
+        clip: true
+        model: 30
+        headerPositioning: ListView.OverlayHeader
+        delegate: myDelegate
+
+        header: Rectangle {
+            width: list.orientation == Qt.Vertical ? 240 : 30
+            height: list.orientation == Qt.Vertical ? 30 : 240
+            color: "green"
+            z: 11
+            Text {
+                anchors.centerIn: parent
+                text: "header " + (list.orientation == ListView.Vertical ? parent.y : parent.x).toFixed(1)
+            }
+        }
+    }
+
+    // debug
+    Rectangle {
+        color: "#40ff0000"
+        border.width: txt.x
+        border.color: "black"
+        radius: 5
+        width: txt.implicitWidth + 50
+        height: txt.implicitHeight + 2 * txt.x
+        anchors.bottom: parent.bottom
+        anchors.right: parent.right
+        anchors.left: parent.left
+
+        Text {
+            id: txt
+            x: 3
+            y: x
+            text:  "header position: " + (list.orientation == ListView.Vertical ? list.headerItem.y : list.headerItem.x).toFixed(1)
+              + "\ncontent position: " + (list.orientation == ListView.Vertical ? list.contentY : list.contentX).toFixed(1)
+        }
+    }
+}

tests/auto/quick/qquicklistview2/tst_qquicklistview2.cpp
@@ -31,6 +31,7 @@
 #include <QtQuick/private/qquickitemview_p_p.h>
 #include <QtQuick/private/qquicklistview_p.h>
 #include <QtQuickTest/QtQuickTest>
+
 #include "../shared/viewtestutil.h"
 #include "../shared/visualtestutil.h"
 #include "../../shared/util.h"
@@ -46,6 +47,8 @@ public:
 
 private slots:
     void urlListModel();
+    void dragDelegateWithMouseArea_data();
+    void dragDelegateWithMouseArea();
 };
 
 tst_QQuickListView2::tst_QQuickListView2()
@@ -69,6 +72,95 @@ void tst_QQuickListView2::urlListModel()
     if (QQuickTest::qIsPolishScheduled(view))
         QVERIFY(QQuickTest::qWaitForItemPolished(view));
     QCOMPARE(view->count(), model.size());
+}
+
+static void dragListView(QWindow *window, QPoint *startPos, const QPoint &delta)
+{
+    auto drag_helper = [&](QWindow *window, QPoint *startPos, const QPoint &d) {
+        QPoint pos = *startPos;
+        const int dragDistance = d.manhattanLength();
+        const QPoint unitVector(qBound(-1, d.x(), 1), qBound(-1, d.y(), 1));
+        for (int i = 0; i < dragDistance; ++i) {
+            QTest::mouseMove(window, pos);
+            pos += unitVector;
+        }
+        // Move to the final position
+        pos = *startPos + d;
+        QTest::mouseMove(window, pos);
+        *startPos = pos;
+    };
+
+    if (delta.manhattanLength() == 0)
+        return;
+    const int dragThreshold = QGuiApplication::styleHints()->startDragDistance();
+    const QPoint unitVector(qBound(-1, delta.x(), 1), qBound(-1, delta.y(), 1));
+    // go just beyond the drag theshold
+    drag_helper(window, startPos, unitVector * (dragThreshold + 1));
+    drag_helper(window, startPos, unitVector);
+
+    // next drag will actually scroll the listview
+    drag_helper(window, startPos, delta);
+}
+
+void tst_QQuickListView2::dragDelegateWithMouseArea_data()
+{
+    QTest::addColumn<QQuickItemView::LayoutDirection>("layoutDirection");
+
+    for (int layDir = QQuickItemView::LeftToRight; layDir <= (int)QQuickItemView::VerticalBottomToTop; layDir++) {
+        const char *enumValueName = QMetaEnum::fromType<QQuickItemView::LayoutDirection>().valueToKey(layDir);
+        QTest::newRow(enumValueName) << static_cast<QQuickItemView::LayoutDirection>(layDir);
+    }
+}
+
+void tst_QQuickListView2::dragDelegateWithMouseArea()
+{
+    QFETCH(QQuickItemView::LayoutDirection, layoutDirection);
+
+    QScopedPointer<QQuickView> window(createView());
+    QVERIFY(window);
+    window->setSource(testFileUrl("delegateWithMouseArea.qml"));
+    window->show();
+    QVERIFY(QTest::qWaitForWindowExposed(window.data()));
+
+    QQuickListView *listview = findItem<QQuickListView>(window->rootObject(), "list");
+    QVERIFY(listview != nullptr);
+
+    const bool horizontal = layoutDirection < QQuickItemView::VerticalTopToBottom;
+    listview->setOrientation(horizontal ? QQuickListView::Horizontal : QQuickListView::Vertical);
+
+    if (horizontal)
+        listview->setLayoutDirection(static_cast<Qt::LayoutDirection>(layoutDirection));
+    else
+        listview->setVerticalLayoutDirection(static_cast<QQuickItemView::VerticalLayoutDirection>(layoutDirection));
+
+    QVERIFY(QQuickTest::qWaitForItemPolished(listview));
+
+    auto contentPosition = [&](QQuickListView *listview) {
+        return (listview->orientation() == QQuickListView::Horizontal ? listview->contentX(): listview->contentY());
+    };
+
+    qreal expectedContentPosition = contentPosition(listview);
+    QPoint startPos = (QPointF(listview->width(), listview->height())/2).toPoint();
+    QTest::mousePress(window.data(), Qt::LeftButton, Qt::NoModifier, startPos, 200);
+
+    QPoint dragDelta(0, -10);
+
+    if (layoutDirection == QQuickItemView::RightToLeft || layoutDirection == QQuickItemView::VerticalBottomToTop)
+        dragDelta = -dragDelta;
+    expectedContentPosition -= dragDelta.y();
+    if (horizontal)
+        dragDelta = dragDelta.transposed();
+
+    dragListView(window.data(), &startPos, dragDelta);
+
+    QTest::mouseRelease(window.data(), Qt::LeftButton, Qt::NoModifier, startPos, 200);     // Wait 200 ms before we release to avoid trigger a flick
+
+    // wait for the "fixup" animation to finish
+    QVERIFY(QTest::qWaitFor([&]()
+        { return !listview->isMoving();}
+    ));
+
+    QCOMPARE(contentPosition(listview), expectedContentPosition);
 }
 
 QTEST_MAIN(tst_QQuickListView2)