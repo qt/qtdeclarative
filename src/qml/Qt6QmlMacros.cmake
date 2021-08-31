bf8860b4993de7d2467be7af22bcdb1482844924
Craig Scott <craig.scott@qt.io>
2021-08-31 16:15:53 +1000

    Revert "Ensure versionless wrappers do not introduce a new variable scope"
    This reverts commit fdc1e18f8b829efd1f57dc8514d2b0a0ccb7f447.
    
    Macros add another level of escaping that functions do not. The
    conversion of the versionless wrappers to macros may alter the
    behavior, so revert that change.
    
    Task-number: QTBUG-96219
    Change-Id: Iab36fba8543b0ece7eb505046d886db7064972f0
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 995a254f9727352f84889ceabead58a12f659455)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/Qt6QmlMacros.cmake
+++ src/qml/Qt6QmlMacros.cmake
@@ -595,9 +595,9 @@ function(qt6_add_qml_module target)
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_add_qml_module)
+    function(qt_add_qml_module)
         qt6_add_qml_module(${ARGV})
-    endmacro()
+    endfunction()
 endif()
 
 function(_qt_internal_get_escaped_uri uri out_var)
@@ -1190,9 +1190,9 @@ function(qt6_add_qml_plugin target)
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_add_qml_plugin)
+    function(qt_add_qml_plugin)
         qt6_add_qml_plugin(${ARGV})
-    endmacro()
+    endfunction()
 endif()
 
 
@@ -1578,9 +1578,9 @@ function(qt6_target_qml_sources target)
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_target_qml_sources)
+    function(qt_target_qml_sources)
         qt6_target_qml_sources(${ARGV})
-    endmacro()
+    endfunction()
 endif()
 
 # target: Expected to be the backing target for a qml module. Certain target
@@ -2023,13 +2023,13 @@ but this file does not exist.  Possible reasons include:
 endfunction()
 
 if(NOT QT_NO_CREATE_VERSIONLESS_FUNCTIONS)
-    macro(qt_import_qml_plugins)
+    function(qt_import_qml_plugins)
         if(QT_DEFAULT_MAJOR_VERSION EQUAL 5)
             qt5_import_qml_plugins(${ARGV})
         elseif(QT_DEFAULT_MAJOR_VERSION EQUAL 6)
             qt6_import_qml_plugins(${ARGV})
         endif()
-    endmacro()
+    endfunction()
 endif()
 
 function(_qt_internal_add_static_qml_plugin_dependencies plugin_target backing_target)
