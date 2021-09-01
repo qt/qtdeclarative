b0e567e7e3927945c904e9a613af4b9064d70bc1
Oliver Eftevaag <oliver.eftevaag@qt.io>
2021-09-01 15:40:28 +0200

    Fix localstorage example to use a QML module
    Removing the directory 'localstorage' that is inside the top-level
    'localstorage' directory, and moving the content one level up in the
    file hierarchy.
    
    Pick-to: 6.2
    Change-Id: I97cf1ff92be9860d9a92bce66c88af927618e2b0
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- examples/quick/localstorage/main.cpp
+++ examples/quick/localstorage/main.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/localstorage.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/localstorage/localstorage.qml")));
 
     return app.exec();
 }
