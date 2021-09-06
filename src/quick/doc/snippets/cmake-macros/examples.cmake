9fdb12bdac0efd9875b2d98ae82c3702cf722469
Craig Scott <craig.scott@qt.io>
2021-09-06 16:17:55 +1000

    Add documentation for qt_import_qml_plugin()
    Pick-to: 6.2
    Change-Id: I1f503cd03c35e48f2219e95db57b153c69badb0b
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/quick/doc/snippets/cmake-macros/examples.cmake
+++
@@ -1,6 +0,0 @@
-#! [qt5_import_qml_plugins]
-find_package(Qt5 COMPONENTS Quick QmlImportScanner)
-add_executable(myapp main.cpp)
-target_link_libraries(myapp Qt5::Quick)
-qt5_import_qml_plugins(myapp)
-#! [qt5_import_qml_plugins]
