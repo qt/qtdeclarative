a960c2b53abc94698f498cf6c84eeb471431e337
Ulf Hermann <ulf.hermann@qt.io>
2021-09-29 10:36:30 +0200

    Pass file name and code to the qmllint codegen logger
    We need the file name in order to see when the file we're examining is a
    qmltypes file.
    
    Change-Id: I72b6c3fe63e721b2b2a116fcafcb55e45b23ae39
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Maximilian Goldstein <max.goldstein@qt.io>

--- tools/qmllint/codegen.cpp
+++ tools/qmllint/codegen.cpp
@@ -56,12 +56,12 @@ void Codegen::setDocument(QmlIR::JSCodeGen *codegen, QmlIR::Document *document)
             // dynamically (QTBUG-95530). Currently this has no other side effects. Re-evaluate once
             // that changes.
             QQmlJSTypeResolver::Indirect, QQmlJSTypeResolver::Static, m_logger);
-    // TODO: using emptyLogger for visitor actually hides potential issues but
+    // TODO: using silentLogger for visitor actually hides potential issues but
     // using m_logger instead fails some tests, so for now let's leave the old
     // behavior for consistency. the proper fix is anyway to remove this visitor
     // and use FindWarningsVisitor instead.
-    QQmlJSLogger emptyLogger(QString(), QString(), /* silent */ true);
-    QQmlJSImportVisitor visitor(m_importer, &emptyLogger,
+    QQmlJSLogger silentLogger(m_fileName, document->code, /* silent */ true);
+    QQmlJSImportVisitor visitor(m_importer, &silentLogger,
                                 QQmlJSImportVisitor::implicitImportDirectory(
                                         m_fileName, m_importer->resourceFileMapper()),
                                 m_qmltypesFiles);
