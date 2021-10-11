93ccc3ddf918156a8b967e66e6db3f6e1880bc17
Ulf Hermann <ulf.hermann@qt.io>
2021-10-11 11:51:06 +0200

    Avoid memory leak in tst_qquickdesignersupport.cpp
    Change-Id: I25bcb7c9681da8120a1f254c1f13412d58b47b2b
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/quick/qquickdesignersupport/tst_qquickdesignersupport.cpp
+++ tests/auto/quick/qquickdesignersupport/tst_qquickdesignersupport.cpp
@@ -351,10 +351,13 @@ void tst_qquickdesignersupport::createComponent()
 
     QVERIFY(rootItem);
 
-    QObject *testComponentObject = QQuickDesignerSupportItems::createComponent(testFileUrl("TestComponent.qml"), view->rootContext());
+    QScopedPointer<QObject> testComponentObject(
+                QQuickDesignerSupportItems::createComponent(
+                    testFileUrl("TestComponent.qml"), view->rootContext()));
     QVERIFY(testComponentObject);
 
-    QVERIFY(QQuickDesignerSupportMetaInfo::isSubclassOf(testComponentObject, "QtQuick/Item"));
+    QVERIFY(QQuickDesignerSupportMetaInfo::isSubclassOf(
+                testComponentObject.data(), "QtQuick/Item"));
 }
 
 void tst_qquickdesignersupport::basicStates()
