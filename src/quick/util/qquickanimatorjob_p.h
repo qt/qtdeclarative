c86885735656a421b89ef547f6a6133aec4a7df2
Antti Määttä <antti.maatta@qt.io>
2021-12-02 09:26:18 +0200

    Bound animatorjob value to the given range
    The value() might get called before updateCurrentTime has been called
    for the animator so the m_value has not been initialized.
    Bound the value to the given range in the function to ensure it
    doesn't return invalid values.
    
    Fixes: QTBUG-75799
    Change-Id: I8d60430ed95082b975f49cf7c778e196f2fd5384
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit e0850c918cbe32dc1aca7398becdd9b1bce4ce27)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/util/qquickanimatorjob_p.h
+++ src/quick/util/qquickanimatorjob_p.h
@@ -122,10 +122,16 @@ public:
     virtual void setTarget(QQuickItem *target);
     QQuickItem *target() const { return m_target; }
 
-    void setFrom(qreal from) { m_from = from; }
+    void setFrom(qreal from) {
+        m_from = from;
+        boundValue();
+    }
     qreal from() const { return m_from; }
 
-    void setTo(qreal to) { m_to = to; }
+    void setTo(qreal to) {
+        m_to = to;
+        boundValue();
+    }
     qreal to() const { return m_to; }
 
     void setDuration(int duration) { m_duration = duration; }
@@ -171,6 +177,7 @@ protected:
     void debugAnimation(QDebug d) const override;
 
     qreal progress(int time) const;
+    void boundValue();
 
     QPointer<QQuickItem> m_target;
     QQuickAnimatorController *m_controller;
