a3484ba93997cbf63acb0fc7636b5e888bff65ba
Kai Köhne <kai.koehne@qt.io>
2021-12-10 09:17:42 +0100

    Doc: Use find_package(Qt6 REQUIRED COMPONENTS ...) idiom
    Using REQUIRED as a prefix instead of suffix works better with
    OPTIONAL_COMPONENTS, and is also the order in the CMake manual.
    
    Task-number: QTBUG-98867
    Change-Id: I021c8f0c7f886d8f7037a1dd92ef1e8d747a45f4
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit ad69aca9a6c2fdcc12bd6ecd55d2e417d1af6335)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qmltest/doc/snippets/overview.cmake
+++ src/qmltest/doc/snippets/overview.cmake
@@ -1,4 +1,4 @@
 #! [cmake_use]
-find_package(Qt6 COMPONENTS QuickTest REQUIRED)
+find_package(Qt6 REQUIRED COMPONENTS QuickTest)
 target_link_libraries(mytarget PRIVATE Qt6::QuickTest)
 #! [cmake_use]
