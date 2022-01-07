709e78842567fa464f158bfba609a0b149bb54bb
Ulf Hermann <ulf.hermann@qt.io>
2022-01-07 09:59:47 +0100

    qml{plugindump|importscanner}: Don't mix std::cerr and std::wcerr
    Apparently that causes output to be suppressed.
    
    Fixes: QTBUG-99400
    Change-Id: I4ac6a66a10c7d2c27dfc1efa6d52afa60bdc58d6
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Fawzi Mohamed <fawzi.mohamed@qt.io>
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 5fa9093191c8a0fbf765f3b51a68dea996fed76e)

--- tools/qmlimportscanner/main.cpp
+++ tools/qmlimportscanner/main.cpp
@@ -76,18 +76,18 @@ inline QString linkTargetLiteral()
 
 void printUsage(const QString &appNameIn)
 {
-    const std::wstring appName = appNameIn.toStdWString();
+    const std::string appName = appNameIn.toStdString();
 #ifndef QT_BOOTSTRAPPED
     const QString qmlPath = QLibraryInfo::path(QLibraryInfo::QmlImportsPath);
 #else
     const QString qmlPath = QStringLiteral("/home/user/dev/qt-install/qml");
 #endif
-    std::wcerr
+    std::cerr
         << "Usage: " << appName << " -rootPath path/to/app/qml/directory -importPath path/to/qt/qml/directory\n"
            "       " << appName << " -qmlFiles file1 file2 -importPath path/to/qt/qml/directory\n"
            "       " << appName << " -qrcFiles file1.qrc file2.qrc -importPath path/to/qt/qml/directory\n\n"
            "Example: " << appName << " -rootPath . -importPath "
-        << QDir::toNativeSeparators(qmlPath).toStdWString()
+        << QDir::toNativeSeparators(qmlPath).toStdString()
         << '\n';
 }
 
