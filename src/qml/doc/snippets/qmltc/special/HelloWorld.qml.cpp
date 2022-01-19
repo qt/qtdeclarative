2398f4bdff283789b001bee9f0997db521c86677
Andrei Golubev <andrei.golubev@qt.io>
2022-01-19 17:26:58 +0100

    Document qmltc output format
    Make understanding qmltc output easier by showing how it
    works on a trivial example. Add a paragraph on the generated
    code location as an addition
    
    Fixes: QTBUG-100051
    Change-Id: I0fa0f2c6c60fef7accbe855159275591d9e8bbc6
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 8ec6b990e13c8b41d0415af83c5890ef7a9ee227)

---
+++ src/qml/doc/snippets/qmltc/special/HelloWorld.qml.cpp
@@ -0,0 +1,85 @@
+/****************************************************************************
+**
+** Copyright (C) 2022 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the documentation of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+/* Disclaimer: This file is an "as is" copy of the C++ header generated for the
+   accompanying HelloWorld.qml. Its pieces are used to:
+
+   * verify that the generated code is similar to what is contained in this file
+   * provide documentation snippets for the qmltc docs
+
+   Note: all the stuff below MAGIC_QMLTC_TEST_DELIMITER_LINE comment
+   participates in the aforementioned activities. Prefer to put arbitrary stuff
+   before that comment.
+*/
+
+// MAGIC_QMLTC_TEST_DELIMITER_LINE
+
+//! [qmltc-hello-world-generated]
+class HelloWorld : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    Q_PROPERTY(QString hello WRITE setHello READ hello BINDABLE bindableHello)
+public:
+    HelloWorld(QQmlEngine * engine, QObject * parent = nullptr);
+
+public:
+    void setHello(const QString& hello_);
+    QString hello();
+    QBindable<QString> bindableHello();
+
+    Q_INVOKABLE void printHello(QString prefix, QString suffix);
+
+signals:
+    void created();
+
+    // ...
+};
+//! [qmltc-hello-world-generated]
