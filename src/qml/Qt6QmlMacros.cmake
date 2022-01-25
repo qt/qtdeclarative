f90aba4a53d7b30a963154e01010e683c9e6a869
Andrei Golubev <andrei.golubev@qt.io>
2022-01-25 21:30:41 +0100

    qmltc: Rename CMake command's argument FILES -> QML_FILES
    This way we make it consistent with qt_add_qml_module()
    
    Fixes: QTBUG-100214
    Change-Id: I9f38a8ba3dec978ccdf4ea937ff662ae2449e582
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 97123dbe6755a787e93797f717f8ebf193352b85)
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/qml/Qt6QmlMacros.cmake
+++ src/qml/Qt6QmlMacros.cmake
@@ -1028,7 +1028,7 @@ endfunction()
 function(qt6_target_compile_qml_to_cpp target)
     set(args_option "")
     set(args_single NAMESPACE)
-    set(args_multi FILES IMPORT_PATHS)
+    set(args_multi QML_FILES IMPORT_PATHS)
 
     cmake_parse_arguments(PARSE_ARGV 1 arg
         "${args_option}" "${args_single}" "${args_multi}"
@@ -1037,7 +1037,7 @@ function(qt6_target_compile_qml_to_cpp target)
         message(FATAL_ERROR "Unknown/unexpected arguments: ${arg_UNPARSED_ARGUMENTS}")
     endif()
 
-    if (NOT arg_FILES)
+    if (NOT arg_QML_FILES)
         message(FATAL_ERROR "FILES option not given or contains empty list for target ${target}")
     endif()
 
@@ -1111,7 +1111,7 @@ function(qt6_target_compile_qml_to_cpp target)
         endif()
     endforeach()
 
-    foreach(qml_file_src IN LISTS arg_FILES)
+    foreach(qml_file_src IN LISTS arg_QML_FILES)
         if(NOT qml_file_src MATCHES "\\.(qml)$")
             list(APPEND non_qml_files ${qml_file_src})
             continue()
