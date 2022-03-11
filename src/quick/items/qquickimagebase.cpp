qt/qtdeclarative

commit b6f085a275c76912862463657c2a91e35bd8936b

Image: make default sourceSize respect DPR for scalable images
When loading SVGs where no sourceSize is set, we need to take the DPR
into account so that the image isn't upscaled when the DPR is greater
than 1.

Fixes: QTBUG-58416
Task-number: QTBUG-81018
Pick-to: 6.2
Change-Id: I4356407f63477594c1a057555bf0fb6dabdab415
Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

src/quick/items/qquickimagebase.cpp
@@ -51,6 +51,19 @@
 
 QT_BEGIN_NAMESPACE
 
+bool isScalableImageFormat(const QUrl &url)
+{
+    if (url.scheme() == QLatin1String("image")) {
+        // Custom image provider; let the provider deal with it.
+        return false;
+    }
+
+    const QString stringUrl = url.path(QUrl::PrettyDecoded);
+    return stringUrl.endsWith(QLatin1String("svg"))
+        || stringUrl.endsWith(QLatin1String("svgz"))
+        || stringUrl.endsWith(QLatin1String("pdf"));
+}
+
 // This function gives derived classes the chance set the devicePixelRatio
 // if they're not happy with our implementation of it.
 bool QQuickImageBasePrivate::updateDevicePixelRatio(qreal targetDevicePixelRatio)
@@ -58,17 +71,7 @@ bool QQuickImageBasePrivate::updateDevicePixelRatio(qreal targetDevicePixelRatio
     // QQuickImageProvider and SVG and PDF can generate a high resolution image when
     // sourceSize is set. If sourceSize is not set then the provider default size will
     // be used, as usual.
-    bool setDevicePixelRatio = false;
-    if (url.scheme() == QLatin1String("image")) {
-        setDevicePixelRatio = true;
-    } else {
-        QString stringUrl = url.path(QUrl::PrettyDecoded);
-        if (stringUrl.endsWith(QLatin1String("svg")) ||
-            stringUrl.endsWith(QLatin1String("svgz")) ||
-            stringUrl.endsWith(QLatin1String("pdf"))) {
-            setDevicePixelRatio = true;
-        }
-    }
+    const bool setDevicePixelRatio = isScalableImageFormat(url);
 
     if (setDevicePixelRatio)
         devicePixelRatio = targetDevicePixelRatio;
@@ -326,7 +329,7 @@ void QQuickImageBase::loadPixmap(const QUrl &url, LoadPixmapOptions loadOptions)
         const qreal targetDevicePixelRatio = (window() ? window()->effectiveDevicePixelRatio() : qApp->devicePixelRatio());
         d->devicePixelRatio = 1.0;
         bool updatedDevicePixelRatio = false;
-        if (d->sourcesize.isValid())
+        if (d->sourcesize.isValid() || isScalableImageFormat(d->url))
             updatedDevicePixelRatio = d->updateDevicePixelRatio(targetDevicePixelRatio);
 
         if (!updatedDevicePixelRatio) {
@@ -343,7 +346,8 @@ void QQuickImageBase::loadPixmap(const QUrl &url, LoadPixmapOptions loadOptions)
                 (loadOptions & HandleDPR) ? d->sourcesize * d->devicePixelRatio : QSize(),
                 options,
                 (loadOptions & UseProviderOptions) ? d->providerOptions : QQuickImageProviderOptions(),
-                d->currentFrame, d->frameCount);
+                d->currentFrame, d->frameCount,
+                d->devicePixelRatio);
 
     if (d->pix.isLoading()) {
         if (d->progress != 0.0) {

src/quick/util/qquickimageprovider.cpp
@@ -681,14 +681,21 @@ QQuickImageResponse *QQuickImageProviderWithOptions::requestImageResponse(const 
     and \a options. If the calculation otherwise concludes that scaled loading
     is not recommended, an invalid size is returned.
 */
-QSize QQuickImageProviderWithOptions::loadSize(const QSize &originalSize, const QSize &requestedSize, const QByteArray &format, const QQuickImageProviderOptions &options)
+QSize QQuickImageProviderWithOptions::loadSize(const QSize &originalSize, const QSize &requestedSize, const QByteArray &format, const QQuickImageProviderOptions &options,
+                                               qreal devicePixelRatio)
 {
     QSize res;
-    if ((requestedSize.width() <= 0 && requestedSize.height() <= 0) || originalSize.isEmpty())
+    const bool formatIsScalable = (format == "svg" || format == "svgz" || format == "pdf");
+    const bool noRequestedSize = requestedSize.width() <= 0 && requestedSize.height() <= 0;
+    if ((noRequestedSize && !formatIsScalable) || originalSize.isEmpty())
         return res;
 
+    // If no sourceSize was set and we're loading an SVG, ensure that we provide
+    // a default size that accounts for DPR so that the image isn't blurry.
+    if (noRequestedSize && formatIsScalable)
+        return originalSize * devicePixelRatio;
+
     const bool preserveAspectCropOrFit = options.preserveAspectRatioCrop() || options.preserveAspectRatioFit();
-    const bool formatIsScalable = (format == "svg" || format == "svgz" || format == "pdf");
 
     if (!preserveAspectCropOrFit && formatIsScalable && !requestedSize.isEmpty())
         return requestedSize;

src/quick/util/qquickpixmapcache.cpp
@@ -419,7 +419,8 @@ static void maybeRemoveAlpha(QImage *image)
 
 static bool readImage(const QUrl& url, QIODevice *dev, QImage *image, QString *errorString, QSize *impsize, int *frameCount,
                       const QRect &requestRegion, const QSize &requestSize, const QQuickImageProviderOptions &providerOptions,
-                      QQuickImageProviderOptions::AutoTransform *appliedTransform = nullptr, int frame = 0)
+                      QQuickImageProviderOptions::AutoTransform *appliedTransform = nullptr, int frame = 0,
+                      qreal devicePixelRatio = 1.0)
 {
     QImageReader imgio(dev);
     if (providerOptions.autoTransform() != QQuickImageProviderOptions::UsePluginDefaultTransform)
@@ -433,7 +434,7 @@ static bool readImage(const QUrl& url, QIODevice *dev, QImage *image, QString *e
     if (frameCount)
         *frameCount = imgio.imageCount();
 
-    QSize scSize = QQuickImageProviderWithOptions::loadSize(imgio.size(), requestSize, imgio.format(), providerOptions);
+    QSize scSize = QQuickImageProviderWithOptions::loadSize(imgio.size(), requestSize, imgio.format(), providerOptions, devicePixelRatio);
     if (scSize.isValid())
         imgio.setScaledSize(scSize);
     if (!requestRegion.isNull())
@@ -1334,7 +1335,8 @@ void QQuickPixmapData::removeFromCache()
 
 static QQuickPixmapData* createPixmapDataSync(QQuickPixmap *declarativePixmap, QQmlEngine *engine, const QUrl &url,
                                               const QRect &requestRegion, const QSize &requestSize,
-                                              const QQuickImageProviderOptions &providerOptions, int frame, bool *ok)
+                                              const QQuickImageProviderOptions &providerOptions, int frame, bool *ok,
+                                              qreal devicePixelRatio)
 {
     if (url.scheme() == QLatin1String("image")) {
         QSize readSize;
@@ -1424,7 +1426,8 @@ static QQuickPixmapData* createPixmapDataSync(QQuickPixmap *declarativePixmap, Q
             QImage image;
             QQuickImageProviderOptions::AutoTransform appliedTransform = providerOptions.autoTransform();
             int frameCount;
-            if (readImage(url, &f, &image, &errorString, &readSize, &frameCount, requestRegion, requestSize, providerOptions, &appliedTransform, frame)) {
+            if (readImage(url, &f, &image, &errorString, &readSize, &frameCount, requestRegion, requestSize,
+                          providerOptions, &appliedTransform, frame, devicePixelRatio)) {
                 *ok = true;
                 return new QQuickPixmapData(declarativePixmap, url, QQuickTextureFactory::textureFactoryForImage(image), readSize, requestRegion, requestSize,
                                             providerOptions, appliedTransform, frame, frameCount);
@@ -1641,7 +1644,8 @@ void QQuickPixmap::load(QQmlEngine *engine, const QUrl &url, const QRect &reques
 }
 
 void QQuickPixmap::load(QQmlEngine *engine, const QUrl &url, const QRect &requestRegion, const QSize &requestSize,
-                        QQuickPixmap::Options options, const QQuickImageProviderOptions &providerOptions, int frame, int frameCount)
+                        QQuickPixmap::Options options, const QQuickImageProviderOptions &providerOptions, int frame, int frameCount,
+                        qreal devicePixelRatio)
 {
     if (d) {
         d->declarativePixmaps.remove(this);
@@ -1691,7 +1695,7 @@ void QQuickPixmap::load(QQmlEngine *engine, const QUrl &url, const QRect &reques
         if (!(options & QQuickPixmap::Asynchronous)) {
             bool ok = false;
             PIXMAP_PROFILE(pixmapStateChanged<QQuickProfiler::PixmapLoadingStarted>(url));
-            d = createPixmapDataSync(this, engine, url, requestRegion, requestSize, providerOptions, frame, &ok);
+            d = createPixmapDataSync(this, engine, url, requestRegion, requestSize, providerOptions, frame, &ok, devicePixelRatio);
             if (ok) {
                 PIXMAP_PROFILE(pixmapLoadingFinished(url, QSize(width(), height())));
                 if (options & QQuickPixmap::Cache)

src/quick/util/qquickpixmapcache_p.h
@@ -172,7 +172,8 @@ public:
     void load(QQmlEngine *, const QUrl &, const QRect &requestRegion, const QSize &requestSize);
     void load(QQmlEngine *, const QUrl &, const QRect &requestRegion, const QSize &requestSize, QQuickPixmap::Options options);
     void load(QQmlEngine *, const QUrl &, const QRect &requestRegion, const QSize &requestSize,
-              QQuickPixmap::Options options, const QQuickImageProviderOptions &providerOptions, int frame = 0, int frameCount = 1);
+              QQuickPixmap::Options options, const QQuickImageProviderOptions &providerOptions, int frame = 0, int frameCount = 1,
+              qreal devicePixelRatio = 1.0);
 
     void clear();
     void clear(QObject *);
@@ -214,7 +215,8 @@ public:
     virtual QQuickTextureFactory *requestTexture(const QString &id, QSize *size, const QSize &requestedSize, const QQuickImageProviderOptions &options);
     virtual QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize, const QQuickImageProviderOptions &options);
 
-    static QSize loadSize(const QSize &originalSize, const QSize &requestedSize, const QByteArray &format, const QQuickImageProviderOptions &options);
+    static QSize loadSize(const QSize &originalSize, const QSize &requestedSize, const QByteArray &format, const QQuickImageProviderOptions &options,
+                          qreal devicePixelRatio = 1.0);
     static QQuickImageProviderWithOptions *checkedCast(QQuickImageProvider *provider);
 };
 

commit 3c3acf73346bed194db9b76b99196ece5f03b1b1

Ensure that image providers scale by default
This was broken in b6f085a275c76912862463657c2a91e35bd8936b.

Change-Id: Ic2f9da5cf18098528de3165867f2befbaf0ed1c2
Fixes: QTBUG-100355
Pick-to: 6.2 6.3
Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

src/quick/items/qquickimagebase.cpp
@@ -53,10 +53,8 @@ QT_BEGIN_NAMESPACE
 
 bool isScalableImageFormat(const QUrl &url)
 {
-    if (url.scheme() == QLatin1String("image")) {
-        // Custom image provider; let the provider deal with it.
-        return false;
-    }
+    if (url.scheme() == QLatin1String("image"))
+        return true;
 
     const QString stringUrl = url.path(QUrl::PrettyDecoded);
     return stringUrl.endsWith(QLatin1String("svg"))