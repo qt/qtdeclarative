qt/qtdeclarative

commit 0711dd1ad767bd8375638b678871a8c99dca90e2

Fix some qmllint warnings
Most—if not all—of these are unqualified accesses.

Task-number: QTBUG-83755
Change-Id: I5545bc868114f27099c882d41bc799cafb27ded8
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
(cherry picked from commit 095524dd31aef56d804e89afd74a881e8575ef20)
Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

src/quickcontrols2/doc/snippets/qtquickcontrols2-combobox-custom.qml
@@ -43,6 +43,8 @@ ComboBox {
             verticalAlignment: Text.AlignVCenter
         }
         highlighted: control.highlightedIndex === index
+
+        required property int index
     }
 
     indicator: Canvas {

src/quickcontrols2/doc/snippets/qtquickcontrols2-swipedelegate.qml
@@ -40,8 +40,11 @@ ListView {
     }
     delegate: SwipeDelegate {
         id: swipeDelegate
-        text: model.sender + " - " + model.title
+        text: sender + " - " + title
         width: parent.width
+
+        required property string sender
+        required property string title
 
         ListView.onRemove: SequentialAnimation {
             PropertyAction {
@@ -72,6 +75,8 @@ ListView {
             anchors.right: parent.right
 
             SwipeDelegate.onClicked: listView.model.remove(index)
+
+            required property int index
 
             background: Rectangle {
                 color: deleteLabel.SwipeDelegate.pressed ? Qt.darker("tomato", 1.1) : "tomato"

src/quickcontrols2/doc/snippets/qtquickcontrols2-tumbler-custom.qml
@@ -57,6 +57,8 @@ Tumbler {
         horizontalAlignment: Text.AlignHCenter
         verticalAlignment: Text.AlignVCenter
         opacity: 1.0 - Math.abs(Tumbler.displacement) / (control.visibleItemCount / 2)
+
+        required property var modelData
     }
 
     Rectangle {

src/quickcontrols2/basic/PageIndicator.qml
@@ -56,7 +56,10 @@ T.PageIndicator {
         radius: width / 2
         color: control.palette.dark
 
-        opacity: index === currentIndex ? 0.95 : pressed ? 0.7 : 0.45
+        opacity: index === control.currentIndex ? 0.95 : pressed ? 0.7 : 0.45
+
+        required property int index
+
         Behavior on opacity { OpacityAnimator { duration: 100 } }
     }
 

src/quickcontrols2/basic/ToolSeparator.qml
@@ -50,8 +50,8 @@ T.ToolSeparator {
     verticalPadding: vertical ? 2 : 6
 
     contentItem: Rectangle {
-        implicitWidth: vertical ? 1 : 30
-        implicitHeight: vertical ? 30 : 1
+        implicitWidth: control.vertical ? 1 : 30
+        implicitHeight: control.vertical ? 30 : 1
         color: control.palette.mid
     }
 }

src/quickcontrols2/basic/Tumbler.qml
@@ -53,6 +53,12 @@ T.Tumbler {
         opacity: 1.0 - Math.abs(Tumbler.displacement) / (control.visibleItemCount / 2)
         horizontalAlignment: Text.AlignHCenter
         verticalAlignment: Text.AlignVCenter
+
+        // We use required property here to satisfy qmllint, but that means
+        // we also need to declare the index for the attached properties
+        // (see QQuickTumblerAttachedPrivate::init).
+        required property var modelData
+        required property int index
     }
 
     contentItem: TumblerView {

src/quickcontrols2/doc/snippets/qtquickcontrols2-busyindicator-custom.qml
@@ -64,18 +64,22 @@ BusyIndicator {
                 model: 6
 
                 Rectangle {
+                    id: delegate
                     x: item.width / 2 - width / 2
                     y: item.height / 2 - height / 2
                     implicitWidth: 10
                     implicitHeight: 10
                     radius: 5
                     color: "#21be2b"
+
+                    required property int index
+
                     transform: [
                         Translate {
                             y: -Math.min(item.width, item.height) * 0.5 + 5
                         },
                         Rotation {
-                            angle: index / repeater.count * 360
+                            angle: delegate.index / repeater.count * 360
                             origin.x: 5
                             origin.y: 5
                         }

src/quickcontrols2/doc/snippets/qtquickcontrols2-itemdelegate.qml
@@ -39,6 +39,8 @@ ListView {
         text: modelData
         width: parent.width
         onClicked: console.log("clicked:", modelData)
+
+        required property string modelData
     }
 
     ScrollIndicator.vertical: ScrollIndicator { }

commit b1b52d5e44f52611488bdb310ed863631d2c3280

Doc: fix broken customization example snippets
Some of these were broken by 0711dd1ad767bd8375638b678871a8c99dca90e2.

Change-Id: I7a97edb0ae5c5172fda7aa9cd8d44b51879857b8
Fixes: QTBUG-102138
Pick-to: 6.2 6.3
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/quickcontrols2/doc/snippets/qtquickcontrols2-combobox-custom.qml
@@ -45,6 +45,7 @@ ComboBox {
         highlighted: control.highlightedIndex === index
 
         required property int index
+        required property var modelData
     }
 
     indicator: Canvas {

src/quickcontrols2/doc/snippets/qtquickcontrols2-swipedelegate.qml
@@ -41,7 +41,7 @@ ListView {
     delegate: SwipeDelegate {
         id: swipeDelegate
         text: sender + " - " + title
-        width: parent.width
+        width: listView.width
 
         required property string sender
         required property string title

src/quickcontrols2/doc/snippets/qtquickcontrols2-tumbler-custom.qml
@@ -59,6 +59,7 @@ Tumbler {
         opacity: 1.0 - Math.abs(Tumbler.displacement) / (control.visibleItemCount / 2)
 
         required property var modelData
+        required property int index
     }
 
     Rectangle {