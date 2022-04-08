cc50055e615ca4498ac5679c04a3a76ec40720a5
Shawn Rutledge <shawn.rutledge@qt.io>
2022-04-08 21:16:30 +0200

    tst_QQuickControl::flickable: prove touch move after pressDelay is OK
    This test was added to prove that a control (such as a Button) receives
    the touch release even when the touch was converted to a mouse event by
    going through Flickable's delayed press logic. In Qt 6, that replays an
    authentic delayed touch event; so the test continued to pass. Now we
    extend it to prove that even if the touch point "wiggles" a little
    before the release, it's OK: the Control keeps the grab, so it gets the
    touch release. We are no longer relying on the workaround from
    025f938c1b4676782674d54375e1e4e560e4b6cd to compare the press and
    release positions.
    
    Task-number: QTBUG-102036
    Task-number: QTBUG-102037
    Change-Id: I27b528b4d1180a2557c2c24874e780c26780c4dd
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

--- tests/auto/quickcontrols2/qquickcontrol/tst_qquickcontrol.cpp
+++ tests/auto/quickcontrols2/qquickcontrol/tst_qquickcontrol.cpp
@@ -88,9 +88,12 @@ void tst_QQuickControl::flickable()
     QSignalSpy buttonClickedSpy(button, SIGNAL(clicked()));
     QVERIFY(buttonClickedSpy.isValid());
 
-    QTest::touchEvent(window, touchDevice.data()).press(0, QPoint(button->width() / 2, button->height() / 2));
+    QPoint p(button->width() / 2, button->height() / 2);
+    QTest::touchEvent(window, touchDevice.data()).press(0, p);
     QTRY_COMPARE(buttonPressedSpy.count(), 1);
-    QTest::touchEvent(window, touchDevice.data()).release(0, QPoint(button->width() / 2, button->height() / 2));
+    p += QPoint(1, 1); // less than the drag threshold
+    QTest::touchEvent(window, touchDevice.data()).move(0, p);
+    QTest::touchEvent(window, touchDevice.data()).release(0, p);
     QTRY_COMPARE(buttonReleasedSpy.count(), 1);
     QTRY_COMPARE(buttonClickedSpy.count(), 1);
 }
