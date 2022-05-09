7d8efe56cbe75c4769b24ba978da4d308a2a291f
Marc Mutz <marc.mutz@qt.io>
2022-05-09 15:40:41 +0200

    Add a TU for QQmlDebugServerConnection{,Factory} to de-inline dtor & includemocs
    Destructors of polymorphic classes should be out-of-line.
    Header moc files should be included into the implementation files.
    
    Both need a TU, so add one.
    
    Task-number: QTBUG-102948
    Task-number: QTBUG-45582
    Change-Id: I2b59d18fb4530245cccace895ff86ab0634d9f55
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qml/debugger/qqmldebugserverconnection.cpp
+++ src/qml/debugger/qqmldebugserverconnection.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2022 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQml module of the Qt Toolkit.
@@ -37,53 +37,16 @@
 **
 ****************************************************************************/
 
-#ifndef QQMLDEBUGSERVERCONNECTION_P_H
-#define QQMLDEBUGSERVERCONNECTION_P_H
-
-#include <private/qtqmlglobal_p.h>
-#include <QtCore/qobject.h>
-
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
+#include "qqmldebugserverconnection_p.h"
 
 QT_BEGIN_NAMESPACE
 
-class QQmlDebugServer;
-class Q_QML_PRIVATE_EXPORT QQmlDebugServerConnection : public QObject
-{
-    Q_OBJECT
-public:
-    QQmlDebugServerConnection(QObject *parent = nullptr) : QObject(parent) {}
-    ~QQmlDebugServerConnection() override;
+QQmlDebugServerConnection::~QQmlDebugServerConnection()
+    = default;
 
-    virtual void setServer(QQmlDebugServer *server) = 0;
-    virtual bool setPortRange(int portFrom, int portTo, bool block, const QString &hostaddress) = 0;
-    virtual bool setFileName(const QString &fileName, bool block) = 0;
-    virtual bool isConnected() const = 0;
-    virtual void disconnect() = 0;
-    virtual void waitForConnection() = 0;
-    virtual void flush() = 0;
-};
-
-class Q_QML_PRIVATE_EXPORT QQmlDebugServerConnectionFactory : public QObject
-{
-    Q_OBJECT
-public:
-    ~QQmlDebugServerConnectionFactory() override;
-    virtual QQmlDebugServerConnection *create(const QString &key) = 0;
-};
-
-#define QQmlDebugServerConnectionFactory_iid "org.qt-project.Qt.QQmlDebugServerConnectionFactory"
-Q_DECLARE_INTERFACE(QQmlDebugServerConnectionFactory, QQmlDebugServerConnectionFactory_iid)
+QQmlDebugServerConnectionFactory::~QQmlDebugServerConnectionFactory()
+    = default;
 
 QT_END_NAMESPACE
 
-#endif // QQMLDEBUGSERVERCONNECTION_H
+#include "moc_qqmldebugserverconnection_p.cpp"
