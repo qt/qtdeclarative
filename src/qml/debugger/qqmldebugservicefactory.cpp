868a5c19bcd45df27dca365264084c3bbb365701
Marc Mutz <marc.mutz@qt.io>
2022-05-09 15:40:41 +0200

    Add a TU for QQmlDebugServiceFactory to de-inline dtor & includemocs
    Destructors of polymorphic classes should be out-of-line.
    Header moc files should be included into the implementation files.
    
    Both need a TU, so add one.
    
    Task-number: QTBUG-102948
    Task-number: QTBUG-45582
    Change-Id: Ifbfb7f462aa94c7f7e2db8a179e06392f021e097
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qml/debugger/qqmldebugservicefactory.cpp
+++ src/qml/debugger/qqmldebugservicefactory.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2022 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQml module of the Qt Toolkit.
@@ -37,34 +37,13 @@
 **
 ****************************************************************************/
 
-#ifndef QQMLDEBUGSERVICEFACTORY_P_H
-#define QQMLDEBUGSERVICEFACTORY_P_H
-
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
-
-#include "qqmldebugservice_p.h"
+#include "qqmldebugservicefactory_p.h"
 
 QT_BEGIN_NAMESPACE
 
-class Q_QML_PRIVATE_EXPORT QQmlDebugServiceFactory : public QObject
-{
-    Q_OBJECT
-public:
-    ~QQmlDebugServiceFactory() override;
-    virtual QQmlDebugService *create(const QString &key) = 0;
-};
-
-#define QQmlDebugServiceFactory_iid "org.qt-project.Qt.QQmlDebugServiceFactory"
+QQmlDebugServiceFactory::~QQmlDebugServiceFactory()
+    = default;
 
 QT_END_NAMESPACE
 
-#endif // QQMLDEBUGSERVICEFACTORY_P_H
+#include "moc_qqmldebugservicefactory_p.cpp"
