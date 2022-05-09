c2b1aad7918d23f3094525c0a57ae4c9fd88b81e
Marc Mutz <marc.mutz@qt.io>
2022-05-09 15:40:41 +0200

    Add a TU for QV4::Debugging::Debugger to de-inline dtor & includemocs
    Destructors of polymorphic classes should be out-of-line.
    Header moc files should be included into the implementation files.
    
    Both need a TU, so add one.
    
    Task-number: QTBUG-102948
    Task-number: QTBUG-45582
    Change-Id: Ia596ec1cab1e4c1f91e42cf217a7df7cbd0d91ad
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qml/jsruntime/qv4debugging.cpp
+++ src/qml/jsruntime/qv4debugging.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2022 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the QtQml module of the Qt Toolkit.
@@ -37,61 +37,17 @@
 **
 ****************************************************************************/
 
-#ifndef QV4DEBUGGING_H
-#define QV4DEBUGGING_H
+#include "qv4debugging_p.h"
 
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
-
-#include "qv4global_p.h"
-#include <private/qv4staticvalue_p.h>
-#include <QtCore/qobject.h>
+#if QT_CONFIG(qml_debug)
 
 QT_BEGIN_NAMESPACE
 
-namespace QV4 {
-namespace Debugging {
-
-#if !QT_CONFIG(qml_debug)
-
-class Debugger
-{
-public:
-    bool pauseAtNextOpportunity() const { return false; }
-    void maybeBreakAtInstruction() {}
-    void enteringFunction() {}
-    void leavingFunction(const ReturnedValue &) {}
-    void aboutToThrow() {}
-};
-
-#else
-
-class Q_QML_EXPORT Debugger : public QObject
-{
-    Q_OBJECT
-
-public:
-    ~Debugger() override;
-    virtual bool pauseAtNextOpportunity() const = 0;
-    virtual void maybeBreakAtInstruction() = 0;
-    virtual void enteringFunction() = 0;
-    virtual void leavingFunction(const ReturnedValue &retVal) = 0;
-    virtual void aboutToThrow() = 0;
-};
-
-#endif // QT_NO_QML_DEBUGGING
-
-} // namespace Debugging
-} // namespace QV4
+QV4::Debugging::Debugger::~Debugger()
+    = default;
 
 QT_END_NAMESPACE
 
-#endif // QV4DEBUGGING_H
+#include "moc_qv4debugging_p.cpp"
+
+#endif // QT_CONFIG(qml_debug)
