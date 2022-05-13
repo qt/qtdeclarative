9d16facde09d7bc9c16f832f3d46cb6e01cda0cc
Michal Klocek <michal.klocek@qt.io>
2022-05-13 19:55:54 +0200

    Make find_package for qmlsc look only in host qt
    This is a edge case, where host qt does not have qmlsc compiler,
    however cmake pulls targets from target's sysroot and
    complains that the compiler is missing.
    
    Make sure we look only in the qt host and if there is no compiler
    simply return not found.
    
    Change-Id: I452dd29cd01e2968b962f99f7355dad34e58723c
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 80cb4091d6cc5b7d13b80668f6e393788d763b3e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/Qt6QmlFindQmlscInternal.cmake
+++ src/qml/Qt6QmlFindQmlscInternal.cmake
@@ -3,6 +3,23 @@ if (QT_NO_FIND_QMLSC)
 endif()
 
 set(QT_NO_FIND_QMLSC TRUE)
+if(NOT "${QT_HOST_PATH}" STREQUAL "")
+    # Make sure that the tools find the host tools and does not try the target
+    set(BACKUP_qmlsc_CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH})
+    set(BACKUP_qmlsc_CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH})
+    set(BACKUP_qmlsc_CMAKE_SYSROOT ${CMAKE_SYSROOT})
+    set(CMAKE_PREFIX_PATH "${QT_HOST_PATH_CMAKE_DIR}")
+    list(APPEND CMAKE_PREFIX_PATH "${_qt_additional_host_packages_prefix_paths}")
+    set(CMAKE_FIND_ROOT_PATH "${QT_HOST_PATH}")
+    list(APPEND CMAKE_FIND_ROOT_PATH "${_qt_additional_host_packages_root_paths}")
+    unset(CMAKE_SYSROOT)
+endif()
 
-# FIXME: Make this work with cross-builds
 find_package(Qt6QmlCompilerPlusPrivate QUIET)
+
+if(NOT "${QT_HOST_PATH}" STREQUAL "")
+    set(CMAKE_PREFIX_PATH ${BACKUP_qmlsc_CMAKE_PREFIX_PATH})
+    set(CMAKE_FIND_ROOT_PATH ${BACKUP_qmlsc_CMAKE_FIND_ROOT_PATH})
+    set(CMAKE_SYSROOT ${BACKUP_qmlsc_CMAKE_SYSROOT})
+endif()
+
