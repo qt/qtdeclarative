688060adf9d6bd5fff885120ecce040e900edb88
Fawzi Mohamed <fawzi.mohamed@qt.io>
2022-05-18 13:22:26 +0200

    qmldom: standalone fix macro clash in 6.2 and 6.3
    qtqmlcompilerglobal_p.h used to define Q_QMLCOMPILER_PRIVATE_EXPORT
    which clashes with qtqmlcompilerexports_p.h.
    This was fixed in ce53e48504f, but when compiling standalone against
    those versions, handle the case in which the dome include is included
    after the QtQml/private/qtqmlcompilerglobal_p.h
    
    Change-Id: I6c0e237e4d101bf260fb73aaca940a40080cdb7f
    Reviewed-by: Maximilian Goldstein <max.goldstein@qt.io>

--- src/qmldom/standalone/private/qtqmlcompilerexports_p.h
+++ src/qmldom/standalone/private/qtqmlcompilerexports_p.h
@@ -38,7 +38,6 @@
 #include "../qqmldom_global.h"
 #ifndef QTQMLCOMPILEREXPORTS_P_H
 #define QTQMLCOMPILEREXPORTS_P_H
-#define Q_QMLCOMPILER_PRIVATE_EXPORT QMLDOM_EXPORT
 #if QT_VERSION < QT_VERSION_CHECK(6, 4, 0)
 #include <QtCore/qstring.h>
 namespace Qt {
@@ -58,5 +57,9 @@ inline QString operator"" _s(const char16_t *str, size_t size) noexcept
 } // StringLiterals
 } // Literals
 } // Qt
+#ifdef Q_QMLCOMPILER_PRIVATE_EXPORT // clashing declaration fixed in ce53e48504f
+#undef Q_QMLCOMPILER_PRIVATE_EXPORT
 #endif
+#endif
+#define Q_QMLCOMPILER_PRIVATE_EXPORT QMLDOM_EXPORT
 #endif
