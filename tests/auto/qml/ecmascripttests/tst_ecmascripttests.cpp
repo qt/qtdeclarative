c9377cd6499e86ee147c3644fb9271a19a199443
Edward Welbourne <edward.welbourne@qt.io>
2022-05-30 11:39:42 +0200

    Suppress ES-compiler warnings against the ES-262 test-suite
    V4's ES-compiler warns about assorted variables in the ES-262
    test-suite being used before declaration. We aren't about to fix that
    test-suite, so the warnings are just noise to us. As there are many of
    them, they overflow the QTestLib logger's -maxwarnings threshold,
    causing actually relevant messages (like test failures) to be lost in
    the output.
    
    Install a trivial QLoggingCategory::CategoryFilter to filter out
    warnings and debug for the qt.qml.compiler category (and all
    sub-categories of it). Condition this on a define so that someone who
    actually wants to see those warnings can turn them on.
    
    Pick-to: 6.4
    Change-Id: If7decc241b407e816f5d7979b32f31a9f614d30f
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- tests/auto/qml/ecmascripttests/tst_ecmascripttests.cpp
+++ tests/auto/qml/ecmascripttests/tst_ecmascripttests.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2017 The Qt Company Ltd.
+** Copyright (C) 2022 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the test suite of the Qt Toolkit.
@@ -26,7 +26,6 @@
 **
 ****************************************************************************/
 
-
 #include <QtTest/QtTest>
 #include <QProcess>
 #include <QLibraryInfo>
@@ -37,9 +36,52 @@ class tst_EcmaScriptTests : public QObject
     Q_OBJECT
 
 private slots:
+    void initTestCase();
+    void cleanupTestCase();
     void runInterpreted();
     void runJitted();
+
+private:
+    static QLoggingCategory::CategoryFilter priorFilter;
+    static void filterCategories(QLoggingCategory *category);
 };
+QLoggingCategory::CategoryFilter tst_EcmaScriptTests::priorFilter = nullptr;
+
+static inline bool isNoise(QByteArrayView name)
+{
+#ifdef QT_V4_WANT_ES262_WARNINGS
+    return false;
+#else
+    const QByteArrayView noisy("qt.qml.compiler");
+    return name.startsWith(noisy) && (name.size() <= noisy.size() || name[noisy.size()] == '.');
+#endif
+}
+
+void tst_EcmaScriptTests::filterCategories(QLoggingCategory *category)
+{
+    if (priorFilter)
+        priorFilter(category);
+
+    if (isNoise(category->categoryName())) {
+        category->setEnabled(QtDebugMsg, false);
+        category->setEnabled(QtWarningMsg, false);
+    }
+}
+
+void tst_EcmaScriptTests::initTestCase()
+{
+    /* Suppress lcQmlCompiler's "qt.qml.compiler" warnings; we aren't in a
+       position to fix test262's many warnings and they flood messages so we
+       didn't get to see actual failures unless we passed -maxwarnings with a
+       huge value on the command-line (resulting in huge log output).
+    */
+    priorFilter = QLoggingCategory::installFilter(filterCategories);
+}
+
+void tst_EcmaScriptTests::cleanupTestCase()
+{
+    QLoggingCategory::installFilter(priorFilter);
+}
 
 void tst_EcmaScriptTests::runInterpreted()
 {
