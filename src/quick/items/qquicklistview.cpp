538c36af0a175451ea5a4f6f3d05ac68a7fc09a3
Mitch Curtis <mitch.curtis@qt.io>
2022-06-06 16:02:15 +0800

    Item views: fix error when accessing attached view from non-delegates
    Ensure that the view is set on the relevant attached objects before
    QQmlComponent::completeCreate() is called, which would otherwise result
    in a TypeError because the view would be set too late.
    
    Fixes: QTBUG-104026
    Task-number: QTBUG-98718
    Change-Id: Ic65370bd4534e7452f2377ab4d60a74badf02079
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
    (cherry picked from commit dbd108a7997f129004bbed523db75d4aa9d0ab6c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/items/qquicklistview.cpp
+++ src/quick/items/qquicklistview.cpp
@@ -130,6 +130,8 @@ public:
     bool hasStickyHeader() const override;
     bool hasStickyFooter() const override;
 
+    void initializeComponentItem(QQuickItem *item) const override;
+
     void changedVisibleIndex(int newIndex) override;
     void initializeCurrentItem() override;
 
@@ -1573,6 +1575,14 @@ bool QQuickListViewPrivate::hasStickyHeader() const
 bool QQuickListViewPrivate::hasStickyFooter() const
 {
     return footer && footerPositioning != QQuickListView::InlineFooter;
+}
+
+void QQuickListViewPrivate::initializeComponentItem(QQuickItem *item) const
+{
+    QQuickListViewAttached *attached = static_cast<QQuickListViewAttached *>(
+            qmlAttachedPropertiesObject<QQuickListView>(item));
+    if (attached) // can be null for default components (see createComponentItem)
+        attached->setView(const_cast<QQuickListView*>(q_func()));
 }
 
 void QQuickListViewPrivate::itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change,
