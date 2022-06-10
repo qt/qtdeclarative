c3503e19e71a9fecf08a181c50d90043a4ea4edf
Maximilian Goldstein <max.goldstein@qt.io>
2022-06-10 11:38:49 +0200

    tst_qmlsplitlib: Utilize qmlintegration.h
    Now that we have split integration into a separate module, use that
    instead.
    
    Also fixes a warning about redefining QML_SINGLETON.
    
    Change-Id: Iad54ac7815957222d64e96342e48bb9eb25be2c0
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: Andrei Golubev <andrei.golubev@qt.io>

--- tests/auto/qml/qmlsplitlib/lib.h
+++ tests/auto/qml/qmlsplitlib/lib.h
@@ -5,38 +5,9 @@
 #define SPLITLIB_LIB_H
 #include "tst_qmlsplitlib_library_export.h"
 
+#include <QtQmlIntegration/qqmlintegration.h>
 #include <QtCore/qglobal.h>
 #include <QObject>
-
-// FIXME: should at some point be in qtbase or separate repo
-// taken from qqmlregistration.h
-#define QML_ELEMENT \
-    Q_CLASSINFO("QML.Element", "auto")
-
-#define QML_PRIVATE_NAMESPACE \
-    QT_PREPEND_NAMESPACE(QQmlPrivate)
-
-#define QML_REGISTER_TYPES_AND_REVISIONS \
-    QT_PREPEND_NAMESPACE(qmlRegisterTypesAndRevisions)
-
-QT_BEGIN_NAMESPACE
-namespace QQmlPrivate {
-    template<typename, typename> struct QmlSingleton;
-
-}
-template<typename... Args>
-void qmlRegisterTypesAndRevisions(const char *uri, int versionMajor, QList<int> *);
-QT_END_NAMESPACE
-
-#define QML_SINGLETON \
-    Q_CLASSINFO("QML.Singleton", "true") \
-    enum class QmlIsSingleton {yes = true}; \
-    template<typename, typename> friend struct QML_PRIVATE_NAMESPACE::QmlSingleton; \
-    template<typename... Args> \
-    friend void QML_REGISTER_TYPES_AND_REVISIONS(const char *uri, int versionMajor, QList<int> *);
-
-#define QML_NAMED_ELEMENT(NAME) \
-    Q_CLASSINFO("QML.Element", #NAME)
 
 class TST_QMLSPLITLIB_LIBRARY_EXPORT SplitLib : public QObject
 {
