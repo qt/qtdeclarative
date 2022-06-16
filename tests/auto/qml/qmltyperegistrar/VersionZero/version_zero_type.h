ba7cc28ee508c3230deccef78ca4952e50380313
Alexandru Croitor <alexandru.croitor@qt.io>
2022-06-16 17:07:46 +0200

    Qml: Allow major version of module to be zero
    If there is no QML.AddedInVersion then the component should be available
    from version X.0, with X being the current version of the module.
    
    Amends 464abaec05e0aa0ab699243412c50a5d5ac30742
    
    Fixes: QTBUG-104373
    Change-Id: If30b66df384fe815c41d2a0b5ca7b034b9955956
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 9225723a317b8c7e805416592b78aca0ed320049)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/qml/qmltyperegistrar/VersionZero/version_zero_type.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef VERSION_ZERO_TYPE_H
+#define VERSION_ZERO_TYPE_H
+
+#include <QtQml/qqml.h>
+
+class TypeInModuleMajorVersionZero : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+public:
+    TypeInModuleMajorVersionZero(QObject *parent = nullptr) : QObject(parent) {}
+};
+
+#endif // VERSION_ZERO_TYPE_H
