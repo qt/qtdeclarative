a5c8d96e073238f0e04d66d68e8f36775e19884e
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2022-06-29 11:38:02 +0200

    QQuickWindow: Implement QWindowPrivate::clearFocusObject()
    Change 68c103225f4e8bd removed the overridden function
    QQuickWindowPrivate::clearFocusObject(). This is quite
    unfortunate, since it's used by e.g QPA/iOS to clear
    the focus object when the input panel is closed.
    
    The result is that if you hide the input panel by
    swiping it down, a TextField with focus will not loose
    focus anymore, but continue to show a blinking cursor.
    Moreover, you cannot bring the input panel up again by
    clicking on the TextField, since it's focused already, so
    no change will be reported back to the platform IM.
    
    Fixes: QTBUG-104642
    Change-Id: Ic27aaa2cc8d11cbf64ba8d3797ab4008bc2eb3a6
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 65891f65fb26d4383a60aae745f6065642174261)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/items/qquickwindow.cpp
+++ src/quick/items/qquickwindow.cpp
@@ -1726,6 +1726,12 @@ QPair<QQuickItem*, QQuickPointerHandler*> QQuickWindowPrivate::findCursorItemAnd
 }
 #endif
 
+void QQuickWindowPrivate::clearFocusObject()
+{
+    if (auto da = deliveryAgentPrivate())
+        da->clearFocusObject();
+}
+
 /*!
     \qmlproperty list<Object> Window::data
     \qmldefault
