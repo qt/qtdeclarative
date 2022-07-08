8a511021d54825e5a4e430cf76646ff3ea256b08
Moody Liu <mooodyhunter@outlook.com>
2022-07-08 13:55:27 +0100

    qmldomaspdumper: fix invalid XML node names and syntax
    a valid XML should not contain commas
    
    Change-Id: I5109fdc11f7022a64df2dfac3c9daf35d30b15e9
    Reviewed-by: Fawzi Mohamed <fawzi.mohamed@qt.io>

--- src/qmldom/qqmldomastdumper.cpp
+++ src/qmldom/qqmldomastdumper.cpp
@@ -238,7 +238,7 @@ public:
     void endVisit(AST::UiArrayBinding *) override { stop(u"UiArrayBinding"); }
 
     bool visit(AST::UiParameterList *el) override {
-        start(QLatin1String("UiArrayBinding name=%1 commaToken=%2 propertyTypeToken=%3 identifierToken=%4 colonToken=%5")
+        start(QLatin1String("UiParameterList name=%1 commaToken=%2 propertyTypeToken=%3 identifierToken=%4 colonToken=%5")
               .arg(quotedString(el->name), loc(el->commaToken), loc(el->propertyTypeToken), loc(el->identifierToken), loc(el->colonToken)));
         Node::accept(el->type, this);
         return true;
@@ -402,7 +402,7 @@ public:
     void endVisit(AST::RegExpLiteral *) override { stop(u"RegExpLiteral"); }
 
     bool visit(AST::ArrayPattern *el) override {
-        start(QLatin1String("ArrayPattern lbracketToken=%1, commaToken=%2, rbracketToken=%3 parseMode=%4")
+        start(QLatin1String("ArrayPattern lbracketToken=%1 commaToken=%2 rbracketToken=%3 parseMode=%4")
               .arg(loc(el->lbracketToken),loc(el->commaToken),loc(el->rbracketToken), quotedString(QString::number(el->parseMode, 16))));
         return true;
     }
