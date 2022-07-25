4815700b7842d70a08647e0f5942032fb63f004f
Ulf Hermann <ulf.hermann@qt.io>
2022-07-25 16:14:49 +0200

    V4: Allow conversion from string to QByteArray when setting bindables
    We allow it everywhere else, too.
    
    Fixes: QTBUG-105044
    Change-Id: I714e5d501a780310791523c5f35a87681c69b1fb
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 00e95e35061c169237402af4b017e70175a881db)

--- src/qml/jsruntime/qv4engine.cpp
+++ src/qml/jsruntime/qv4engine.cpp
@@ -2337,6 +2337,8 @@ bool ExecutionEngine::metaTypeFromJS(const Value &value, QMetaType metaType, voi
     case QMetaType::QByteArray:
         if (const ArrayBuffer *ab = value.as<ArrayBuffer>())
             *reinterpret_cast<QByteArray*>(data) = ab->asByteArray();
+        else if (const String *string = value.as<String>())
+            *reinterpret_cast<QByteArray*>(data) = string->toQString().toUtf8();
         else
             *reinterpret_cast<QByteArray*>(data) = QByteArray();
         return true;
