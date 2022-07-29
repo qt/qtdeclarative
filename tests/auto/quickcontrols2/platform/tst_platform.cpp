acf2ed0c5fb7594ab01ff48e3dc5c3c16e203755
Mitch Curtis <mitch.curtis@qt.io>
2022-07-29 14:26:50 +0800

    Platform: fix MenuItem being triggered when disabled
    Ensure that we remove shortcuts for a MenuItem when it's disabled,
    not just when a new shortcut is set.
    
    Fixes: QTBUG-89567
    Pick-to: 6.2 6.3 6.4
    Change-Id: I9c4eeda1a68a23317fe5e7bf517046e4e403c46a
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>

--- tests/auto/quickcontrols2/platform/tst_platform.cpp
+++ tests/auto/quickcontrols2/platform/tst_platform.cpp
@@ -1,5 +1,31 @@
 // Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
+#include <QtQml/qqmlengine.h>
 #include <QtQuickTest/quicktest.h>
-QUICK_TEST_MAIN(tst_platform)
+
+class Setup : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool shortcutsSupported READ areShortcutsSupported CONSTANT FINAL)
+
+public:
+    bool areShortcutsSupported() const
+    {
+#if QT_CONFIG(shortcut)
+        return true;
+#else
+        return false;
+#endif
+    }
+
+public slots:
+    void qmlEngineAvailable(QQmlEngine *)
+    {
+        qmlRegisterSingletonInstance("org.qtproject.Test", 1, 0, "TestHelper", this);
+    }
+};
+
+QUICK_TEST_MAIN_WITH_SETUP(tst_platform, Setup)
+
+#include "tst_platform.moc"
