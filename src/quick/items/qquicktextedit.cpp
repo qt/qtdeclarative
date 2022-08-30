qt/qtdeclarative

commit 2fb43a35dfa08bb0746913e3363f1fcf21815e72

Invalidate text when application fonts are added or removed
We had multiple related issues due to application fonts being added
or removed during the application lifetime.

1. If a text had font family "foo" set, and this font did not
exist at the time, then loading "foo" at a later stage would not
update the text to use the correct font, since the result of
the previous request had been cached.

2. A variation of #1 was if the font "foo" was loaded by a FontLoader
in the scene and referred to by name in the text component. In this
case, there was a race condition, where the font lookup would sometimes
yield different results on the main thread and on the render thread,
and text would be garbled.

3. When a font was removed from the font database, then references to
it would remain in the caches (glyph cache + font cache) in the render
thread. With certain backends (DirectWrite, CoreText) this caused errors
or even crashes, as the cached font engines would be referring to data
that had been removed.

The work-around for #1 and #2 was merely to avoid hardcoding names for
fonts, but instead getting them from the FontLoader. This way, you can
avoid requesting the font family before it is available (and thus avoid
caching the wrong result). However, for #3 there is no known work-around.

This patch fixes all three (together with a smaller patch for qtbase) by
invalidating all text and related caches in Qt Quick when fonts are
either added or removed from the font database. This does add some
overhead if font loading happens during runtime, but the alternative is
broken behavior and dangling pointers.

This is done during the synchronization step. Before synchronization,
the font cache is flushed and all text components are marked for update,
so that fonts are re-requested against the new font database.

After synchronization, we delete all distance field glyph caches which
are not currently in use, to avoid having references to stale application
font data in the list.

src/quick/items/qquicktextedit.cpp
@@ -435,6 +435,18 @@ void QQuickTextEdit::setText(const QString &text)
     setFlag(QQuickItem::ItemObservesViewport, text.size() > QQuickTextEditPrivate::largeTextSizeThreshold);
 }
 
+void QQuickTextEdit::invalidate()
+{
+    Q_D(QQuickTextEdit);
+    if (isComponentComplete()) {
+        if (d->document != nullptr)
+            d->document->markContentsDirty(0, d->document->characterCount());
+        invalidateFontCaches();
+        d->updateType = QQuickTextEditPrivate::UpdateAll;
+        update();
+    }
+}
+
 /*!
     \qmlproperty string QtQuick::TextEdit::preeditText
     \readonly
@@ -2078,19 +2090,24 @@ QSGNode *QQuickTextEdit::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *
     Q_UNUSED(updatePaintNodeData);
     Q_D(QQuickTextEdit);
 
-    if (d->updateType != QQuickTextEditPrivate::UpdatePaintNode && oldNode != nullptr) {
+    if (d->updateType != QQuickTextEditPrivate::UpdatePaintNode
+          && d->updateType != QQuickTextEditPrivate::UpdateAll
+          && oldNode != nullptr) {
         // Update done in preprocess() in the nodes
         d->updateType = QQuickTextEditPrivate::UpdateNone;
         return oldNode;
     }
 
-    d->updateType = QQuickTextEditPrivate::UpdateNone;
+    if (!oldNode || d->updateType == QQuickTextEditPrivate::UpdateAll) {
+        delete oldNode;
+        oldNode = nullptr;
 
-    if (!oldNode) {
         // If we had any QQuickTextNode node references, they were deleted along with the root node
         // But here we must delete the Node structures in textNodeMap
         d->textNodeMap.clear();
     }
+
+    d->updateType = QQuickTextEditPrivate::UpdateNone;
 
     RootNode *rootNode = static_cast<RootNode *>(oldNode);
     TextNodeIterator nodeIterator = d->textNodeMap.begin();

src/quick/items/qquicktextedit_p.h
@@ -52,6 +52,7 @@
 //
 
 #include "qquickimplicitsizeitem_p.h"
+#include "qquicktextinterface_p.h"
 
 #include <QtGui/qtextoption.h>
 
@@ -61,9 +62,10 @@ class QQuickTextDocument;
 class QQuickTextEditPrivate;
 class QTextBlock;
 
-class Q_QUICK_PRIVATE_EXPORT QQuickTextEdit : public QQuickImplicitSizeItem
+class Q_QUICK_PRIVATE_EXPORT QQuickTextEdit : public QQuickImplicitSizeItem, public QQuickTextInterface
 {
     Q_OBJECT
+    Q_INTERFACES(QQuickTextInterface)
 
     Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
     Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
@@ -302,6 +304,8 @@ public:
 
     int tabStopDistance() const;
     void setTabStopDistance(qreal distance);
+
+    void invalidate() override;
 
 Q_SIGNALS:
     void textChanged();

src/quick/CMakeLists.txt
@@ -78,6 +78,7 @@ qt_internal_add_qml_module(Quick
         items/qquickstateoperations.cpp items/qquickstateoperations_p.h
         items/qquicktext.cpp items/qquicktext_p.h
         items/qquicktext_p_p.h
+        items/qquicktextinterface_p.h
         items/qquicktextcontrol.cpp items/qquicktextcontrol_p.h
         items/qquicktextcontrol_p_p.h
         items/qquicktextdocument.cpp items/qquicktextdocument.h items/qquicktextdocument_p.h

src/quick/items/qquicktext.cpp
@@ -3008,6 +3008,13 @@ void QQuickText::hoverLeaveEvent(QHoverEvent *event)
     d->processHoverEvent(event);
 }
 
+void QQuickText::invalidate()
+{
+    Q_D(QQuickText);
+    d->textHasChanged = true;
+    d->updateLayout();
+}
+
 bool QQuickTextPrivate::transformChanged(QQuickItem *transformedItem)
 {
     // If there's a lot of text, we may need QQuickText::updatePaintNode() to call

src/quick/items/qquicktext_p.h
@@ -52,6 +52,7 @@
 //
 
 #include "qquickimplicitsizeitem_p.h"
+#include "qquicktextinterface_p.h"
 #include <private/qtquickglobal_p.h>
 #include <QtGui/qtextoption.h>
 
@@ -59,9 +60,10 @@ QT_BEGIN_NAMESPACE
 
 class QQuickTextPrivate;
 class QQuickTextLine;
-class Q_QUICK_PRIVATE_EXPORT QQuickText : public QQuickImplicitSizeItem
+class Q_QUICK_PRIVATE_EXPORT QQuickText : public QQuickImplicitSizeItem, public QQuickTextInterface
 {
     Q_OBJECT
+    Q_INTERFACES(QQuickTextInterface)
 
     Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
     Q_PROPERTY(QFont font READ font WRITE setFont NOTIFY fontChanged)
@@ -273,6 +275,8 @@ public:
 
     QJSValue fontInfo() const;
     QSizeF advance() const;
+
+    void invalidate() override;
 
 Q_SIGNALS:
     void textChanged(const QString &text);

src/quick/items/qquicktextedit_p_p.h
@@ -197,7 +197,8 @@ public:
     enum UpdateType {
         UpdateNone,
         UpdateOnlyPreprocess,
-        UpdatePaintNode
+        UpdatePaintNode,
+        UpdateAll
     };
 
     QQuickTextEdit::HAlignment hAlign;

src/quick/items/qquicktextinput.cpp
@@ -136,6 +136,13 @@ QString QQuickTextInput::text() const
     return (res.isNull() ? QString::fromLatin1("") : res);
 }
 
+void QQuickTextInput::invalidate()
+{
+    Q_D(QQuickTextInput);
+    d->updateLayout();
+    invalidateFontCaches();
+}
+
 void QQuickTextInput::setText(const QString &s)
 {
     Q_D(QQuickTextInput);

src/quick/items/qquicktextinput_p.h
@@ -52,6 +52,7 @@
 //
 
 #include "qquickimplicitsizeitem_p.h"
+#include "qquicktextinterface_p.h"
 #include <QtGui/qtextoption.h>
 #include <QtGui/qvalidator.h>
 
@@ -59,9 +60,10 @@ QT_BEGIN_NAMESPACE
 
 class QQuickTextInputPrivate;
 class QValidator;
-class Q_QUICK_PRIVATE_EXPORT QQuickTextInput : public QQuickImplicitSizeItem
+class Q_QUICK_PRIVATE_EXPORT QQuickTextInput : public QQuickImplicitSizeItem, public QQuickTextInterface
 {
     Q_OBJECT
+    Q_INTERFACES(QQuickTextInterface)
 
     Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
     Q_PROPERTY(int length READ length NOTIFY textChanged)
@@ -310,6 +312,8 @@ public:
     qreal bottomPadding() const;
     void setBottomPadding(qreal padding);
     void resetBottomPadding();
+
+    void invalidate() override;
 
 Q_SIGNALS:
     void textChanged();

commit 286ca997d1ebfdd046c7dc1ada884de3fd6acc01

Fix thread issue in TextEdit when font db updates
This amends 2fb43a35dfa08bb0746913e3363f1fcf21815e72, which
introduced invalidation of text when the font database changes.

The text invalidation is done from the render thread, which
mostly works well because it is done during sync, so the main
thread is locked. However, for TextEdit, the invalidation is
more complex and would implicitly call documentChanged() which
would then try to stop a timer. Since the timer has to be
stopped from the owner thread, it would fail and throw a
warning.

So for TextEdit, we invoke the method on the main thread
instead, which means it will use one extra update pass
in order to properly invalidate its contents.

Pick-to: 6.4
Fixes: QTBUG-105992
Change-Id: Idc020c682dcb2d1f3c66d043cf98fe9c6e4dc2a1
Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>

src/quick/items/qquicktextedit.cpp
@@ -405,6 +405,11 @@ void QQuickTextEdit::setText(const QString &text)
 
 void QQuickTextEdit::invalidate()
 {
+    QMetaObject::invokeMethod(this, &QQuickTextEdit::q_invalidate);
+}
+
+void QQuickTextEdit::q_invalidate()
+{
     Q_D(QQuickTextEdit);
     if (isComponentComplete()) {
         if (d->document != nullptr)

src/quick/items/qquicktextedit_p.h
@@ -338,6 +338,7 @@ public Q_SLOTS:
     Q_REVISION(2, 7) void clear();
 
 private Q_SLOTS:
+    void q_invalidate();
     void q_textChanged();
     void q_contentsChange(int, int, int);
     void updateSelection();