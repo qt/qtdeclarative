2a37ff2f49140272d0122ccc097cc14c2fa4133e
Fabian Kosmale <fabian.kosmale@qt.io>
2022-09-08 13:59:15 +0200

    Fix alias to inline component type
    When we create an alias property, we resolve its targets metatype. In
    the case of inline components of the same file, that's however not
    possible via QQmlType, as the type will only be available once the outer
    type has been fully set up.
    Fix this by looking up the type in the ExecutableCompilationUnit, where
    the information is available while the type registraiton process is still
    ongoing.
    
    As a drive-by, also set the alias flag correctly when constructing the
    QMetaObject of a QML element.
    
    Pick-to: 6.2 6.3 6.4
    Fixes: QTBUG-106392
    Change-Id: Ie8f55dd0894cc5c8d683dd3c685980878956d3ca
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qml/qml/qqmlpropertycache.cpp
+++ src/qml/qml/qqmlpropertycache.cpp
@@ -998,6 +998,7 @@ void QQmlPropertyCache::toMetaObjectBuilder(QMetaObjectBuilder &builder) const
         property.setWritable(data->isWritable());
         property.setResettable(data->isResettable());
         property.setBindable(data->isBindable());
+        property.setAlias(data->isAlias());
     }
 
     for (int ii = 0; ii < methods.count(); ++ii) {
