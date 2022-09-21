24d199824bb5c5072b2ab01d537fda5132cfcbe4
Mitch Curtis <mitch.curtis@qt.io>
2022-09-21 15:24:56 +0800

    Improve tst_material.qml
    - Replace var with let.
    - Use createTemporaryObject for parent objects instead of manually
      destroying them.
    - Fix bug where a component was passed to createObject instead of
      the intended parent window.
    - Replace custom binding loop check with failOnWarning.
    - Rename Component ids to make it obvious that they're components.
    - Remove unused Components.
    - Remove unused ids.
    
    Change-Id: I383b98a2e5694d841ef17eda24495140970f23ee
    Pick-to: 6.4
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/quickcontrols2/qquickmaterialstyle/tst_qquickmaterialstyle.cpp
+++ tests/auto/quickcontrols2/qquickmaterialstyle/tst_qquickmaterialstyle.cpp
@@ -1,42 +1,5 @@
 // Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include <QtQml/qqmlengine.h>
-#include <QtQml/qqmlcontext.h>
 #include <QtQuickTest/quicktest.h>
-
-class Setup : public QObject
-{
-    Q_OBJECT
-    Q_PROPERTY(bool bindingLoopDetected READ wasBindingLoopDetected FINAL)
-
-public:
-    Setup() {}
-
-    bool wasBindingLoopDetected() const { return mBindingLoopDetected; }
-
-public slots:
-    void reset() { mBindingLoopDetected = false; }
-
-    void qmlEngineAvailable(QQmlEngine *engine)
-    {
-        connect(engine, &QQmlEngine::warnings, this, &Setup::qmlWarnings);
-
-        qmlRegisterSingletonInstance("org.qtproject.Test", 1, 0, "BindingLoopDetector", this);
-    }
-
-    void qmlWarnings(const QList<QQmlError> &warnings)
-    {
-        for (const auto &error : warnings) {
-            if (error.messageType() == QtWarningMsg && error.description().contains(QStringLiteral("Binding loop detected")))
-                mBindingLoopDetected = true;
-        }
-    }
-
-private:
-    bool mBindingLoopDetected = false;
-};
-
-QUICK_TEST_MAIN_WITH_SETUP(tst_qquickmaterialstyle, Setup)
-
-#include "tst_qquickmaterialstyle.moc"
+QUICK_TEST_MAIN(tst_qquickmaterialstyle)
