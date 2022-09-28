ee9a7b1bee0ab783c84188e098870f5afcb00434
Ulf Hermann <ulf.hermann@qt.io>
2022-09-28 09:00:18 +0200

    masm: Treat Android as generic Posix regarding mmap and friends
    Apparently we cannot rely on madvise() to work as we expect it on linux.
    
    Fixes: QTBUG-106864
    Fixes: QTBUG-106269
    Change-Id: Ie488ad788386c1a8c493d6bba632787f5282baaa
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit ffecc122d785de9c4c5defd8724526b8dd4982dc)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/memory/qv4mm.cpp
+++ src/qml/memory/qv4mm.cpp
@@ -141,7 +141,7 @@ struct MemorySegment {
 
         size_t pageSize = WTF::pageSize();
         size = (size + pageSize - 1) & ~(pageSize - 1);
-#if !defined(Q_OS_LINUX) && !defined(Q_OS_WIN)
+#if (!defined(Q_OS_LINUX) && !defined(Q_OS_WIN)) || defined(Q_OS_ANDROID)
         // Linux and Windows zero out pages that have been decommitted and get committed again.
         // unfortunately that's not true on other OSes (e.g. BSD based ones), so zero out the
         // memory before decommit, so that we can be sure that all chunks we allocate will be
