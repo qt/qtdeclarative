qt/qtdeclarative

commit fc636af3a723ee8b4ee42cf71864ae0df5ca4621

PinchHandler: scale incrementally when new pinch gesture begins
When the gesture begins, we begin multiplying the target item's scale
by 1.0 at first; it doesn't make sense to start immediately with
the accumulated scale remembered from previous pinch gestures, because
the target item remembers its own scale.

When QQuickPinchHandler::wantsPointerEvent() returns false because
some irrelevant gesture was received (for example a PanNativeGesture),
that's not a good reason to deactivate.  Deactivating and re-activating
with each ZoomNativeGesture event results in extreme behavior, because
PinchHandler depends on the BeginNativeGesture and EndNativeGesture
events to reset internal state.  Likewise, the fact that the button
state is NoButton is not a good reason for wantsPointerEvent() to
return false.

Added an autotest: the first of its kind that actually simulates the
native gesture events.

Fixes: QTBUG-92064
Pick-to: 5.15 6.0 6.1
Change-Id: I3a9b92d70f99497ee58ad8557d90d521fbe16d41
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/quick/handlers/qquickpinchhandler.cpp
@@ -91,6 +91,8 @@ Q_LOGGING_CATEGORY(lcPinchHandler, "qt.quick.handler.pinch")
 QQuickPinchHandler::QQuickPinchHandler(QQuickItem *parent)
     : QQuickMultiPointHandler(parent, 2)
 {
+    // Tell QQuickPointerDeviceHandler::wantsPointerEvent() to ignore button state
+    d_func()->acceptedButtons = Qt::NoButton;
 }
 
 /*!
@@ -235,7 +237,7 @@ void QQuickPinchHandler::onActiveChanged()
             m_startRotation = t->rotation();
             m_startPos = t->position();
         } else {
-            m_startScale = m_accumulatedScale;
+            m_startScale = 1;
             m_startRotation = 0;
         }
         qCDebug(lcPinchHandler) << "activated with starting scale" << m_startScale << "rotation" << m_startRotation;
@@ -448,7 +450,7 @@ void QQuickPinchHandler::handlePointerEventImpl(QPointerEvent *event)
 
     qCDebug(lcPinchHandler) << "centroid" << centroid().scenePressPosition() << "->"  << centroid().scenePosition()
                             << ", distance" << m_startDistance << "->" << dist
-                            << ", startScale" << m_startScale << "->" << m_accumulatedScale
+                            << ", scale" << m_startScale << "->" << m_accumulatedScale
                             << ", activeRotation" << m_activeRotation
                             << ", rotation" << rotation
                             << " from " << event->device()->type();

tests/auto/quick/pointerhandlers/qquickpinchhandler/tst_qquickpinchhandler.cpp
@@ -50,6 +50,7 @@ private slots:
     void pinchProperties();
     void scale();
     void scaleThreeFingers();
+    void scaleNativeGesture();
     void pan();
     void dragAxesEnabled_data();
     void dragAxesEnabled();
@@ -351,6 +352,79 @@ void tst_QQuickPinchHandler::scaleThreeFingers()
         QQuickTouchUtils::flush(window);
     }
     QCOMPARE(pinchHandler->active(), false);
+}
+
+void tst_QQuickPinchHandler::scaleNativeGesture()
+{
+    QQuickView *window = createView();
+    QScopedPointer<QQuickView> scope(window);
+    window->setSource(testFileUrl("pinchproperties.qml"));
+    window->show();
+    QVERIFY(QTest::qWaitForWindowExposed(window));
+    QVERIFY(window->rootObject() != nullptr);
+    qApp->processEvents();
+
+    QQuickPinchHandler *pinchHandler = window->rootObject()->findChild<QQuickPinchHandler*>("pinchHandler");
+    QVERIFY(pinchHandler != nullptr);
+    QQuickItem *root = qobject_cast<QQuickItem*>(window->rootObject());
+    QVERIFY(root != nullptr);
+    QQuickItem *target = window->rootObject()->findChild<QQuickItem*>("blackrect");
+    QVERIFY(target != nullptr);
+
+    QPointF targetPos = target->position();
+    ulong ts = 1;
+
+    // first pinch: scale it up
+    const qreal expectedScale = 1.1;
+    QPointF pinchPos(75, 75);
+    QPointF pinchLocalPos = target->mapFromScene(pinchPos);
+    // target position is adjusted in QQuickItemPrivate::adjustedPosForTransform()
+    // so as to compensate for the change in size, to hold the centroid in place
+    const QPointF expectedPos = targetPos + QPointF( (pinchPos.x() - target->x()) * (expectedScale - 1),
+                                                     (pinchPos.y() - target->y()) * (expectedScale - 1) );
+    QWindowSystemInterface::handleGestureEvent(window, ts++, QPointingDevice::primaryPointingDevice(),
+                                               Qt::BeginNativeGesture, pinchPos, pinchPos);
+    QWindowSystemInterface::handleGestureEventWithRealValue(window, ts++, QPointingDevice::primaryPointingDevice(),
+                                                            Qt::ZoomNativeGesture, expectedScale - 1, pinchPos, pinchPos);
+    QTRY_COMPARE(target->scale(), expectedScale);
+    QCOMPARE(pinchHandler->active(), true);
+    QCOMPARE(pinchHandler->centroid().position(), pinchLocalPos);
+    QCOMPARE(pinchHandler->centroid().scenePosition(), pinchPos);
+    QVERIFY(qAbs(target->position().x() - expectedPos.x()) < 0.001);
+    QVERIFY(qAbs(target->position().y() - expectedPos.y()) < 0.001);
+    QCOMPARE(pinchHandler->scale(), expectedScale);
+    QCOMPARE(pinchHandler->activeScale(), expectedScale);
+    QCOMPARE(pinchHandler->translation(), QVector2D());
+    QCOMPARE(pinchHandler->rotation(), 0);
+    QWindowSystemInterface::handleGestureEvent(window, ts++, QPointingDevice::primaryPointingDevice(),
+                                               Qt::EndNativeGesture, pinchPos, pinchPos);
+    QTRY_COMPARE(pinchHandler->active(), false);
+    QCOMPARE(target->scale(), expectedScale);
+    QCOMPARE(pinchHandler->scale(), expectedScale);
+    QCOMPARE(pinchHandler->activeScale(), 1);
+    QCOMPARE(pinchHandler->translation(), QVector2D());
+    QCOMPARE(pinchHandler->rotation(), 0);
+
+    // second pinch at a different position: scale it down to original size again
+    const qreal reverseScale = (1 / expectedScale);
+    pinchPos = QPointF(125, 125);
+    pinchLocalPos = target->mapFromScene(pinchPos);
+    QWindowSystemInterface::handleGestureEvent(window, ts++, QPointingDevice::primaryPointingDevice(),
+                                               Qt::BeginNativeGesture, pinchPos, pinchPos);
+    QWindowSystemInterface::handleGestureEventWithRealValue(window, ts++, QPointingDevice::primaryPointingDevice(),
+                                                            Qt::ZoomNativeGesture, reverseScale - 1, pinchPos, pinchPos);
+    QTRY_COMPARE(target->scale(), 1);
+    QCOMPARE(pinchHandler->active(), true);
+    QCOMPARE(pinchHandler->centroid().position(), pinchLocalPos);
+    QCOMPARE(pinchHandler->centroid().scenePosition(), pinchPos);
+    QCOMPARE(pinchHandler->scale(), 1);
+    QCOMPARE(pinchHandler->activeScale(), reverseScale);
+    QWindowSystemInterface::handleGestureEvent(window, ts++, QPointingDevice::primaryPointingDevice(),
+                                               Qt::EndNativeGesture, pinchPos, pinchPos);
+    QTRY_COMPARE(pinchHandler->active(), false);
+    QCOMPARE(target->scale(), 1);
+    QCOMPARE(pinchHandler->scale(), 1);
+    QCOMPARE(pinchHandler->activeScale(), 1);
 }
 
 void tst_QQuickPinchHandler::pan()

src/quick/handlers/qquickpointerdevicehandler.cpp
@@ -305,7 +305,7 @@ bool QQuickPointerDeviceHandler::wantsPointerEvent(QPointerEvent *event)
         return false;
     if (d->acceptedModifiers != Qt::KeyboardModifierMask && event->modifiers() != d->acceptedModifiers)
         return false;
-    // HoverHandler sets acceptedButtons to Qt::NoButton to indicate that button state is irrelevant.
+    // Some handlers (HoverHandler, PinchHandler) set acceptedButtons to Qt::NoButton to indicate that button state is irrelevant.
     if (event->pointingDevice()->pointerType() != QPointingDevice::PointerType::Finger &&
             acceptedButtons() != Qt::NoButton && event->type() != QEvent::Wheel &&
             (static_cast<QSinglePointEvent *>(event)->buttons() & acceptedButtons()) == 0 &&

src/quick/handlers/qquickpointerhandler.cpp
@@ -625,7 +625,8 @@ void QQuickPointerHandler::handlePointerEvent(QPointerEvent *event)
     if (wants) {
         handlePointerEventImpl(event);
     } else {
-        setActive(false);
+        if (event->type() != QEvent::NativeGesture)
+            setActive(false);
         for (int i = 0; i < event->pointCount(); ++i) {
             auto &pt = event->point(i);
             if (event->exclusiveGrabber(pt) == this && pt.state() != QEventPoint::Stationary) {

commit 3046fe153dd2c5679479eb512dcee6bdc80bd1cf

PinchHandler null target: remember accumulated scale between pinches
This restores behavior from b4d31c9ff5f0c5821ea127c663532d9fc2cae43e
which got broken in fc636af3a723ee8b4ee42cf71864ae0df5ca4621.
As documented, PinchHandler.scale is the accumulated scale that would
be applied to the target item (even if there is no target), whereas
activeScale is the scale during one pinch gesture. After the first
gesture, these two values are supposed to diverge, even if there is
no target; that way you can bind scale to some property, to scale
something else in the same way that PinchHandler would normally scale
its target.

Pick-to: 6.2 6.4
Fixes: QTBUG-108549
Task-number: QTBUG-68941
Task-number: QTBUG-92064
Change-Id: I32ff37e394fd8466128603eddd5697ba1cc1a0ed
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/quick/handlers/qquickpinchhandler.cpp
@@ -203,8 +203,8 @@ void QQuickPinchHandler::onActiveChanged()
             m_startRotation = t->rotation();
             m_startPos = t->position();
         } else {
-            m_startScale = 1;
-            m_startRotation = 0;
+            m_startScale = m_accumulatedScale;
+            m_startRotation = 0; // TODO m_accumulatedRotation (QTBUG-94168)
         }
         qCDebug(lcPinchHandler) << "activated with starting scale" << m_startScale << "rotation" << m_startRotation;
     } else {

tests/auto/quick/pointerhandlers/qquickpinchhandler/data/nullTarget.qml
@@ -1,0 +1,33 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+import QtQuick 2.15
+
+Item {
+    width: 320; height: 320
+    property alias pinchScale: pinch.scale
+
+    Rectangle {
+        objectName: "blackrect"
+        width: 200; height: 200
+        color: "black"
+        antialiasing: true
+        scale: pinch.scale
+        rotation: pinch.rotation
+        x: pinch.translation.x
+        y: pinch.translation.y
+
+        PinchHandler {
+            id: pinch
+            target: null
+            minimumScale: 0.5
+            maximumScale: 4
+        }
+
+        Text {
+            color: "cyan"
+            anchors.centerIn: parent
+            text: "scale " + pinch.scale.toFixed(2) + " activeScale " + pinch.activeScale.toFixed(2)
+        }
+    }
+}

tests/auto/quick/pointerhandlers/qquickpinchhandler/tst_qquickpinchhandler.cpp
@@ -29,6 +29,7 @@ public:
 private slots:
     void cleanupTestCase();
     void pinchProperties();
+    void scale_data();
     void scale();
     void scaleThreeFingers();
     void scaleNativeGesture_data();
@@ -179,16 +180,26 @@ QEventPoint makeTouchPoint(int id, QPoint p, QQuickView *v, QQuickItem *i)
     return touchPoint;
 }
 
+void tst_QQuickPinchHandler::scale_data()
+{
+    QTest::addColumn<QUrl>("qmlfile");
+    QTest::addColumn<bool>("hasTarget");
+    QTest::newRow("targetModifying") << testFileUrl("pinchproperties.qml") << true;
+    QTest::newRow("nullTarget") << testFileUrl("nullTarget.qml") << false;
+}
+
 void tst_QQuickPinchHandler::scale()
 {
-    QQuickView window;
-    QVERIFY(QQuickTest::showView(window, testFileUrl("pinchproperties.qml")));
+    QFETCH(QUrl, qmlfile);
+    QFETCH(bool, hasTarget);
 
+    QQuickView window;
+    QVERIFY(QQuickTest::showView(window, qmlfile));
     QQuickItem *root = qobject_cast<QQuickItem*>(window.rootObject());
     QVERIFY(root != nullptr);
     auto *pinchHandler = static_cast<PinchHandler *>(root->findChild<QQuickPinchHandler*>());
     QVERIFY(pinchHandler != nullptr);
-    QQuickItem *blackRect = pinchHandler->target();
+    QQuickItem *blackRect = (hasTarget ? pinchHandler->target() : pinchHandler->parentItem());
     QVERIFY(blackRect != nullptr);
     QSignalSpy grabChangedSpy(pinchHandler, SIGNAL(grabChanged(QPointingDevice::GrabTransition, QEventPoint)));
 
@@ -238,6 +249,40 @@ void tst_QQuickPinchHandler::scale()
         QCOMPARE(pinchHandler->scale(), pinchHandler->activeScale()); // in sync for the first gesture
         QPointF expectedCentroid = p0 + (p1 - p0) / 2;
         QCOMPARE(pinchHandler->centroid().scenePosition(), expectedCentroid);
+    }
+
+    qreal lastScale = pinchHandler->scale();
+    pinchSequence.release(0, p0, &window).release(1, p1, &window).commit();
+    QQuickTouchUtils::flush(&window);
+    if (lcPointerTests().isDebugEnabled()) QTest::qWait(500);
+    // scale property is persistent after release
+    QCOMPARE(pinchHandler->scale(), lastScale);
+
+    // pinch a second time: scale picks up where we left off
+    p0 = QPoint(80, 80);
+    p1 = QPoint(100, 100);
+    pinchSequence.press(0, p0, &window).press(1, p1, &window).commit();
+    // move one point until PinchHandler activates
+    for (int pi = 0; pi < 10 && !pinchHandler->active(); ++pi) {
+        p1 += pd;
+        pinchSequence.stationary(0).move(1, p1, &window).commit();
+        QQuickTouchUtils::flush(&window);
+    }
+    if (lcPointerTests().isDebugEnabled()) QTest::qWait(500);
+    QCOMPARE(pinchHandler->active(), true);
+    QCOMPARE(pinchHandler->scale(), lastScale); // just activated, not scaling further yet
+    for (int i = 0; i < 2; ++i) {
+        lastScale = pinchHandler->scale();
+        p1 += pd;
+        pinchSequence.stationary(0).move(1, p1, &window).commit();
+        QQuickTouchUtils::flush(&window);
+        if (lcPointerTests().isDebugEnabled()) QTest::qWait(500);
+        QCOMPARE_GT(pinchHandler->scale(), lastScale);
+        line.setP2(p1);
+        qreal expectedActiveScale = line.length() / startLength;
+        QVERIFY(qFloatDistance(pinchHandler->activeScale(), expectedActiveScale) < 10);
+        QCOMPARE(pinchHandler->scale(), root->property("pinchScale").toReal());
+        QCOMPARE_NE(pinchHandler->scale(), pinchHandler->activeScale()); // not in sync anymore
     }
 
     // scale beyond maximumScale

tests/manual/pointer/pinchNullTarget.qml
@@ -1,0 +1,50 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+import QtQuick 2.15
+
+Rectangle {
+    width: 1024; height: 600
+    color: "#eee"
+
+    function getTransformationDetails(item, pinchhandler) {
+        return "\n\npinch.scale:" + pinchhandler.scale.toFixed(2)
+                + "\npinch.activeScale:" + pinchhandler.activeScale.toFixed(2)
+                + "\npinch.rotation:" + pinchhandler.rotation.toFixed(2)
+                + "\npinch.translation:" + "(" + pinchhandler.translation.x.toFixed(2) + "," + pinchhandler.translation.y.toFixed(2) + ")"
+                + "\nrect.scale: " + item.scale.toFixed(2)
+                + "\nrect.rotation: " + item.rotation.toFixed(2)
+                + "\nrect.position: " + "(" + item.x.toFixed(2) + "," + item.y.toFixed(2) + ")"
+    }
+
+    Rectangle {
+        width: parent.width - 100; height: parent.height - 100; x: 50; y: 50
+        color: "lightsteelblue"
+        antialiasing: true
+        scale: pinch.scale
+
+        PinchHandler {
+            id: pinch
+            target: null
+            minimumScale: 0.5
+            maximumScale: 3
+        }
+
+        Text {
+            text: "Pinch with 2 fingers to scale, rotate and translate"
+                  + getTransformationDetails(parent, pinch)
+        }
+    }
+
+    Rectangle {
+        id: centroidIndicator
+        x: pinch.centroid.scenePosition.x - radius
+        y: pinch.centroid.scenePosition.y - radius
+        z: 1
+        visible: pinch.active
+        radius: width / 2
+        width: 10
+        height: width
+        color: "red"
+    }
+}