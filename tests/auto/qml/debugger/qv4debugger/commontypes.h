2e2ffdea3e210a87fd60e75359865d8600a9ed11
Semih Yavuz <semih.yavuz@qt.io>
2022-11-17 14:36:47 +0100

    Debugger: Initialize scoped context correctly
    Initialize scopedContext with valid qmlContext. Otherwise debugger
    crashes on attempt to lookup an object that includes a v4Function
    property
    
    Fixes: QTBUG-107607
    Change-Id: Iea59bdf9d379a5415abe1767f76f851978b1be3f
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit fe32f58008f89b7174e9f41a781e6fba5799c026)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/qml/debugger/qv4debugger/commontypes.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#ifndef COMMONTYPES_H
+#define COMMONTYPES_H
+
+#include <QtQuick/qquickitem.h>
+#include <QtQml/qqmlregistration.h>
+#include <QtQml/private/qv4engine_p.h>
+
+class MyType : public QQuickItem
+{
+    Q_OBJECT
+    QML_ELEMENT
+public:
+    MyType(QQuickItem *parent = nullptr) : QQuickItem(parent) {}
+    Q_INVOKABLE void name(QQmlV4Function*) const {}
+};
+
+#endif // COMMONTYPES_H
