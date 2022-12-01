e5f0b33d33c8d412bbe8ef151d9665f744bf929c
Timur Pocheptsov <timur.pocheptsov@qt.io>
2022-12-01 11:39:26 +0100

    QQuickMacStyle: stop using deprecated constants, use suggested replacements
    Change-Id: I6f0dfb34d836cfa3afc2fb5fc5990eb77f506e5c
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 90da0220e77cb6d71df460bd0e30358d734346d1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quicknativestyle/qstyle/mac/qquickmacstyle_mac.mm
+++ src/quicknativestyle/qstyle/mac/qquickmacstyle_mac.mm
@@ -2771,7 +2771,7 @@ void QMacStyle::drawPrimitive(PrimitiveElement pe, const QStyleOption *opt, QPai
         [triangleCell setState:(opt->state & State_Open) ? NSControlStateValueOn : NSControlStateValueOff];
 //        bool viewHasFocus = (w && w->hasFocus()) || (opt->state & State_HasFocus);
         bool viewHasFocus = false;
-        [triangleCell setBackgroundStyle:((opt->state & State_Selected) && viewHasFocus) ? NSBackgroundStyleDark : NSBackgroundStyleLight];
+        [triangleCell setBackgroundStyle:((opt->state & State_Selected) && viewHasFocus) ? NSBackgroundStyleEmphasized : NSBackgroundStyleNormal];
 
         d->setupNSGraphicsContext(cg, NO);
 
