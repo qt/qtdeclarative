36eead2ffae0eada4cf0b82403920701de029869
Vladimir Belyavsky <belyavskyv@gmail.com>
2022-12-14 12:55:16 +0300

    QQDAPrivate::deliverUpdatedPoints: skip HoverHandlers if wheel event
    This complements 85db031c3f449cbeba41eb5667cf6725cca8b32d.
    When delivering a mouse wheel event, we shouldn't just pass it
    to the item's HoverHandler, because those are normally visited in
    QQuickDeliveryAgentPrivate::deliverHoverEventToItem().
    
    Fixes: QTBUG-108600
    Fixes: QTBUG-108601
    Change-Id: Ie6066807f80140e1007347413f1095f98f6cb5c0
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit 1309bac26b71d184c924237ba3c8723aa79b5ba1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/util/qquickdeliveryagent.cpp
+++ src/quick/util/qquickdeliveryagent.cpp
@@ -1389,6 +1389,11 @@ bool QQuickDeliveryAgentPrivate::isMouseEvent(const QPointerEvent *ev)
     }
 }
 
+bool QQuickDeliveryAgentPrivate::isMouseOrWheelEvent(const QPointerEvent *ev)
+{
+    return isMouseEvent(ev) || ev->type() == QEvent::Wheel;
+}
+
 bool QQuickDeliveryAgentPrivate::isHoverEvent(const QPointerEvent *ev)
 {
     switch (ev->type()) {
