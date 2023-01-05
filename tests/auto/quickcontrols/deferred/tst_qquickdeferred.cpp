76e81a3ba4f6d3109530328d655c117454217a2c
Fabian Kosmale <fabian.kosmale@qt.io>
2023-01-05 08:54:57 +0100

    Deferred properties: avoid spurious binding loop
    During binding evaluation, it might happen that we do an initial read of
    a deferred property. If that deferred property is in turn an object
    property, further binding evaluations might occur. Those bindings should
    not depend on whatever trigerred the initial binding evaluation.
    However, with the new C++ properties/bindings, that would happen, as the
    bindingStatus would still indicate that binding evaluation is active.
    
    To remedy this, we temporarily suspend the binding status ind begin and
    completeDeferred.
    
    Fixes: QTBUG-96351
    Change-Id: I7e9778aa5339f044ce3082e962a2ede34b5d1e69
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 9b1d3a86023b47f9c3bff93c1ebbe18f8c7ad657)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ tests/auto/quickcontrols/deferred/tst_qquickdeferred.cpp
@@ -0,0 +1,68 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include <QtQuickTestUtils/private/qmlutils_p.h>
+#include <QtTest/qtest.h>
+#include <QQmlEngine>
+#include <QtQuick/qquickitem.h>
+#include <QtQuickTemplates2/private/qquickdeferredexecute_p_p.h>
+
+class DeferredPropertyTester : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QQuickItem *objectProperty READ objectProperty WRITE setObjectProperty NOTIFY objectChanged)
+    Q_CLASSINFO("DeferredPropertyNames", "objectProperty")
+
+public:
+    DeferredPropertyTester() {}
+
+
+    QQuickItem *objectProperty() {
+
+        if (!m_object.wasExecuted()) {
+            quickBeginDeferred(this, "objectProperty", m_object);
+            quickCompleteDeferred(this, "objectProperty", m_object);
+        }
+
+        return m_object;
+    }
+    void setObjectProperty(QQuickItem *obj) {
+        if (m_object == obj)
+            return;
+        m_object = obj;
+        if (!m_object.isExecuting()) // first read
+            emit objectChanged();
+    }
+
+signals:
+    void objectChanged();
+
+private:
+    QQuickDeferredPointer<QQuickItem> m_object = nullptr;
+};
+
+class tst_qquickdeferred : public QQmlDataTest
+{
+    Q_OBJECT
+public:
+    tst_qquickdeferred() : QQmlDataTest(QT_QMLTEST_DATADIR) {}
+private slots:
+    void noSpuriousBinding();
+};
+
+
+
+void tst_qquickdeferred::noSpuriousBinding() {
+    QTest::failOnWarning(QRegularExpression(QLatin1StringView(".*Binding loop detected.*")));
+    qmlRegisterType<DeferredPropertyTester>("test", 1, 0, "DeferredPropertyTester");
+
+    QQmlEngine engine;
+    QQmlComponent comp(&engine, testFileUrl("noSpuriousBinding.qml"));
+    std::unique_ptr<QObject> root(comp.create());
+    QVERIFY2(root, qPrintable(comp.errorString()));
+    root->setProperty("toggle", false);
+}
+
+QTEST_MAIN(tst_qquickdeferred)
+
+#include "tst_qquickdeferred.moc"
