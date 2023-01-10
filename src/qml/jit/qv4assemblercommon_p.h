ed8618a82d68fa1485e37e24593365bd6e4c920c
Ulf Hermann <ulf.hermann@qt.io>
2023-01-10 12:39:00 +0100

    Allow building the JIT on solaris
    It's not really supported but we can add this one clause if it helps.
    
    Pick-to: 6.5
    Fixes: QTBUG-100010
    Change-Id: Ibdc54b9da0ca9b4e92851dd9bbe2abf1a9b693c7
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>

--- src/qml/jit/qv4assemblercommon_p.h
+++ src/qml/jit/qv4assemblercommon_p.h
@@ -30,7 +30,7 @@ namespace QV4 {
 namespace JIT {
 
 #if defined(Q_PROCESSOR_X86_64) || defined(ENABLE_ALL_ASSEMBLERS_FOR_REFACTORING_PURPOSES)
-#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN)
+#if defined(Q_OS_LINUX) || defined(Q_OS_QNX) || defined(Q_OS_FREEBSD) || defined(Q_OS_DARWIN) || defined(Q_OS_SOLARIS)
 
 class PlatformAssembler_X86_64_SysV : public JSC::MacroAssembler<JSC::MacroAssemblerX86_64>
 {
