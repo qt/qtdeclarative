a48015f05e8272ff7ac1b307b9477540800bd94f
Ulf Hermann <ulf.hermann@qt.io>
2023-02-10 09:07:38 +0100

    QML: Invalidate any existing cache files when saving a CU to disk
    Otherwise we just re-load the old cache file next time. That's clearly
    not intended.
    
    Fixes: QTBUG-111078
    Change-Id: Ia65b46880eca2b6e8c4792a09f20716125beada3
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 0a5eff09203ebb2547431de689d9c07e6c97f636)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/jsruntime/qv4compilationunitmapper.cpp
+++ src/qml/jsruntime/qv4compilationunitmapper.cpp
@@ -29,6 +29,11 @@ public:
         s_staticUnits.insert(file, staticUnit);
     }
 
+    void remove(const QString &file)
+    {
+        s_staticUnits.remove(file);
+    }
+
 private:
     QMutexLocker<QMutex> m_lock;
 
@@ -67,6 +72,12 @@ CompiledData::Unit *CompilationUnitMapper::get(
         cache.set(cacheFilePath, *this);
 
     return data;
+}
+
+void CompilationUnitMapper::invalidate(const QString &cacheFilePath)
+{
+    StaticUnitCache cache;
+    cache.remove(cacheFilePath);
 }
 
 QT_END_NAMESPACE
