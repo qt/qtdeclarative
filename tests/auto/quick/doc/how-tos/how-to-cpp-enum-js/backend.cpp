bc6abc010ad5483240ac2e4ae6761864d37a7315
Mitch Curtis <mitch.curtis@qt.io>
2023-03-29 14:53:28 +0800

    Doc: add how-to for using C++ enums in JavaScript
    The best search result for "qt how do i expose a C++ enum to
    javascript" is currently a Qt Forum post, and the relevant information
    in our docs is hard to find within QJSEngine's page.
    
    Add a how-to to make it easier to find the answer.
    
    Task-number: QTBUG-109634
    Change-Id: Ia9c53c5d3826459655aa5b094c4d601b938ef3a3
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit c504227e235885199717129e7c05addd728361fa)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

---
+++ tests/auto/quick/doc/how-tos/how-to-cpp-enum-js/backend.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+//! [file]
+#include "backend.h"
+
+Backend::Backend(QJSEngine *engine) :
+    mEngine(engine)
+{
+}
+
+bool Backend::load()
+{
+    // Do some loading here...
+
+    const QJSValue module = mEngine->importModule(":/script.mjs");
+    if (module.isError()) {
+        qWarning() << "Error loading script.mjs:" << module.toString();
+        return false;
+    }
+
+    const QJSValue function = module.property("backendStatusUpdate");
+    if (!function.isCallable()) {
+        qWarning() << "backendStatusUpdate script function is not callable!";
+        return false;
+    }
+
+    const QJSValue functionResult = function.call(QJSValueList() << Loaded);
+    if (functionResult.isError()) {
+        qWarning() << "backendStatusUpdate script function had errors:" << functionResult.toString();
+        return false;
+    }
+
+    return true;
+}
+//! [file]
