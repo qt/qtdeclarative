d88b52b541a0482b69639dbea8976287db9ca52b
Olivier De Cannière <olivier.decanniere@qt.io>
2023-04-28 11:08:02 +0200

    Examples: Move Qml plugin example to manual tests
    Outdated documention has been removed.
    
    Task-number: QTBUG-111036
    Change-Id: I8db138ff71615a6ed6ac4cebdb365c27252fa5ee
    Pick-to: 6.5
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- tests/auto/quick/examples/tst_examples.cpp
+++ tests/auto/quick/examples/tst_examples.cpp
@@ -54,7 +54,6 @@ tst_examples::tst_examples()
     // Add directories you want excluded here
     excludedDirs << "shared"; //Not an example
     excludedDirs << "snippets/qml/path"; //No root QQuickItem
-    excludedDirs << "examples/qml/qmlextensionplugins"; //Requires special import search path
 
     // These snippets are not expected to run on their own.
     excludedDirs << "snippets/qml/visualdatamodel_rootindex";
