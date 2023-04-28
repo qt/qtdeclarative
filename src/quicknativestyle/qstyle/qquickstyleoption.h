a9644b46cac8b17f62cfc032d8868916523b2663
Fabian Kosmale <fabian.kosmale@qt.io>
2023-04-28 15:16:32 +0200

    Remove obsolete Q_ENUMS usage
    In qgfxsourceproxy_p.h, we can use the modern Q_ENUM. In
    QQuickStyleOption, he usage of Q_ENUMS did not make sense to begin with,
    as the class is neither a QObject nor a Q_GADGET (nor a Q_NAMESPACE).
    
    Change-Id: I5b85a46f57033442504a27bf2e1a05d7b0aaac48
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

--- src/quicknativestyle/qstyle/qquickstyleoption.h
+++ src/quicknativestyle/qstyle/qquickstyleoption.h
@@ -35,7 +35,6 @@ public:
         SO_CustomBase = 0xf00,
         SO_ComplexCustomBase = 0xf000000
     };
-    Q_ENUMS(OptionType)
 
     enum StyleOptionType { Type = SO_Default };
     enum StyleOptionVersion { Version = 1 };
