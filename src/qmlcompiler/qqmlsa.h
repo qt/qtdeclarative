26acad75e8b9b853b3a0dd6a98e6f503c1011029
Ulf Hermann <ulf.hermann@qt.io>
2023-06-16 14:42:03 +0200

    QQmlSA: Fix Element move assignment
    We don't want to leak the previous value and we want to guard against
    self-assignment.
    
    Pick-to: 6.6
    Change-Id: I139ed27e2bc6e858adb9c080df6ec0433d115fae
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/qmlcompiler/qqmlsa.h
+++ src/qmlcompiler/qqmlsa.h
@@ -207,12 +207,7 @@ public:
         memset(other.m_data, 0, sizeofElement);
     }
     Element &operator=(const Element &);
-    Element &operator=(Element &&other) noexcept
-    {
-        memcpy(m_data, other.m_data, sizeofElement);
-        memset(other.m_data, 0, sizeofElement);
-        return *this;
-    }
+    QT_MOVE_ASSIGNMENT_OPERATOR_IMPL_VIA_MOVE_AND_SWAP(Element)
     ~Element();
 
     ScopeType scopeType() const;
@@ -269,6 +264,15 @@ private:
 
     static constexpr qsizetype sizeofElement = 2 * sizeof(QSharedPointer<int>);
     alignas(QSharedPointer<int>) char m_data[sizeofElement];
+
+    void swap(Element &other) noexcept
+    {
+        char t[sizeofElement];
+        memcpy(t, m_data, sizeofElement);
+        memcpy(m_data, other.m_data, sizeofElement);
+        memcpy(other.m_data, t, sizeofElement);
+    }
+    friend void swap(Element &lhs, Element &rhs) noexcept { lhs.swap(rhs); }
 };
 
 class Q_QMLCOMPILER_EXPORT GenericPass
