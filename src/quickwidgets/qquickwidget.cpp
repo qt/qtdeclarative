5f2fdfc64043454c44ba76eb8a275a2c5f9eb198
Shawn Rutledge <shawn.rutledge@qt.io>
2023-07-27 22:07:37 +0200

    QQuickWidget: give each mapped mouse event the same QEventPoint
    Counter-intuitively, this is done by setting the timestamp. Every time
    you construct a new mouse event, you always need to call setTimestamp()
    for the obvious reason: the timestamp is not a ctor argument, but it's
    important for some event receivers. But the non-obvious reason is that
    QMutableEventPoint::setTimestamp() has other useful side effects:
    the velocity calculation is done there, sensibly enough. But to make
    that possible, it also looks up the persistent QEventPoint with the same
    ID as the one in the QMouseEvent, and that's the most important to fix
    QTBUG-114258 specifically.
    
    QQuickFlickablePrivate::handleMoveEvent() calculates delta as
    QEventPoint::position() - mapFromGlobal(globalPressPosition())
    and then QQuickFlickablePrivate::drag() does the drag threshold check.
    If globalPressPosition() is 0,0 because the persistent QEventPoint
    was not passed along in the mapped QMouseEvent, that delta is nearly
    always over the drag threshold. So Flickable took the exclusive grab
    at the first possible opportunity: the second move after press.
    
    Fixes: QTBUG-114258
    Change-Id: Icaf7fb8fde0ef01b486ccf16852ef0f6cfb6a64c
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@qt.io>
    (cherry picked from commit 7ae07f49c1f7f86f37512c74bf0a49244ae46e1f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quickwidgets/qquickwidget.cpp
+++ src/quickwidgets/qquickwidget.cpp
@@ -1462,6 +1462,8 @@ void QQuickWidget::mouseMoveEvent(QMouseEvent *e)
     // top-level window always.
     QMouseEvent mappedEvent(e->type(), e->position(), e->position(), e->globalPosition(),
                             e->button(), e->buttons(), e->modifiers(), e->source());
+    // It's not just the timestamp but also the globalPressPosition, velocity etc.
+    mappedEvent.setTimestamp(e->timestamp());
     QCoreApplication::sendEvent(d->offscreenWindow, &mappedEvent);
     e->setAccepted(mappedEvent.isAccepted());
 }
@@ -1477,10 +1479,12 @@ void QQuickWidget::mouseDoubleClickEvent(QMouseEvent *e)
     // See QTBUG-25831
     QMouseEvent pressEvent(QEvent::MouseButtonPress, e->position(), e->position(), e->globalPosition(),
                            e->button(), e->buttons(), e->modifiers(), e->source());
+    pressEvent.setTimestamp(e->timestamp());
     QCoreApplication::sendEvent(d->offscreenWindow, &pressEvent);
     e->setAccepted(pressEvent.isAccepted());
     QMouseEvent mappedEvent(e->type(), e->position(), e->position(), e->globalPosition(),
                             e->button(), e->buttons(), e->modifiers(), e->source());
+    mappedEvent.setTimestamp(e->timestamp());
     QCoreApplication::sendEvent(d->offscreenWindow, &mappedEvent);
 }
 
@@ -1540,6 +1544,7 @@ void QQuickWidget::mousePressEvent(QMouseEvent *e)
 
     QMouseEvent mappedEvent(e->type(), e->position(), e->position(), e->globalPosition(),
                             e->button(), e->buttons(), e->modifiers(), e->source());
+    mappedEvent.setTimestamp(e->timestamp());
     QCoreApplication::sendEvent(d->offscreenWindow, &mappedEvent);
     e->setAccepted(mappedEvent.isAccepted());
 }
@@ -1553,6 +1558,7 @@ void QQuickWidget::mouseReleaseEvent(QMouseEvent *e)
 
     QMouseEvent mappedEvent(e->type(), e->position(), e->position(), e->globalPosition(),
                             e->button(), e->buttons(), e->modifiers(), e->source());
+    mappedEvent.setTimestamp(e->timestamp());
     QCoreApplication::sendEvent(d->offscreenWindow, &mappedEvent);
     e->setAccepted(mappedEvent.isAccepted());
 }
