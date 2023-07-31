e466290b1608490bcce578de8fad029dd669bdde
Fabian Kosmale <fabian.kosmale@qt.io>
2023-07-31 16:20:50 +0200

    qqmlintsuggestions: Refactor for readability
    Extract methods, combine a few repeated conditions and use early
    returns.
    
    Change-Id: I82b811b7b9a5bc6353a8f8b2a9bb42bb9777acad
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: Semih Yavuz <semih.yavuz@qt.io>

--- src/qmlls/qqmllintsuggestions.cpp
+++ src/qmlls/qqmllintsuggestions.cpp
@@ -103,12 +103,110 @@ QmlLintSuggestions::QmlLintSuggestions(QLanguageServer *server, QmlLsp::QQmlCode
                      &QmlLintSuggestions::diagnose, Qt::DirectConnection);
 }
 
+static void advancePositionPastLocation_helper(const QString &fileContents, const QQmlJS::SourceLocation &location, Position &position) {
+    const int startOffset = location.offset;
+    const int length = location.length;
+    int i = startOffset;
+    int iEnd = i + length;
+    if (iEnd > int(fileContents.size()))
+        iEnd = fileContents.size();
+    while (i < iEnd) {
+        if (fileContents.at(i) == u'\n') {
+            ++position.line;
+            position.character = 0;
+            if (i + 1 < iEnd && fileContents.at(i) == u'\r')
+                ++i;
+        } else {
+            ++position.character;
+        }
+        ++i;
+    }
+};
+
+static Diagnostic createMissingBuildDirDiagnostic()
+{
+    Diagnostic diagnostic;
+    diagnostic.severity = DiagnosticSeverity::Warning;
+    Range &range = diagnostic.range;
+    Position &position = range.start;
+    position.line = 0;
+    position.character = 0;
+    Position &positionEnd = range.end;
+    positionEnd.line = 1;
+    diagnostic.message =
+            "qmlls could not find a build directory, without a build directory "
+            "containing a current build there could be spurious warnings, you might "
+            "want to pass the --build-dir <buildDir> option to qmlls, or set the "
+            "environment variable QMLLS_BUILD_DIRS.";
+    diagnostic.source = QByteArray("qmllint");
+    return diagnostic;
+}
+
+using AdvanceFunc = qxp::function_ref<void(const QQmlJS::SourceLocation &, Position &)>;
+static Diagnostic messageToDiagnostic_helper(AdvanceFunc advancePositionPastLocation,
+                                             std::optional<int> version,
+                                             const Message &message)
+{
+    Diagnostic diagnostic;
+    diagnostic.severity = severityFromMsgType(message.type);
+    Range &range = diagnostic.range;
+    Position &position = range.start;
+
+    QQmlJS::SourceLocation srcLoc = message.loc;
+
+    if (srcLoc.isValid()) {
+        position.line = srcLoc.startLine - 1;
+        position.character = srcLoc.startColumn - 1;
+        advancePositionPastLocation(message.loc,
+                                    position);
+    }
+    range.end = position;
+    diagnostic.message = message.message.toUtf8();
+    diagnostic.source = QByteArray("qmllint");
+
+    auto suggestion = message.fixSuggestion;
+    if (!suggestion.has_value())
+        return diagnostic;
+
+    // We need to interject the information about where the fix suggestions end
+    // here since we don't have access to the textDocument to calculate it later.
+    const QQmlJS::SourceLocation cut = suggestion->location();
+
+    const int line = cut.isValid() ? cut.startLine - 1 : 0;
+    const int column = cut.isValid() ? cut.startColumn - 1 : 0;
+
+    QJsonObject object;
+    object.insert("lspBeginLine"_L1, line);
+    object.insert("lspBeginCharacter"_L1, column);
+
+    Position end = { line, column };
+
+    if (srcLoc.isValid())
+        advancePositionPastLocation(cut, end);
+    object.insert("lspEndLine"_L1, end.line);
+    object.insert("lspEndCharacter"_L1, end.character);
+
+    object.insert("message"_L1, suggestion->fixDescription());
+    object.insert("replacement"_L1, suggestion->replacement());
+
+    QJsonArray fixedSuggestions;
+    fixedSuggestions.append(object);
+    QJsonObject data;
+    data[u"suggestions"] = fixedSuggestions;
+
+    Q_ASSERT(version.has_value());
+    data[u"version"] = version.value();
+
+    diagnostic.data = data;
+
+    return diagnostic;
+};
+
 void QmlLintSuggestions::diagnose(const QByteArray &url)
 {
     const int maxInvalidMsec = 4000;
     qCDebug(lintLog) << "diagnose start";
     QmlLsp::OpenDocumentSnapshot snapshot = m_codeModel->snapshotByUrl(url);
-    QList<Diagnostic> diagnostics;
     std::optional<int> version;
     DomItem doc;
     {
@@ -143,140 +241,73 @@ void QmlLintSuggestions::diagnose(const QByteArray &url)
             lastUpdate.invalidUpdatesSince.reset();
         }
     }
-    QString fileContents;
-    if (doc) {
-        qCDebug(lintLog) << "has doc, do real lint";
-        QStringList imports = m_codeModel->buildPathsForFileUrl(url);
-        imports.append(QLibraryInfo::path(QLibraryInfo::QmlImportsPath));
-        // add m_server->clientInfo().rootUri & co?
-        bool silent = true;
-        QString filename = doc.canonicalFilePath();
-        fileContents = doc.field(Fields::code).value().toString();
-        QStringList qmltypesFiles;
-        QStringList resourceFiles;
-        QList<QQmlJS::LoggerCategory> categories;
 
-        QQmlJSLinter linter(imports);
-
-        linter.lintFile(filename, &fileContents, silent, nullptr, imports, qmltypesFiles,
-                        resourceFiles, categories);
-        auto addLength = [&fileContents](Position &position, int startOffset, int length) {
-            int i = startOffset;
-            int iEnd = i + length;
-            if (iEnd > int(fileContents.size()))
-                iEnd = fileContents.size();
-            while (i < iEnd) {
-                if (fileContents.at(i) == u'\n') {
-                    ++position.line;
-                    position.character = 0;
-                    if (i + 1 < iEnd && fileContents.at(i) == u'\r')
-                        ++i;
-                } else {
-                    ++position.character;
-                }
-                ++i;
-            }
-        };
-
-        auto messageToDiagnostic = [&addLength, &version](const Message &message) {
-            Diagnostic diagnostic;
-            diagnostic.severity = severityFromMsgType(message.type);
-            Range &range = diagnostic.range;
-            Position &position = range.start;
-
-            QQmlJS::SourceLocation srcLoc = message.loc;
-
-            position.line = srcLoc.isValid() ? srcLoc.startLine - 1 : 0;
-            position.character = srcLoc.isValid() ? srcLoc.startColumn - 1 : 0;
-            range.end = position;
-            addLength(range.end, srcLoc.isValid() ? message.loc.offset : 0, srcLoc.isValid() ? message.loc.length : 0);
-            diagnostic.message = message.message.toUtf8();
-            diagnostic.source = QByteArray("qmllint");
-
-            auto suggestion = message.fixSuggestion;
-            if (suggestion.has_value()) {
-                // We need to interject the information about where the fix suggestions end
-                // here since we don't have access to the textDocument to calculate it later.
-                QJsonArray fixedSuggestions;
-                const QQmlJS::SourceLocation cut = suggestion->location();
-
-                const int line = cut.isValid() ? cut.startLine - 1 : 0;
-                const int column = cut.isValid() ? cut.startColumn - 1 : 0;
-
-                QJsonObject object;
-                object.insert("lspBeginLine"_L1, line);
-                object.insert("lspBeginCharacter"_L1, column);
-
-                Position end = { line, column };
-
-                addLength(end, srcLoc.isValid() ? cut.offset : 0,
-                          srcLoc.isValid() ? cut.length : 0);
-                object.insert("lspEndLine"_L1, end.line);
-                object.insert("lspEndCharacter"_L1, end.character);
-
-                object.insert("message"_L1, suggestion->fixDescription());
-                object.insert("replacement"_L1, suggestion->replacement());
-
-                fixedSuggestions << object;
-                QJsonObject data;
-                data[u"suggestions"] = fixedSuggestions;
-
-                Q_ASSERT(version.has_value());
-                data[u"version"] = version.value();
-
-                diagnostic.data = data;
-            }
-            return diagnostic;
-        };
-        doc.iterateErrors(
-                [&diagnostics, &addLength](DomItem, ErrorMessage msg) {
-                    Diagnostic diagnostic;
-                    diagnostic.severity = severityFromMsgType(QtMsgType(int(msg.level)));
-                    // do something with msg.errorGroups ?
-                    auto &location = msg.location;
-                    Range &range = diagnostic.range;
-                    range.start.line = location.startLine - 1;
-                    range.start.character = location.startColumn - 1;
-                    range.end = range.start;
-                    addLength(range.end, location.offset, location.length);
-                    diagnostic.code = QByteArray(msg.errorId.data(), msg.errorId.size());
-                    diagnostic.source = "domParsing";
-                    diagnostic.message = msg.message.toUtf8();
-                    diagnostics.append(diagnostic);
-                    return true;
-                },
-                true);
-
-        if (const QQmlJSLogger *logger = linter.logger()) {
-            qsizetype nDiagnostics = diagnostics.size();
-            for (const auto &messages : { logger->infos(), logger->warnings(), logger->errors() }) {
-                for (const Message &message : messages) {
-                    diagnostics.append(messageToDiagnostic(message));
-                }
-            }
-            if (diagnostics.size() != nDiagnostics && imports.size() == 1) {
-                Diagnostic diagnostic;
-                diagnostic.severity = DiagnosticSeverity::Warning;
-                Range &range = diagnostic.range;
-                Position &position = range.start;
-                position.line = 0;
-                position.character = 0;
-                Position &positionEnd = range.end;
-                positionEnd.line = 1;
-                diagnostic.message =
-                        "qmlls could not find a build directory, without a build directory "
-                        "containing a current build there could be spurious warnings, you might "
-                        "want to pass the --build-dir <buildDir> option to qmlls, or set the "
-                        "environment variable QMLLS_BUILD_DIRS.";
-                diagnostic.source = QByteArray("qmllint");
-                diagnostics.append(diagnostic);
-            }
-        }
-    }
     PublishDiagnosticsParams diagnosticParams;
     diagnosticParams.uri = url;
-    diagnosticParams.diagnostics = diagnostics;
     diagnosticParams.version = version;
+
+    if (!doc) {
+        // without a valid document, we can't find diagnostics
+        m_server->protocol()->notifyPublishDiagnostics(diagnosticParams);
+        return;
+    }
+
+    qCDebug(lintLog) << "has doc, do real lint";
+    QStringList imports = m_codeModel->buildPathsForFileUrl(url);
+    imports.append(QLibraryInfo::path(QLibraryInfo::QmlImportsPath));
+    // add m_server->clientInfo().rootUri & co?
+    bool silent = true;
+    QString filename = doc.canonicalFilePath();
+    QString fileContents = doc.field(Fields::code).value().toString();
+    QStringList qmltypesFiles;
+    QStringList resourceFiles;
+    QList<QQmlJS::LoggerCategory> categories;
+
+    QQmlJSLinter linter(imports);
+
+    linter.lintFile(filename, &fileContents, silent, nullptr, imports, qmltypesFiles,
+                    resourceFiles, categories);
+
+    // ###  TODO: C++20 replace with bind_front
+    auto advancePositionPastLocation = [&fileContents](const QQmlJS::SourceLocation &location, Position &position)
+    {
+        advancePositionPastLocation_helper(fileContents, location, position);
+    };
+    auto messageToDiagnostic = [&advancePositionPastLocation, version](const Message &message)
+    {
+        return messageToDiagnostic_helper(advancePositionPastLocation, version, message);
+    };
+
+    QList<Diagnostic> diagnostics;
+    doc.iterateErrors(
+            [&diagnostics, &advancePositionPastLocation](DomItem, ErrorMessage msg) {
+                Diagnostic diagnostic;
+                diagnostic.severity = severityFromMsgType(QtMsgType(int(msg.level)));
+                // do something with msg.errorGroups ?
+                auto &location = msg.location;
+                Range &range = diagnostic.range;
+                range.start.line = location.startLine - 1;
+                range.start.character = location.startColumn - 1;
+                range.end = range.start;
+                advancePositionPastLocation(location, range.end);
+                diagnostic.code = QByteArray(msg.errorId.data(), msg.errorId.size());
+                diagnostic.source = "domParsing";
+                diagnostic.message = msg.message.toUtf8();
+                diagnostics.append(diagnostic);
+                return true;
+            },
+            true);
+
+    if (const QQmlJSLogger *logger = linter.logger()) {
+        qsizetype nDiagnostics = diagnostics.size();
+        for (const auto &messages : { logger->infos(), logger->warnings(), logger->errors() })
+            for (const Message &message : messages)
+                diagnostics.append(messageToDiagnostic(message));
+        if (diagnostics.size() != nDiagnostics && imports.size() == 1)
+            diagnostics.append(createMissingBuildDirDiagnostic());
+    }
+
+    diagnosticParams.diagnostics = diagnostics;
 
     m_server->protocol()->notifyPublishDiagnostics(diagnosticParams);
     qCDebug(lintLog) << "lint" << QString::fromUtf8(url) << "found"
