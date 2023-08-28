qt/qtdeclarative

commit 1875ad7f92cad270cc5857d71096a4b46c27c562

Introduce new mechanism to manage palette functionality in QML
Main goals of these changes:
1) Add an ability to work with disabled and inactive palettes from QML
2) Eliminate massive code duplication in qtquickcontrols2 module
3) Provide easily extensible architecture for this piece of
functionality

Architectural part.

Palette
It was decided to not change existing QPalette, but add thin wrappers
around it to provide all required functionality. These wrappers are
highly coupled with QPalette class because of using some enum values
from it.
There are two new classes QQuickPalette and QQuickColorGroup.
QQuickPalette class inherits QQuickColorGroup class and represents
Active/All color group. QQuickPalette also provides an access to three
color groups: Active, Inactive, and Disabled.
In order to access colors the special class QQuickPaletteColorProvider
is used. This is a wrapper around QPalette that provides some
convenience functions.

Interface
The private property "palette" should be exposed.

Implementation
All private parts of classes that implement
QQuickAbstractPaletteProvider have to inherit
QQuickPaletteProviderPrivateBase class. This template class implement
all functionality: create palette, resolve dependencies, connect objects
etc. This is important to mention that related data is lazily
allocatable on demand only. Hence, there is no memory overhead for
regular items.

Change-Id: I911424b730451b1ad47f68fd8007953b66eddb28
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/quick/items/qquickpaletteproviderprivatebase_p.h
@@ -1,0 +1,398 @@
+/****************************************************************************
+**
+** Copyright (C) 2020 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the QtQuick module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+#ifndef QQUICKPALETTEPROVIDERPRIVATEBASE_H
+#define QQUICKPALETTEPROVIDERPRIVATEBASE_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtQuick/private/qquickpalette_p.h>
+#include <QtQuick/private/qquickabstractpaletteprovider_p.h>
+
+#include <QtQml/private/qlazilyallocated_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class QWindow;
+class QQuickWindow;
+class QQuickWindowPrivate;
+class QQuickItem;
+class QQuickItemPrivate;
+class QQuickPopup;
+class QQuickPopupPrivate;
+
+/*!
+    \internal
+
+    Implements all required operations with palette.
+
+    I -- is interface class (e.g. QQuickItem).
+    Impl -- is implementation class (e.g. QQuickItemPrivate).
+
+    To use this class you need to inherit implementation class from it.
+ */
+template <class I, class Impl>
+class QQuickPaletteProviderPrivateBase : public QQuickAbstractPaletteProvider
+{
+    static_assert(std::is_base_of<QObject, I>{}, "The interface class must inherit QObject");
+
+public:
+    virtual ~QQuickPaletteProviderPrivateBase() = default;
+
+    /*!
+        \internal
+
+        Get current palette.
+
+        \note Palette might be lazily allocated. Signal \p paletteCreated()
+              will be emitted by an object of interface class in this case.
+
+        \note This function doesn't ask an object of interface class to emit
+              paletteChanged() signal in order to avoid problems with
+              property bindigns.
+    */
+    virtual QQuickPalette *palette() const;
+
+    /*!
+        \internal
+
+        Set new palette. Doesn't transfer ownership.
+    */
+    virtual void setPalette(QQuickPalette *p);
+
+    /*!
+        \internal
+
+        Reset palette to the default one.
+    */
+    virtual void resetPalette();
+
+    /*!
+        \internal
+
+        Check if everything is internally allocated and palette exists.
+
+        Use before call \p palette() to avoid unnecessary allocations.
+    */
+    virtual bool providesPalette() const;
+
+    /*!
+        \internal
+
+        A default palette for this component.
+    */
+    virtual QPalette defaultPalette() const;
+
+    /*!
+        \internal
+
+        A parent palette for this component. Can be null.
+    */
+    virtual QPalette parentPalette() const;
+
+    /*!
+        \internal
+
+        Inherit from \p parentPalette. This function is also called when
+        either parent or window of this item is changed.
+    */
+    void inheritPalette(const QPalette &parentPalette);
+
+    /*!
+        \internal
+
+        Updates children palettes. The default implementation invokes
+        inheritPalette for all visual children.
+
+        This function is also called when palette is changed
+        (signal changed() is emitted).
+     */
+    virtual void updateChildrenPalettes(const QPalette &parentPalette);
+
+private:
+    using PalettePtr = std::unique_ptr<QQuickPalette>;
+    using Self = QQuickPaletteProviderPrivateBase<I, Impl>;
+
+    void registerPalette(PalettePtr palette);
+
+    bool isValidPalette(const QQuickPalette *palette) const;
+
+    QQuickPalette *windowPalette() const;
+
+    void setCurrentColorGroup();
+
+    void connectItem();
+
+    const I *itemWithPalette() const;
+    I *itemWithPalette();
+
+    QQuickPalette *paletteData() const;
+
+    QPalette toQPalette() const;
+
+private:
+    PalettePtr m_palette;
+};
+
+template<class I, class Impl>
+QQuickPalette *QQuickPaletteProviderPrivateBase<I, Impl>::palette() const
+{
+    if (!providesPalette()) {
+        // It's required to create a new palette without parent,
+        // because this method can be called from the rendering thread
+        const_cast<Self*>(this)->registerPalette(std::make_unique<QQuickPalette>());
+        Q_EMIT const_cast<Self*>(this)->itemWithPalette()->paletteCreated();
+    }
+
+    return paletteData();
+}
+
+template<class I, class Impl>
+bool QQuickPaletteProviderPrivateBase<I, Impl>::isValidPalette(const QQuickPalette *palette) const
+{
+    if (!palette) {
+        qWarning("Palette cannot be null.");
+        return false;
+    }
+
+    if (providesPalette() && paletteData() == palette) {
+        qWarning("Self assignment makes no sense.");
+        return false;
+    }
+
+    return true;
+}
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::setPalette(QQuickPalette *p)
+{
+    if (isValidPalette(p)) {
+        palette()->fromQPalette(p->toQPalette());
+    }
+}
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::resetPalette()
+{
+    paletteData()->reset();
+}
+
+template<class I, class Impl>
+bool QQuickPaletteProviderPrivateBase<I, Impl>::providesPalette() const
+{
+    return !!m_palette;
+}
+
+template<class I, class Impl>
+QPalette QQuickPaletteProviderPrivateBase<I, Impl>::defaultPalette() const
+{
+    return QPalette();
+}
+
+template <class Window>
+inline constexpr bool isRootWindow() { return std::is_base_of_v<QWindow, Window>; }
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::registerPalette(PalettePtr palette)
+{
+    if constexpr (!isRootWindow<I>()) {
+        // Connect item only once, before initial data allocation
+        if (!providesPalette()) {
+            connectItem();
+        }
+    }
+
+    m_palette = std::move(palette);
+    m_palette->setPaletteProvider(this);
+    m_palette->inheritPalette(parentPalette());
+
+    setCurrentColorGroup();
+
+    // In order to avoid extra noise, we should connect
+    // the following signals only after everything is already setup
+    I::connect(paletteData(), &QQuickPalette::changed, itemWithPalette(), &I::paletteChanged);
+    I::connect(paletteData(), &QQuickPalette::changed, [this]{ updateChildrenPalettes(toQPalette()); });
+}
+
+template<class T> struct dependent_false : std::false_type {};
+template<class Impl, class I> decltype(auto) getPrivateImpl(I &t) { return Impl::get(&t); }
+
+template <class T>
+decltype(auto) getPrivate(T &item)
+{
+    if constexpr (std::is_same_v<T, QQuickWindow>) {
+        return getPrivateImpl<QQuickWindowPrivate>(item);
+    } else if constexpr (std::is_same_v<T, QQuickItem>) {
+        return getPrivateImpl<QQuickItemPrivate>(item);
+    } else {
+        static_assert (dependent_false<T>::value, "Extend please.");
+    }
+}
+
+template<class I, class Impl>
+QQuickPalette *QQuickPaletteProviderPrivateBase<I, Impl>::windowPalette() const
+{
+    if constexpr (!isRootWindow<I>()) {
+        if (auto window = itemWithPalette()->window()) {
+            if (getPrivate(*window)->providesPalette()) {
+                return getPrivate(*window)->palette();
+            }
+        }
+    }
+
+    return nullptr;
+}
+
+template<class I, class Impl>
+QPalette QQuickPaletteProviderPrivateBase<I, Impl>::parentPalette() const
+{
+    if constexpr (!isRootWindow<I>()) {
+        for (auto parentItem = itemWithPalette()->parentItem(); parentItem;
+             parentItem = parentItem->parentItem()) {
+
+            // Don't allocate a new palette here. Use only if it's already pre allocated
+            if (parentItem && getPrivate(*parentItem)->providesPalette()) {
+                return getPrivate(*parentItem)->palette()->toQPalette();
+            }
+        }
+
+        if (auto wp = windowPalette()) {
+            return wp->toQPalette();
+        }
+    }
+
+    return defaultPalette();
+}
+
+template<class I>
+const QQuickItem* rootItem(const I &item)
+{
+    if constexpr (isRootWindow<I>()) {
+        return item.contentItem();
+    } else if constexpr (std::is_same_v<QQuickPopup, I>) {
+        return nullptr;
+    } else {
+        return &item;
+    }
+}
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::inheritPalette(const QPalette &parentPalette)
+{
+    if (providesPalette()) {
+        // If palette is changed, then this function will be invoked
+        // for all children because of connection with signal changed()
+        m_palette->inheritPalette(parentPalette);
+    } else {
+        // Otherwise, just propagate parent palette to all children
+        updateChildrenPalettes(parentPalette);
+    }
+}
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::setCurrentColorGroup()
+{
+    if constexpr (!isRootWindow<I>()) {
+        if (paletteData()) {
+            const bool enabled = itemWithPalette()->isEnabled();
+            paletteData()->setCurrentGroup(enabled ? QPalette::Active : QPalette::Disabled);
+        }
+    }
+}
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::updateChildrenPalettes(const QPalette &parentPalette)
+{
+    if (auto root = rootItem(*itemWithPalette())) {
+        for (auto &&child : root->childItems()) {
+            if (Q_LIKELY(child)) {
+                getPrivate(*child)->inheritPalette(parentPalette);
+            }
+        }
+    }
+}
+
+template<class I, class Impl>
+void QQuickPaletteProviderPrivateBase<I, Impl>::connectItem()
+{
+    Q_ASSERT(itemWithPalette());
+
+    if constexpr (!isRootWindow<I>()) {
+        // Item with palette has the same lifetime as its implementation that inherits this class
+        I::connect(itemWithPalette(), &I::parentChanged , [this]() { inheritPalette(parentPalette()); });
+        I::connect(itemWithPalette(), &I::windowChanged , [this]() { inheritPalette(parentPalette()); });
+        I::connect(itemWithPalette(), &I::enabledChanged, [this]() { setCurrentColorGroup(); });
+    }
+}
+
+template<class I, class Impl>
+const I *QQuickPaletteProviderPrivateBase<I, Impl>::itemWithPalette() const
+{
+    static_assert(std::is_base_of<QObjectData, Impl>{},
+                  "The Impl class must inherit QObjectData");
+
+    return static_cast<const I*>(static_cast<const Impl*>(this)->q_ptr);
+}
+
+template<class I, class Impl>
+I *QQuickPaletteProviderPrivateBase<I, Impl>::itemWithPalette()
+{
+    return const_cast<I*>(const_cast<const Self*>(this)->itemWithPalette());
+}
+
+template<class I, class Impl>
+QQuickPalette *QQuickPaletteProviderPrivateBase<I, Impl>::paletteData() const
+{
+    Q_ASSERT(m_palette); return m_palette.get();
+}
+
+template<class I, class Impl>
+QPalette QQuickPaletteProviderPrivateBase<I, Impl>::toQPalette() const
+{
+    return palette()->toQPalette();
+}
+
+QT_END_NAMESPACE
+
+#endif // QQUICKPALETTEPROVIDERPRIVATEBASE_H

src/quick/CMakeLists.txt
@@ -80,6 +80,11 @@ qt_add_module(Quick
         items/qquickwindow.cpp items/qquickwindow.h items/qquickwindow_p.h
         items/qquickwindowattached.cpp items/qquickwindowattached_p.h
         items/qquickwindowmodule.cpp items/qquickwindowmodule_p.h
+        items/qquickwindowmodule_p_p.h
+        items/qquickpalette.cpp items/qquickpalette_p.h
+        items/qquickcolorgroup.cpp items/qquickcolorgroup_p.h
+        items/qquickpalettecolorprovider.cpp items/qquickpalettecolorprovider_p.h
+        items/qquickpaletteproviderprivatebase_p.h
         qtquick2.cpp qtquick2_p.h
         qtquickglobal.h qtquickglobal_p.h
         scenegraph/adaptations/software/qsgabstractsoftwarerenderer.cpp scenegraph/adaptations/software/qsgabstractsoftwarerenderer_p.h

src/quick/designer/qquickdesignerwindowmanager.cpp
@@ -39,6 +39,7 @@
 
 #include "qquickdesignerwindowmanager_p.h"
 #include "private/qquickwindow_p.h"
+#include "private/qquickitem_p.h"
 #include <QtQuick/QQuickWindow>
 #if QT_CONFIG(opengl)
 #include <private/qsgdefaultrendercontext_p.h>

src/quick/items/items.pri
@@ -1,4 +1,5 @@
 HEADERS += \
+    $$PWD/qquickabstractpaletteprovider_p.h \
     $$PWD/qquickevents_p_p.h \
     $$PWD/qquickanchors_p.h \
     $$PWD/qquickanchors_p_p.h \
@@ -56,10 +57,15 @@ HEADERS += \
     $$PWD/qquickscreen_p.h \
     $$PWD/qquickwindowattached_p.h \
     $$PWD/qquickwindowmodule_p.h \
+    $$PWD/qquickwindowmodule_p_p.h \
     $$PWD/qquickrendercontrol.h \
     $$PWD/qquickrendercontrol_p.h \
     $$PWD/qquickgraphicsinfo_p.h \
-    $$PWD/qquickitemgrabresult.h
+    $$PWD/qquickitemgrabresult.h \
+    $$PWD/qquickpalette_p.h \
+    $$PWD/qquickcolorgroup_p.h \
+    $$PWD/qquickpalettecolorprovider_p.h \
+    $$PWD/qquickpaletteproviderprivatebase_p.h
 
 SOURCES += \
     $$PWD/qquickevents.cpp \
@@ -99,7 +105,10 @@ SOURCES += \
     $$PWD/qquickwindowattached.cpp \
     $$PWD/qquickrendercontrol.cpp \
     $$PWD/qquickgraphicsinfo.cpp \
-    $$PWD/qquickitemgrabresult.cpp
+    $$PWD/qquickitemgrabresult.cpp \
+    $$PWD/qquickpalettecolorprovider.cpp \
+    $$PWD/qquickcolorgroup.cpp \
+    $$PWD/qquickpalette.cpp
 
 qtConfig(quick-draganddrop) {
     HEADERS += \

src/quick/items/qquickabstractpaletteprovider_p.h
@@ -1,0 +1,64 @@
+/****************************************************************************
+**
+** Copyright (C) 2020 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
+**
+** This file is part of the QtQuick module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL3$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPL included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+#ifndef QQUICKABSTRACTPALETTEPROVIDER_P_H
+#define QQUICKABSTRACTPALETTEPROVIDER_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtQuick/private/qtquickglobal_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class QQuickAbstractPaletteProvider
+{
+public:
+    virtual ~QQuickAbstractPaletteProvider() = default;
+    virtual QPalette defaultPalette() const = 0;
+    virtual QPalette parentPalette() const = 0;
+};
+
+QT_END_NAMESPACE
+
+#endif // QQUICKABSTRACTPALETTEPROVIDER_P_H

commit 950cd5888e3098bc4c04fc0af4beabe265c78a6e

Fix child popup palette inheritance
Qt Quick Controls Popups are always top-level popups, even if declared
as a child of another popup. Therefore, e.g. a font set on a parent
popup should not propagate to a child popup. The same behavior applies
to palettes, but we regressed with this in
1875ad7f92cad270cc5857d71096a4b46c27c562 and/or
31f5c21ddb571d744efd1885c0687816b3a12225.

This patch fixes the regression by returning the window's palette in
QQuickPaletteProviderPrivateBase<I, Impl>::parentPalette() if I is
a QQuickPopup.

Fixes: QTBUG-115707
Pick-to: 6.5 6.6
Change-Id: Ib89852787dd45e3edc9e846b6c182340c052f0fd
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/quick/items/qquickpaletteproviderprivatebase_p.h
@@ -230,9 +230,9 @@ template<class Impl, class I> decltype(auto) getPrivateImpl(I &t) { return Impl:
 template <class T>
 decltype(auto) getPrivate(T &item)
 {
-    if constexpr (std::is_same_v<T, QQuickWindow>) {
+    if constexpr (std::is_base_of_v<T, QQuickWindow>) {
         return getPrivateImpl<QQuickWindowPrivate>(item);
-    } else if constexpr (std::is_same_v<T, QQuickItem>) {
+    } else if constexpr (std::is_base_of_v<T, QQuickItem>) {
         return getPrivateImpl<QQuickItemPrivate>(item);
     } else {
         static_assert (dependent_false<T>::value, "Extend please.");
@@ -257,12 +257,15 @@ template<class I, class Impl>
 QPalette QQuickPaletteProviderPrivateBase<I, Impl>::parentPalette(const QPalette &fallbackPalette) const
 {
     if constexpr (!isRootWindow<I>()) {
-        for (auto parentItem = itemWithPalette()->parentItem(); parentItem;
-             parentItem = parentItem->parentItem()) {
+        // Popups should always inherit from their window, even child popups: QTBUG-115707.
+        if (!std::is_base_of_v<QQuickPopup, I>) {
+            for (auto parentItem = itemWithPalette()->parentItem(); parentItem;
+                 parentItem = parentItem->parentItem()) {
 
-            // Don't allocate a new palette here. Use only if it's already pre allocated
-            if (parentItem && getPrivate(*parentItem)->providesPalette()) {
-                return getPrivate(*parentItem)->palette()->toQPalette();
+                // Don't allocate a new palette here. Use only if it's already pre allocated
+                if (parentItem && getPrivate(*parentItem)->providesPalette()) {
+                    return getPrivate(*parentItem)->palette()->toQPalette();
+                }
             }
         }
 
@@ -279,7 +282,7 @@ const QQuickItem* rootItem(const I &item)
 {
     if constexpr (isRootWindow<I>()) {
         return item.contentItem();
-    } else if constexpr (std::is_same_v<QQuickPopup, I>) {
+    } else if constexpr (std::is_base_of_v<QQuickPopup, I>) {
         return nullptr;
     } else {
         return &item;

tests/auto/quickcontrols/palette/data/childPopupInheritance.qml
@@ -1,0 +1,40 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+import QtQuick
+import QtQuick.Controls
+
+ApplicationWindow {
+    id: window
+
+    Popup {
+        id: parentPopup
+        objectName: "parentPopup"
+        palette.windowText: "#ffdead"
+
+        Label {
+            text: "parentPopupLabel"
+            objectName: text
+        }
+
+        Popup {
+            id: childPopup
+            objectName: "childPopup"
+
+            Label {
+                text: "childPopupLabel"
+                objectName: text
+            }
+
+            Popup {
+                id: grandchildPopup
+                objectName: "grandchildPopup"
+
+                Label {
+                    text: "grandchildPopupLabel"
+                    objectName: text
+                }
+            }
+        }
+    }
+}

tests/auto/quickcontrols/palette/tst_palette.cpp
@@ -7,6 +7,7 @@
 #include <QtQml/qqmlengine.h>
 #include <QtQml/qqmlcomponent.h>
 #include <QtQuickTestUtils/private/qmlutils_p.h>
+#include <QtQuickControlsTestUtils/private/controlstestutils_p.h>
 #include <QtQuick/private/qquickitem_p.h>
 #include <QtQuickTemplates2/private/qquickapplicationwindow_p.h>
 #include <QtQuickTemplates2/private/qquickcontrol_p.h>
@@ -18,7 +19,7 @@
 #include <QtQuickControls2/qquickstyle.h>
 #include <QSignalSpy>
 
-//using namespace QQuickVisualTestUtils;
+using namespace QQuickControlsTestUtils;
 
 // Need a more descriptive failure message: QTBUG-87039
 #define COMPARE_PALETTES(actualPalette, expectedPalette) \
@@ -41,6 +42,7 @@ private slots:
 
     void inheritance_data();
     void inheritance();
+    void childPopupInheritance();
 
     void defaultPalette_data();
     void defaultPalette();
@@ -204,6 +206,40 @@ void tst_palette::inheritance()
     auto grandChildMo = grandChild->metaObject();
     grandChildMo->property(grandChildMo->indexOfProperty("palette")).reset(grandChild);
     QCOMPARE(grandChildPalette->window(), windowPalette->window());
+}
+
+// The child popups in inheritance() don't test actual nested child popups,
+// only popups that are children of items and the items within those popups.
+// We need to specifically test this to prevent QTBUG-115707 from happening again.
+void tst_palette::childPopupInheritance()
+{
+    QQuickControlsApplicationHelper helper(this, QLatin1String("childPopupInheritance.qml"));
+    QVERIFY2(helper.ready, helper.failureMessage());
+
+    const auto *windowPrivate = QQuickWindowPrivate::get(helper.window);
+    const auto windowsWindowTextColor = windowPrivate->palette()->toQPalette().color(QPalette::WindowText);
+
+    // parentPopup sets windowText explicitly, so its label should use that color.
+    auto *parentPopup = helper.appWindow->findChild<QQuickPopup *>("parentPopup");
+    QVERIFY(parentPopup);
+    parentPopup->open();
+    QTRY_VERIFY(parentPopup->isOpened());
+    auto *parentPopupLabel = helper.appWindow->findChild<QObject *>("parentPopupLabel");
+    QVERIFY(parentPopupLabel);
+    QCOMPARE(parentPopupLabel->property("color").value<QColor>(), "#ffdead");
+
+    // All other child popups don't set anything explicitly, and should inherit from their window.
+    auto *childPopup = helper.appWindow->findChild<QQuickPopup *>("childPopup");
+    QVERIFY(childPopup);
+    auto *childPopupLabel = helper.appWindow->findChild<QObject *>("childPopupLabel");
+    QVERIFY(childPopupLabel);
+    QCOMPARE(childPopupLabel->property("color").value<QColor>(), windowsWindowTextColor);
+
+    auto *grandchildPopup = helper.appWindow->findChild<QQuickPopup *>("grandchildPopup");
+    QVERIFY(grandchildPopup);
+    auto *grandchildPopupLabel = helper.appWindow->findChild<QObject *>("grandchildPopupLabel");
+    QVERIFY(grandchildPopupLabel);
+    QCOMPARE(grandchildPopupLabel->property("color").value<QColor>(), windowsWindowTextColor);
 }
 
 class TestTheme : public QQuickTheme