da8e94e2445a45b9a880d23a602f29b2e1158ace
Sze Howe Koh <szehowe.koh@gmail.com>
2023-08-31 14:09:55 +0800

    Doc: Fix incorrect default value for FolderListModel.folder
    This behavior has been in place since Qt 5 (or perhaps even earlier)
    
    Change-Id: If60192e4302f5b1316d29dc42139eb66855789a1
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit e041ba46f2f407a306746b6a1dbe7d48a40401b7)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/labs/folderlistmodel/qquickfolderlistmodel.cpp
+++ src/labs/folderlistmodel/qquickfolderlistmodel.cpp
@@ -395,7 +395,8 @@ QModelIndex QQuickFolderListModel::index(int row, int , const QModelIndex &) con
 
     The value must be a \c file: or \c qrc: URL, or a relative URL.
 
-    The default value is an invalid URL.
+    The default value is the application's working directory at the time
+    when the FolderListModel is first initialized.
 */
 QUrl QQuickFolderListModel::folder() const
 {
