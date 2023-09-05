1d1ba46f54bcec6449d285c8f1f71540cdb02015
Tinja Paavoseppä <tinja.paavoseppa@qt.io>
2023-09-05 13:26:42 +0300

    Android: Add QtQuickView class to embed QQuickView to Android
    Add Java class QtQuickView to enable embedding a QQuickView to
    an Android app form Java or Kotlin. Also add a JNI helper method
    to be able to create the QQuickView from Java/Kotlin.
    
    Pick-to: 6.7
    Task-number: QTBUG-116953
    Change-Id: I131eb84f3247fea3ec8d08612e6dd6567f5dca83
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

---
+++ src/quick/platform/android/qandroidquickviewembedding_p.h
@@ -0,0 +1,34 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QANDROIDQUICKVIEWEMBEDDING_P_H
+#define QANDROIDQUICKVIEWEMBEDDING_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include <QtCore/qjnienvironment.h>
+#include <QtCore/qjnitypes.h>
+#include <QtQuick/qquickview.h>
+
+QT_BEGIN_NAMESPACE
+
+namespace QtAndroidQuickViewEmbedding
+{
+    bool registerNatives(QJniEnvironment& env);
+    void createQuickView(JNIEnv *env, jobject nativeWindow, jstring qmlUri, jint width, jint height,
+                         jlong parentWindowReference);
+    Q_DECLARE_JNI_NATIVE_METHOD_IN_CURRENT_SCOPE(createQuickView)
+};
+
+QT_END_NAMESPACE
+
+#endif // QANDROIDQUICKVIEWEMBEDDING_P_H
