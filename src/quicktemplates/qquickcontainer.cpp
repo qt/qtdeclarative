47b4037ccca3d9d38c2d172bafdeb5dcce699eb7
Jan Arve Sæther <jan-arve.saether@qt.io>
2023-10-25 17:00:47 +0200

    Fix crash when calling QQmlIncubator::abort()
    QQuickContainerPrivate::itemDestroyed did not properly
    clean up its internal data structures to have no references to the item
    that got destroyed. This was mainly because the call to
    
      q->isContent(item)
    
    in QQuickContainerPrivate::removeItem() would return false, so it would
    not continue doing the rest of the required cleanup that removeItem
    does, and it was therefore leaving dangling pointers to the destroyed
    item in QQuickContainers data structures.
    
    Pick-to: 6.5 6.6
    Fixes: QTBUG-118397
    Change-Id: I7f23b4f98df33d69b42455d20016adb7e132b510
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

--- src/quicktemplates/qquickcontainer.cpp
+++ src/quicktemplates/qquickcontainer.cpp
@@ -256,7 +256,8 @@ void QQuickContainerPrivate::moveItem(int from, int to, QQuickItem *item)
 void QQuickContainerPrivate::removeItem(int index, QQuickItem *item)
 {
     Q_Q(QQuickContainer);
-    if (!q->isContent(item))
+    const bool item_inDestructor = QQuickItemPrivate::get(item)->inDestructor;
+    if (!item_inDestructor && !q->isContent(item))
         return;
     contentData.removeOne(item);
 
@@ -271,8 +272,11 @@ void QQuickContainerPrivate::removeItem(int index, QQuickItem *item)
         currentChanged = true;
     }
 
-    QQuickItemPrivate::get(item)->removeItemChangeListener(this, changeTypes);
-    item->setParentItem(nullptr);
+    if (!item_inDestructor) {
+        // already handled by ~QQuickItem
+        QQuickItemPrivate::get(item)->removeItemChangeListener(this, changeTypes);
+        item->setParentItem(nullptr);
+    }
     contentModel->remove(index);
     --count;
 
