qt/qtdeclarative

commit d0fcb75aab1b5a91260fb378d761257e1f2e4787

qmlls: use resource files, when available
When linting via qmllint, the resource files are used to map from source
folders to buildfolders and are needed to load qmldir files from the
build folder, for example.

The linting module of qmlls was not loading qmldir, for example, because
it did not pass the resource folders to the linting method.
This is the reason why qmldir imports, for example, were not working in
qmlls.

Find the resource .qrc files from the build folder and let qmlls pass
them to the linting methods. Also fix the import folders to discover
in-source-directory qmldir files, when no qmldir file was found in the
build folder, in the same way as qmllint does it.

Add some tests for both cases (qmldir in build directory and qmldir in
the source directory).

Fixes: QTBUG-111429
Change-Id: I885c962cac5b1276f12bc28d6d47c43212f853e4
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/qmldom/qqmldomastcreator.cpp
@@ -12,8 +12,10 @@
 #include "qqmldomastdumper_p.h"
 #include "qqmldomattachedinfo_p.h"
 #include "qqmldomastcreator_p.h"
+#include "qqmldom_utils_p.h"
 
 #include <QtQml/private/qqmljsast_p.h>
+#include <QtQmlCompiler/private/qqmljsutils_p.h>
 
 #include <QtCore/QDir>
 #include <QtCore/QFileInfo>
@@ -280,17 +282,19 @@ bool QQmlDomAstCreator::visit(UiProgram *program)
     // we hide the component span because the component s written after the imports
     FileLocations::addRegion(rootMap, QString(), combineLocations(program));
     pushEl(p, *cPtr, program);
-    // implicit imports
+
     // add implicit directory import
     if (!fInfo.canonicalPath().isEmpty()) {
         Import selfDirImport(QmlUri::fromDirectoryString(fInfo.canonicalPath()));
         selfDirImport.implicit = true;
         qmlFilePtr->addImport(selfDirImport);
     }
+    // add implicit imports from the environment (QML, QtQml for example)
     for (Import i : qmlFile.environment().ownerAs<DomEnvironment>()->implicitImports()) {
         i.implicit = true;
         qmlFilePtr->addImport(i);
     }
+
     return true;
 }
 
@@ -2118,10 +2122,11 @@ static QStringList qmldirFilesFrom(MutableDomItem &qmlFile)
 }
 
 QQmlDomAstCreatorWithQQmlJSScope::QQmlDomAstCreatorWithQQmlJSScope(MutableDomItem &qmlFile,
-                                                                   QQmlJSLogger *logger)
+                                                                   QQmlJSLogger *logger,
+                                                                   QQmlJSResourceFileMapper *mapper)
     : m_root(QQmlJSScope::create()),
       m_logger(logger),
-      m_importer(importPathsFrom(qmlFile), nullptr, true),
+      m_importer(importPathsFrom(qmlFile), mapper, true),
       m_implicitImportDirectory(QQmlJSImportVisitor::implicitImportDirectory(
               m_logger->fileName(), m_importer.resourceFileMapper())),
       m_scopeCreator(m_root, &m_importer, m_logger, m_implicitImportDirectory,
@@ -2226,10 +2231,19 @@ void createDom(MutableDomItem qmlFile, DomCreationOptions options)
 {
     if (std::shared_ptr<QmlFile> qmlFilePtr = qmlFile.ownerAs<QmlFile>()) {
         QQmlJSLogger logger; // TODO
+
+        std::unique_ptr<QQmlJSResourceFileMapper> mapper;
+        if (auto environmentPtr = qmlFile.environment().ownerAs<DomEnvironment>()) {
+            const QStringList resourceFiles =
+                    resourceFilesFromBuildFolders(environmentPtr->loadPaths());
+            mapper = std::make_unique<QQmlJSResourceFileMapper>(
+                    resourceFilesFromBuildFolders(resourceFiles));
+        }
         // the logger filename is used to populate the QQmlJSScope filepath.
         logger.setFileName(qmlFile.canonicalFilePath());
         if (options.testFlag(DomCreationOption::WithSemanticAnalysis)) {
-            auto v = std::make_unique<QQmlDomAstCreatorWithQQmlJSScope>(qmlFile, &logger);
+            auto v = std::make_unique<QQmlDomAstCreatorWithQQmlJSScope>(qmlFile, &logger,
+                                                                        mapper.get());
             v->enableScriptExpressions(options.testFlag(DomCreationOption::WithScriptExpressions));
 
             AST::Node::accept(qmlFilePtr->ast(), v.get());

tests/auto/qmlls/modules/tst_qmlls_modules.cpp
@@ -1342,6 +1342,61 @@ void tst_qmlls_modules::rangeFormatting()
     QTRY_VERIFY_WITH_TIMEOUT(*didFinish, 10000);
 }
 
+void tst_qmlls_modules::qmldirImportsFromBuild()
+{
+    const QString filePath = u"completions/fromBuildDir.qml"_s;
+    const auto uri = openFile(filePath);
+    QVERIFY(uri);
+
+    Notifications::AddBuildDirsParams bDirs;
+    UriToBuildDirs ub;
+    ub.baseUri = *uri;
+    ub.buildDirs.append(testFile("buildDir").toUtf8());
+    bDirs.buildDirsToSet.append(ub);
+    m_protocol->typedRpc()->sendNotification(QByteArray(Notifications::AddBuildDirsMethod), bDirs);
+
+    bool diagnosticOk = false;
+    m_protocol->registerPublishDiagnosticsNotificationHandler(
+            [&diagnosticOk, &uri](const QByteArray &, const PublishDiagnosticsParams &p) {
+                if (p.uri != *uri)
+                    return;
+
+                if constexpr (enable_debug_output) {
+                    for (const auto &x : p.diagnostics) {
+                        qDebug() << x.message;
+                    }
+                }
+                QCOMPARE(p.diagnostics.size(), 0);
+                diagnosticOk = true;
+            });
+
+    QTRY_VERIFY_WITH_TIMEOUT(diagnosticOk, 5000);
+}
+
+void tst_qmlls_modules::qmldirImportsFromSource()
+{
+    const QString filePath = u"sourceDir/Main.qml"_s;
+    const auto uri = openFile(filePath);
+    QVERIFY(uri);
+
+    bool diagnosticOk = false;
+    m_protocol->registerPublishDiagnosticsNotificationHandler(
+            [&diagnosticOk, &uri](const QByteArray &, const PublishDiagnosticsParams &p) {
+                if (p.uri != *uri)
+                    return;
+
+                if constexpr (enable_debug_output) {
+                    for (const auto &x : p.diagnostics) {
+                        qDebug() << x.message;
+                    }
+                }
+                QCOMPARE(p.diagnostics.size(), 0);
+                diagnosticOk = true;
+            });
+
+    QTRY_VERIFY_WITH_TIMEOUT(diagnosticOk, 5000);
+}
+
 QTEST_MAIN(tst_qmlls_modules)
 
 #include <tst_qmlls_modules.moc>

tests/auto/qmlls/modules/tst_qmlls_modules.h
@@ -60,6 +60,8 @@ private slots:
     void linting();
     void rangeFormatting_data();
     void rangeFormatting();
+    void qmldirImportsFromBuild();
+    void qmldirImportsFromSource();
 
 private:
     QProcess m_server;

src/qmldom/qqmldom_utils.cpp
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qqmldom_utils_p.h"
+#include <QtCore/qdir.h>
+#include <QtCore/qdiriterator.h>
+#include <QtCore/qstring.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -9,6 +12,25 @@ Q_LOGGING_CATEGORY(QQmlJSDomImporting, "qt.qqmljsdom.importing")
 
 namespace QQmlJS {
 namespace Dom {
+
+using namespace Qt::StringLiterals;
+
+QStringList resourceFilesFromBuildFolders(const QStringList &buildFolders)
+{
+    QStringList result;
+    for (const QString &path : buildFolders) {
+        QDir dir(path);
+        if (!dir.cd(u".rcc"_s))
+            continue;
+
+        QDirIterator it(dir.canonicalPath(), QStringList{ u"*.qrc"_s }, QDir::Files,
+                        QDirIterator::Subdirectories);
+        while (it.hasNext()) {
+            result.append(it.next());
+        }
+    }
+    return result;
+}
 
 } // namespace Dom
 }; // namespace QQmlJS

src/qmldom/qqmldom_utils_p.h
@@ -17,6 +17,7 @@
 
 #include <QtCore/qglobal.h>
 #include "qqmldomitem_p.h"
+#include <QtCore/qstringlist.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -34,7 +35,7 @@ namespace QQmlJS {
 namespace Dom {
 
 void createDom(MutableDomItem qmlFile, DomCreationOptions options = None);
-
+QStringList resourceFilesFromBuildFolders(const QStringList &buildFolders);
 }
 }; // namespace QQmlJS
 

src/qmldom/qqmldomastcreator_p.h
@@ -447,7 +447,8 @@ public:
 class QQmlDomAstCreatorWithQQmlJSScope : public AST::Visitor
 {
 public:
-    QQmlDomAstCreatorWithQQmlJSScope(MutableDomItem &qmlFile, QQmlJSLogger *logger);
+    QQmlDomAstCreatorWithQQmlJSScope(MutableDomItem &qmlFile, QQmlJSLogger *logger,
+                                     QQmlJSResourceFileMapper *mapper);
 
 #define X(name)                       \
     bool visit(AST::name *) override; \

src/qmldom/qqmldomexternalitems.cpp
@@ -63,7 +63,7 @@ ErrorGroups QmldirFile::myParsingErrors()
     return res;
 }
 
-std::shared_ptr<QmldirFile> QmldirFile::fromPathAndCode(QString path, QString code)
+std::shared_ptr<QmldirFile> QmldirFile::fromPathAndCode(const QString &path, const QString &code)
 {
     QString canonicalFilePath = QFileInfo(path).canonicalFilePath();
 

src/qmldom/qqmldomexternalitems_p.h
@@ -162,7 +162,7 @@ public:
     }
     QmldirFile(const QmldirFile &o) = default;
 
-    static std::shared_ptr<QmldirFile> fromPathAndCode(QString path, QString code);
+    static std::shared_ptr<QmldirFile> fromPathAndCode(const QString &path, const QString &code);
 
     std::shared_ptr<QmldirFile> makeCopy(DomItem &self) const
     {

commit 675aea369c0906600fedc7ab8164dfeb05924fdc

QQmlDomAstCreator: do not call resourceFilesFromBuildFolders twice
Its seems that d0fcb75aab1b5a91260fb378d761257e1f2e4787 introduced a bug
where the resourceFilesFromBuildFolders was called twice during Dom
creation: the resources files were not found in the second call and the
Dom was not able to find the qmldir, qmltypes, etc.

The problem was that the current test only tests if there are linting
errors, but currently, the Dom created by the linting module is not the
Dom used by all other modules. Therefore, extend the tests to test this
second Dom by asking for completions of the type of the build/source
folder. Also change the test to be a data test.

Making the linting module of qmlls reuse the Dom that the other modules
also uses is tracked in QTBUG-119126.

Task-number: QTBUG-119126
Change-Id: I3a8db0f854512323b61b4763e8a2d84d407f46d8
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

src/qmldom/qqmldomastcreator.cpp
@@ -2341,8 +2341,7 @@ void createDom(MutableDomItem &&qmlFile, DomCreationOptions options)
             if (auto environmentPtr = qmlFile.environment().ownerAs<DomEnvironment>()) {
                 const QStringList resourceFiles =
                         resourceFilesFromBuildFolders(environmentPtr->loadPaths());
-                mapper = std::make_shared<QQmlJSResourceFileMapper>(
-                        resourceFilesFromBuildFolders(resourceFiles));
+                mapper = std::make_shared<QQmlJSResourceFileMapper>(resourceFiles);
             }
             auto importer =
                     std::make_shared<QQmlJSImporter>(importPathsFrom(qmlFile), mapper.get(), true);

tests/auto/qmlls/modules/tst_qmlls_modules.cpp
@@ -1221,44 +1221,44 @@ void tst_qmlls_modules::rangeFormatting()
     QTRY_VERIFY_WITH_TIMEOUT(*didFinish, 10000);
 }
 
-void tst_qmlls_modules::qmldirImportsFromBuild()
+enum AddBuildDirOption : bool { AddBuildDir, DoNotAddBuildDir };
+
+void tst_qmlls_modules::qmldirImports_data()
 {
-    const QString filePath = u"completions/fromBuildDir.qml"_s;
-    const auto uri = openFile(filePath);
-    QVERIFY(uri);
+    QTest::addColumn<QString>("filePath");
+    QTest::addColumn<AddBuildDirOption>("addBuildDirectory");
+    QTest::addColumn<int>("line");
+    QTest::addColumn<int>("character");
+    QTest::addColumn<QString>("expectedCompletion");
 
-    Notifications::AddBuildDirsParams bDirs;
-    UriToBuildDirs ub;
-    ub.baseUri = *uri;
-    ub.buildDirs.append(testFile("buildDir").toUtf8());
-    bDirs.buildDirsToSet.append(ub);
-    m_protocol->typedRpc()->sendNotification(QByteArray(Notifications::AddBuildDirsMethod), bDirs);
-
-    bool diagnosticOk = false;
-    m_protocol->registerPublishDiagnosticsNotificationHandler(
-            [&diagnosticOk, &uri](const QByteArray &, const PublishDiagnosticsParams &p) {
-                if (p.uri != *uri)
-                    return;
-
-                if constexpr (enable_debug_output) {
-                    for (const auto &x : p.diagnostics) {
-                        qDebug() << x.message;
-                    }
-                }
-                QCOMPARE(p.diagnostics.size(), 0);
-                diagnosticOk = true;
-            });
-
-    QTRY_VERIFY_WITH_TIMEOUT(diagnosticOk, 5000);
+    QTest::addRow("fromBuildFolder")
+            << u"completions/fromBuildDir.qml"_s << AddBuildDir << 3 << 1 << u"BuildDirType"_s;
+    QTest::addRow("fromSourceFolder")
+            << u"sourceDir/Main.qml"_s << DoNotAddBuildDir << 3 << 1 << u"Button"_s;
 }
 
-void tst_qmlls_modules::qmldirImportsFromSource()
+void tst_qmlls_modules::qmldirImports()
 {
-    const QString filePath = u"sourceDir/Main.qml"_s;
+    QFETCH(QString, filePath);
+    QFETCH(AddBuildDirOption, addBuildDirectory);
+    QFETCH(int, line);
+    QFETCH(int, character);
+    QFETCH(QString, expectedCompletion);
+
     const auto uri = openFile(filePath);
     QVERIFY(uri);
 
+    if (addBuildDirectory == AddBuildDir) {
+        Notifications::AddBuildDirsParams bDirs;
+        UriToBuildDirs ub;
+        ub.baseUri = *uri;
+        ub.buildDirs.append(testFile("buildDir").toUtf8());
+        bDirs.buildDirsToSet.append(ub);
+        m_protocol->typedRpc()->sendNotification(QByteArray(Notifications::AddBuildDirsMethod), bDirs);
+    }
+
     bool diagnosticOk = false;
+    bool completionOk = false;
     m_protocol->registerPublishDiagnosticsNotificationHandler(
             [&diagnosticOk, &uri](const QByteArray &, const PublishDiagnosticsParams &p) {
                 if (p.uri != *uri)
@@ -1273,7 +1273,25 @@ void tst_qmlls_modules::qmldirImportsFromSource()
                 diagnosticOk = true;
             });
 
-    QTRY_VERIFY_WITH_TIMEOUT(diagnosticOk, 5000);
+    // Currently, the Dom is created twice in qmlls: once for the linting and once for all other
+    // features. Therefore, also test that this second dom also uses the right resource files.
+    CompletionParams cParams;
+    cParams.position.line = line - 1; // LSP is 0 based
+    cParams.position.character = character - 1; // LSP is 0 based
+    cParams.textDocument.uri = *uri;
+
+    m_protocol->requestCompletion(cParams, [&completionOk, &expectedCompletion](auto res) {
+        const QList<CompletionItem> *cItems = std::get_if<QList<CompletionItem>>(&res);
+
+        QSet<QString> labels;
+        for (const CompletionItem &c : *cItems) {
+            labels << c.label;
+        }
+        QVERIFY(labels.contains(expectedCompletion));
+        completionOk = true;
+    });
+
+    QTRY_VERIFY_WITH_TIMEOUT(diagnosticOk && completionOk, 5000);
 }
 
 void tst_qmlls_modules::quickFixes_data()

tests/auto/qmlls/modules/tst_qmlls_modules.h
@@ -58,8 +58,8 @@ private slots:
     void linting();
     void rangeFormatting_data();
     void rangeFormatting();
-    void qmldirImportsFromBuild();
-    void qmldirImportsFromSource();
+    void qmldirImports_data();
+    void qmldirImports();
     void quickFixes_data();
     void quickFixes();
 