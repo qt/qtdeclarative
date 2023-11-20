qt/qtdeclarative

commit ddab485bd2b841c2135d10ff70ce912a9db927d0

Use lambdas for repeating code
This shouldn't change any functionality, add no test.

Change-Id: Ibf37be1d1e57b12c244de1656010c0eb9e1bb0fe
Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>

src/qmldom/qqmldomastcreator_p.h
@@ -483,6 +483,24 @@ private:
     using RequiresCustomIteration = IsInList<U, AST::PatternElementList, AST::PatternPropertyList,
                                              AST::FormalParameterList>;
 
+    enum VisitorKind : bool { DomCreator, ScopeCreator };
+    /*! \internal
+        \brief Holds the information to reactivate a visitor
+        This struct tracks a visitor during its inactive phases
+        and holds the information needed to reactivate the visitor.
+    */
+    struct InactiveVisitorMarker
+    {
+        qsizetype count;
+        AST::Node::Kind nodeKind;
+        VisitorKind inactiveVisitorKind;
+
+        VisitorKind stillActiveVisitorKind() const
+        {
+            return inactiveVisitorKind == DomCreator ? ScopeCreator : DomCreator;
+        }
+    };
+
     template<typename T>
     void customListIteration(T *t)
     {
@@ -501,12 +519,37 @@ private:
         }
     }
 
+    static void initMarkerForActiveVisitor(std::optional<InactiveVisitorMarker> &inactiveVisitorMarker,
+                               AST::Node::Kind nodeKind, bool continueForDom)
+    {
+        inactiveVisitorMarker.emplace();
+        inactiveVisitorMarker->inactiveVisitorKind = continueForDom ? ScopeCreator : DomCreator;
+        inactiveVisitorMarker->count = 1;
+        inactiveVisitorMarker->nodeKind = nodeKind;
+    };
+
+    template<typename T>
+    bool performListIterationIfRequired(T *t)
+    {
+        if constexpr (RequiresCustomIteration<T>::value) {
+            customListIteration(t);
+            return false;
+        }
+        Q_UNUSED(t);
+        return true;
+    }
+
     template<typename T>
     bool visitT(T *t)
     {
-        if (m_inactiveVisitorMarker && m_inactiveVisitorMarker->nodeKind == t->kind) {
-            m_inactiveVisitorMarker->count += 1;
-        }
+        const auto handleVisitResult = [this, t](const bool continueVisit) {
+            if (m_inactiveVisitorMarker && m_inactiveVisitorMarker->nodeKind == t->kind)
+                m_inactiveVisitorMarker->count += 1;
+
+            if (continueVisit)
+                return performListIterationIfRequired(t);
+            return continueVisit;
+        };
 
         // first case: no marker, both can visit
         if (!m_inactiveVisitorMarker) {
@@ -515,52 +558,22 @@ private:
             if (!continueForDom && !continueForScope)
                 return false;
             else if (continueForDom ^ continueForScope) {
-                m_inactiveVisitorMarker.emplace();
-                m_inactiveVisitorMarker->inactiveVisitorKind = continueForDom ? ScopeCreator : DomCreator;
-                m_inactiveVisitorMarker->count = 1;
-                m_inactiveVisitorMarker->nodeKind = AST::Node::Kind(t->kind);
-
-                if constexpr (RequiresCustomIteration<T>::value) {
-                    customListIteration(t);
-                    return false;
-                } else {
-                    return true;
-                }
+                initMarkerForActiveVisitor(m_inactiveVisitorMarker, AST::Node::Kind(t->kind),
+                                           continueForDom);
+                return performListIterationIfRequired(t);
             } else {
                 Q_ASSERT(continueForDom && continueForScope);
-
-                if constexpr (RequiresCustomIteration<T>::value) {
-                    customListIteration(t);
-                    return false;
-                } else {
-                    return true;
-                }
+                return performListIterationIfRequired(t);
             }
             Q_UNREACHABLE();
         }
 
         // second case: a marker, just one visit
         switch (m_inactiveVisitorMarker->stillActiveVisitorKind()) {
-        case DomCreator: {
-            const bool continueForDom = m_domCreator.visit(t);
-            if (continueForDom) {
-                if constexpr (RequiresCustomIteration<T>::value) {
-                    customListIteration(t);
-                    return false;
-                }
-            }
-            return continueForDom;
-        }
-        case ScopeCreator: {
-            const bool continueForScope = m_scopeCreator.visit(t);
-            if (continueForScope) {
-                if constexpr (RequiresCustomIteration<T>::value) {
-                    customListIteration(t);
-                    return false;
-                }
-            }
-            return continueForScope;
-        }
+        case DomCreator:
+            return handleVisitResult(m_domCreator.visit(t));
+        case ScopeCreator:
+            return handleVisitResult(m_scopeCreator.visit(t));
         };
         Q_UNREACHABLE();
     }
@@ -568,22 +581,19 @@ private:
     template<typename T>
     void endVisitT(T *t)
     {
-        if (m_inactiveVisitorMarker && m_inactiveVisitorMarker->nodeKind == t->kind) {
-            m_inactiveVisitorMarker->count -= 1;
-            if (m_inactiveVisitorMarker->count == 0)
-                m_inactiveVisitorMarker.reset();
-        }
-
         if (m_inactiveVisitorMarker) {
+            if (m_inactiveVisitorMarker->nodeKind == t->kind) {
+                m_inactiveVisitorMarker->count -= 1;
+                if (m_inactiveVisitorMarker->count == 0)
+                    m_inactiveVisitorMarker.reset();
+            }
             switch (m_inactiveVisitorMarker->stillActiveVisitorKind()) {
-            case DomCreator: {
+            case DomCreator:
                 m_domCreator.endVisit(t);
                 return;
-            }
-            case ScopeCreator: {
+            case ScopeCreator:
                 m_scopeCreator.endVisit(t);
                 return;
-            }
             };
             Q_UNREACHABLE();
         }
@@ -601,23 +611,6 @@ private:
     QQmlJSImportVisitor m_scopeCreator;
     QQmlDomAstCreator m_domCreator;
 
-    enum VisitorKind : bool { DomCreator, ScopeCreator };
-    /*! \internal
-        \brief Holds the information to reactivate a visitor
-        This struct tracks a visitor during its inactive phases
-        and holds the information needed to reactivate the visitor.
-    */
-    struct InactiveVisitorMarker
-    {
-        qsizetype count;
-        AST::Node::Kind nodeKind;
-        VisitorKind inactiveVisitorKind;
-
-        VisitorKind stillActiveVisitorKind() const
-        {
-            return inactiveVisitorKind == DomCreator ? ScopeCreator : DomCreator;
-        }
-    };
     std::optional<InactiveVisitorMarker> m_inactiveVisitorMarker;
     bool m_enableScriptExpressions = false;
 };

commit 9ddb947c7c1b44dbc3f130a4312f541e1b5df155

qmlls: check optional before accessing it
Commit ddab485bd2b841c2135d10ff70ce912a9db927d0 introduced a crash on
windows by removing a check whether the m_marker in qqmldomastcreator
was set or not. Add the qml file that crashed qmlls as test.

Change-Id: Ie33aacb4d70dbb228ac8aab308fee986bad87680
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/qmldom/qqmldomastcreator_p.h
@@ -580,12 +580,12 @@ private:
     template<typename T>
     void endVisitT(T *t)
     {
+        if (m_inactiveVisitorMarker && m_inactiveVisitorMarker->nodeKind == t->kind) {
+            m_inactiveVisitorMarker->count -= 1;
+            if (m_inactiveVisitorMarker->count == 0)
+                m_inactiveVisitorMarker.reset();
+        }
         if (m_inactiveVisitorMarker) {
-            if (m_inactiveVisitorMarker->nodeKind == t->kind) {
-                m_inactiveVisitorMarker->count -= 1;
-                if (m_inactiveVisitorMarker->count == 0)
-                    m_inactiveVisitorMarker.reset();
-            }
             switch (m_inactiveVisitorMarker->stillActiveVisitorKind()) {
             case DomCreator:
                 m_domCreator.endVisit(t);

tests/auto/qmldom/domdata/domitem/inactiveVisitorMarkerCrash.qml
@@ -1,0 +1,13 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+import QtQuick
+
+Window {
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+
+    HelloWorld { myP: 55; myPPP: 55 }
+}

tests/auto/qmldom/domitem/tst_qmldomitem.h
@@ -2692,6 +2692,22 @@ private slots:
                  sourceLocationToQCborValue(fileLocations.foundTree->info().regions[ColonTokenRegion]));
     }
 
+    // add qml files here that should not crash the dom construction
+    void crashes_data()
+    {
+        QTest::addColumn<QString>("filePath");
+
+        QTest::addRow("inactiveVisitorMarkerCrash")
+                << baseDir + u"/inactiveVisitorMarkerCrash.qml"_s;
+    }
+    void crashes()
+    {
+        QFETCH(QString, filePath);
+
+        const DomItem rootQmlObject = rootQmlObjectFromFile(filePath, qmltypeDirs);
+        QVERIFY(rootQmlObject);
+    }
+
 private:
     QString baseDir;
     QStringList qmltypeDirs;