1a8911a13d47ef6a1e903ff414d6a12605cfda30
Ulf Hermann <ulf.hermann@qt.io>
2023-11-21 17:44:26 +0100

    QmlCompiler: Don't generate invalid code for argument conversion
    We need an additional pair of parentheses here.
    
    Pick-to: 6.6 6.5 6.2
    Fixes: QTBUG-119165
    Change-Id: I0d8e810ebb5baad35e2cc1bc5c6581d1ba180dc8
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen.cpp
+++ tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen.cpp
@@ -4273,6 +4273,7 @@ void tst_QmlCppCodegen::urlString()
     QCOMPARE(qvariant_cast<QUrl>(rootObject->property("c")), QUrl(u"http://dddddd.com"_s));
     QCOMPARE(qvariant_cast<QUrl>(rootObject->property("d")), QUrl(u"http://aaaaaa.com"_s));
     QCOMPARE(qvariant_cast<QUrl>(rootObject->property("e")), QUrl(u"http://a112233.de"_s));
+    QCOMPARE(rootObject->objectName(), QLatin1String("http://dddddd.com"));
 }
 
 void tst_QmlCppCodegen::valueTypeBehavior()
