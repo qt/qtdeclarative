qt/qtdeclarative

commit fd7459a60c3b396984c948f244ec0996fba56837

SignalSpy: manually disconnect from target signal when we're destroyed
The SignalSpy is potentially destroyed before the target object, and
since only the sender (target) being destroyed destroys a connection in
QML, and not the receiver (SignalSpy), we need to manually disconnect.

When QTBUG-118166 is implemented, we can remove this.

Task-number: QTBUG-118163
Pick-to: 6.2 6.5 6.6
Change-Id: I76aaffba114a6db5b5aab6a2ff58541aa3b2e025
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/qmltest/SignalSpy.qml
@@ -45,6 +45,16 @@ Item {
     id: spy
     visible: false
 
+    Component.onDestruction: {
+        // We are potentially destroyed before the target object,
+        // and since only the sender (target) being destroyed destroys a connection
+        // in QML, and not the receiver (us/"spy"), we need to manually disconnect.
+        // When QTBUG-118166 is implemented, we can remove this.
+        let signalFunc = target ? target[signalName] : null
+        if (signalFunc)
+            signalFunc.disconnect(spy.qtest_activated)
+    }
+
     TestUtil {
         id: util
     }

commit f8b6cdf63400103a6ca53c19bb845577e94b41be

SignalSpy: fix potential issues on disconnecting a signal
In 2e7d4ecdc59942b484159ca827f5d5dbc8787a1b and
b19ebe1d23e1f2fd334cef4ec2731ab5cc69dbd7 we added support for cases
where a signal and a property name of an object are the same, like
"pressed" for MouseArea or "opened" for Popup.

But this was missed in fd7459a60c3b396984c948f244ec0996fba56837,
which caused warnings like "SignalSpy.qml:55: TypeError: Property
'disconnect' of object true is not a function" in various tests.

So, fix this and avoid the code duplication as much as possible.

Task-number: QTBUG-118163
Pick-to: 6.6 6.5 6.2
Change-Id: Ic6dc3ffeaf470e40cd14a0800f32212a0c1defa9
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/qmltest/SignalSpy.qml
@@ -50,7 +50,7 @@ Item {
         // and since only the sender (target) being destroyed destroys a connection
         // in QML, and not the receiver (us/"spy"), we need to manually disconnect.
         // When QTBUG-118166 is implemented, we can remove this.
-        let signalFunc = target ? target[signalName] : null
+        let signalFunc = target ? qttest_signalFunc(target, signalName) : null
         if (signalFunc)
             signalFunc.disconnect(spy.qtest_activated)
     }
@@ -201,8 +201,7 @@ Item {
         qtest_reentrancy_guard = true;
 
         if (qtest_prevTarget != null) {
-            var prevHandlerName = qtest_signalHandlerName(qtest_prevSignalName)
-            var prevFunc = qtest_prevTarget[prevHandlerName]
+            let prevFunc = qttest_signalFunc(qtest_prevTarget, qtest_prevSignalName)
             if (prevFunc)
                 prevFunc.disconnect(spy.qtest_activated)
             qtest_prevTarget = null
@@ -210,22 +209,16 @@ Item {
         }
         if (target != null && signalName != "") {
             // Look for the signal name in the object
-            var func = target[signalName]
-            if (typeof func !== "function") {
-                // If it is not a function, try looking for signal handler
-                // i.e. (onSignal) this is needed for cases where there is a property
-                // and a signal with the same name, e.g. Mousearea.pressed
-                func = target[qtest_signalHandlerName(signalName)]
-            }
-            if (func === undefined) {
-                spy.qtest_valid = false
-                console.log("Signal '" + signalName + "' not found")
-            } else {
+            let func = qttest_signalFunc(target, signalName)
+            if (func) {
                 qtest_prevTarget = target
                 qtest_prevSignalName = signalName
                 func.connect(spy.qtest_activated)
                 spy.qtest_valid = true
                 spy.qtest_signalArguments = []
+            } else {
+                spy.qtest_valid = false
+                console.log("Signal '" + signalName + "' not found")
             }
         } else {
             spy.qtest_valid = false
@@ -243,5 +236,17 @@ Item {
     /*! \internal */
     function qtest_signalHandlerName(sn) {
         return util.signalHandlerName(sn)
+    }
+
+    /*! \internal */
+    function qttest_signalFunc(_target, _signalName) {
+        let signalFunc = _target[_signalName]
+        if (typeof signalFunc !== "function") {
+            // If it is not a function, try looking for signal handler
+            // i.e. (onSignal) this is needed for cases where there is a property
+            // and a signal with the same name, e.g. Mousearea.pressed
+            signalFunc = _target[qtest_signalHandlerName(_signalName)]
+        }
+        return signalFunc
     }
 }

tests/auto/qmltest/selftests/tst_signalspy.qml
@@ -1,0 +1,53 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+import QtQuick
+import QtTest
+
+TestCase {
+    id: testCase
+    name: "SignalSpy"
+
+    Component {
+        id: itemWithAmbigiousSignalNameOnChild
+        Item {
+            property Item child: Item {
+                property bool opened: true
+                signal opened()
+            }
+        }
+    }
+
+    Component {
+        id: signalSpy
+        SignalSpy {}
+    }
+
+    function init() {
+        failOnWarning(/.?/)
+    }
+
+    function test_ambigiousSignalName() {
+        let control = createTemporaryObject(itemWithAmbigiousSignalNameOnChild, testCase)
+        verify(control)
+
+        let openedSpy = signalSpy.createObject(control, {target: control.child, signalName: "opened"})
+        verify(openedSpy)
+        compare(openedSpy.target, control.child)
+        compare(openedSpy.signalName, "opened")
+        verify(openedSpy.valid)
+
+        // reset the target, i.e. disconnect the spy
+        openedSpy.target = null
+        compare(openedSpy.target, null)
+        compare(openedSpy.signalName, "opened")
+        verify(!openedSpy.valid)
+
+        // connect again to check that it will be disconnected
+        // on SignalSpy destruction with no issues/warnings/etc.
+        openedSpy.target = control.child
+        compare(openedSpy.target, control.child)
+        compare(openedSpy.signalName, "opened")
+        verify(openedSpy.valid)
+    }
+}