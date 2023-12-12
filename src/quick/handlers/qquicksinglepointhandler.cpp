07e0138fa5ade030f5b157b6e75c9e083910a017
Shawn Rutledge <shawn.rutledge@qt.io>
2023-12-12 13:22:31 -0700

    QQuickSinglePointHandler: check isSinglePointEvent before cast
    QQuickSinglePointHandler is not only for QSinglePointEvent, but rather
    it's the intermediate base class of any handler that only knows how to
    handle one QEventPoint, regardless which device it came from.
    The naked cast was quite wrong for touch events, such that the
    result returned from buttons() could come from reinterpreted
    data of some kind.
    
    Amends a97759a336c597327cb82eebc9f45c793aec32c9 (and then
    79cde77f23358adbe57ab8ce08730d2de5bb1288). QQuickPointerEvent
    had buttons(), but QPointerEvent doesn't; so the cast became necessary,
    but should have been done safely.
    
    f2ba3bd9792500b4d3fcfd23b03098a32641ef4f began checking the buttons
    here. In case of a touch event, buttons() always returned NoButton
    (regardless whether the touchpoint was being pressed or released);
    so in Qt 5, QQuickSinglePointHandler::handlePointerEventImpl() in fact
    caused the ungrab and reset() to occur when a touchpoint was released.
    We continue doing that: if the event is not a QSinglePointEvent,
    buttons() are irrelevant, and we deactivate the handler when handling
    the release of the chosen point (as remembered by pointInfo).
    But for QSinglePointEvents, checking buttons() is necessary to keep
    QTBUG-66360 fixed.
    
    Task-number: QTBUG-66360
    Task-number: QTBUG-83980
    Fixes: QTBUG-117387
    Pick-to: 6.6 6.5 6.2
    Change-Id: I1bc7a72629f5840e660635f2228bf2c0abb7d2bd
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    (cherry picked from commit 7b8bea13e7c0b80ffc307cc8ccb94d73a6743544)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/handlers/qquicksinglepointhandler.cpp
+++ src/quick/handlers/qquicksinglepointhandler.cpp
@@ -117,11 +117,16 @@ void QQuickSinglePointHandler::handlePointerEventImpl(QPointerEvent *event)
 
 void QQuickSinglePointHandler::handleEventPoint(QPointerEvent *event, QEventPoint &point)
 {
-    if (point.state() != QEventPoint::Released)
-        return;
+    if (point.state() == QEventPoint::Released) {
+        // If it's a mouse or tablet event, with buttons,
+        // do not deactivate unless all acceptable buttons are released.
+        if (event->isSinglePointEvent()) {
+            const Qt::MouseButtons releasedButtons = static_cast<QSinglePointEvent *>(event)->buttons();
+            if ((releasedButtons & acceptedButtons()) != Qt::NoButton)
+                return;
+        }
 
-    const Qt::MouseButtons releasedButtons = static_cast<QSinglePointEvent *>(event)->buttons();
-    if ((releasedButtons & acceptedButtons()) == Qt::NoButton) {
+        // Deactivate this handler on release
         setExclusiveGrab(event, point, false);
         d_func()->reset();
     }
