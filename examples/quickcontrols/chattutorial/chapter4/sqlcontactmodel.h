c11436857fe2ee6951c97d2d58a9227120c7884c
Mitch Curtis <mitch.curtis@qt.io>
2023-12-18 13:21:39 +0800

    Improve chattutorial example
    - Use modern QML type registration.
    - Fix qmllint warnings.
    - Tidy up code.
    - Update copyright year.
    
    Fixes: QTBUG-119986
    Change-Id: Ibb47c929a14cd0e786acb7c7496e6cce34f624df
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- examples/quickcontrols/chattutorial/chapter4/sqlcontactmodel.h
+++ examples/quickcontrols/chattutorial/chapter4/sqlcontactmodel.h
@@ -1,13 +1,17 @@
-// Copyright (C) 2017 The Qt Company Ltd.
+// Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #ifndef SQLCONTACTMODEL_H
 #define SQLCONTACTMODEL_H
 
+#include <QQmlEngine>
 #include <QSqlQueryModel>
 
 class SqlContactModel : public QSqlQueryModel
 {
+    Q_OBJECT
+    QML_ELEMENT
+
 public:
     SqlContactModel(QObject *parent = nullptr);
 };
