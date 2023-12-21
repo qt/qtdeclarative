be481412cc704c1272833712ffe7df5ce8156775
Ulf Hermann <ulf.hermann@qt.io>
2023-12-21 15:06:28 +0100

    QmlCompiler: Allow primitive conversion if output is stored in var
    We can always convertStored() if the contained type has been fixed.
    
    Pick-to: 6.7
    Fixes: QTBUG-120322
    Change-Id: I7d834fa32a12503341c863c095d578ca6e838531
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen.cpp
+++ tests/auto/qml/qmlcppcodegen/tst_qmlcppcodegen.cpp
@@ -71,6 +71,7 @@ private slots:
     void conversionDecrement();
     void conversionInDeadCode();
     void conversions();
+    void convertPrimitiveToVar();
     void convertQJSPrimitiveValueToIntegral();
     void convertToOriginalReadAcumulatorForUnaryOperators();
     void cppValueTypeList();
@@ -1305,6 +1306,19 @@ void tst_QmlCppCodegen::conversions()
     QVERIFY(object->metaObject()->invokeMethod(object.data(), "retUndefined",
                                                Q_RETURN_ARG(QVariant, undef)));
     QVERIFY(!undef.isValid());
+}
+
+void tst_QmlCppCodegen::convertPrimitiveToVar()
+{
+    QQmlEngine engine;
+    QQmlComponent c(&engine, QUrl(u"qrc:/qt/qml/TestTypes/convertPrimitiveToVar.qml"_s));
+
+    QVERIFY2(c.isReady(), qPrintable(c.errorString()));
+
+    QScopedPointer<QObject> o(c.create());
+    QVERIFY(!o.isNull());
+
+    QCOMPARE(o->property("offsetValue").toInt(), 41);
 }
 
 void tst_QmlCppCodegen::convertQJSPrimitiveValueToIntegral()
