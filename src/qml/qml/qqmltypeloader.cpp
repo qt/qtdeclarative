9fc2067add335198a67ba97c61d62f278812b898
Morten Sørvig <morten.sorvig@qt.io>
2024-01-09 10:05:30 +0100

    no-thread: don't block the main thread
    We can't block the main thread if the type loading
    happens on the main thread - similar to the existing
    case where we avoid blocking the type loader thread.
    
    This effectively treats all urls as async urls here,
    despite what QQmlFile::isSynchronous() might say.
    
    Fixes: QTBUG-120620
    Change-Id: I69c6c829d8c5a7950f457efaab0bb631a42554b5
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Lorn Potter <lorn.potter@gmail.com>
    (cherry picked from commit 0a3290f2288695a2b05ae3824c18d01abe4195e0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/qml/qqmltypeloader.cpp
+++ src/qml/qml/qqmltypeloader.cpp
@@ -971,6 +971,7 @@ QQmlRefPointer<QQmlTypeData> QQmlTypeLoader::getType(const QUrl &unNormalizedUrl
         // this was started Asynchronous, but we need to force Synchronous
         // completion now (if at all possible with this type of URL).
 
+#if QT_CONFIG(thread)
         if (!m_thread->isThisThread()) {
             // this only works when called directly from the UI thread, but not
             // when recursively called on the QML thread via resolveTypes()
@@ -979,6 +980,7 @@ QQmlRefPointer<QQmlTypeData> QQmlTypeLoader::getType(const QUrl &unNormalizedUrl
                 m_thread->waitForNextMessage();
             }
         }
+#endif
     }
 
     return typeData;
