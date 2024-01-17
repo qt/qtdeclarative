c37b7612c5592cd76e1991e5a9fb650cfbebe867
Olivier De Cannière <olivier.decanniere@qt.io>
2024-01-17 17:41:29 +0100

    QQmlEngine: Set translationsDirectory in loadFromModule
    In the process, loadFromModule had to be split in two parts. The type
    resolution has to happen so that the translationDirectory can be set and
    the translations have to be loaded before loading the actual module.
    
    Fixes: QTBUG-116589
    Change-Id: Ife7999f418ba35bfb0eed9050198e5a886fa74ae
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 986b4d155bd39836b1aa3c3da332ef07d2a949fd)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 8206c3d86c27327e31bcefaef6a6606e207c9b18)
    (cherry picked from commit 8e4a2a888b3bb9be65503646768cef5a0a32129e)

---
+++ tests/auto/qml/qqmlapplicationengine/loadFromModuleTranslationsQmlType/i18n/qml_fr.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../Main.qml" line="5"/>
+        <source>Hello</source>
+        <translation>Salut</translation>
+    </message>
+</context>
+</TS>
