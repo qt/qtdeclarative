e6811c930e2a3a4ea4e0f04f3191d0fad7ed3b73
Robert Griebl <robert.griebl@qt.io>
2024-01-18 02:01:34 +0100

    Workaround for crash on derefWindow on application shutdown
    Ideally there shouldn't be nullptrs in the list. This fix will however
    prevent crashes, until we find out why.
    
    Fixes: QTBUG-116426
    Pick-to: 6.6.2 6.5
    Change-Id: I2c85ad73832ab4d8b9992b4d8e661758fae85ac7
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 5d79b207c7a47ac96a32c9aff3527e052364b209)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit fe0b983fc5261d5ef90ae56f35623e37513bfc50)

--- src/quick/items/qquickitem.cpp
+++ src/quick/items/qquickitem.cpp
@@ -3045,8 +3045,8 @@ void QQuickItemPrivate::derefWindow()
     paintNode = nullptr;
 
     for (int ii = 0; ii < childItems.size(); ++ii) {
-        QQuickItem *child = childItems.at(ii);
-        QQuickItemPrivate::get(child)->derefWindow();
+        if (QQuickItem *child = childItems.at(ii))
+            QQuickItemPrivate::get(child)->derefWindow();
     }
 
     dirty(Window);
