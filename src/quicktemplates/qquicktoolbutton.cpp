12c74de1d07f6165361dcb57e916015a8e60c6ec
Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
2024-02-14 09:56:03 +0100

    Update default size policy for quick tool button
    The tool button can have default size policy as 'Fixed' similar to
    widgets. This patch updates the same.
    
    Amends patch 2451ff25a531a2427825726f4b5f015fb01d2663
    
    Task-number: QTBUG-117597
    Change-Id: Ic0fb4851c3f4a3111de26a9a6905d3d87051793a
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>
    (cherry picked from commit 864623cc90450c1207d437609fa34120f9f27e15)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit cf52f8653944175f5b856e4ad0939168054d126b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quicktemplates/qquicktoolbutton.cpp
+++ src/quicktemplates/qquicktoolbutton.cpp
@@ -36,6 +36,8 @@ class Q_QUICKTEMPLATES2_PRIVATE_EXPORT QQuickToolPrivate : public QQuickButtonPr
 {
     Q_DECLARE_PUBLIC(QQuickToolButton)
 
+    QQuickToolPrivate() { setSizePolicy(QLayoutPolicy::Fixed, QLayoutPolicy::Fixed); }
+
 public:
     QPalette defaultPalette() const override { return QQuickTheme::palette(QQuickTheme::ToolBar); }
 };
