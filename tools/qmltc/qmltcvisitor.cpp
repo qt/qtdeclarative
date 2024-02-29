2bf1da16c9edba827ed1791f535c92e9b616c18c
Nicolas Fella <nicolas.fella@kdab.com>
2024-02-29 15:11:11 +0100

    QmltcVisitor: Add constinit to list of C++ keywords
    It's the only one missing based on
    https://en.cppreference.com/w/cpp/keyword
    
    Pick-to: 6.7
    Change-Id: Ic0efa463494e240343b598407f080eaaac294152
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- tools/qmltc/qmltcvisitor.cpp
+++ tools/qmltc/qmltcvisitor.cpp
@@ -690,6 +690,7 @@ void QmltcVisitor::checkNamesAndTypes(const QQmlJSScope::ConstPtr &type)
         u"const_cast"_s,
         u"consteval"_s,
         u"constexpr"_s,
+        u"constinit"_s,
         u"continue"_s,
         u"decltype"_s,
         u"default"_s,
