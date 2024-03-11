6e853af5b97d71d0cffa978a2bf502083c222348
Tim Jenssen <tim.jenssen@qt.io>
2024-03-11 12:50:24 +0100

    fix crash in qquickspinbox.cpp
    With a custom style, the validator might not be set
    initially or at all.
    
    Task-number: QTBUG-123160
    Pick-to: 6.7 6.7.0 6.6 6.6.3
    Change-Id: Idd4b801e75737fa2cfac112d06f9f7ad04278c26
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/quicktemplates/qquickspinbox.cpp
+++ src/quicktemplates/qquickspinbox.cpp
@@ -244,7 +244,8 @@ void QQuickSpinBoxPrivate::contentItemTextChanged()
         return;
     QString text = inputTextItem->text();
 #if QT_CONFIG(validator)
-    validator->fixup(text);
+    if (validator)
+        validator->fixup(text);
 #endif
 
     if (live) {
