qt/qtdeclarative

commit f03a9839b689a3a1810846bb8ec08e02a9bf23b5

ListView: position delegate items at zero on the inactive axis
FxListItemSG::setPosition() only cares about a single axis, which is
based on the current orientation. If the orientation is Vertical, then
the y axis will be used with positioning the delegate item, and if the
orientation is Horizontal, then the x axis will be used instead.

This would cause problems when the ListView is changing its orientation.
Because if the ListView's orientation suddently changes from Vertical to
Horizontal, then all the delegate items will keep their old y value,
which FxListItemSG::setPosition() won't do anything about since the new
"active" axis is now the x axis.

This patch fixes the issue by resetting the value for the "inactive"
axis back to zero, in pointForPosition() which is used by
FxListItemSG::setPosition().

The problem wasn't noticeable when using a normal QQmlDelegateModel,
since the delegate items would be created lazily when they needed to
appear inside the viewport, at which point they'd be given the correct x
and y values. (The position is first (0, 0), and then layoutVisibleItems
will updated one of those values based on the orientation).
But if the user decides to use a QQmlObjectModel based model instead,
then the delegate items would all be created immediately with a
persistent lifetime, which would never have their position reset as a
natural result of the item view delegate lifecycle mechanism.

Fixes: QTBUG-115696
Pick-to: 6.6 6.5
Change-Id: I5377aeb556be2f536794f489b8232c5b9675ab78
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

src/quick/items/qquicklistview.cpp
@@ -325,21 +325,21 @@ private:
             if (view->verticalLayoutDirection() == QQuickItemView::BottomToTop) {
                 if (section())
                     pos += section()->height();
-                return QPointF(itemX(), -itemHeight() - pos);
+                return QPointF(0, -itemHeight() - pos);
             } else {
                 if (section())
                     pos += section()->height();
-                return QPointF(itemX(), pos);
+                return QPointF(0, pos);
             }
         } else {
             if (view->effectiveLayoutDirection() == Qt::RightToLeft) {
                 if (section())
                     pos += section()->width();
-                return QPointF(-itemWidth() - pos, itemY());
+                return QPointF(-itemWidth() - pos, 0);
             } else {
                 if (section())
                     pos += section()->width();
-                return QPointF(pos, itemY());
+                return QPointF(pos, 0);
             }
         }
     }

tests/auto/quick/qquicklistview2/tst_qquicklistview2.cpp
@@ -60,6 +60,9 @@ private slots:
     void fetchMore_data();
     void fetchMore();
 
+    void changingOrientationResetsPreviousAxisValues_data();
+    void changingOrientationResetsPreviousAxisValues();
+
 private:
     void flickWithTouch(QQuickWindow *window, const QPoint &from, const QPoint &to);
     QScopedPointer<QPointingDevice> touchDevice = QScopedPointer<QPointingDevice>(QTest::createTouchDevice());
@@ -1116,6 +1119,40 @@ void tst_QQuickListView2::fetchMore() // QTBUG-95107
         QCOMPARE_GE(model.m_lines, listView->count()); // fetchMore() was called
     }
 }
+
+void tst_QQuickListView2::changingOrientationResetsPreviousAxisValues_data()
+{
+    QTest::addColumn<QByteArray>("sourceFile");
+    QTest::newRow("ObjectModel") << QByteArray("changingOrientationWithObjectModel.qml");
+    QTest::newRow("ListModel") << QByteArray("changingOrientationWithListModel.qml");
+}
+
+void tst_QQuickListView2::changingOrientationResetsPreviousAxisValues() // QTBUG-115696
+{
+    QFETCH(QByteArray, sourceFile);
+
+    QQuickView window;
+    QVERIFY(QQuickTest::showView(window, testFileUrl(QString::fromLatin1(sourceFile))));
+    auto *listView = qobject_cast<QQuickListView *>(window.rootObject());
+    QVERIFY(listView);
+
+    // Starts of with vertical orientation. X should be 0 for all delegates, but not Y.
+    QVERIFY(listView->property("isXReset").toBool());
+    QVERIFY(!listView->property("isYReset").toBool());
+
+    listView->setOrientation(QQuickListView::Orientation::Horizontal);
+
+    // Y should be 0 for all delegates, but not X.
+    QVERIFY(!listView->property("isXReset").toBool());
+    QVERIFY(listView->property("isYReset").toBool());
+
+    listView->setOrientation(QQuickListView::Orientation::Vertical);
+
+    // X should be 0 for all delegates, but not Y.
+    QVERIFY(listView->property("isXReset").toBool());
+    QVERIFY(!listView->property("isYReset").toBool());
+}
+
 QTEST_MAIN(tst_QQuickListView2)
 
 #include "tst_qquicklistview2.moc"

tests/auto/quick/qquicklistview2/data/changingOrientationWithListModel.qml
@@ -1,0 +1,47 @@
+import QtQuick
+
+ListView {
+    id: root
+
+    function allDelegates(valueSelector) {
+        let sum = 0;
+        for (let i = 0; i < root.count; i++)
+            sum += valueSelector(root.itemAtIndex(i));
+	return sum;
+    }
+
+    readonly property bool isXReset: allDelegates(function(item) { return item?.x ?? 0; }) === 0
+    readonly property bool isYReset: allDelegates(function(item) { return item?.y ?? 0; }) === 0
+
+    width: 500
+    height: 500
+    delegate: Rectangle {
+        width: root.width
+        height: root.height
+        color: c
+    }
+    model: ListModel {
+        ListElement {
+            c: "red"
+        }
+        ListElement {
+            c: "green"
+        }
+        ListElement {
+            c: "blue"
+        }
+        ListElement {
+            c: "cyan"
+        }
+        ListElement {
+            c: "magenta"
+        }
+        ListElement {
+            c: "teal"
+        }
+    }
+    clip: true
+    orientation: ListView.Vertical
+    snapMode: ListView.SnapOneItem
+    highlightRangeMode: ListView.StrictlyEnforceRange
+}

tests/auto/quick/qquicklistview2/data/changingOrientationWithObjectModel.qml
@@ -1,0 +1,54 @@
+import QtQuick
+
+ListView {
+    id: root
+
+    readonly property bool isXReset: red.x === 0 && green.x === 0 && blue.x === 0 && cyan.x === 0 && magenta.x === 0 && teal.x === 0
+
+    readonly property bool isYReset: red.y === 0 && green.y === 0 && blue.y === 0 && cyan.y === 0 && magenta.y === 0 && teal.y === 0
+
+    width: 500
+    height: 500
+    model: ObjectModel {
+          Rectangle {
+            id: red
+            width: root.width
+            height: root.height
+            color: "red"
+        }
+        Rectangle {
+	    id: green
+            width: root.width
+            height: root.height
+            color: "green"
+        }
+        Rectangle {
+	    id: blue
+            width: root.width
+            height: root.height
+            color: "blue"
+        }
+        Rectangle {
+	    id: cyan
+            width: root.width
+            height: root.height
+            color: "cyan"
+        }
+        Rectangle {
+	    id: magenta
+            width: root.width
+            height: root.height
+            color: "magenta"
+        }
+        Rectangle {
+	    id: teal
+            width: root.width
+            height: root.height
+            color: "teal"
+        }
+    }
+    clip: true
+    orientation: ListView.Vertical
+    snapMode: ListView.SnapOneItem
+    highlightRangeMode: ListView.StrictlyEnforceRange
+}

commit 27b40410613ad0ca753b7020527d03345c5aa8cf

ListView: allow binding on x and y in footer and header delegates
This is generally a bad idea, but it's technically a regression that was
introduced in f03a9839b689a3a1810846bb8ec08e02a9bf23b5.

The reason why it's not recommended to bind directly on x and y in
delegates, is because the delegate item positions are determined by the
item views layout, which are in conflict with the positional bindings.

However, it should be possible to partially support positional bindings
in the header and footer delegates, in ListView.
The reason for using the word "partially", is because a ListView with
horizontal orientation, should always place the header first (with x
typically being 0). But the y axis, can in theory be set directly, since
it won't cause overlap with the remaining delegate items.

Same idea for vertical ListViews. Except that the y axis become the
"primary" axis, when the orientation changes to vertical, meaning that
the x axis will then be safe to modify.

The model-view-delegate doc page is also updated, to discourage people
from binding directly on x and y, in delegates that are meant to be used
for item views.

Fixes: QTBUG-122665
Pick-to:  6.7
Change-Id: I78363e159f14827a28dba7c72d1ecef45b0c6d2a
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/quick/doc/src/concepts/modelviewsdata/modelview.qdoc
@@ -117,6 +117,14 @@ To visualize data, bind the view's \c model property to a model and the
     \snippet qml/listview.qml delegate
     \image listview-setup.png
 
+    \section2 Positioning of View Delegates
+
+    The type of view will determine how the items are positioned. \l {ListView}
+    will position the items in a straight line, depending on the \l {ListView::}{orientation},
+    while a \l {GridView} can lay them out in a 2 dimentional grid. It's \b {not} recommended
+    to bind directly on \l {Item::x}{x} and \l {Item::y}{y}, since the view's layouting
+    behavior will always take precedence over any positional binding.
+
     \section2 Accessing Views and Models from Delegates
 
     The list view to which the delegate is bound is accessible from the delegate

src/quick/items/qquicklistview.cpp
@@ -289,7 +289,8 @@ public:
                     : itemX() + itemWidth());
         }
     }
-    void setPosition(qreal pos, bool immediate = false) {
+
+    void setPosition(qreal pos, bool immediate = false, bool resetInactiveAxis = true) {
         // position the section immediately even if there is a transition
         if (section()) {
             if (view->orientation() == QQuickListView::Vertical) {
@@ -304,8 +305,9 @@ public:
                     section()->setX(pos);
             }
         }
-        moveTo(pointForPosition(pos), immediate);
+        moveTo(pointForPosition(pos, resetInactiveAxis), immediate);
     }
+
     void setSize(qreal size) {
         if (view->orientation() == QQuickListView::Vertical)
             item->setHeight(size);
@@ -320,26 +322,26 @@ public:
     QQuickListView *view;
 
 private:
-    QPointF pointForPosition(qreal pos) const {
+    QPointF pointForPosition(qreal pos, bool resetInactiveAxis) const {
         if (view->orientation() == QQuickListView::Vertical) {
             if (view->verticalLayoutDirection() == QQuickItemView::BottomToTop) {
                 if (section())
                     pos += section()->height();
-                return QPointF(0, -itemHeight() - pos);
+                return QPointF(resetInactiveAxis ? 0 : itemX(), -itemHeight() - pos);
             } else {
                 if (section())
                     pos += section()->height();
-                return QPointF(0, pos);
+                return QPointF(resetInactiveAxis ? 0 : itemX(), pos);
             }
         } else {
             if (view->effectiveLayoutDirection() == Qt::RightToLeft) {
                 if (section())
                     pos += section()->width();
-                return QPointF(-itemWidth() - pos, 0);
+                return QPointF(-itemWidth() - pos, resetInactiveAxis ? 0 : itemY());
             } else {
                 if (section())
                     pos += section()->width();
-                return QPointF(pos, 0);
+                return QPointF(pos, resetInactiveAxis ? 0 : itemY());
             }
         }
     }
@@ -1469,26 +1471,26 @@ void QQuickListViewPrivate::updateFooter()
 
     FxListItemSG *listItem = static_cast<FxListItemSG*>(footer);
     if (footerPositioning == QQuickListView::OverlayFooter) {
-        listItem->setPosition(isContentFlowReversed() ? -position() - footerSize() : position() + size() - footerSize());
+        listItem->setPosition(isContentFlowReversed() ? -position() - footerSize() : position() + size() - footerSize(), false, false);
     } else if (visibleItems.size()) {
         if (footerPositioning == QQuickListView::PullBackFooter) {
             qreal viewPos = isContentFlowReversed() ? -position() : position() + size();
             // using qBound() would throw an assert here, because max < min is a valid case
             // here, if the list's delegates do not fill the whole view
             qreal clampedPos = qMax(originPosition() - footerSize() + size(), qMin(listItem->position(), lastPosition()));
-            listItem->setPosition(qBound(viewPos - footerSize(), clampedPos, viewPos));
+            listItem->setPosition(qBound(viewPos - footerSize(), clampedPos, viewPos), false, false);
         } else {
             qreal endPos = lastPosition();
             if (findLastVisibleIndex() == model->count()-1) {
-                listItem->setPosition(endPos);
+                listItem->setPosition(endPos, false, false);
             } else {
                 qreal visiblePos = position() + q->height();
                 if (endPos <= visiblePos || listItem->position() < endPos)
-                    listItem->setPosition(endPos);
+                    listItem->setPosition(endPos, false, false);
             }
         }
     } else {
-        listItem->setPosition(visiblePos);
+        listItem->setPosition(visiblePos, false, false);
     }
 
     if (created)
@@ -1535,7 +1537,7 @@ void QQuickListViewPrivate::updateHeader()
 
     FxListItemSG *listItem = static_cast<FxListItemSG*>(header);
     if (headerPositioning == QQuickListView::OverlayHeader) {
-        listItem->setPosition(isContentFlowReversed() ? -position() - size() : position());
+        listItem->setPosition(isContentFlowReversed() ? -position() - size() : position(), false, false);
     } else if (visibleItems.size()) {
         const bool fixingUp = (orient == QQuickListView::Vertical ? vData : hData).fixingUp;
         if (headerPositioning == QQuickListView::PullBackHeader) {
@@ -1547,18 +1549,18 @@ void QQuickListViewPrivate::updateHeader()
             // using qBound() would throw an assert here, because max < min is a valid case
             // here, if the list's delegates do not fill the whole view
             qreal clampedPos = qMax(originPosition() - headerSize(), qMin(headerPosition, lastPosition() - size()));
-            listItem->setPosition(qBound(viewPos - headerSize(), clampedPos, viewPos));
+            listItem->setPosition(qBound(viewPos - headerSize(), clampedPos, viewPos), false, false);
         } else {
             qreal startPos = originPosition();
             if (visibleIndex == 0) {
-                listItem->setPosition(startPos - headerSize());
+                listItem->setPosition(startPos - headerSize(), false, false);
             } else {
                 if (position() <= startPos || listItem->position() > startPos - headerSize())
-                    listItem->setPosition(startPos - headerSize());
+                    listItem->setPosition(startPos - headerSize(), false, false);
             }
         }
     } else {
-        listItem->setPosition(-headerSize());
+        listItem->setPosition(-headerSize(), false, false);
     }
 
     if (created)

tests/auto/quick/qquicklistview2/data/bindOnHeaderAndFooterXPosition.qml
@@ -1,0 +1,29 @@
+import QtQuick
+
+ListView {
+    width: 200
+    height: 300
+    spacing: 20
+    orientation: ListView.Vertical
+
+    header: Rectangle {
+        x: (ListView.view.width - width) / 2
+        color: 'tomato'
+        width: 50
+        height: 50
+    }
+
+    footer: Rectangle {
+        x: (ListView.view.width - width) / 2
+        color: 'lime'
+        width: 50
+        height: 50
+    }
+
+    model: 3
+    delegate: Text {
+        text: 'Foobar'
+        horizontalAlignment: Text.AlignHCenter
+        width: ListView.view.width
+    }
+}

tests/auto/quick/qquicklistview2/data/bindOnHeaderAndFooterYPosition.qml
@@ -1,0 +1,29 @@
+import QtQuick
+
+ListView {
+    width: 300
+    height: 200
+    spacing: 20
+    orientation: ListView.Horizontal
+
+    header: Rectangle {
+        y: (ListView.view.height - height) / 2
+        color: 'tomato'
+        width: 50
+        height: 50
+    }
+
+    footer: Rectangle {
+        y: (ListView.view.height - height) / 2
+        color: 'lime'
+        width: 50
+        height: 50
+    }
+
+    model: 3
+    delegate: Text {
+        text: 'Foobar'
+        verticalAlignment: Text.AlignVCenter
+        height: ListView.view.height
+    }
+}

tests/auto/quick/qquicklistview2/tst_qquicklistview2.cpp
@@ -62,6 +62,8 @@ private slots:
 
     void changingOrientationResetsPreviousAxisValues_data();
     void changingOrientationResetsPreviousAxisValues();
+    void bindingDirectlyOnPositionInHeaderAndFooterDelegates_data();
+    void bindingDirectlyOnPositionInHeaderAndFooterDelegates();
 
     void clearObjectListModel();
 
@@ -1153,6 +1155,38 @@ void tst_QQuickListView2::changingOrientationResetsPreviousAxisValues() // QTBUG
     // X should be 0 for all delegates, but not Y.
     QVERIFY(listView->property("isXReset").toBool());
     QVERIFY(!listView->property("isYReset").toBool());
+}
+
+void tst_QQuickListView2::bindingDirectlyOnPositionInHeaderAndFooterDelegates_data()
+{
+    QTest::addColumn<QByteArray>("sourceFile");
+    QTest::addColumn<qreal(QQuickItem::*)()const>("pos");
+    QTest::addColumn<qreal(QQuickItem::*)()const>("size");
+    QTest::newRow("XPosition") << QByteArray("bindOnHeaderAndFooterXPosition.qml") << &QQuickItem::x << &QQuickItem::width;
+    QTest::newRow("YPosition") << QByteArray("bindOnHeaderAndFooterYPosition.qml") << &QQuickItem::y << &QQuickItem::height;
+}
+void tst_QQuickListView2::bindingDirectlyOnPositionInHeaderAndFooterDelegates()
+{
+
+    typedef qreal (QQuickItem::*position_func_t)() const;
+    QFETCH(QByteArray, sourceFile);
+    QFETCH(position_func_t, pos);
+    QFETCH(position_func_t, size);
+
+    QQuickView window;
+    QVERIFY(QQuickTest::showView(window, testFileUrl(QString::fromLatin1(sourceFile))));
+    auto *listView = qobject_cast<QQuickListView *>(window.rootObject());
+    QVERIFY(listView);
+
+    const qreal widthOrHeight = (listView->*size)();
+
+    QCOMPARE((listView->headerItem()->*pos)(), (widthOrHeight - 50) / 2);
+    QCOMPARE((listView->footerItem()->*pos)(), (widthOrHeight - 50) / 2);
+
+    // Verify that the "regular" delegate items, don't honor x and y bindings.
+    // This should only be allowed for header and footer delegates.
+    for (int i = 0; i < listView->count(); ++i)
+        QCOMPARE((listView->itemAtIndex(i)->*pos)(), 0);
 }
 
 void tst_QQuickListView2::clearObjectListModel()