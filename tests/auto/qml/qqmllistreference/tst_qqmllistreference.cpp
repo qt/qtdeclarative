fff113b4a3259ed558e97bc66ecdacd7455dc490
Ulf Hermann <ulf.hermann@qt.io>
2024-03-21 11:20:32 +0100

    QQmlListWrapper: Don't use QQmlListReference for virtualGetLength()
    We cannot use QQmlListReference on detached list properties. Also, it's
    wasteful.
    
    Pick-to: 6.7
    Fixes: QTBUG-118076
    Change-Id: I754f19ed8c3f6eae1b3613f6866f3cf792cee8cf
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>

--- tests/auto/qml/qqmllistreference/tst_qqmllistreference.cpp
+++ tests/auto/qml/qqmllistreference/tst_qqmllistreference.cpp
@@ -60,6 +60,7 @@ private slots:
     void jsArrayMethodsWithParams();
     void listIgnoresNull_data() { modeData(); }
     void listIgnoresNull();
+    void consoleLogSyntheticList();
 };
 
 class TestType : public QObject
@@ -1080,6 +1081,17 @@ void tst_qqmllistreference::listIgnoresNull()
     }
     QScopedPointer<QObject> object( component.create() );
     QVERIFY(object != nullptr);
+}
+
+void tst_qqmllistreference::consoleLogSyntheticList()
+{
+    QQmlEngine engine;
+    QQmlComponent component(&engine, testFileUrl("consoleLogSyntheticList.qml"));
+    QVERIFY2(component.isReady(), qPrintable(component.errorString()));
+    QTest::ignoreMessage(
+            QtDebugMsg, QRegularExpression("\\[QObject_QML_[0-9]+\\(0x[0-9a-f]+\\)\\]"));
+    QScopedPointer<QObject> object(component.create());
+    QVERIFY(!object.isNull());
 }
 
 QTEST_MAIN(tst_qqmllistreference)
