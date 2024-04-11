59a3bfa1c2d559e71fe690e58c63b85243f5cc56
Kaj Grönholm <kaj.gronholm@qt.io>
2024-04-11 12:06:13 +0300

    Make sure PathView index isn't negative when filling the model
    Fixes: QTBUG-46487
    Change-Id: Iba2a875756f0c06b86bb0a0aaf66eac329b2382b
    Reviewed-by: Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit b31bfde4bd840a94cdd4b2223e8f7c345850bd66)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 61247583dc0273d954370d6f585de44c6f73fff1)
    Reviewed-by: Kaj Grönholm <kaj.gronholm@qt.io>

--- tests/auto/quick/qquickpathview/tst_qquickpathview.cpp
+++ tests/auto/quick/qquickpathview/tst_qquickpathview.cpp
@@ -133,6 +133,7 @@ private slots:
     void requiredPropertiesInDelegatePreventUnrelated();
     void touchMove();
     void mousePressAfterFlick();
+    void qtbug46487();
 
 private:
     QScopedPointer<QPointingDevice> touchDevice = QScopedPointer<QPointingDevice>(QTest::createTouchDevice());
@@ -2893,6 +2894,68 @@ void tst_QQuickPathView::mousePressAfterFlick() // QTBUG-115121
     QTRY_VERIFY(!pathview->isMoving());
     QCOMPARE(flickEndedSpy.size(), 1);
     QCOMPARE(pressedSpy.size(), 0);
+}
+
+class CustomModel : public QAbstractListModel
+{
+public:
+    CustomModel(QObject *parent = 0) : QAbstractListModel(parent) {
+        m_values << 0 << 1 << 2 << 3 << 4;
+    }
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const {
+        Q_UNUSED(parent);
+        return m_values.count();
+    }
+    QVariant data(const QModelIndex &index, int role) const {
+        if (index.row() < 0 || m_values.count() <= index.row())
+            return QVariant();
+
+        return m_values[index.row()];
+    }
+
+    Q_INVOKABLE void shrink() {
+        beginResetModel();
+        m_values.takeLast();
+        m_values.takeLast();
+        endResetModel();
+    }
+
+private:
+    QList<int> m_values;
+};
+
+void tst_QQuickPathView::qtbug46487()
+{
+    QScopedPointer<QQuickView> window(createView());
+
+    CustomModel* model = new CustomModel;
+    QQmlContext *ctxt = window->rootContext();
+    ctxt->setContextProperty("customModel", model);
+
+    window->setSource(testFileUrl("qtbug46487.qml"));
+    window->show();
+    qApp->processEvents();
+
+    QQuickPathView *pathview = qobject_cast<QQuickPathView*>(window->rootObject());
+    QVERIFY(pathview);
+
+    QTest::qWait(500);
+
+    // Should create just pathItemCount amount and not destroy any
+    QCOMPARE(pathview->count(), 5);
+    QCOMPARE(pathview->property("delegatesCreated").toInt(), 5);
+    QCOMPARE(pathview->property("delegatesDestroyed").toInt(), 0);
+
+    // Resets the model and removes 2 items.
+    model->shrink();
+    QTest::qWait(500);
+
+    // Should destroy previous items (begin/endResetModel) and
+    // (re)create 3 new items.
+    QCOMPARE(pathview->count(), 3);
+    QCOMPARE(pathview->property("delegatesCreated").toInt(), 5 + 3);
+    QCOMPARE(pathview->property("delegatesDestroyed").toInt(), 5);
 }
 
 QTEST_MAIN(tst_QQuickPathView)
