b6b934f830bd355359accaafef4f7e4ca0b8d4bd
Luca Di Sera <luca.disera@qt.io>
2024-04-17 12:28:48 +0200

    qmltc: Test failure for unbound required property in inline component
    `qmltc` should fail when it processes a QML file that instances an
    inline component that has some top-level required properties without
    setting them when creating the instance.
    
    For example:
    
    ```
    import QtQuick
    
    Item {
       component IC: Item { required property int i }
    
       IC {
         // i needs to be set here as it cannot be set later
       }
    }
    ```
    
    To parse a QML file, `qmltc` uses `QQmlJSImportVisitor`, which will
    produce an error on encountering such a case.
    
    `qmltc` will in turn propagate the error and fail itself, thus acting
    properly.
    
    A new test was added to keep track of the behavior and ensure that
    `qmltc` rejects a file when this erroneous form is encountered.
    
    Task-number: QTBUG-120698
    Change-Id: I1a1b7a3d55f3ba5dde7d04b014b7d6a9b53a546d
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- tests/auto/qml/qmltc_qprocess/tst_qmltc_qprocess.cpp
+++ tests/auto/qml/qmltc_qprocess/tst_qmltc_qprocess.cpp
@@ -55,6 +55,7 @@ private slots:
     void qmlBaseFromAnotherModule();
     void invalidTypeAnnotation();
     void constructFromString();
+    void unboundRequiredPropertyInInlineComponent();
 };
 
 #ifndef TST_QMLTC_QPROCESS_RESOURCES
@@ -338,6 +339,16 @@ void tst_qmltc_qprocess::constructFromString()
     QVERIFY(errors.contains(warningMessage.arg(4).arg(23).arg(u"QPointF")));
     QVERIFY(errors.contains(warningMessage.arg(5).arg(23).arg(u"QRectF")));
     QVERIFY(errors.contains(warningMessage.arg(6).arg(23).arg(u"QSizeF")));
+}
+
+void tst_qmltc_qprocess::unboundRequiredPropertyInInlineComponent()
+{
+    {
+        const auto errors = runQmltc(u"unboundRequiredPropertyInInlineComponent.qml"_s, false);
+        QVERIFY(errors.contains(
+                u"unboundRequiredPropertyInInlineComponent.qml:9:5: Component is missing required property foo from InlineComponent [required]"_s
+        ));
+    }
 }
 
 QTEST_MAIN(tst_qmltc_qprocess)
