b5d8e343c2e4dcb8498b3e4f038802c9298b29ab
Soheil Armin <soheil.armin@qt.io>
2024-05-27 02:36:01 +0300

    Android: Docs: Add Java QtQmlComponent documentation
    Change-Id: I03d3d8aa9f1088afa6cbfec410ed53ab4258f60a
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- src/quick/jar/org/qtproject/qt/android/QtQmlComponent.java
+++ src/quick/jar/org/qtproject/qt/android/QtQmlComponent.java
@@ -8,6 +8,14 @@ import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.HashSet;
 
+/**
+ * @Since 6.8
+ *
+ * The QtQmlComponent represents a QML component that can be loaded by a QtQuickView instance
+ * This abstract class should be extended to be used by a QtQuickView. It provides QtQuickView with
+ * essential information to load the QML component it represents.
+ * It also offers convenient methods for seamless interaction with the QtQuickView that loads it.
+ **/
 public abstract class QtQmlComponent
 {
     private final static String TAG = "QtQmlComponent";
@@ -16,10 +24,24 @@ public abstract class QtQmlComponent
     private QtQmlStatusChangeListener m_statusChangeListener = null;
     private HashSet<Integer> m_signalListenerIds = new HashSet<>();
 
+    /**
+     * Implement this to return the library name that this component belongs to.
+     **/
     protected abstract String getLibraryName();
+    /**
+     * Implement this to return the module name that this component belongs to.
+     **/
     protected abstract String getModuleName();
+    /**
+     * Implement this to return the qrc (Qt Resource) path of this QML component.
+     **/
     protected abstract String getFilePath();
 
+    /**
+     * Sets a StatusChangeListener to listen to status changes.
+     * <p>
+     * @param listener an instance of a StatusChangeListener interface
+     **/
     public void setStatusChangeListener(QtQmlStatusChangeListener listener)
     {
         m_statusChangeListener = listener;
@@ -28,6 +50,12 @@ public abstract class QtQmlComponent
             view.setStatusChangeListener(listener);
     }
 
+    /**
+     * Gets the QtQuickView instance that has loaded this component.
+     * <p>
+     * @return Returns an instance of QtQuickView or null if this component is not loaded by any
+     * QtQuickView.
+     **/
     protected QtQuickView getQuickView()
     {
         if (m_viewReference != null)
@@ -35,8 +63,16 @@ public abstract class QtQmlComponent
         return null;
     }
 
+    /**
+     * Checks if this is currently attached to a QtQuickView instance
+     * <p>
+     * @return Returns true if this is attached to a QtQuickView instance, otherwise, returns false.
+     **/
     protected boolean isViewAttached() { return getQuickView() != null; }
 
+    /**
+     * Attaches this to a QtQuickView instance.
+     **/
     protected void attachView(QtQuickView view)
     {
         m_viewReference = new WeakReference<>(view);
@@ -44,6 +80,10 @@ public abstract class QtQmlComponent
             view.setStatusChangeListener(m_statusChangeListener);
     }
 
+    /**
+     * Detaches this from the QtQuickView to which it has previously been attached. A call to this
+     * method will disconnect all signal listeners that have been connected before.
+     **/
     protected void detachView()
     {
         QtQuickView view = getQuickView();
@@ -58,8 +98,27 @@ public abstract class QtQmlComponent
         }
     }
 
+    /**
+     * Implement this to return more information about the QML Component.
+     * Default implementation returns an empty HashMap.
+     **/
     protected HashMap<String, Object> attributes() { return new HashMap<>(); }
 
+    /**
+     * Sets the value of an existing property on the QML component if it has already been attached
+     * and loaded by a QtQuickView instance. The supported types are
+     * {@link java.lang.Integer}, {@link java.lang.Double}, {@link java.lang.Float},
+     * {@link java.lang.Boolean} and {@link java.lang.String}. These types get converted to their
+     * corresponding QML types int, double/float, bool, and string. This function does not add
+     * properties to the QML root object if they do not exist but prints a warning.
+     * <p>
+     * @param propertyName the name of the existing QML property to set the value of
+     * @param value        the value to set the property to
+     * @see <a href="https://doc.qt.io/qt-6/qml-int.html">QML int</a>,
+     * @see <a href="https://doc.qt.io/qt-6/qml-double.html">QML double/float</a>,
+     * @see <a href="https://doc.qt.io/qt-6/qml-bool.html">QML bool</a>,
+     * @see <a href="https://doc.qt.io/qt-6/qml-string.html">QML string</a>.
+     **/
     protected void setProperty(String propertyName, Object value)
     {
         QtQuickView view = getQuickView();
@@ -70,6 +129,22 @@ public abstract class QtQmlComponent
         view.setProperty(propertyName, value);
     }
 
+    /**
+     * Gets the value of an existing property of the QML component if it has already been attached
+     * and loaded by a QtQuickView instance. The supported types are
+     * {@link java.lang.Integer}, {@link java.lang.Double}, {@link java.lang.Float},
+     * {@link java.lang.Boolean} and {@link java.lang.String}. These types get converted to their
+     * corresponding QML types int, double/float, bool and string. If the property does not
+     * exist or the status of the QML component is anything other than
+     * {@link QtQuickView#STATUS_READY STATUS_READY}, this function will return null.
+     * <p>
+     * @param propertyName the name of the existing root object property
+     * @throws ClassCastException if the returned type cannot be cast to the requested type.
+     * @see <a href="https://doc.qt.io/qt-6/qml-int.html">QML int</a>,
+     * @see <a href="https://doc.qt.io/qt-6/qml-double.html">QML double/float</a>,
+     * @see <a href="https://doc.qt.io/qt-6/qml-bool.html">QML bool</a>,
+     * @see <a href="https://doc.qt.io/qt-6/qml-string.html">QML string</a>.
+     **/
     protected <T> T getProperty(String propertyName)
     {
         QtQuickView view = getQuickView();
@@ -80,6 +155,17 @@ public abstract class QtQmlComponent
         return view.<T>getProperty(propertyName);
     }
 
+    /**
+     * Connects a SignalListener to a signal of the QML component if it has already been attached
+     * and loaded by a QtQuickView instance.
+     * <p>
+     * @param signalName the name of the root object signal
+     * @param argType    the Class type of the signal argument
+     * @param listener   an instance of the QtSignalListener interface
+     * @return a connection ID between signal and listener or the existing connection ID if there is
+     *         an existing connection between the same signal and listener. Return a negative value
+     *         if the signal does not exist on the QML root object.
+     **/
     protected <T> int connectSignalListener(String signalName, Class<T> argType,
                                             QtSignalListener<T> listener)
     {
@@ -95,6 +181,15 @@ public abstract class QtQmlComponent
         return signalListenerId;
     }
 
+    /**
+     * Disconnects a SignalListener with a given id obtained from
+     * {@link QtQuickView#connectSignalListener() connectSignalListener} call, from listening to
+     * a signal.
+     * <p>
+     * @param signalListenerId the connection id
+     * @return Returns true if the connection id is valid and has been successfully removed,
+     *         otherwise returns false.
+     **/
     public boolean disconnectSignalListener(int signalListenerId)
     {
         QtQuickView view = getQuickView();
