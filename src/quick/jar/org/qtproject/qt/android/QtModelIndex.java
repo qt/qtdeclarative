64cd7ca0c455722c5cbeef98b58a103874061fa6
Nicholas Bennett <nicholas.bennett@qt.io>
2024-05-29 13:41:58 +0300

    Docs: Document QtAbstractItemModel and QtModelIndex Java Classes
    Add, amend, and correct documentation for classes based on the C++ classes these java classes wrap.
    
    Fixes: QTBUG-125604
    Change-Id: Iaeb61628bda22940973d4e684ff46cfaba6969fa
    Reviewed-by: Soheil Armin <soheil.armin@qt.io>
    Reviewed-by: Ville Voutilainen <ville.voutilainen@qt.io>
    (cherry picked from commit 62a5d80402c0bedfc07e33cb7d59330c55a30627)
    Reviewed-by: Assam Boudjelthia - 🏖️ <assam.boudjelthia@qt.io>

--- src/quick/jar/org/qtproject/qt/android/QtModelIndex.java
+++ src/quick/jar/org/qtproject/qt/android/QtModelIndex.java
@@ -3,14 +3,53 @@
 
 package org.qtproject.qt.android;
 
+/**
+ * Represents an index in a custom item model, similar to
+ * {@link https://doc.qt.io/qt-6/https://doc.qt.io/qt-6/qmodelindex.html QModelindex}
+ * in c++.
+ */
 public class QtModelIndex
 {
+    /**
+     * Constructs a new QtModelIndex.
+     */
     public QtModelIndex() { }
+    /**
+     * Returns the column of this index.
+     *
+     * @return The column.
+     */
     public int column() { return (int)m_privateData[1]; }
+    /**
+     * Retrieves data for this index based on the specified role.
+     *
+     * @param role The role for which data is requested.
+     * @return The data object.
+     */
     public native Object data(int role);
+    /**
+     * Returns the internal ID associated with this index.
+     *
+     * @return The internal ID.
+     */
     public native long internalId();
+    /**
+     * Checks if this index is valid.
+     *
+     * @return True if the index is valid, false otherwise.
+     */
     public native boolean isValid();
+    /**
+     * Returns the parent index of this index.
+     *
+     * @return The parent index.
+     */
     public native QtModelIndex parent();
+    /**
+     * Returns the row of this index.
+     *
+     * @return The row.
+     */
     public int row() { return (int)m_privateData[0]; }
 
     private long[] m_privateData = { -1 /*row*/, -1 /*column*/, 0 /*internalId*/,
