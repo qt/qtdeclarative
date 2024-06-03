2435e16255854946cc4fd90ba5e9cb8fe442fdcf
Ivan Tkachenko <me@ratijas.tk>
2024-06-04 02:56:15 +0600

    Flickable: Don't accept ScrollEnd wheel event
    ...to fix snapping behavior of
    ListView { snapMode: SnapOneItem; delegate: ListView { ...} }
    
    It doesn't accept the event with ScrollBegin phase, so shouldn't do it
    for the ScrollEnd phase either.
    
    Accepting ScrollEnd unconditionally broke nested ListView, for example,
    where the higher-level one wouldn't snap back to the delgate and would
    get stuck in a moving state -- that is, until you click it so that the
    mouseReleaseEvent cleans it up.
    
    Fixes: QTBUG-126042
    Pick-to: 6.5
    Change-Id: I94b0a3570e4750d68501095c56f06f5cca31923f
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit c3a1a91581d1cb9517ee4ee45e139f80340248ee)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 39dfd69e79652a630abefe24ffc730f1d89deb48)

--- src/quick/items/qquickflickable.cpp
+++ src/quick/items/qquickflickable.cpp
@@ -1646,7 +1646,6 @@ void QQuickFlickable::wheelEvent(QWheelEvent *event)
         d->pressed = false;
         d->scrollingPhase = false;
         d->draggingEnding();
-        event->accept();
         returnToBounds();
         d->lastPosTime = -1;
         d->stealMouse = false;
