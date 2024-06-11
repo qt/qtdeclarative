72eeb438e1a1a2e22dca82673e03b142c438c1b0
Doris Verria <doris.verria@qt.io>
2024-06-11 17:23:57 +0200

    Make sure ToolTip's children palette is updated
    Popups are not QQuickItems so when updating the palette, we explicitly
    called QQuickPopupPrivate::updateContentPalettes for all children of
    type QQuickPopup of the application window.
    However, this didn't always work for tooltips when they were created
    via attached objects (QQuickToolTipAttached), which is a common way to
    create them. When QQuickToolTipAttached created the tooltip instance,
    the parent of it wouldn't be the parent item, but the QML engine. In
    this case we wouldn't find the toolTip in the list of children of the
    application window.
    To fix, we explicitly search for children of type QQuickToolTipAttached
    instead, whose parent is the parent item.
    
    Change-Id: I70425b731f998622c93421eed11067a36b39fd75
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>
    (cherry picked from commit 0e92b68ab2f1efdde7a54a3df4a9494eb9fc0a51)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 842f6979aaa73c151f4b5173f3c6b36c18fe6a2f)
    (cherry picked from commit 6de2d69081c7198cbb5db88076bb8b5ef1980194)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/quicktemplates/qquickapplicationwindow.cpp
+++ src/quicktemplates/qquickapplicationwindow.cpp
@@ -8,6 +8,7 @@
 #include "qquicktextarea_p.h"
 #include "qquicktextfield_p.h"
 #include "qquicktoolbar_p.h"
+#include "qquicktooltip_p.h"
 #include "qquicktabbar_p.h"
 #include "qquickdialogbuttonbox_p.h"
 #include "qquickdeferredexecute_p_p.h"
@@ -137,9 +138,15 @@ public:
         // Update regular children
         QQuickWindowPrivate::updateChildrenPalettes(parentPalette);
 
-        // And cover one special case
-        for (auto &&popup : q_func()->findChildren<QQuickPopup *>())
-            QQuickPopupPrivate::get(popup)->updateContentPalettes(parentPalette);
+        // And cover special cases
+        for (auto &&child : q_func()->findChildren<QObject *>()) {
+            if (auto *popup = qobject_cast<QQuickPopup *>(child))
+                QQuickPopupPrivate::get(popup)->updateContentPalettes(parentPalette);
+            else if (auto *toolTipAttached = qobject_cast<QQuickToolTipAttached *>(child)) {
+                if (auto *toolTip = toolTipAttached->toolTip())
+                    QQuickPopupPrivate::get(toolTip)->updateContentPalettes(parentPalette);
+            }
+        }
     }
 
     QQuickDeferredPointer<QQuickItem> background;
