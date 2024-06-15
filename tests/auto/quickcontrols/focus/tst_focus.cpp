qt/qtdeclarative

commit 644ee4d23464cb04b5162051ffa1524f006b544d

API Cleanup: Remove focusReason from QQuickItem's public API and add docs
Make changes according to API review comments:

- Remove focusReason property and relevant API from QQuickItem. Move
it to private API in QQuickItemPrivate and rename it to
lastFocusChangeReason. Defer exposing this property to
QML for now. (In Qt7 we can expose it by adding it as a signal
parameter to QQuickItem's focusChanged signal)

- Add documentation for focusPolicy C++ API

- Don't mark focusPolicy property as FINAL

Partially reverts 1ca9928856c47108000ffe5f310f2b99740f087b

Task-number: QTBUG-119952
Task-number: QTBUG-117596
Pick-to: 6.7
Change-Id: Ib4632b86c64e3221ca5e651d80d58b8c89fcf2b7
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quick/items/qquickitem.cpp
@@ -1739,6 +1739,19 @@ bool QQuickItemPrivate::setFocusIfNeeded(QEvent::Type eventType)
     return false;
 }
 
+Qt::FocusReason QQuickItemPrivate::lastFocusChangeReason() const
+{
+    return static_cast<Qt::FocusReason>(focusReason);
+}
+
+void QQuickItemPrivate::setLastFocusChangeReason(Qt::FocusReason reason)
+{
+    if (focusReason == reason)
+        return;
+
+    focusReason = reason;
+}
+
 /*!
     \class QQuickItem
     \brief The QQuickItem class provides the most basic of all visual items in \l {Qt Quick}.
@@ -4098,6 +4111,7 @@ void QQuickItem::inputMethodEvent(QInputMethodEvent *event)
   */
 void QQuickItem::focusInEvent(QFocusEvent *event)
 {
+    Q_D(QQuickItem);
 #if QT_CONFIG(accessibility)
     if (QAccessible::isActive()) {
         if (QObject *acc = QQuickAccessibleAttached::findAccessible(this)) {
@@ -4106,7 +4120,7 @@ void QQuickItem::focusInEvent(QFocusEvent *event)
         }
     }
 #endif
-    setFocusReason(event->reason());
+    d->setLastFocusChangeReason(event->reason());
 }
 
 /*!
@@ -4118,7 +4132,8 @@ void QQuickItem::focusInEvent(QFocusEvent *event)
   */
 void QQuickItem::focusOutEvent(QFocusEvent *event)
 {
-    setFocusReason(event->reason());
+    Q_D(QQuickItem);
+    d->setLastFocusChangeReason(event->reason());
 }
 
 /*!
@@ -7916,31 +7931,6 @@ QQuickItem *QQuickItem::scopedFocusItem() const
 }
 
 /*!
-    \qmlproperty enumeration QtQuick::Item::focusReason
-    \readonly
-    \since 6.7
-
-    \input item.qdocinc focus-reason
-
-    \note This property was a member of {QQuickControl} {Control} until Qt 6.7.
-*/
-Qt::FocusReason QQuickItem::focusReason() const
-{
-    Q_D(const QQuickItem);
-    return static_cast<Qt::FocusReason>(d->focusReason);
-}
-
-void QQuickItem::setFocusReason(Qt::FocusReason reason)
-{
-    Q_D(QQuickItem);
-    if (d->focusReason == reason)
-        return;
-
-    d->focusReason = reason;
-    emit focusReasonChanged();
-}
-
-/*!
     \qmlproperty enumeration QtQuick::Item::focusPolicy
     \since 6.7
 
@@ -7952,7 +7942,15 @@ void QQuickItem::setFocusReason(Qt::FocusReason reason)
     \value Qt.WheelFocus  The item accepts focus by tabbing, clicking, and using the mouse wheel.
     \value Qt.NoFocus     The item does not accept focus.
 
-    \note This property was a member of {QQuickControl} {Control} until Qt 6.7.
+    \note This property was a member of \l {QQuickControl} {Control} until Qt 6.7.
+*/
+/*!
+    \property QQuickItem::focusPolicy
+    \since 6.7
+
+    This property determines the way the item accepts focus.
+
+    \note This property was a member of \l {QQuickControl} {Control} until Qt 6.7.
 */
 Qt::FocusPolicy QQuickItem::focusPolicy() const
 {
@@ -7963,6 +7961,11 @@ Qt::FocusPolicy QQuickItem::focusPolicy() const
     return static_cast<Qt::FocusPolicy>(policy);
 }
 
+/*!
+    Sets the focus policy of this item to \a policy.
+
+    \sa focusPolicy()
+*/
 void QQuickItem::setFocusPolicy(Qt::FocusPolicy policy)
 {
     Q_D(QQuickItem);

src/quick/items/qquickitem_p.h
@@ -561,6 +561,8 @@ public:
     void updateSubFocusItem(QQuickItem *scope, bool focus);
 
     bool setFocusIfNeeded(QEvent::Type);
+    Qt::FocusReason lastFocusChangeReason() const;
+    void setLastFocusChangeReason(Qt::FocusReason reason);
 
     QTransform windowToItemTransform() const;
     QTransform itemToWindowTransform() const;

src/quicktemplates/qquickcontrol.cpp
@@ -898,7 +898,7 @@ void QQuickControlPrivate::itemFocusChanged(QQuickItem *item, Qt::FocusReason re
 {
     Q_Q(QQuickControl);
     if (item == contentItem || item == q)
-        q->setFocusReason(reason);
+        setLastFocusChangeReason(reason);
 }
 
 QQuickControl::QQuickControl(QQuickItem *parent)
@@ -1341,6 +1341,44 @@ bool QQuickControl::isMirrored() const
 {
     Q_D(const QQuickControl);
     return d->isMirrored();
+}
+
+/*!
+    \qmlproperty enumeration QtQuick.Controls::Control::focusReason
+    \readonly
+
+    This property holds the reason of the last focus change.
+
+    \note This property does not indicate whether the item has \l {Item::activeFocus}
+        {active focus}, but the reason why the item either gained or lost focus.
+
+    \value Qt.MouseFocusReason         A mouse action occurred.
+    \value Qt.TabFocusReason           The Tab key was pressed.
+    \value Qt.BacktabFocusReason       A Backtab occurred. The input for this may include the Shift or Control keys; e.g. Shift+Tab.
+    \value Qt.ActiveWindowFocusReason  The window system made this window either active or inactive.
+    \value Qt.PopupFocusReason         The application opened/closed a pop-up that grabbed/released the keyboard focus.
+    \value Qt.ShortcutFocusReason      The user typed a label's buddy shortcut
+    \value Qt.MenuBarFocusReason       The menu bar took focus.
+    \value Qt.OtherFocusReason         Another reason, usually application-specific.
+
+    \sa Item::activeFocus
+
+    \sa visualFocus
+*/
+Qt::FocusReason QQuickControl::focusReason() const
+{
+    Q_D(const QQuickControl);
+    return d->lastFocusChangeReason();
+}
+
+void QQuickControl::setFocusReason(Qt::FocusReason reason)
+{
+    Q_D(QQuickControl);
+    Qt::FocusReason oldReason = static_cast<Qt::FocusReason>(d->focusReason);
+    d->setLastFocusChangeReason(reason);
+    emit focusReasonChanged();
+    if (isKeyFocusReason(oldReason) != isKeyFocusReason(reason))
+        emit visualFocusChanged();
 }
 
 /*!

src/quicktemplates/qquicktextarea.cpp
@@ -634,6 +634,39 @@ void QQuickTextArea::setPlaceholderTextColor(const QColor &color)
     emit placeholderTextColorChanged();
 }
 
+/*!
+    \qmlproperty enumeration QtQuick.Controls::TextArea::focusReason
+
+    This property holds the reason of the last focus change.
+
+    \note This property does not indicate whether the item has \l {Item::activeFocus}
+        {active focus}, but the reason why the item either gained or lost focus.
+
+    \value Qt.MouseFocusReason         A mouse action occurred.
+    \value Qt.TabFocusReason           The Tab key was pressed.
+    \value Qt.BacktabFocusReason       A Backtab occurred. The input for this may include the Shift or Control keys; e.g. Shift+Tab.
+    \value Qt.ActiveWindowFocusReason  The window system made this window either active or inactive.
+    \value Qt.PopupFocusReason         The application opened/closed a pop-up that grabbed/released the keyboard focus.
+    \value Qt.ShortcutFocusReason      The user typed a label's buddy shortcut
+    \value Qt.MenuBarFocusReason       The menu bar took focus.
+    \value Qt.OtherFocusReason         Another reason, usually application-specific.
+
+    \note Prefer \l {Item::focusReason} to this property.
+*/
+Qt::FocusReason QQuickTextArea::focusReason() const
+{
+    Q_D(const QQuickTextArea);
+    return d->lastFocusChangeReason();
+}
+
+void QQuickTextArea::setFocusReason(Qt::FocusReason reason)
+{
+    Q_D(QQuickTextArea);
+    d->setLastFocusChangeReason(reason);
+    emit focusReasonChanged();
+}
+
+
 bool QQuickTextArea::contains(const QPointF &point) const
 {
     Q_D(const QQuickTextArea);

src/quicktemplates/qquicktextfield.cpp
@@ -521,6 +521,38 @@ void QQuickTextField::setPlaceholderTextColor(const QColor &color)
 }
 
 /*!
+    \qmlproperty enumeration QtQuick.Controls::TextField::focusReason
+
+    This property holds the reason of the last focus change.
+
+    \note This property does not indicate whether the item has \l {Item::activeFocus}
+        {active focus}, but the reason why the item either gained or lost focus.
+
+    \value Qt.MouseFocusReason         A mouse action occurred.
+    \value Qt.TabFocusReason           The Tab key was pressed.
+    \value Qt.BacktabFocusReason       A Backtab occurred. The input for this may include the Shift or Control keys; e.g. Shift+Tab.
+    \value Qt.ActiveWindowFocusReason  The window system made this window either active or inactive.
+    \value Qt.PopupFocusReason         The application opened/closed a pop-up that grabbed/released the keyboard focus.
+    \value Qt.ShortcutFocusReason      The user typed a label's buddy shortcut
+    \value Qt.MenuBarFocusReason       The menu bar took focus.
+    \value Qt.OtherFocusReason         Another reason, usually application-specific.
+
+    \note Prefer \l {Item::focusReason} to this property.
+*/
+Qt::FocusReason QQuickTextField::focusReason() const
+{
+    Q_D(const QQuickTextField);
+    return d->lastFocusChangeReason();
+}
+
+void QQuickTextField::setFocusReason(Qt::FocusReason reason)
+{
+    Q_D(QQuickTextField);
+    d->setLastFocusChangeReason(reason);
+    emit focusReasonChanged();
+}
+
+/*!
     \since QtQuick.Controls 2.1 (Qt 5.8)
     \qmlproperty bool QtQuick.Controls::TextField::hovered
     \readonly

src/quick/doc/src/includes/item.qdocinc
@@ -17,23 +17,3 @@ event, call \c event->ignore().
 
 //! [accepting-events]
 
-
-//! [focus-reason]
-
-This property holds the reason of the last focus change.
-
-\note This property does not indicate whether the item has \l {Item::activeFocus}
-      {active focus}, but the reason why the item either gained or lost focus.
-
-\value Qt.MouseFocusReason         A mouse action occurred.
-\value Qt.TabFocusReason           The Tab key was pressed.
-\value Qt.BacktabFocusReason       A Backtab occurred. The input for this may include the Shift or Control keys; e.g. Shift+Tab.
-\value Qt.ActiveWindowFocusReason  The window system made this window either active or inactive.
-\value Qt.PopupFocusReason         The application opened/closed a pop-up that grabbed/released the keyboard focus.
-\value Qt.ShortcutFocusReason      The user typed a label's buddy shortcut
-\value Qt.MenuBarFocusReason       The menu bar took focus.
-\value Qt.OtherFocusReason         Another reason, usually application-specific.
-
-\sa Item::activeFocus
-
-//! [focus-reason]

src/quick/items/qquickitem.h
@@ -102,10 +102,7 @@ class Q_QUICK_EXPORT QQuickItem : public QObject, public QQmlParserStatus
     Q_PROPERTY(bool activeFocus READ hasActiveFocus NOTIFY activeFocusChanged FINAL)
     Q_PROPERTY(bool activeFocusOnTab READ activeFocusOnTab WRITE setActiveFocusOnTab NOTIFY activeFocusOnTabChanged FINAL REVISION(2, 1))
 
-    QT6_ONLY(Q_PROPERTY(Qt::FocusPolicy focusPolicy READ focusPolicy WRITE setFocusPolicy NOTIFY focusPolicyChanged))
-    QT7_ONLY(Q_PROPERTY(Qt::FocusPolicy focusPolicy READ focusPolicy WRITE setFocusPolicy NOTIFY focusPolicyChanged FINAL))
-    QT6_ONLY(Q_PROPERTY(Qt::FocusReason focusReason READ focusReason WRITE setFocusReason NOTIFY focusReasonChanged))
-    QT7_ONLY(Q_PROPERTY(Qt::FocusReason focusReason READ focusReason NOTIFY focusReasonChanged FINAL))
+    Q_PROPERTY(Qt::FocusPolicy focusPolicy READ focusPolicy WRITE setFocusPolicy NOTIFY focusPolicyChanged)
 
     Q_PROPERTY(qreal rotation READ rotation WRITE setRotation NOTIFY rotationChanged)
     Q_PROPERTY(qreal scale READ scale WRITE setScale NOTIFY scaleChanged)
@@ -278,9 +275,6 @@ public:
     Qt::FocusPolicy focusPolicy() const;
     void setFocusPolicy(Qt::FocusPolicy policy);
 
-    Qt::FocusReason focusReason() const;
-    void setFocusReason(Qt::FocusReason reason);
-
     bool isAncestorOf(const QQuickItem *child) const;
 
     Qt::MouseButtons acceptedMouseButtons() const;
@@ -394,7 +388,6 @@ Q_SIGNALS:
     void stateChanged(const QString &);
     void focusChanged(bool);
     void activeFocusChanged(bool);
-    Q_REVISION(6, 7) void focusReasonChanged();
     Q_REVISION(6, 7) void focusPolicyChanged();
     Q_REVISION(2, 1) void activeFocusOnTabChanged(bool);
     void parentChanged(QQuickItem *);

src/quicktemplates/qquickcontrol_p.h
@@ -40,7 +40,7 @@ class Q_QUICKTEMPLATES2_EXPORT QQuickControl : public QQuickItem
     Q_PROPERTY(QLocale locale READ locale WRITE setLocale RESET resetLocale NOTIFY localeChanged FINAL)
     Q_PROPERTY(bool mirrored READ isMirrored NOTIFY mirroredChanged FINAL)
     QT6_ONLY(Q_PROPERTY(Qt::FocusPolicy focusPolicy READ focusPolicy WRITE setFocusPolicy NOTIFY focusPolicyChanged FINAL))
-    QT6_ONLY(Q_PROPERTY(Qt::FocusReason focusReason READ focusReason WRITE setFocusReason NOTIFY focusReasonChanged FINAL))
+    Q_PROPERTY(Qt::FocusReason focusReason READ focusReason WRITE setFocusReason NOTIFY focusReasonChanged FINAL)
     Q_PROPERTY(bool visualFocus READ hasVisualFocus NOTIFY visualFocusChanged FINAL)
     Q_PROPERTY(bool hovered READ isHovered NOTIFY hoveredChanged FINAL)
     Q_PROPERTY(bool hoverEnabled READ isHoverEnabled WRITE setHoverEnabled RESET resetHoverEnabled NOTIFY hoverEnabledChanged FINAL)
@@ -103,6 +103,9 @@ public:
     void resetLocale();
 
     bool isMirrored() const;
+
+    Qt::FocusReason focusReason() const;
+    void setFocusReason(Qt::FocusReason reason);
 
     bool hasVisualFocus() const;
 
@@ -168,6 +171,7 @@ Q_SIGNALS:
     void bottomPaddingChanged();
     void spacingChanged();
     void localeChanged();
+    void focusReasonChanged();
     void mirroredChanged();
     void visualFocusChanged();
     void hoveredChanged();

commit d887758bf4727d891ae05958d2c10d395c34be81

Make QQuickItemPrivate::setLastFocusChangeReason virtual
Fix a regression that 644ee4d23464cb04b5162051ffa1524f006b544d
introduced by moving focusReason to QQuickItemPrivate. The already
existing QQuickControl::focusReasonChanged signal was not being
emitted anymore when the focus would change. To fix, make
setLastFocusChangeReason virtual in QQuickItemPrivate so that the
subclasses can override and emit the relevant signals.

Fixes: QTBUG-125725
Pick-to: 6.7 6.8
Change-Id: I88d664ec4fa3cc9262ee2703d63e7e716cac68e9
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/quick/items/qquickitem.cpp
@@ -1744,12 +1744,13 @@ Qt::FocusReason QQuickItemPrivate::lastFocusChangeReason() const
     return static_cast<Qt::FocusReason>(focusReason);
 }
 
-void QQuickItemPrivate::setLastFocusChangeReason(Qt::FocusReason reason)
+bool QQuickItemPrivate::setLastFocusChangeReason(Qt::FocusReason reason)
 {
     if (focusReason == reason)
-        return;
+        return false;
 
     focusReason = reason;
+    return true;
 }
 
 /*!

src/quick/items/qquickitem_p.h
@@ -564,7 +564,7 @@ public:
 
     bool setFocusIfNeeded(QEvent::Type);
     Qt::FocusReason lastFocusChangeReason() const;
-    void setLastFocusChangeReason(Qt::FocusReason reason);
+    virtual bool setLastFocusChangeReason(Qt::FocusReason reason);
 
     QTransform windowToItemTransform() const;
     QTransform itemToWindowTransform() const;

src/quicktemplates/qquickcontrol.cpp
@@ -911,6 +911,19 @@ void QQuickControlPrivate::itemFocusChanged(QQuickItem *item, Qt::FocusReason re
         setLastFocusChangeReason(reason);
 }
 
+bool QQuickControlPrivate::setLastFocusChangeReason(Qt::FocusReason reason)
+{
+    Q_Q(QQuickControl);
+    Qt::FocusReason oldReason = static_cast<Qt::FocusReason>(focusReason);
+    const auto focusReasonChanged = QQuickItemPrivate::setLastFocusChangeReason(reason);
+    if (focusReasonChanged)
+        emit q->focusReasonChanged();
+    if (isKeyFocusReason(oldReason) != isKeyFocusReason(reason))
+        emit q->visualFocusChanged();
+
+    return focusReasonChanged;
+}
+
 QQuickControl::QQuickControl(QQuickItem *parent)
     : QQuickItem(*(new QQuickControlPrivate), parent)
 {
@@ -1384,11 +1397,7 @@ Qt::FocusReason QQuickControl::focusReason() const
 void QQuickControl::setFocusReason(Qt::FocusReason reason)
 {
     Q_D(QQuickControl);
-    Qt::FocusReason oldReason = static_cast<Qt::FocusReason>(d->focusReason);
     d->setLastFocusChangeReason(reason);
-    emit focusReasonChanged();
-    if (isKeyFocusReason(oldReason) != isKeyFocusReason(reason))
-        emit visualFocusChanged();
 }
 
 /*!
@@ -1403,7 +1412,7 @@ void QQuickControl::setFocusReason(Qt::FocusReason reason)
     \l Item::activeFocus. This ensures that key focus is only visualized when
     interacting with keys - not when interacting via touch or mouse.
 
-    \sa Item::focusReason, Item::activeFocus
+    \sa focusReason, Item::activeFocus
 */
 bool QQuickControl::hasVisualFocus() const
 {
@@ -1969,22 +1978,12 @@ QFont QQuickControl::defaultFont() const
 
 void QQuickControl::focusInEvent(QFocusEvent *event)
 {
-    Q_D(QQuickControl);
-    Qt::FocusReason oldReason = static_cast<Qt::FocusReason>(d->focusReason);
     QQuickItem::focusInEvent(event);
-    Qt::FocusReason reason = event->reason();
-    if (isKeyFocusReason(oldReason) != isKeyFocusReason(reason))
-        emit visualFocusChanged();
 }
 
 void QQuickControl::focusOutEvent(QFocusEvent *event)
 {
-    Q_D(QQuickControl);
-    Qt::FocusReason oldReason = static_cast<Qt::FocusReason>(d->focusReason);
     QQuickItem::focusOutEvent(event);
-    Qt::FocusReason reason = event->reason();
-    if (isKeyFocusReason(oldReason) != isKeyFocusReason(reason))
-        emit visualFocusChanged();
 }
 
 #if QT_CONFIG(quicktemplates2_hover)

src/quicktemplates/qquickcontrol_p_p.h
@@ -155,6 +155,8 @@ public:
     void itemDestroyed(QQuickItem *item) override;
     void itemFocusChanged(QQuickItem *item, Qt::FocusReason reason) override;
 
+    bool setLastFocusChangeReason(Qt::FocusReason) override;
+
     virtual qreal getContentWidth() const;
     virtual qreal getContentHeight() const;
 

src/quicktemplates/qquicktextarea.cpp
@@ -472,6 +472,16 @@ QPalette QQuickTextAreaPrivate::defaultPalette() const
     return QQuickTheme::palette(QQuickTheme::TextArea);
 }
 
+bool QQuickTextAreaPrivate::setLastFocusChangeReason(Qt::FocusReason reason)
+{
+    Q_Q(QQuickTextArea);
+    const auto focusReasonChanged = QQuickItemPrivate::setLastFocusChangeReason(reason);
+    if (focusReasonChanged)
+        emit q->focusReasonChanged();
+
+    return focusReasonChanged;
+}
+
 QQuickTextArea::QQuickTextArea(QQuickItem *parent)
     : QQuickTextEdit(*(new QQuickTextAreaPrivate), parent)
 {
@@ -663,7 +673,6 @@ void QQuickTextArea::setFocusReason(Qt::FocusReason reason)
 {
     Q_D(QQuickTextArea);
     d->setLastFocusChangeReason(reason);
-    emit focusReasonChanged();
 }
 
 

src/quicktemplates/qquicktextarea_p_p.h
@@ -96,6 +96,8 @@ public:
 
     QPalette defaultPalette() const override;
 
+    bool setLastFocusChangeReason(Qt::FocusReason reason) override;
+
 #if QT_CONFIG(quicktemplates2_hover)
     bool hovered = false;
     bool explicitHoverEnabled = false;

src/quicktemplates/qquicktextfield.cpp
@@ -364,6 +364,16 @@ QPalette QQuickTextFieldPrivate::defaultPalette() const
     return QQuickTheme::palette(QQuickTheme::TextField);
 }
 
+bool QQuickTextFieldPrivate::setLastFocusChangeReason(Qt::FocusReason reason)
+{
+    Q_Q(QQuickTextField);
+    const auto focusReasonChanged = QQuickItemPrivate::setLastFocusChangeReason(reason);
+    if (focusReasonChanged)
+        emit q->focusReasonChanged();
+
+    return focusReasonChanged;
+}
+
 QQuickTextField::QQuickTextField(QQuickItem *parent)
     : QQuickTextInput(*(new QQuickTextFieldPrivate), parent)
 {
@@ -549,7 +559,6 @@ void QQuickTextField::setFocusReason(Qt::FocusReason reason)
 {
     Q_D(QQuickTextField);
     d->setLastFocusChangeReason(reason);
-    emit focusReasonChanged();
 }
 
 /*!

src/quicktemplates/qquicktextfield_p_p.h
@@ -94,6 +94,8 @@ public:
 
     QPalette defaultPalette() const override;
 
+    bool setLastFocusChangeReason(Qt::FocusReason reason) override;
+
 #if QT_CONFIG(quicktemplates2_hover)
     bool hovered = false;
     bool explicitHoverEnabled = false;

tests/auto/quickcontrols/focus/tst_focus.cpp
@@ -15,6 +15,7 @@
 #include <QtQuickTestUtils/private/visualtestutils_p.h>
 #include <QtQuickTemplates2/private/qquickapplicationwindow_p.h>
 #include <QtQuickTemplates2/private/qquickcontrol_p.h>
+#include <QtQuickTemplates2/private/qquicktextfield_p.h>
 #include <QtQuickControls2/qquickstyle.h>
 #include <QtQuickControlsTestUtils/private/controlstestutils_p.h>
 
@@ -244,7 +245,7 @@ void tst_focus::reason()
     QQuickControl *customText = view.findChild<QQuickControl *>("customText");
     QQuickControl *customItem = view.findChild<QQuickControl *>("customItem");
     // not a QQuickControl subclass
-    QQuickItem *textfield = view.findChild<QQuickItem *>("textfield");
+    QQuickTextField *textfield = view.findChild<QQuickTextField *>("textfield");
 
     // helper for clicking into a control
     const auto itemCenter = [](const QQuickItem *item) -> QPoint {
@@ -273,21 +274,35 @@ void tst_focus::reason()
     }
     QCOMPARE(control->focusReason(), Qt::ActiveWindowFocusReason);
 
+    QSignalSpy controlSpy(control, SIGNAL(focusReasonChanged()));
+    QSignalSpy textfieldSpy(textfield, SIGNAL(focusReasonChanged()));
+    QSignalSpy customItemSpy(customItem, SIGNAL(focusReasonChanged()));
+    int controlSpyCount = 0, textfieldSpyCount = 0, customItemSpyCount = 0;
+    QVERIFY(!controlSpy.count());
+    QVERIFY(!textfieldSpy.count());
+    QVERIFY(!customItemSpy.count());
+
     // test setter/getter
     control->setFocus(false, Qt::MouseFocusReason);
     QCOMPARE(control->focusReason(), Qt::MouseFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
     control->setFocus(true, Qt::TabFocusReason);
     QCOMPARE(control->focusReason(), Qt::TabFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
     control->setFocus(false, Qt::BacktabFocusReason);
     QCOMPARE(control->focusReason(), Qt::BacktabFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
     control->forceActiveFocus(Qt::ShortcutFocusReason);
     QCOMPARE(control->focusReason(), Qt::ShortcutFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
     control->setFocusReason(Qt::PopupFocusReason);
     QCOMPARE(control->focusReason(), Qt::PopupFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
 
     // programmatic focus changes
     combobox->setFocus(true, Qt::OtherFocusReason);
     QCOMPARE(control->focusReason(), Qt::OtherFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
 
     QVERIFY(combobox->hasFocus());
     QVERIFY(combobox->hasActiveFocus());
@@ -324,27 +339,35 @@ void tst_focus::reason()
     QVERIFY(customItem->hasActiveFocus());
     QCOMPARE(customText->focusReason(), Qt::TabFocusReason);
     QCOMPARE(customItem->focusReason(), Qt::TabFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
     customItem->setFocusReason(Qt::NoFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
 
     QTest::keyClick(&view, Qt::Key_Tab);
     QVERIFY(textfield->hasFocus());
     QVERIFY(textfield->hasActiveFocus());
     QCOMPARE(qApp->focusObject(), textfield);
     QCOMPARE(customItem->focusReason(), Qt::TabFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
+    QCOMPARE(textfieldSpy.count(), ++textfieldSpyCount);
 
     QTest::keyClick(&view, Qt::Key_Tab);
     QVERIFY(control->hasFocus());
     QVERIFY(control->hasActiveFocus());
     QCOMPARE(control->focusReason(), Qt::TabFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
 
     // backtab -> BacktabFocusReason
     QTest::keyClick(&view, Qt::Key_Tab, Qt::ShiftModifier);
     QVERIFY(textfield->hasFocus());
     QCOMPARE(control->focusReason(), Qt::BacktabFocusReason);
+    QCOMPARE(controlSpy.count(), ++controlSpyCount);
+    QCOMPARE(textfieldSpy.count(), ++textfieldSpyCount);
 
     QTest::keyClick(&view, Qt::Key_Tab, Qt::ShiftModifier);
     QVERIFY(customItem->hasFocus());
     QCOMPARE(customItem->focusReason(), Qt::BacktabFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
 
     QTest::keyClick(&view, Qt::Key_Tab, Qt::ShiftModifier);
     QVERIFY(customText->hasFocus());
@@ -399,11 +422,15 @@ void tst_focus::reason()
     QVERIFY(customItem->hasActiveFocus());
     QCOMPARE(customText->focusReason(), Qt::MouseFocusReason);
     QCOMPARE(customItem->focusReason(), Qt::MouseFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
     customItem->setFocusReason(Qt::NoFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
 
     QTest::mouseClick(&view, Qt::LeftButton, {}, itemCenter(textfield));
     QCOMPARE(customItem->focusReason(), Qt::MouseFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
     customItem->setFocusReason(Qt::NoFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
     customText->setFocusReason(Qt::NoFocusReason);
 
 #if QT_CONFIG(wheelevent)
@@ -414,11 +441,13 @@ void tst_focus::reason()
     QGuiApplication::sendEvent(customItem, &wheelEvent);
     QVERIFY(customItem->hasActiveFocus());
     QCOMPARE(customItem->focusReason(), Qt::MouseFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
 
     // Popup opens -> PopupFocusReason
     QTest::mouseClick(&view, Qt::RightButton, {}, itemCenter(control));
     QTRY_VERIFY(!customItem->hasActiveFocus());
     QCOMPARE(customItem->focusReason(), Qt::PopupFocusReason);
+    QCOMPARE(customItemSpy.count(),++customItemSpyCount);
     QTest::keyClick(&view, Qt::Key_Escape); // close the popup
 #endif
 }