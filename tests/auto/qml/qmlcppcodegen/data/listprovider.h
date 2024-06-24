3d3ff946e3986e2ab2c28416b70b76b7988ab1da
Ulf Hermann <ulf.hermann@qt.io>
2024-06-24 09:08:47 +0200

    QmlCompiler: Don't generate invalid code for unstorable lists
    When printing a sequence type through one of the console methods, we
    have to consider the adjusted type, not only the one we intended to
    read.
    
    Amends commit 7894f271ab3714102e14744914de6d0549b9a7d8.
    
    Pick-to: 6.8
    Task-number: QTBUG-126398
    Change-Id: I4606ed2006a547bdf93ec136ebbfab10b706b917
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tests/auto/qml/qmlcppcodegen/data/listprovider.h
+++ tests/auto/qml/qmlcppcodegen/data/listprovider.h
@@ -1,13 +1,17 @@
 #ifndef QLISTPROVIDER_H
 #define QLISTPROVIDER_H
 
-#include <QObject>
-#include <QQmlEngine>
+#include <QtQml/qqmlengine.h>
+
+#include <QtCore/qobject.h>
+#include <QtCore/qjsonarray.h>
 
 class QListProvider : public QObject
 {
     Q_OBJECT
     QML_NAMED_ELEMENT(ListProvider)
+    Q_PROPERTY(QJsonArray json READ json WRITE setJson NOTIFY jsonChanged FINAL)
+    Q_PROPERTY(QStringList strings READ strings WRITE setStrings NOTIFY stringsChanged FINAL)
 
 public:
     explicit QListProvider(QObject *parent = nullptr) : QObject(parent) { }
@@ -19,6 +23,32 @@ public:
             list.append(i);
         return list;
     }
+
+    QStringList strings() const { return m_strings; }
+    void setStrings(const QStringList &strings)
+    {
+        if (strings != m_strings) {
+            m_strings = strings;
+            emit stringsChanged();
+        }
+    }
+
+    QJsonArray json() const { return m_json; }
+    void setJson(const QJsonArray &json)
+    {
+        if (json != m_json) {
+            m_json = json;
+            emit jsonChanged();
+        }
+    }
+
+signals:
+    void jsonChanged();
+    void stringsChanged();
+
+private:
+    QJsonArray m_json;
+    QStringList m_strings;
 };
 
 #endif // QLISTPROVIDER_H
