dd7d8f38b5e0f58e8edd490757069aaed862a340
Mitch Curtis <mitch.curtis@qt.io>
2024-06-25 17:13:36 +0800

    QQmlDelegateModel: fix delegates not being created in certain cases
    When calling beginResetModel from the constructor of a
    QAbstractItemModel subclass, QQmlDelegateModel has not yet connected
    to modelAboutToBeReset. endResetModel is then called later, but after
    837c2f18cd223707e7cedb213257b0158ea07146, we no longer connect to
    modelReset, so we never caught this and delegates wouldn't be visible.
    
    This patch ensures that we connect to modelReset and call
    QQmlDelegateModel::handleModelReset().
    
    Fixes: QTBUG-125053
    Pick-to: 6.5
    Change-Id: Ib9c179420053a8733b505f3d8d51c712df28a348
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 6561344dd2d1ba69abe6edec4fe340b256da9e13)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit cc3d7ef3228316e88a67292404d2cf72f66f9f1a)

--- src/qmlmodels/qqmldelegatemodel.cpp
+++ src/qmlmodels/qqmldelegatemodel.cpp
@@ -167,6 +167,7 @@ QQmlDelegateModelPrivate::QQmlDelegateModelPrivate(QQmlContext *ctxt)
     , m_transaction(false)
     , m_incubatorCleanupScheduled(false)
     , m_waitingToFetchMore(false)
+    , m_maybeResetRoleNames(false)
     , m_cacheItems(nullptr)
     , m_items(nullptr)
     , m_persistedItems(nullptr)
@@ -371,6 +372,8 @@ void QQmlDelegateModelPrivate::connectToAbstractItemModel()
     QObject::connect(aim, &QAbstractItemModel::modelAboutToBeReset, q, &QQmlDelegateModel::_q_modelAboutToBeReset);
     qmlobject_connect(aim, QAbstractItemModel, SIGNAL(layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
                       q, QQmlDelegateModel, SLOT(_q_layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
+    QObject::connect(aim, &QAbstractItemModel::modelReset, q, &QQmlDelegateModel::handleModelReset);
+    QObject::connect(aim, &QAbstractItemModel::layoutChanged, q, &QQmlDelegateModel::_q_layoutChanged);
 }
 
 void QQmlDelegateModelPrivate::disconnectFromAbstractItemModel()
@@ -400,6 +403,8 @@ void QQmlDelegateModelPrivate::disconnectFromAbstractItemModel()
     QObject::disconnect(aim, &QAbstractItemModel::modelAboutToBeReset, q, &QQmlDelegateModel::_q_modelAboutToBeReset);
     QObject::disconnect(aim, SIGNAL(layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)),
                         q, SLOT(_q_layoutChanged(QList<QPersistentModelIndex>,QAbstractItemModel::LayoutChangeHint)));
+    QObject::disconnect(aim, &QAbstractItemModel::modelReset, q, &QQmlDelegateModel::handleModelReset);
+    QObject::disconnect(aim, &QAbstractItemModel::layoutChanged, q, &QQmlDelegateModel::_q_layoutChanged);
 }
 
 void QQmlDelegateModel::setModel(const QVariant &model)
@@ -1909,25 +1914,28 @@ void QQmlDelegateModel::_q_modelAboutToBeReset()
     Q_D(QQmlDelegateModel);
     if (!d->m_adaptorModel.adaptsAim())
         return;
-    auto aim = d->m_adaptorModel.aim();
-    auto oldRoleNames = aim->roleNames();
-    // this relies on the fact that modelAboutToBeReset must be followed
-    // by a modelReset signal before any further modelAboutToBeReset can occur
-    QObject::connect(aim, &QAbstractItemModel::modelReset, this, [this, d, oldRoleNames, aim](){
-        if (!d->m_adaptorModel.adaptsAim() || d->m_adaptorModel.aim() != aim)
-            return;
-        if (oldRoleNames == aim->roleNames()) {
-            // if the rolenames stayed the same (most common case), then we don't have
-            // to throw away all the setup that we did
-            handleModelReset();
-        } else {
-            // If they did change, we give up and just start from scratch via setMode
-            setModel(QVariant::fromValue(model()));
-            // but we still have to call handleModelReset, otherwise views will
-            // not refresh
-            handleModelReset();
-        }
-    }, Qt::SingleShotConnection);
+
+    /*
+        roleNames are generally guaranteed to be stable (given that QAIM has no
+        change signal for them), except that resetting the model is allowed to
+        invalidate them (QTBUG-32132). DelegateModel must take this into account by
+        snapshotting the current roleNames before the model is reset.
+        Afterwards, if we detect that roleNames has changed, we throw the
+        current model set up away and rebuild everything from scratch – it is
+        unlikely that a more efficient implementation would be worth it.
+
+        If we detect no changes, we simply use the existing logic to handle the
+        model reset.
+
+        This (role name resetting) logic relies on the fact that
+        modelAboutToBeReset must be followed by a modelReset signal before any
+        further modelAboutToBeReset can occur. However, it's possible for user
+        code to begin the reset before connectToAbstractItemModel is called
+        (QTBUG-125053), in which case we don't attempt to reset the role names.
+    */
+    Q_ASSERT(!d->m_maybeResetRoleNames);
+    d->m_maybeResetRoleNames = true;
+    d->m_roleNamesBeforeReset = d->m_adaptorModel.aim()->roleNames();
 }
 
 void QQmlDelegateModel::handleModelReset()
@@ -1937,6 +1945,23 @@ void QQmlDelegateModel::handleModelReset()
         return;
 
     int oldCount = d->m_count;
+
+    if (d->m_maybeResetRoleNames) {
+        auto aim = d->m_adaptorModel.aim();
+        if (!d->m_adaptorModel.adaptsAim() || d->m_adaptorModel.aim() != aim)
+            return;
+
+        // If the role names stayed the same (most common case), then we don't have
+        // to throw away all the setup that we did.
+        // If they did change, we give up and just start from scratch via setModel.
+        // We do this before handling the reset to ensure that views refresh.
+        if (aim->roleNames() != d->m_roleNamesBeforeReset)
+            setModel(QVariant::fromValue(model()));
+
+        d->m_maybeResetRoleNames = false;
+        d->m_roleNamesBeforeReset.clear();
+    }
+
     d->m_adaptorModel.rootIndex = QModelIndex();
 
     if (d->m_complete) {
