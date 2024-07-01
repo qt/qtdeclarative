dca47a523beb84f6c97a593e4cfd2131b5d99ac3
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2024-07-01 13:37:29 +0200

    Detect dirty child window stacking order without recursing all items
    Recursing all items was causing a high CPU utilization in scenarios
    with deep and wide item trees (>10k items).
    
    We can limit the search for a dirty item with ChildrenStackingChanged
    to the list of dirty items.
    
    For a synthetic benchmark this reduces the iteration time fro 0.60 ms
    to 0.13 ms.
    
    Fixes: QTBUG-126673
    Change-Id: Icb05a25365589268d5fb64f6f33179adb3406886
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit a84a2f737da1058f987882ad2c1cde6691cb0d05)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 783ce84b8ff7858f4013bde9e5e84fcb15923550)

--- tests/auto/quick/qquickwindowcontainer/tst_qquickwindowcontainer.cpp
+++ tests/auto/quick/qquickwindowcontainer/tst_qquickwindowcontainer.cpp
@@ -14,6 +14,7 @@
 #include <QtQml/qqmlapplicationengine.h>
 
 #include <QtQuick/qquickwindow.h>
+#include <QtQuick/private/qquickwindow_p.h>
 #include <QtQuick/private/qquickwindowcontainer_p.h>
 
 class tst_QQuickWindowContainer : public QQmlDataTest
@@ -36,6 +37,8 @@ private slots:
     void windowLifetimeFollowsContainer();
     void deferredVisibilityWithoutWindow();
     void windowComponent();
+
+    void updateStackingOrderPerformance();
 
 private:
     std::unique_ptr<QQmlApplicationEngine> m_engine;
@@ -185,6 +188,21 @@ void tst_QQuickWindowContainer::windowComponent()
     QCOMPARE(qobject_cast<QQuickWindow *>(window_window_parent)->transientParent(), nullptr);
     QCOMPARE(qobject_cast<QQuickWindow *>(window_item_parent)->parent(), root);
     QCOMPARE(qobject_cast<QQuickWindow *>(window_window_parent)->parent(), windowParent);
+}
+
+void tst_QQuickWindowContainer::updateStackingOrderPerformance()
+{
+    QQuickWindow quickWindow;
+    for (int i = 0; i < 100; ++i) {
+        QQuickItem *item = new QQuickItem(quickWindow.contentItem());
+        for (int j = 0; j < 100; ++j)
+            item = new QQuickItem(item);
+    }
+
+    QBENCHMARK {
+        quickWindow.contentItem()->polish();
+        QQuickWindowPrivate::get(&quickWindow)->polishItems();
+    }
 }
 
 QTEST_MAIN(tst_QQuickWindowContainer)
