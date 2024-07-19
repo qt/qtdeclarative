2d8a93c08c1d943329bfa0fc657d917aea5cb5d1
Kai Köhne <kai.koehne@qt.io>
2024-07-19 14:19:41 +0200

    AssetDownloader: Fix build on macOS
    Fixes error: member access into incomplete type 'const QQmlContext'
    
    Change-Id: Ic31a3fc14c5343635640790f4fcc080752029468
    Reviewed-by: Ali Kianian <ali.kianian@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit c44ade292e3a5ec7c279b5d63db0673f8013f2b4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/assets/downloader/qqmlassetdownloader.cpp
+++ src/assets/downloader/qqmlassetdownloader.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qqmlassetdownloader_p.h"
+#include <QtQml/qqmlcontext.h>
 
 QT_BEGIN_NAMESPACE
 
