qt/qtdeclarative

commit 17318c4805d089f10c25f8165dae7d1f908bd311

QQuickDrag: correctly support text and image mime types
If the variant contains a string or image, try to encode the QString or
QImage, respectively, according to the specified mime type. If it's
neither, or if the mime type is not supported by known encoders, then
generate a warning.

To make the code unit-testable, move the QQuickDragAttachedPrivate into
a private header that we can include in a new test case. Verify that data
that matches the mime type results in correctly populated QMimeData, and
that mismatches produce runtime warnings.

[ChangeLog][Qt Quick][Drag] The attaching type Drag supports more mime
types in the mimeData property, which is now of type 'variantlist'
instead of 'stringlist'. Qt Quick Drag validates the type of the data
against supported mime types, and generates warnings in case of a
mismatch. If the type of the data is ArrayBuffer (i.e. a QByteArray),
then the data is accepted without any validation.

Task-number: QTBUG-71922
Change-Id: I82789086fed9c7bc399183e76ffb83b819df440f
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

src/quick/items/qquickdrag.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qquickdrag_p.h"
+#include "qquickdrag_p_p.h"
 
 #include <private/qguiapplication_p.h>
 #include <qpa/qplatformintegration.h>
@@ -10,78 +11,22 @@
 #include <private/qquickitemchangelistener_p.h>
 #include <private/qquickpixmapcache_p.h>
 #include <private/qv4scopedvalue_p.h>
+#include <QtCore/qbuffer.h>
 #include <QtCore/qmimedata.h>
+#include <QtCore/qstringconverter.h>
 #include <QtQml/qqmlinfo.h>
 #include <QtGui/qevent.h>
 #include <QtGui/qstylehints.h>
 #include <QtGui/qguiapplication.h>
+#include <QtGui/qimagewriter.h>
 
 #include <qpa/qplatformdrag.h>
 #include <QtGui/qdrag.h>
 
 QT_BEGIN_NAMESPACE
 
-class QQuickDragAttachedPrivate : public QObjectPrivate, public QQuickItemChangeListener
-{
-    Q_DECLARE_PUBLIC(QQuickDragAttached)
-    QML_ANONYMOUS
-    QML_ADDED_IN_VERSION(2, 0)
+using namespace Qt::StringLiterals;
 
-public:
-    static QQuickDragAttachedPrivate *get(QQuickDragAttached *attached) {
-        return static_cast<QQuickDragAttachedPrivate *>(QObjectPrivate::get(attached)); }
-
-    QQuickDragAttachedPrivate()
-        : attachedItem(nullptr)
-        , mimeData(nullptr)
-        , proposedAction(Qt::MoveAction)
-        , supportedActions(Qt::MoveAction | Qt::CopyAction | Qt::LinkAction)
-        , active(false)
-        , listening(false)
-        , inEvent(false)
-        , dragRestarted(false)
-        , itemMoved(false)
-        , eventQueued(false)
-        , overrideActions(false)
-        , dragType(QQuickDrag::Internal)
-    {
-    }
-
-    void itemGeometryChanged(QQuickItem *, QQuickGeometryChange, const QRectF &) override;
-    void itemParentChanged(QQuickItem *, QQuickItem *parent) override;
-    void updatePosition();
-    void restartDrag();
-    void deliverEnterEvent();
-    void deliverMoveEvent();
-    void deliverLeaveEvent();
-    void deliverEvent(QQuickWindow *window, QEvent *event);
-    void start(Qt::DropActions supportedActions);
-    Qt::DropAction startDrag(Qt::DropActions supportedActions);
-    void setTarget(QQuickItem *item);
-
-    QQuickDragGrabber dragGrabber;
-
-    QPointer<QObject> source;
-    QPointer<QObject> target;
-    QPointer<QQuickWindow> window;
-    QQuickItem *attachedItem;
-    QQuickDragMimeData *mimeData;
-    Qt::DropAction proposedAction;
-    Qt::DropActions supportedActions;
-    bool active : 1;
-    bool listening : 1;
-    bool inEvent : 1;
-    bool dragRestarted : 1;
-    bool itemMoved : 1;
-    bool eventQueued : 1;
-    bool overrideActions : 1;
-    QPointF hotSpot;
-    QUrl imageSource;
-    QQuickPixmap pixmapLoader;
-    QStringList keys;
-    QVariantMap externalMimeData;
-    QQuickDrag::DragType dragType;
-};
 
 /*!
     \qmltype Drag
@@ -443,12 +388,13 @@ void QQuickDragAttached::setKeys(const QStringList &keys)
 }
 
 /*!
-    \qmlattachedproperty stringlist QtQuick::Drag::mimeData
+    \qmlattachedproperty var QtQuick::Drag::mimeData
     \since 5.2
 
     This property holds a map from mime type to data that is used during startDrag.
-    The mime data needs to be a \c string, or an \c ArrayBuffer with the data encoded
-    according to the mime type.
+    The mime data needs to be of a type that matches the mime type (e.g. a string if
+    the mime type is "text/plain", or an image if the mime type is "image/png"), or
+    an \c ArrayBuffer with the data encoded according to the mime type.
 */
 
 QVariantMap QQuickDragAttached::mimeData() const
@@ -726,24 +672,108 @@ void QQuickDragAttached::cancel()
     \sa drop()
  */
 
+QMimeData *QQuickDragAttachedPrivate::createMimeData() const
+{
+    Q_Q(const QQuickDragAttached);
+    QMimeData *mimeData = new QMimeData();
+
+    for (const auto [mimeType, value] : externalMimeData.asKeyValueRange()) {
+        switch (value.typeId()) {
+        case QMetaType::QByteArray:
+            // byte array assumed to already be correctly encoded
+            mimeData->setData(mimeType, value.toByteArray());
+            break;
+        case QMetaType::QString: {
+            const QString text = value.toString();
+            if (mimeType == u"text/plain"_s) {
+                mimeData->setText(text);
+            } else if (mimeType == u"text/html"_s) {
+                mimeData->setHtml(text);
+            } else if (mimeType == u"text/uri-list"_s) {
+                const QUrl url(text);
+                if (url.isValid())
+                    mimeData->setUrls({url});
+                else
+                    qmlWarning(q) << text << " is not a valid URI";
+            } else if (mimeType.startsWith(u"text/"_s)) {
+                if (qsizetype charsetIdx = mimeType.lastIndexOf(u";charset="_s); charsetIdx != -1) {
+                    charsetIdx += sizeof(";charset=") - 1;
+                    const QByteArray encoding = mimeType.mid(charsetIdx).toUtf8();
+                    QStringEncoder encoder(encoding);
+                    if (encoder.isValid())
+                        mimeData->setData(mimeType, encoder.encode(text));
+                    else
+                        qmlWarning(q) << "Don't know how to encode text as " << mimeType;
+                } else {
+                    mimeData->setData(mimeType, text.toUtf8().constData());
+                }
+            } else {
+                qmlWarning(q) << "Mime data contains a string, but mime type " << mimeType
+                              << " is not a supported text type";
+            }
+            break;
+        }
+        case QMetaType::QVariantList:
+        case QMetaType::QStringList:
+            if (mimeType == u"text/uri-list"_s) {
+                const QVariantList values = value.toList();
+                QList<QUrl> urls;
+                urls.reserve(values.size());
+                bool error = false;
+                for (qsizetype index = 0; index < values.size(); ++index) {
+                    const QUrl url = values.at(index).value<QUrl>();
+                    if (url.isValid()) {
+                        urls += url;
+                    } else {
+                        error = true;
+                        qmlWarning(q) << "Value '" << values.at(index) << "' at index " << index
+                                      << " is not a valid URI";
+                    }
+                }
+                if (!error)
+                    mimeData->setUrls(urls);
+            }
+            break;
+        case QMetaType::QImage:
+            if (const QByteArray mimeTypeUtf8 = mimeType.toUtf8();
+                QImageWriter::supportedMimeTypes().contains(mimeTypeUtf8)) {
+                const auto imageFormats = QImageWriter::imageFormatsForMimeType(mimeTypeUtf8);
+                if (imageFormats.isEmpty()) { // shouldn't happen, but we can fall back
+                    mimeData->setImageData(value);
+                    break;
+                }
+                const QImage image = value.value<QImage>();
+                QByteArray bytes;
+                {
+                    QBuffer buffer(&bytes);
+                    QImageWriter encoder(&buffer, imageFormats.first());
+                    encoder.write(image);
+                }
+                mimeData->setData(mimeType, bytes);
+                break;
+            }
+            Q_FALLTHROUGH();
+        default:
+            qmlWarning(q) << "Don't know how to encode variant of type " << value.metaType()
+                          << " as mime type " << mimeType;
+            // compatibility with pre-6.5 - probably a bad idea
+            mimeData->setData(mimeType, value.toString().toUtf8());
+            break;
+        }
+    }
+
+    return mimeData;
+}
+
 Qt::DropAction QQuickDragAttachedPrivate::startDrag(Qt::DropActions supportedActions)
 {
     Q_Q(QQuickDragAttached);
 
     QDrag *drag = new QDrag(source ? source : q);
-    QMimeData *mimeData = new QMimeData();
 
-    for (auto it = externalMimeData.cbegin(), end = externalMimeData.cend(); it != end; ++it) {
-        if (it.value().typeId() == QMetaType::QByteArray)
-            mimeData->setData(it.key(), it.value().toByteArray());
-        else
-            mimeData->setData(it.key(), it.value().toString().toUtf8());
-    }
-
-    drag->setMimeData(mimeData);
-    if (pixmapLoader.isReady()) {
+    drag->setMimeData(createMimeData());
+    if (pixmapLoader.isReady())
         drag->setPixmap(QPixmap::fromImage(pixmapLoader.image()));
-    }
 
     drag->setHotSpot(hotSpot.toPoint());
     emit q->dragStarted();

tests/auto/quick/qquickdragattached/tst_qquickdragattached.cpp
@@ -1,0 +1,127 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include <QtTest/QtTest>
+#include <QtTest/QSignalSpy>
+#include <QtQuick/qquickwindow.h>
+#include <QtQuick/private/qquickdrag_p_p.h>
+
+class tst_QQuickDragAttached: public QObject
+{
+    Q_OBJECT
+private slots:
+    void setMimeData_data();
+    void setMimeData();
+
+    void startDrag_data();
+    void startDrag();
+};
+
+void tst_QQuickDragAttached::setMimeData_data()
+{
+    QTest::addColumn<QVariantMap>("mimeData");
+    QTest::addColumn<QStringList>("formats");
+
+    const QImage image(50, 50, QImage::Format_RGB32);
+
+    QTest::addRow("empty") << QVariantMap{} << QStringList{};
+
+    const auto makeMap = [](const QString &mime, const QVariant &value) {
+        QVariantMap map;
+        map[mime] = value;
+        return map;
+    };
+
+    QTest::addRow("text/plain, string")
+        << makeMap("text/plain", QString("string"))
+        << QStringList{"text/plain"};
+    QTest::addRow("valid charset")
+        << makeMap("text/plain;charset=utf-16", QString("string"))
+        << QStringList{"text/plain;charset=utf-16"};
+    QTest::addRow("image/png, image")
+        << makeMap("image/png", image)
+        << QStringList{"image/png"};
+    QTest::addRow("text/uri-list, string")
+        << makeMap("text/uri-list", QString("https://qt-project.org"))
+        << QStringList{"text/uri-list"};
+    QTest::addRow("text/uri-list, strings")
+        << makeMap("text/uri-list", QStringList{"file://foo", "https://www.test.com"})
+        << QStringList{"text/uri-list"};
+    QTest::addRow("text/uri-list, url")
+        << makeMap("text/uri-list", QVariantList{QVariant(QUrl("file://foo"))})
+        << QStringList{"text/uri-list"};
+    QTest::addRow("text/uri-list, variant")
+        << makeMap("text/uri-list", QVariantList{QVariant("file://foo")})
+        << QStringList{"text/uri-list"};
+    QTest::addRow("mismatch image")
+        << makeMap("image/png", "not an image")
+        << QStringList{};
+    QTest::addRow("missing charset")
+        << makeMap("text/plain;charset=", QString("foo"))
+        << QStringList{};
+    QTest::addRow("invalid charset")
+        << makeMap("text/plain;charset=ugh4", QString("ugh4"))
+        << QStringList{};
+    QTest::addRow("mismatch compat")
+        << makeMap("text/plain", image)
+        << QStringList{"text/plain"};
+    QTest::addRow("mismatch list")
+        << makeMap("text/uri-list", QVariantList{image})
+        << QStringList{};
+}
+
+void tst_QQuickDragAttached::setMimeData()
+{
+    QFETCH(QVariantMap, mimeData);
+
+    QQuickDragAttached attached(nullptr);
+    QSignalSpy spy(&attached, &QQuickDragAttached::mimeDataChanged);
+
+    attached.setMimeData(mimeData);
+    int expectedCount = mimeData.isEmpty() ? 0 : 1;
+    QCOMPARE(spy.count(), expectedCount);
+
+    QCOMPARE(attached.mimeData(), mimeData);
+
+    attached.setMimeData(mimeData);
+    QCOMPARE(spy.count(), expectedCount);
+
+    expectedCount += mimeData.isEmpty() ? 0 : 1;
+    attached.setMimeData({});
+    QCOMPARE(spy.count(), expectedCount);
+}
+
+void tst_QQuickDragAttached::startDrag_data()
+{
+    setMimeData_data();
+}
+
+void tst_QQuickDragAttached::startDrag()
+{
+#ifdef QT_BUILD_INTERNAL
+    QFETCH(QVariantMap, mimeData);
+    QFETCH(QStringList, formats);
+
+    QQuickDragAttached attached(nullptr);
+    attached.setMimeData(mimeData);
+
+    auto *d = static_cast<QQuickDragAttachedPrivate *>(QObjectPrivate::get(&attached));
+
+    if (formats.isEmpty()) {
+        if (!mimeData.isEmpty())
+            QTest::ignoreMessage(QtWarningMsg, QRegularExpression(".*QML QQuickDragAttached:.*"));
+        else
+            QTest::failOnWarning(QRegularExpression(".*"));
+    }
+    std::unique_ptr<QMimeData> data(d->createMimeData());
+    QVERIFY(data);
+
+    QCOMPARE(data->formats(), formats);
+#else
+    QSKIP("This test relies on private APIs that are only exported in developer-builds");
+#endif
+}
+
+QTEST_MAIN(tst_QQuickDragAttached)
+
+#include "tst_qquickdragattached.moc"

src/quick/CMakeLists.txt
@@ -417,7 +417,7 @@ qt_internal_extend_target(Quick CONDITION QT_FEATURE_quick_sprite
 
 qt_internal_extend_target(Quick CONDITION QT_FEATURE_quick_draganddrop
     SOURCES
-        items/qquickdrag.cpp items/qquickdrag_p.h
+        items/qquickdrag.cpp items/qquickdrag_p.h items/qquickdrag_p_p.h
         items/qquickdroparea.cpp items/qquickdroparea_p.h
 )
 

src/quick/items/qquickdrag_p_p.h
@@ -1,0 +1,97 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef QQUICKDRAG_P_P_H
+#define QQUICKDRAG_P_P_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qquickdrag_p.h"
+
+#include <QtCore/qobject.h>
+#include <QtCore/private/qobject_p.h>
+#include <QtCore/qpointer.h>
+#include <QtCore/qurl.h>
+#include <QtCore/qvariant.h>
+
+#include <QtQuick/qquickitem.h>
+#include <QtQuick/private/qquickitemchangelistener_p.h>
+#include <QtQuick/private/qquickpixmapcache_p.h>
+
+QT_BEGIN_NAMESPACE
+
+class Q_AUTOTEST_EXPORT QQuickDragAttachedPrivate : public QObjectPrivate, public QQuickItemChangeListener
+{
+    Q_DECLARE_PUBLIC(QQuickDragAttached)
+    QML_ANONYMOUS
+    QML_ADDED_IN_VERSION(2, 0)
+
+public:
+    static QQuickDragAttachedPrivate *get(QQuickDragAttached *attached) {
+        return static_cast<QQuickDragAttachedPrivate *>(QObjectPrivate::get(attached)); }
+
+    QQuickDragAttachedPrivate()
+        : attachedItem(nullptr)
+        , mimeData(nullptr)
+        , proposedAction(Qt::MoveAction)
+        , supportedActions(Qt::MoveAction | Qt::CopyAction | Qt::LinkAction)
+        , active(false)
+        , listening(false)
+        , inEvent(false)
+        , dragRestarted(false)
+        , itemMoved(false)
+        , eventQueued(false)
+        , overrideActions(false)
+        , dragType(QQuickDrag::Internal)
+    {
+    }
+
+    void itemGeometryChanged(QQuickItem *, QQuickGeometryChange, const QRectF &) override;
+    void itemParentChanged(QQuickItem *, QQuickItem *parent) override;
+    void updatePosition();
+    void restartDrag();
+    void deliverEnterEvent();
+    void deliverMoveEvent();
+    void deliverLeaveEvent();
+    void deliverEvent(QQuickWindow *window, QEvent *event);
+    void start(Qt::DropActions supportedActions);
+    Qt::DropAction startDrag(Qt::DropActions supportedActions);
+    void setTarget(QQuickItem *item);
+    QMimeData *createMimeData() const;
+
+    QQuickDragGrabber dragGrabber;
+
+    QPointer<QObject> source;
+    QPointer<QObject> target;
+    QPointer<QQuickWindow> window;
+    QQuickItem *attachedItem;
+    QQuickDragMimeData *mimeData;
+    Qt::DropAction proposedAction;
+    Qt::DropActions supportedActions;
+    bool active : 1;
+    bool listening : 1;
+    bool inEvent : 1;
+    bool dragRestarted : 1;
+    bool itemMoved : 1;
+    bool eventQueued : 1;
+    bool overrideActions : 1;
+    QPointF hotSpot;
+    QUrl imageSource;
+    QQuickPixmap pixmapLoader;
+    QStringList keys;
+    QVariantMap externalMimeData;
+    QQuickDrag::DragType dragType;
+};
+
+QT_END_NAMESPACE
+
+#endif

tests/auto/quick/CMakeLists.txt
@@ -44,6 +44,7 @@ if(QT_FEATURE_private_tests)
     add_subdirectory(qquickwindow)
     add_subdirectory(qquickdeliveryagent)
     add_subdirectory(qquickdrag)
+    add_subdirectory(qquickdragattached)
     add_subdirectory(qquickdroparea)
     add_subdirectory(qquickflickable)
     add_subdirectory(qquickflipable)

tests/auto/quick/qquickdragattached/CMakeLists.txt
@@ -1,0 +1,11 @@
+qt_internal_add_test(tst_qquickdragattached
+    SOURCES
+        tst_qquickdragattached.cpp
+    LIBRARIES
+        Qt::CorePrivate
+        Qt::Gui
+        Qt::GuiPrivate
+        Qt::Network
+        Qt::QmlPrivate
+        Qt::QuickPrivate
+)

commit c25f53b0d48c71dbec8aef69036ab181d69bdae1

QQuickDrag: Parse "text/uri-list" more generously
This change allows "text/uri-list" to be given as a direct string, while
still keeping the URI validity checks and fixes a regression that was
introduced in 17318c4805d089f10c25f8165dae7d1f908bd311.

Before that commit, the "text/uri-list" mimetype could be set via a
string following RFC2483.
https://www.ietf.org/rfc/rfc2483.txt

RFC2483 essentially specifies that "text/uri-list" properties are split
by CRLF with each line indicating one URI, for example the string
"file:///tmp/a.txt\r\nfile:///tmp/b.txt" would be interpreted as a list
of two local files.

And indeed, setting Drag.mimeData's "text/uri-list" to such a string
worked before the above commit.

The commit responsible for the regression introduces some sanity checks
(which are good), but also regresses the string assignment, by always
assuming that there is only one URL available and hardcoding the urls
list to just one URL:
mimeData->setUrls({url});

It was however at least possible to properly set a list of URLs from QML
by assigning a QVariantList (or a direct QByteArray).
While this is a neat convenience function, the original way of being
able to directly set RFC2483 conformant strings should be restored.

This patch fixes the regression by properly splitting the URI list again
on CRLF and it is also easy to verify that this patch does not introduce
any new regression for any behavior that was working before:
- If the uri list does contain CRLF, then before this patch it would be
  interpreted as a single URI, which is not a valid URI (the CRLF would
  need to be URI-encoded)
- If the uri-list does not contain CRLF, then the behavior is unchanged
  (a single URI is added)

A simple way to test this patch is to set:
Drag.mimeData: { "text/uri-list": "file:///tmp/a\r\nfile:///tmp/b" }

Pick-to: 6.5 6.7 6.8
Change-Id: I5fbbc1a6b92a8782d62efb94b07a1a18a33c88fb
Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>

src/quick/items/qquickdrag.cpp
@@ -728,11 +728,18 @@ QMimeData *QQuickDragAttachedPrivate::createMimeData() const
             } else if (mimeType == u"text/html"_s) {
                 mimeData->setHtml(text);
             } else if (mimeType == u"text/uri-list"_s) {
-                const QUrl url(text);
-                if (url.isValid())
-                    mimeData->setUrls({url});
-                else
-                    qmlWarning(q) << text << " is not a valid URI";
+                QList<QUrl> urls;
+                // parse and split according to RFC2483
+                const auto lines = text.split(u"\r\n"_s, Qt::SkipEmptyParts);
+                for (const auto &line : lines) {
+                    const QUrl url(line);
+                    if (url.isValid())
+                        urls.push_back(url);
+                    else
+                        qmlWarning(q) << line << " is not a valid URI";
+
+                }
+                mimeData->setUrls(urls);
             } else if (mimeType.startsWith(u"text/"_s)) {
                 if (qsizetype charsetIdx = mimeType.lastIndexOf(u";charset="_s); charsetIdx != -1) {
                     charsetIdx += sizeof(";charset=") - 1;

tests/auto/quick/qquickdragattached/tst_qquickdragattached.cpp
@@ -57,6 +57,9 @@ void tst_QQuickDragAttached::setMimeData_data()
     QTest::addRow("text/uri-list, string")
         << makeMap("text/uri-list", QString("https://qt-project.org"))
         << QStringList{"text/uri-list"};
+    QTest::addRow("text/uri-list, RFC2483 string")
+        << makeMap("text/uri-list", QString("https://qt-project.org\r\nhttps://www.test.com"))
+        << QStringList{"text/uri-list"};
     QTest::addRow("text/uri-list, strings")
         << makeMap("text/uri-list", QStringList{"file://foo", "https://www.test.com"})
         << QStringList{"text/uri-list"};