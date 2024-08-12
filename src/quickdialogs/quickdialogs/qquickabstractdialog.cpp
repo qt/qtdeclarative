37ae99b2c1618f46277147e243da2a9adb4286c0
Tor Arne Vestbø <tor.arne.vestbo@qt.io>
2024-08-12 15:13:15 +0200

    Dialogs: Fall back to non-native dialog if native dialog can't be shown
    A native dialog helper might not be able to show a dialog due to the
    dialog options not being possible to represent with the native dialog.
    
    If so the helper returns false from show(). We were not handling this
    case, so failing to show a native dialog would result in no dialog
    shown.
    
    We now fall back to the non-native dialog by destroying the native
    helper and using the non-native dialog helper explicitly.
    
    Fixes: QTBUG-124124
    Pick-to: 6.5
    Change-Id: If50c63e419ae7b13e9317cd2cbbcaa02fe13bbd5
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>
    (cherry picked from commit c821284d97b6be6755d1bddeec85d222ecb924e6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit e0b536e01d6d07db4f4cf012b8deca811d7d9bde)

--- src/quickdialogs/quickdialogs/qquickabstractdialog.cpp
+++ src/quickdialogs/quickdialogs/qquickabstractdialog.cpp
@@ -53,7 +53,14 @@ Q_LOGGING_CATEGORY(lcDialogs, "qt.quick.dialogs")
                          v                                  |
                  +-----------------+                        |
                  | m_handle valid? |--------------------->false
-                 +-----------------+
+                 +-----------------+                        ^
+                         |                                  |
+                         v                                  |
+                        true                                |
+                         |                                  |
+                 +-------------------+                      |
+                 | m_handle->show()? |------------------->false
+                 +-------------------+
                          |
                          v
                         true
@@ -302,6 +309,23 @@ void QQuickAbstractDialog::open()
     onShow(m_handle.get());
 
     m_visible = m_handle->show(m_flags, m_modality, windowForOpen());
+    if (!m_visible && useNativeDialog()) {
+        // Fall back to non-native dialog
+        destroy();
+        if (!create(CreateOptions::DontTryNativeDialog))
+            return;
+
+        onShow(m_handle.get());
+        m_visible = m_handle->show(m_flags, m_modality, windowForOpen());
+
+        if (m_visible) {
+            // The conditions that caused the non-native fallback might have
+            // changed the next time open() is called, so we should try again
+            // with a native dialog when that happens.
+            QObject::connect(this, &QQuickAbstractDialog::visibleChanged,
+                m_handle.get(), [this]{ if (!isVisible()) destroy(); });
+        }
+    }
     if (m_visible) {
         m_result = Rejected; // in case an accepted dialog gets re-opened, then closed
         emit visibleChanged();
@@ -409,15 +433,15 @@ QPlatformTheme::DialogType toPlatformDialogType(QQuickDialogType quickDialogType
         ? QPlatformTheme::FileDialog : static_cast<QPlatformTheme::DialogType>(quickDialogType);
 }
 
-bool QQuickAbstractDialog::create()
+bool QQuickAbstractDialog::create(CreateOptions createOptions)
 {
     qCDebug(lcDialogs) << qmlTypeName(this) << "attempting to create dialog backend of type"
         << int(m_type) << "with parent window" << m_parentWindow;
     if (m_handle)
         return m_handle.get();
 
-    qCDebug(lcDialogs) << "- attempting to create a native dialog";
-    if (useNativeDialog()) {
+    if ((createOptions != CreateOptions::DontTryNativeDialog) && useNativeDialog()) {
+        qCDebug(lcDialogs) << "- attempting to create a native dialog";
         m_handle.reset(QGuiApplicationPrivate::platformTheme()->createPlatformDialogHelper(
             toPlatformDialogType(m_type)));
     }
