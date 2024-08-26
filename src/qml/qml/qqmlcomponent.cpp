47aab5890a93e2c141e234c09aef9c1e3966824e
Mitch Curtis <mitch.curtis@qt.io>
2024-08-26 13:04:01 +0800

    QQmlComponent::setInitialProperties: rename "component" parameter
    It's not a component, it's an object instance.
    
    Also amend the documentation to be clearer that it is to be used with
    objects created from QQmlComponents.
    
    Pick-to: 6.7 6.5
    Change-Id: I164d894bcd61b9c39bde902cfcf5ada17a2478c4
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit dc92f5ddee1330901c7f0a8768563306a9a84cf1)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/qml/qqmlcomponent.cpp
+++ src/qml/qml/qqmlcomponent.cpp
@@ -1495,11 +1495,13 @@ void QQmlComponent::create(QQmlIncubator &incubator, QQmlContext *context, QQmlC
 }
 
 /*!
-    Set top-level \a properties of the \a component.
+    Set top-level \a properties of the \a object that was created from a
+    QQmlComponent.
 
     This method provides advanced control over component instance creation.
     In general, programmers should use
-    \l QQmlComponent::createWithInitialProperties to create a component.
+    \l QQmlComponent::createWithInitialProperties to create an object instance
+    from a component.
 
     Use this method after beginCreate and before completeCreate has been called.
     If a provided property does not exist, a warning is issued.
@@ -1515,7 +1517,7 @@ void QQmlComponent::create(QQmlIncubator &incubator, QQmlContext *context, QQmlC
 
     \since 5.14
 */
-void QQmlComponent::setInitialProperties(QObject *component, const QVariantMap &properties)
+void QQmlComponent::setInitialProperties(QObject *object, const QVariantMap &properties)
 {
     Q_D(QQmlComponent);
     for (auto it = properties.constBegin(); it != properties.constEnd(); ++it) {
@@ -1531,10 +1533,10 @@ void QQmlComponent::setInitialProperties(QObject *component, const QVariantMap &
             QQmlError error{};
             error.setUrl(url());
             error.setDescription(description);
-            qmlWarning(component, error);
+            qmlWarning(object, error);
             return;
         }
-        d->setInitialProperty(component, it.key(), it.value());
+        d->setInitialProperty(object, it.key(), it.value());
     }
 }
 
