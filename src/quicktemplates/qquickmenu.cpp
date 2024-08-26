f1cd4c57b28ba60b72ba0d978fc932199071ab8a
Morten Sørvig <morten.sorvig@qt.io>
2024-08-26 18:08:45 +0200

    Fix multi-screen menu pop-up location
    Follow-up to 7574111a56d38832fd330df5ba4839d0263fbc09.
    The position passed to QPlatformMenu::showPopup() is in the
    window-local coordinate system, which means the the "local"
    toNative() scaling function must be used.
    
    Task-number: QTBUG-127906
    Task-number: QTBUG-128126
    Pick-to: 6.7 6.5
    Change-Id: I3ec34bdae4ce9a7235c68eab248f4a9cb127eb3f
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    Reviewed-by: Inho Lee <inho.lee@qt.io>
    (cherry picked from commit b6dde1f317894fe0ade761f25871a89887611f7b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quicktemplates/qquickmenu.cpp
+++ src/quicktemplates/qquickmenu.cpp
@@ -636,7 +636,7 @@ void QQuickMenuPrivate::setNativeMenuVisible(bool visible)
                 }
             }
         }
-        handle->showPopup(window, QHighDpi::toNativePixels(targetRect, window),
+        handle->showPopup(window, QHighDpi::toNativeLocalPosition(targetRect, window),
             /*menuItem ? menuItem->handle() : */nullptr);
     } else {
         handle->dismiss();
