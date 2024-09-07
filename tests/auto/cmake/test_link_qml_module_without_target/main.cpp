9b5bf0531b647157a0b44d65f2e938ad107b1792
Ulf Hermann <ulf.hermann@qt.io>
2024-09-07 18:30:41 +0200

    tests: Don't create a QQuickView when testing linkage of static modules
    Opening a window creates flakiness since it can fail under various
    platform-specific conditions. We don't need to open a window here. We
    just want to verify that the QML modules compiled into the executable
    are functional.
    
    Fixes: QTBUG-128645
    Change-Id: Ic1707007114619fc522f737e3431fa4873b7d138
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 2346edfd6d29035286fbb4bc2818b4e2141770c7)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- tests/auto/cmake/test_link_qml_module_without_target/main.cpp
+++ tests/auto/cmake/test_link_qml_module_without_target/main.cpp
@@ -4,16 +4,16 @@
 #include <QtCore/QDirIterator>
 #include <QtGui/QGuiApplication>
 #include <QtTest/QtTest>
-#include <QtQml/QQmlApplicationEngine>
-#include <QtQml/QQmlContext>
+#include <QtQml/qqmlengine.h>
 #include <QtQml/qqmlextensionplugin.h>
-#include <QtQuick/QQuickView>
-#include <QtQuickTestUtils/private/viewtestutils_p.h>
+#include <QtQml/qqmlcomponent.h>
 
 // Manually import the qml plugins so that the qml modules and their resources
 // are initialized.
 Q_IMPORT_QML_PLUGIN(BasePlugin)
 Q_IMPORT_QML_PLUGIN(DerivedPlugin)
+
+using namespace Qt::StringLiterals;
 
 class TestLinkStaticQmlModule : public QObject
 {
@@ -28,15 +28,20 @@ void TestLinkStaticQmlModule::canRun()
     QDirIterator it(":", QDirIterator::Subdirectories);
     while (it.hasNext()) {
         const auto path = it.next();
-        if (!path.startsWith(u":/qt-project.org"_qs)) {
+        if (!path.startsWith(u":/qt-project.org"_s)) {
             qDebug() << path;
         }
     }
 
-    QQuickView view;
     QVERIFY(QFile::exists(":/qt/qml/Base/qml/Red.qml"));
     QVERIFY(QFile::exists(":/qt/qml/Derived/qml/Blue.qml"));
-    QVERIFY(QQuickTest::showView(view, QUrl(u"qrc:/qt/qml/Derived/qml/main.qml"_qs)));
+
+    QQmlEngine engine;
+    QQmlComponent c(&engine, QUrl(u"qrc:/qt/qml/Derived/qml/main.qml"_s));
+    QVERIFY2(c.isReady(), qPrintable(c.errorString()));
+
+    QScopedPointer<QObject> o(c.create());
+    QVERIFY(!o.isNull());
 }
 
 QTEST_MAIN(TestLinkStaticQmlModule)
