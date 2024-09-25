18a066a0bdf65d20cdb230c54d4014beaf77b649
Ulf Hermann <ulf.hermann@qt.io>
2024-09-25 14:45:39 +0200

    QtQml: Disable AOT compiled code when QML-previewing
    We cannot replace AOT-compiled compilation units while an object is
    still holding on to them and we cannot delete all objects holding on to
    a CU because they might not belong to the preview to begin with.
    
    Pick-to: 6.5
    Fixes: QTBUG-129329
    Change-Id: Icbcb7822be770a440f3216955c0ae51151390e17
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 9a9a2184cefb777457984c965416c7ebf4c22e1a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/qmltooling/qmldbg_preview/qqmlpreviewservice.cpp
+++ src/plugins/qmltooling/qmldbg_preview/qqmlpreviewservice.cpp
@@ -120,8 +120,13 @@ void QQmlPreviewServiceImpl::engineAboutToBeRemoved(QJSEngine *engine)
 
 void QQmlPreviewServiceImpl::stateChanged(QQmlDebugService::State state)
 {
-    m_fileEngine.reset(state == Enabled ? new QQmlPreviewFileEngineHandler(m_loader.data())
-                                        : nullptr);
+    if (state == Enabled) {
+        QV4::ExecutionEngine::setPreviewing(true);
+        m_fileEngine.reset(new QQmlPreviewFileEngineHandler(m_loader.data()));
+    } else {
+        QV4::ExecutionEngine::setPreviewing(false);
+        m_fileEngine.reset();
+    }
 }
 
 void QQmlPreviewServiceImpl::forwardRequest(const QString &file)
