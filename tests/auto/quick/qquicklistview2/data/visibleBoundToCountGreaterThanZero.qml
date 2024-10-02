qt/qtdeclarative

commit 477c15def834bd49553c00b90f3a2006456ea931

Container: cull items when our size is zero and contentItem is a view
The typical use case for SwipeView is that the user declares items
directly as children. Those children are added to its QQmlObjectModel,
which are then used as the delegates for its ListView. ListView culls
the items it manages, but in the case of a zero-sized view, those that
are outside the view - and hence not in its visibleItems list - are not
its responsibility. That responsibility instead falls to
QQuickContainer.

It's also necessary to clear and return early in
QQuickItemViewPrivate::layout if the size of the view is zero,
otherwise the tests that check if a valid-sized view is made
zero-sized fail.

Add a new C++-based test for Container which is only run with the Basic
style (similar to e.g. tst_qquickmenubar) so that we can check the
culled member of QQuickItemPrivate. tst_container.qml is run
with all styles (even though it probably doesn't need to be, but it
would also be difficult to rewrite in C++, even though it's fairly
small).

Fix some tests that were using zero-sized views.

Fixes: QTBUG-125416
Pick-to: 6.5 6.7 6.8
Change-Id: Ia05791b69568d51bea310e4f09266645ca2f1829
Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@qt.io>

src/quick/items/qquickitemview.cpp
@@ -1860,7 +1860,10 @@ void QQuickItemViewPrivate::layout()
     // viewBounds contains bounds before any add/remove/move operation to the view
     QRectF viewBounds(q->contentX(),  q->contentY(), q->width(), q->height());
 
-    if (!isValid() && !visibleItems.size()) {
+    // We use isNull for the size check, because isEmpty returns true
+    // if either dimension is negative, but apparently we support negative-sized
+    // views (see tst_QQuickListView::resizeView).
+    if ((!isValid() && !visibleItems.size()) || q->size().isNull()) {
         clear();
         setPosition(contentStartOffset());
         updateViewport();

src/quicktemplates/qquickcontainer.cpp
@@ -5,6 +5,7 @@
 #include "qquickcontainer_p_p.h"
 
 #include <QtQuick/private/qquickflickable_p.h>
+#include <QtQuick/private/qquickitemview_p.h>
 
 QT_BEGIN_NAMESPACE
 
@@ -211,6 +212,7 @@ void QQuickContainerPrivate::insertItem(int index, QQuickItem *item)
     updatingCurrent = true;
 
     item->setParentItem(effectiveContentItem(q->contentItem()));
+    maybeCullItem(item);
     QQuickItemPrivate::get(item)->addItemChangeListener(this, changeTypes);
     contentModel->insert(index, item);
 
@@ -308,6 +310,38 @@ void QQuickContainerPrivate::reorderItems()
         int index = contentModel->indexOf(sibling, nullptr);
         q->moveItem(index, to++);
     }
+}
+
+void QQuickContainerPrivate::maybeCullItem(QQuickItem *item)
+{
+    if (QQuickItemPrivate::get(item)->isTransparentForPositioner())
+        return;
+
+    // Items like Repeater don't control the visibility of the items they create,
+    // so we can't expected them to uncull items added dynamically. As mentioned
+    // below, Repeater _does_ uncull items added to it, but unlike e.g. ListView,
+    // it shouldn't care if its size becomes zero and so it shouldn't manage
+    // the culled state of items in the same way.
+    if (!qobject_cast<QQuickItemView *>(contentItem))
+        return;
+
+    // Only cull items if the contentItem has a zero size; otherwise let the
+    // contentItem manage it.
+    const bool hasZeroSize = qFuzzyIsNull(width) && qFuzzyIsNull(height);
+    if (!hasZeroSize)
+        return;
+
+    QQuickItemPrivate::get(item)->setCulled(true);
+}
+
+void QQuickContainerPrivate::maybeCullItems()
+{
+    if (!contentItem)
+        return;
+
+    const QList<QQuickItem *> childItems = effectiveContentItem(contentItem)->childItems();
+    for (auto &childItem : childItems)
+        maybeCullItem(childItem);
 }
 
 void QQuickContainerPrivate::_q_currentIndexChanged()
@@ -809,6 +843,7 @@ void QQuickContainer::componentComplete()
     Q_D(QQuickContainer);
     QQuickControl::componentComplete();
     d->reorderItems();
+    d->maybeCullItems();
 }
 
 void QQuickContainer::itemChange(ItemChange change, const ItemChangeData &data)

src/quicktemplates/qquickcontainer_p_p.h
@@ -39,6 +39,8 @@ public:
     void moveItem(int from, int to, QQuickItem *item);
     void removeItem(int index, QQuickItem *item);
     void reorderItems();
+    void maybeCullItem(QQuickItem *item);
+    void maybeCullItems();
 
     void _q_currentIndexChanged();
 

tests/auto/qmltest/listview/tst_listview.qml
@@ -38,12 +38,16 @@ Item {
 
     ListView {
         id: modelchange
+        width: 100
+        height: 100
         model: firstmodel
         delegate: Text { text: model.name }
     }
 
     ListView {
         id: modelalter
+        width: 100
+        height: 100
         model: altermodel
         delegate: Text { text: model.name }
     }

tests/auto/qmltest/selftests/tst_findChild.qml
@@ -71,6 +71,8 @@ TestCase {
             }
 
             ListView {
+                width: 100
+                height: 100
                 model: 5
                 delegate: Item {
                     objectName: "listViewItem" + index

tests/auto/quick/qquicklistview/data/emptymodel.qml
@@ -6,6 +6,8 @@ Rectangle {
     }
     ListView {
         id: list
+        width: 100
+        height: 100
         model: model
         delegate: Item {
         }

tests/auto/quick/qquickview_extra/data/qtbug_87228.qml
@@ -15,6 +15,8 @@ Item {
         Rectangle {
             ListView {
                 objectName: "listView"
+                width: 100
+                height: 100
                 delegate: Text {
                     required property string desc
                     text: desc

tests/auto/quickcontrols/CMakeLists.txt
@@ -24,6 +24,7 @@ add_subdirectory(pressandhold)
 add_subdirectory(qquickapplicationwindow)
 add_subdirectory(qquickcontrol)
 add_subdirectory(qquickcolor)
+add_subdirectory(qquickcontainer)
 add_subdirectory(qquickdrawer)
 add_subdirectory(qquickheaderview)
 add_subdirectory(qquickiconimage)

commit 0a1d8fb26d1e89c82a0d35dd728d7e928a94d197

ListView: fix countChanged not being emitted in certain cases
This broke after 477c15def834bd49553c00b90f3a2006456ea931.
countChanged would have normally been emitted in the call to
applyModelChanges that we skipped after adding the q->size().isNull()
check.

We can check hasPendingChanges() to know if a count change
is pending (which is also the first thing applyModelChanges checks
before doing its work), and if so, emit countChanged so that
any bindings that are relying on it can function as expected.

Fixes: QTBUG-129165
Pick-to: 6.5 6.7 6.8 dev
Change-Id: Ic045f1870b39d192f6880e23daab03fd73a16d58
Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

src/quick/items/qquickitemview.cpp
@@ -10,6 +10,7 @@
 QT_BEGIN_NAMESPACE
 
 Q_LOGGING_CATEGORY(lcItemViewDelegateLifecycle, "qt.quick.itemview.lifecycle")
+Q_LOGGING_CATEGORY(lcCount, "qt.quick.itemview.count")
 
 // Default cacheBuffer for all views.
 #ifndef QML_VIEW_DEFAULTCACHEBUFFER
@@ -223,7 +224,7 @@ void QQuickItemView::setModel(const QVariant &m)
                 this, SLOT(modelUpdated(QQmlChangeSet,bool)));
         if (QQmlDelegateModel *dataModel = qobject_cast<QQmlDelegateModel*>(d->model))
             QObjectPrivate::connect(dataModel, &QQmlDelegateModel::delegateChanged, d, &QQuickItemViewPrivate::applyDelegateChange);
-        emit countChanged();
+        d->emitCountChanged();
     }
     emit modelChanged();
     d->moveReason = QQuickItemViewPrivate::Other;
@@ -255,7 +256,7 @@ void QQuickItemView::setDelegate(QQmlComponent *delegate)
         int oldCount = dataModel->count();
         dataModel->setDelegate(delegate);
         if (oldCount != dataModel->count())
-            emit countChanged();
+            d->emitCountChanged();
     }
     emit delegateChanged();
     d->delegateValidated = false;
@@ -1125,6 +1126,14 @@ void QQuickItemViewPrivate::showVisibleItems() const
     }
 }
 
+// Simplifies debugging of count.
+void QQuickItemViewPrivate::emitCountChanged()
+{
+    Q_Q(QQuickItemView);
+    qCDebug(lcCount).nospace() << "about to emit countChanged for " << q << "; count changed to " << q->count();
+    emit q->countChanged();
+}
+
 void QQuickItemViewPrivate::itemGeometryChanged(QQuickItem *item, QQuickGeometryChange change,
                                                 const QRectF &oldGeometry)
 {
@@ -1224,7 +1233,7 @@ void QQuickItemView::modelUpdated(const QQmlChangeSet &changeSet, bool reset)
             d->updateTrackedItem();
         }
         d->moveReason = QQuickItemViewPrivate::Other;
-        emit countChanged();
+        d->emitCountChanged();
 #if QT_CONFIG(quick_viewtransitions)
         if (d->transitioner && d->transitioner->populateTransition)
             d->forceLayoutPolish();
@@ -1487,7 +1496,7 @@ void QQuickItemView::componentComplete()
         d->fixupPosition();
     }
     if (d->model && d->model->count())
-        emit countChanged();
+        d->emitCountChanged();
 }
 
 
@@ -1813,7 +1822,7 @@ void QQuickItemViewPrivate::refill(qreal from, qreal to)
         }
 
         if (prevCount != itemCount)
-            emit q->countChanged();
+            emitCountChanged();
     } while (currentChanges.hasPendingChanges() || bufferedChanges.hasPendingChanges());
     storeFirstVisibleItemPosition();
 }
@@ -1864,6 +1873,16 @@ void QQuickItemViewPrivate::layout()
     // if either dimension is negative, but apparently we support negative-sized
     // views (see tst_QQuickListView::resizeView).
     if ((!isValid() && !visibleItems.size()) || q->size().isNull()) {
+        if (q->size().isNull() && hasPendingChanges()) {
+            // count() refers to the number of items in the model, not in the view
+            // (which is why we don't emit for the !visibleItems.size() case).
+            // If there are pending model changes, emit countChanged in order to
+            // support the use case of QTBUG-129165, where visible is bound to count > 0
+            // and the ListView is in a layout with Layout.preferredHeight bound to
+            // contentHeight. This ensures that a hidden ListView will become visible.
+            emitCountChanged();
+        }
+
         clear();
         setPosition(contentStartOffset());
         updateViewport();
@@ -2138,7 +2157,7 @@ bool QQuickItemViewPrivate::applyModelChanges(ChangeResult *totalInsertionResult
 
     updateSections();
     if (prevItemCount != itemCount)
-        emit q->countChanged();
+        emitCountChanged();
     if (!visibleAffected && viewportChanged)
         updateViewport();
 

src/quick/items/qquickitemview_p_p.h
@@ -229,6 +229,8 @@ public:
             releaseItem(item, reusableFlag);
     }
 
+    void emitCountChanged();
+
     virtual QQuickItemViewAttached *getAttachedObject(const QObject *) const { return nullptr; }
 
     QPointer<QQmlInstanceModel> model;

tests/auto/quick/qquicklistview2/data/visibleBoundToCountGreaterThanZero.qml
@@ -1,0 +1,31 @@
+import QtQuick
+import QtQuick.Layouts
+
+ColumnLayout {
+    property alias listView: listView
+
+    ListView {
+        id: listView
+
+        visible: count > 0 // actual defect. countChanged never fires so this never turns true
+
+        Layout.fillWidth: true
+        Layout.preferredHeight: contentHeight // grow with content, initially 0
+
+        model: ListModel {
+            id: idModel
+        }
+
+        delegate: Text {
+            required property string name
+            text: name
+        }
+
+        Timer {
+            running: true
+            interval: 10
+            repeat: true
+            onTriggered: idModel.append({name:"Hello"})
+        }
+    }
+}

tests/auto/quick/qquicklistview2/tst_qquicklistview2.cpp
@@ -18,6 +18,8 @@ Q_LOGGING_CATEGORY(lcTests, "qt.quick.tests")
 using namespace QQuickViewTestUtils;
 using namespace QQuickVisualTestUtils;
 
+static const int oneSecondInMs = 1000;
+
 class tst_QQuickListView2 : public QQmlDataTest
 {
     Q_OBJECT
@@ -68,6 +70,7 @@ private slots:
     void bindingDirectlyOnPositionInHeaderAndFooterDelegates();
 
     void clearObjectListModel();
+    void visibleBoundToCountGreaterThanZero();
 
 private:
     void flickWithTouch(QQuickWindow *window, const QPoint &from, const QPoint &to);
@@ -1310,6 +1313,23 @@ void tst_QQuickListView2::clearObjectListModel()
     list.setModel(QVariantList());
 
     QVERIFY(!list.itemAtIndex(0));
+}
+
+void tst_QQuickListView2::visibleBoundToCountGreaterThanZero()
+{
+    QQuickView window;
+    QVERIFY(QQuickTest::showView(window, testFileUrl("visibleBoundToCountGreaterThanZero.qml")));
+
+    auto *listView = window.rootObject()->property("listView").value<QQuickListView *>();
+    QVERIFY(listView);
+
+    QSignalSpy countChangedSpy(listView, SIGNAL(countChanged()));
+    QVERIFY(countChangedSpy.isValid());
+
+    QTRY_COMPARE_GT_WITH_TIMEOUT(listView->count(), 1, oneSecondInMs);
+    // Using the TRY variant here as well is necessary.
+    QTRY_COMPARE_GT_WITH_TIMEOUT(countChangedSpy.count(), 1, oneSecondInMs);
+    QVERIFY(listView->isVisible());
 }
 
 QTEST_MAIN(tst_QQuickListView2)