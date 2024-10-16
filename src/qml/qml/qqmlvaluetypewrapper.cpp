097712ae79924837dd7ac02965475e152978133d
Ulf Hermann <ulf.hermann@qt.io>
2024-10-16 08:59:00 +0200

    QtQml: Protect value type lookupSetter() from identical COMDAT folding
    MSVC thinks it can deviate from the standard and fold separate functions
    into the same code block even if their addresses are taken. This makes
    the addresses equal and breaks the lookup mechanism. We set up a dilemma
    for the compiler by explicitly comparing the function addresses and
    failing when they are equal. Now the new function has "different" code
    that will however be optimized away.
    
    Pick-to: 6.8 6.5 6.2
    Task-number: QTBUG-128843
    Change-Id: Id9512397fcbb238d880ee9966d8708fc576590c7
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/qml/qml/qqmlvaluetypewrapper.cpp
+++ src/qml/qml/qqmlvaluetypewrapper.cpp
@@ -677,6 +677,16 @@ ReturnedValue QQmlValueTypeWrapper::lookupGetter(Lookup *lookup, ExecutionEngine
 bool QQmlValueTypeWrapper::lookupSetter(
         Lookup *l, ExecutionEngine *engine, Value &object, const Value &value)
 {
+    if (&QQmlValueTypeWrapper::lookupSetter == &QV4::Lookup::setterFallback) {
+        // Certain compilers, e.g. MSVC, will "helpfully" deduplicate methods that are completely
+        // equal. As a result, the pointers are the same, which wreaks havoc on the logic that
+        // decides how to retrieve the property.
+        qFatal("Your C++ compiler is broken.");
+    }
+
+    // This setter marks the presence of a value type setter lookup.
+    // It falls back to the fallback lookup when run through the interpreter, but AOT-compiled
+    // code can get clever with it.
     return QV4::Lookup::setterFallback(l, engine, object, value);
 }
 
