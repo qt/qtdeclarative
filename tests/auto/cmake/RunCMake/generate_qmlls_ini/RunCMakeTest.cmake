42644aa4768bda365306d2b2cb8f67469aa8b66f
Sami Shalayel <sami.shalayel@qt.io>
2025-03-03 14:28:12 +0100

    cmake: test .qmlls.ini change on build folder change
    Add a test using runCMake to make sure that .qmlls.ini is updated when
    the build folder changes.
    
    The test is automatic and can be run locally via:
    ctest -V -R RunCMake.generate_qmlls_ini
    
    Fixes: QTBUG-133793
    Change-Id: Icde34425e8755e04dff454c3875e567ff29fcd6e
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 36de3c1aa8278aca973e44127f0e7ec0531ef18d)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 08fe549ee2e50a26293c8ec1134a465240252955)

---
+++ tests/auto/cmake/RunCMake/generate_qmlls_ini/RunCMakeTest.cmake
@@ -0,0 +1,57 @@
+# Copyright (C) 2025 The Qt Company Ltd.
+# SPDX-License-Identifier: BSD-3-Clause
+
+include(RunCMake)
+
+function(check_qmlls_ini expectedBuild)
+    set(sourceQmllsIni "${RunCMake_SOURCE_DIR}/.qmlls.ini")
+
+    file(READ "${sourceQmllsIni}" qmllsIniContent)
+
+    string(FIND "${qmllsIniContent}" "${expectedBuild}" result)
+    if(result EQUAL -1)
+        message(FATAL_ERROR "Test failed: qmlls.ini does not contain expected ${expectedBuild} "
+            "in its content:\n${qmllsIniContent}")
+    endif()
+endfunction()
+
+function(buildProject name build)
+    set(RunCMake_TEST_BINARY_DIR "${build}")
+    run_cmake_command(${name} ${CMAKE_COMMAND} --build ${build})
+endfunction()
+
+set(build1 "${RunCMake_BINARY_DIR}/MyApplication-build1/generate_qmlls_ini")
+set(build2 "${RunCMake_BINARY_DIR}/MyApplication-build2/generate_qmlls_ini")
+set(expectedBuild1 "${RunCMake_BINARY_DIR}/MyApplication-build1")
+set(expectedBuild2 "${RunCMake_BINARY_DIR}/MyApplication-build2")
+
+# configure and build the project into build1
+set(RunCMake_TEST_BINARY_DIR "${build1}")
+run_cmake_with_options(
+    configuration1
+    "-DQT_QML_GENERATE_QMLLS_INI=ON"
+    "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+)
+set(RunCMake_TEST_NO_CLEAN 1)
+set(RunCMake_TEST_OUTPUT_MERGE 1)
+buildProject(build1 "${build1}")
+
+# check qmlls.ini
+check_qmlls_ini("${expectedBuild1}")
+
+# configure the project again into build2
+set(RunCMake_TEST_BINARY_DIR "${build2}")
+run_cmake_with_options(
+    configuration2
+    "-DQT_QML_GENERATE_QMLLS_INI=ON"
+    "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+)
+buildProject(build2 "${build2}")
+check_qmlls_ini("${expectedBuild2}")
+
+#rebuild in previous build folder and check that qmlls gets updated:
+buildProject(build1again "${build1}")
+check_qmlls_ini("${expectedBuild1}")
+
+buildProject(build2again "${build2}")
+check_qmlls_ini("${expectedBuild2}")
