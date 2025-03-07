79ce9244ee02a65a0c8ef7884aa9079f8ba3b141
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2025-03-07 10:41:40 +0100

    Remove request for activation tst_QQuickApplication::state
    tst_QQuickApplication::state is flaky on openSuSE where when changing
    the focus to nullptr, it remains on the window. This is due to some
    asynchronicity when requestActivate is call, requesting a second
    activation when the focus should be changed to nullptr.
    
    Do not call requestActivate() on the window when the platform uses xcb
    
    Unblacklist tst_QQuickapplication::state() on openSuSE as no longer
    flaky.
    
    Fixes: QTBUG-122031
    Change-Id: I66ebc02bc44c6ba27d13f5eb5f026006ddf13263
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 11404fd4ea6d1c61f49b39afaa1951961809c2f5)
    (cherry picked from commit 3fb265450b3167a5a9918334e4bc0f4b0ec180a5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 479cbe08ebad17be3be8454b98c18a223f54b544)
    Reviewed-by: Frederic Lefebvre <frederic.lefebvre@qt.io>

--- tests/auto/quick/qquickapplication/tst_qquickapplication.cpp
+++ tests/auto/quick/qquickapplication/tst_qquickapplication.cpp
@@ -165,7 +165,8 @@ void tst_qquickapplication::state()
             // Otherwise, the application can only be in two states, Active and Inactive. These are
             // triggered by window activation.
             window.show();
-            window.requestActivate();
+            if (QGuiApplication::platformName().toLower() != QLatin1String("xcb"))
+                window.requestActivate();
             QVERIFY(QTest::qWaitForWindowActive(&window));
             QCOMPARE(QGuiApplication::focusWindow(), &window);
             QCOMPARE(Qt::ApplicationState(item->property("state").toInt()), Qt::ApplicationActive);
