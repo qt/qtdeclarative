ef739f6046c66d0682f5250a4301ca8aca88655b
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2025-03-13 08:06:11 +0100

    Fix divide by zero when processing invalid arcs
    If the start point and end point are equal, we will end up with
    a divide by zero later.
    
    df97b6b2de6282bd6422f1e531a42475dadc980d in Qt Base added an
    assert which detected this.
    
    Fixes: QTBUG-134664
    Pick-to: 5.15
    Change-Id: Ifc00d988713b8495942a1445e69d35c44605dcbc
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>
    (cherry picked from commit 7ca1222296a9650f24979df57cc2c974a7e571ed)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 3e8837472e5fc9649f8eb8b21f48bac6bbb7d4e4)
    (cherry picked from commit 1d61b3c063dedbbf05ee25de27c9ddda6793cf23)

--- src/quick/util/qquickpath.cpp
+++ src/quick/util/qquickpath.cpp
@@ -1982,6 +1982,8 @@ void QQuickPathArc::addToPath(QPainterPath &path, const QQuickPathData &data)
 {
     const QPointF &startPoint = path.currentPosition();
     const QPointF &endPoint = positionForCurve(data, startPoint);
+    if (startPoint == endPoint)
+        return;
     QQuickSvgParser::pathArc(path,
             _radiusX,
             _radiusY,
