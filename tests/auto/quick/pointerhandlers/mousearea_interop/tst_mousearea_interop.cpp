c20fc36e6e7f8a08b907500440c684ee67c1dc56
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2025-03-14 10:06:32 +0100

    Set flag FramelessWindowHint to tst_mouseAreaInterop::createView
    Set the FramelessWindowHint flag to tst_mouseAreaInterop::createview.
    Removes flakiness from
    tst_mouseAreaInterop::dragHandlerInSiblingGrabFromMouseAreaViaMouse.
    
    Pick-to: 6.5
    Change-Id: I2cdbc9701a9e463bcf0a5f326df5726deb8fa093
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit 6870b8044d6363787bfe4a72ba28b4cf2e78048b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 8915f2ca8bd35f6fbe2b570984fa1c5fbb41e757)
    (cherry picked from commit eafe9e22ebea21fe003c3f0176976e0187ffb3ff)

--- tests/auto/quick/pointerhandlers/mousearea_interop/tst_mousearea_interop.cpp
+++ tests/auto/quick/pointerhandlers/mousearea_interop/tst_mousearea_interop.cpp
@@ -45,6 +45,7 @@ void tst_MouseAreaInterop::createView(QScopedPointer<QQuickView> &window, const 
     window.reset(new QQuickView);
     window->setSource(testFileUrl(fileName));
     QTRY_COMPARE(window->status(), QQuickView::Ready);
+    window.data()->setFlag(Qt::FramelessWindowHint);
     QQuickViewTestUtils::centerOnScreen(window.data());
     QQuickViewTestUtils::moveMouseAway(window.data());
 
