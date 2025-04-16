220cab5f741fe5c954cd1eb110811ea931fcf939
Sami Shalayel <sami.shalayel@qt.io>
2025-04-16 16:46:40 +0200

    qmllint: don't warn about required properties in inline components
    Amends daf57e29de918b7b4be7bb0d469db0c51d41bb07 that recurses into
    inline components when checking for required properties that were not
    set. We should only check inline components that are base types of the
    scope, and ignore all inline components in children of the scope.
    
    Fixes: QTBUG-136008
    Pick-to: 6.8
    Change-Id: I8c687ce97b3f2eac699e2fd535193197b5239ada
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    (cherry picked from commit 71967f318a9cfb617937c89963b50889b3ff6d26)

--- tests/auto/qml/qmllint/tst_qmllint.cpp
+++ tests/auto/qml/qmllint/tst_qmllint.cpp
@@ -1301,6 +1301,9 @@ void TestQmllint::dirtyQmlSnippet_data()
 
     QTest::newRow("testSnippet") << u"property int qwer: \"Hello\""_s
                                  << Result{ { { "Cannot assign literal of type string to int"_L1 } } };
+    QTest::newRow("requiredInInlineComponent")
+            << u"Item { component Foo: Item { required property var bla; } } Foo {}"_s
+            << Result{ { { "Component is missing required property bla from Foo"_L1, 1, 61 } } };
 }
 
 void TestQmllint::dirtyQmlSnippet()
@@ -1323,6 +1326,10 @@ void TestQmllint::cleanQmlSnippet_data()
     QTest::addColumn<QString>("code");
 
     QTest::newRow("testSnippet") << u"property int qwer: 123"_s;
+    QTest::newRow("requiredInComponent")
+            << u"Item { Component { id: comp; required property var bla; } }"_s;
+    QTest::newRow("requiredInInlineComponent")
+            << u"Item { component Foo: Item { required property var bla; } }"_s;
 }
 
 void TestQmllint::cleanQmlSnippet()
