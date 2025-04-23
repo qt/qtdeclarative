bc4bc3ab9975a3b7ad4a188207319d9cf239cd1d
Olivier De Cannière <olivier.decanniere@qt.io>
2025-04-23 10:58:49 +0200

    qml-tool: Treat all arguments passed after "--" as positional
    The argument "-a" passed after "--" was being interpreted as the -a
    option of the qml tool itself instead of being passed along to the qml
    program as a positional argument.
    
    Fixes: QTBUG-136120
    Change-Id: I602aea84e4766abeb47adce0f739f12315a70b24
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 6ac4833a5172e60611987b02145de721061a2906)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 84b4ad481dffaffd83f99595955a3d5901453426)
    (cherry picked from commit 965b9399c56075fba25837337084893af3703d3e)
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>

--- tests/auto/qml/qml/tst_qml.cpp
+++ tests/auto/qml/qml/tst_qml.cpp
@@ -6,6 +6,8 @@
 #include <QtCore/qprocess.h>
 #include <QtQuickTestUtils/private/qmlutils_p.h>
 
+#include <QTemporaryFile>
+
 class tst_qml : public QQmlDataTest
 {
     Q_OBJECT
@@ -15,6 +17,7 @@ public:
 private slots:
     void initTestCase() override;
     void nonWindow();
+    void extraPositionalArguments();
 
 private:
     QString qmlPath;
@@ -40,6 +43,38 @@ void tst_qml::nonWindow()
     qml.start(qmlPath, { testFile("nonWindow.qml") });
     QVERIFY(qml.waitForFinished());
     QCOMPARE(qml.exitCode(), 0); // Should not exit with code 2
+}
+
+void tst_qml::extraPositionalArguments()
+{
+    QProcess qml;
+    QStringList args;
+
+    QTemporaryFile f;
+    QVERIFY(f.open());
+    QVERIFY(f.write(R"(import QtQml
+
+QtObject {
+    Component.onCompleted: {
+        let s = ""
+        for (let i = 2; i < Qt.application.arguments.length; ++i)
+            s += Qt.application.arguments[i].substring(1)
+        console.log(s)
+        Qt.quit()
+    }
+}
+)"));
+    f.flush();
+
+    args << f.fileName();
+    args << "--";
+    for (char c = 'a'; c <= 'z'; ++c)
+        args << u'-' + QString(c);
+
+    qml.start(qmlPath, args);
+    QVERIFY(qml.waitForFinished());
+    QVERIFY(qml.exitStatus() == QProcess::NormalExit && qml.exitCode() == 0);
+    QVERIFY(qml.readAllStandardError().contains("abcdefghijklmnopqrstuvwxyz"));
 }
 
 QTEST_MAIN(tst_qml)
