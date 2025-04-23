7b0a72af0f960e21d2dbb4e8482eaacf2c08fa53
Olivier De Cannière <olivier.decanniere@qt.io>
2025-04-23 14:18:40 +0200

    Compiler: Optimize checking required properties
    Checking required properties had become quite slow, especially on huge
    files, after daf57e29de918b7b4be7bb0d469db0c51d41bb07.
    
    Immediatelly checking for the presence of a property binding speeds
    things up a lot. fluentwinui3/Slider.qml, which reference a 20k line
    file, now compiles almost instantly again instead of talking several
    tens of seconds.
    
    Amends daf57e29de918b7b4be7bb0d469db0c51d41bb07
    
    Fixes: QTBUG-135244
    Change-Id: I4391db27f391b179ad03dabadc910f23fa932037
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit a6bacbbbb97849df0a185791ec0cf623b0d28717)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 7a6d4740ac5ca180a5893f391b8c8d856a8ef997)
    (cherry picked from commit fe10687e2a6b1d7a80c9c81a7faa7931211f85be)

--- src/qmlcompiler/qqmljsimportvisitor.cpp
+++ src/qmlcompiler/qqmljsimportvisitor.cpp
@@ -800,8 +800,8 @@ void QQmlJSImportVisitor::checkRequiredProperties()
         }
     }
 
-    const auto isInComponent = [this](const QQmlJSScope::ConstPtr &requiredScope) {
-        const auto compType = m_rootScopeImports.type(u"Component"_s).scope;
+    const auto compType = m_rootScopeImports.type(u"Component"_s).scope;
+    const auto isInComponent = [&](const QQmlJSScope::ConstPtr &requiredScope) {
         for (auto s = requiredScope; s; s = s->parentScope()) {
             if (s->isWrappedInImplicitComponent() || s->baseType() == compType)
                 return true;
@@ -921,6 +921,8 @@ void QQmlJSImportVisitor::checkRequiredProperties()
                 for (auto propertyIt = ownProperties.constBegin();
                      propertyIt != ownProperties.constEnd(); ++propertyIt) {
                     const QString propName = propertyIt.key();
+                    if (descendant->hasOwnPropertyBindings(propName))
+                        continue;
 
                     QQmlJSScope::ConstPtr prevRequiredScope;
                     for (QQmlJSScope::ConstPtr requiredScope : std::as_const(scopesToSearch)) {
