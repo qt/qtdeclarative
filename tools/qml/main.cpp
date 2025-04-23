84b4ad481dffaffd83f99595955a3d5901453426
Olivier De Cannière <olivier.decanniere@qt.io>
2025-04-23 10:58:49 +0200

    qml-tool: Treat all arguments passed after "--" as positional
    The argument "-a" passed after "--" was being interpreted as the -a
    option of the qml tool itself instead of being passed along to the qml
    program as a positional argument.
    
    Fixes: QTBUG-136120
    Pick-to: 6.8 6.5
    Change-Id: I602aea84e4766abeb47adce0f739f12315a70b24
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 6ac4833a5172e60611987b02145de721061a2906)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tools/qml/main.cpp
+++ tools/qml/main.cpp
@@ -316,6 +316,9 @@ static void getAppFlags(int argc, char **argv)
 {
 #ifdef QT_GUI_LIB
     for (int i=0; i<argc; i++) {
+        if (strcmp(argv[i], "--"))
+            return; // After "--", arguments are interpreted as positional and not as options.
+
         if (!strcmp(argv[i], "--apptype") || !strcmp(argv[i], "-a") || !strcmp(argv[i], "-apptype")) {
             applicationType = QmlApplicationTypeUnknown;
             if (i+1 < argc) {
