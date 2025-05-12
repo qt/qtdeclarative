469c3cbc6a20f9ee10d379f0de5bc8001ca3b0c9
Joni Poikelin <joni.poikelin@qt.io>
2025-05-12 10:25:00 +0300

    Fix Rectangle with gradient and color set to transparent
    Fixes: QTBUG-134403
    Pick-to: 6.8
    Change-Id: Ibfbe6bc513d1e279d3d1d21661f8041500f02570
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit 7d3c2da9ffefeaf99d69e0faf88b65068579a6ab)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 83bbc685a22d423c32bbb830964cdfd675d5dd4f)

--- src/quick/items/qquickrectangle.cpp
+++ src/quick/items/qquickrectangle.cpp
@@ -780,7 +780,7 @@ QSGNode *QQuickRectangle::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData 
     Q_D(QQuickRectangle);
 
     if (width() <= 0 || height() <= 0
-            || (d->color.alpha() == 0 && (!d->pen || d->pen->width() == 0 || d->pen->color().alpha() == 0))) {
+            || (d->gradient.isUndefined() && d->color.alpha() == 0 && (!d->pen || d->pen->width() == 0 || d->pen->color().alpha() == 0))) {
         delete oldNode;
         return nullptr;
     }
