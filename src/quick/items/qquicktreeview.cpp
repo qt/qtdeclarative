35fc5bfe1610a746d743b33db3fe739c46927208
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2025-05-14 11:13:37 +0200

    QQuickTableView: use correct model when checking if an item is editable
    When TableView needs to resolve if a model item is editable, it needs
    to call QAbstractItemModel::flags(modelIndex). But as it stood, it would
    call this function on the internal proxy model set by TreeModel, using a
    QModelIndex from the application model (tappedIndex).
    
    Since tappedIndex belongs to the application model, this patch
    makes sure that we ask the application model, and not the proxy
    model, if the given model item is editable.
    
    Fixes: QTBUG-136492
    Change-Id: I0011c46f1b07470ebcb83d93aa3d592e7339345e
    Reviewed-by: Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
    (cherry picked from commit e3ef5ef2f19ad5ee6c920122939172695ed311f5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 7874babe93fdafdebf882e9c94dfe90d7cb50cb4)
    (cherry picked from commit fbbdeac6040d60b81577592e40019d8a6fc5722d)
    (cherry picked from commit f9d0c5ff80f2fd4da82ec9d3b2464bca693f3349)

--- src/quick/items/qquicktreeview.cpp
+++ src/quick/items/qquicktreeview.cpp
@@ -650,8 +650,10 @@ void QQuickTreeView::keyPressEvent(QKeyEvent *event)
         break;
     }
 
-    if (!event->isAccepted())
+    if (!event->isAccepted()) {
+        event->accept();
         QQuickTableView::keyPressEvent(event);
+    }
 }
 
 QT_END_NAMESPACE
