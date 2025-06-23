27374e477f35712b12c9f219c12c8772702b73a6
Sze Howe Koh <szehowe.koh@gmail.com>
2025-06-23 09:34:32 +0800

    QQuickVelocityCalculator: Remove unused QElapsedTimer
    This is a helper class for QQuickDrawer and QQuickSwipeDelegate. Neither
    of them make use of the default parameters in start/stopMeasuring(), so
    the QElapsedTimer is never used. Remove it to simplify business logic.
    
    Change-Id: I9dfda4ebffd076b3ec05de8a96702e507eb3fcd9
    Pick-to: 6.8
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit ad5063893bbaed61d5d8e45572484e212918f5f5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 66213fb0ae999254137e8ffb567d1c63a5c1ea87)

--- src/quicktemplates/qquickvelocitycalculator.cpp
+++ src/quicktemplates/qquickvelocitycalculator.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 The Qt Company Ltd.
+// Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qquickvelocitycalculator_p_p.h"
@@ -28,23 +28,18 @@ QT_BEGIN_NAMESPACE
 void QQuickVelocityCalculator::startMeasuring(const QPointF &point1, qint64 timestamp)
 {
     m_point1 = point1;
-
-    if (timestamp != 0)
-        m_point1Timestamp = timestamp;
-    else
-        m_timer.start();
+    m_point1Timestamp = timestamp;
 }
 
 void QQuickVelocityCalculator::stopMeasuring(const QPointF &point2, qint64 timestamp)
 {
-    if (timestamp == 0 && !m_timer.isValid()) {
+    if (timestamp == 0) {
         qWarning() << "QQuickVelocityCalculator: a call to stopMeasuring() must be preceded by a call to startMeasuring()";
         return;
     }
 
     m_point2 = point2;
-    m_point2Timestamp = timestamp != 0 ? timestamp : m_timer.elapsed();
-    m_timer.invalidate();
+    m_point2Timestamp = timestamp;
 }
 
 void QQuickVelocityCalculator::reset()
@@ -53,15 +48,14 @@ void QQuickVelocityCalculator::reset()
     m_point2 = QPointF();
     m_point1Timestamp = 0;
     m_point2Timestamp = 0;
-    m_timer.invalidate();
 }
 
 QPointF QQuickVelocityCalculator::velocity() const
 {
-    if ((m_point2Timestamp == 0 || m_point1Timestamp == m_point2Timestamp) && !m_timer.isValid())
+    if (m_point2Timestamp == 0 || m_point1Timestamp == m_point2Timestamp)
         return QPointF();
 
-    const qreal secondsElapsed = (m_point2Timestamp != 0 ? m_point2Timestamp - m_point1Timestamp : m_timer.elapsed()) / 1000.0;
+    const qreal secondsElapsed = (m_point2Timestamp - m_point1Timestamp) / 1000.0;
     const QPointF distance = m_point2 - m_point1;
     return distance / secondsElapsed;
 }
