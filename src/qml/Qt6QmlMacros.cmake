5aced7fa7e160bbb3e25aa24d08dbacde3e8e5f8
Kai Köhne <kai.koehne@qt.io>
2025-07-01 16:28:05 +0200

    qt_generate_deploy_app_script: Handle executable names with spaces
    Task-number: QTBUG-138155
    Change-Id: Ia06b2b88dc309cd12f6874ba984b37c294670659
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    (cherry picked from commit 847afd03cd4ebc9b87bc51573448cc5ac6fcf4bb)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2e9a37eb8fbeb241b46c8febf456f9708a566292)
    (cherry picked from commit c7723e6c24266bd2c59496f5c68b9ef4b9d158de)
    (cherry picked from commit cae573bd7078897323173302e7dd49172a9f78e7)
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/qml/Qt6QmlMacros.cmake
+++ src/qml/Qt6QmlMacros.cmake
@@ -3262,7 +3262,7 @@ function(qt6_generate_deploy_qml_app_script)
 qt_deploy_qml_imports(TARGET ${arg_TARGET} PLUGINS_FOUND plugins_found)
 if(NOT DEFINED __QT_DEPLOY_POST_BUILD)
     qt_deploy_runtime_dependencies(
-        EXECUTABLE $<TARGET_FILE_NAME:${arg_TARGET}>.app
+        EXECUTABLE \"$<TARGET_FILE_NAME:${arg_TARGET}>.app\"
         ADDITIONAL_MODULES \${plugins_found}
     ${common_deploy_args})
 endif()")
@@ -3290,7 +3290,7 @@ endif()")
             CONTENT "
 qt_deploy_qml_imports(TARGET ${arg_TARGET} PLUGINS_FOUND plugins_found)
 qt_deploy_runtime_dependencies(
-    EXECUTABLE $<TARGET_FILE:${arg_TARGET}>
+    EXECUTABLE \"$<TARGET_FILE:${arg_TARGET}>\"
     ADDITIONAL_MODULES \${plugins_found}
     GENERATE_QT_CONF
 ${common_deploy_args})")
@@ -3303,7 +3303,7 @@ ${common_deploy_args})")
             CONTENT "
 qt_deploy_qml_imports(TARGET ${arg_TARGET} PLUGINS_FOUND plugins_found)
 qt_deploy_runtime_dependencies(
-    EXECUTABLE $<TARGET_FILE:${arg_TARGET}>
+    EXECUTABLE \"$<TARGET_FILE:${arg_TARGET}>\"
     ADDITIONAL_MODULES \${plugins_found}
     GENERATE_QT_CONF
 ${common_deploy_args})")
