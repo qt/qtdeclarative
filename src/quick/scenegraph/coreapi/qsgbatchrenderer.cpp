e7cd1c21990944f7cd0bd7c3602664e218679c60
Dominik Holland <dominik.holland@qt.io>
2025-07-10 11:59:10 +0200

    Correctly update the current projection matrix for RenderNodes
    Pick-to: 6.8
    Change-Id: Idc6257d5ec3942a00869daf737613165c0e60ebc
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>
    (cherry picked from commit 55e4cca1c9ebf27bdd168821c8fec5f057e398cf)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 60c1fae9c837c090038944d2add2f5cb977473cb)

--- src/quick/scenegraph/coreapi/qsgbatchrenderer.cpp
+++ src/quick/scenegraph/coreapi/qsgbatchrenderer.cpp
@@ -4064,6 +4064,14 @@ bool Renderer::prepareRhiRenderNode(Batch *batch, PreparedRenderBatch *renderBat
     if (Q_UNLIKELY(debug_render()))
         qDebug() << " -" << batch << "rendernode";
 
+    const int viewCount = projectionMatrixCount();
+    m_current_projection_matrix.resize(viewCount);
+    for (int viewIndex = 0; viewIndex < viewCount; ++viewIndex)
+        m_current_projection_matrix[viewIndex] = projectionMatrix(viewIndex);
+    m_current_projection_matrix_native_ndc.resize(projectionMatrixWithNativeNDCCount());
+    for (int viewIndex = 0; viewIndex < projectionMatrixWithNativeNDCCount(); ++viewIndex)
+        m_current_projection_matrix_native_ndc[viewIndex] = projectionMatrixWithNativeNDC(viewIndex);
+
     Q_ASSERT(batch->first->isRenderNode);
     RenderNodeElement *e = static_cast<RenderNodeElement *>(batch->first);
 
@@ -4112,7 +4120,6 @@ bool Renderer::prepareRhiRenderNode(Batch *batch, PreparedRenderBatch *renderBat
 
     rd->m_rt = renderTarget();
 
-    const int viewCount = projectionMatrixCount();
     rd->m_projectionMatrix.resize(viewCount);
     for (int viewIndex = 0; viewIndex < viewCount; ++viewIndex)
         rd->m_projectionMatrix[viewIndex] = projectionMatrix(viewIndex);
