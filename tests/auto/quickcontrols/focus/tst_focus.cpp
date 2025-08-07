0bfa3632bd025ee5a8bc4233ffdf4a975741732c
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2025-08-07 12:07:04 +0200

    Remove request for activation tst_focus::policy to fix flakiness
    tst_focus::policy is flaky on openSuSE 15.6 due to a redundant call of
    requestActivate.
    
    Remove requestActivate as it is already ensure that the window is active
    before moving forward.
    
    Unblacklist tst_focus::policy on openSuSE-leap
    
    Pick-to: 6.8 6.5
    Fixes: QTBUG-78261
    Change-Id: If56d23d8d79aeecd203fe8cf51de031b80d3923a
    Reviewed-by: Mitch Curtis <mitch.curtis@qt.io>
    (cherry picked from commit e0b9c581b815996ce1b278e3cb5fd94ab5489f8d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit fa80b7e239443e90a8a1ab5495c963b8c27c9fd1)

--- tests/auto/quickcontrols/focus/tst_focus.cpp
+++ tests/auto/quickcontrols/focus/tst_focus.cpp
@@ -145,7 +145,6 @@ void tst_focus::policy()
     QVERIFY(!control->hasVisualFocus());
 
     window->show();
-    window->requestActivate();
     QVERIFY(QTest::qWaitForWindowActive(window.data()));
 
     control->setFocusPolicy(Qt::NoFocus);
