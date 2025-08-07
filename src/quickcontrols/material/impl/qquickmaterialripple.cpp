ed5a70f518319131a71646508fe3fcf26e612c07
Mitch Curtis <mitch.curtis@qt.io>
2025-08-07 14:46:27 +0800

    Material: don't set individual radii properties on rectangle SG node
    As of 727d2350391512cb8449f9db781b4e1041a8068c this isn't necessary and
    turns our lovely circle into a square.
    
    Fixes: QTBUG-136958
    Change-Id: I96d3d8e3e2061a3a2b387bf8e35a9bcc9fd3e9ed
    Reviewed-by: Jani Heikkinen <jani.heikkinen@qt.io>

--- src/quickcontrols/material/impl/qquickmaterialripple.cpp
+++ src/quickcontrols/material/impl/qquickmaterialripple.cpp
@@ -88,10 +88,6 @@ void QQuickMaterialRippleWaveNode::updateCurrentTime(int time)
     Q_ASSERT(rectNode->type() == QSGNode::GeometryNodeType);
     rectNode->setRect(QRectF(0, 0, m_value, m_value));
     rectNode->setRadius(m_value / 2);
-    rectNode->setTopLeftRadius(-1);
-    rectNode->setTopRightRadius(-1);
-    rectNode->setBottomLeftRadius(-1);
-    rectNode->setBottomRightRadius(-1);
     rectNode->update();
 }
 
@@ -178,10 +174,6 @@ void QQuickMaterialRippleBackgroundNode::sync(QQuickItem *item)
         rectNode->setRect(QRectF(0, 0, w, h));
         rectNode->setRadius(ripple->clipRadius());
     }
-    rectNode->setTopLeftRadius(-1);
-    rectNode->setTopRightRadius(-1);
-    rectNode->setBottomLeftRadius(-1);
-    rectNode->setBottomRightRadius(-1);
 
     setMatrix(matrix);
     rectNode->setColor(ripple->color());
