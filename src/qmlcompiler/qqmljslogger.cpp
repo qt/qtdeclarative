8bb96b67aeb9f9ca3e4b2723d3da5adf1c90b24d
Olivier De Cannière <olivier.decanniere@qt.io>
2025-08-15 14:23:52 +0200

    QQmlJSLogger: Also print out hints of fixSuggestions
    It otherwise went unused.
    
    Pick-to: 6.8 6.5
    Change-Id: I255a53247d0cfce29d958425d1f8f2895eb093e7
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 319dea79241ec1b7f3fc4f6acfefbe441c91d913)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b97486de82d4aaba11e8f704e501c39c425dafea)

--- src/qmlcompiler/qqmljslogger.cpp
+++ src/qmlcompiler/qqmljslogger.cpp
@@ -454,13 +454,15 @@ void QQmlJSLogger::printFix(const QQmlJSFixSuggestion &fixItem)
     int tabCount = issueLocationWithContext.beforeText().count(u'\t');
 
     // Do not draw location indicator for multiline replacement strings
-    if (replacementString.contains(u'\n'))
-        return;
+    if (!replacementString.contains(u'\n')) {
+        m_output.write(u" "_s.repeated(
+                               issueLocationWithContext.beforeText().size() - tabCount)
+                       + u"\t"_s.repeated(tabCount)
+                       + u"^"_s.repeated(replacement.size()) + u'\n');
+    }
 
-    m_output.write(u" "_s.repeated(
-                           issueLocationWithContext.beforeText().size() - tabCount)
-                   + u"\t"_s.repeated(tabCount)
-                   + u"^"_s.repeated(replacement.size()) + u'\n');
+    if (!fixItem.hint().isEmpty())
+        m_output.write("      "_L1 + fixItem.hint());
 }
 
 QQmlJSFixSuggestion::QQmlJSFixSuggestion(const QString &fixDescription,
