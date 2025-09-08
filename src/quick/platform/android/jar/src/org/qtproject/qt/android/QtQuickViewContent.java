40de1bbaa24cdc18180370aae5ea2d1a226f3bbd
Nicholas Bennett <nicholas.bennett@qt.io>
2025-09-08 15:53:21 +0300

    Docs: Add brief to QtQuickViewContent class comment
    @since seems to have been stoppping the comment entirely.
    
    Task-number: QTBUG-139688
    Change-Id: Iaf0e915a2304d1f25a7d87a82b94d06988d988e7
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>
    (cherry picked from commit d0b65b954ffa65b0e38703373ab456b06f524a78)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 2b540abef57e19ff2661b4b839d34af7319862b9)

--- src/quick/platform/android/jar/src/org/qtproject/qt/android/QtQuickViewContent.java
+++ src/quick/platform/android/jar/src/org/qtproject/qt/android/QtQuickViewContent.java
@@ -9,12 +9,12 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * @Since 6.8
+ * The QtQuickViewContent represents a QML component that can be loaded by a QtQuickView instance.
  *
- * The QtQuickViewContent represents a QML component that can be loaded by a QtQuickView instance
  * This abstract class should be extended to be used by a QtQuickView. It provides QtQuickView with
  * essential information to load the QML component it represents.
  * It also offers convenient methods for seamless interaction with the QtQuickView that loads it.
+ * @Since 6.8
  **/
 public abstract class QtQuickViewContent
 {
