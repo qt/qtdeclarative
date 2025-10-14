7936c866a022ca53082dd665e116d94984c08b89
Santhosh Kumar <santhosh.kumar.selvaraj@qt.io>
2025-10-14 15:46:25 +0200

    Docs: Correct the code snippet for the QML SFPM
    Fixes: QTBUG-141151
    Change-Id: I093ee5d42e2e4c98cb84fe6ad189b8f582bd25e4
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit cbbc8356f407d2bbf1a8832265d01c5b145cb99c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5f0df8058b1b923e0861561709989f15a14b5fc5)

--- src/qmlmodels/sfpm/qqmlsortfilterproxymodel.cpp
+++ src/qmlmodels/sfpm/qqmlsortfilterproxymodel.cpp
@@ -46,17 +46,17 @@ Q_LOGGING_CATEGORY (lcSortFilterProxyModel, "qt.qml.sortfilterproxymodel")
         id: sfpm
         model: processModel
         sorters: [
-            RoleSorter: {
+            RoleSorter {
                 roleName: "user"
                 priority: 0
             },
-            RoleSorter: {
+            RoleSorter {
                 roleName: "pid"
                 priority: 1
             }
         ]
         filters: [
-            FunctionFilter: {
+            FunctionFilter {
                 component RoleData: QtObject { property qreal cpuUsage }
                 function filter(data: RoleData) : bool {
                     return (data.cpuUsage > 90)
