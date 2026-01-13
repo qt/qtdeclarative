b870fd5e0d48a02a676f115958b14b6426a5c788
Anton Kudryavtsev <anton.kudryavtsev@vk.team>
2026-01-13 15:03:22 +0300

    QQuickItem: avoid virtual calls in dtor
    Violates rule OOP50-CPP of the SEI Cert C++ Coding Standard. (https://wiki.sei.cmu.edu/confluence/display/cplusplus/OOP50-CPP.+Do+not+invoke+virtual+functions+from+constructors+or+destructors)
    
    I'm not 100% sure of the code path, but stack trace of attached bug looks like:
    
    QQuickItem::~QQuickItem -> QuickItem::setParentItem -> QQuickItemPrivate::setEffectiveVisibleRecur ->  QAccessible::updateAccessibility -> QCocoaAccessibility::notifyAccessibilityUpdate -> QAccessibleQuickItem::interface_cast -> QAccessibleQuickItem::role() ->
    QQuickItemPrivate::effectiveAccessibleRole ->
    QQuickItemPrivate::accessibleRole ->
    QQuickControlPrivate::accessibleRole -> QQuickControl::accessibleRole
    
    After we have sent ObjectDestroyed, it doesn't make sense to send
    further a11y events, so we can just set isAccessible = false
    and don't trigger a11y anymore in dtor.
    
    Fixes: QTBUG-139107
    Change-Id: If3615693b07fea77bbda3fb9180252a7ac1f61b2
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@qt.io>
    (cherry picked from commit 3c70e7fa7db4fc3b255bfc9eab69aa14aac7fd8f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quick/items/qquickitem.cpp
+++ src/quick/items/qquickitem.cpp
@@ -2392,6 +2392,8 @@ QQuickItem::~QQuickItem()
 #if QT_CONFIG(accessibility)
     if (QGuiApplicationPrivate::is_app_running && !QGuiApplicationPrivate::is_app_closing && QAccessible::isActive())
         QAccessibleCache::instance()->sendObjectDestroyedEvent(this);
+
+    d->isAccessible = false;
 #endif
 
     if (d->windowRefCount > 1)
