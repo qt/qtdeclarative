6bd71599e7a122a6edf1f5b07a3187b2e8cbac49
Fabian Kosmale <fabian.kosmale@qt.io>
2026-02-03 15:03:26 +0100

    CRA review: mark files in qml/memory folder
    Mark everything as critical. While QML code is expected to be trusted,
    this does not hold true for data (like images and text) used in QML.
    Given that any mistake in the memory subsystem could result in some data
    being interpreted as code[1], we should take extra caution.
    
    [1] This is currently limited though, as data tends to be stored on
    the C++ heap, with only references to it on the JS heap. Nevertheless,
    it can still happen, e.g. with SharedArrayBuffer.
    
    Pick-to: 6.10 6.8
    Task-number: QTBUG-136194
    Change-Id: Iac784fa55fdc29eaf493706e280ff8b853ee8162
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 6c7f8e1cbcd71623661f451ee4329b42696ddad0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qml/memory/qv4heap_p.h
+++ src/qml/memory/qv4heap_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:low-level-memory-management
 #ifndef QV4HEAP_P_H
 #define QV4HEAP_P_H
 
