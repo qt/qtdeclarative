11d82771bfb353cc7743287c04552b88ff80fe73
Sami Shalayel <sami.shalayel@qt.io>
2026-02-06 16:39:47 +0100

    qqmlincubator: add QT_(BEGIN|END)_NAMESPACE
    Spotted while adding the security headers, it seems that the missing
    macros were an oversight.
    
    Pick-to: 6.8
    Change-Id: I97119b3b13941fc9919293aa5f9907b03b79cf37
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit fef130d263c9aa65399b545dad10a120620940f8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 57da118f81f87bb4093555eaf176cc9c8e24901d)

--- src/qml/qml/qqmlincubator.cpp
+++ src/qml/qml/qqmlincubator.cpp
@@ -9,6 +9,8 @@
 #include "qqmlobjectcreator_p.h"
 #include <private/qqmlcomponent_p.h>
 
+QT_BEGIN_NAMESPACE
+
 void QQmlEnginePrivate::incubate(
         QQmlIncubator &i, const QQmlRefPointer<QQmlContextData> &forContext)
 {
@@ -808,3 +810,4 @@ QQmlIncubator::Status QQmlIncubatorPrivate::calculateStatus() const
         return QQmlIncubator::Null;
 }
 
+QT_END_NAMESPACE
