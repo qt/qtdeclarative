3883e32585b9724b3f1185408fd5e4ec25d22169
Sami Shalayel <sami.shalayel@qt.io>
2026-02-06 11:20:37 +0100

    CRA: review qtdeclarative/src/qml/qml
    qqmlstringconverters_p.h was already marked by
    5a6edaea35f399a9b4b097640929df307b10a7db as a data-parser.
    
    qqmlpluginimporter.cpp loads plugins by filenames from non-dedicated
    plugin folders, and executes the type registration function of those
    plugins. The plugin filenames are obtained from qmldir files, which are
    supposed to be trusted, but we should nevertheless be careful to not
    pick up plugins from elsewhere. Therefore, mark qqmlpluginimporter.cpp
    as critical.
    
    All other files get default significance: they do no parsing, no code
    execution, no cryptography and no network protocols
    implementation.
    
    Notes:
    * qqmlxmlhttprequest.cpp seems to use QNetworkAccessManager for all
      of its networking activites, and all parsing is done via
      QXmlStreamReader and the Json parser at jsruntime/qv4jsonobject.cpp.
      qv4jsonobject.cpp is already marked as critical. Therefore, don't mark
      qqmlxmlhttprequest.cpp as critical.
    * qqmllocale.cpp seems to have lots of "data-parser-ressembling" checks
      in some functions that take a QV4::Value* argv, but the
      "security-boundary" should be at the code that creates the QV4::Value
      from the user's QML code.
    * qqmlglobal.cpp has stubs for colorFromString and rgbaFromString that
      don't actually parse anything and always return an empty variant.
    * qqmlcustomparser.cpp does not parse anything, its a base class
      without any parsing implementation.
    
    Pick-to: 6.10 6.8
    Fixes: QTBUG-143932
    Change-Id: I40dfa37f037de7dfbcdcfe48bb35116d633ae1d3
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit 3f57095a30743159ceace0ec39822795092540c7)

--- src/qml/qml/qqmlnotifyingblob_p.h
+++ src/qml/qml/qqmlnotifyingblob_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2025 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant
 
 #ifndef QQMLNOTIFYINGBLOB_P_H
 #define QQMLNOTIFYINGBLOB_P_H
