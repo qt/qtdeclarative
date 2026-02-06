620a66d4b17943dd7299ec42f28c44ae2bfe4e06
Sami Shalayel <sami.shalayel@qt.io>
2026-02-06 09:16:52 +0100

    CRA: mark src/qmltyperegistrar files
    src/qmltyperegistrar/qqmltyperegistrarutils.cpp gets critical
    significance because it contains code to parse qt.conf files. All other
    files that work on potential user input use QJson, QCbor, or the QML
    parser classes, and therefore get default significance.
    
    Pick-to: 6.10 6.8
    Fixes: QTBUG-136208
    Change-Id: Ifbe65276c90c296507e5f77100536a5a34e4cbe7
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit c60fba4813fa066c98fe5a098dffd7ef31f32425)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qmltyperegistrar/qqmltyperegistrarutils.cpp
+++ src/qmltyperegistrar/qqmltyperegistrarutils.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:critical reason:data-parser
 
 #include "qqmltyperegistrarutils_p.h"
 
