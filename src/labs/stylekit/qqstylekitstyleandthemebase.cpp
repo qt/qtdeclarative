a64a5e01bd2d7965a3f5c2776dc08809b8b23684
Richard Moe Gustavsen <richard.gustavsen@qt.io>
2026-02-13 10:42:04 +0100

    StyleKit: add docs for AbstractStyle
    Add documentation for AbstractStyle. This QML contains
    font and palette API that both Style and Theme have in
    common.
    
    (Note: the QML name is AbstractStyle, even if the c++ class
     that implements it is named QQStyleKitStyleAndThemeBase).
    
    Change-Id: I0fb555ed8a95c4eb237366c8e04855d3d1561945
    Reviewed-by: Doris Verria <doris.verria@qt.io>
    (cherry picked from commit 043534d894a186a7048c6774cc6c77c14e6aa3d2)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 6daa5fc1ea8f6b862297c3a7366da057036275dc)

--- src/labs/stylekit/qqstylekitstyleandthemebase.cpp
+++ src/labs/stylekit/qqstylekitstyleandthemebase.cpp
@@ -5,16 +5,64 @@
 
 QT_BEGIN_NAMESPACE
 
+/*!
+    \qmltype AbstractStyle
+    \inqmlmodule Qt.labs.StyleKit
+    \inherits AbstractStylableControls
+    \brief Abstract base type with properties common to both \l Style and \l Theme.
+
+    AbstractStyle contains properties, such and fonts and palettes, that are
+    common to both a \l Style and a \l Theme.
+
+    \labs
+*/
+
 QQStyleKitStyleAndThemeBase::QQStyleKitStyleAndThemeBase(QObject *parent)
     : QQStyleKitControls(parent)
 {
 }
 
+/*!
+    \qmlproperty StyleFont AbstractStyle::fonts
+
+    Grouped property for configuring per-control \l [QML]{font}{fonts}.
+    Fonts can be set for individual control types such as button,
+    textField, or label.
+
+    \qml
+    Style {
+        fonts {
+            system.pixelSize: 14
+            button.bold: true
+            textField.family: "Monospace"
+        }
+    }
+    \endqml
+*/
 QQStyleKitFont *QQStyleKitStyleAndThemeBase::fonts()
 {
     return &m_fonts;
 }
 
+/*!
+    \qmlproperty StylePalette AbstractStyle::palettes
+
+    Grouped property for configuring per-control \l [QML]{Palette}{palettes}.
+    Palettes can be set system-wide or for individual control types.
+
+    \qml
+    light: Theme {
+        palettes {
+            system {
+                window: "gainsboro"
+                windowText: "black"
+            }
+            button.buttonText: "black"
+            textField.text: "#4e4e4e"
+        }
+    }
+    \endqml
+*/
 QQStyleKitPalette *QQStyleKitStyleAndThemeBase::palettes()
 {
     return &m_palettes;
