bcd40fc33944bf35e4e1e84fb93f29257ab79801
Sergio Martins <sergio.martins@kdab.com>
2026-02-15 00:25:42 +0000

    Fix undefined behaviour when initializing TextArea
    While TextArea was still partially constructed (TextEdit constructor
    still in progress), it would trigger a series of signals resulting
    in QQuickTextAreaPrivate::implicitHeightChanged() which dereferences
    q_func().
    
    qquicktextarea_p_p.h:36:5: runtime error: downcast of address 0x7c56b22055c0 which does not point to an object of type 'QQuickTextArea'
    0x7c56b22055c0: note: object is of type 'QQuickTextEdit'
     00 00 00 00  c0 37 ca a0 46 7b 00 00  80 77 1f b2 d6 7c 00 00  48 39 ca a0 46 7b 00 00  00 00 00 00
    
    The fix is to not emit those signals while we're being constructed,
    nobody could catch them anyway, as they're not connected to them.
    
    Task-number: QTBUG-144225
    Change-Id: Ie3b7a10aa3f3be77c16663c3a384dd65e0a996de
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit 992259cd23d685f9800159f59b40231a6950daf5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 977de2409e2079773fb3b52f9ae6373f058e688f)

--- src/quicktemplates/qquicktextarea.cpp
+++ src/quicktemplates/qquicktextarea.cpp
@@ -408,6 +408,10 @@ qreal QQuickTextAreaPrivate::getImplicitHeight() const
 
 void QQuickTextAreaPrivate::implicitWidthChanged()
 {
+    if (!initialized) {
+        QQuickTextEditPrivate::implicitWidthChanged();
+        return;
+    }
     Q_Q(QQuickTextArea);
     QQuickItemPrivate::implicitWidthChanged();
     emit q->implicitWidthChanged3();
@@ -415,6 +419,10 @@ void QQuickTextAreaPrivate::implicitWidthChanged()
 
 void QQuickTextAreaPrivate::implicitHeightChanged()
 {
+    if (!initialized) {
+        QQuickTextEditPrivate::implicitHeightChanged();
+        return;
+    }
     Q_Q(QQuickTextArea);
     QQuickItemPrivate::implicitHeightChanged();
     emit q->implicitHeightChanged3();
@@ -492,6 +500,7 @@ QQuickTextArea::QQuickTextArea(QQuickItem *parent)
     : QQuickTextEdit(*(new QQuickTextAreaPrivate), parent)
 {
     Q_D(QQuickTextArea);
+    d->initialized = true;
     setActiveFocusOnTab(true);
     setAcceptedMouseButtons(Qt::AllButtons);
     d->setImplicitResizeEnabled(false);
