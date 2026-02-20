cdb9fdcc96523e2d6a7806422e240405472f9a85
Mate Barany <mate.barany@qt.io>
2026-02-20 16:47:00 +0100

    Add testcase that inits QQmlTreeModel from JSON
    We have a manual test for TableModel that does the same, but the
    equivalent is missing for TreeModel.
    
    Add a test case that initializes a TreeModel via setRows.
    
    (Strictly speaking this could have been part of one of the other
    setRows test cases but I wanted to reuse the code for the snippets
    in the documentation so this had to look the same.
    Besides, maybe it is a good idea to separate this from
    the other tests because of the JSON).
    
    Task-number: QTBUG-111830
    Change-Id: I7cede51f808f245d267cbb41c469b82b259b4d78
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit 4f2677557ef50e8e0ded10fd26d7005b35745137)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 22f006730208ba2c0ec440109b149191c278f3ad)

---
+++ tests/auto/qml/qqmltreemodel/data/TreeData.js
@@ -0,0 +1,34 @@
+let folders = [
+    {
+        "checked": false,
+        "size": "—",
+        "type": "folder",
+        "name": "Documents",
+        "lastModified": "2025-07-01",
+        "rows": [
+            {
+                "checked": true,
+                "size": "24 KB",
+                "type": "file",
+                "name": "Resume.pdf",
+                "lastModified": "2025-06-20"
+            }
+        ]
+    },
+    {
+        "checked": false,
+        "size": "—",
+        "type": "folder",
+        "name": "Pictures",
+        "lastModified": "2025-05-30",
+        "rows": [
+            {
+                "checked": true,
+                "size": "3.5 MB",
+                "type": "file",
+                "name": "Vacation.jpg",
+                "lastModified": "2025-05-15"
+            }
+        ]
+    }
+]
