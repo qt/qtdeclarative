qt/qtdeclarative

commit 32c8d7f9da04f40eebee04915d0861ce3084a4e5

QQuickPathRectangle: Merge corner radius and bevel flags into a bitset
Replace separate storage for corner radius and bevel with a single 8-bit
field cornerProperties. The first 4 bits track radius flags, and the
next 4 track bevel flags.

Fixes: QTBUG-134908
Change-Id: Ie2d706112965fc5dde07fa698e32e29695da219b
Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

src/quick/util/qquickpath.cpp
@@ -2486,13 +2486,13 @@ void QQuickPathRectangle::setRadius(qreal newRadius)
         return;
     _extra->radius = newRadius;
     emit radiusChanged();
-    if (!(_extra->isCornerRadiusSet & (1 << Qt::TopLeftCorner)))
+    if (!(_extra->isRadiusSet(Qt::TopLeftCorner)))
         emit topLeftRadiusChanged();
-    if (!(_extra->isCornerRadiusSet & (1 << Qt::TopRightCorner)))
+    if (!(_extra->isRadiusSet(Qt::TopRightCorner)))
         emit topRightRadiusChanged();
-    if (!(_extra->isCornerRadiusSet & (1 << Qt::BottomLeftCorner)))
+    if (!(_extra->isRadiusSet(Qt::BottomLeftCorner)))
         emit bottomLeftRadiusChanged();
-    if (!(_extra->isCornerRadiusSet & (1 << Qt::BottomRightCorner)))
+    if (!(_extra->isRadiusSet(Qt::BottomRightCorner)))
         emit bottomRightRadiusChanged();
     emit changed();
 }
@@ -2504,7 +2504,7 @@ void QQuickPathRectangle::setRadius(qreal newRadius)
 qreal QQuickPathRectangle::cornerRadius(Qt::Corner corner) const
 {
     if (_extra.isAllocated())
-        return (_extra->isCornerRadiusSet & (1 << corner)) ? _extra->cornerRadii[corner] : _extra->radius;
+        return (_extra->isRadiusSet(corner)) ? _extra->cornerRadii[corner] : _extra->radius;
     else
         return 0;
 }
@@ -2512,19 +2512,19 @@ qreal QQuickPathRectangle::cornerRadius(Qt::Corner corner) const
 void QQuickPathRectangle::setCornerRadius(Qt::Corner corner, qreal newCornerRadius)
 {
     if (_extra.value().cornerRadii[corner] == newCornerRadius
-        && (_extra->isCornerRadiusSet & (1 << corner)))
+        && (_extra->isRadiusSet(corner)))
         return;
     _extra->cornerRadii[corner] = newCornerRadius;
-    _extra->isCornerRadiusSet |= (1 << corner);
+    _extra->cornerProperties |= (1 << corner);
 
     emitCornerRadiusChanged(corner);
 }
 
 void QQuickPathRectangle::resetCornerRadius(Qt::Corner corner)
 {
-    if (!_extra.isAllocated() || !(_extra->isCornerRadiusSet & (1 << corner)))
+    if (!_extra.isAllocated() || !(_extra->isRadiusSet(corner)))
         return;
-    _extra->isCornerRadiusSet &= ~(1 << corner);
+    _extra->cornerProperties &= ~(1 << corner);
     emitCornerRadiusChanged(corner);
 }
 
@@ -2556,22 +2556,26 @@ void QQuickPathRectangle::emitCornerRadiusChanged(Qt::Corner corner)
 
 bool QQuickPathRectangle::hasBevel() const
 {
-    return _extra.isAllocated() ? _extra->bevel : 0;
+    return _extra.isAllocated() ? (_extra->cornerProperties & (1 << 8)) != 0 : false;
 }
 
 void QQuickPathRectangle::setBevel(bool bevel)
 {
-    if (_extra.value().bevel == bevel)
+    if (((_extra.value().cornerProperties & (1 << 8)) != 0) == bevel)
         return;
-    _extra->bevel = bevel;
+    if (bevel)
+        _extra->cornerProperties |= (1 << 8);
+    else
+        _extra->cornerProperties &= ~(1 << 8);
+
     emit bevelChanged();
-    if (!_extra->cornerBevel[Qt::TopLeftCorner].has_value())
+    if (!(_extra->isBevelSet(Qt::TopLeftCorner)))
         emit topLeftBevelChanged();
-    if (!_extra->cornerBevel[Qt::TopRightCorner].has_value())
+    if (!(_extra->isBevelSet(Qt::TopRightCorner)))
         emit topRightBevelChanged();
-    if (!_extra->cornerBevel[Qt::BottomLeftCorner].has_value())
+    if (!(_extra->isBevelSet(Qt::BottomLeftCorner)))
         emit bottomLeftBevelChanged();
-    if (!_extra->cornerBevel[Qt::BottomRightCorner].has_value())
+    if (!(_extra->isBevelSet(Qt::BottomRightCorner)))
         emit bottomRightBevelChanged();
     emit changed();
 }
@@ -2583,24 +2587,28 @@ void QQuickPathRectangle::setBevel(bool bevel)
 bool QQuickPathRectangle::cornerBevel(Qt::Corner corner) const
 {
     if (_extra.isAllocated())
-        return !_extra->cornerBevel[corner].has_value() ? _extra->bevel : _extra->cornerBevel[corner].value();
+        return _extra->isBevelSet(corner);
     else
         return false;
 }
 
 void QQuickPathRectangle::setCornerBevel(Qt::Corner corner, bool newCornerBevel)
 {
-    if (_extra.value().cornerBevel[corner] == newCornerBevel)
+    if ((_extra.value().isBevelSet(corner)) == newCornerBevel)
         return;
-    _extra->cornerBevel[corner] = newCornerBevel;
+    if (!newCornerBevel) {
+        resetCornerBevel(corner);
+        return;
+    }
+    _extra->cornerProperties |= (1 << (corner + 4));
     emitCornerBevelChanged(corner);
 }
 
 void QQuickPathRectangle::resetCornerBevel(Qt::Corner corner)
 {
-    if (!_extra.isAllocated() || !_extra->cornerBevel[corner].has_value())
+    if (!_extra.isAllocated() || !(_extra->isBevelSet(corner)))
         return;
-    _extra->cornerBevel[corner].reset();
+    _extra->cornerProperties &= ~(1 << (corner + 4));
     emitCornerBevelChanged(corner);
 }
 
@@ -2641,7 +2649,7 @@ void QQuickPathRectangle::addToPath(QPainterPath &path, const QQuickPathData &da
         const qreal generalDiameter = qMax(qreal(0), qMin(maxDiameter, 2 * _extra->radius));
         auto effectiveDiameter = [&](Qt::Corner corner) {
             qreal radius = _extra->cornerRadii[corner];
-            return (_extra->isCornerRadiusSet & ( 1 << corner)) ? qMin(maxDiameter, 2 * radius) : generalDiameter;
+            return (_extra->isRadiusSet(corner)) ? qMin(maxDiameter, 2 * radius) : generalDiameter;
         };
         const qreal diamTL = effectiveDiameter(Qt::TopLeftCorner);
         const qreal diamTR = effectiveDiameter(Qt::TopRightCorner);

tests/auto/quick/qquickpath/tst_qquickpath.cpp
@@ -28,6 +28,7 @@ private slots:
     void rectangleRadii();
     void appendRemove();
     void asynchronous();
+    void cornerProperties();
 
 private:
     void arc(QSizeF scale);
@@ -482,6 +483,71 @@ void tst_QuickPath::asynchronous()
     QCOMPARE(pathElements.count(), 2);
     QTRY_VERIFY(!path->path().isEmpty());
     QCOMPARE(changedSpy.count(), 1);
+}
+
+void tst_QuickPath::cornerProperties()
+{
+    QQuickPathRectangle pathRectangle;
+
+    // Bevel
+    QVERIFY(!pathRectangle.hasBevel());
+    QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasTopRightBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomRightBevel(), false);
+
+    pathRectangle.setBevel(true);
+    QVERIFY(pathRectangle.hasBevel());
+    QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasTopRightBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomRightBevel(), false);
+
+    pathRectangle.setBottomLeftBevel(true);
+    QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasTopRightBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomLeftBevel(), true);
+    QCOMPARE(pathRectangle.hasBottomRightBevel(), false);
+
+    pathRectangle.setBottomLeftBevel(false);
+    pathRectangle.setTopRightBevel(true);
+    QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasTopRightBevel(), true);
+    QCOMPARE(pathRectangle.hasBottomLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomRightBevel(), false);
+
+    pathRectangle.resetTopRightBevel();
+    QCOMPARE(pathRectangle.hasTopRightBevel(), false);
+    QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomLeftBevel(), false);
+    QCOMPARE(pathRectangle.hasBottomRightBevel(), false);
+
+    // Radius
+    pathRectangle.setRadius(10.0);
+    QCOMPARE(pathRectangle.radius(), 10.0);
+    QCOMPARE(pathRectangle.topRightRadius(), 10.0);
+    QCOMPARE(pathRectangle.topLeftRadius(), 10.0);
+    QCOMPARE(pathRectangle.bottomLeftRadius(), 10.0);
+    QCOMPARE(pathRectangle.bottomRightRadius(), 10.0);
+
+    pathRectangle.setTopRightRadius(3.0);
+    QCOMPARE(pathRectangle.topRightRadius(), 3.0);
+    QCOMPARE(pathRectangle.radius(), 10.0);
+    QCOMPARE(pathRectangle.topLeftRadius(), 10.0);
+    QCOMPARE(pathRectangle.bottomLeftRadius(), 10.0);
+    QCOMPARE(pathRectangle.bottomRightRadius(), 10.0);
+
+    pathRectangle.setRadius(0.0);
+    QCOMPARE(pathRectangle.topRightRadius(), 3.0);
+    QCOMPARE(pathRectangle.topLeftRadius(), 0.0);
+    QCOMPARE(pathRectangle.bottomLeftRadius(), 0.0);
+    QCOMPARE(pathRectangle.bottomRightRadius(), 0.0);
+
+    pathRectangle.resetTopRightRadius();
+    QCOMPARE(pathRectangle.topRightRadius(), 0.0);
+    QCOMPARE(pathRectangle.topLeftRadius(), 0.0);
+    QCOMPARE(pathRectangle.bottomLeftRadius(), 0.0);
+    QCOMPARE(pathRectangle.bottomRightRadius(), 0.0);
 }
 
 QTEST_MAIN(tst_QuickPath)

src/quick/util/qquickpath_p.h
@@ -511,14 +511,17 @@ private:
     {
         ExtraData() {
             std::fill_n(cornerRadii, 4, 0);
-            std::fill_n(cornerBevel, 4, std::optional<bool>());
-            isCornerRadiusSet = 0;
+            cornerProperties = 0;
         }
         qreal radius = 0;
         qreal cornerRadii[4];
-        unsigned isCornerRadiusSet : 4;
-        bool bevel = false;
-        std::optional<bool> cornerBevel[4];
+        unsigned cornerProperties :9;
+        inline bool isRadiusSet(Qt::Corner corner) {
+            return cornerProperties & (1 << corner);
+        };
+        inline bool isBevelSet(Qt::Corner corner) {
+            return cornerProperties & (1 << (corner + 4));
+        };
     };
     QLazilyAllocated<ExtraData> _extra;
 };

commit 230bcac6f63725ed29d7e284d2a503cb3d2834d9

RectangleShape: fix bevel property not being respected
32c8d7f9da04f40eebee04915d0861ce3084a4e5 broke the logic in QQuickPathRectangle::cornerBevel by not respecting the 8th bit it
introduced.

The code before that change was:

    bool QQuickPathRectangle::cornerBevel(Qt::Corner corner) const
    {
        if (_extra.isAllocated())
            return !_extra->cornerBevel[corner].has_value() ? _extra->bevel : _extra->cornerBevel[corner].value();
        else
            return false;
    }

After the change, it became:

    bool QQuickPathRectangle::cornerBevel(Qt::Corner corner) const
    {
        if (_extra.isAllocated())
            return _extra->isBevelSet(corner);
        else
            return false;
    }

Since it started only calling isBevelSet, which only checks bits 4 to 7,
it didn't checked bit 8, which stores the state of the bevel property:

    inline bool isBevelSet(Qt::Corner corner) {
        return cornerProperties & (1 << (corner + 4));
    };

    bool QQuickPathRectangle::hasBevel() const
    {
        return _extra.isAllocated() ? (_extra->cornerProperties & (1 << 8)) != 0 : false;
    }

Fix it by also considering the 8th bit.

This fix is different than the one in later branches because the
code has since diverged.

Fixes: QTBUG-142369
Change-Id: I404bcad7fe3a6da98e90a499eaff2891fdcf5220
Reviewed-by: Magdalena Stojek <magdalena.stojek@qt.io>
Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>

src/quick/util/qquickpath.cpp
@@ -2633,10 +2633,11 @@ void QQuickPathRectangle::setBevel(bool bevel)
 
 bool QQuickPathRectangle::cornerBevel(Qt::Corner corner) const
 {
-    if (_extra.isAllocated())
-        return _extra->isBevelSet(corner);
-    else
+    if (!_extra.isAllocated())
         return false;
+
+    // Check both if a specific corner is set, or if the more general bevel property is set.
+    return _extra->isBevelSet(corner) || _extra->cornerProperties & (1 << 8);
 }
 
 void QQuickPathRectangle::setCornerBevel(Qt::Corner corner, bool newCornerBevel)

tests/auto/quick/qquickpath/tst_qquickpath.cpp
@@ -500,11 +500,12 @@ void tst_QuickPath::cornerProperties()
 
     pathRectangle.setBevel(true);
     QVERIFY(pathRectangle.hasBevel());
-    QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
-    QCOMPARE(pathRectangle.hasTopRightBevel(), false);
-    QCOMPARE(pathRectangle.hasBottomLeftBevel(), false);
-    QCOMPARE(pathRectangle.hasBottomRightBevel(), false);
+    QCOMPARE(pathRectangle.hasTopLeftBevel(), true);
+    QCOMPARE(pathRectangle.hasTopRightBevel(), true);
+    QCOMPARE(pathRectangle.hasBottomLeftBevel(), true);
+    QCOMPARE(pathRectangle.hasBottomRightBevel(), true);
 
+    pathRectangle.setBevel(false);
     pathRectangle.setBottomLeftBevel(true);
     QCOMPARE(pathRectangle.hasTopLeftBevel(), false);
     QCOMPARE(pathRectangle.hasTopRightBevel(), false);

tests/baseline/scenegraph/data/designhelpers/designhelpers_rectangleshape.qml
@@ -85,10 +85,9 @@ Rectangle {
             bottomLeftBevel: true
             bottomRightBevel: true
         }
-
-        // strokeColor
         RectangleShapeBase {
             strokeColor: "red"
+            bevel: true
         }
 
         // strokeWidth