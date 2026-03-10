ed78562cc38f516c2fa800e8263b7d399a5f6668
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2026-03-10 14:43:13 +0100

    curve renderer: Make sure uniforms update when path is changed
    When the path is changed, we also have to update the uniforms
    of the node. If the path had a visible fill, then this would
    happen because the code handling FillDirty always also dirtied
    the uniforms. But if the fill was not visible, the StrokeDirty
    would not do the same. This caused update errors for shapes
    which only has a stroke.
    
    Pick-to: 6.8
    Fixes: QTBUG-144907
    Change-Id: Ib433fdc69e780f56357d96a25c110c6e78ddd12d
    Reviewed-by: Eirik Aavitsland <eirik.aavitsland@qt.io>
    (cherry picked from commit dc10dc19f3910d743da41235fd836b98d660bb99)
    (cherry picked from commit fde6390b03e7813e2f50497f9fab28c22939725f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quickshapes/qquickshapecurverenderer.cpp
+++ src/quickshapes/qquickshapecurverenderer.cpp
@@ -543,6 +543,7 @@ void QQuickShapeCurveRenderer::processPath(PathData *pathData)
                 pathData->strokeNodes = addTriangulatingStrokerNodes(strokePath, pen);
             else
                 pathData->strokeNodes = addCurveStrokeNodes(strokePath, pen);
+            dirtyFlags |= UniformsDirty;
         }
     }
 }
