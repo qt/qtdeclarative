fa20148237a172f0f08eb031df7c5d2cfd41235e
Marc Mutz <marc.mutz@qt.io>
2026-04-16 11:57:37 +0200

    QQuickDesignerCustomObjectData: fix 3-arg connect
    Use the source object as the context object, since `this` is not a
    QObject.
    
    As a drive-by, pass HandleDestroyedFunctor as an rvalue.
    
    Amends 537172dc9f76fa08e995042021f620cef7bd4929 (5.6).
    
    Pick-to: 6.11 6.8
    Task-number: QTBUG-145828
    Change-Id: I61bd6faed425097b95da74045109e731f7808165
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>

--- src/quick/designer/qquickdesignercustomobjectdata.cpp
+++ src/quick/designer/qquickdesignercustomobjectdata.cpp
@@ -32,9 +32,8 @@ QQuickDesignerCustomObjectData::QQuickDesignerCustomObjectData(QObject *object)
         populateResetHashes();
         s_designerObjectToDataHash()->insert(object, this);
 
-        HandleDestroyedFunctor functor;
-        functor.data = this;
-        QObject::connect(object, &QObject::destroyed, functor);
+        QObject::connect(object, &QObject::destroyed,
+                         object, HandleDestroyedFunctor{this});
     }
 }
 
