d36a0e6b7dd5aca76986fb3076abd4286565f033
Marc Mutz <marc.mutz@qt.io>
2026-04-16 12:17:38 +0200

    QQuickLayoutItemProxy: fix two 3-arg connect() calls
    Use `this` as the context objects each time, since the lambdas are
    capturing `this`, too, and act on it.
    
    As a drive-by, on reviewer request, std::move() the lambdas into
    the connect() call.
    
    Amends 90a5e8d217b614cedfeec40132067c7c6222b5b9 (6.6).
    
    Pick-to: 6.8
    Task-number: QTBUG-145828
    Change-Id: Ife37c129d298bb8261875c13796424e95d183924
    Reviewed-by: Ahmad Samir <a.samirh78@gmail.com>
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>
    (cherry picked from commit 90add913a360b82ee75ef795179323aa598baf96)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/quicklayouts/qquicklayoutitemproxy.cpp
+++ src/quicklayouts/qquicklayoutitemproxy.cpp
@@ -290,12 +290,14 @@ void QQuickLayoutItemProxy::setTarget(QQuickItem *newTarget)
             // proxy.implicitWidth: target.implicitWidth
             auto fnBindImplW = [newTarget, this](){ this->setImplicitWidth(newTarget->implicitWidth()); };
             fnBindImplW();
-            connect(newTarget, &QQuickItem::implicitWidthChanged, fnBindImplW);
+            connect(newTarget, &QQuickItem::implicitWidthChanged,
+                    this, std::move(fnBindImplW));
 
             // proxy.implicitHeight: target.implicitHeight
             auto fnBindImplH = [newTarget, this](){ this->setImplicitHeight(newTarget->implicitHeight()); };
             fnBindImplH();
-            connect(newTarget, &QQuickItem::implicitHeightChanged, fnBindImplH);
+            connect(newTarget, &QQuickItem::implicitHeightChanged,
+                    this, std::move(fnBindImplH));
         }
     }
 
