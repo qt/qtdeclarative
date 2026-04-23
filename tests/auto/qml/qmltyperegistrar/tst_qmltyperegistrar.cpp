5f8f7fdc52657af1bbf0f9d72173be044dab3daf
Olivier De Cannière <olivier.decanniere@qt.io>
2026-04-23 12:22:08 +0000

    Revert "qmltyperegistrar: Update file and line only in TopLevel mode"
    This reverts commit d7666a15ce36f3e5e5ab5722da43c82124cba8b7.
    
    Reason for revert: Breaks qmlsc (QTBUG-145981)
    
    Task-number: QTBUG-145981
    Change-Id: I5bacc0b026fb79085e7e608de9b6634318880eb4
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>
    (cherry picked from commit f843e84926a27b0baf7345e57bf7ac323f3c5711)

--- tests/auto/qml/qmltyperegistrar/tst_qmltyperegistrar.cpp
+++ tests/auto/qml/qmltyperegistrar/tst_qmltyperegistrar.cpp
@@ -1030,7 +1030,7 @@ void tst_qmltyperegistrar::withNamespace()
 
     QVERIFY(qmltypesData.contains(R"(Component {
         file: "tst_qmltyperegistrar.h"
-        lineNumber: 684
+        lineNumber: 652
         name: "Testing::Inner::Baz"
         accessSemantics: "reference"
         prototype: "Testing::Bar"
@@ -1443,22 +1443,6 @@ void tst_qmltyperegistrar::foreignFileAndLinePointsToNonForeign()
         prototype: "QObject"
         exports: ["QmlTypeRegistrarTest/ForeignFromAnotherFile 1.0"]
 )"));
-}
-
-void tst_qmltyperegistrar::extendedDoesNotPolluteFile() {
-    QVERIFY(qmltypesData.contains(R"(Component {
-        file: "tst_qmltyperegistrar.h"
-        lineNumber: 945
-        name: "FakeQtObject"
-        accessSemantics: "reference"
-        prototype: "QObject"
-        extension: "Qt"
-        extensionIsNamespace: true
-        exports: ["QmlTypeRegistrarTest/FakeQt 1.0"]
-        isCreatable: false
-        isSingleton: true
-        exportMetaObjectRevisions: [256]
-    })"));
 }
 
 void tst_qmltyperegistrar::foreignNamespacedWithEnum()
