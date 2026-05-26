c21c355bd6f4a5712e8db2da33b68b6b8ad77058
Mitch Curtis <mitch.curtis@qt.io>
2026-05-26 11:02:59 +0800

    grabToImage: improve invalid dimension warning
    Explain why it's invalid and print the actual dimensions.
    
    Change-Id: Id585f6aafb3cb49e321f3e239fc726a749608021
    Reviewed-by: Oliver Eftevaag <oliver.eftevaag@qt.io>
    (cherry picked from commit 6cd1520263553d3951a7271b33ff28fdba7005d9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 06d553ed96968aaf5ab400e4484c0ced13b0a512)

--- src/quick/items/qquickitemgrabresult.cpp
+++ src/quick/items/qquickitemgrabresult.cpp
@@ -329,7 +329,7 @@ QQuickItemGrabResult *QQuickItemGrabResultPrivate::create(QQuickItem *item, cons
         size = QSize(item->width(), item->height());
 
     if (size.width() < 1 || size.height() < 1) {
-        qmlWarning(item) << "grabToImage: item has invalid dimensions";
+        qmlWarning(item) << "grabToImage: item's width and/or height are less than 1: " << size;
         return nullptr;
     }
 
@@ -459,7 +459,7 @@ bool QQuickItem::grabToImage(const QJSValue &callback, const QSize &targetSize)
         size = QSize(width(), height());
 
     if (size.width() < 1 || size.height() < 1) {
-        qmlWarning(this) << "grabToImage: item has invalid dimensions";
+        qmlWarning(this) << "grabToImage: item's width and/or height are less than 1: " << size;
         return false;
     }
 
