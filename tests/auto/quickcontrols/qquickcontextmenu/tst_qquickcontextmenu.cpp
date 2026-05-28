6b02c6c13969fdfb9725d4190682bcef05a8dff2
Frédéric Lefebvre <frederic.lefebvre@qt.io>
2026-05-28 12:20:46 +0200

    Unblacklist tst_QQuickContextMenu::menuItemShouldntTriggerOnRelease
    tst_QQuickContextMenu::menuItemShouldntTriggerOnRelease is no longer
    flaking or failing.
    
    Unblacklist it from every platform and remove its QSKIP.
    
    Fixes: QTBUG-141406
    Change-Id: I73978f83b07bf9e69be67e41fe46e6e236157e9f
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

--- tests/auto/quickcontrols/qquickcontextmenu/tst_qquickcontextmenu.cpp
+++ tests/auto/quickcontrols/qquickcontextmenu/tst_qquickcontextmenu.cpp
@@ -370,8 +370,6 @@ void tst_QQuickContextMenu::menuItemShouldntTriggerOnRelease() // QTBUG-133302
     QSKIP("Crashes on android. See QTBUG-137400");
 #endif
 
-    QSKIP("Test function has been flaky from the start, QTBUG-141406.");
-
     QQuickApplicationHelper helper(this, "windowedContextMenuOnControl.qml");
     QVERIFY2(helper.ready, helper.failureMessage());
     QQuickWindow *window = helper.window;
