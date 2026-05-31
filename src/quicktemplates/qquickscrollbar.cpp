qt/qtdeclarative

commit 3e233634d834ad9cdbe337b99cbd59e8b130f62f

a11y: Report + allow modifying QQuickScrollBar position
Introduce QAccessibleQuickScrollBar as the a11y
implementation for QQuickScrollBar and override
the base class implementations for the
QAccessibleValueInterface methods to report and
allow setting the position of the scroll bar.
(The base class implementation would always use
meaningless values of 0.0.)

"Normalize" the valid values for
QQuickScrollBar::position which range
from 0.0 to 1.0 - QQuickScrollBar::position (see [1])
to a range of 0.0 to 100.0 for the accessible
interface.

In QQuickScrollBarPrivate::setPosition, send a
QAccessibleValueChangeEvent when the value
changes, so assistive technology are notified
of the new value.
This e.g. ensures that the value shown in
Accerciser's "Interface Viewer", section "Value"
gets updated when scrolling happens.

[1] https://doc.qt.io/qt-6/qml-qtquick-controls-scrollbar.html#position-prop

Fixes: QTBUG-140673
Task-number: QTBUG-139833
Change-Id: I4333d91a70c6056424fe1e09cb075cf6ca7266e2
Reviewed-by: MohammadHossein Qanbari <mohammad.qanbari@qt.io>
Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

src/quicktemplates/qquickscrollbar.cpp
@@ -468,6 +468,17 @@ void QQuickScrollBarPrivate::setPosition(qreal newPosition, bool notifyVisualCha
     emit q->positionChanged();
     if (notifyVisualChange)
         visualAreaChange(visualArea(), oldVisualArea);
+
+#if QT_CONFIG(accessibility)
+    if (QAccessible::isActive()) {
+        if (QAccessibleInterface *accessible = QAccessible::queryAccessibleInterface(q)) {
+            QAccessibleValueInterface *valueInterface = accessible->valueInterface();
+            Q_ASSERT(valueInterface);
+            QAccessibleValueChangeEvent event(q, valueInterface->currentValue());
+            QAccessible::updateAccessibility(&event);
+        }
+    }
+#endif
 }
 
 /*!

src/quicktemplates/CMakeLists.txt
@@ -144,6 +144,7 @@ qt_internal_extend_target(QuickTemplates2 CONDITION QT_FEATURE_accessibility
     SOURCES
         accessible/qaccessiblequickcontrol.cpp accessible/qaccessiblequickcontrol_p.h
         accessible/qaccessiblequickpage.cpp accessible/qaccessiblequickpage_p.h
+        accessible/qaccessiblequickscrollbar.cpp accessible/qaccessiblequickscrollbar_p.h
 )
 
 qt_internal_extend_target(QuickTemplates2 CONDITION QT_FEATURE_quicktemplates2_container

src/quicktemplates/accessible/qaccessiblequickscrollbar.cpp
@@ -1,0 +1,63 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#include "qaccessiblequickscrollbar_p.h"
+#include "qquickscrollbar_p.h"
+
+QT_BEGIN_NAMESPACE
+
+QAccessibleQuickScrollBar::QAccessibleQuickScrollBar(QQuickScrollBar *scrollBar)
+    : QAccessibleQuickControl(scrollBar)
+{
+}
+
+QVariant QAccessibleQuickScrollBar::currentValue() const
+{
+    return normalizeValue(scrollBar()->position());
+}
+
+void QAccessibleQuickScrollBar::setCurrentValue(const QVariant &value)
+{
+    scrollBar()->setPosition(denormalizeValue(value.toReal()));
+}
+
+QVariant QAccessibleQuickScrollBar::maximumValue() const
+{
+    return normalizeValue(1.0 - scrollBar()->size());
+}
+
+QVariant QAccessibleQuickScrollBar::minimumValue() const
+{
+    return normalizeValue(0.0);
+}
+
+QVariant QAccessibleQuickScrollBar::minimumStepSize() const
+{
+    return normalizeValue(scrollBar()->stepSize());
+}
+
+QQuickScrollBar *QAccessibleQuickScrollBar::scrollBar() const
+{
+    return static_cast<QQuickScrollBar *>(object());
+}
+
+qreal QAccessibleQuickScrollBar::maximumPositionValue() const
+{
+    return 1.0 - scrollBar()->size();
+}
+
+qreal QAccessibleQuickScrollBar::normalizeValue(qreal value) const
+{
+    const qreal maxPositionValue = maximumPositionValue();
+    if (maxPositionValue > 0.0)
+        return value / maxPositionValue * 100.0;
+    return 0;
+}
+
+qreal QAccessibleQuickScrollBar::denormalizeValue(qreal value) const
+{
+    return value / 100.0 * maximumPositionValue();
+}
+
+QT_END_NAMESPACE

src/quicktemplates/accessible/qaccessiblequickscrollbar_p.h
@@ -1,0 +1,48 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:significant reason:default
+
+#ifndef QACCESSIBLEQUICKSCROLLBAR_H
+#define QACCESSIBLEQUICKSCROLLBAR_H
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
+#include "qaccessiblequickcontrol_p.h"
+
+QT_BEGIN_NAMESPACE
+
+class QQuickScrollBar;
+
+class QAccessibleQuickScrollBar : public QAccessibleQuickControl
+{
+public:
+    QAccessibleQuickScrollBar(QQuickScrollBar *scrollBar);
+
+    QVariant currentValue() const override;
+    void setCurrentValue(const QVariant &value) override;
+    QVariant maximumValue() const override;
+    QVariant minimumValue() const override;
+    QVariant minimumStepSize() const override;
+
+private:
+    QQuickScrollBar *scrollBar() const;
+
+    qreal maximumPositionValue() const;
+    // convert QQuickScrollBar::position value to value in the range 0.0 to 100.0
+    qreal normalizeValue(qreal value) const;
+    // convert normalized value in range 0.0 to 100.0 to QQuickScrollBar::position value
+    qreal denormalizeValue(qreal value) const;
+};
+
+QT_END_NAMESPACE
+
+#endif // QACCESSIBLEQUICKSCROLLBAR_H

src/quicktemplates/qtquicktemplates2global.cpp
@@ -8,8 +8,10 @@
 
 #if QT_CONFIG(accessibility)
 #include "qquickpage_p.h"
+#include "qquickscrollbar_p.h"
 #include "accessible/qaccessiblequickcontrol_p.h"
 #include "accessible/qaccessiblequickpage_p.h"
+#include "accessible/qaccessiblequickscrollbar_p.h"
 #endif
 
 QT_BEGIN_NAMESPACE
@@ -17,9 +19,10 @@ QT_BEGIN_NAMESPACE
 #if QT_CONFIG(accessibility)
 static QAccessibleInterface *qQuickAccessibleFactory(const QString &classname, QObject *object)
 {
-    if (classname == u"QQuickPage") {
+    if (classname == u"QQuickPage")
         return new QAccessibleQuickPage(qobject_cast<QQuickPage *>(object));
-    }
+    if (classname == u"QQuickScrollBar")
+        return new QAccessibleQuickScrollBar(qobject_cast<QQuickScrollBar *>(object));
     if (classname == u"QQuickControl")
         return new QAccessibleQuickControl(qobject_cast<QQuickControl *>(object));
 

commit 0093e85bbff86a25f3ab069de8ac19b3d723f706

a11y: Guard against nullptr for scrollbar valueInterface
The regression was introduced in 3e233634d83, which mistakenly assumed
that valueInterface cannot be nullptr.

Fixes: QTBUG-146959
Pick-to: 6.11
Change-Id: Ia47a05c9da6bf3889b12475f737aeab20485fe59
Reviewed-by: MohammadHossein Qanbari <mohammad.qanbari@qt.io>

src/quicktemplates/qquickscrollbar.cpp
@@ -474,10 +474,10 @@ void QQuickScrollBarPrivate::setPosition(qreal newPosition, bool notifyVisualCha
 #if QT_CONFIG(accessibility)
     if (QAccessible::isActive()) {
         if (QAccessibleInterface *accessible = QAccessible::queryAccessibleInterface(q)) {
-            QAccessibleValueInterface *valueInterface = accessible->valueInterface();
-            Q_ASSERT(valueInterface);
-            QAccessibleValueChangeEvent event(q, valueInterface->currentValue());
-            QAccessible::updateAccessibility(&event);
+            if (QAccessibleValueInterface *valueInterface = accessible->valueInterface()) {
+                QAccessibleValueChangeEvent event(q, valueInterface->currentValue());
+                QAccessible::updateAccessibility(&event);
+            }
         }
     }
 #endif