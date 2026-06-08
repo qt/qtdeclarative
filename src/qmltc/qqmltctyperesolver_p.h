f069f91bc9c21b67c53b8dbcd2429ed92eedbfe7
Olivier De Cannière <olivier.decanniere@qt.io>
2026-06-08 10:53:35 +0200

    qqmltc: Add security headers
    Everything is significant by default except the codegen parts that are
    critical.
    
    Pick-to: 6.11
    Change-Id: I01c0cc48158a0daa6ac3aed9e953c0e79d223728
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 46f209d9f7decac757aacda66d290c748b06ed09)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/qmltc/qqmltctyperesolver_p.h
+++ src/qmltc/qqmltctyperesolver_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2021 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:significant
 
 #ifndef QQMLTCTYPERESOLVER_P_H
 #define QQMLTCTYPERESOLVER_P_H
