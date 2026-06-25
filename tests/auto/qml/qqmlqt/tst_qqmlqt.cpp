qt/qtdeclarative

commit 054873dfea0ce07c94bb603edc3b7d61a6190c4f

Loader: Also drop engine when clearing contexts recursively
Simply clearing the expressions is not enough. We might have internal
signal/slot connections in addition. The main protagonists of such
behavior listen to context invalidation, though, and a context without
engine counts as invalid. At the same time, we leave the context object
in place so that we don't re-open QTBUG-66822. The currently running
binding will still be able to complete this way.

Pick-to: 6.10 6.9 6.8 6.5
Fixes: QTBUG-118188
Change-Id: Ia973be26f0e4e3244adc71f95df7e0b4bef412c3
Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>

src/qml/qml/qqmlcontextdata.cpp
@@ -100,7 +100,7 @@ void QQmlContextData::invalidate()
     }
 
     if (!m_hasWeakImportedScripts) { // invalidate might be called multiple times
-        if (!m_importedScripts.isNullOrUndefined()) {
+        if (m_engine && !m_importedScripts.isNullOrUndefined()) {
             QV4::Scope scope(m_engine->handle());
             QV4::ScopedValue val(scope, m_importedScripts.value());
             m_importedScripts.~PersistentValue();
@@ -123,6 +123,8 @@ void QQmlContextData::clearContextRecursively()
 
     for (auto ctxIt = m_childContexts; ctxIt; ctxIt = ctxIt->m_nextChild)
         ctxIt->clearContextRecursively();
+
+    m_engine = nullptr;
 }
 
 void QQmlContextData::clearContext()
@@ -154,8 +156,7 @@ QQmlContextData::~QQmlContextData()
         // just to throw it away afterwards
         m_importedScripts.clear();
     }
-    if (m_engine)
-        invalidate();
+    invalidate();
     if (m_hasWeakImportedScripts)
         m_weakImportedScripts.~WeakValue();
     else

tests/auto/quick/qquickloader/data/invalidateContext.qml
@@ -1,0 +1,28 @@
+import QtQuick
+
+Item  {
+    Loader {
+        active: loaderActive
+        sourceComponent: comp
+    }
+
+    Component {
+        id: comp
+        Column {
+            Repeater {
+                id: repeater
+                model: cppModel
+
+                Component.onCompleted: console.log("Repeater constructed");
+                Component.onDestruction: console.log("Repeater destroyed");
+
+                delegate: Text {
+                    text: {
+                        console.log("updating text");
+                        return display + rootData.getValue();
+                    }
+                }
+            }
+        }
+    }
+}

tests/auto/quick/qquickloader/tst_qquickloader.cpp
@@ -119,6 +119,8 @@ private slots:
     void stackOverflow();
     void stackOverflow2();
     void boundComponent();
+
+    void invalidateContext();
 };
 
 Q_DECLARE_METATYPE(QList<QQmlError>)
@@ -1618,6 +1620,60 @@ void tst_QQuickLoader::boundComponent()
     QVERIFY2(component.isReady(), qPrintable(component.errorString()));
     QScopedPointer<QObject> o(component.create());
     QCOMPARE(o->objectName(), QStringLiteral("loaded"));
+}
+
+class CppModel : public QAbstractListModel
+{
+public:
+    virtual int rowCount(const QModelIndex &) const override {return 1;}
+    virtual QVariant data(const QModelIndex &, int) const override { return "test"; }
+
+    void reset () {beginResetModel(); endResetModel();}
+};
+
+class RootData : public QObject
+{
+    Q_OBJECT
+public:
+    RootData() : object(std::make_unique<QObject>())
+    {
+        object->setObjectName("objectName");
+    }
+
+    Q_INVOKABLE QString getValue() const {  return object->objectName(); }
+
+    void deleteObject() { object.reset(); }
+
+private:
+    std::unique_ptr<QObject> object;
+};
+
+void tst_QQuickLoader::invalidateContext()
+{
+    CppModel model;
+    RootData rootData;
+
+    QQmlEngine engine;
+    QQmlContext *rootContext = engine.rootContext();
+    rootContext->setContextProperty("cppModel", &model);
+    rootContext->setContextProperty("loaderActive", true);
+    rootContext->setContextProperty("rootData", &rootData);
+
+    QQmlComponent component(&engine, testFileUrl("invalidateContext.qml"));\
+    QVERIFY2(component.isReady(), qPrintable(component.errorString()));
+
+    QTest::ignoreMessage(QtDebugMsg, "Repeater constructed");
+    QTest::ignoreMessage(QtDebugMsg, "updating text");
+    QScopedPointer<QObject> o(component.create());
+
+    QTest::ignoreMessage(QtDebugMsg, "Repeater destroyed");
+    rootContext->setContextProperty("loaderActive", false);
+
+    // The object should never be used anymore, as loader is inactive
+    rootData.deleteObject();
+
+    // Should not trigger any re-evaluation (which would crash in getValue())
+    model.reset();
 }
 
 QTEST_MAIN(tst_QQuickLoader)

commit aa6f2986609e2f3affca7e4e4cafe44d8dcf4268

Qt.callLater: Fix crash with invalidated context
An invalidated context might still be linked, and a function queued by
callLater might access it. A name-lookup in it would trigger
initPropertyNames, which at that point would dereference a null pointer
because we try to access the nulled engine.

Part of the issue lies in the fact that creating an IdentifierHash
requires a handle even for an always empty hash. Changing this is left
for another patch.

The crash appears in KDE's Sentry as issue
6fe76d02b7ad41aea6a0e7689a59c11e
Reproducer crafted via AI.

Most likely amends 054873dfea0ce07c94bb603edc3b7d61a6190c4f, which
caused Loader to drop the engine from the context.

Pick-to: 6.12 6.11 6.8 6.5
Task-number: QTBUG-142387
Task-number: QTBUG-104016
Change-Id: I92b0e91dea3e23d448fc4a3d3a4291cc9c7a260e
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/qml/qml/qqmlcontextdata.cpp
@@ -377,10 +377,23 @@ void QQmlContextData::addExpression(QQmlJavaScriptExpression *expression)
 
 void QQmlContextData::initPropertyNames() const
 {
-    if (m_typeCompilationUnit)
+    if (m_typeCompilationUnit) {
         m_propertyNameCache = m_typeCompilationUnit->namedObjectsPerComponent(m_componentObjectIndex);
-    else
-        m_propertyNameCache = QV4::IdentifierHash(m_engine->handle());
+    } else {
+        auto engine = m_engine;
+        if (!engine) {
+            // in some circumstances, we run into an invalidated context. In that case, we have no engine
+            // obviously, there's also no names to be found. Ideally, we'd have a special empty IdentifierHash
+            // for this which doesn't depend on an engine being available, but that currently doesn't exist.
+            // If we're evaluating, we should however still be able to find a parent context with an engine
+            for (auto ctxt = parent(); ctxt; ctxt = ctxt->parent()) {
+                if ((engine = ctxt->engine()))
+                    break;
+            }
+        }
+        Q_ASSERT(engine);
+        m_propertyNameCache = QV4::IdentifierHash(engine->handle());
+    }
     Q_ASSERT(m_propertyNameCache.isValid());
 }
 

tests/auto/qml/qqmlqt/tst_qqmlqt.cpp
@@ -20,6 +20,7 @@
 #include <QFontDatabase>
 #include <QMatrix4x4>
 #include <QQuaternion>
+#include <QRegularExpression>
 #include <QSignalSpy>
 #include <QVector2D>
 #include <QVector3D>
@@ -29,6 +30,7 @@
 #include <QtQuickTestUtils/private/qmlutils_p.h>
 #include <private/qtenvironmentvariables_p.h> // for qTzSet()
 #include <private/qqmlengine_p.h>
+#include <private/qqmlcontextdata_p.h>
 
 using namespace Qt::StringLiterals;
 
@@ -84,6 +86,7 @@ private slots:
     void resolvedUrl();
     void later_data();
     void later();
+    void laterInvalidatedContext();
     void qtObjectContents();
 
     void timeRoundtrip_data();
@@ -1436,6 +1439,52 @@ void tst_qqmlqt::later()
             QCOMPARE(root->property(qPrintable(propNames.at(i))), values.at(i));
         }
     }
+}
+
+//  a function scheduled via Qt.callLater() may have the QQmlContextData it (or one of its parent
+//  contexts) lives in invalidated before the queued call is serviced. The invalidation is
+//  artificial here, but it seems to be possible with pure QML using a Loader
+void tst_qqmlqt::laterInvalidatedContext()
+{
+    QQmlEngine engine;
+
+    // A C++-created context has no compilation unit, and - as long as no name is
+    // ever resolved against it - no property-name cache either.
+    QScopedPointer<QQmlContext> context(new QQmlContext(engine.rootContext()));
+
+    QQmlComponent component(&engine);
+    component.setData(
+            "import QtQml\n"
+            "QtObject {\n"
+            "    function arm() {\n"
+            // The closure is unguarded (its immediate scope is this CallContext,
+            // not a QmlContext, so QQmlDelayedCallQueue installs no object guard)
+            // and references a free name, forcing a lookup that walks up into the
+            // soon-to-be-invalidated parent context.
+            "        Qt.callLater(function() { return someUnresolvableName; });\n"
+            "    }\n"
+            "}\n",
+            QUrl("LaterInvalidContext.qml"));
+    QVERIFY2(component.isReady(), qPrintable(component.errorString()));
+
+    QScopedPointer<QObject> root(component.create(context.data()));
+    QVERIFY(root);
+    QMetaObject::invokeMethod(root.data(), "arm");
+
+    // Invalidate the context subtree the way QQuickLoader::clear() does: detach
+    // the engine but keep the objects and the parent-context links alive.
+    QQmlContextData::get(context.data())->clearContextRecursively();
+
+    // Service the queued Qt.callLater(). Before the fix this dereferenced the
+    // null engine in QQmlContextData::initPropertyNames(); now the lookup must
+    // fail gracefully.
+    QTest::ignoreMessage(
+            QtWarningMsg,
+            QRegularExpression(
+                    "ReferenceError: someUnresolvableName is not defined "
+                    "\\(exception occurred during delayed function evaluation\\)"));
+    QCoreApplication::processEvents();
+    // should not crash
 }
 
 void tst_qqmlqt::qtObjectContents()