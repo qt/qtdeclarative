65e76c7ac1a313488e532b8d17036c90232a3922
Marc Mutz <marc.mutz@qt.io>
2026-08-14 09:07:25 +0200

    Reduce stack use in Codegen::destructureElementList()
    The sizeof(Reference) is 88 on my machine, so a QVLA<Reference> with
    default Prealloc of 256 uses 22KiB of stack, above the limit of 20KiB
    we try to enforce with -Wframe-larger-than.
    
    Reduce the Prealloc to 32, putting the size well below the 4KiB we
    eventually want to reach to reliably hit the stack end's guard page
    even without special stack protector options.
    
    Amends e7eb542a553c75cdb917450915addb3b9e20c0db (6.7).
    
    Pick-to: 6.12 6.11 6.8
    Change-Id: Ibc5cefe2e313e0493663f661934fd1ed84fc1ddb
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>

--- src/qml/compiler/qv4codegen.cpp
+++ src/qml/compiler/qv4codegen.cpp
@@ -774,7 +774,7 @@ void Codegen::destructureElementList(const Codegen::Reference &array, PatternEle
     RegisterScope scope(this);
 
     Reference iterator = Reference::fromStackSlot(this);
-    QVarLengthArray<Reference> iteratorValues;
+    QVarLengthArray<Reference, 32> iteratorValues;
     Reference ignored;
 
     array.loadInAccumulator();
