dcaaee0f4d5ebe7b9d42038db8f2f99d3f798bea
Jani Heikkinen <jani.heikkinen@qt.io>
2026-09-11 13:55:22 +0300

    Bump version to 6.12.1
    Change-Id: I75d0dc8870e844e1bde79c96e08b9990048afa5e
    Reviewed-by: Qt Submodule Update Bot <qt_submodule_update_bot@qt-project.org>

--- examples/platforms/android/models/qtabstractlistmodel_kotlin/app/build.gradle.kts
+++ examples/platforms/android/models/qtabstractlistmodel_kotlin/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 //! [build.gradle QtBuild config]
 QtBuild {
     // Relative for Qt (Installer or MaintenanceTool) installations.
-    qtPath = file("../../../../../../../6.12.0")
+    qtPath = file("../../../../../../../6.12.1")
     projectPath = file("../../qtabstractlistmodel")
 }
 //! [build.gradle QtBuild config]
